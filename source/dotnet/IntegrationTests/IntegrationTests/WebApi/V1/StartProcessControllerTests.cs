c92d4e040fb1c29da9fd32a9eb56a290e319c6ab
Jonas Michelsen <jvm@energinet.dk>
2022-06-01 13:28:45 +0200

    Add integration test wip

---
+++ source/dotnet/IntegrationTests/IntegrationTests/WebApi/V1/StartProcessControllerTests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IntegrationTestCore.Fixtures.WebApi;
+using Xunit;
+using Xunit.Categories;
+using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace IntegrationTests.IntegrationTests.WebApi.V1;
+
+[IntegrationTest]
+[Collection(nameof(WholesaleWebApiCollectionFixture))]
+public class StartProcessControllerTests : 
+    WebApiTestBase<WholesaleWebApiFixture>,
+    IClassFixture<WholesaleWebApiFixture>,
+    IClassFixture<WebApiFactory>,
+    IAsyncLifetime
+{
+    private const string BaseUrl = "/v1/StartProcess";
+    
+    private readonly HttpClient _client;
+
+    public StartProcessControllerTests(
+        WholesaleWebApiFixture wholesaleWebApiFixture,
+        WebApiFactory factory,
+        ITestOutputHelper testOutputHelper)
+        : base(wholesaleWebApiFixture, testOutputHelper)
+    {
+        _client = factory.CreateClient();
+    }
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        _client.Dispose();
+        return Task.CompletedTask;
+    }
+    
+    [Fact]
+    public async Task GetAsync_WhenMeteringPointIdHasChargeLinks_ReturnsOkAndCorrectContentType()
+    {
+        // Act
+        var response = await _client.GetAsync($"{BaseUrl}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
