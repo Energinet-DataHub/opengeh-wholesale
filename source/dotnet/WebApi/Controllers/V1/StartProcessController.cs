b7c279c3a27142b5061d65449e29c9c569916945
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2022-06-01 15:53:57 +0200

    Add WebApi (#13)
    * Add WebApi with simple post
    
    * Add missing copyright license
    
    * Return 200 ok when post call succeeded
    
    * Move to root
    
    * Add more namespace tags
    
    * Add directory.build.props
    
    * Clean up csproj
    
    * Add signature to startprocess

---
+++ source/dotnet/WebApi/Controllers/V1/StartProcessController.cs
@@ -0,0 +1,44 @@
+// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Contracts.WholesaleProcess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Energinet.DataHub.Wholesale.WebApi.Controllers.V1;
+
+/// <summary>
+/// This controller handle starting a process of a aggregation
+/// </summary>
+[ApiController]
+[ApiVersion(Version)]
+[Route("v{version:apiVersion}/[controller]")]
+public class StartProcessController : ControllerBase
+{
+    private const string Version = "1.0";
+        
+    /// <summary>
+    /// Starts a process
+    /// </summary>
+    /// <returns>Always 200 OK</returns>
+    [HttpPost]
+    [MapToApiVersion(Version)]
+    public async Task<IActionResult> StartProcessAsync(
+        WholesaleProcessType processType,
+        [FromQuery]List<string> gridAreas,
+        DateTimeOffset periodStartDateTime,
+        DateTimeOffset periodEndDateTime)
+    {
+        return await Task.FromResult(Ok());
+    }
+}
---
+++ source/dotnet/WebApi/Program.cs
@@ -0,0 +1,47 @@
+// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(config =>
+{
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    config.AssumeDefaultVersionWhenUnspecified = true;
+    config.ReportApiVersions = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
