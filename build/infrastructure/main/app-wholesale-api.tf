febbc8277fbf9b839a9f56f36356f6386c4b4271
BjarkeMeier <hej@bjarkemeier.dk>
2022-06-15 09:28:08 +0200

    Add wholesale-api IaC

---
+++ build/infrastructure/main/app-wholesale-api.tf
@@ -0,0 +1,40 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module "app_wholesale_api" {
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=6.0.0"
+
+  name                                      = "api"
+  project_name                              = var.domain_name_short
+  environment_short                         = var.environment_short
+  environment_instance                      = var.environment_instance
+  resource_group_name                       = azurerm_resource_group.this.name
+  location                                  = azurerm_resource_group.this.location
+  app_service_plan_id                       = data.azurerm_key_vault_secret.plan_shared_id.value
+  application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
+  vnet_integration_subnet_id                = data.azurerm_key_vault_secret.snet_vnet_integrations_id.value
+  private_endpoint_subnet_id                = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
+
+  app_settings                              = {}
+
+  connection_strings                        = [
+    {
+      name  = "DB_CONNECTION_STRING"
+      type  = "SQLAzure"
+      value = local.MS_CHARGE_DB_CONNECTION_STRING
+    }
+  ]
+
+  tags                                  = azurerm_resource_group.this.tags
+}
