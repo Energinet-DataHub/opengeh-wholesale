e947202fbbb36af06c60e3cc5d7b1eca09e1147e
Lasse Ringgren Nielsen <sixxth@gmail.com>
2022-06-16 14:56:45 +0200

    push

---
+++ source/dotnet/IntegrationTests.Core/Fixtures/FunctionApp/WholesaleFunctionAppFixture.cs
@@ -0,0 +1,126 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics;
+using Energinet.DataHub.Core.FunctionApp.TestCommon;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Azurite;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Configuration;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.EventHub.ResourceProvider;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.FunctionAppHost;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.ServiceBus.ResourceProvider;
+using Energinet.DataHub.Wholesale.IntegrationEventListener.Common;
+using Energinet.DataHub.Wholesale.IntegrationTests.Core.TestCommon.Authorization;
+using Microsoft.Extensions.Configuration;
+
+namespace Energinet.DataHub.Wholesale.IntegrationTests.Core.Fixtures.FunctionApp
+{
+    public class WholesaleFunctionAppFixture : FunctionAppFixture
+    {
+        public WholesaleFunctionAppFixture()
+        {
+            AzuriteManager = new AzuriteManager();
+            IntegrationTestConfiguration = new IntegrationTestConfiguration();
+            AuthorizationConfiguration = new AuthorizationConfiguration(
+                "u002",
+                "integrationtest.local.settings.json",
+                "AZURE_SECRETS_KEYVAULT_URL");
+            // ServiceBusResourceProvider = new ServiceBusResourceProvider(
+            //     IntegrationTestConfiguration.ServiceBusConnectionString, TestLogger);
+            //
+            // EventHubResourceProvider = new EventHubResourceProvider(
+            //     IntegrationTestConfiguration.EventHubConnectionString,
+            //     new AzureResourceManagementSettings(),
+            //     TestLogger);
+        }
+
+        public AuthorizationConfiguration AuthorizationConfiguration { get; }
+
+        public TopicResource? MeteringPointCreatedTopic { get; private set; }
+
+        private AzuriteManager AzuriteManager { get; }
+
+        private IntegrationTestConfiguration IntegrationTestConfiguration { get; }
+
+        //private ServiceBusResourceProvider ServiceBusResourceProvider { get; }
+
+       // private EventHubResourceProvider EventHubResourceProvider { get; }
+
+        /// <inheritdoc/>
+        protected override void OnConfigureHostSettings(FunctionAppHostSettings hostSettings)
+        {
+            if (hostSettings == null)
+                return;
+
+            var currentDir = Environment.CurrentDirectory;
+
+            var buildConfiguration = GetBuildConfiguration();
+            hostSettings.FunctionApplicationPath = $"..\\..\\..\\..\\GreenEnergyHub.Charges.FunctionHost\\bin\\{buildConfiguration}\\net6.0"; //TODO FIX THIS
+        }
+
+        /// <inheritdoc/>
+        protected override void OnConfigureEnvironment()
+        {
+            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AppInsightsInstrumentationKey, IntegrationTestConfiguration.ApplicationInsightsInstrumentationKey);
+            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebJobsStorage, "UseDevelopmentStorage=true");
+        }
+
+        /// <inheritdoc/>
+        protected override async Task OnInitializeFunctionAppDependenciesAsync(IConfiguration localSettingsSnapshot)
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+            AzuriteManager.StartAzurite();
+
+            // var eventHubResource = await EventHubResourceProvider
+            //     .BuildEventHub("wholesaleeventhub")
+            //     .SetEnvironmentVariableToEventHubName(EnvironmentSettingNames.MasterDataEventHubName)
+            //     .CreateAsync();
+            //
+            // MeteringPointCreatedTopic = await ServiceBusResourceProvider
+            //     .BuildTopic("metering-point-created")
+            //     .SetEnvironmentVariableToTopicName(EnvironmentSettingNames.MeteringPointCreatedTopicName)
+            //     .AddSubscription("metering-point-created-sub-wholesale")
+            //     .SetEnvironmentVariableToSubscriptionName(EnvironmentSettingNames.MeteringPointCreatedSubscriptionName)
+            //     .CreateAsync();
+        }
+
+        /// <inheritdoc/>
+        protected override Task OnFunctionAppHostFailedAsync(IReadOnlyList<string> hostLogSnapshot, Exception exception)
+        {
+            if (Debugger.IsAttached)
+                Debugger.Break();
+
+            return base.OnFunctionAppHostFailedAsync(hostLogSnapshot, exception);
+        }
+
+        /// <inheritdoc/>
+        protected override async Task OnDisposeFunctionAppDependenciesAsync()
+        {
+            AzuriteManager.Dispose();
+            await Task.CompletedTask.ConfigureAwait(false);
+
+            // => Service Bus
+            // await ServiceBusResourceProvider.DisposeAsync();
+            // await EventHubResourceProvider.DisposeAsync();
+        }
+
+        private static string GetBuildConfiguration()
+        {
+#if DEBUG
+            return "Debug";
+#else
+            return "Release";
+#endif
+        }
+    }
+}
---
+++ source/dotnet/IntegrationTests/Endpoint/MeteringPointCreatedListenerEndpointTests.cs
@@ -0,0 +1,97 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Azure.Messaging.ServiceBus;
+using Energinet.DataHub.Core.FunctionApp.TestCommon;
+using Energinet.DataHub.MeteringPoints.IntegrationEventContracts;
+using Energinet.DataHub.Wholesale.IntegrationEventListener;
+using Energinet.DataHub.Wholesale.IntegrationTests.Core.Fixtures.FunctionApp;
+using Energinet.DataHub.Wholesale.IntegrationTests.Core.TestCommon.Function;
+using Google.Protobuf;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Categories;
+
+namespace Energinet.DataHub.Wholesale.IntegrationTests.Endpoint;
+
+    [IntegrationTest]
+    public class MeteringPointCreatedListenerEndpointTests
+    {
+        [Collection(nameof(WholesaleFunctionAppCollectionFixture))]
+        public class RunAsync : FunctionAppTestBase<WholesaleFunctionAppFixture>, IAsyncLifetime
+        {
+            public RunAsync(WholesaleFunctionAppFixture fixture, ITestOutputHelper testOutputHelper)
+                : base(fixture, testOutputHelper)
+            {
+            }
+
+            public Task InitializeAsync()
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task DisposeAsync()
+            {
+                return Task.CompletedTask;
+            }
+
+            [Fact]
+            public async Task When_ReceivingMeteringPointCreatedMessage_MeteringPointCreatedDtoIsSentToEventHub()
+            {
+                // Arrange
+                var message = CreateServiceBusMessage();
+
+                // Act
+                await Fixture.MeteringPointCreatedTopic!.SenderClient.SendMessageAsync(message);
+
+                // Assert
+                await FunctionAsserts.AssertHasExecutedAsync(Fixture.HostManager, nameof(MeteringPointCreatedListenerEndpoint)).ConfigureAwait(false);
+            }
+
+            private static ServiceBusMessage CreateServiceBusMessage()
+            {
+                var date = new DateTime(2021, 1, 2, 3, 4, 5, DateTimeKind.Utc);
+                var effectiveDate = Google.Protobuf.WellKnownTypes.
+                    Timestamp.FromDateTime(
+                        DateTime.SpecifyKind(
+                            new DateTime(2020, 01, 01, 0, 0, 0),
+                            DateTimeKind.Utc));
+                var r = new Random();
+                var meteringPointId = r.Next(1, 100000);
+                var message = new MeteringPointCreated
+                {
+                    MeteringPointId = Guid.NewGuid().ToString(),
+                    ConnectionState = MeteringPointCreated.Types.ConnectionState.CsNew,
+                    EffectiveDate = effectiveDate,
+                    GridAreaCode = Guid.NewGuid().ToString(),
+                    GsrnNumber = meteringPointId.ToString(),
+                    MeteringPointType = MeteringPointCreated.Types.MeteringPointType.MptConsumption,
+                    MeteringMethod = MeteringPointCreated.Types.MeteringMethod.MmPhysical,
+                    SettlementMethod = MeteringPointCreated.Types.SettlementMethod.SmFlex,
+                };
+
+                var byteArray = message.ToByteArray();
+                var serviceBusMessage = new ServiceBusMessage(byteArray)
+                {
+                    CorrelationId = Guid.NewGuid().ToString().Replace("-", string.Empty),
+                };
+                serviceBusMessage.ApplicationProperties.Add("OperationTimestamp", date.ToUniversalTime());
+                serviceBusMessage.ApplicationProperties.Add("OperationCorrelationId", "1bf1b76337f14b78badc248a3289d021");
+                serviceBusMessage.ApplicationProperties.Add("MessageVersion", 1);
+                serviceBusMessage.ApplicationProperties.Add("MessageType", "MeteringPointCreated");
+                serviceBusMessage.ApplicationProperties.Add("EventIdentification", "2542ed0d242e46b68b8b803e93ffbf7b");
+                return serviceBusMessage;
+            }
+        }
+    }
