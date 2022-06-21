590d901536dbf2b45c2be936b9530dbc97402f02
BjarkeMeier <hej@bjarkemeier.dk>
2022-06-21 14:16:15 +0200

    Add process manager

---
+++ build/infrastructure/main/func-processmanager.tf
@@ -0,0 +1,50 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+module "func_processmanager" {
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=7.0.0"
+
+  name                                      = "processmanager"
+  project_name                              = var.domain_name_short
+  environment_short                         = var.environment_short
+  environment_instance                      = var.environment_instance
+  resource_group_name                       = azurerm_resource_group.this.name
+  location                                  = azurerm_resource_group.this.location
+  vnet_integration_subnet_id                = data.azurerm_key_vault_secret.snet_vnet_integrations_id.value
+  private_endpoint_subnet_id                = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
+  app_service_plan_id                       = data.azurerm_key_vault_secret.plan_shared_id.value
+  application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
+  log_analytics_workspace_id                = data.azurerm_key_vault_secret.log_shared_id.value
+  always_on                                 = true
+  health_check_path                         = "/api/monitor/ready"
+  health_check_alert_action_group_id        = data.azurerm_key_vault_secret.primary_action_group_id.value
+  health_check_alert_enabled                = var.enable_health_check_alerts
+  dotnet_framework_version                  = "6"
+  use_dotnet_isolated_runtime               = true
+
+  connection_strings                        = [
+    {
+      name  = "DB_CONNECTION_STRING"
+      type  = "SQLAzure"
+      value = local.DB_CONNECTION_STRING
+    }
+  ]
+
+  app_settings                              = {
+    SERVICE_BUS_LISTENER_CONNECTION_STRING           = module.sb_wholesale.primary_connection_strings["listen"]
+    SERVICE_BUS_MANAGE_CONNECTION_STRING             = module.sb_wholesale.primary_connection_strings["manage"]
+    PROCESS_COMPLETED_TOPIC_NAME                     = "completed-process"
+  }
+
+  tags                                  = azurerm_resource_group.this.tags
+}
