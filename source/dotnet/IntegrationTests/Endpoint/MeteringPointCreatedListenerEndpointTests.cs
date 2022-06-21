01a8ead4c541b985209b0d546f151489643dc3fb
Lasse Ringgren Nielsen <sixxth@gmail.com>
2022-06-21 15:05:42 +0200

    xunit.catagories back in

--- source/dotnet/IntegrationTests/Endpoint/MeteringPointCreatedListenerEndpointTests.cs
+++ source/dotnet/IntegrationTests/Endpoint/MeteringPointCreatedListenerEndpointTests.cs
@@ -27,9 +27,11 @@ using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 using Xunit;
 using Xunit.Abstractions;
+using Xunit.Categories;
 
 namespace Energinet.DataHub.Wholesale.IntegrationTests.Endpoint;
 
+    [IntegrationTest]
     public class MeteringPointCreatedListenerEndpointTests
     {
         [Collection(nameof(WholesaleFunctionAppCollectionFixture))]
