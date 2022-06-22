8f146aa5b024a1817c09c5b3c63bdfbc69467e6c
Jonas Michelsen <jvm@energinet.dk>
2022-06-22 10:44:58 +0200

    Update kv-shared-access-policies.tf

--- build/infrastructure/main/kv-shared-access-policies.tf
+++ build/infrastructure/main/kv-shared-access-policies.tf
@@ -31,3 +31,10 @@ module "kv_shared_access_policy_func_processmanager" {
   key_vault_id              = data.azurerm_key_vault.kv_shared_resources.id
   app_identity              = module.func_processmanager.identity.0
 }
+
+module "kv_shared_access_policy_func_processmanager" {
+  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-access-policy?ref=6.1.0"
+
+  key_vault_id              = data.azurerm_key_vault.kv_shared_resources.id
+  app_identity              = module.func_sender.identity.0
+}
