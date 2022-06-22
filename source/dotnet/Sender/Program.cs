e22834bdae1fed9b2b423d99954489adde205a1d
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2022-06-22 12:59:58 +0200

    feat: Sender function (#81)
    * Create SenderFunctionApp 
    * Create func-sender.tf
    * Update kv-shared-access-policies.tf
    * Update action.yml
    * Add Program
    * Update kv-shared-access-policies.tf

---
+++ source/dotnet/Sender/Program.cs
@@ -0,0 +1,73 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.App.Common.Abstractions.IntegrationEventContext;
+using Energinet.DataHub.Core.App.Common.Diagnostics.HealthChecks;
+using Energinet.DataHub.Core.App.FunctionApp.Diagnostics.HealthChecks;
+using Energinet.DataHub.Core.App.FunctionApp.Middleware;
+using Energinet.DataHub.Core.App.FunctionApp.Middleware.CorrelationId;
+using Energinet.DataHub.Core.JsonSerialization;
+using Energinet.DataHub.Wholesale.Infrastructure.Core;
+using Energinet.DataHub.Wholesale.Sender.Monitor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Energinet.DataHub.Wholesale.Sender;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        using var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults(builder =>
+            {
+                builder.UseMiddleware<CorrelationIdMiddleware>();
+                builder.UseMiddleware<FunctionTelemetryScopeMiddleware>();
+                builder.UseMiddleware<IntegrationEventMetadataMiddleware>();
+            })
+            .ConfigureServices(Middlewares)
+            .ConfigureServices(Infrastructure)
+            .ConfigureServices(HealthCheck)
+            .Build();
+
+        await host.RunAsync().ConfigureAwait(false);
+    }
+
+    private static void Middlewares(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<ICorrelationContext, CorrelationContext>();
+        serviceCollection.AddScoped<CorrelationIdMiddleware>();
+        serviceCollection.AddScoped<FunctionTelemetryScopeMiddleware>();
+        serviceCollection.AddScoped<IIntegrationEventContext, IntegrationEventContext>();
+        serviceCollection.AddScoped<IntegrationEventMetadataMiddleware>();
+    }
+
+    private static void Infrastructure(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddLogging();
+        serviceCollection.AddApplicationInsightsTelemetryWorkerService(
+            EnvironmentVariableHelper.GetEnvVariable(EnvironmentSettingNames.AppInsightsInstrumentationKey));
+        serviceCollection.AddSingleton<IJsonSerializer, JsonSerializer>();
+    }
+
+    private static void HealthCheck(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<IHealthCheckEndpointHandler, HealthCheckEndpointHandler>();
+        serviceCollection.AddScoped<HealthCheckEndpoint>();
+
+        serviceCollection
+            .AddHealthChecks()
+            .AddLiveCheck();
+    }
+}
