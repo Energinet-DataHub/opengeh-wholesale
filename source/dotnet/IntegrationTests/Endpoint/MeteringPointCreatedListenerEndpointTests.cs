53f5cf0629677160025924b6f6391ff6eddb307f
Alex <77341673+defectiveAi@users.noreply.github.com>
2022-06-22 11:48:53 +0200

    feat: Add Health Endpoint integration tests. (#85)

--- source/dotnet/IntegrationTests/Endpoint/MeteringPointCreatedListenerEndpointTests.cs
+++ source/dotnet/IntegrationTests/Endpoint/MeteringPointCreatedListenerEndpointTests.cs
@@ -31,78 +31,78 @@ using Xunit.Abstractions;
 
 namespace Energinet.DataHub.Wholesale.IntegrationTests.Endpoint;
 
-    public class MeteringPointCreatedListenerEndpointTests
+public class MeteringPointCreatedListenerEndpointTests
+{
+    [Collection(nameof(WholesaleFunctionAppCollectionFixture))]
+    public class RunAsync : FunctionAppTestBase<WholesaleFunctionAppFixture>, IAsyncLifetime
     {
-        [Collection(nameof(WholesaleFunctionAppCollectionFixture))]
-        public class RunAsync : FunctionAppTestBase<WholesaleFunctionAppFixture>, IAsyncLifetime
+        public RunAsync(WholesaleFunctionAppFixture fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture, testOutputHelper)
         {
-            public RunAsync(WholesaleFunctionAppFixture fixture, ITestOutputHelper testOutputHelper)
-                : base(fixture, testOutputHelper)
+        }
+
+        public Task InitializeAsync()
+        {
+            Fixture.EventHubListener.Reset();
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public async Task When_ReceivingMeteringPointCreatedMessage_MeteringPointCreatedDtoIsSentToEventHub()
+        {
+            // Arrange
+            using var whenAllEvent = await Fixture.EventHubListener
+                .WhenAny()
+                .VerifyCountAsync(1)
+                .ConfigureAwait(false);
+
+            var effectiveDate = Timestamp.FromDateTime(
+                DateTime.SpecifyKind(
+                    new DateTime(2020, 01, 01, 0, 0, 0),
+                    DateTimeKind.Utc));
+
+            var meteringPointCreatedEvent = CreateMeteringPointCreatedEvent(effectiveDate);
+            var operationTimestamp = new DateTime(2021, 1, 2, 3, 4, 5, DateTimeKind.Utc);
+            var message = ServiceBusTestMessage.Create(meteringPointCreatedEvent.ToByteArray(), operationTimestamp);
+            var jsonSerializer = new JsonSerializer();
+
+            // Act
+            await Fixture.MeteringPointCreatedTopic.SenderClient.SendMessageAsync(message);
+
+            // Assert
+            await FunctionAsserts.AssertHasExecutedAsync(Fixture.HostManager, nameof(MeteringPointCreatedListenerEndpoint)).ConfigureAwait(false);
+
+            var allReceived = whenAllEvent.Wait(TimeSpan.FromSeconds(5));
+            allReceived.Should().BeTrue();
+
+            // Only one event is expected
+            var actual = jsonSerializer.Deserialize<MeteringPointCreatedDto>(
+                Fixture.EventHubListener
+                    .ReceivedEvents.Single()
+                    .Data.ToString());
+            actual.EffectiveDate.Should().Be(effectiveDate.ToInstant());
+        }
+
+        private static MeteringPointCreated CreateMeteringPointCreatedEvent(Timestamp effectiveDate)
+        {
+            var r = new Random();
+            var meteringPointId = r.Next(1, 100000);
+            return new MeteringPointCreated
             {
-            }
-
-            public Task InitializeAsync()
-            {
-                Fixture.EventHubListener.Reset();
-                return Task.CompletedTask;
-            }
-
-            public Task DisposeAsync()
-            {
-                return Task.CompletedTask;
-            }
-
-            [Fact]
-            public async Task When_ReceivingMeteringPointCreatedMessage_MeteringPointCreatedDtoIsSentToEventHub()
-            {
-                // Arrange
-                using var whenAllEvent = await Fixture.EventHubListener
-                    .WhenAny()
-                    .VerifyCountAsync(1)
-                    .ConfigureAwait(false);
-
-                var effectiveDate = Timestamp.FromDateTime(
-                    DateTime.SpecifyKind(
-                        new DateTime(2020, 01, 01, 0, 0, 0),
-                        DateTimeKind.Utc));
-
-                var meteringPointCreatedEvent = CreateMeteringPointCreatedEvent(effectiveDate);
-                var operationTimestamp = new DateTime(2021, 1, 2, 3, 4, 5, DateTimeKind.Utc);
-                var message = ServiceBusTestMessage.Create(meteringPointCreatedEvent.ToByteArray(), operationTimestamp);
-                var jsonSerializer = new JsonSerializer();
-
-                // Act
-                await Fixture.MeteringPointCreatedTopic.SenderClient.SendMessageAsync(message);
-
-                // Assert
-                await FunctionAsserts.AssertHasExecutedAsync(Fixture.HostManager, nameof(MeteringPointCreatedListenerEndpoint)).ConfigureAwait(false);
-
-                var allReceived = whenAllEvent.Wait(TimeSpan.FromSeconds(5));
-                allReceived.Should().BeTrue();
-
-                // Only one event is expected
-                var actual = jsonSerializer.Deserialize<MeteringPointCreatedDto>(
-                    Fixture.EventHubListener
-                        .ReceivedEvents.Single()
-                        .Data.ToString());
-                actual.EffectiveDate.Should().Be(effectiveDate.ToInstant());
-            }
-
-            private static MeteringPointCreated CreateMeteringPointCreatedEvent(Timestamp effectiveDate)
-            {
-                var r = new Random();
-                var meteringPointId = r.Next(1, 100000);
-                return new MeteringPointCreated
-                {
-                    MeteringPointId = Guid.NewGuid().ToString(),
-                    ConnectionState = MeteringPointCreated.Types.ConnectionState.CsNew,
-                    EffectiveDate = effectiveDate,
-                    GridAreaCode = Guid.NewGuid().ToString(),
-                    GsrnNumber = meteringPointId.ToString(),
-                    MeteringPointType = MeteringPointCreated.Types.MeteringPointType.MptConsumption,
-                    MeteringMethod = MeteringPointCreated.Types.MeteringMethod.MmPhysical,
-                    SettlementMethod = MeteringPointCreated.Types.SettlementMethod.SmFlex,
-                };
-            }
+                MeteringPointId = Guid.NewGuid().ToString(),
+                ConnectionState = MeteringPointCreated.Types.ConnectionState.CsNew,
+                EffectiveDate = effectiveDate,
+                GridAreaCode = Guid.NewGuid().ToString(),
+                GsrnNumber = meteringPointId.ToString(),
+                MeteringPointType = MeteringPointCreated.Types.MeteringPointType.MptConsumption,
+                MeteringMethod = MeteringPointCreated.Types.MeteringMethod.MmPhysical,
+                SettlementMethod = MeteringPointCreated.Types.SettlementMethod.SmFlex,
+            };
         }
     }
+}
