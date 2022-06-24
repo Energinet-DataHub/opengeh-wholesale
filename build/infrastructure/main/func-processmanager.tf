9656cb87da6d1efdd8a37cd5106d7633ea73cbaa
BjarkeMeier <hej@bjarkemeier.dk>
2022-06-24 22:47:49 +0200

    Revert name fix

--- build/infrastructure/main/func-processmanager.tf
+++ build/infrastructure/main/func-processmanager.tf
@@ -14,7 +14,7 @@
 module "func_processmanager" {
   source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=7.0.0"
 
-  name                                      = "procmanager"
+  name                                      = "processmanager"
   project_name                              = var.domain_name_short
   environment_short                         = var.environment_short
   environment_instance                      = var.environment_instance
