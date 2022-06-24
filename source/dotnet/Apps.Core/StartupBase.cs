0a424aae829a98e289017946905cf2768bb96bde
Alexei Mihalchuk <AIU@energinet.dk>
2022-06-24 12:15:51 +0200

    Remove old using.

--- source/dotnet/Apps.Core/StartupBase.cs
+++ source/dotnet/Apps.Core/StartupBase.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Apps.Core;
 using Energinet.DataHub.Core.App.Common.Abstractions.IntegrationEventContext;
 using Energinet.DataHub.Core.App.FunctionApp.Middleware;
 using Energinet.DataHub.Core.App.FunctionApp.Middleware.CorrelationId;
