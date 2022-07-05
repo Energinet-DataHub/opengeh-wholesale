56a53c16f7a90b5fc907a35ec79b085ef4ae253c
Jonas Michelsen <jvm@energinet.dk>
2022-07-05 12:09:39 +0000

    update databricks provider

--- build/databricks_jobs/main/providers.tf
+++ build/databricks_jobs/main/providers.tf
@@ -16,7 +16,7 @@ terraform {
 
   required_providers {
     databricks = {
-      source = "databrickslabs/databricks"
+      source = "databricks/databricks"
       version = "0.5.1"
     }
     azurerm = "=3.9.0"
