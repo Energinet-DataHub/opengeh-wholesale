b13c329ebf5a6be237b90c2bc36aaf14b3361cd1
Jonas Michelsen <jvm@energinet.dk>
2022-07-07 07:51:32 +0000

    black

--- source/databricks/package/integration_events_persister_streaming.py
+++ source/databricks/package/integration_events_persister_streaming.py
@@ -13,8 +13,9 @@
 # limitations under the License.
 
 import sys
-sys.path.append(r'/workspaces/opengeh-wholesale/source/databricks')
-sys.path.append(r'/opt/conda/lib/python3.8/site-packages')
+
+sys.path.append(r"/workspaces/opengeh-wholesale/source/databricks")
+sys.path.append(r"/opt/conda/lib/python3.8/site-packages")
 
 import configargparse
 
@@ -22,12 +23,15 @@ from package import integration_events_persister, initialize_spark
 
 
 def start():
-    p = configargparse.ArgParser(description='Timeseries events stream ingestor', formatter_class=configargparse.ArgumentDefaultsHelpFormatter)
-    p.add('--data-storage-account-name', type=str, required=True)
-    p.add('--data-storage-account-key', type=str, required=True)
-    p.add('--event-hub-connectionstring', type=str, required=True)
-    p.add('--integration-events-path', type=str, required=True)
-    p.add('--integration-events-checkpoint-path', type=str, required=True)
+    p = configargparse.ArgParser(
+        description="Timeseries events stream ingestor",
+        formatter_class=configargparse.ArgumentDefaultsHelpFormatter,
+    )
+    p.add("--data-storage-account-name", type=str, required=True)
+    p.add("--data-storage-account-key", type=str, required=True)
+    p.add("--event-hub-connectionstring", type=str, required=True)
+    p.add("--integration-events-path", type=str, required=True)
+    p.add("--integration-events-checkpoint-path", type=str, required=True)
 
     args, unknown_args = p.parse_known_args()
 
@@ -37,8 +41,16 @@ def start():
     integration_events_checkpoint_path = f"{args.integration_events_checkpoint_path}"
 
     input_configuration = {}
-    input_configuration["eventhubs.connectionString"] = spark.sparkContext._gateway.jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(f"{args.event_hub_connectionstring}")
-    streamingDF = spark.readStream.format("eventhubs").options(**input_configuration).load()
+    input_configuration[
+        "eventhubs.connectionString"
+    ] = spark.sparkContext._gateway.jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(
+        f"{args.event_hub_connectionstring}"
+    )
+    streamingDF = (
+        spark.readStream.format("eventhubs").options(**input_configuration).load()
+    )
 
     # start the timeseries persister job
-    integration_events_persister(streamingDF, integration_events_checkpoint_path, integration_events_path)
+    integration_events_persister(
+        streamingDF, integration_events_checkpoint_path, integration_events_path
+    )
