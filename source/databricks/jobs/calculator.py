bf7f0542b716ad66cfd00cbfb02c954d6f6c5de3
Alexei Mihalchuk <AIU@energinet.dk>
2022-07-11 10:47:56 +0200

    Update DB script.

--- source/databricks/jobs/calculator.py
+++ source/databricks/jobs/calculator.py
@@ -34,6 +34,10 @@ def run(
 if __name__ == "__main__":
     try:
         run()
+
+    # TODO: Try to remove when using wheel directly.
+    # Clicks uses sys.exit(0) when job completes, which is intepreted as an error.
+    # https://github.com/ipython/ipython/issues/12831#issuecomment-1064866151
     except SystemExit as e:
         if e.code != 0:
             raise
