d0abde934c263bd34653d4141032ab35d10d390d
Alexei Mihalchuk <AIU@energinet.dk>
2022-07-20 14:38:16 +0200

    Updated UT.

--- source/dotnet/Tests/Sender/Infrastructure/Services/DocumentFactoryTests.cs
+++ source/dotnet/Tests/Sender/Infrastructure/Services/DocumentFactoryTests.cs
@@ -16,6 +16,7 @@ using AutoFixture.Xunit2;
 using Energinet.DataHub.Core.TestCommon.AutoFixture.Attributes;
 using Energinet.DataHub.MessageHub.Client.Storage;
 using Energinet.DataHub.MessageHub.Model.Model;
+using Energinet.DataHub.Wholesale.Sender.Infrastructure;
 using Energinet.DataHub.Wholesale.Sender.Infrastructure.Persistence.Processes;
 using Energinet.DataHub.Wholesale.Sender.Infrastructure.Services;
 using FluentAssertions;
@@ -39,7 +40,24 @@ public class DocumentFactoryTests
     <cim:receiver_MarketParticipant.marketRole.type>MDR</cim:receiver_MarketParticipant.marketRole.type>
     <cim:createdDateTime>2022-07-04T08:05:30Z</cim:createdDateTime>
     <cim:Series>
-        <!-- content will be added in future releases -->
+	    <cim:mRID>1B8E673E-DBBD-4611-87A9-C7154937786A</cim:mRID>
+		<cim:version>1</cim:version>
+        <cim:marketEvaluationPoint.type>E18</cim:marketEvaluationPoint.type>
+        <cim:meteringGridArea_Domain.mRID codingScheme=""NDK"">805</cim:meteringGridArea_Domain.mRID>
+		<cim:product>8716867000030</cim:product>
+		<cim:quantity_Measure_Unit.name>KWH</cim:quantity_Measure_Unit.name>
+            <cim:Period>
+                <cim:resolution>PT15M</cim:resolution>
+		        <cim:timeInterval>
+				    <cim:start>2022-06-30T22:00:00Z</cim:start>
+				    <cim:end>2022-07-01T22:00:00Z</cim:end>
+			     </cim:timeInterval>
+                 <cim:Point>
+		             <cim:position>1</cim:position>
+                     <cim:quantity>2</cim:quantity>
+                     
+		         </cim:Point>
+            </cim:Period>
     </cim:Series>
 </cim:NotifyAggregatedMeasureData_MarketDocument>";
 
@@ -51,7 +69,9 @@ public class DocumentFactoryTests
     public async Task CreateAsync_ReturnsRsm014(
         DataBundleRequestDto request,
         Guid anyNotificationId,
+        [Frozen] Mock<ICalculatedResultReader> calculatedResultReaderMock,
         [Frozen] Mock<IDocumentIdGenerator> documentIdGeneratorMock,
+        [Frozen] Mock<ISeriesIdGenerator> seriesIdGeneratorMock,
         [Frozen] Mock<IProcessRepository> processRepositoryMock,
         [Frozen] Mock<IStorageHandler> storageHandlerMock,
         [Frozen] Mock<NodaTime.IClock> clockMock,
@@ -62,10 +82,20 @@ public class DocumentFactoryTests
             .Setup(generator => generator.Create())
             .Returns("f0de3417-71ce-426e-9001-12600da9102a");
 
+        seriesIdGeneratorMock
+            .Setup(generator => generator.Create())
+            .Returns("1B8E673E-DBBD-4611-87A9-C7154937786A");
+
         var anyGridAreaCode = "805";
         processRepositoryMock
             .Setup(repository => repository.GetAsync(It.IsAny<MessageHubReference>()))
             .ReturnsAsync((MessageHubReference messageHubRef) => new Process(messageHubRef, anyGridAreaCode, Guid.NewGuid()));
+
+        var point = new PointDto(1, "2", string.Empty);
+
+        calculatedResultReaderMock
+            .Setup(x => x.ReadResultAsync(It.IsAny<Process>()))
+            .ReturnsAsync(new BalanceFixingResultDto(new[] { point }));
 
         storageHandlerMock
             .Setup(handler => handler.GetDataAvailableNotificationIdsAsync(request))
@@ -73,13 +103,15 @@ public class DocumentFactoryTests
 
         const string expectedIsoString = "2022-07-04T08:05:30Z";
         var instant = InstantPattern.General.Parse(expectedIsoString).Value;
-        clockMock.Setup(clock => clock.GetCurrentInstant())
+        clockMock
+            .Setup(clock => clock.GetCurrentInstant())
             .Returns(instant);
 
         using var outStream = new MemoryStream();
 
         // Act
         await sut.CreateAsync(request, outStream);
+        outStream.Position = 0;
 
         // Assert
         using var stringReader = new StreamReader(outStream);
