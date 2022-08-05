56eb7e28c9d04a25ef70d08b0f05f9b14d82e1fb
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2022-08-05 06:29:22 -0700

    Update balance-fixing-poc.py

--- source/notebooks/balance-fixing-poc.py
+++ source/notebooks/balance-fixing-poc.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# COMMAND ----------
-
 # Databricks notebook source
 # What has changed with issue #32?
 # - Store event data in the body column
