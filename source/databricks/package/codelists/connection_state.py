3b5b6b1f2a3dafa2f50e7ced1933f9e658b43a9c
BjarkeMeier <hej@bjarkemeier.dk>
2022-08-10 11:59:43 +0200

    Clean up code lists

--- source/databricks/package/codelists/connection_state.py
+++ source/databricks/package/codelists/connection_state.py
@@ -11,5 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .resolution import Resolution
-from .connection_state import ConnectionState
+
+
+class ConnectionState:
+    new = 1
+    connected = 2
+    disconnected = 3
+    closedDown = 4
