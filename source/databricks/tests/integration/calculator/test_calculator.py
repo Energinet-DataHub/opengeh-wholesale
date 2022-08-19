16d2e51de25b66228ec271eb6c64c4bac9908237
Lasse Ringgren Nielsen <sixxth@gmail.com>
2022-08-19 14:37:30 +0200

    Revert "Merge branch 'bjarke/calculate-balance-fixing-total-production' of https://github.com/Energinet-DataHub/opengeh-wholesale into bjarke/calculate-balance-fixing-total-production"
    This reverts commit 4881fdaec16ef7ba4e4a450c7f8069ed508812da, reversing
    changes made to 6a2a32179feb5fac6c45754e5e86aae69409aefe.

--- source/databricks/tests/integration/calculator/test_calculator.py
+++ source/databricks/tests/integration/calculator/test_calculator.py
@@ -85,3 +85,90 @@ def test_calculator_job_accepts_parameters_from_process_manager(
 
     # Assert
     assert exit_code == 0, "Calculator job failed to accept provided input arguments"
+
+
+def test_calculator_job_creates_file(
+    json_test_files, databricks_path, delta_lake_path, source_path
+):
+    spark.read.json(f"{json_test_files}/integration_events.json").write.mode(
+        "overwrite"
+    ).parquet(f"{delta_lake_path}/parquet_test_files/integration_events")
+    spark.read.json(f"{json_test_files}/time_series_points.json").write.mode(
+        "overwrite"
+    ).parquet(f"{delta_lake_path}/parquet_test_files/time_series_points")
+
+    # Arrange
+    process_manager_parameters = _get_process_manager_parameters(
+        f"{source_path}/contracts/calculation-job-parameters-reference.txt"
+    )
+    python_parameters = [
+        "python",
+        f"{databricks_path}/package/calculator_job_v2_draft.py",
+        "--data-storage-account-name",
+        "foo",
+        "--data-storage-account-key",
+        "foo",
+        "--integration-events-path",
+        f"{delta_lake_path}/parquet_test_files/integration_events",
+        "--time-series-points-path",
+        f"{delta_lake_path}/parquet_test_files/time_series_points",
+        "--process-results-path",
+        f"{delta_lake_path}/result",
+    ]
+    python_parameters.extend(process_manager_parameters)
+
+    # Act
+    exit_code = subprocess.call(python_parameters)
+
+    # Assert
+    assert 1 == 1, "Calculator job failed write files"
+
+
+"""
+def test_calculator_job_creates_file(
+    spark, delta_lake_path, find_first_file, json_lines_reader
+):
+    batchId = 1234
+    process_results_path = f"{delta_lake_path}/results"
+
+    raw_integration_events_df = spark.read.format("json").load(integration_events_path)
+
+    calculator_job(spark, raw_integration_events_df, process_results_path, batchId)
+
+    jsonFile = find_first_file(
+        f"{delta_lake_path}/results/batch_id={batchId}/grid_area=805", "part-*.json"
+    )
+
+    result = json_lines_reader(jsonFile)
+    assert len(result) > 0, "Could not verify created json file."
+"""
+
+
+""".devcontainer/
+def test__when_stored_time_equals_snapshot_datetime__returns_grid_area(
+    grid_area_df_factory,
+):
+    # Arrange
+    raw_integration_events_df = grid_area_df_factory(stored_time=first_of_june)
+
+    # Act
+    actual_df = _get_grid_areas_df(
+        raw_integration_events_df, [grid_area_code], snapshot_datetime=first_of_june
+    )
+
+    # Assert
+    assert actual_df.count() == 1
+
+
+def test__when_stored_after_snapshot_time__throws_because_grid_area_not_found(
+    grid_area_df_factory,
+):
+    # Arrange
+    raw_integration_events_df = grid_area_df_factory(stored_time=second_of_june)
+
+    # Act and assert exception
+    with pytest.raises(Exception, match=r".* grid areas .*"):
+        _get_grid_areas_df(
+            raw_integration_events_df, [grid_area_code], snapshot_datetime=first_of_june
+        )
+"""
