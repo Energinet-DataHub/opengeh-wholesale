3c3d9407570b580f144916f40c3e9ef2aac2398f
Alexei Mihalchuk <AIU@energinet.dk>
2022-08-23 07:44:50 +0000

    Cleanup testfile.

--- source/databricks/tests/integration/test_codelists.py
+++ source/databricks/tests/integration/test_codelists.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import shutil
-import subprocess
 import pytest
 from package.codelists import ConnectionState, MeteringPointType, Resolution
 from tests.contract_utils import assert_contract_matches_literal
