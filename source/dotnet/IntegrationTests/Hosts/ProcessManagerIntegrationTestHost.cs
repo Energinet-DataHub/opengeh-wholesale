1d945293ff0e99eef3592dc721d1db96c3c292bb
Alex <77341673+defectiveAi@users.noreply.github.com>
2022-08-24 11:23:27 +0200

    test: Add integration tests for cancelled batches. (#216)

---
+++ source/dotnet/IntegrationTests/Hosts/ProcessManagerIntegrationTestHost.cs
@@ -0,0 +1,74 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Azure.Messaging.ServiceBus;
+using Energinet.DataHub.Core.App.FunctionApp.Middleware.CorrelationId;
+using Energinet.DataHub.Wholesale.IntegrationTests.Mock;
+using Energinet.DataHub.Wholesale.ProcessManager;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace Energinet.DataHub.Wholesale.IntegrationTests.Hosts;
+
+public sealed class ProcessManagerIntegrationTestHost : IDisposable
+{
+    private readonly IHost _processManagerHost;
+
+    private ProcessManagerIntegrationTestHost(IHost processManagerHost)
+    {
+        _processManagerHost = processManagerHost;
+    }
+
+    public static Task<ProcessManagerIntegrationTestHost> CreateAsync(
+        Action<IServiceCollection>? serviceConfiguration = default)
+    {
+        ConfigureEnvironmentVars();
+
+        var hostBuilder = Program
+            .BuildAppHost()
+            .ConfigureServices(ConfigureServices);
+
+        if (serviceConfiguration != null)
+        {
+            hostBuilder = hostBuilder.ConfigureServices(serviceConfiguration);
+        }
+
+        return Task.FromResult(new ProcessManagerIntegrationTestHost(hostBuilder.Build()));
+    }
+
+    public AsyncServiceScope BeginScope()
+    {
+        return _processManagerHost.Services.CreateAsyncScope();
+    }
+
+    public void Dispose()
+    {
+        _processManagerHost.Dispose();
+    }
+
+    private static void ConfigureEnvironmentVars()
+    {
+        Environment.SetEnvironmentVariable(EnvironmentSettingNames.AppInsightsInstrumentationKey, "fake_value");
+        Environment.SetEnvironmentVariable(EnvironmentSettingNames.ServiceBusSendConnectionString, "fake_value");
+        Environment.SetEnvironmentVariable(EnvironmentSettingNames.ServiceBusManageConnectionString, "fake_value");
+        Environment.SetEnvironmentVariable(EnvironmentSettingNames.ProcessCompletedTopicName, "fake_value");
+    }
+
+    private static void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.Replace(ServiceDescriptor.Singleton<ServiceBusClient, MockedServiceBusClient>());
+        serviceCollection.Replace(ServiceDescriptor.Scoped<ICorrelationContext, MockedCorrelationContext>());
+    }
+}
