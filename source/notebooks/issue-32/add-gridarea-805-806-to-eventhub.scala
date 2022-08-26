4cfd851529a5fb65fc6dddd5ab9bad4e46180c52
Finn Skødt Toft <fkt@energinet.dk>
2022-08-26 12:50:43 +0200

    Removed licene for test

--- source/notebooks/issue-32/add-gridarea-805-806-to-eventhub.scala
+++ source/notebooks/issue-32/add-gridarea-805-806-to-eventhub.scala
@@ -1,18 +1,4 @@
 // Databricks notebook source
-// Copyright 2020 Energinet DataHub A/S
-//
-// Licensed under the Apache License, Version 2.0 (the "License2");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 //addd com.microsoft.azure:azure-eventhubs-spark_2.12:2.3.22 to cluster
 import org.apache.spark.sql.functions._
 import spark.implicits._
