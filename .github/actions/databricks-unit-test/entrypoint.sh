da797f5d3733c7be8a37359b5a8e15c2bfdf41a2
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-09-20 15:33:54 +0200

    .

--- .github/actions/databricks-unit-test/entrypoint.sh
+++ .github/actions/databricks-unit-test/entrypoint.sh
@@ -27,7 +27,7 @@ export PYSPARK_DRIVER_PYTHON=/opt/conda/bin/python
 # python ../setup.py install
 # python coverage-threshold install
 #pip install coverage-threshold
-coverage run --branch -m pytest -n 2
+coverage run --branch -m pytest
 # Create data for threshold evaluation
 coverage json
 # Create human reader friendly HTML report
