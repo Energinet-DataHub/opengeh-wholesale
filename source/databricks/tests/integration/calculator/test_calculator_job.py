7de6b597633f36e60e37d24baa7d4c9b523e06cb
Lasse Ringgren Nielsen <sixxth@gmail.com>
2022-09-21 12:07:43 +0200

    test__creates_csv_per_resolution_per_grid_area

--- source/databricks/tests/integration/calculator/test_calculator_job.py
+++ source/databricks/tests/integration/calculator/test_calculator_job.py
@@ -325,8 +325,28 @@ def test__creates_quarter_csv_with_expected_columns_names(
     ]
 
 
-def test__creates_csv_per_resolution_per_grid_area():
-    raise Exception("todo")
+def test__creates_csv_per_resolution_per_grid_area(
+    spark, test_data, test_data_job_parameters, databricks_path, data_lake_path
+):
+    # Act
+    start_calculator(spark, test_data_job_parameters)
+
+    # Assert
+    basis_data_805 = spark.read.option("header", "true").csv(
+        f"{data_lake_path}/results/basis-data/batch_id=1/time-series-quarter/grid_area=805"
+    )
+
+    basis_data_806 = spark.read.option("header", "true").csv(
+        f"{data_lake_path}/results/basis-data/batch_id=1/time-series-quarter/grid_area=806"
+    )
+
+    assert (
+        basis_data_805.count() >= 1
+    ), "Calculator job failed to write basis data files for grid area 805"
+
+    assert (
+        basis_data_806.count() >= 1
+    ), "Calculator job failed to write basis data files for grid area 806"
 
 
 def test__creates_csv_with_rows():
