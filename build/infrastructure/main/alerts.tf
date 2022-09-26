26d70c48da3dec0989396ecdb02dba6794f6f697
Finn Skødt Toft <fkt@energinet.dk>
2022-09-26 10:17:45 +0200

    Update alerts.tf

--- build/infrastructure/main/alerts.tf
+++ build/infrastructure/main/alerts.tf
@@ -11,26 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-resource "azurerm_monitor_action_group" "wholesales" {
-  name                      = "ag-wholesales-${lower(var.environment_short)}-${lower(var.environment_instance)}"
-  resource_group_name       = azurerm_resource_group.this.name
-  short_name                = "ag-ws-${lower(var.environment_short)}-${lower(var.environment_instance)}"
-
-  email_receiver {
-    name                    = "Alerts-Wholesales-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
-    email_address           = "0e195df3.energinet.onmicrosoft.com@emea.teams.ms"
-    use_common_alert_schema = true
-  }
-}
-
 resource "azurerm_monitor_scheduled_query_rules_alert" "wholesales_databricks_job_alert" {
   name                      = "alert-wholesales-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
   location                  = azurerm_resource_group.this.location
   resource_group_name       = var.shared_resources_resource_group_name
 
   action {
-    action_group            = [azurerm_monitor_action_group.wholesales.id]
+    action_group = [data.azurerm_key_vault_secret.primary_action_group_id.value]
   }
+  
   data_source_id            = data.azurerm_key_vault_secret.log_shared_id.value
   description               = "One or more databricks jobs has failed"
   enabled                   = true
