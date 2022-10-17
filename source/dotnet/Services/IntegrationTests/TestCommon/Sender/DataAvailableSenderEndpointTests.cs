a67f7c610ad76fef0f8082869cf77c9ad490cf8b
Lasse Ringgren Nielsen <sixxth@gmail.com>
2022-10-17 14:58:42 +0200

    moved testcommon tests to testcommon

---
+++ source/dotnet/Services/IntegrationTests/TestCommon/Sender/DataAvailableSenderEndpointTests.cs
@@ -0,0 +1,77 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.FunctionApp.TestCommon;
+using Energinet.DataHub.Core.TestCommon.AutoFixture.Attributes;
+using Energinet.DataHub.Wholesale.Contracts.WholesaleProcess;
+using Energinet.DataHub.Wholesale.IntegrationTests.TestCommon.Fixture;
+using Energinet.DataHub.Wholesale.IntegrationTests.TestCommon.Fixture.FunctionApp;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Energinet.DataHub.Wholesale.IntegrationTests.TestCommon.Sender;
+
+public class DataAvailableSenderEndpointTests
+{
+    [Collection(nameof(SenderFunctionAppCollectionFixture))]
+    public class RunAsync : FunctionAppTestBase<SenderFunctionAppFixture>, IAsyncLifetime
+    {
+        public RunAsync(SenderFunctionAppFixture fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture, testOutputHelper)
+        {
+        }
+
+        public Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            Fixture.HostManager.ClearHostLog();
+            Fixture.MessageHubMock.Clear();
+            return Task.CompletedTask;
+        }
+
+        [Theory(Skip = "There is currently no way to test DataLake containers.")]
+        [InlineAutoMoqData]
+        public async Task Given_ProcessCompleted_When_MeteredDataResponsiblePeeks_Then_MessageHubReceivesReply(
+            Guid batchId,
+            Guid correlationId,
+            DateTime operationTimestamp,
+            string messageType)
+        {
+            // Arrange
+            var gridAreaCode = "805";
+
+            var completedProcess = new ProcessCompletedEventDto(gridAreaCode, batchId);
+            var message = ServiceBusTestMessage.Create(
+                completedProcess,
+                operationTimestamp.AsUtc(),
+                correlationId.ToString(),
+                messageType);
+
+            // Act -> Publish process completed event, which will transitively invoke
+            await Fixture.CompletedProcessTopic.SenderClient.SendMessageAsync(message);
+
+            // Assert
+            await Fixture.MessageHubMock.WaitForNotificationsInDataAvailableQueueAsync(correlationId.ToString());
+
+            var response = await Fixture.MessageHubMock.PeekAsync();
+            response.Content.Should().NotBeNull();
+        }
+    }
+}
---
+++ source/dotnet/Services/IntegrationTests/TestCommon/WebApi/V1/BatchControllerTests.cs
@@ -0,0 +1,101 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net;
+using System.Net.Http.Json;
+using Energinet.DataHub.Wholesale.Application.Batches;
+using Energinet.DataHub.Wholesale.Contracts.WholesaleProcess;
+using Energinet.DataHub.Wholesale.IntegrationTests.TestCommon.Fixture.WebApi;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Energinet.DataHub.Wholesale.IntegrationTests.TestCommon.WebApi.V1;
+
+[Collection(nameof(WholesaleWebApiCollectionFixture))]
+public class BatchControllerTests :
+    WebApiTestBase<WholesaleWebApiFixture>,
+    IClassFixture<WholesaleWebApiFixture>,
+    IClassFixture<WebApiFactory>,
+    IAsyncLifetime
+{
+    private const string BaseUrl = "/v1/batch";
+
+    private readonly HttpClient _client;
+
+    public BatchControllerTests(
+        WholesaleWebApiFixture wholesaleWebApiFixture,
+        WebApiFactory factory,
+        ITestOutputHelper testOutputHelper)
+        : base(wholesaleWebApiFixture, testOutputHelper)
+    {
+        _client = factory.CreateClient();
+        factory.ReconfigureJwtTokenValidatorMock(isValid: true);
+    }
+
+    public Task InitializeAsync()
+    {
+        _client.DefaultRequestHeaders.Add("Authorization", $"Bearer xxx");
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        _client.Dispose();
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task CreateAsync_WhenCalled_AlwaysReturnsOk()
+    {
+        // Arrange
+        var periodStart = DateTimeOffset.Now;
+        var periodEnd = periodStart.AddHours(1);
+        var batchRequest = new BatchRequestDto(
+            WholesaleProcessType.BalanceFixing,
+            new List<string> { "805" },
+            periodStart,
+            periodEnd);
+
+        // Act
+        var response = await _client.PostAsJsonAsync(BaseUrl, batchRequest, CancellationToken.None);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task SearchAsync_WhenCalled_AlwaysReturnsOk()
+    {
+        // Arrange
+        var minExecutionTime = new DateTimeOffset(
+            2022,
+            01,
+            02,
+            1,
+            2,
+            3,
+            50,
+            TimeSpan.Zero);
+        var maxExecutionTime = minExecutionTime + TimeSpan.FromMinutes(33);
+        var batchSearchDto = new BatchSearchDto(minExecutionTime, maxExecutionTime);
+
+        // Act
+        const string searchUrl = BaseUrl + "/Search";
+        var response = await _client.PostAsJsonAsync(searchUrl, batchSearchDto, CancellationToken.None);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
