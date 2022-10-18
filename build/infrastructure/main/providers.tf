c7bfa7ee6fd130c6fe1b187fedf44faa0aa5d6a1
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2022-10-18 00:03:00 -0700

    Update providers.tf

--- build/infrastructure/main/providers.tf
+++ build/infrastructure/main/providers.tf
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     databricks = {
       source = "databricks/databricks"
-      version = "1.4.0"
+      version = "1.5.0"
     }
     # It is recommended to pin to a given version of the Azure provider
     azurerm = "=3.9.0"
