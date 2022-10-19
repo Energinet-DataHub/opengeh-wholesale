1bdee6d1ce24efc4e8cf87cc08311157d5f014f2
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2022-10-19 13:44:14 +0200

    feat: Enable creation of zip file when batch completed (#394)
    Co-authored-by: Lasse Ringgren Nielsen <sixxth@gmail.com>

--- source/dotnet/Services/Application/Processes/IBasisDataApplicationService.cs
+++ source/dotnet/Services/Application/Processes/IBasisDataApplicationService.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.Wholesale.Application.Batches;
 using Energinet.DataHub.Wholesale.Contracts.WholesaleProcess;
 
 namespace Energinet.DataHub.Wholesale.Application.Processes;
