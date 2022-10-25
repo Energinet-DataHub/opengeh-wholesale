13a2e86100e83d696452c93bbcdc49ce3a7d9280
Jonas Michelsen <jvm@energinet.dk>
2022-10-25 16:10:55 +0200

    Add sql script for BatchHasBeenZipped

---
+++ source/dotnet/Services/DatabaseMigration/Scripts/202210251400_Add_BatchHasBeenZipped.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE Batch
+    ADD BatchHasBeenZipped BIT;
+GO
+
+UPDATE Batch SET BatchHasBeenZipped=1 WHERE BatchHasBeenZipped IS NULL
+ALTER TABLE Batch
+ALTER COLUMN BatchHasBeenZipped BIT NOT NULL;
+GO
