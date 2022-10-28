7477ecaa8ba2935cd808450bdd515ab47aa746a7
Jonas Michelsen <jvm@energinet.dk>
2022-10-28 10:25:09 +0200

    Update balance_fixing_total_production.py

--- source/databricks/package/balance_fixing_total_production.py
+++ source/databricks/package/balance_fixing_total_production.py
@@ -297,8 +297,6 @@ def _get_metering_point_periods_df(
 
     window = Window.partitionBy("MeteringPointId").orderBy("EffectiveDate")
 
-    metering_point_events_df.show()
-
     metering_point_periods_df = (
         metering_point_events_df.withColumn(
             "toEffectiveDate",
