dc7c19507314c46ba59e0a65bf905bf6599d841d
Lasse Ringgren Nielsen <sixxth@gmail.com>
2022-11-10 11:20:46 +0100

    Update data_lake_file_manager.py

--- source/databricks/package/datamigration/data_lake_file_manager.py
+++ source/databricks/package/datamigration/data_lake_file_manager.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from azure.identity import DefaultAzureCredential
 from azure.storage.filedatalake import DataLakeServiceClient
 from io import StringIO
 import csv
