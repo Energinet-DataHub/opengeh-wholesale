d8b1bb3bf3c418a326ca63e6bb5c3e575356134e
Kristian Bank Erbou <contact@kbeconsult.dk>
2022-11-15 10:37:16 +0100

    Add TF output vars to enable SQL IAM authentication (#32)

--- market-participant/terraform/main/outputs.tf
+++ market-participant/terraform/main/outputs.tf
@@ -13,6 +13,18 @@
 # limitations under the License.
 output ms_market_participant_connection_string {
   description = "Connection string of the market participant database created in the shared server"
-  value       = local.MS_MARKET_PARTICIPANT_CONNECTION_STRING_SQL_AUTH
+  value       = local.MS_MARKET_PARTICIPANT_CONNECTION_STRING
   sensitive   = true
+}
+
+output "ms_market_participant_database_name" {
+  description = "Database name in the shared sql server"
+  value = module.mssqldb_market_participant.name
+  sensitive = true  
+}
+
+output "ms_market_participant_database_server" {
+  description = "Database server instance hosting the Market Participant database"
+  value = data.azurerm_key_vault_secret.mssql_data_url.value
+  sensitive = true  
 }
