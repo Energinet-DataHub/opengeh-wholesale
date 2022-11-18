e9111f511a6985df76cc88926c10a23932d62649
BjarkeMeier <hej@bjarkemeier.dk>
2022-11-18 12:01:06 +0100

    Add test dummies

--- source/databricks/tests/integration/calculator/balance_fixing_total_production/test_get_metering_point_periods_from_static_datasource.py
+++ source/databricks/tests/integration/calculator/balance_fixing_total_production/test_get_metering_point_periods_from_static_datasource.py
@@ -36,38 +36,88 @@ def grid_area_df(spark):
     return spark.createDataFrame([row])
 
 
-def test_metering_points_have_energy_supplier(
-    databricks_path,
-    spark,
-    grid_area_df,
-):
-    metering_points_periods_df = spark.read.option("header", "true").csv(
-        f"{databricks_path}/package/datasources/MeteringPointsPeriods.csv"
-    )
-    market_roles_periods_df = spark.read.option("header", "true").csv(
-        f"{databricks_path}/package/datasources/MarketRolesPeriods.csv"
-    )
-    period_start_datetime = datetime.strptime("01/01/2018 00:00", "%d/%m/%Y %H:%M")
-    period_end_datetime = datetime.strptime("01/03/2018 22:00", "%d/%m/%Y %H:%M")
+# def test_metering_points_have_energy_supplier(
+#     databricks_path,
+#     spark,
+#     grid_area_df,
+# ):
+#     metering_points_periods_df = spark.read.option("header", "true").csv(
+#         f"{databricks_path}/package/datasources/MeteringPointsPeriods.csv"
+#     )
+#     market_roles_periods_df = spark.read.option("header", "true").csv(
+#         f"{databricks_path}/package/datasources/MarketRolesPeriods.csv"
+#     )
+#     period_start_datetime = datetime.strptime("01/01/2018 00:00", "%d/%m/%Y %H:%M")
+#     period_end_datetime = datetime.strptime("01/03/2018 22:00", "%d/%m/%Y %H:%M")
 
-    actual = _get_metering_point_periods_from_static_datasource_df(
-        metering_points_periods_df,
-        market_roles_periods_df,
-        grid_area_df,
-        period_start_datetime,
-        period_end_datetime,
-    )
+#     actual = _get_metering_point_periods_from_static_datasource_df(
+#         metering_points_periods_df,
+#         market_roles_periods_df,
+#         grid_area_df,
+#         period_start_datetime,
+#         period_end_datetime,
+#     )
 
-    actual_mps_without_energy_supplier = actual.filter(
-        col("EnergySupplierGln").isNull()
-    )
+#     actual_mps_without_energy_supplier = actual.filter(
+#         col("EnergySupplierGln").isNull()
+#     )
 
-    assert actual_mps_without_energy_supplier.count() == 0
-
-
-def test_meteringpoints_start_date_is_included():
-    assert 1 == 1
+#     assert actual_mps_without_energy_supplier.count() == 0
 
 
-def test_meteringpoints_end_date_is_included():
-    assert 1 == 1
+def test__when_metering_point_period_is_in_grid_areas__returns_metering_point_period():
+    pass
+
+
+# What about market participant periods outside the selected period?
+def test__when_energy_supplier_changes_in_batch_period__returns_two_periods_with_expected_energy_supplier_and_dates():
+    pass
+
+
+# Both periodized energy supplier and metering point
+# Test combinations with 0, 1 and 2 periods
+def test__returns_expected_periods():
+    pass
+
+
+def test__when_connection_state_is_E22__returns_metering_point_period():
+    pass
+
+
+def test__when_connection_state_is_not_E22__returns_none():
+    pass
+
+
+def test__when_type_is_E18__returns_metering_point_period():
+    pass
+
+
+def test__when_type_is_not_E18__returns_metering_point_period():
+    pass
+
+
+def test__metering_points_have_expected_columns():  # expected_column_name, expected_value):
+    pass
+
+
+def test__when_in_period__returns_metering_point_period():  # mp_start, mp_end):
+    # <= and >= combinations
+    # period_start = xx
+    # period_end = xx
+    pass
+
+
+# def test_meteringpoints_start_date_is_included():
+#     assert 1 == 1
+
+
+# def test_meteringpoints_end_date_is_included():
+#     assert 1 == 1
+
+
+# def test__when_market_participant_has_periods__metering_point_period_is_split():
+#     pass
+
+
+# def test__when_in_period__metering_point_is_returned():
+#     pass
