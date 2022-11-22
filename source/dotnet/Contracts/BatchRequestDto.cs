e774c7fdaceecae43c72a26f1e37a075e18cd99c
Lasse Ringgren Nielsen <sixxth@gmail.com>
2022-11-22 14:40:16 +0100

    fix package usings

--- source/dotnet/Contracts/BatchRequestDto.cs
+++ source/dotnet/Contracts/BatchRequestDto.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.Wholesale.Contracts.WholesaleProcess;
-
 namespace Energinet.DataHub.Wholesale.Contracts;
 
 /// <summary>
