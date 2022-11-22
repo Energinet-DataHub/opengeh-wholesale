76b69e88e2aad0dfb470bf411a7f71eb7ce2a85a
Jonas Michelsen <jvm@energinet.dk>
2022-11-22 11:51:14 +0100

    Use WholesaleProcessType

--- source/dotnet/Contracts/BatchGridAreaDto.cs
+++ source/dotnet/Contracts/BatchGridAreaDto.cs
@@ -12,12 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.Wholesale.Contracts.WholesaleProcess;
 using NodaTime;
 
 namespace Energinet.DataHub.Wholesale.Application.Batches;
 
 public sealed record BatchGridAreaDto(
     BatchState State,
-    string Type,
+    WholesaleProcessType Type,
     GridAreaDto GridArea,
     ProcessDto[] Processes);
