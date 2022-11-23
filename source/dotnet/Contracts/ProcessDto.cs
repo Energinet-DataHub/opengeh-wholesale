daf666382937c462887c2191aa52200557dec970
Jonas Michelsen <jvm@energinet.dk>
2022-11-23 12:46:49 +0100

    Update ProcessDto.cs

--- source/dotnet/Contracts/ProcessDto.cs
+++ source/dotnet/Contracts/ProcessDto.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.Wholesale.Contracts;
-
 namespace Energinet.DataHub.Wholesale.Contracts;
 
 public sealed record ProcessDto(
