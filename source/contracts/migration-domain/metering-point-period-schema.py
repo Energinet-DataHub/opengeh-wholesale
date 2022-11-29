9eaf05bfba23cef0af555d83f58b2ed67458c5f8
BjarkeMeier <hej@bjarkemeier.dk>
2022-11-29 16:21:49 +0100

    PO/SME review

--- source/contracts/migration-domain/metering-point-period-schema.py
+++ source/contracts/migration-domain/metering-point-period-schema.py
@@ -19,45 +19,62 @@ from pyspark.sql.types import (
     StructType,
 )
 
-# Schema for metering point
-# Data must be stored in a Delta table.
-# The table must be partitioned by `FromDate`.
-# The table data must always contain updated periods.
+"""
+Schema for metering point
+
+Data must be stored in a Delta table.
+The table holds all consumption, production, exchange, and child metering points.
+The table must be partitioned by `FromDate`.
+The table data must always contain updated periods.
+"""
 metering_point_period_schema = StructType(
     [
-        # The metering point GSRN number that uniquely identifies the metering point
-        # TODO Does DH2 have other ids than GSRN for metering points?
+        # The metering point GSRN number (18 characters) that uniquely identifies the metering point
         StructField("MeteringPointId", StringType(), True),
-        # "E17" | "E18" | "E20" | child?
+        
+        # "E17" (consumption) | "E18" (production) | "E20" (exchange) | "D01"-"D99" (child)
         StructField("Type", StringType(), True),
-        # TODO "" | "" | ""
+        
+        # "E02" (quarterly)| "D01" (flex)
         StructField("SettlementMethod", StringType(), True),
+        
         # 3 character grid area code uniquely identifying the grid area. All characters must be digits (0-9).
         StructField("GridAreaCode", StringType(), True),
-        # TODO "" | "" | ""
+        
+        # "E22" (connected) | "E23" (disconnected) | "D03" (new) | "D02" (closed down)
         StructField("ConnectionState", StringType(), True),
-        # "PT1H" | "PT15M"
+        
+        # "PT1H" (hourly) | "PT15M" (quarterly)
         StructField("Resolution", StringType(), True),
+        
         # 3 character grid area code uniquely identifying the from-grid area. All characters must be digits (0-9).
         StructField("FromGridAreaCode", StringType(), True),
+        
         # 3 character grid area code uniquely identifying the to-grid area. All characters must be digits (0-9).
         StructField("ToGridAreaCode", StringType(), True),
-        # The metering point GSRN number that uniquely identifies the parent metering point
+        
+        # The id of the parent metering point
         StructField("ParentMeteringPointId", StringType(), True),
+        
         # The start date of the period. The start date must be the UTC time of the begining of a date in the given timezone/DST.
         # The date is inclusive.
-        # TODO Or effective date?
         StructField("FromDate", TimestampType(), True),
+        
         # The to-date of the period. The to-date must be the UTC time of the begining of a date in the given timezone/DST.
-        # The date is exclusive.
+        # The moment is exclusive.
+        # The date of the last period is null.
         StructField("ToDate", TimestampType(), True),
-        # The unique actor id of the energy supplier.
+        
+        # The unique actor id of the energy supplier (as provided by the market participant domain)
         StructField("EnergySupplierId", StringType(), True),
+        
         # The year part of the `ToDate`. Used in partition.
-        StructField("ToDateYear", IntegerType(), True),
+        StructField("ToDate_Year", IntegerType(), True),
+        
         # The month part of the `ToDate`. Used in partition.
-        StructField("ToDateMonth", IntegerType(), True),
+        StructField("ToDate_Month", IntegerType(), True),
+        
         # The date part of the `ToDate`. Used in partition.
-        StructField("ToDateDate", IntegerType(), True),
+        StructField("ToDate_Date", IntegerType(), True),
     ]
 )
--- source/contracts/migration-domain/time-series-point-schema.py
+++ source/contracts/migration-domain/time-series-point-schema.py
@@ -20,36 +20,37 @@ from pyspark.sql.types import (
 )
 
 """
-            col("TransactionId"),
-            col("Period.Resolution"),
+Schema for time series points
+Data must be stored in a Delta table.
+The table must be partitioned by the observation time elements: year/month/date.
+New data must be appended.
 """
-
-
-# Schema for time series points
-# Data must be stored in a Delta table.
-# The table must be partitioned by the observation time elements: year/month/date.
-# New data must be appended.
 time_series_point_schema = StructType(
     [
-        # The GSRN that uniquely identifies the metering point
+        # The metering point GSRN number (18 characters) that uniquely identifies the metering point
         StructField("MeteringPointId", StringType(), True),
-        # Measured energy amount
+
+        # Energy quantity for the given observation time.
+        # Null when quality is missing.
         StructField("Quantity", DecimalType(18,6), True),
-        # "A02": Quantity missing (reset)
-        # "A03": for estimated
-        # "A04": Measured
-        # "A05": for incomplete values
+
+        # "A02" (missing) | "A03" (estimated) | "A04" (measured)
         StructField("Quality", StringType(), True),
-        # The time when the energy was measured
-        # Operation time (the time when the power was consumed/produced/exchanged)
+        
+        # The time when the energy was consumed/produced/exchanged
         StructField("ObservationTime", TimestampType(), True),
+        
         # The year part of the `ObservationTime`. Used in partition.
-        StructField("ObservationYear", IntegerType(), True),
+        StructField("ObservationTime_Year", IntegerType(), True),
+        
         # The month part of the `ObservationTime`. Used in partition.
-        StructField("ObservationMonth", IntegerType(), True),
+        StructField("ObservationTime_Month", IntegerType(), True),
+        
         # The date part of the `ObservationTime`. Used in partition.
-        StructField("ObservationDate", IntegerType(), True),
-        # The registration time as indicated by the MDR actor
+        StructField("ObservationTime_Date", IntegerType(), True),
+        
+        # The registration time as specified by the MDR actor.
+        # In DataHub 2 this is the creation date time.
         StructField("RegistrationTime", TimestampType(), True),
     ]
 )
