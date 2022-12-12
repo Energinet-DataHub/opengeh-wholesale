0fccf05419a8e6f91b788162a07422295c784693
Jonas Michelsen <jvm@energinet.dk>
2022-12-12 13:02:12 +0100

    remove services

--- source/databricks/geh_stream/shared/__init__.py
+++ source/databricks/geh_stream/shared/__init__.py
@@ -12,13 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .period import Period
-from .filters import filter_on_date, filter_on_grid_areas, time_series_points_where_date_condition
-from .data_loader import load_charge_links, \
-    load_charge_prices, \
-    load_charges, \
-    load_es_brp_relations, \
-    load_grid_loss_sys_corr, \
-    load_market_roles, \
-    load_metering_points, \
-    load_time_series_points, \
-    initialize_spark
+from .filters import (
+    filter_on_date,
+    filter_on_grid_areas,
+    time_series_points_where_date_condition,
+)
