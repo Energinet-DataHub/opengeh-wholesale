ebe89997389bfe462c6fba5aa080522c27375a9e
Jonas Michelsen <jvm@energinet.dk>
2022-12-16 11:48:30 +0100

    Update aggregators.py

--- source/databricks/package/steps/aggregation/aggregators.py
+++ source/databricks/package/steps/aggregation/aggregators.py
@@ -314,7 +314,6 @@ def aggregate_per_ga_and_brp_and_es(
             Colname.time_window,
             Colname.quality,
             Colname.sum_quantity,
-            # Colname.resolution,
             lit(MeteringPointResolution.quarter.value).alias(
                 Colname.resolution
             ),  # TODO take resolution from metadata
