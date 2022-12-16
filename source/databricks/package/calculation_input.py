6a0558403089e19af8188b090af9adb0bca11059
BjarkeMeier <hej@bjarkemeier.dk>
2022-12-16 11:20:31 +0100

    Use partitionsd

--- source/databricks/package/calculation_input.py
+++ source/databricks/package/calculation_input.py
@@ -18,6 +18,8 @@ from pyspark.sql.functions import (
     col,
     when,
     year,
+    month,
+    dayofmonth,
 )
 from package.codelists import (
     MeteringPointType,
@@ -43,8 +45,18 @@ def get_metering_point_periods_df(
 
     metering_point_periods_df = (
         metering_points_in_grid_area.where(col("FromDate") < period_end_datetime)
-        .where(col("ToDate_Year") < year(period_end_datetime))
         .where(col("ToDate") > period_start_datetime)
+        .where(col("ToDate_Year") >= period_start_datetime.year)
+        # Use partitions
+        .where(
+            (col("ToDate_Year") == period_start_datetime.year)
+            & (col("ToDate_Month") >= period_start_datetime.month)
+        )
+        .where(
+            (col("ToDate_Year") == period_start_datetime.year)
+            & (col("ToDate_Month") == period_start_datetime.month)
+            & (col("ToDate_Day") > period_start_datetime.day)
+        )
         .where(col("Type") == MeteringPointType.production.value)
         .withColumnRenamed("FromDate", "EffectiveDate")
         .withColumnRenamed("ToDate", "toEffectiveDate")
