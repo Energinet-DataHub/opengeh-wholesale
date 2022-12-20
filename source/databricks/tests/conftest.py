68552258a8cf0825dddb1a1ffdb29f456606783c
seblapela <sebastian@lapela.dk>
2022-12-20 15:27:53 +0100

    remove comm

--- source/databricks/tests/conftest.py
+++ source/databricks/tests/conftest.py
@@ -25,14 +25,6 @@ from typing import Generator, Callable, Optional
 
 from delta import configure_spark_with_delta_pip
 
-#
-# builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
-#    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-#    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-#
-# spark = configure_spark_with_delta_pip(builder).getOrCreate()
-#
-
 
 @pytest.fixture(scope="session")
 def spark() -> SparkSession:
