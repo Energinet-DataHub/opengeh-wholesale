889c31bdc96eb6480b7d6ff56af34b528ae6d733
Kristian F. Thomsen <kft@energinet.dk>
2022-12-21 13:52:57 +0100

    Remove unused configuration settings

--- market-roles/terraform/main/func-api.tf
+++ market-roles/terraform/main/func-api.tf
@@ -38,24 +38,11 @@ module "func_receiver" {
     # Endregion: Default Values
     DB_CONNECTION_STRING                                          = local.MS_MARKETROLES_CONNECTION_STRING
     INCOMING_CHANGE_OF_SUPPLIER_MESSAGE_QUEUE_NAME                = module.sbq_incoming_change_supplier_messagequeue.name
-    INCOMING_CHANGE_CUSTOMER_CHARACTERISTICS_MESSAGE_QUEUE_NAME   = module.sbq_incoming_change_customer_characteristics_message_queue.name
-    MOVE_IN_REQUEST_ENDPOINT                                      = "https://func-processing-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}.azurewebsites.net/api/MoveIn"
     SERVICE_BUS_CONNECTION_STRING_FOR_DOMAIN_RELAY_LISTENER       = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=sb-domain-relay-listen-connection-string)"
     SERVICE_BUS_CONNECTION_STRING_FOR_DOMAIN_RELAY_MANAGE         = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=sb-domain-relay-manage-connection-string)"
     SERVICE_BUS_CONNECTION_STRING_FOR_DOMAIN_RELAY_SEND           = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=sb-domain-relay-send-connection-string)"
-    METERING_POINT_MASTER_DATA_RESPONSE_QUEUE_NAME                = data.azurerm_key_vault_secret.sbq_metering_point_master_data_response_name.value,
-    ENERGY_SUPPLIER_CHANGED_TOPIC                                 = data.azurerm_key_vault_secret.sbt_energy_supplier_changed_name.value,
-    ENERGY_SUPPLIER_CHANGED_SUBSCRIPTION                          = data.azurerm_key_vault_secret.sbs_energy_supplier_changed_to_messaging_name.value
-    MASTER_DATA_REQUEST_QUEUE_NAME                                = data.azurerm_key_vault_secret.sbq_metering_point_master_data_request_name.value,
-    CUSTOMER_MASTER_DATA_RESPONSE_QUEUE_NAME                      = module.sbq_customermasterdataresponsequeue.name
-    CUSTOMER_MASTER_DATA_REQUEST_QUEUE_NAME                       = module.sbq_customermasterdatarequestqueue.name
     INTEGRATION_EVENT_TOPIC_NAME                                  = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=sbt-sharedres-integrationevent-received-name)"
-    CONSUMER_MOVED_IN_EVENT_SUBSCRIPTION_NAME                     = module.sbs_consumer_moved_in.name
-    ENERGY_SUPPLIER_CHANGED_EVENT_SUBSCRIPTION_NAME               = module.sbs_energy_supplier_changed.name
     MARKET_PARTICIPANT_CHANGED_ACTOR_CREATED_SUBSCRIPTION_NAME    = module.sbs_market_roles_b2b_actor_created.name
-    METERING_POINT_CREATED_EVENT_B2B_SUBSCRIPTION_NAME            = module.sbs_metering_point_created_b2b_event.name
-    CREATE_METERING_POINT_TRANSACTIONS_QUEUE_NAME                 = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=sbq-create-metering-point-transactions)"
-    CUSTOMER_MASTER_DATA_UPDATE_RESPONSE_QUEUE_NAME               = module.sbq_customermasterdataupdateresponsequeue.name
     FEATUREFLAG_ACTORMESSAGEQUEUE                                 = true
   }
 
