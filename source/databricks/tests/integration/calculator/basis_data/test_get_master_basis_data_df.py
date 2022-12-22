ce05d680a5ce8840392de2de380cb4e14e1893e2
Jonas Michelsen <jvm@energinet.dk>
2022-12-22 15:40:35 +0100

    Update test_get_master_basis_data_df.py

--- source/databricks/tests/integration/calculator/basis_data/test_get_master_basis_data_df.py
+++ source/databricks/tests/integration/calculator/basis_data/test_get_master_basis_data_df.py
@@ -182,7 +182,7 @@ def test__from_date_must_not_be_earlier_than_period_start(
 def test__to_date_must_not_be_after_period_end(
     metering_point_period_df_factory,
 ):
-    expected_vaild_to = "2022-06-010T12:09:15.000Z"
+    expected_vaild_to = "2022-06-10T12:09:15.000Z"
     metering_point_period_df = metering_point_period_df_factory(meteringpoint_id="1")
 
     master_basis_data = get_master_basis_data_df(
