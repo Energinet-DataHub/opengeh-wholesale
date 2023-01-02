51e5fdc7f980e88a945480919e279a18262e63d0
Jonas Michelsen <jvm@energinet.dk>
2023-01-02 15:09:55 +0100

    rename results folder

---
+++ source/databricks/package/datamigration/migration_scripts/202301021400_Rename_Results_Folder_To_Calculation-output.py
@@ -0,0 +1,60 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from azure.storage.filedatalake import DataLakeDirectoryClient
+from package.datamigration.migration_script_args import MigrationScriptArgs
+
+
+def apply(args: MigrationScriptArgs) -> None:
+    source_container = "wholesale"
+    destination_container = source_container
+
+    # Rename 'results' folder to 'calculation-output'
+    source_directory = "results"
+    destination_directory = "calculation-output"
+    move_directory(
+        args.storage_account_url,
+        args.storage_account_key,
+        source_container,
+        source_directory,
+        destination_container,
+        destination_directory,
+    )
+
+
+def move_directory(
+    storage_account_url: str,
+    storage_account_key: str,
+    source_container: str,
+    source_directory: str,
+    destination_container: str,
+    destination_directory: str,
+) -> None:
+    directory_client = DataLakeDirectoryClient(
+        storage_account_url,
+        source_container,
+        source_directory,
+        storage_account_key,
+    )
+
+    if not directory_client.exists():
+        source_path = source_container + "/" + source_directory
+        print(
+            f"Skipping migration ({__file__}). Source directory not found:{source_path}"
+        )
+        return
+
+    directory_client.rename_directory(
+        new_name=destination_container + "/" + destination_directory
+    )
