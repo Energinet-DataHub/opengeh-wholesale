2f8b58dd1b78307f6fa233d193307ac1b0cd3b0e
Kristian F. Thomsen <kft@energinet.dk>
2023-01-11 15:23:50 +0100

    Add service bus subscription for process completed integration event

--- market-roles/terraform/main/kv-shared-resources.tf
+++ market-roles/terraform/main/kv-shared-resources.tf
@@ -86,3 +86,8 @@ data "azurerm_key_vault_secret" "sb_domain_relay_namespace_id" {
   name         = "sb-domain-relay-namespace-id"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
+
+data "azurerm_key_vault_secret" "sbt_sharedres_integrationevent_received_id" {
+  name         = "sbt-sharedres-integrationevent-received-id"
+  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
+}
---
+++ market-roles/terraform/main/sbs-integrationsevents.tf
@@ -0,0 +1,10 @@
+﻿module "sbtsub_wholesale_process_completed_event_listener" {
+  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic-subscription?ref=v10"
+  name                = "wholesale_process_completed_event_listener"
+  topic_id            = data.azurerm_key_vault_secret.sbt_sharedres_integrationevent_received_id.value
+  project_name        = var.domain_name_short
+  max_delivery_count  = 1 
+  correlation_filter  = {
+    label   = "ProcessCompleted"
+  }
+}
