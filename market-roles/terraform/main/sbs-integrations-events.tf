69fb9cf8f7c76875d6416c8907aa7f7adddd8506
Kristian F. Thomsen <kft@energinet.dk>
2023-01-12 09:08:56 +0100

    Fix type-o

--- market-roles/terraform/main/sbs-integrations-events.tf
+++ market-roles/terraform/main/sbs-integrations-events.tf
@@ -7,11 +7,11 @@
 
 module "sbtsub_wholesale_process_completed_event_listener" {
   source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic-subscription?ref=v10"
-  name                = locals.WHOLESALE_PROCESS_COMPLETED_EVENT_SUBSCRIPTION_NAME
+  name                = local.WHOLESALE_PROCESS_COMPLETED_EVENT_SUBSCRIPTION_NAME
   topic_id            = module.sbt_domain_events.id
   project_name        = var.domain_name_short
   max_delivery_count  = 1 
   correlation_filter  = {
-    label   = locals.WHOLESALE_PROCESS_COMPLETED_EVENT_TYPE_NAME
+    label   = local.WHOLESALE_PROCESS_COMPLETED_EVENT_TYPE_NAME
   }
 }
