c3cc83da26394552a302c628032418cec5ca53fd
Kristian F. Thomsen <kft@energinet.dk>
2023-01-12 10:19:42 +0100

    Reference topic in module

--- market-roles/terraform/main/sbs-integrations-events.tf
+++ market-roles/terraform/main/sbs-integrations-events.tf
@@ -1,14 +1,7 @@
-﻿module "sbt_domain_events" {
-  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic?ref=v10"
-  name                = "integration-events"
-  project_name        = var.domain_name_short
-  namespace_id        = data.azurerm_key_vault_secret.sb_integration_events_id.value
-}
-
-module "sbtsub_wholesale_process_completed_event_listener" {
+﻿module "sbtsub_wholesale_process_completed_event_listener" {
   source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic-subscription?ref=v10"
   name                = local.WHOLESALE_PROCESS_COMPLETED_EVENT_SUBSCRIPTION_NAME
-  topic_id            = module.sbt_domain_events.id
+  topic_id            = module.sbt_domainrelay_integrationevent_received.id
   project_name        = var.domain_name_short
   max_delivery_count  = 1 
   correlation_filter  = {
