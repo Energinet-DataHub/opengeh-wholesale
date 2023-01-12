96cc3574f42e541a639349a5984e4506e38b61b9
Dan Stenrøjl <XDAST@energinet.dk>
2023-01-12 12:20:56 +0100

    Example output

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -22,12 +22,34 @@ export PYSPARK_DRIVER_PYTHON=/opt/conda/bin/python
 
 # Writing output to log with 'tee' cause exit code to be '0' even if tests fails
 coverage run --branch -m pytest -n 2 . | tee pytest-results.log
+
+################################################
+# Scenario: Test errors (exceptions) occured
+################################################
+
+# EXAMPLE test summary which our regex can detect:
+# ================== 65 passed, 195 errors in 94.15s (0:01:34) ===================
+
 # If test summary contains errors we return exit code 2 to signal we want to retry
 matchTestErrors=$(grep -Po '^=+.* [[:digit:]]+ errors.* in .*=+$' pytest-results.log)
 if [ ! -z "$matchTestErrors" ]; then
   echo "Test errors occured, which is typically caused by network issues (download failure). We should retry."
   exit 2
 fi
+
+################################################
+# Scenario: Only 'entry point tests' failed
+################################################
+
+# EXAMPLE test summary which our regex can detect:
+# =========================== short test summary info ============================
+# FAILED entry_points/test_entry_points.py::test__entry_point__start_calculator__returns_0
+# FAILED entry_points/test_entry_points.py::test__entry_point__uncommitted_migrations_count__returns_0
+# FAILED entry_points/test_entry_points.py::test__entry_point__unlock_storage__returns_0
+# FAILED entry_points/test_entry_points.py::test__entry_point__lock_storage__returns_0
+# FAILED entry_points/test_entry_points.py::test__entry_point__migrate_data_lake__returns_0
+# ============= 5 failed, 264 passed, 3 skipped in 191.60s (0:03:11) =============
+
 # If test summary only contains 5 failing 'entry point tests' return exit code 2 to signal we want to retry
 # See https://unix.stackexchange.com/questions/637959/regex-matching-multi-line-search for understanding use of
 #   z parameter to grep
@@ -38,6 +60,11 @@ if [ ! -z "$matchFailedEntryPointTests" ]; then
   echo "Only 'entry point tests' failed. We should retry."
   exit 2
 fi
+
+################################################
+# Scenario: Not only 'entry point tests' failed
+################################################
+
 # If test summary contains other combination of failed tests we return exit code 1 to signal we DO NOT want to retry
 matchFailedTests=$(grep -Po '^=+.* [[:digit:]]+ failed.* in .*=+$' pytest-results.log)
 if [ ! -z "$matchFailedTests" ]; then
