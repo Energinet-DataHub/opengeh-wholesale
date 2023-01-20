22abef3214319fbe3cd1fed66349db496d3582dd
Chris Brohus <CBS@energinet.dk>
2023-01-20 09:41:37 +0100

    change delivery count to 10

--- market-roles/terraform/main/sbs-integrations-events.tf
+++ market-roles/terraform/main/sbs-integrations-events.tf
@@ -16,7 +16,7 @@ module "sbtsub_wholesale_process_completed_event_listener" {
   name                = local.WHOLESALE_PROCESS_COMPLETED_EVENT_SUBSCRIPTION_NAME
   topic_id            = data.azurerm_key_vault_secret.sbt_domainrelay_integrationevent_received_id.value
   project_name        = var.domain_name_short
-  max_delivery_count  = 1 
+  max_delivery_count  = 10 
   correlation_filter  = {
     label   = local.WHOLESALE_PROCESS_COMPLETED_EVENT_TYPE_NAME
   }
