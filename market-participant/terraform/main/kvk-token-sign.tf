317dc3e4ac00cdab913f62e36e38249ecbf07079
René T. Nielsen <renetdev@gmail.com>
2023-01-26 11:32:44 +0100

    remove license headers (#135)

--- market-participant/terraform/main/kvk-token-sign.tf
+++ market-participant/terraform/main/kvk-token-sign.tf
@@ -1,16 +1,3 @@
-# Copyright 2020 Energinet DataHub A/S
-#
-# Licensed under the Apache License, Version 2.0 (the "License2");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 resource "azurerm_key_vault_key" "token_sign" {
   name         = "token-sign"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
--- market-roles/terraform/main/outputs.tf
+++ market-roles/terraform/main/outputs.tf
@@ -1,16 +1,3 @@
-# Copyright 2020 Energinet DataHub A/S
-#
-# Licensed under the Apache License, Version 2.0 (the "License2");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 output ms_marketroles_connection_string {
   description = "Connectionstring to the database in the shared sql server"
   value = local.MS_MARKETROLES_CONNECTION_STRING_SQL_AUTH 
