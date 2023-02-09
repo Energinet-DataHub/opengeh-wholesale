efc830e0642bfb53565330fcc20a15007f3dfbd8
René T. Nielsen <renetdev@gmail.com>
2023-02-09 14:10:41 +0100

    add sendgrid_api_key (#148)
    * add sendgrid_api_key
    
    * add from email
    
    * add allowed vnet to key vault

---
+++ market-participant/terraform/main/kv-internal.tf
@@ -0,0 +1,33 @@
+module "kv_internal" {
+  source                          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault?ref=v10"
+
+  name                            = "int"
+  project_name                    = var.domain_name_short
+  environment_short               = var.environment_short
+  environment_instance            = var.environment_instance
+  resource_group_name             = azurerm_resource_group.this.name
+  location                        = azurerm_resource_group.this.location
+  enabled_for_template_deployment = true
+  sku_name                        = "premium"
+  log_analytics_workspace_id      = data.azurerm_key_vault_secret.log_shared_id.value
+  private_endpoint_subnet_id      = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
+  allowed_subnet_ids              = [
+    data.azurerm_key_vault_secret.snet_vnet_integrations_id.value,
+  ]
+}
+
+module "kvs_sendgrid_api_key" {
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=v10"
+
+  name          = "sendgrid-api-key"
+  value         = var.sendgrid_api_key
+  key_vault_id  = module.kv_internal.id
+}
+
+module "kvs_sendgrid_from_email" {
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=v10"
+
+  name          = "sendgrid-from-email"
+  value         = var.sendgrid_from_email
+  key_vault_id  = module.kv_internal.id
+}
