36c8339a762f2abbebd671b8fbf35a3396aa67a9
BjarkeMeier <hej@bjarkemeier.dk>
2023-02-11 10:27:47 +0100

    Fix code documentation about quantity quality

--- source/dotnet/Services/WebApi/V3/ProcessStepResult/TimeSeriesPointDto.cs
+++ source/dotnet/Services/WebApi/V3/ProcessStepResult/TimeSeriesPointDto.cs
@@ -19,7 +19,7 @@ namespace Energinet.DataHub.Wholesale.WebApi.V3.ProcessStepResult;
 /// </summary>
 /// <param name="Time">The observation time for the measured 'Quantity'</param>
 /// <param name="Quantity">Quantity has a scale of 3</param>
-/// <param name="Quality">Any of the following values: "A02" (missing), "A03" (estimated), "A04" (measured), "A06" (calculated)</param>
+/// <param name="Quality">Any of the values from <see cref="TimeSeriesPointQuality"/></param>
 public sealed record TimeSeriesPointDto(
     DateTimeOffset Time,
     decimal Quantity,
