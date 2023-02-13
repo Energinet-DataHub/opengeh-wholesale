fdbcbece85ba65387e6d706cf7b5459ff2d9d3e0
BjarkeMeier <hej@bjarkemeier.dk>
2023-02-13 08:55:07 +0100

    Add `TimeSeriesPointQuality` unit tests

---
+++ source/dotnet/Services/WebApi.UnitTests/WebApi/V3/TimeSeriesPointQuality.cs
@@ -0,0 +1,33 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.TestCommon.AutoFixture.Attributes;
+using Energinet.DataHub.Wholesale.WebApi.V3.ProcessStepResult;
+using FluentAssertions;
+using Xunit;
+
+namespace Energinet.DataHub.Wholesale.WebApi.UnitTests.WebApi.V3;
+
+public class TimeSeriesPointQualityTests
+{
+    [Theory]
+    [InlineAutoMoqData(TimeSeriesPointQuality.Missing, "missing")]
+    [InlineAutoMoqData(TimeSeriesPointQuality.Estimated, "estimated")]
+    [InlineAutoMoqData(TimeSeriesPointQuality.Measured, "measured")]
+    [InlineAutoMoqData(TimeSeriesPointQuality.Calculated, "calculated")]
+    public void QualityConstant_HasCorrectValue(string actualQualityName, string expectedQualityName)
+    {
+        actualQualityName.Should().Be(expectedQualityName);
+    }
+}
