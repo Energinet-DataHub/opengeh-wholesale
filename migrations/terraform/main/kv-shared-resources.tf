20f5b9de1372180ca10a58581507a532d1a4cb09
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2023-02-15 14:27:28 +0100

    Added appi_instrumentation_key to migration variables

--- migrations/terraform/main/kv-shared-resources.tf
+++ migrations/terraform/main/kv-shared-resources.tf
@@ -3,6 +3,11 @@ data "azurerm_key_vault" "kv_shared_resources" {
   resource_group_name = var.shared_resources_resource_group_name
 }
 
+data "azurerm_key_vault_secret" "appi_instrumentation_key" {
+  name         = "appi-shared-instrumentation-key"
+  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
+}
+
 data "azurerm_key_vault_secret" "snet_private_endpoints_id" {
   name         = "snet-private-endpoints-id"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
