d61f008e92bc1be2bb7e79ea2899cd15c5bd51a0
Lasse Ringgren Nielsen <sixxth@gmail.com>
2023-02-20 14:25:58 +0100

    Update ProcessStepResultRepository.cs

--- source/dotnet/Services/Infrastructure/Processes/ProcessStepResultRepository.cs
+++ source/dotnet/Services/Infrastructure/Processes/ProcessStepResultRepository.cs
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using System.Globalization;
-using Energinet.DataHub.Wholesale.Domain.ActorAggregate;
 using Energinet.DataHub.Wholesale.Domain.GridAreaAggregate;
 using Energinet.DataHub.Wholesale.Domain.ProcessStepResultAggregate;
 using Energinet.DataHub.Wholesale.Infrastructure.Integration.DataLake;
