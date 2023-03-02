c7b7df97bed88a94817e85e6a2c98bbd2de3bc43
René Nielsen <renetdev@gmail.com>
2023-03-02 10:21:13 +0100

    add access policy

---
+++ market-participant/terraform/main/kv-inernal-access-policy.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault_access_policy" "kv_internal_access_policy_func_entrypoint_marketparticipant" {
+  key_vault_id            = module.kv_internal.id
+
+  tenant_id               = module.func_entrypoint_marketparticipant.identity.0.tenant_id
+  object_id               = module.func_entrypoint_marketparticipant.identity.0.principal_id
+
+  secret_permissions  =[
+    "List",
+    "Get"
+  ]
+  key_permissions     = [
+    "List",
+    "Get",
+    "Sign"
+  ]
+}
