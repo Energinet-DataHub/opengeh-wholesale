93b67ef94132d9389362e3eea8efe1dc52f81a38
KarstenM <xnetsrak@gmail.com>
2023-03-02 09:38:09 +0100

    update

--- market-participant/terraform/main/kv-internal-access-policy.tf
+++
@@ -1,6 +0,0 @@
-module "kv_internal_access_policy_func_entrypoint_marketparticipant" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-access-policy?ref=v10"
-
-  key_vault_id              = data.azurerm_key_vault.kv_internal.id
-  app_identity              = module.func_entrypoint_marketparticipant.identity.0
-}
