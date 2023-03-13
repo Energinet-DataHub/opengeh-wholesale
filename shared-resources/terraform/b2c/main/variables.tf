3d65eedd1f29a4a8f05b7eb5117e81eec9bb2560
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2023-03-13 13:07:20 +0100

    Remove b2c_client_secret from B2C artifact - use OIDC instead (#183)

--- shared-resources/terraform/b2c/main/variables.tf
+++ shared-resources/terraform/b2c/main/variables.tf
@@ -7,8 +7,3 @@ variable "b2c_client_id" {
   type        = string
   description = "Client ID of the service principal managing resources in the B2C tenant."
 }
-
-variable "b2c_client_secret" {
-  type        = string
-  description = "Client secret of the service principal managing resources in the B2C tenant"
-}
