c1dfb434965df5358243ef59b5b9d0df625d44a2
Lasse Ringgren Nielsen <LRN@energinet.dk>
2023-03-14 15:13:50 +0100

    feat: publish result for brp_ga (#906)
    * publish result for brp_ga
    
    * Update ProcessApplicationServiceTest.cs
    
    * Update CalculationResultReadyIntegrationEventFactory.cs
    
    * Update source/dotnet/Services/WebApi.UnitTests/Infrastructure/Integration/CalculationResultReadyIntegrationEventFactoryTests.cs
    
    Co-authored-by: BjarkeMeier <BjarkeMeier@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: BjarkeMeier <BjarkeMeier@users.noreply.github.com>

--- source/dotnet/Services/WebApi.UnitTests/Infrastructure/Integration/CalculationResultReadyIntegrationEventFactoryTests.cs
+++ source/dotnet/Services/WebApi.UnitTests/Infrastructure/Integration/CalculationResultReadyIntegrationEventFactoryTests.cs
@@ -202,4 +202,93 @@ public class CalculationResultReadyIntegrationEventFactoryTests
             processCompletedEventDto,
             "AGlnNumber"));
     }
+
+    [Fact]
+    public void CreateCalculationResultCompletedForBalanceResponsibleParty_ReturnsResultForBalanceResponsibleParty()
+    {
+        // Arrange
+        var sut = new CalculationResultReadyIntegrationEventFactory();
+        var processStepResultDto = new ProcessStepResult(
+            TimeSeriesType.Production,
+            new TimeSeriesPoint[] { new(DateTimeOffset.Now, 10.0m, QuantityQuality.Estimated) });
+        var processCompletedEventDto = new ProcessCompletedEventDto(
+            "805",
+            Guid.NewGuid(),
+            ProcessType.Aggregation,
+            Instant.FromUtc(2022, 5, 1, 0, 0),
+            Instant.FromUtc(2022, 5, 2, 0, 0));
+
+        // Act
+        var actual = sut.CreateCalculationResultCompletedForBalanceResponsibleParty(
+            processStepResultDto,
+            processCompletedEventDto,
+            "ABrpGlnNumber");
+
+        // Assert
+        actual.AggregationPerGridarea.Should().BeNull();
+        actual.AggregationPerEnergysupplierPerBalanceresponsiblepartyPerGridarea.Should().BeNull();
+        actual.AggregationPerEnergysupplierPerGridarea.Should().BeNull();
+        actual.AggregationPerBalanceresponsiblepartyPerGridarea.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void CreateCalculationResultCompletedForBalanceResponsibleParty_WhenCreating_PropertiesAreMappedCorrectly()
+    {
+        // Arrange
+        var sut = new CalculationResultReadyIntegrationEventFactory();
+        var timeSeriesPoint = new TimeSeriesPoint(DateTimeOffset.Now, 10.101000000m, QuantityQuality.Estimated);
+        var processStepResultDto = new ProcessStepResult(
+            TimeSeriesType.Production,
+            new[] { timeSeriesPoint });
+        var processCompletedEventDto = new ProcessCompletedEventDto(
+            "805",
+            Guid.NewGuid(),
+            ProcessType.Aggregation,
+            Instant.FromUtc(2022, 5, 1, 0, 0),
+            Instant.FromUtc(2022, 5, 2, 0, 0));
+        var expectedGln = "TheBrpGln";
+
+        // Act
+        var actual = sut.CreateCalculationResultCompletedForBalanceResponsibleParty(
+            processStepResultDto,
+            processCompletedEventDto,
+            expectedGln);
+
+        // Assert
+        actual.AggregationPerBalanceresponsiblepartyPerGridarea.GridAreaCode.Should().Be(processCompletedEventDto.GridAreaCode);
+        actual.AggregationPerBalanceresponsiblepartyPerGridarea.BalanceResponsiblePartyGlnOrEic.Should().Be(expectedGln);
+        actual.BatchId.Should().Be(processCompletedEventDto.BatchId.ToString());
+        actual.Resolution.Should().Be(Resolution.Quarter);
+        actual.QuantityUnit.Should().Be(QuantityUnit.Kwh);
+        actual.PeriodEndUtc.Should().Be(processCompletedEventDto.PeriodEnd.ToTimestamp());
+        actual.PeriodStartUtc.Should().Be(processCompletedEventDto.PeriodStart.ToTimestamp());
+        actual.TimeSeriesType.Should().Be(TimeSeriesTypeMapper.MapTimeSeriesType(processStepResultDto.TimeSeriesType));
+
+        actual.TimeSeriesPoints[0].Quantity.Units.Should().Be(10);
+        actual.TimeSeriesPoints[0].Quantity.Nanos.Should().Be(101000000);
+        actual.TimeSeriesPoints[0].Time.Should().Be(timeSeriesPoint.Time.ToTimestamp());
+        actual.TimeSeriesPoints[0].QuantityQuality.Should().Be(QuantityQualityMapper.MapQuantityQuality(timeSeriesPoint.Quality));
+    }
+
+    [Fact]
+    public void CreateCalculationResultCompletedForBalanceResponsibleParty_WhenQuantityQualityCalculated_ExceptionIsThrown()
+    {
+        // Arrange
+        var sut = new CalculationResultReadyIntegrationEventFactory();
+        var processStepResultDto = new ProcessStepResult(
+            TimeSeriesType.Production,
+            new TimeSeriesPoint[] { new(DateTimeOffset.Now, 10.0m, QuantityQuality.Calculated) });
+        var processCompletedEventDto = new ProcessCompletedEventDto(
+            "805",
+            Guid.NewGuid(),
+            ProcessType.Aggregation,
+            Instant.FromUtc(2022, 5, 1, 0, 0),
+            Instant.FromUtc(2022, 5, 2, 0, 0));
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => sut.CreateCalculationResultCompletedForBalanceResponsibleParty(
+            processStepResultDto,
+            processCompletedEventDto,
+            "ABrpGlnNumber"));
+    }
 }
