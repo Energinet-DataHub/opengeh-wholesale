36cea3e51a4ddd55bb76832e0293ca41a0cf91f9
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2023-03-14 16:14:56 +0100

    fix: Handle 0-prefixed grid area codes correctly in calculator job args (#910)

--- source/databricks/package/args_helper.py
+++ source/databricks/package/args_helper.py
@@ -27,12 +27,21 @@ def valid_date(s: str) -> datetime:
 
 
 def valid_list(s: str) -> list[str]:
-    """See https://stackoverflow.com/questions/25470844/specify-date-format-for-python-argparse-input-arguments"""
-    try:
-        return ast.literal_eval(s)
-    except ValueError:
-        msg = "not a valid grid area list"
+    if not s.startswith("[") or not s.endswith("]"):
+        msg = "Grid area codes must be a list enclosed by an opening '[' and a closing ']'"
         raise configargparse.ArgumentTypeError(msg)
+
+    # 1. Remove enclosing list characters 2. Split each grid area code 3. Remove possibly enclosing spaces.
+    tokens = [token.strip() for token in s.strip("[]").split(",")]
+
+    # Grid area codes must always consist of 3 digits
+    if any(
+        len(token) != 3 or any(c < "0" or c > "9" for c in token) for token in tokens
+    ):
+        msg = "Grid area codes must consist of 3 digits"
+        raise configargparse.ArgumentTypeError(msg)
+
+    return tokens
 
 
 def valid_log_level(s: str) -> str:
