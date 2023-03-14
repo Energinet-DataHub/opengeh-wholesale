537290ff843cfab12b9cbfc29e2b3e3fddd0dc26
Chris Brohus <CBS@energinet.dk>
2023-03-14 09:47:47 +0100

    Remove filter

--- edi/terraform/main/sbs-integrations-events.tf
+++ edi/terraform/main/sbs-integrations-events.tf
@@ -3,6 +3,5 @@
   name                = local.WHOLESALE_PROCESS_COMPLETED_EVENT_SUBSCRIPTION_NAME
   topic_id            = data.azurerm_key_vault_secret.sbt_domainrelay_integrationevent_received_id.value
   project_name        = var.domain_name_short
-  max_delivery_count  = 10 
-  sql_filter = "sys.Label = 'BalanceFixingCompleted' OR sys.Label = 'BalanceFixingCalculationResultCompleted'"
+  max_delivery_count  = 10
 }
