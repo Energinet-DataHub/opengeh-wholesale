4ec0753796bd99f74052191139a3d3e5650cb3bd
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2023-03-15 11:00:36 +0100

    Simplify infrastructure build prerelease workflows (#190)

--- shared-resources/terraform/infrastructure/env/u-001/ra-security-group.tf
+++ shared-resources/terraform/infrastructure/env/u-001/ra-security-group.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "blob_reader" {
-  scope                 = data.azurerm_subscription.this.id
-  role_definition_name  = "Storage Blob Data Reader"
-  principal_id          = var.developers_security_group_object_id
+  scope                = data.azurerm_subscription.this.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = var.developers_security_group_object_id
 }
