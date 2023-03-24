65bee39a3b22336a140e2c860ddc60b899c774cf
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-03-24 21:37:51 +0100

    Debug

--- source/databricks/calculation-engine/package/environment_variables.py
+++ source/databricks/calculation-engine/package/environment_variables.py
@@ -42,4 +42,8 @@ def get_env_variable_or_throw(variable: EnvironmentVariable) -> Any:
 
 
 def get_env_variable(variable: EnvironmentVariable) -> Any:
+    print("debug start")
+    print(variable.value)
+    print(os.getenv(variable.value))
+    print("debug end")
     return os.getenv(variable.value)
