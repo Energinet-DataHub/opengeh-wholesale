50db770f9ab7bd58104a9007c726ff167bcc6f11
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-03-24 08:26:48 +0100

    WiP

--- source/databricks/calculation-engine/package/__init__.py
+++ source/databricks/calculation-engine/package/__init__.py
@@ -17,4 +17,3 @@ from .balance_fixing import calculate_balance_fixing
 from .db_logging import log, debug, loglevel
 from .args_helper import valid_date, valid_list, valid_log_level
 from .calculator_job import _map_cim_quality_to_wholesale_quality
-from .databricks_secrets import get_client_secret_credential
