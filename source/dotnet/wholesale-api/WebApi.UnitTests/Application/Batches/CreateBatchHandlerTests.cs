9f60d18615916e4cb918c17b472cff3820bc47b0
Alexander Harrow <122119488+AHdevelopment@users.noreply.github.com>
2023-03-27 11:29:07 +0200

    Create CreateBatchHandlerTests.cs

---
+++ source/dotnet/wholesale-api/WebApi.UnitTests/Application/Batches/CreateBatchHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using AutoFixture.Xunit2;
+using Energinet.DataHub.Core.TestCommon.AutoFixture.Attributes;
+using Energinet.DataHub.Wholesale.Application;
+using Energinet.DataHub.Wholesale.Application.Batches;
+using Moq;
+using Xunit;
+using Xunit.Categories;
+
+namespace Energinet.DataHub.Wholesale.WebApi.UnitTests.Application.Batches;
+
+[UnitTest]
+public class CreateBatchHandlerTests
+{
+    [Theory]
+    [InlineAutoMoqData]
+    public async Task Handle_CallsCommit(
+        [Frozen] Mock<IUnitOfWork> unitOfWorkMock,
+        CreateBatchCommand createBatchCommand,
+        CreateBatchHandler sut)
+    {
+        // Arrange
+
+        // Act
+        await sut.Handle(createBatchCommand, default);
+
+        // Assert
+        unitOfWorkMock.Verify(x => x.CommitAsync());
+    }
+}
