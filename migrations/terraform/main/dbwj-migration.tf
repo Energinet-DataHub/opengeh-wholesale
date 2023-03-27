f283b9c602abb5d6a6c9df3d0af10ddbd6939830
HenrikSommer <xheso@energinet.dk>
2023-03-27 13:22:02 +0200

    Add all-purpose cluster for manual execution

--- migrations/terraform/main/dbwj-migration.tf
+++ migrations/terraform/main/dbwj-migration.tf
@@ -41,6 +41,40 @@ resource "databricks_sql_global_config" "this" {
   }
 }
 
+resource "databricks_cluster" "shared_all_purpose" {
+  cluster_name            = "Shared all-purpose"
+  spark_version           = data.databricks_spark_version.latest_lts.id
+  node_type_id            = "Standard_DS5_v2"
+  autotermination_minutes = 15
+  num_workers             = 0
+  spark_conf = {
+    "fs.azure.account.oauth2.client.endpoint.${data.azurerm_storage_account.drop.name}.dfs.core.windows.net": "https://login.microsoftonline.com/${var.tenant_id}/oauth2/token"
+    "fs.azure.account.oauth2.client.endpoint.${module.st_migrations.name}.dfs.core.windows.net": "https://login.microsoftonline.com/${var.tenant_id}/oauth2/token"
+    "fs.azure.account.oauth2.client.endpoint.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net": "https://login.microsoftonline.com/${var.tenant_id}/oauth2/token"
+    "fs.azure.account.auth.type.${data.azurerm_storage_account.drop.name}.dfs.core.windows.net": "OAuth"
+    "fs.azure.account.auth.type.${module.st_migrations.name}.dfs.core.windows.net": "OAuth"
+    "fs.azure.account.auth.type.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net": "OAuth"
+    "fs.azure.account.oauth.provider.type.${data.azurerm_storage_account.drop.name}.dfs.core.windows.net": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider"
+    "fs.azure.account.oauth.provider.type.${module.st_migrations.name}.dfs.core.windows.net": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider"
+    "fs.azure.account.oauth.provider.type.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider"
+    "fs.azure.account.oauth2.client.id.${data.azurerm_storage_account.drop.name}.dfs.core.windows.net": databricks_secret.spn_app_id.config_reference
+    "fs.azure.account.oauth2.client.id.${module.st_migrations.name}.dfs.core.windows.net": databricks_secret.spn_app_id.config_reference
+    "fs.azure.account.oauth2.client.id.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net": databricks_secret.spn_app_id.config_reference
+    "fs.azure.account.oauth2.client.secret.${data.azurerm_storage_account.drop.name}.dfs.core.windows.net": databricks_secret.spn_app_secret.config_reference
+    "fs.azure.account.oauth2.client.secret.${module.st_migrations.name}.dfs.core.windows.net": databricks_secret.spn_app_secret.config_reference
+    "fs.azure.account.oauth2.client.secret.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net": databricks_secret.spn_app_secret.config_reference
+    "spark.databricks.delta.preview.enabled": true
+    "spark.databricks.io.cache.enabled": true
+    "spark.master": "local[*, 4]"
+  }
+  spark_env_vars = {
+    "APPI_INSTRUMENTATION_KEY" = databricks_secret.appi_instrumentation_key.config_reference
+    "LANDING_STORAGE_ACCOUNT" = data.azurerm_storage_account.drop.name
+    "DATALAKE_STORAGE_ACCOUNT" = module.st_migrations.name
+    "DATALAKE_SHARED_STORAGE_ACCOUNT" = data.azurerm_key_vault_secret.st_data_lake_name.value
+  }
+}
+
 resource "databricks_job" "this" {
   name = "Landing_To_Gold"
 
@@ -72,7 +106,7 @@ resource "databricks_job" "this" {
       }
       spark_env_vars = {
         "APPI_INSTRUMENTATION_KEY"        = databricks_secret.appi_instrumentation_key.config_reference
-        "LANDING_STORAGE_ACCOUNT"         = data.azurerm_storage_account.drop.name # Should we use this or another datalake for dump
+        "LANDING_STORAGE_ACCOUNT"         = data.azurerm_storage_account.drop.name
         "DATALAKE_STORAGE_ACCOUNT"        = module.st_migrations.name
         "DATALAKE_SHARED_STORAGE_ACCOUNT" = data.azurerm_key_vault_secret.st_data_lake_name.value
       }
@@ -107,7 +141,7 @@ resource "databricks_job" "this" {
       }
       spark_env_vars = {
         "APPI_INSTRUMENTATION_KEY"        = databricks_secret.appi_instrumentation_key.config_reference
-        "LANDING_STORAGE_ACCOUNT"         = data.azurerm_storage_account.drop.name # Should we use this or another datalake for dump
+        "LANDING_STORAGE_ACCOUNT"         = data.azurerm_storage_account.drop.name
         "DATALAKE_STORAGE_ACCOUNT"        = module.st_migrations.name
         "DATALAKE_SHARED_STORAGE_ACCOUNT" = data.azurerm_key_vault_secret.st_data_lake_name.value
       }
