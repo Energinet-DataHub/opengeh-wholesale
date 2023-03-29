a78e1743d91151068bc8ef046155e2ee15600dd6
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-03-29 15:05:00 +0200

    add utc to spark_conf

--- source/databricks/calculation-engine/package/spark_initializor.py
+++ source/databricks/calculation-engine/package/spark_initializor.py
@@ -17,4 +17,10 @@ from pyspark.sql.session import SparkSession
 
 
 def initialize_spark() -> SparkSession:
-    return SparkSession.builder.getOrCreate()
+    # Set spark config with the session timezone so that datetimes are displayed consistently (in UTC)
+    spark_conf = (
+        SparkConf(loadDefaults=True)
+        .set("spark.sql.session.timeZone", "UTC")
+        .set("spark.databricks.io.cache.enabled", "True")
+    )
+    return SparkSession.builder.config(conf=spark_conf).getOrCreate()
