0a7835b40007de2ec28b93cb923c987f1f9dfbc0
Alexander Harrow <122119488+AHdevelopment@users.noreply.github.com>
2023-04-13 20:39:59 +0200

    Updates

---
+++ source/dotnet/wholesale-api/WebApi.IntegrationTests/Application/RequestMatchingHandlerTests.cs
@@ -0,0 +1,72 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using AutoFixture.Xunit2;
+using Energinet.DataHub.Wholesale.Application.Base;
+using FluentAssertions;
+using MediatR;
+using Xunit;
+
+namespace Energinet.DataHub.Wholesale.WebApi.IntegrationTests.Application;
+
+public class RequestMatchingHandlerTests
+{
+    /// <summary>
+    /// A request can be either a command or a query.
+    /// </summary>
+    [Theory]
+    [InlineAutoData(typeof(Root))]
+    [InlineAutoData(typeof(Wholesale.Application.Root))]
+    [InlineAutoData(typeof(Domain.Root))]
+    [InlineAutoData(typeof(Wholesale.Infrastructure.Root))]
+    public void EachRequest_HasMatchingHandler(Type type)
+    {
+        // Arrange
+        var commandTypes = type.Assembly.GetTypes()
+            .Where(IsIRequest)
+            .ToList();
+
+        var handlerTypes = type.Assembly.GetTypes()
+            .Where(IsRequestHandler)
+            .ToList();
+
+        // Act
+        foreach (var requestType in commandTypes)
+        {
+            // Assert
+            handlerTypes.Should().ContainSingle(handlerType => IsHandlerForCommand(handlerType, requestType), $"handler for type {requestType} expected");
+        }
+    }
+
+    private static bool IsIRequest(Type type)
+    {
+        // Ignore these interfaces as they don't have a handler
+        if (type == typeof(ICommand) || type == typeof(ICommand<>) || type == typeof(IQuery<>))
+        {
+            return false;
+        }
+
+        return typeof(IBaseRequest).IsAssignableFrom(type);
+    }
+
+    private static bool IsRequestHandler(Type type)
+    {
+        return type.GetInterfaces().Any(interfaceType => interfaceType.IsGenericType && interfaceType.GetGenericTypeDefinition() == typeof(IRequestHandler<,>));
+    }
+
+    private static bool IsHandlerForCommand(Type handlerType, Type requestType)
+    {
+        return handlerType.GetInterfaces().Any(i => i.GenericTypeArguments.Any(ta => ta == requestType));
+    }
+}
