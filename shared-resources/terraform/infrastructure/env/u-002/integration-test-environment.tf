c6ac4a331d75f2d6f0830709ddd4c9c679cd413e
HenrikSommer <xheso@energinet.dk>
2023-04-25 10:15:02 +0200

    Update tenant id

--- shared-resources/terraform/infrastructure/env/u-002/integration-test-environment.tf
+++ shared-resources/terraform/infrastructure/env/u-002/integration-test-environment.tf
@@ -527,7 +527,7 @@ resource "databricks_cluster" "shared_all_purpose" {
   instance_pool_id        = databricks_instance_pool.my_pool.id
   spark_version           = data.databricks_spark_version.latest_lts.id
   spark_conf = {
-    "fs.azure.account.oauth2.client.endpoint.${azurerm_storage_account.playground.name}.dfs.core.windows.net" : "https://login.microsoftonline.com/${var.tenant_id}/oauth2/token"
+    "fs.azure.account.oauth2.client.endpoint.${azurerm_storage_account.playground.name}.dfs.core.windows.net" : "https://login.microsoftonline.com/${data.azurerm_client_config.this.tenant_id}/oauth2/token"
     "fs.azure.account.auth.type.${azurerm_storage_account.playground.name}.dfs.core.windows.net" : "OAuth"
     "fs.azure.account.oauth.provider.type.${azurerm_storage_account.playground.name}.dfs.core.windows.net" : "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider"
     "fs.azure.account.oauth2.client.id.${azurerm_storage_account.playground.name}.dfs.core.windows.net" : databricks_secret.spn_app_id.config_reference
