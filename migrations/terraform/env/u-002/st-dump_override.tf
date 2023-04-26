812e2f723dc4a2d616fce7b645b870f9e1632222
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2023-04-26 13:24:41 +0200

    Update storage account name for u002 (#243)
    Change from sttsdropdatmigendku2 to sttsdropdatmigendku002

--- migrations/terraform/env/u-002/st-dump_override.tf
+++ migrations/terraform/env/u-002/st-dump_override.tf
@@ -4,6 +4,6 @@ data "azurerm_resource_group" "migrations" {
 }
 data "azurerm_storage_account" "drop" {
   #Name in U-002 differs from all other environments
-  name                = "sttsdropdatmigendk${lower(var.environment_short)}2"
+  name                = "sttsdropdatmigendk${lower(var.environment_short)}${var.environment_instance}"
   resource_group_name = data.azurerm_resource_group.migrations.name
 }
