4611fcca88b0ec53eb7b35b7c36a296258ee4b93
Lasse Ringgren Nielsen <LRN@energinet.dk>
2023-05-08 11:11:34 +0200

    refac: populate Batches module (#1110)
    #972

---
+++ source/dotnet/wholesale-api/Batches/Batches.Infrastructure/BatchAggregate/Batch.cs
@@ -0,0 +1,216 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Wholesale.Batches.Infrastructure.GridAreaAggregate;
+using Energinet.DataHub.Wholesale.Contracts;
+using NodaTime;
+
+namespace Energinet.DataHub.Wholesale.Batches.Infrastructure.BatchAggregate;
+
+public class Batch
+{
+    private readonly List<GridAreaCode> _gridAreaCodes;
+
+    public Batch(
+        ProcessType processType,
+        List<GridAreaCode> gridAreaCodes,
+        Instant periodStart,
+        Instant periodEnd,
+        Instant executionTimeStart,
+        DateTimeZone dateTimeZone)
+        : this()
+    {
+        _gridAreaCodes = gridAreaCodes.ToList();
+        if (!IsValid(_gridAreaCodes, periodStart, periodEnd, dateTimeZone, out var errorMessages))
+            throw new BusinessValidationException(string.Join(" ", errorMessages));
+
+        ExecutionState = BatchExecutionState.Created;
+        ProcessType = processType;
+        PeriodStart = periodStart;
+        PeriodEnd = periodEnd;
+        ExecutionTimeStart = executionTimeStart;
+        ExecutionTimeEnd = null;
+        AreSettlementReportsCreated = false;
+    }
+
+    /// <summary>
+    /// Validate if parameters are valid for a Batch.
+    /// </summary>
+    /// <param name="gridAreaCodes"></param>
+    /// <param name="periodStart"></param>
+    /// <param name="periodEnd"></param>
+    /// <param name="dateTimeZone"></param>
+    /// <param name="validationErrors"></param>
+    /// <returns>If the parameters are valid for a Batch</returns>
+    public static bool IsValid(
+        IEnumerable<GridAreaCode> gridAreaCodes,
+        Instant periodStart,
+        Instant periodEnd,
+        DateTimeZone dateTimeZone,
+        out IEnumerable<string> validationErrors)
+    {
+        var errors = new List<string>();
+
+        if (!gridAreaCodes.Any())
+            errors.Add("Batch must contain at least one grid area code.");
+
+        if (periodStart >= periodEnd)
+            errors.Add("periodStart is greater or equal to periodEnd");
+
+        // Validate that period end is set to midnight
+        if (new ZonedDateTime(periodEnd, dateTimeZone).TimeOfDay != LocalTime.Midnight)
+            errors.Add($"The period end '{periodEnd.ToString()}' must be midnight.");
+
+        if (new ZonedDateTime(periodStart, dateTimeZone).TimeOfDay != LocalTime.Midnight)
+            errors.Add($"The period start '{periodStart.ToString()}' must be midnight.");
+
+        validationErrors = errors;
+        return !errors.Any();
+    }
+
+    /// <summary>
+    /// Required by Entity Framework
+    /// </summary>
+    // ReSharper disable once UnusedMember.Local
+    private Batch()
+    {
+        Id = Guid.NewGuid();
+        _gridAreaCodes = new List<GridAreaCode>();
+    }
+
+    // Private setter is used implicitly by tests
+    public Guid Id { get; private set; }
+
+    public ProcessType ProcessType { get; }
+
+    public IReadOnlyCollection<GridAreaCode> GridAreaCodes => _gridAreaCodes;
+
+    public BatchExecutionState ExecutionState { get; private set; }
+
+    public Instant ExecutionTimeStart { get; private set; }
+
+    public Instant? ExecutionTimeEnd { get; private set; }
+
+    public CalculationId? CalculationId { get; private set; }
+
+    /// <summary>
+    /// Must be exactly at the beginning (at 00:00:00 o'clock) of the local date.
+    /// </summary>
+    public Instant PeriodStart { get; }
+
+    /// <summary>
+    /// Must be exactly 1 ms before the end (midnight) of the local date.
+    /// The 1 ms off is by design originating from the front-end decision on how to handle date periods.
+    /// </summary>
+    public Instant PeriodEnd { get; }
+
+    public bool AreSettlementReportsCreated { get; set; }
+
+    /// <summary>
+    /// Get the ISO 8601 duration for the given process type.
+    /// </summary>
+    public string GetResolution()
+    {
+        switch (ProcessType)
+        {
+            case ProcessType.BalanceFixing:
+            case ProcessType.Aggregation:
+                return "PT15M";
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Get the unit for result values (an energy unit for aggregations and a price unit/currency for settlements).
+    /// </summary>
+    public string GetQuantityUnit()
+    {
+        switch (ProcessType)
+        {
+            case ProcessType.BalanceFixing:
+            case ProcessType.Aggregation:
+                return "kWh";
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    public void MarkAsSubmitted(CalculationId calculationId)
+    {
+        ArgumentNullException.ThrowIfNull(calculationId);
+        if (ExecutionState is BatchExecutionState.Submitted or BatchExecutionState.Pending
+            or BatchExecutionState.Executing or BatchExecutionState.Completed or BatchExecutionState.Failed)
+        {
+            ThrowInvalidStateTransitionException(ExecutionState, BatchExecutionState.Submitted);
+        }
+
+        CalculationId = calculationId;
+        ExecutionState = BatchExecutionState.Submitted;
+    }
+
+    public void MarkAsPending()
+    {
+        if (ExecutionState is BatchExecutionState.Pending or BatchExecutionState.Executing or BatchExecutionState.Completed or BatchExecutionState.Failed)
+            ThrowInvalidStateTransitionException(ExecutionState, BatchExecutionState.Pending);
+        ExecutionState = BatchExecutionState.Pending;
+    }
+
+    public void MarkAsExecuting()
+    {
+        if (ExecutionState is BatchExecutionState.Executing or BatchExecutionState.Completed or BatchExecutionState.Failed)
+            ThrowInvalidStateTransitionException(ExecutionState, BatchExecutionState.Executing);
+
+        ExecutionState = BatchExecutionState.Executing;
+    }
+
+    public void MarkAsCompleted(Instant executionTimeEnd)
+    {
+        if (ExecutionState is BatchExecutionState.Completed or BatchExecutionState.Failed)
+            ThrowInvalidStateTransitionException(ExecutionState, BatchExecutionState.Completed);
+
+        if (executionTimeEnd < ExecutionTimeStart)
+        {
+            throw new BusinessValidationException(
+                $"Execution time end '{executionTimeEnd}' cannot be before execution time start '{ExecutionTimeStart}'");
+        }
+
+        ExecutionState = BatchExecutionState.Completed;
+        ExecutionTimeEnd = executionTimeEnd;
+    }
+
+    public void MarkAsFailed()
+    {
+        if (ExecutionState is BatchExecutionState.Failed)
+            ThrowInvalidStateTransitionException(ExecutionState, BatchExecutionState.Failed);
+
+        ExecutionState = BatchExecutionState.Failed;
+    }
+
+    /// <summary>
+    /// Reset a batch. This will ensure that it will be picked up and run again in a new calculation.
+    /// </summary>
+    public void Reset()
+    {
+        if (ExecutionState is BatchExecutionState.Completed)
+            ThrowInvalidStateTransitionException(ExecutionState, BatchExecutionState.Created);
+
+        ExecutionState = BatchExecutionState.Created;
+    }
+
+    private void ThrowInvalidStateTransitionException(BatchExecutionState currentState, BatchExecutionState desiredState)
+    {
+        throw new BusinessValidationException($"Cannot change batchExecutionState from {currentState} to {desiredState}");
+    }
+}
--- source/dotnet/wholesale-api/WebApi/Configuration/ServiceCollectionExtensions.cs
+++ source/dotnet/wholesale-api/WebApi/Configuration/ServiceCollectionExtensions.cs
@@ -19,11 +19,17 @@ using Energinet.DataHub.Core.App.FunctionApp.Middleware.CorrelationId;
 using Energinet.DataHub.Core.App.WebApp.Authentication;
 using Energinet.DataHub.Core.App.WebApp.Authorization;
 using Energinet.DataHub.Core.JsonSerialization;
-using Energinet.DataHub.Wholesale.Application;
-using Energinet.DataHub.Wholesale.Application.Batches;
-using Energinet.DataHub.Wholesale.Application.Batches.Model;
 using Energinet.DataHub.Wholesale.Application.Processes.Model;
 using Energinet.DataHub.Wholesale.Application.SettlementReport;
+using Energinet.DataHub.Wholesale.Batches.Application;
+using Energinet.DataHub.Wholesale.Batches.Application.Model;
+using Energinet.DataHub.Wholesale.Batches.Infrastructure.BatchAggregate;
+using Energinet.DataHub.Wholesale.Batches.Infrastructure.BatchExecutionStateDomainService;
+using Energinet.DataHub.Wholesale.Batches.Infrastructure.CalculationDomainService;
+using Energinet.DataHub.Wholesale.Batches.Infrastructure.Calculations;
+using Energinet.DataHub.Wholesale.Batches.Infrastructure.Persistence;
+using Energinet.DataHub.Wholesale.Batches.Infrastructure.Persistence.Batches;
+using Energinet.DataHub.Wholesale.Batches.Interfaces;
 using Energinet.DataHub.Wholesale.CalculationResults.Application;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.BatchActor;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.DataLake;
@@ -36,18 +42,16 @@ using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.ProcessStep.Mode
 using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.SettlementReport;
 using Energinet.DataHub.Wholesale.Components.DatabricksClient.DatabricksWheelClient;
 using Energinet.DataHub.Wholesale.Domain.BatchAggregate;
-using Energinet.DataHub.Wholesale.Domain.BatchExecutionStateDomainService;
-using Energinet.DataHub.Wholesale.Domain.CalculationDomainService;
-using Energinet.DataHub.Wholesale.Infrastructure.Calculations;
 using Energinet.DataHub.Wholesale.Infrastructure.Core;
 using Energinet.DataHub.Wholesale.Infrastructure.EventPublishers;
 using Energinet.DataHub.Wholesale.Infrastructure.Persistence;
-using Energinet.DataHub.Wholesale.Infrastructure.Persistence.Batches;
 using Energinet.DataHub.Wholesale.WebApi.Configuration.Options;
 using Energinet.DataHub.Wholesale.WebApi.V3.ProcessStepResult;
 using Microsoft.EntityFrameworkCore;
 using NodaTime;
+using IUnitOfWork = Energinet.DataHub.Wholesale.Application.IUnitOfWork;
 using ProcessTypeMapper = Energinet.DataHub.Wholesale.Application.Processes.Model.ProcessTypeMapper;
+using UnitOfWork = Energinet.DataHub.Wholesale.Infrastructure.Persistence.UnitOfWork;
 
 namespace Energinet.DataHub.Wholesale.WebApi.Configuration;
 
@@ -65,6 +69,17 @@ internal static class ServiceCollectionExtensions
 
     public static void AddCommandStack(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
+        serviceCollection.AddDbContext<IntegrationEventPublishingDatabaseContext>(
+            options => options.UseSqlServer(
+                configuration
+                    .GetSection(ConnectionStringsOptions.ConnectionStrings)
+                    .Get<ConnectionStringsOptions>()!.DB_CONNECTION_STRING,
+                o =>
+                {
+                    o.UseNodaTime();
+                    o.EnableRetryOnFailure();
+                }));
+
         serviceCollection.AddDbContext<DatabaseContext>(
             options => options.UseSqlServer(
                 configuration
@@ -77,8 +92,11 @@ internal static class ServiceCollectionExtensions
                 }));
 
         serviceCollection.AddScoped<IClock>(_ => SystemClock.Instance);
+        serviceCollection.AddScoped<IIntegrationEventPublishingDatabaseContext, IntegrationEventPublishingDatabaseContext>();
         serviceCollection.AddScoped<IDatabaseContext, DatabaseContext>();
+        // This is a temporary fix until we move registration out to each of the modules
         serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+        serviceCollection.AddScoped<Energinet.DataHub.Wholesale.Batches.Infrastructure.Persistence.IUnitOfWork, Energinet.DataHub.Wholesale.Batches.Infrastructure.Persistence.UnitOfWork>();
         serviceCollection.AddScoped<IBatchApplicationService, BatchApplicationService>();
         serviceCollection.AddScoped<ISettlementReportApplicationService, SettlementReportApplicationService>();
         serviceCollection.AddScoped<ISettlementReportRepository, SettlementReportRepository>();
@@ -118,7 +136,7 @@ internal static class ServiceCollectionExtensions
         var dataLakeOptions = configuration.Get<DataLakeOptions>()!;
         serviceCollection.AddHealthChecks()
             .AddLiveCheck()
-            .AddDbContextCheck<DatabaseContext>(name: "SqlDatabaseContextCheck")
+            .AddDbContextCheck<IntegrationEventPublishingDatabaseContext>(name: "SqlDatabaseContextCheck")
             .AddDataLakeContainerCheck(dataLakeOptions.STORAGE_ACCOUNT_URI, dataLakeOptions.STORAGE_CONTAINER_NAME)
             .AddAzureServiceBusTopic(
                 serviceBusOptions.SERVICE_BUS_MANAGE_CONNECTION_STRING,
