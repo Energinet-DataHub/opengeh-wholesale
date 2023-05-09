c22cc7b1a82edce032e153b5424479310a5d4515
Lasse Ringgren Nielsen <sixxth@gmail.com>
2023-05-09 07:33:32 +0200

    namespace updates

--- source/dotnet/wholesale-api/WebApi/V3/Batch/BatchRequestDto.cs
+++ source/dotnet/wholesale-api/WebApi/V3/Batch/BatchRequestDto.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.Wholesale.Batches.Interfaces.Models;
-
 namespace Energinet.DataHub.Wholesale.WebApi.V3.Batch;
 
 /// <summary>
