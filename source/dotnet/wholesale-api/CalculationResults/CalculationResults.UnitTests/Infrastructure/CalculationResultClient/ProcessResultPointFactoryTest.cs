d523820d7be26d7078239b65f91d2a90b811ab29
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-05-09 18:26:18 +0200

    Revert "refac: Trigger batch execution state updates from web API (#1121)"
    This reverts commit cc56c833

---
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.UnitTests/Infrastructure/CalculationResultClient/ProcessResultPointFactoryTest.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.TestCommon.AutoFixture.Attributes;
+using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.CalculationResultClient;
+using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResultClient;
+using FluentAssertions;
+using Xunit;
+using Xunit.Categories;
+
+namespace Energinet.DataHub.Wholesale.CalculationResults.UnitTests.Infrastructure.CalculationResultClient;
+
+[UnitTest]
+public class ProcessResultPointFactoryTest
+{
+    [Fact]
+    public void Create_WhenInputIsNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var stream = EmbeddedResources.GetStream("Infrastructure.CalculationResultClient.CalculationResult.json");
+        using var reader = new StreamReader(stream);
+        var json = reader.ReadToEnd();
+        var sut = new ProcessResultPointFactory();
+
+        // Act
+        sut.Create(json);
+    }
+}
