8b5d8672dfd03ae44db9b729959949a92728176d
Mads Due <madsmagnusdue@hotmail.com>
2023-05-15 15:02:23 +0200

    align u001 and u002 environments

--- edi/terraform/env/u-002/kv-shared-resources-additional.tf
+++
@@ -1,8 +0,0 @@
-#
-# Due to Azure limitation of integration subnet can be used by only one App Service plan, 
-# we use the "old" integrations-subnet for VNet integrating the applications host in this environment
-#
-data "azurerm_key_vault_secret" "snet_vnet_integrations_id_performance_test" {
-  name         = "snet-vnet-integrations-id"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
--- edi/terraform/env/u-002/variables-additional.tf
+++
@@ -1,4 +0,0 @@
-variable ad_group_directory_reader {
-  type          = string
-  description   = "(Required) Name of a Active Directory group with the Directory Reader permission."
-}
