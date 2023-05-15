4abf4fdf79d1c2355c53f1c8fa9dc2863b2766ce
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2023-05-15 14:19:10 +0200

    refac: Move settlement reports to correct module (#1135)

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.Application/ISettlementReportRepository.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.Application/ISettlementReportRepository.cs
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.Wholesale.CalculationResults.Interfaces.SettlementReport;
+using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.SettlementReport;
+
+namespace Energinet.DataHub.Wholesale.CalculationResults.Application;
 
 public interface ISettlementReportRepository
 {
