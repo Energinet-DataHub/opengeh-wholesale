12c3deec31319be9cf05ac90ddd827c768a7c68c
HenrikSommer <xheso@energinet.dk>
2023-05-17 13:20:30 +0200

    Move SQL Warehouse setup to env

--- migrations/terraform/env/t-001/dbw-warehouse.tf
+++ migrations/terraform/env/t-001/dbw-warehouse.tf
@@ -1,9 +1,9 @@
 # SQL warehouse to host Databricks Alerts and Dashboard queries
 resource "databricks_sql_endpoint" "this" {
-  name             = "Migration endpoint"
+  name             = "Migration SQL endpoint"
   cluster_size     = "Small"
   max_num_clusters = 1
-  auto_stop_mins   = 60
+  auto_stop_mins   = 10
   enable_serverless_compute = true
   warehouse_type   = "PRO"
 }
