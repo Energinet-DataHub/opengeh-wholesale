d5be085303eb2a3731b8ee5f32387dcce83db629
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2023-05-23 12:50:01 +0200

    Test IP rules on SB namespace for GitHub runners (#286)

--- shared-resources/terraform/infrastructure/main/sb-domainrelay.tf
+++ shared-resources/terraform/infrastructure/main/sb-domainrelay.tf
@@ -1,14 +1,15 @@
 module "sb_domain_relay" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-namespace?ref=v11"
 
-  name                       = "domain-relay"
-  project_name               = var.domain_name_short
-  environment_short          = var.environment_short
-  environment_instance       = var.environment_instance
-  resource_group_name        = azurerm_resource_group.this.name
-  location                   = azurerm_resource_group.this.location
-  private_endpoint_subnet_id = module.snet_private_endpoints.id
-  log_analytics_workspace_id = module.log_workspace_shared.id
+  name                          = "domain-relay"
+  project_name                  = var.domain_name_short
+  environment_short             = var.environment_short
+  environment_instance          = var.environment_instance
+  resource_group_name           = azurerm_resource_group.this.name
+  location                      = azurerm_resource_group.this.location
+  private_endpoint_subnet_id    = module.snet_private_endpoints.id
+  log_analytics_workspace_id    = module.log_workspace_shared.id
+  ip_restriction_allow_ip_range = var.hosted_deployagent_public_ip_range
   auth_rules = [
     {
       name   = "listen",
