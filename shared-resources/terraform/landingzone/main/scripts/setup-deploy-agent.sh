67f5c2f83424f5fff5f5a4012bdf235783e0d621
Kristian Bank Erbou <contact@kbeconsult.dk>
2023-05-23 11:08:03 +0200

    Remove .NET 6 from deployagents (#281)

--- shared-resources/terraform/landingzone/main/scripts/setup-deploy-agent.sh
+++ shared-resources/terraform/landingzone/main/scripts/setup-deploy-agent.sh
@@ -169,12 +169,6 @@ rm packages-microsoft-prod.deb
 # See https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#how-to-install-other-versions
 #
 
-# .NET SDK 6.0
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-6.0
-
 # .NET SDK 7.0
 sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
