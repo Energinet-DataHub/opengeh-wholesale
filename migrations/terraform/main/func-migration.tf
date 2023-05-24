dba44fe322f1be07fbfe252d4a8221e0a7fb166f
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2023-05-24 09:22:54 +0200

    feat: Create synchronization function (#287)
    Create Migration function app i Azure.

---
+++ migrations/terraform/main/func-migration.tf
@@ -0,0 +1,19 @@
+module "func_migration" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=v11"
+
+  name                                     = "migration"
+  project_name                             = var.domain_name_short
+  environment_short                        = var.environment_short
+  environment_instance                     = var.environment_instance
+  resource_group_name                      = azurerm_resource_group.this.name
+  location                                 = azurerm_resource_group.this.location
+  vnet_integration_subnet_id               = data.azurerm_key_vault_secret.snet_vnet_integrations_id.value
+  private_endpoint_subnet_id               = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
+  app_service_plan_id                      = data.azurerm_key_vault_secret.plan_shared_id.value
+  application_insights_instrumentation_key = data.azurerm_key_vault_secret.appi_instrumentation_key.value
+  log_analytics_workspace_id               = data.azurerm_key_vault_secret.log_shared_id.value
+  always_on                                = true
+  dotnet_framework_version                 = "v7.0"
+  use_dotnet_isolated_runtime              = true
+  health_check_path                        = "/api/monitor/ready"
+}
