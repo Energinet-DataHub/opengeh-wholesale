38be987af895ff3d90456cef56d58dd083fc280a
BjarkeMeier <hej@bjarkemeier.dk>
2023-05-26 15:51:44 +0200

    fix: Workaround for broken database migrations

---
+++ source/dotnet/database-migration/Scripts/202305261500_Add_Batch_CreatedTime.sql
@@ -0,0 +1,14 @@
+﻿-- This guard was added as a workaround because apparently the migration was run without being registered
+-- and thus failing when running subsequently (in t-001).
+IF NOT EXISTS (
+   SELECT 1
+   FROM sys.columns
+   WHERE Name = 'CreatedTime' AND Object_ID = Object_ID('batches.Batch')
+)
+BEGIN
+    ALTER TABLE batches.Batch
+        ADD CreatedTime DATETIME2 NOT NULL DEFAULT GETDATE();
+    
+    UPDATE batches.Batch
+    SET CreatedTime = ExecutionTimeStart;
+END
