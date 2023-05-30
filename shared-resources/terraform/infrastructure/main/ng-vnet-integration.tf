29407b04574259c1c391f65808c0408109d73f2c
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2023-05-30 09:03:02 +0200

    Add NAT gateway and Public IP prefix for VNet integration subnet (#293)

---
+++ shared-resources/terraform/infrastructure/main/ng-vnet-integration.tf
@@ -0,0 +1,23 @@
+resource "azurerm_public_ip_prefix" "vnet_integration_public_ip_prefix" {
+  name                = "ippre-vnet-integration"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  prefix_length       = 28
+}
+
+resource "azurerm_nat_gateway" "nat_gateway" {
+  name                = "ng-vnet-integration"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  sku_name            = "Standard"
+}
+
+resource "azurerm_nat_gateway_public_ip_prefix_association" "nat_gateway_public_ip_prefix_association" {
+  public_ip_prefix_id = azurerm_public_ip_prefix.vnet_integration_public_ip_prefix.id
+  nat_gateway_id      = azurerm_nat_gateway.nat_gateway.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "vnet_integration_nat_gateway_association" {
+  subnet_id      = module.snet_vnet_integration.id
+  nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
+}
