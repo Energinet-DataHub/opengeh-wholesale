7f27e844613278ef749095bd264049f0484b4ce9
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-05-30 12:10:49 +0200

    Resolve merge conflicts

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/CalculationResultClient/SettlementReportSqlStatementFactory.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/CalculationResultClient/SettlementReportSqlStatementFactory.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.CalculationResultClient.DeltaTableConstants;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.CalculationResultClient.Mappers;
 using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResultClient;
 using Energinet.DataHub.Wholesale.Common.Models;
--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.UnitTests/Infrastructure/CalculationResultClient/SettlementReportSqlStatementFactoryTests.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.UnitTests/Infrastructure/CalculationResultClient/SettlementReportSqlStatementFactoryTests.cs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.CalculationResultClient;
+using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.CalculationResultClient.DeltaTableConstants;
 using Energinet.DataHub.Wholesale.Common.Models;
 using FluentAssertions;
 using NodaTime;
