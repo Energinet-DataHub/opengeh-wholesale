d2892a35fa7fa5cff79bcf080fe64c4f3142c292
BjarkeMeier <hej@bjarkemeier.dk>
2023-05-31 10:02:49 +0200

    refac: Rename integration event publishing module to `Events`

---
+++ source/dotnet/wholesale-api/Events/Events.Application/UseCases/PublishCalculationResultsHandler.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Wholesale.Events.Application.Processes;
+using Energinet.DataHub.Wholesale.Events.Application.Processes.Model;
+using NodaTime;
+
+namespace Energinet.DataHub.Wholesale.Events.Application.UseCases;
+
+public class PublishCalculationResultsHandler : IPublishCalculationResultsHandler
+{
+    private readonly ICompletedBatchRepository _completedBatchRepository;
+    private readonly IProcessApplicationService _processApplicationService;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IClock _clock;
+
+    public PublishCalculationResultsHandler(ICompletedBatchRepository completedBatchRepository, IProcessApplicationService processApplicationService, IUnitOfWork unitOfWork, IClock clock)
+    {
+        _completedBatchRepository = completedBatchRepository;
+        _processApplicationService = processApplicationService;
+        _unitOfWork = unitOfWork;
+        _clock = clock;
+    }
+
+    public async Task PublishCalculationResultsAsync()
+    {
+        do
+        {
+            var batch = await _completedBatchRepository.GetNextUnpublishedOrNullAsync().ConfigureAwait(false);
+            if (batch == null) break;
+
+            // TODO BJM: Reuse existing functionality of ProcessApplicationService and refactor in upcoming PR
+            foreach (var gridAreaCode in batch.GridAreaCodes)
+            {
+                var @event = new ProcessCompletedEventDto(gridAreaCode, batch.Id, batch.ProcessType, batch.PeriodStart, batch.PeriodEnd);
+                await _processApplicationService.PublishCalculationResultCompletedIntegrationEventsAsync(@event).ConfigureAwait(false);
+            }
+
+            batch.PublishedTime = _clock.GetCurrentInstant();
+            await _unitOfWork.CommitAsync().ConfigureAwait(false);
+        }
+        while (true);
+    }
+}
--- source/dotnet/wholesale-api/WebApi.UnitTests/Infrastructure/InstantExtensionsTests.cs
+++ source/dotnet/wholesale-api/WebApi.UnitTests/Infrastructure/InstantExtensionsTests.cs
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.Wholesale.IntegrationEventPublishing.Infrastructure;
-using Energinet.DataHub.Wholesale.IntegrationEventPublishing.Infrastructure.IntegrationEvents;
-using Energinet.DataHub.Wholesale.IntegrationEventPublishing.Infrastructure.IntegrationEvents.Types;
+using Energinet.DataHub.Wholesale.Events.Infrastructure.IntegrationEvents.Types;
 using FluentAssertions;
 using NodaTime;
 using Xunit;
