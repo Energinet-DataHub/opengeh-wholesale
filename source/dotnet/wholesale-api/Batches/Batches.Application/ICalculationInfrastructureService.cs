7d2337d486e1c080df6c6c93b3a5d017cd0c00b5
Jonas Michelsen <jvm@energinet.dk>
2023-06-01 08:47:06 +0200

    move calculation state

--- source/dotnet/wholesale-api/Batches/Batches.Application/ICalculationInfrastructureService.cs
+++ source/dotnet/wholesale-api/Batches/Batches.Application/ICalculationInfrastructureService.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.Wholesale.Batches.Application.Model;
 using Energinet.DataHub.Wholesale.Batches.Application.Model.Batches;
 
 namespace Energinet.DataHub.Wholesale.Batches.Application;
