9223d6c371b0081b5b898b228b4efd663d1fcaca
Dan Stenrøjl <XDAST@energinet.dk>
2023-06-14 14:59:41 +0200

    Move classes to TestCommon namespace

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Fixtures/TestCommon/DatabricksWarehouseSettings.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Fixtures/TestCommon/DatabricksWarehouseSettings.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.Wholesale.CalculationResults.IntegrationTests.Fixtures
+namespace Energinet.DataHub.Wholesale.CalculationResults.IntegrationTests.Fixtures.TestCommon
 {
     /// <summary>
     /// Settings used by <see cref="DatabricksWarehouseManager"/>.
