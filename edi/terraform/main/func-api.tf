3413a2391ffdee68567ef0006761513ec47cd9b2
Martin Hvidberg <xmahv@energinet.dk>
2023-07-04 10:47:35 +0200

    removed line

--- edi/terraform/main/func-api.tf
+++ edi/terraform/main/func-api.tf
@@ -9,7 +9,6 @@ module "func_receiver" {
   location                                 = azurerm_resource_group.this.location
   app_service_plan_id                      = data.azurerm_key_vault_secret.plan_shared_id.value
   application_insights_instrumentation_key = data.azurerm_key_vault_secret.appi_instrumentation_key.value
-  log_analytics_workspace_id               = data.azurerm_key_vault_secret.log_shared_id.value
   vnet_integration_subnet_id               = data.azurerm_key_vault_secret.snet_vnet_integrations_id.value
   private_endpoint_subnet_id               = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
   always_on                                = true
