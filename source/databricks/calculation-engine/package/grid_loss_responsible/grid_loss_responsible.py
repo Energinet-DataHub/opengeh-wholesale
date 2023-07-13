b3a64cc4ac962ceda001bce47c9d1d54e021f379
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-07-13 08:35:11 +0200

    Revert "refac: read grid loss responsibles from csv file (#1296)"
    This reverts commit cdff9ad6d94ca94abf544585c99f77020a71e2bf.

--- source/databricks/calculation-engine/package/grid_loss_responsible/grid_loss_responsible.py
+++ source/databricks/calculation-engine/package/grid_loss_responsible/grid_loss_responsible.py
@@ -21,11 +21,13 @@ from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StringType, StructType, StructField, TimestampType
 from package.constants import Colname
 from pyspark.sql.functions import col, when
+from datetime import datetime
 
 
 def get_grid_loss_responsible() -> DataFrame:
+    # script_dir = os.path.dirname(os.path.normpath(__file__))
 
-    file_path = os.path.join(os.path.dirname(__file__), 'GridLossResponsible.csv')
+    # file_path = os.path.join(script_dir, 'GridLossResponsible.csv')
 
     schema = StructType([
         StructField("METERING_POINT_ID", StringType(), nullable=False),
@@ -37,7 +39,25 @@ def get_grid_loss_responsible() -> DataFrame:
     ])
 
     spark = SparkSession.builder.getOrCreate()
-    grid_loss_responsible_df = spark.read.option("header", True).csv(file_path, schema=schema)
+    # grid_loss_responsible_df = spark.read.option("header", True).csv(file_path, schema=schema)
+
+    default_valid_from = datetime.strptime("2000-01-01T23:00:00+0000", "%Y-%m-%dT%H:%M:%S%z")
+    data = [
+        ('571313180480500149', 804, default_valid_from, None, 'production', '8100000000108'),
+        ('570715000000682292', 512, default_valid_from, None, 'production', '5790002437717'),
+        ('571313154313676325', 543, default_valid_from, None, 'production', '5790002437717'),
+        ('571313153313676335', 533, default_valid_from, None, 'production', '5790002437717'),
+        ('571313154391364862', 584, default_valid_from, None, 'production', '5790002437717'),
+        ('579900000000000026', 990, default_valid_from, None, 'production', '4260024590017'),
+        ('571313180300014979', 803, default_valid_from, None, 'production', '8100000000108'),
+        ('571313180400100657', 804, default_valid_from, None, 'consumption', '8100000000115'),
+        ('578030000000000012', 803, default_valid_from, None, 'consumption', '8100000000108'),
+        ('571313154312753911', 543, default_valid_from, None, 'consumption', '5790001103095'),
+        ('571313153308031507', 533, default_valid_from, None, 'consumption', '5790001102357'),
+        ('571313158410300060', 584, default_valid_from, None, 'consumption', '5790001103095')
+    ]
+
+    grid_loss_responsible_df = spark.createDataFrame(data, schema)
 
     grid_loss_responsible_df = grid_loss_responsible_df.select(
         col("METERING_POINT_ID").alias(Colname.metering_point_id),
