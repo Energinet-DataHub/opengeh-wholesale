7893016b4dd10535566325d4d0dac819b10a0295
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-07-13 07:56:40 +0200

    Feature/remove duplicate v net integrations (#353)
    * bumped to v12 in .github
    
    * removed duplicate vnet integration
    
    * removed duplicate vnet integration

--- shared-resources/terraform/infrastructure/main/vnet.tf
+++ shared-resources/terraform/infrastructure/main/vnet.tf
@@ -94,44 +94,6 @@ module "kvs_snet_vnet_integration_id" {
   key_vault_id = module.kv_shared.id
 }
 
-
-// DEPRECATED, IS BEING REMOVED
-module "snet_vnet_integrations" {
-  source               = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/subnet?ref=v12"
-  name                 = "vnet-integrations"
-  project_name         = var.domain_name_short
-  environment_short    = var.environment_short
-  environment_instance = var.environment_instance
-  resource_group_name  = var.virtual_network_resource_group_name
-  virtual_network_name = data.azurerm_virtual_network.this.name
-  address_prefixes = [
-    var.vnet_integrations_address_space
-  ]
-  enforce_private_link_service_network_policies = true
-
-  # Delegate the subnet to "Microsoft.Web/serverFarms"
-  delegations = [{
-    name                    = "delegation"
-    service_delegation_name = "Microsoft.Web/serverFarms"
-    service_delegation_actions = [
-      "Microsoft.Network/virtualNetworks/subnets/action"
-    ]
-  }]
-
-  service_endpoints = [
-    "Microsoft.KeyVault",
-    "Microsoft.EventHub"
-  ]
-}
-
-module "kvs_snet_vnet_integrations_id" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=v12"
-
-  name         = "snet-vnet-integrations-id"
-  value        = module.snet_vnet_integrations.id
-  key_vault_id = module.kv_shared.id
-}
-
 module "kvs_snet_vnet_deployment_agents_id" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=v12"
 
