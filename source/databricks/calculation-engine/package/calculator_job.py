03bd83ea23fbc8c3d4336e86f95436812a26e41c
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-07-20 13:08:12 +0200

    WiP

--- source/databricks/calculation-engine/package/calculator_job.py
+++ source/databricks/calculation-engine/package/calculator_job.py
@@ -21,8 +21,6 @@ from package import (
     initialize_spark,
     log,
 )
-from package.output_writers.basis_data_writer import BasisDataWriter
-from package.output_writers.calculation_result_writer import CalculationResultWriter
 import package.calculation_input as input
 from package.calculator_args import CalculatorArgs, get_calculator_args
 import package.steps.setup as setup
@@ -31,10 +29,9 @@ from package.storage_account_access import islocked
 
 def _start_calculator(args: CalculatorArgs, spark: SparkSession) -> None:
 
-    calculation_input_reader = input.CalculationInputReader(spark, args.wholesale_container_path)
-
     metering_point_periods_df, time_series_points_df, grid_loss_responsible_df = input.get_calculation_input(
-        calculation_input_reader,
+        spark,
+        args.wholesale_container_path,
         args.batch_period_start_datetime,
         args.batch_period_end_datetime,
         args.batch_grid_areas,
@@ -47,10 +44,11 @@ def _start_calculator(args: CalculatorArgs, spark: SparkSession) -> None:
         args.batch_period_end_datetime,
     )
 
-
-
     energy_calculation.execute(
-        calculation_result_writer,
+        args.batch_id,
+        args.batch_process_type,
+        args.batch_execution_time_start,
+        args.wholesale_container_path,
         metering_point_periods_df,
         enriched_time_series_point_df,
         grid_loss_responsible_df,
