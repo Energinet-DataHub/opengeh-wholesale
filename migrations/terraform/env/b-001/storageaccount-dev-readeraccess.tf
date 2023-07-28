dab08a882814050fe15540caec7006c1398002c6
Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>
2023-07-28 14:27:10 +0200

    Add XDAST as storage blob contributor (#375)

--- migrations/terraform/env/b-001/storageaccount-dev-readeraccess.tf
+++ migrations/terraform/env/b-001/storageaccount-dev-readeraccess.tf
@@ -6,3 +6,9 @@ resource "azurerm_role_assignment" "dh2data_henrik_sommer_storage_blob_data_cont
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = "c2f75d02-f64f-4111-aaea-6c43f5bc8d65"
 }
+
+resource "azurerm_role_assignment" "dh2data_dan_stenroejl_storage_blob_data_contributor" {
+  scope                = module.st_dh2data.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = "7f417f9a-36c2-40c4-bd81-3d2fc95cc113"
+}
