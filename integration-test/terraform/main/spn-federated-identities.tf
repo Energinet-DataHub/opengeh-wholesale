fa5ed5f1ebaa46c51aca354e71dd374a83cc4f38
René T. Nielsen <renetdev@gmail.com>
2023-08-10 17:00:51 +0200

    add remaining federated credetnials (#391)
    * add remaining federated credetnials
    
    * Update integration-test/terraform/main/spn-federated-identities.tf
    
    Co-authored-by: Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>
    
    * Update integration-test/terraform/main/spn-federated-identities.tf
    
    Co-authored-by: Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>
    
    * Update integration-test/terraform/main/spn-federated-identities.tf
    
    Co-authored-by: Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>

--- integration-test/terraform/main/spn-federated-identities.tf
+++ integration-test/terraform/main/spn-federated-identities.tf
@@ -5,26 +5,78 @@
 #
 
 locals {
-  fic_audience_azuread = "api://AzureADTokenExchange"
-  fic_issuer_github    = "https://token.actions.githubusercontent.com"
+  federated_identity_credential = {
+    audience_azuread = "api://AzureADTokenExchange"
+    issuer_github    = "https://token.actions.githubusercontent.com"
+  }
 }
 
-resource "azuread_application_federated_identity_credential" "fic_core" {
+resource "azuread_application_federated_identity_credential" "geh_core" {
   application_object_id = azuread_application.app_ci.object_id
 
   display_name = "geh-core-azureauth"
   subject      = "repo:Energinet-DataHub/geh-core:environment:AzureAuth"
 
-  audiences = [local.fic_audience_azuread]
-  issuer    = local.fic_issuer_github
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
 }
 
-resource "azuread_application_federated_identity_credential" "fic_market_participant" {
+resource "azuread_application_federated_identity_credential" "geh_market_participant" {
   application_object_id = azuread_application.app_ci.object_id
 
   display_name = "geh-market-participant-azureauth"
   subject      = "repo:Energinet-DataHub/geh-market-participant:environment:AzureAuth"
 
-  audiences = [local.fic_audience_azuread]
-  issuer    = local.fic_issuer_github
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "opengeh_wholesale" {
+  application_object_id = azuread_application.app_ci.object_id
+
+  display_name = "opengeh-wholesale-azureauth"
+  subject      = "repo:Energinet-DataHub/opengeh-wholesale:environment:AzureAuth"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "opengeh_edi" {
+  application_object_id = azuread_application.app_ci.object_id
+
+  display_name = "opengeh-edi-azureauth"
+  subject      = "repo:Energinet-DataHub/opengeh-edi:environment:AzureAuth"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "opengeh_esett_exchange" {
+  application_object_id = azuread_application.app_ci.object_id
+
+  display_name = "opengeh-esett-exchange-azureauth"
+  subject      = "repo:Energinet-DataHub/opengeh-esett-exchange:environment:AzureAuth"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "opengeh_migration" {
+  application_object_id = azuread_application.app_ci.object_id
+
+  display_name = "opengeh-migration-azureauth"
+  subject      = "repo:Energinet-DataHub/opengeh-migration:environment:AzureAuth"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "greenforce_frontend" {
+  application_object_id = azuread_application.app_ci.object_id
+
+  display_name = "greenforce-frontend-azureauth"
+  subject      = "repo:Energinet-DataHub/greenforce-frontend:environment:AzureAuth"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
 }
