fd488fce1013899cfc66f39d3c8db3a6ee21dd7e
René T. Nielsen <renetdev@gmail.com>
2023-08-15 08:46:31 +0200

    add role assignment (#394)

--- integration-test/terraform/main/resource-group.tf
+++ integration-test/terraform/main/resource-group.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "this" {
   name     = "rg-main-${lower(var.domain_name_short)}-${lower(var.environment_short)}-we-${lower(var.environment_instance)}"
   location = "West Europe"
 }
+
+resource "azurerm_role_assignment" "developer_teams" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Contributor"
+  principal_id         = var.developers_security_group_object_id
+}
