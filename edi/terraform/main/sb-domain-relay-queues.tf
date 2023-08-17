83118343788da532d10b51f16e32470e5238c0e1
Lasse Ringgren Nielsen <sixxth@gmail.com>
2023-08-17 15:12:04 +0200

    Rename Wholesale and Edi inbox name (#403)

--- edi/terraform/main/sb-domain-relay-queues.tf
+++ edi/terraform/main/sb-domain-relay-queues.tf
@@ -17,7 +17,7 @@ module "sbq_incoming_aggregated_measure_data_messagequeue" {
 module "sbq_edi_inbox_messagequeue" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-queue?ref=v12"
 
-  name         = "edi-inbox"
+  name         = "inbox"
   namespace_id = data.azurerm_key_vault_secret.sb_domain_relay_namespace_id.value
   project_name = var.domain_name_short
 }
