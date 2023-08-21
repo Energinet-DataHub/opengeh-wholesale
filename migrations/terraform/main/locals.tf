b496eb00e14253a2301adf8e5f589e5888b372ba
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2023-08-21 10:25:32 +0200

    Add TimeSeriesApi module (#397)
    * Add time series api module
    
    * Fix typo
    
    * Fix typo
    
    * Updated timeseriesapi module parameters
    
    * Cleanup
    
    * Cleanup
    
    ---------
    
    Co-authored-by: Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>

--- migrations/terraform/main/locals.tf
+++ migrations/terraform/main/locals.tf
@@ -1,6 +1,6 @@
 locals {
-    task_workflow_setup_trigger = "workflow_setup_${uuid()}"
-    alert_trigger_cron = "18 6 2/12 * * ?"
-    monitor_trigger_cron = "0 0/5 * * * ?"
+  task_workflow_setup_trigger = "workflow_setup_${uuid()}"
+  alert_trigger_cron = "18 6 2/12 * * ?"
+  monitor_trigger_cron = "0 0/5 * * * ?"
 }
 
