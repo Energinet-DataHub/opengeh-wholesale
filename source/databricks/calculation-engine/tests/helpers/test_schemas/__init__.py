f4ada3548e0a2a971c81cc823eec1646aef16bab
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-08-22 21:18:09 +0200

    remove the last market role stuff

--- source/databricks/calculation-engine/tests/helpers/test_schemas/__init__.py
+++ source/databricks/calculation-engine/tests/helpers/test_schemas/__init__.py
@@ -17,9 +17,6 @@ from .charges_flex_consumption_schema import (
 from .charges_per_day_schema import charges_per_day_schema
 from .charges_with_prices_schema import charges_with_prices_schema
 from .charges_with_price_and_links_schema import charges_with_price_and_links_schema
-from .charges_with_price_and_links_and_market_roles_schema import (
-    charges_with_price_and_links_and_market_roles_schema,
-)
 from .charges_complete_schema import charges_complete_schema
 from .tariff_schema import tariff_schema
 from .tariff_distinct_schema import tariff_distinct_schema
