a2f55fc537cab5975373ff6ccf7a20d82f82c6c7
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-08-23 12:32:47 +0200

    test: verify that test input data has expected contracts

--- source/databricks/calculation-engine/tests/integration/calculator/calculator_job/test_calculator_job.py
+++ source/databricks/calculation-engine/tests/integration/calculator/calculator_job/test_calculator_job.py
@@ -14,34 +14,77 @@
 
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import StructType
 import pytest
 from unittest.mock import patch, Mock
 from package.calculator_job import start
-from package.schemas.calculation_input import time_series_point_schema
+from package.schemas.calculation_input import (
+    time_series_point_schema, metering_point_period_schema, charge_link_periods_schema, charge_master_data_periods_schema, charge_price_points_schema
+)
+import package.infrastructure as infra
 
 
-def test__published_time_series_points_contract_matches_schema_from_input_time_series_points(
-    spark: SparkSession, test_files_folder_path: str, executed_balance_fixing: None
+def test__input_time_series_point_schema__matches_published_contract(
+    spark: SparkSession, energy_input_data_written_to_delta: None
 ) -> None:
     # Act: Calculator job is executed just once per session. See the fixture `executed_balance_fixing`
 
     # Assert
-    input_time_series_points = (
-        spark.read.format("csv")
-        .schema(time_series_point_schema)
-        .option("header", "true")
-        .option("mode", "FAILFAST")
-        .load(f"{test_files_folder_path}/TimeSeriesPoints.csv")
-    )
+    actual_input_data = spark.read.table(f"{infra.INPUT_DATABASE_NAME}.{infra.TIME_SERIES_POINTS_TABLE_NAME}")
+
     # When asserting both that the calculator creates output and it does it with input data that matches
     # the time series points contract from the time-series domain (in the same test), then we can infer that the
     # calculator works with the format of the data published from the time-series domain.
     # NOTE:It is not evident from this test that it uses the same input as the calculator job
     # Apparently nullability is ignored for CSV sources so we have to compare schemas in this slightly odd way
     # See more at https://stackoverflow.com/questions/50609548/compare-schema-ignoring-nullable
+    _assert_is_equal(actual_input_data.schema, time_series_point_schema)
+
+
+def test__input_metering_point_period_schema__matches_published_contract(
+    spark: SparkSession, energy_input_data_written_to_delta: None
+) -> None:
+    # Act: See the fixture `energy_input_data_written_to_delta`
+
+    # Assert
+    test_input_data = spark.read.table(f"{infra.INPUT_DATABASE_NAME}.{infra.METERING_POINT_PERIODS_TABLE_NAME}")
+    _assert_is_equal(test_input_data.schema, metering_point_period_schema)
+
+
+def test__input_charge_link_period_schema__matches_published_contract(
+    spark: SparkSession, price_input_data_written_to_delta: None
+) -> None:
+    # Act: See the fixture `energy_input_data_written_to_delta`
+
+    # Assert
+    test_input_data = spark.read.table(f"{infra.INPUT_DATABASE_NAME}.{infra.CHARGE_LINK_PERIODS_TABLE_NAME}")
+    _assert_is_equal(test_input_data.schema, charge_link_periods_schema)
+
+
+def test__input_charge_price_points_schema__matches_published_contract(
+    spark: SparkSession, price_input_data_written_to_delta: None
+) -> None:
+    # Act: See the fixture `energy_input_data_written_to_delta`
+
+    # Assert
+    test_input_data = spark.read.table(f"{infra.INPUT_DATABASE_NAME}.{infra.CHARGE_PRICE_POINTS_TABLE_NAME}")
+    _assert_is_equal(test_input_data.schema, charge_price_points_schema)
+
+
+def test__input_charge_master_data_periods_schema__matches_published_contract(
+    spark: SparkSession, price_input_data_written_to_delta: None
+) -> None:
+    # Act: See the fixture `energy_input_data_written_to_delta`
+
+    # Assert
+    test_input_data = spark.read.table(f"{infra.INPUT_DATABASE_NAME}.{infra.CHARGE_MASTER_DATA_PERIODS_TABLE_NAME}")
+    _assert_is_equal(test_input_data.schema, charge_master_data_periods_schema)
+
+
+def _assert_is_equal(actual_schema: StructType, expected_schema: StructType) -> None:
     assert all(
         (a.name, a.dataType) == (b.name, b.dataType)
-        for a, b in zip(input_time_series_points.schema, time_series_point_schema)
+        for a, b in zip(actual_schema, expected_schema)
     )
 
 
