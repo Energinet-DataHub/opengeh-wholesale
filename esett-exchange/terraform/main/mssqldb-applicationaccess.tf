669fa63a0f559fe9e014cd9fc280b0c3edb5bb4d
Karsten M <xnetsrak@gmail.com>
2023-08-30 05:50:45 -0700

    webapi infrastructure (#422)

--- esett-exchange/terraform/main/mssqldb-applicationaccess.tf
+++ esett-exchange/terraform/main/mssqldb-applicationaccess.tf
@@ -8,11 +8,13 @@ module "mssql_database_application_access" {
     module.func_entrypoint_exchange_event_receiver.name,
     module.func_entrypoint_ecp_inbox.name,
     module.func_entrypoint_ecp_outbox.name,
+    module.app_webapi.name,
   ]
 
   depends_on = [
     module.func_entrypoint_exchange_event_receiver.name,
     module.func_entrypoint_ecp_inbox.name,
     module.func_entrypoint_ecp_outbox.name,
+    module.app_webapi.name,
   ]
 }
