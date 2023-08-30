e3078b7e46ceb8e8376da9f33918e249e9655e1e
BjarkeMeier <hej@bjarkemeier.dk>
2023-08-30 09:57:31 +0200

    Charge type fix

--- source/databricks/calculation-engine/package/calculation/wholesale/wholesale_initializer.py
+++ source/databricks/calculation-engine/package/calculation/wholesale/wholesale_initializer.py
@@ -87,8 +87,8 @@ def get_charges_based_on_resolution(
     return df
 
 
-def get_charges_based_on_charge_type(charge_master_data: DataFrame, charge_type: str) -> DataFrame:
-    df = charge_master_data.filter(col(Colname.charge_type) == charge_type)
+def get_charges_based_on_charge_type(charge_master_data: DataFrame, charge_type: ChargeType) -> DataFrame:
+    df = charge_master_data.filter(col(Colname.charge_type) == charge_type.value)
     return df
 
 
@@ -257,7 +257,7 @@ def __join_properties_on_charges_with_given_charge_type(
     charge_prices: DataFrame,
     charge_links: DataFrame,
     metering_points: DataFrame,
-    charge_type: str,
+    charge_type: ChargeType,
 ) -> DataFrame:
     # filter on charge_type
     charge_master_data = get_charges_based_on_charge_type(charge_master_data, charge_type)
