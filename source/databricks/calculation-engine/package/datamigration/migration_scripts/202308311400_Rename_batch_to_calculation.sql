8eacd3a3038428c329b18d2ac57b1cf7da485777
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-08-30 17:43:02 +0200

    migration script

---
+++ source/databricks/calculation-engine/package/datamigration/migration_scripts/202308311400_Rename_batch_to_calculation.sql
@@ -0,0 +1,8 @@
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results RENAME COLUMN 'batch_id' TO 'calculation_id'
+GO
+
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results RENAME COLUMN 'batch_process_type' TO 'calculation_type'
+GO
+
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results RENAME COLUMN 'batch_execution_time_start' TO 'calculation_execution_time_start'
+GO
