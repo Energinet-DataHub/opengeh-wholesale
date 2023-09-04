88f59f4d4207de97b5c5e8c76ed12d558159b6f9
BjarkeMeier <hej@bjarkemeier.dk>
2023-09-04 15:36:28 +0200

    wip

--- source/databricks/calculation-engine/package/calculation/wholesale/tariff_calculators.py
+++ source/databricks/calculation-engine/package/calculation/wholesale/tariff_calculators.py
@@ -13,16 +13,57 @@
 # limitations under the License.
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import col, sum, count, lit
+from pyspark.sql.types import (
+    BooleanType,
+    DecimalType,
+    StructType,
+    StructField,
+    StringType,
+    TimestampType,
+)
 from package.codelists import ChargeQuality, ChargeUnit
 from package.constants import Colname
 
 
+tariff_schema = StructType(
+    [
+        StructField(Colname.charge_key, StringType(), False),
+        StructField(Colname.charge_id, StringType(), False),
+        StructField(Colname.charge_type, StringType(), False),
+        StructField(Colname.charge_owner, StringType(), False),
+        StructField(Colname.charge_tax, BooleanType(), False),
+        StructField(Colname.charge_resolution, StringType(), False),
+        StructField(Colname.charge_time, TimestampType(), False),
+        StructField(Colname.charge_price, DecimalType(18, 8), False),
+        StructField(Colname.metering_point_id, StringType(), False),
+        StructField(Colname.energy_supplier_id, StringType(), False),
+        StructField(Colname.metering_point_type, StringType(), False),
+        StructField(Colname.settlement_method, StringType(), False),
+        StructField(Colname.grid_area, StringType(), False),
+        StructField(Colname.quantity, DecimalType(18, 3), False),
+    ]
+)
+"""Schema contract for tariffs"""
+
+
 def calculate_tariff_price_per_ga_co_es(tariffs: DataFrame) -> DataFrame:
-    """Calculate tariff price per grid area, charge owner and energy supplier."""
+    """
+    Calculate tariff price time series.
+    A result is calculated per
+    - grid area
+    - charge key (charge id, charge type, charge owner)
+    - settlement method
+    - metering point type (except exchange metering points)
+    - energy supplier
 
-    agg_df = _sum_quantity_and_count_charges(tariffs)
+    Resolution has already been filtered, so only one resolution is present in the tariffs data frame.
+    So responsibility of creating results per resolution is managed outside this module.
+    """
 
-    df = _join_with_agg_df(tariffs, agg_df)
+    if tariffs.schema != tariff_schema:
+        raise ValueError("Input DataFrame does not have the correct schema")
+
+    df = _sum_quantity_and_count_charges(tariffs)
 
     return df.select(
         Colname.energy_supplier_id,
@@ -39,55 +80,32 @@ def calculate_tariff_price_per_ga_co_es(tariffs: DataFrame) -> DataFrame:
         Colname.charge_price,
         Colname.total_quantity,
         Colname.charge_count,
-        Colname.total_amount,
+        (col(Colname.charge_price) * col(Colname.total_quantity)).alias(Colname.total_amount),
         lit(ChargeUnit.KWH.value).alias(Colname.unit),
         lit(ChargeQuality.CALCULATED.value).alias(Colname.quality),  # TODO JMG: Replace with correct value
     )
 
 
 def _sum_quantity_and_count_charges(tariffs: DataFrame) -> DataFrame:
+    # Group by all columns that actually defines the groups, but also the additional columns that need to be present after aggregation
     agg_df = (
         tariffs.groupBy(
-            Colname.grid_area,
             Colname.energy_supplier_id,
+            Colname.grid_area,
             Colname.charge_time,
             Colname.metering_point_type,
             Colname.settlement_method,
             Colname.charge_key,
+            Colname.charge_id,
+            Colname.charge_type,
+            Colname.charge_owner,
+            Colname.charge_tax,
+            Colname.charge_resolution,
+            Colname.charge_price,
         )
         .agg(
             sum(Colname.quantity).alias(Colname.total_quantity),
             count(Colname.metering_point_id).alias(Colname.charge_count),
         )
-        .select("*")
     )
     return agg_df
-
-
-def _join_with_agg_df(df: DataFrame, agg_df: DataFrame) -> DataFrame:
-    df = (
-        df.join(
-            agg_df,
-            [
-                Colname.energy_supplier_id,
-                Colname.grid_area,
-                Colname.charge_time,
-                Colname.metering_point_type,
-                Colname.settlement_method,
-                Colname.charge_key,
-            ],
-            "inner",
-        )
-        .withColumn(Colname.total_amount, col(Colname.charge_price) * col(Colname.total_quantity))
-        .orderBy(
-            [
-                Colname.charge_key,
-                Colname.grid_area,
-                Colname.energy_supplier_id,
-                Colname.charge_time,
-                Colname.metering_point_type,
-                Colname.settlement_method,
-            ]
-        )
-    )
-    return df
--- source/databricks/calculation-engine/package/calculation/wholesale_calculation.py
+++ source/databricks/calculation-engine/package/calculation/wholesale_calculation.py
@@ -69,6 +69,6 @@ def _calculate_tariff_charges(
 
 def _get_production_and_consumption_metering_points(metering_points_periods_df: DataFrame) -> DataFrame:
     return metering_points_periods_df.filter(
-        (col(Colname.metering_point_type) == InputMeteringPointType.CONSUMPTION.value)
+        (col(Colname.metering_point_type) != InputMeteringPointType.EXCHANGE.value)
         | (col(Colname.metering_point_type) == InputMeteringPointType.PRODUCTION.value)
     )
