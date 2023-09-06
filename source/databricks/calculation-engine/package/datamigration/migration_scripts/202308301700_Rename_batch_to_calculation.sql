f1c8a8bac87ebaa9ce1277ce08cac33be76a8dab
BjarkeMeier <hej@bjarkemeier.dk>
2023-09-06 15:32:03 +0200

    Enable column mapping mode

--- source/databricks/calculation-engine/package/datamigration/migration_scripts/202308301700_Rename_batch_to_calculation.sql
+++ source/databricks/calculation-engine/package/datamigration/migration_scripts/202308301700_Rename_batch_to_calculation.sql
@@ -1,3 +1,10 @@
+-- Enable Databricks column mapping mode in order to rename columns
+ALTER TABLE <table_name> SET TBLPROPERTIES (
+   'delta.columnMapping.mode' = 'name',
+   'delta.minReaderVersion' = '2',
+   'delta.minWriterVersion' = '5'
+GO
+
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results RENAME COLUMN batch_id TO calculation_id
 GO
 
