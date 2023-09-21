4fb0d41c48ac4e8b143b81af5e43b433be8bcae4
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2023-09-21 10:07:20 +0200

    Update AggregationLevelMapper.cs

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/SqlStatements/Mappers/AggregationLevelMapper.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/SqlStatements/Mappers/AggregationLevelMapper.cs
@@ -37,7 +37,9 @@ public static class AggregationLevelMapper
             case TimeSeriesType.TotalConsumption:
                 if (energySupplierGln == null && balanceResponsiblePartyGln == null)
                     return DeltaTableAggregationLevel.GridArea;
-                throw new NotImplementedException($"Mapping of '{timeSeriesType}' not implemented.");
+                throw new InvalidOperationException($"Invalid combination of time series type: {timeSeriesType}," +
+                                                    $" energy supplier: {energySupplierGln}" +
+                                                    $" and balance responsible: {balanceResponsiblePartyGln} .");
             default:
                 throw new NotImplementedException($"Mapping of '{timeSeriesType}' not implemented.");
         }
