07e2644b5c90b370934dd70afcf50955e36247e9
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-09-29 13:19:28 +0200

    added overrides for test_001 (#496)
    Co-authored-by: Kristian Bank Erbou <contact@kbeconsult.dk>

---
+++ shared-resources/terraform/infrastructure/env/test_001/ad-groups-access.tf
@@ -0,0 +1,8 @@
+data "azurerm_subscription" "current" {
+}
+
+resource "azurerm_role_assignment" "developers_subscription_reader" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Reader"
+  principal_id         = var.developers_security_group_object_id
+}
