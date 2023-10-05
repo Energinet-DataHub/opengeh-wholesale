5f493c5c637bced03d7df3d7f1810a4a687abd4b
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-10-05 12:11:09 +0200

    ...

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.Interfaces/CalculationResults/Model/WholesaleResults/WholesaleResult.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.Interfaces/CalculationResults/Model/WholesaleResults/WholesaleResult.cs
@@ -12,11 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults;
 using Energinet.DataHub.Wholesale.Common.Models;
 using NodaTime;
 
-namespace Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResults.Model;
+namespace Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults;
 
 public sealed class WholesaleResult
 {
--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.UnitTests/Interfaces/CalculationResults/CalculationResultTests.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.UnitTests/Interfaces/CalculationResults/CalculationResultTests.cs
@@ -35,7 +35,7 @@ public class CalculationResultTests
         Instant anyPeriodStart,
         Instant anyPeriodEnd)
     {
-        var emptyTimeSeriesPoints = new EnergyTimeSeriesPoint[] { };
+        var emptyTimeSeriesPoints = Array.Empty<EnergyTimeSeriesPoint>();
         var exception = Assert.Throws<ArgumentException>(() =>
             new EnergyResult(anyId, anyBatchId, anyGridArea, anyTimeSeriesType, null, null, emptyTimeSeriesPoints, anyProcessType, anyPeriodStart, anyPeriodEnd, anyFromGridArea));
         exception.Message.Should().Contain("empty");
