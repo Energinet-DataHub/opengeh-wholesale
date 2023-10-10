376de5a192eb33f567a9868e9b4a79f9f0c532a2
René T. Nielsen <renetdev@gmail.com>
2023-10-10 11:29:03 +0200

    remove usage of service-bus-topic module (#517)

--- shared-resources/terraform/infrastructure/env/preprod_001/sbt-domainrelay-integrationevent-received_override.tf
+++
@@ -1,3 +0,0 @@
-module "sbt_domainrelay_integrationevent_received" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic?ref=v13"
-}
--- shared-resources/terraform/infrastructure/main/sbt-domainrelay-integrationevent-received.tf
+++ shared-resources/terraform/infrastructure/main/sbt-domainrelay-integrationevent-received.tf
@@ -1,16 +1,13 @@
-module "sbt_domainrelay_integrationevent_received" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic?ref=v12"
-
-  name         = "integrationevent-received"
-  namespace_id = module.sb_domain_relay.id
-  project_name = var.domain_name_short
+resource "azurerm_servicebus_topic" "domainrelay_integrationevent_received" {
+  name                         = "sbt-integrationevent-received-${lower(var.domain_name_short)}"
+  namespace_id                 = module.sb_domain_relay.id
 }
 
 module "kvs_sbt_domainrelay_integrationevent_received_id" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=v12"
 
   name         = "sbt-shres-integrationevent-received-id"
-  value        = module.sbt_domainrelay_integrationevent_received.id
+  value        = azurerm_servicebus_topic.domainrelay_integrationevent_received.id
   key_vault_id = module.kv_shared.id
 }
 
@@ -18,6 +15,6 @@ module "kvs_sbt_domainrelay_integrationevent_received_name" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=v12"
 
   name         = "sbt-shres-integrationevent-received-name"
-  value        = module.sbt_domainrelay_integrationevent_received.name
+  value        = azurerm_servicebus_topic.domainrelay_integrationevent_received.name
   key_vault_id = module.kv_shared.id
 }
