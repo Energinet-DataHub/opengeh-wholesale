603323ce6efee98ee7560bdfb11f329e552e7f68
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-10-11 14:49:55 +0200

    adjust comment

--- source/dotnet/wholesale-api/Common/Databricks/Options/DeltaTableOptions.cs
+++ source/dotnet/wholesale-api/Common/Databricks/Options/DeltaTableOptions.cs
@@ -17,7 +17,7 @@ namespace Energinet.DataHub.Wholesale.Common.Databricks.Options;
 public class DeltaTableOptions
 {
     /// <summary>
-    /// Name of the schema/database under which the result table is associated.
+    /// Name of the schema/database under which the result tables are associated.
     /// </summary>
     public string SCHEMA_NAME { get; set; } = "wholesale_output";
 
