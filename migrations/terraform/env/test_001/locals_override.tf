43e342e32a5ac3594727a3cf9c0478875c149c24
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2023-10-17 09:16:22 +0200

    Set DataHub 2 certificat on new subscription envs (#533)

--- migrations/terraform/env/test_001/locals_override.tf
+++ migrations/terraform/env/test_001/locals_override.tf
@@ -1,3 +1,4 @@
 locals {
   resources_suffix = "${lower(var.domain_name_short)}-${lower(var.environment_short)}-we-${lower(var.environment_instance)}"
+  datahub2_certificate_thumbprint = resource.azurerm_app_service_certificate.dh2_certificate_app.thumbprint
 }
