73e71035e91dbe8eec7d9352e200fde9844959ad
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2023-10-18 13:25:29 +0200

    Output variables for the DB migration script in eSett deprecated project (#537)
    * Output variables for the DB migration script in eSett deprecated project
    
    * add sql data to kv shared res
    
    * ref esett module instead of shared res
    
    * revert changes to shared kv
    
    * add contributer role assignments

---
+++ esett-deprecated/terraform/main/outputs.tf
@@ -0,0 +1,17 @@
+output "ms_esett_deprecated_connection_string" {
+  description = "Connection string of the Esett database created in the Esett deprecated server"
+  value       = local.connection_string_database
+  sensitive   = true
+}
+
+output "ms_esett_deprecated_database_name" {
+  description = "Database name in the Esett deprecated sql server"
+  value       = module.mssqldb_esett.name
+  sensitive   = true
+}
+
+output "ms_esett_deprecated_database_server" {
+  description = "Database server instance hosting the Esett database"
+  value       = module.mssql_esett.fully_qualified_domain_name
+  sensitive   = true
+}
