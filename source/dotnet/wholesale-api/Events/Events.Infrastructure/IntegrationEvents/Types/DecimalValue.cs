3cfb0584a0d5d2dad6c12a1371504e47e343ea66
Alexander <alexanderharrow@hotmail.com>
2023-10-19 22:01:04 +0200

    Create DecimalValue.cs

---
+++ source/dotnet/wholesale-api/Events/Events.Infrastructure/IntegrationEvents/Types/DecimalValue.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// ReSharper disable once CheckNamespace - the namespace must match that of the (protobuf) contracts
+namespace Energinet.DataHub.Wholesale.Contracts.IntegrationEvents
+{
+    /// <summary>
+    /// This implementation is taken from https://learn.microsoft.com/en-us/dotnet/architecture/grpc-for-wcf-developers/protobuf-data-types#decimals
+    /// </summary>
+    public partial class DecimalValue
+    {
+        private const decimal NanoFactor = 1_000_000_000;
+
+        public DecimalValue(long units, int nanos)
+        {
+            Units = units;
+            Nanos = nanos;
+        }
+
+        public static implicit operator decimal(DecimalValue grpcDecimal)
+        {
+            return grpcDecimal.Units + (grpcDecimal.Nanos / NanoFactor);
+        }
+
+        public static implicit operator DecimalValue(decimal value)
+        {
+            var units = decimal.ToInt64(value);
+            var nanos = decimal.ToInt32((value - units) * NanoFactor);
+            return new DecimalValue(units, nanos);
+        }
+    }
+}
