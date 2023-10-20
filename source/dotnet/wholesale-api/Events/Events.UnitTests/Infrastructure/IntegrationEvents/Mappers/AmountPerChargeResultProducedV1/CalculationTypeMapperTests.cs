7e3b110a3bdffcde82d05ce17619e88324111d17
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-10-20 12:00:01 +0200

    mapper tests

---
+++ source/dotnet/wholesale-api/Events/Events.UnitTests/Infrastructure/IntegrationEvents/Mappers/AmountPerChargeResultProducedV1/CalculationTypeMapperTests.cs
@@ -0,0 +1,66 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.TestCommon.AutoFixture.Attributes;
+using Energinet.DataHub.Wholesale.Events.Infrastructure.IntegrationEvents.Mappers.AmountPerChargeResultProducedV1;
+using FluentAssertions;
+using Xunit;
+using EventCalculationType = Energinet.DataHub.Wholesale.Contracts.IntegrationEvents.AmountPerChargeResultProducedV1.Types.CalculationType;
+using ModelCalculationType = Energinet.DataHub.Wholesale.Common.Models.ProcessType;
+
+namespace Energinet.DataHub.Wholesale.Events.UnitTests.Infrastructure.IntegrationEvents.Mappers.AmountPerChargeResultProducedV1;
+
+public class CalculationTypeMapperTests
+{
+    [Theory]
+    [InlineAutoMoqData(ModelCalculationType.WholesaleFixing, EventCalculationType.WholesaleFixing)]
+    [InlineAutoMoqData(ModelCalculationType.FirstCorrectionSettlement, EventCalculationType.FirstCorrectionSettlement)]
+    [InlineAutoMoqData(ModelCalculationType.SecondCorrectionSettlement, EventCalculationType.SecondCorrectionSettlement)]
+    [InlineAutoMoqData(ModelCalculationType.ThirdCorrectionSettlement, EventCalculationType.ThirdCorrectionSettlement)]
+    public void MapCalculationType_WhenCalled_MapsCorrectly(ModelCalculationType calculationType, EventCalculationType expected)
+    {
+        // Act & Assert
+        CalculationTypeMapper.MapCalculationType(calculationType).Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineAutoMoqData(ModelCalculationType.Aggregation)]
+    [InlineAutoMoqData(ModelCalculationType.BalanceFixing)]
+    public void MapCalculationType_WhenCalledWithUnexpectedCalculationType_ThrowsArgumentOutOfRangeException(ModelCalculationType calculationType)
+    {
+        // Act
+        var act = () => CalculationTypeMapper.MapCalculationType(calculationType);
+
+        // Assert
+        act.Should().Throw<ArgumentOutOfRangeException>()
+            .And.ActualValue.Should().Be(calculationType);
+    }
+
+    [Fact]
+    public void MapCalculationType_WhenCalledWithAnyValueExceptAggregationOrBalanceFixing_MapsValue()
+    {
+        foreach (var calculationType in Enum.GetValues(typeof(ModelCalculationType)).Cast<ModelCalculationType>())
+        {
+            // Arrange
+            if (calculationType is ModelCalculationType.Aggregation or ModelCalculationType.BalanceFixing)
+                continue;
+
+            // Act
+            var actual = CalculationTypeMapper.MapCalculationType(calculationType);
+
+            // Assert: Is defined (and implicitly that it didn't throw exception)
+            Enum.IsDefined(typeof(EventCalculationType), actual).Should().BeTrue();
+        }
+    }
+}
---
+++ source/dotnet/wholesale-api/Events/Events.UnitTests/Infrastructure/IntegrationEvents/Mappers/AmountPerChargeResultProducedV1/MeteringPointTypeMapperTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.TestCommon.AutoFixture.Attributes;
+using Energinet.DataHub.Wholesale.Events.Infrastructure.IntegrationEvents.Mappers.AmountPerChargeResultProducedV1;
+using FluentAssertions;
+using Xunit;
+using EventMeteringPointType = Energinet.DataHub.Wholesale.Contracts.IntegrationEvents.AmountPerChargeResultProducedV1.Types.MeteringPointType;
+using ModelMeteringPointType = Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResults.Model.MeteringPointType;
+
+namespace Energinet.DataHub.Wholesale.Events.UnitTests.Infrastructure.IntegrationEvents.Mappers.AmountPerChargeResultProducedV1;
+
+public class MeteringPointTypeMapperTests
+{
+    [Theory]
+    [InlineAutoMoqData(ModelMeteringPointType.Consumption, EventMeteringPointType.Consumption)]
+    [InlineAutoMoqData(ModelMeteringPointType.Production, EventMeteringPointType.Production)]
+    [InlineAutoMoqData(null!, EventMeteringPointType.Unspecified)]
+    public void MapMeteringPointType_WhenCalledWithValidMeteringTypes_MapsCorrectly(ModelMeteringPointType? meteringPointType, EventMeteringPointType expected)
+    {
+        // Act & Assert
+        MeteringPointTypeMapper.MapMeteringPointType(meteringPointType).Should().Be(expected);
+    }
+
+    [Fact]
+    public void MapProcessType_WhenInvalidEnumNumberFor_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        var invalidValue = (ModelMeteringPointType)99;
+
+        // Act
+        var act = () => MeteringPointTypeMapper.MapMeteringPointType(invalidValue);
+
+        // Assert
+        act.Should().Throw<ArgumentOutOfRangeException>()
+            .And.ActualValue.Should().Be(invalidValue);
+    }
+}
