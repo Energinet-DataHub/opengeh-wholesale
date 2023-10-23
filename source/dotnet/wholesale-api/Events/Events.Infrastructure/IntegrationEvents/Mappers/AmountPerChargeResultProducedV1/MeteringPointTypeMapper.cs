9fd401b29a7562b708425de21a567b4064c50940
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-10-23 15:00:17 +0200

    feat: Introduce mappers from model to events for wholesale results (#1550)

---
+++ source/dotnet/wholesale-api/Events/Events.Infrastructure/IntegrationEvents/Mappers/AmountPerChargeResultProducedV1/MeteringPointTypeMapper.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using EventMeteringPointType = Energinet.DataHub.Wholesale.Contracts.IntegrationEvents.AmountPerChargeResultProducedV1.Types.MeteringPointType;
+using ModelMeteringPointType = Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResults.Model.MeteringPointType;
+
+namespace Energinet.DataHub.Wholesale.Events.Infrastructure.IntegrationEvents.Mappers.AmountPerChargeResultProducedV1;
+
+public static class MeteringPointTypeMapper
+{
+    public static EventMeteringPointType MapMeteringPointType(ModelMeteringPointType? meteringPointType)
+    {
+        return meteringPointType switch
+        {
+            ModelMeteringPointType.Consumption => EventMeteringPointType.Consumption,
+            ModelMeteringPointType.Production => EventMeteringPointType.Production,
+            null => EventMeteringPointType.Unspecified,
+            _ => throw new ArgumentOutOfRangeException(
+                nameof(meteringPointType),
+                actualValue: meteringPointType,
+                "Value cannot be mapped to a metering point type."),
+        };
+    }
+}
