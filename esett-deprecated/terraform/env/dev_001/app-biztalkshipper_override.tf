6046a1506eab67d475c627010888b60060f4f978
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2023-10-24 14:44:00 +0200

    Fix sender and receiver code for preprod (#554)

--- esett-deprecated/terraform/env/dev_001/app-biztalkshipper_override.tf
+++ esett-deprecated/terraform/env/dev_001/app-biztalkshipper_override.tf
@@ -1,7 +1,5 @@
 module "app_biztalkshipper" {
   app_settings = merge({
-    "biztalk:senderCode"    = "45V000000000056T"
-    "biztalk:receiverCode"  = "44V000000000029A"
     "biztalk:RootUrl"       = "https://datahub.preproduction.biztalk.energinet.local"
   }, local.default_biztalkshipper_app_settings)
 }
