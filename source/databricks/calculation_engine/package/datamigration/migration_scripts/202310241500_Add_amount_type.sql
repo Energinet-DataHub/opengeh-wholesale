816184ff53b077a01eb8be0a91f38c60d95bc4a1
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-10-25 16:03:59 +0200

    fix

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202310241500_Add_amount_type.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202310241500_Add_amount_type.sql
@@ -3,7 +3,5 @@ ALTER TABLE {OUTPUT_DATABASE_NAME}.wholesale_results
 GO
 
 ALTER TABLE {OUTPUT_DATABASE_NAME}.wholesale_results
-    ADD CONSTRAINT amount_type_chk
-    CHECK (array_size(array_except(amount_type, array('amount_per_charge', 'monthly_amount_per_charge', 'total_monthly_amount'))) = 0
-           AND array_size(amount_type) > 0)
+    ADD CONSTRAINT amount_type_chk CHECK (amount_type IN ('amount_per_charge', 'monthly_amount_per_charge', 'total_monthly_amount'))
 GO
