7c95fa6256a6398bcf3b138579d92d8adf33c534
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-10-25 21:07:36 +0200

    amount as last column

--- source/databricks/calculation_engine/tests/datamigration/test_wholesale_results_migrations.py
+++ source/databricks/calculation_engine/tests/datamigration/test_wholesale_results_migrations.py
@@ -46,7 +46,6 @@ def _create_df(spark: SparkSession) -> DataFrame:
             2020, 1, 1, 0, 0
         ),
         WholesaleResultColumnNames.calculation_result_id: "6033ab5c-436b-44e9-8a79-90489d324e53",
-        WholesaleResultColumnNames.amount_type: "amount_per_charge",
         WholesaleResultColumnNames.grid_area: "543",
         WholesaleResultColumnNames.energy_supplier_id: "1234567890123",
         WholesaleResultColumnNames.quantity: Decimal("1.123"),
@@ -62,6 +61,7 @@ def _create_df(spark: SparkSession) -> DataFrame:
         WholesaleResultColumnNames.charge_code: "charge_code",
         WholesaleResultColumnNames.charge_type: "fee",
         WholesaleResultColumnNames.charge_owner_id: "1234567890123",
+        WholesaleResultColumnNames.amount_type: "amount_per_charge",
     }
     return spark.createDataFrame(data=[row], schema=wholesale_results_schema)
 
