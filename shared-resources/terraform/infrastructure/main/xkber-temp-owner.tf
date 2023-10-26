7b14070ad2ec5ee4ffd895ba3b2944d050dfcde2
Kristian Bank Erbou <contact@kbeconsult.dk>
2023-10-26 13:16:24 +0200

    Remove XKBER as subscription owner (#572)

--- shared-resources/terraform/infrastructure/main/xkber-temp-owner.tf
+++
@@ -1,8 +0,0 @@
-# Temporarily allow XKBER full access allow editing flawed TF-state regarding B2C
-# Should be removed once the TF-state is fixed (deploy of PR-565 in dh3-infrastructure runs successfully)
-
-resource "azurerm_role_assignment" "xkber_tf_state" {
-  scope                = data.azurerm_subscription.current.id
-  role_definition_name = "Owner"
-  principal_id         = "787bf5d7-ea80-45dc-b6da-c8e1bc60efdd"  #xkber-aadadmin@energinet.dk
-}
