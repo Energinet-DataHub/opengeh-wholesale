d10c33eb27693f61a682249b505bb97cd33e2454
René T. Nielsen <renetdev@gmail.com>
2023-10-26 14:24:10 +0200

    add new production certificate (#577)

--- esett-deprecated/terraform/env/prod_001/biztalk-certificate_override.tf
+++ esett-deprecated/terraform/env/prod_001/biztalk-certificate_override.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault_certificate" "biztalk_certificate" {
   certificate {
-    contents = filebase64("${path.module}/assets/DataHubBiztalkClientCert_Production.pfx")
+    contents = filebase64("${path.module}/assets/DatahubClientCertificate.pfx")
     password = var.cert_pwd_esett_biztalk_authentication_key1
   }
 }
