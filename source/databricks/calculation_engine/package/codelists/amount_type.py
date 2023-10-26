b953d9135ccb99923e4888eb5d45c0ac73c92e74
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-10-26 21:10:21 +0200

    Add documentation to amount_type

--- source/databricks/calculation_engine/package/codelists/amount_type.py
+++ source/databricks/calculation_engine/package/codelists/amount_type.py
@@ -17,6 +17,18 @@ from enum import Enum
 
 
 class AmountType(Enum):
+    """
+    This type is used to distinguish wholesale result. There are three groups:
+
+    AMOUNT_PER_CHARGE: fee/tariff/subscription per hour/day (later maybe also quarter) per charge key (code, type,
+    and owner). The amounts are calculated per metering point type.
+
+    MONTHLY_AMOUNT_PER_CHARGE: fee/tariff/subscription per month per charge key (code, type, and owner).
+    The amounts are summed across metering point types.
+
+    TOTAL_MONTHLY_AMOUNT: Total sum of amounts per month across charge codes and charge types
+    """
+
     AMOUNT_PER_CHARGE = "amount_per_charge"
     MONTHLY_AMOUNT_PER_CHARGE = "monthly_amount_per_charge"
     TOTAL_MONTHLY_AMOUNT = "total_monthly_amount"
