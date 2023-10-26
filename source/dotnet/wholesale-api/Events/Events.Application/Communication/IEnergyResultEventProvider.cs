ffcbc84fead7335291b6b1114b00f5bc6468d900
Dan Stenrøjl <XDAST@energinet.dk>
2023-10-26 16:19:25 +0200

    Document interfaces

--- source/dotnet/wholesale-api/Events/Events.Application/Communication/IEnergyResultEventProvider.cs
+++ source/dotnet/wholesale-api/Events/Events.Application/Communication/IEnergyResultEventProvider.cs
@@ -13,12 +13,17 @@
 // limitations under the License.
 
 using Energinet.DataHub.Core.Messaging.Communication.Internal;
+using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResults.Model.EnergyResults;
 using Energinet.DataHub.Wholesale.Events.Application.CompletedBatches;
 
 namespace Energinet.DataHub.Wholesale.Events.Application.Communication
 {
     public interface IEnergyResultEventProvider
     {
+        /// <summary>
+        /// Responsible for creating an <see cref="IntegrationEvent"/> for each
+        /// <see cref="EnergyResult"/> available in <paramref name="batch"/>.
+        /// </summary>
         IAsyncEnumerable<IntegrationEvent> GetAsync(CompletedBatch batch);
     }
 }
--- source/dotnet/wholesale-api/Events/Events.Application/Communication/IWholesaleResultEventProvider.cs
+++ source/dotnet/wholesale-api/Events/Events.Application/Communication/IWholesaleResultEventProvider.cs
@@ -13,14 +13,25 @@
 // limitations under the License.
 
 using Energinet.DataHub.Core.Messaging.Communication.Internal;
+using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults;
 using Energinet.DataHub.Wholesale.Events.Application.CompletedBatches;
 
 namespace Energinet.DataHub.Wholesale.Events.Application.Communication
 {
     public interface IWholesaleResultEventProvider
     {
+        /// <summary>
+        /// Determines if <paramref name="batch"/> is in a state where it can
+        /// contain <see cref="WholesaleResult"/>.
+        /// </summary>
+        /// <returns><langword>true</langword> if <paramref name="batch"/> can
+        /// contain <see cref="WholesaleResult"/>; otherwise <langword>false</langword>.</returns>
         bool CanContainWholesaleResults(CompletedBatch batch);
 
+        /// <summary>
+        /// Responsible for creating an <see cref="IntegrationEvent"/> for each
+        /// <see cref="WholesaleResult"/> available in <paramref name="batch"/>.
+        /// </summary>
         IAsyncEnumerable<IntegrationEvent> GetAsync(CompletedBatch batch);
     }
 }
