c7d2925094a313b0d2fc53f124ca48b6b53e8723
Kristian Bank Erbou <contact@kbeconsult.dk>
2023-10-26 12:02:22 +0200

    Reintroduce B2C access (#565)

--- shared-resources/terraform/b2c/main/b2c-access.tf
+++ shared-resources/terraform/b2c/main/b2c-access.tf
@@ -1,22 +1,22 @@
-# locals {
-#   platform_team_members = split(",", var.platform_team_members)
-# }
+locals {
+  platform_team_members = split(",", var.platform_team_members)
+}
 
 resource "azuread_directory_role" "global_reader" {
   display_name = "Global Reader"
 }
 
-# resource "azuread_directory_role_assignment" "platformteam_member" {
-#   count = length(local.platform_team_members)
+resource "azuread_directory_role_assignment" "platformteam_member" {
+  count = length(local.platform_team_members)
 
-#   role_id             = azuread_directory_role.global_reader.template_id
-#   principal_object_id = azuread_invitation.platformteam_member[count.index].user_id
-# }
+  role_id             = azuread_directory_role.global_reader.template_id
+  principal_object_id = azuread_invitation.platformteam_member[count.index].user_id
+}
 
-# resource "azuread_invitation" "platformteam_member" {
-#   count = length(local.platform_team_members)
+resource "azuread_invitation" "platformteam_member" {
+  count = length(local.platform_team_members)
 
-#   user_email_address = local.platform_team_members[count.index]
-#   user_display_name = "SEC-A-Greenforce-PlatformTeamAzure member"
-#   redirect_url       = "https://portal.azure.com"
-# }
+  user_email_address = local.platform_team_members[count.index]
+  user_display_name = "SEC-A-Greenforce-PlatformTeamAzure member"
+  redirect_url       = "https://portal.azure.com"
+}
