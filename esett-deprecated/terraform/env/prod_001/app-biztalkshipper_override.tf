151e486d6df3409cd36115b8bcd84cb36c48fe55
René T. Nielsen <renetdev@gmail.com>
2023-10-30 13:14:59 +0100

    fix correct host of biztal (#595)

--- esett-deprecated/terraform/env/prod_001/app-biztalkshipper_override.tf
+++ esett-deprecated/terraform/env/prod_001/app-biztalkshipper_override.tf
@@ -5,3 +5,7 @@ module "app_biztalkshipper" {
     "biztalk:RootUrl"       = "https://datahub.biztalk.energinet.local"
   })
 }
+
+resource "azurerm_web_app_hybrid_connection" "biztalkshipper_biztalk" {
+  hostname    = "datahub.biztalk.energinet.local"
+}
