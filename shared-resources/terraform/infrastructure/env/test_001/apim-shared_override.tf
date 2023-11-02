b5928466a604c02d49b9f2138755860c76a78686
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2023-11-02 12:34:54 +0100

    Developer portal enable username+password identity provider in test_001 and add eloverblik product (#602)

--- shared-resources/terraform/infrastructure/env/test_001/apim-shared_override.tf
+++ shared-resources/terraform/infrastructure/env/test_001/apim-shared_override.tf
@@ -5,5 +5,6 @@ module "snet_apim" {
 module "apim_shared" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/api-management?ref=v13"
 
-  subnet_id = data.azurerm_subnet.snet_apim.id
+  subnet_id                                     = data.azurerm_subnet.snet_apim.id
+  developer_portal_enable_username_and_password = true
 }
