b1faa8bf2261598cf4c349a82a2b71d1aeaa04b4
René T. Nielsen <renetdev@gmail.com>
2023-11-02 14:00:20 +0100

    MORE POWER to service plan in production (#607)
    * Create plan-services.tf
    
    * Update plan-services_override.tf
    
    * Delete shared-resources/terraform/infrastructure/env/prod_001/plan-services.tf

--- shared-resources/terraform/infrastructure/env/prod_001/plan-services_override.tf
+++ shared-resources/terraform/infrastructure/env/prod_001/plan-services_override.tf
@@ -1,3 +1,4 @@
 module "plan_services" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service-plan?ref=v13"
+  source   = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service-plan?ref=v13"
+  sku_name = "P3v3"
 }
