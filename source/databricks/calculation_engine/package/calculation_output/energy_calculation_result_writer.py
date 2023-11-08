64547263769f51ae4540d7e6c6976148fc5188fd
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-11-08 09:39:28 +0100

    refac: remove schema check in energy_calculation_result_writer because it uses EnergyResult data type

--- source/databricks/calculation_engine/package/calculation_output/energy_calculation_result_writer.py
+++ source/databricks/calculation_engine/package/calculation_output/energy_calculation_result_writer.py
@@ -44,22 +44,9 @@ class EnergyCalculationResultWriter:
     ) -> None:
         f"""
         Write one or more results to storage.
-        The schema of the input data frame must match the schema {_write_input_schema}.
-        Nullable columns are, however, optional.
         """
-        # TODO BJM: Two schemas and duplicate adding nullable columns?
-        results_df = self._add_nullable_columns_if_missing(results.df)
 
-        # Assert schema after adding optional columns but before internal data frame transformations.
-        # The order of the columns in the input data frame doesn't matter.
-        assert_schema(
-            results_df.schema,
-            _write_input_schema,
-            ignore_nullability=True,
-            ignore_column_order=True,
-            ignore_decimal_scale=True,
-            ignore_decimal_precision=True,
-        )
+        results_df = results.df
 
         results_df = self._add_aggregation_level_and_time_series_type(
             results_df, aggregation_level, time_series_type
@@ -82,19 +69,6 @@ class EnergyCalculationResultWriter:
         ).withColumn(
             EnergyResultColumnNames.time_series_type, f.lit(time_series_type.value)
         )
-
-    @staticmethod
-    def _add_nullable_columns_if_missing(results: DataFrame) -> DataFrame:
-        """
-        Nullable columns may not be present in all data frames.
-        Thus, they are added if missing.
-        """
-        for field in _write_input_schema:
-            if field.nullable and field.name not in results.columns:
-                results = results.withColumn(
-                    field.name, f.lit(None).cast(field.dataType)
-                )
-        return results
 
     def _add_batch_columns(self, results: DataFrame) -> DataFrame:
         """Add columns that are the same for all calculation results in batch."""
@@ -175,34 +149,3 @@ class EnergyCalculationResultWriter:
         results.write.format("delta").mode("append").option(
             "mergeSchema", "false"
         ).insertInto(f"{OUTPUT_DATABASE_NAME}.{ENERGY_RESULT_TABLE_NAME}")
-
-
-# TODO BJM: Remove? Kinda duplicate considering the schema of EnergyResults
-_write_input_schema = t.StructType(
-    [
-        t.StructField(Colname.grid_area, t.StringType(), False),
-        t.StructField(Colname.energy_supplier_id, t.StringType(), True),
-        t.StructField(Colname.balance_responsible_id, t.StringType(), True),
-        t.StructField(Colname.sum_quantity, t.DecimalType(18, 3), True),
-        t.StructField(Colname.qualities, t.ArrayType(t.StringType(), False), False),
-        # TODO BJM: Why not just observation time? This complexity isn't needed or desired in the writer
-        t.StructField(
-            Colname.time_window,
-            (
-                t.StructType()
-                .add(Colname.start, t.TimestampType())
-                .add(Colname.end, t.TimestampType())
-            ),
-            False,
-        ),
-        t.StructField(Colname.to_grid_area, t.StringType(), True),
-        t.StructField(Colname.from_grid_area, t.StringType(), True),
-        t.StructField(Colname.metering_point_type, t.StringType(), True),
-        t.StructField(Colname.settlement_method, t.StringType(), True),
-    ]
-)
-"""
-Results data frame that is to be written must match this schema.
-Nullable columns are, however, allowed to be omitted from the results data frame.
-The writer will add them before writing to storage.
-"""
