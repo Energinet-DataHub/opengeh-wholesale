4b419a48bf8ef0a97e907169796679ba8e7b6fe1
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2023-11-08 08:17:20 +0100

    test: aggregate_net_exchange_per_neighbour_ga qualities (#1615)

--- source/databricks/calculation_engine/package/calculation/energy/exchange_aggregators.py
+++ source/databricks/calculation_engine/package/calculation/energy/exchange_aggregators.py
@@ -30,10 +30,11 @@ exchange_out_to_grid_area = "ExOut_ToGridArea"
 exchange_out_from_grid_area = "ExOut_FromGridArea"
 
 
-# Function to aggregate net exchange per neighbouring grid areas
 def aggregate_net_exchange_per_neighbour_ga(
     quarterly_metering_point_time_series: QuarterlyMeteringPointTimeSeries,
 ) -> EnergyResults:
+    """Function to aggregate net exchange per neighbouring grid areas."""
+
     df = quarterly_metering_point_time_series.df.where(
         F.col(Colname.metering_point_type) == MeteringPointType.EXCHANGE.value
     )
@@ -58,24 +59,56 @@ def aggregate_net_exchange_per_neighbour_ga(
     )
 
     from_qualities = "from_qualities"
+    to_time_window = "to_time_window"
+    from_time_window = "from_time_window"
+
+    # Workaround for ambiguous "time_window" column in select after join
+    exchange_to = exchange_to.withColumnRenamed(Colname.time_window, to_time_window)
+    exchange_from = exchange_from.withColumnRenamed(
+        Colname.time_window, from_time_window
+    )
+
     exchange = (
-        exchange_to.join(exchange_from, [Colname.time_window], "inner")
-        .where(
-            exchange_to[exchange_in_to_grid_area]
-            == exchange_from[exchange_out_from_grid_area]
+        # Outer self join: Outer is required as we must support cases where only metering point(s)
+        # in one direction between two neighboring grid areas exist
+        exchange_to.join(
+            exchange_from,
+            (exchange_to[to_time_window] == exchange_from[from_time_window])
+            & (
+                exchange_to[exchange_in_to_grid_area]
+                == exchange_from[exchange_out_from_grid_area]
+            )
+            & (
+                exchange_to[exchange_in_from_grid_area]
+                == exchange_from[exchange_out_to_grid_area]
+            ),
+            "outer",
         )
-        .where(
-            exchange_to[exchange_in_from_grid_area]
-            == exchange_from[exchange_out_to_grid_area]
-        )
+        # Since both exchange_from or exchange_to can be missing we need to coalesce all columns
         .select(
-            exchange_to["*"],
-            exchange_from[from_sum],
-            exchange_from[Colname.qualities].alias(from_qualities),
+            F.coalesce(
+                exchange_to[to_time_window], exchange_from[from_time_window]
+            ).alias(Colname.time_window),
+            F.coalesce(
+                exchange_to[exchange_in_to_grid_area],
+                exchange_from[exchange_out_from_grid_area],
+            ).alias(Colname.to_grid_area),
+            F.coalesce(
+                exchange_to[exchange_in_from_grid_area],
+                exchange_from[exchange_out_to_grid_area],
+            ).alias(Colname.from_grid_area),
+            F.coalesce(exchange_to[to_sum], F.lit(0)).alias(to_sum),
+            F.coalesce(exchange_from[from_sum], F.lit(0)).alias(from_sum),
+            F.coalesce(exchange_from[Colname.qualities], F.array()).alias(
+                from_qualities
+            ),
+            F.coalesce(exchange_to[Colname.qualities], F.array()).alias(
+                Colname.qualities
+            ),
         )
+        # Calculate netto sum between neighboring grid areas
         .withColumn(Colname.sum_quantity, F.col(to_sum) - F.col(from_sum))
-        .withColumnRenamed(exchange_in_to_grid_area, Colname.to_grid_area)
-        .withColumnRenamed(exchange_in_from_grid_area, Colname.from_grid_area)
+        # Finally select the result columns
         .select(
             Colname.to_grid_area,
             Colname.from_grid_area,
