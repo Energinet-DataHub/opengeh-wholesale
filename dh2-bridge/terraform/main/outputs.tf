e3ab261dcf7fdd8e7dd2856c33d4bb2bafe963cc
Karsten M <xnetsrak@gmail.com>
2023-11-10 09:47:13 +0100

    sensitive output to true (#629)

--- dh2-bridge/terraform/main/outputs.tf
+++ dh2-bridge/terraform/main/outputs.tf
@@ -1,14 +1,17 @@
 output "ms_dh2_bridge_connection_string" {
   description = "Connection string for executing database migrations on the dh2 bridge database"
   value       = local.CONNECTION_STRING_DB_MIGRATIONS
+  sensitive = true
 }
 
 output "ms_dh2_bridge_database_name" {
   description = "Database name in the shared server."
   value       = module.mssqldb_dh2_bridge.name
+  sensitive = true
 }
 
 output "ms_dh2_bridge_database_server" {
   description = "Database server instance hosting the dh2 bridge database."
   value       = data.azurerm_key_vault_secret.mssql_data_url.value
+  sensitive = true
 }
