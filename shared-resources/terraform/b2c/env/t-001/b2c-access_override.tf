3f5c28fea2cdd642b32f93886ea10867fc7105bc
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-11-10 14:12:42 +0100

    reverted: made nhq global admin on test_001 (#633)

--- shared-resources/terraform/b2c/env/t-001/b2c-access_override.tf
+++ shared-resources/terraform/b2c/env/t-001/b2c-access_override.tf
@@ -9,17 +9,13 @@ resource "azuread_invitation" "xkber" {
 
 #NHQ
 resource "azuread_directory_role_assignment" "nhq" {
-  count = 1
-  role_id             = azuread_directory_role.global_admin.template_id
-  principal_object_id = azuread_invitation.nhq[0].user_id
+  count = 0
 }
 
 resource "azuread_invitation" "nhq" {
-  count = 1
-  user_email_address = "nhq@energinet.dk"
-  user_display_name = "SEC-A-Greenforce-PlatformTeamAzure member"
-  redirect_url       = "https://portal.azure.com"
+  count = 0
 }
+
 #DBJ
 resource "azuread_directory_role_assignment" "dbj" {
   count = 0
