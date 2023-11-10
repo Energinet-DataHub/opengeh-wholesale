76292375985fa80711bede4d6e96ffdc47809a38
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-11-10 11:58:15 +0100

    made nhq global admin on test_001+t-001 (#632)
    * made nhq global admin
    
    * made nhq global admin on test_001

--- shared-resources/terraform/b2c/env/t-001/b2c-access_override.tf
+++ shared-resources/terraform/b2c/env/t-001/b2c-access_override.tf
@@ -9,13 +9,17 @@ resource "azuread_invitation" "xkber" {
 
 #NHQ
 resource "azuread_directory_role_assignment" "nhq" {
-  count = 0
+  count = 1
+  role_id             = azuread_directory_role.global_admin.template_id
+  principal_object_id = azuread_invitation.nhq[0].user_id
 }
 
 resource "azuread_invitation" "nhq" {
-  count = 0
+  count = 1
+  user_email_address = "nhq@energinet.dk"
+  user_display_name = "SEC-A-Greenforce-PlatformTeamAzure member"
+  redirect_url       = "https://portal.azure.com"
 }
-
 #DBJ
 resource "azuread_directory_role_assignment" "dbj" {
   count = 0
