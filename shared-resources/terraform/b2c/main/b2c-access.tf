08e26dfd36a0b715ffe5c43e356e8cececd6f1d1
Kristian Bank Erbou <contact@kbeconsult.dk>
2023-11-14 10:52:30 +0100

    Revert XKBER to B2C Global Reader (#616)

--- shared-resources/terraform/b2c/main/b2c-access.tf
+++ shared-resources/terraform/b2c/main/b2c-access.tf
@@ -36,7 +36,7 @@ resource "azuread_directory_role" "global_admin" {
 #XKBER
 resource "azuread_directory_role_assignment" "xkber" {
   count = 1
-  role_id             = azuread_directory_role.global_admin.template_id
+  role_id             = azuread_directory_role.global_reader.template_id
   principal_object_id = azuread_invitation.xkber[0].user_id
 }
 
