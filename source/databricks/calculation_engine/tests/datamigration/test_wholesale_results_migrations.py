68ca999791621957415bd2c1b36588b360496a9b
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-11-15 16:25:03 +0100

    more fix

--- source/databricks/calculation_engine/tests/datamigration/test_wholesale_results_migrations.py
+++ source/databricks/calculation_engine/tests/datamigration/test_wholesale_results_migrations.py
@@ -158,7 +158,6 @@ actor_eic = "1234567890123456"
         (WholesaleResultColumnNames.grid_area, "007"),
         (WholesaleResultColumnNames.energy_supplier_id, actor_gln),
         (WholesaleResultColumnNames.energy_supplier_id, actor_eic),
-        (WholesaleResultColumnNames.quantity, None),
         (WholesaleResultColumnNames.quantity, max_18_3_decimal),
         (WholesaleResultColumnNames.quantity, min_18_3_decimal),
         (WholesaleResultColumnNames.quantity_unit, "kWh"),
