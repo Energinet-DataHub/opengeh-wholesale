63a764afaebec9ae15a1cb81fe2ad901cbfb69c1
Dan Stenrøjl <XDAST@energinet.dk>
2023-11-15 16:05:28 +0100

    WholesaleFixingCalculationScenario

--- source/dotnet/domain-tests/Fixtures/CalculationScenarioState.cs
+++ source/dotnet/domain-tests/Fixtures/CalculationScenarioState.cs
@@ -35,5 +35,11 @@ namespace Energinet.DataHub.Wholesale.DomainTests.Fixtures
 
         public IReadOnlyCollection<EnergyResultProducedV2> ReceivedEnergyResultProducedV2 { get; set; }
             = new List<EnergyResultProducedV2>();
+
+        public IReadOnlyCollection<AmountPerChargeResultProducedV1> ReceivedAmountPerChargeResultProducedV1 { get; set; }
+            = new List<AmountPerChargeResultProducedV1>();
+
+        public IReadOnlyCollection<MonthlyAmountPerChargeResultProducedV1> ReceivedMonthlyAmountPerChargeResultProducedV1 { get; set; }
+            = new List<MonthlyAmountPerChargeResultProducedV1>();
     }
 }
