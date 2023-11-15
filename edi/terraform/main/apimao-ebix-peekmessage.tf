51f7a958e38aea7fbf6ccae3fdbec07fd35cb6c9
Ebbe Knudsen <xedkn@energinet.dk>
2023-11-15 10:23:38 +0100

    feat: Add ebIX peek endpoint in API Management (#641)
    - Add ebIX SOAP API to API Management
    - Add ebIX peek endpoint to API Management ebIX API
    
    ---------
    
    Co-authored-by: jbn72 <144002872+jbn72@users.noreply.github.com>
    Co-authored-by: Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
    Co-authored-by: Lasse Ringgren Nielsen <sixxth@gmail.com>

---
+++ edi/terraform/main/apimao-ebix-peekmessage.tf
@@ -0,0 +1,56 @@
+module "apimao_ebix_peekmessage" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/api-management-api-operation?ref=v13"
+
+  resource_group_name     = data.azurerm_key_vault_secret.apim_instance_resource_group_name.value
+  api_management_name     = data.azurerm_key_vault_secret.apim_instance_name.value
+  api_management_api_name = module.apima_b2b_ebix.name
+
+  operation_id            = "peek-message" // Taken from the imported WSDL, is this correct?
+  method                  = "POST"
+  display_name            = "Peek message - ebIX"
+  url_template            = "/?soapAction=peekMessage"
+
+  policies = [
+    {
+      xml_content = <<XML
+        <policies>
+          <inbound>
+            <base />
+            <validate-jwt header-name="Authorization" failed-validation-httpcode="403" failed-validation-error-message="Unauthorized to access this endpoint.">
+                <openid-config url="https://login.microsoftonline.com/${data.azurerm_key_vault_secret.apim_b2c_tenant_id.value}/v2.0/.well-known/openid-configuration" />
+                <required-claims>
+                    <claim name="roles" match="any">
+                        <value>metereddataresponsible</value>
+                        <value>energysupplier</value>
+                        <value>balanceresponsibleparty</value>
+                    </claim>
+                </required-claims>
+            </validate-jwt>
+            <set-backend-service backend-id="${azurerm_api_management_backend.edi.name}" />
+            <rewrite-uri template="/api/peek/Aggregations" copy-unmatched-params="false" /> <!-- TODO: Remove /Aggregations? (response is empty) -->
+            <set-method>GET</set-method>
+          </inbound>
+          <backend>
+            <base />
+          </backend>
+          <outbound>
+            <base />
+            <set-variable name="responseBody" value="@(context.Response.Body.As<string>().Replace("<?xml version=\"1.0\" encoding=\"utf-8\"?>", ""))" />
+            <set-body template="liquid">
+              <soap-env:Envelope xmlns:soap-env="http://schemas.xmlsoap.org/soap/envelope/">
+                <soap-env:Body>
+                  <PeekMessageResponse xmlns="urn:www:datahub:dk:b2b:v01" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    {{context.Variables["responseBody"]}}
+                  </PeekMessageResponse>
+                </soap-env:Body>
+              </soap-env:Envelope>
+            </set-body>
+          </outbound>
+          <on-error>
+            <base />
+          </on-error>
+        </policies>
+      XML
+    }
+  ]
+}
