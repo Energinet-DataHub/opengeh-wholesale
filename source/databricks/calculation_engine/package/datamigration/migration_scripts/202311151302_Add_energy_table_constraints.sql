1c8226f290891bb04e16664ecdbb8b2e8ad8b6a1
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-11-16 09:36:56 +0100

    drop constraints before adding

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202311151302_Add_energy_table_constraints.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202311151302_Add_energy_table_constraints.sql
@@ -1,7 +1,13 @@
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS calculation_type_chk
+GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT calculation_type_chk CHECK (calculation_type IN ('BalanceFixing', 'Aggregation', 'WholesaleFixing', 'FirstCorrectionSettlement', 'SecondCorrectionSettlement', 'ThirdCorrectionSettlement'))
 GO
 
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS time_series_type_chk
+GO
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT time_series_type_chk
         CHECK (time_series_type IN (
@@ -19,13 +25,22 @@ ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
 GO
 
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS grid_area_chk
+GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT grid_area_chk CHECK (LENGTH(grid_area) = 3)
 GO
 
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS out_grid_area_chk
+GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT out_grid_area_chk CHECK (out_grid_area IS NULL OR LENGTH(out_grid_area) = 3)
 GO
 
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS quantity_qualities_chk
+GO
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT quantity_qualities_chk
     CHECK (array_size(array_except(quantity_qualities, array('missing', 'calculated', 'measured', 'estimated'))) = 0
@@ -33,23 +48,38 @@ ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
 GO
 
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS aggregation_level_chk
+GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT aggregation_level_chk CHECK (aggregation_level IN ('total_ga', 'es_brp_ga', 'es_ga', 'brp_ga'))
 GO
 
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS energy_supplier_id_chk
+GO
 -- Length is 16 when EIC and 13 when GLN
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT energy_supplier_id_chk CHECK (energy_supplier_id IS NULL OR LENGTH(energy_supplier_id) = 13 OR LENGTH(energy_supplier_id) = 16)
 GO
 
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS balance_responsible_id_chk
+GO
 -- Length is 16 when EIC and 13 when GLN
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT balance_responsible_id_chk CHECK (balance_responsible_id IS NULL OR LENGTH(balance_responsible_id) = 13 OR LENGTH(balance_responsible_id) = 16)
 GO
 
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS calculation_id_chk
+GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT calculation_id_chk CHECK (LENGTH(calculation_id) = 36)
 GO
 
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS calculation_result_id_chk
+GO
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT calculation_result_id_chk CHECK (LENGTH(calculation_result_id) = 36)
 GO
