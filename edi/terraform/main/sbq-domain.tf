db94931cde39087c6f19af665577f0677f66304b
Lasse Ringgren Nielsen <sixxth@gmail.com>
2023-11-21 15:30:17 +0100

    Update sbq-domain.tf (#671)

--- edi/terraform/main/sbq-domain.tf
+++ edi/terraform/main/sbq-domain.tf
@@ -1,5 +1,5 @@
 resource "azurerm_servicebus_queue" "edi_incoming_messages_queue" {
   name                         = "sbq-${lower(var.domain_name_short)}-incoming-messages"
-  namespace_id                 =  data.azurerm_key_vault_secret.sb_domain_relay_namespace_id
+  namespace_id                 =  data.azurerm_key_vault_secret.sb_domain_relay_namespace_id.value
 }
 
