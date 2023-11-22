acba6e6502a8c3b36efa41755ed8d5a1762044ab
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2023-11-22 15:01:03 +0100

    fix: added /none to peek in apim, such that we hit our function (#674)

--- edi/terraform/main/apimao-ebix-peekmessage.tf
+++ edi/terraform/main/apimao-ebix-peekmessage.tf
@@ -27,7 +27,7 @@ module "apimao_ebix_peekmessage" {
                 </required-claims>
             </validate-jwt>
             <set-backend-service backend-id="${azurerm_api_management_backend.edi.name}" />
-            <rewrite-uri template="/api/peek" copy-unmatched-params="false" />
+            <rewrite-uri template="/api/peek/none" copy-unmatched-params="false" />
             <set-method>GET</set-method>
           </inbound>
           <backend>
