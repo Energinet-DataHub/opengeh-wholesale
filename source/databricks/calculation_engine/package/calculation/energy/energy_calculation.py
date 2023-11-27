1199e8944c33cf12cd98228f79799263127e0cbe
Jonas Michelsen <jvm@energinet.dk>
2023-11-27 13:47:18 +0100

    Update energy_calculation.py

--- source/databricks/calculation_engine/package/calculation/energy/energy_calculation.py
+++ source/databricks/calculation_engine/package/calculation/energy/energy_calculation.py
@@ -53,7 +53,6 @@ def execute(
     quarterly_metering_point_time_series = transform_hour_to_quarter(
         metering_point_time_series
     )
-    quarterly_metering_point_time_series.df.cache()
 
     _calculate(
         batch_process_type,
