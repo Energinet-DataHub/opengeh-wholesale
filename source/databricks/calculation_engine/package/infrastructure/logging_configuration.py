337416ac1e8ae7d2814589ce900a953cf48d4b98
BjarkeMeier <hej@bjarkemeier.dk>
2023-11-28 14:16:20 +0100

    Add interceptors

--- source/databricks/calculation_engine/package/infrastructure/logging_configuration.py
+++ source/databricks/calculation_engine/package/infrastructure/logging_configuration.py
@@ -21,16 +21,45 @@ from azure.monitor.opentelemetry import configure_azure_monitor
 
 
 def initialize_logging() -> None:
-    configure_azure_monitor(
-        connection_string="",
-    )
+    """
+    Configure logging to use OpenTelemetry and Azure Monitor.
 
-    logging.Logger.info = _interceptor(logging.INFO)
-    logging.Logger.error = _interceptor(logging.INFO)
+    The logging is configured to add "Domain"="wholesale" as structured logging.
+    This enables us to filter on the domain in Azure Monitor.
+    """
+    # Configure OpenTelemetry to use Azure Monitor.
+    # The connection string is read from the environment variable APPLICATIONINSIGHTS_CONNECTION_STRING
+    # as it is not provided explicitly.
+    configure_azure_monitor()
+
+    logging.Logger.debug = _create_interceptor(logging.DEBUG)
+    logging.Logger.info = _create_interceptor(logging.INFO)
+    logging.Logger.warn = _create_interceptor(logging.WARN)
+    logging.Logger.warning = _create_interceptor(logging.WARNING)
+    logging.Logger.error = _create_interceptor(logging.ERROR)
+    logging.Logger.exception = _create_exception_interceptor()
+    logging.Logger.critical = _create_interceptor(logging.CRITICAL)
+    logging.Logger.fatal = _create_interceptor(logging.FATAL)
+    logging.Logger.log = _create_log_interceptor()
+
     logging.basicConfig(level=logging.INFO)
 
 
-def _interceptor(self: logging.Logger, msg, *args, **kwargs):
+def _create_interceptor(level: int, msg, *args, **kwargs):
+    _add_extra(**kwargs)
+    return lambda self: self._log(level, msg, args, **kwargs)
+
+
+def _create_exception_interceptor(msg, *args, exc_info=True, **kwargs):
+    _add_extra(**kwargs)
+    return lambda self: self._log(logging.ERROR, msg, args, exc_info=exc_info, **kwargs)
+
+
+def _create_log_interceptor(level: int, msg, *args, **kwargs):
+    _add_extra(**kwargs)
+    return lambda self: self._log(level, msg, args, **kwargs)
+
+
+def _add_extra(**kwargs):
     kwargs["extra"] = {"Domain": "wholesale"}
-    return
-    self._log(logging.INFO, msg, args, **kwargs)
+    return kwargs
