1c7cbcff382128f51e3c47547db071abcf354808
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2023-11-29 14:48:46 +0100

    Add function app hybrid connection workaround (#726)
    * Add function app hybrid connection workaround
    
    * removed unecessary auth rule on HC
    
    * fix relay ref
    
    * fix func app name
    
    * Add comment with link to workaround

--- esett-deprecated/terraform/main/func-biztalkshipper-hc.tf
+++ esett-deprecated/terraform/main/func-biztalkshipper-hc.tf
@@ -3,4 +3,16 @@ resource "azurerm_function_app_hybrid_connection" "biztalkshipper_biztalk" {
   relay_id        = azurerm_relay_hybrid_connection.biztalk.id
   hostname        = "datahub.preproduction.biztalk.energinet.local"
   port            = 443
+
+  lifecycle {
+    ignore_changes = [
+      relay_id
+    ]
+  }
+
+  # This is a workaround for a bug in the azurerm provider, for more info see:
+  # - https://stigvoss.dk/2023/11/16/creating-hybrid-connection-with-terraform/
+  provisioner "local-exec" {
+    command = "az functionapp hybrid-connection add --hybrid-connection ${azurerm_relay_hybrid_connection.biztalk.name} --namespace ${azurerm_relay_namespace.this.name} -n ${module.func_biztalkreceiver.name} -g ${azurerm_resource_group.this.name} --subscription ${var.subscription_id}"
+  }
 }
