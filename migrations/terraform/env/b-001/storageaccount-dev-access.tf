5f320cdadf0b1ed0db8428370246601ec204da36
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2023-11-30 15:44:45 +0100

    Move Storage Blob Data Contributor to st_dh2dropzone (#737)

--- migrations/terraform/env/b-001/storageaccount-dev-access.tf
+++ migrations/terraform/env/b-001/storageaccount-dev-access.tf
@@ -3,14 +3,14 @@
 #
 
 # st_dh2data
-resource "azurerm_role_assignment" "st_dh2data_henrik_sommer_storage_blob_data_contributor" {
-  scope                = module.st_dh2data.id
+resource "azurerm_role_assignment" "st_dh2dropzone_henrik_sommer_storage_blob_data_contributor" {
+  scope                = module.st_dh2dropzone.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = "c2f75d02-f64f-4111-aaea-6c43f5bc8d65"
 }
 
-resource "azurerm_role_assignment" "st_dh2data_peter_tandrup_storage_blob_data_contributor" {
-  scope                = module.st_dh2data.id
+resource "azurerm_role_assignment" "st_dh2dropzone_peter_tandrup_storage_blob_data_contributor" {
+  scope                = module.st_dh2dropzone.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = "b53ad815-1d62-4251-9ac9-962aeaa59edd"
 }
