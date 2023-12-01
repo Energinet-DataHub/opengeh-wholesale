06ec0d578c07dfb45a0540028a3a43245af7984f
Kristian Bank Erbou <contact@kbeconsult.dk>
2023-12-01 14:44:21 +0100

    Rollback PR-742 (#749)

--- shared-resources/terraform/infrastructure/main/ad-groups-access.tf
+++ shared-resources/terraform/infrastructure/main/ad-groups-access.tf
@@ -6,9 +6,9 @@
 data "azurerm_subscription" "current" {
 }
 
-data "azurerm_resource_group" "rg_tfstate" {
-  name = "rg-tfs-${var.environment_short}-${var.region_short}-${var.environment_instance}"
-}
+# data "azurerm_resource_group" "rg_tfstate" {
+#   name = "rg-tfs-${var.environment_short}-${var.region_short}-${var.environment_instance}"
+# }
 
 resource "azurerm_role_assignment" "developers_subscription_reader" {
   scope                = data.azurerm_subscription.current.id
@@ -16,19 +16,19 @@ resource "azurerm_role_assignment" "developers_subscription_reader" {
   principal_id         = var.developers_security_group_object_id
 }
 
-resource "azurerm_role_definition" "deny_dataplane_access_to_tfs_rg" {
-  name        = "deny-dataplace-access-to-tfs-rg"
-  scope       = data.azurerm_resource_group.rg_tfstate.id
-  description = "Deny dataplane access to Terraform state"
+# resource "azurerm_role_definition" "deny_dataplane_access_to_tfs_rg" {
+#   name        = "deny-dataplace-access-to-tfs-rg"
+#   scope       = data.azurerm_resource_group.rg_tfstate.id
+#   description = "Deny dataplane access to Terraform state"
 
-  permissions {
-    not_data_actions = ["Microsoft.Storage/*"]
-  }
-}
+#   permissions {
+#     not_data_actions = ["Microsoft.Storage/*"]
+#   }
+# }
 
-resource "azurerm_role_assignment" "deny_developer_dataplane_access_to_tfs_rg" {
-  scope                = data.azurerm_resource_group.rg_tfstate.id
-  role_definition_name = resource.azurerm_role_definition.deny_dataplane_access_to_tfs_rg.name
-  principal_id         = var.developers_security_group_object_id
-}
+# resource "azurerm_role_assignment" "deny_developer_dataplane_access_to_tfs_rg" {
+#   scope                = data.azurerm_resource_group.rg_tfstate.id
+#   role_definition_name = resource.azurerm_role_definition.deny_dataplane_access_to_tfs_rg.name
+#   principal_id         = var.developers_security_group_object_id
+# }
 
