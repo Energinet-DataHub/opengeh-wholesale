25b36a628faca6104183d5b41348ad965182a7fa
Rasmus Leth Friis <116742936+rf-halfspace@users.noreply.github.com>
2023-12-04 16:33:14 +0100

    Grant access to migrations for xrlfr and jdd (#753)
    * grant data contributor to JDD and XRLFR
    
    * changed esett name
    
    * grant permissions on all
    
    * Delete migrations/terraform/env/dev_001/variables_override.tf
    
    * grant permissions on all
    
    * access to all
    
    ---------
    
    Co-authored-by: René T. Nielsen <renetdev@gmail.com>

--- migrations/terraform/env/dev_001/st-data-lake-role-assignments.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_role_assignment" "st_migrations_access" {
-  count                = length(var.developer_object_ids)
-  principal_id         = var.developer_object_ids[count.index]
-  role_definition_name = "Storage Blob Data Contributor"
-  scope                = module.st_migrations.id
-}
