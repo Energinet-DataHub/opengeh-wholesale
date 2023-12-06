b30ece1efeb46e0c8748ef99b120d6c23fbba5a8
Peter Tandrup <71757561+prtandrup@users.noreply.github.com>
2023-12-06 14:31:52 +0100

    Dev access to `st_dh2data` in `b-001` (#762)
    If applied
    provides Henrik Sommer and Peter Tandrup blob 
    contributor access to the st_dh2data storage, in order
    to clean up before doing an new data import

--- migrations/terraform/env/b-001/storageaccount-dev-access.tf
+++ migrations/terraform/env/b-001/storageaccount-dev-access.tf
@@ -2,19 +2,37 @@
 # Add developer access to storage accounts
 #
 
-# st_dh2data
+# st_dh2dropzone
+# Henrik Sommer (XHESO)
 resource "azurerm_role_assignment" "st_dh2dropzone_henrik_sommer_storage_blob_data_contributor" {
   scope                = module.st_dh2dropzone.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = "c2f75d02-f64f-4111-aaea-6c43f5bc8d65"
 }
 
+# Peter Tandrup (PTA)
 resource "azurerm_role_assignment" "st_dh2dropzone_peter_tandrup_storage_blob_data_contributor" {
   scope                = module.st_dh2dropzone.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = "b53ad815-1d62-4251-9ac9-962aeaa59edd"
 }
 
+# st_dh2data
+# Henrik Sommer (XHESO)
+resource "azurerm_role_assignment" "st_dh2data_henrik_sommer_storage_blob_data_contributor" {
+  scope                = module.st_dh2data.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = "c2f75d02-f64f-4111-aaea-6c43f5bc8d65"
+}
+
+# Peter Tandrup (PTA)
+resource "azurerm_role_assignment" "st_dh2data_peter_tandrup_storage_blob_data_contributor" {
+  scope                = module.st_dh2data.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = "b53ad815-1d62-4251-9ac9-962aeaa59edd"
+}
+
+# st_migrations
 # Henrik Sommer (XHESO)
 resource "azurerm_role_assignment" "st_migrations_henrik_sommer_storage_blob_data_contributor" {
   scope                = module.st_migrations.id
