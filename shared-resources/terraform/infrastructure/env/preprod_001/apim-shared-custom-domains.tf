d48179b9a7097c54f57463b0a07f9296c8f78737
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2023-12-08 13:34:13 +0100

    Preprod001: APIM keyvault permissions (#770)

--- shared-resources/terraform/infrastructure/env/preprod_001/apim-shared-custom-domains.tf
+++ shared-resources/terraform/infrastructure/env/preprod_001/apim-shared-custom-domains.tf
@@ -1,3 +1,20 @@
+resource "azurerm_key_vault_access_policy" "certificate_permissions" {
+  key_vault_id = module.kv_shared.id
+
+  object_id = module.apim_shared.identity.0.principal_id
+  tenant_id = module.apim_shared.identity.0.tenant_id
+
+  certificate_permissions = [
+    "Get",
+    "List",
+    "Import"
+  ]
+
+  secret_permissions = [
+    "Get"
+  ]
+}
+
 resource "azurerm_key_vault_certificate" "b2b_datahub3_certificate" {
   name         = "cert-b2b-datahub3"
   key_vault_id = module.kv_shared.id
