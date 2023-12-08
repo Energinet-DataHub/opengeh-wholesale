8321197c26715435fe23f1538b6f23a6bf12e920
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2023-12-08 10:05:14 +0100

    DH2-Bridge: Override Grid loss simulator in U001 (#766)
    * override in U001
    
    * add keyvault access policy for simulator

---
+++ dh2-bridge/terraform/env/u-001/func-grid-loss-simulator_override.tf
@@ -0,0 +1,3 @@
+module "func_entrypoint_grid_loss_simulator" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=v12"
+}
--- dh2-bridge/terraform/main/kv-shared-access-policy.tf
+++ dh2-bridge/terraform/main/kv-shared-access-policy.tf
@@ -4,3 +4,10 @@ module "kv_shared_access_policy_func_grid_loss_event_receiver" {
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
   app_identity = module.func_entrypoint_grid_loss_event_receiver.identity.0
 }
+
+module "kv_shared_access_policy_func_grid_loss_simulator" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-access-policy?ref=v13"
+
+  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
+  app_identity = module.func_entrypoint_grid_loss_simulator.identity.0
+}
