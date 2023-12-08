1660b86ec667ee7ba4e41f14b8b320b161773b05
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2023-12-08 12:13:01 +0100

    Add certificates for APIM in preprod-001 (#768)

---
+++ shared-resources/terraform/infrastructure/env/preprod_001/apim-shared-custom-domains.tf
@@ -0,0 +1,37 @@
+resource "azurerm_key_vault_certificate" "b2b_datahub3_certificate" {
+  name         = "cert-b2b-datahub3"
+  key_vault_id = module.kv_shared.id
+
+  certificate {
+    contents = filebase64("${path.module}/assets/preprod_b2b_datahub3_dk.pfx")
+    password = var.cert_b2b_datahub3_password
+  }
+}
+
+resource "azurerm_api_management_custom_domain" "b2b_custom_domain" {
+  api_management_id = module.apim_shared.id
+
+  gateway {
+    host_name    = "preprod.b2b.energinet.dk"
+    key_vault_id = azurerm_key_vault_certificate.b2b_datahub3_certificate.versionless_secret_id
+  }
+}
+
+resource "azurerm_key_vault_certificate" "ebix_datahub3_certificate" {
+  name         = "cert-ebix-datahub3"
+  key_vault_id = module.kv_shared.id
+
+  certificate {
+    contents = filebase64("${path.module}/assets/preprod_ebix_datahub3_dk.pfx")
+    password = var.cert_ebix_datahub3_password
+  }
+}
+
+resource "azurerm_api_management_custom_domain" "ebix_custom_domain" {
+  api_management_id = module.apim_shared.id
+
+  gateway {
+    host_name    = "preprod.ebix.datahub3.dk"
+    key_vault_id = azurerm_key_vault_certificate.ebix_datahub3_certificate.versionless_secret_id
+  }
+}
---
+++ shared-resources/terraform/infrastructure/env/preprod_001/variables_additional.tf
@@ -0,0 +1,9 @@
+variable "cert_b2b_datahub3_password" {
+  type        = string
+  description = "Password for the B2C Datahub 3 certificate."
+}
+
+variable "cert_ebix_datahub3_password" {
+  type        = string
+  description = "Password for the eBix Datahub 3 certificate."
+}
