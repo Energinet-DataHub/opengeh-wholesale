a54cbe28b9cee71a46cf37a49ce3c386ea3da39b
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2023-12-11 09:39:54 +0100

    Add validation_type for azurerm_static_site_custom_domain (#776)

--- ui/terraform/env/preprod_001/ui-app-custom-domain.tf
+++ ui/terraform/env/preprod_001/ui-app-custom-domain.tf
@@ -1,4 +1,5 @@
 resource "azurerm_static_site_custom_domain" "this" {
   static_site_id = azurerm_static_site.ui.id
   domain_name    = "preprod.datahub3.dk"
+  validation_type = "cname-delegation"
 }
