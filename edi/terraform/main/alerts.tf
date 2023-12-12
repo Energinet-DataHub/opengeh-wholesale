7e906ffcbccacb04c338666f1f859aecb002c299
Mads Magnus Due <xmadu@energinet.dk>
2023-12-12 12:01:49 +0100

    fix: wrong operation in alert rule for EDI (#787)
    * feat: add b2c web app to edi alert
    
    * added b2c + new subscriptions
    
    * wrong operation

--- edi/terraform/main/alerts.tf
+++ edi/terraform/main/alerts.tf
@@ -26,9 +26,9 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "edi_alert" {
                   exceptions
                   | where timestamp > ago(10m)
                     and (cloud_RoleName == 'func-api-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}'
-                      || cloud_RoleName == 'func-api-${lower(var.domain_name_short)}-${lower(var.environment_short)}-we-${lower(var.environment_instance)}'
-                      || cloud_RoleName == 'app-b2cwebapi-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}'
-                      || cloud_RoleName == 'app-b2cwebapi-${lower(var.domain_name_short)}-${lower(var.environment_short)}-we-${lower(var.environment_instance)}')
+                      or cloud_RoleName == 'func-api-${lower(var.domain_name_short)}-${lower(var.environment_short)}-we-${lower(var.environment_instance)}'
+                      or cloud_RoleName == 'app-b2cwebapi-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}'
+                      or cloud_RoleName == 'app-b2cwebapi-${lower(var.domain_name_short)}-${lower(var.environment_short)}-we-${lower(var.environment_instance)}')
                     and (type !has "Energinet.DataHub.EDI" and type !hasprefix "NotSupported")
                 QUERY
   severity       = 1
