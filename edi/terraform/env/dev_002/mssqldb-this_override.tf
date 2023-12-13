0f80161e0adff16fdf659272072accf893690f5e
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-12-13 15:24:42 +0100

    gave EDI db access (#794)

--- edi/terraform/env/dev_002/mssqldb-this_override.tf
+++ edi/terraform/env/dev_002/mssqldb-this_override.tf
@@ -1,5 +1,6 @@
 module "mssqldb_edi" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=v13"
+  source                  = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=v13"
 
-  datawriter_access = true
+  developer_ad_group_name = var.developer_ad_group_name
+  datawriter_access       = true
 }
--- edi/terraform/env/test_001/mssqldb-this_override.tf
+++ edi/terraform/env/test_001/mssqldb-this_override.tf
@@ -1,3 +1,5 @@
 module "mssqldb_edi" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=v13"
+
+  developer_ad_group_name = var.developer_ad_group_name
 }
