531107391bb3411329586ad99980304ff0a4b95c
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2023-12-14 15:10:40 +0100

    BatchId -> CalculationId

--- source/dotnet/wholesale-api/Shared/Configuration/LoggingConstants.cs
+++ source/dotnet/wholesale-api/Shared/Configuration/LoggingConstants.cs
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+namespace Energinet.DataHub.Wholesale.Shared.Configuration;
 public static class LoggingConstants
 {
-    public const string BatchId = "{BatchId}";
+    public const string CalculationId = "{CalculationId}";
 }
