e416cfbc55f063250d020b63e5bc1b40a55cabce
Kristian Bank Erbou <contact@kbeconsult.dk>
2023-12-18 14:02:16 +0100

    Remove XKBER as Global Admin on B2C test_001 (#815)

--- shared-resources/terraform/b2c/env/test_001/b2c-access_override.tf
+++
@@ -1,5 +0,0 @@
-resource "azuread_directory_role_assignment" "xkber" {
-  count = 1
-  role_id             = azuread_directory_role.global_admin.template_id
-  principal_object_id = azuread_invitation.xkber[0].user_id
-}
