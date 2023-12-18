10bb3ff724287d250d073f8befdd98cc5a7da851
René T. Nielsen <renetdev@gmail.com>
2023-12-18 12:58:12 +0100

    remove unused infrastructure code (#817)

--- esett-exchange/terraform/env/u-001/storage-esett-documents_override.tf
+++
@@ -1,8 +0,0 @@
-module "storage_esett_documents" {
-  role_assignments = [
-    {
-      principal_id         = var.developer_ad_group_object_id
-      role_definition_name = "Storage Blob Data Reader"
-    }
-  ]
-}
