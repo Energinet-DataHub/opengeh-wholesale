abca8feaa643d3ded8a1655796756358fddadfc6
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-12-22 09:20:31 +0100

    onboarding healthchecksui to ccp (#844)
    * onboarded health checks ui
    
    * sauron test_002
    
    * Delete dh3-infrastructure.sln

--- health-checks-ui/terraform/main/backend.tf
+++ health-checks-ui/terraform/main/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "@resource_group_name"
     storage_account_name = "@storage_account_name"
-    container_name       = "tfstate"
-    key                  = "health_checks_ui.tfstate"
+    container_name       = "tfs"
+    key                  = "health_checks_ui.tfs"
     use_oidc             = true
     use_azuread_auth     = true
     subscription_id      = "@azure_subscription_id"
