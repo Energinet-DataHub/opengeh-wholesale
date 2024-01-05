371e153e3d0a742e8a014f7f6daa5cfece9f75b1
Kristian Bank Erbou <contact@kbeconsult.dk>
2024-01-05 13:05:02 +0100

    Remove IP whitelists no longer needed after AVD access has been established (#899)

--- shared-resources/terraform/infrastructure/env/prod_001/mssql-network-ipwhitelists.tf
+++ shared-resources/terraform/infrastructure/env/prod_001/mssql-network-ipwhitelists.tf
@@ -1,36 +1,3 @@
-resource "azurerm_sql_firewall_rule" "jvm_home" {
-  name                = "JVM Home"
-  resource_group_name = azurerm_resource_group.this.name
-  server_name         = module.mssql_data_additional.name
-  start_ip_address    = "128.76.145.127"
-  end_ip_address      = "128.76.145.127"
-}
-
-
-resource "azurerm_sql_firewall_rule" "energinet_local_ip_range" {
-  name                = "Energinet local IP Range"
-  resource_group_name = azurerm_resource_group.this.name
-  server_name         = module.mssql_data_additional.name
-  start_ip_address    = "194.239.2.0"
-  end_ip_address      = "194.239.2.255"
-}
-
-resource "azurerm_sql_firewall_rule" "vesterballevej" {
-  name                = "Vesterballevej"
-  resource_group_name = azurerm_resource_group.this.name
-  server_name         = module.mssql_data_additional.name
-  start_ip_address    = "152.115.37.250"
-  end_ip_address      = "152.115.37.250"
-}
-
-resource "azurerm_sql_firewall_rule" "systemate" {
-  name                = "Systemate"
-  resource_group_name = azurerm_resource_group.this.name
-  server_name         = module.mssql_data_additional.name
-  start_ip_address    = "93.160.57.54"
-  end_ip_address      = "93.160.57.54"
-}
-
 resource "azurerm_mssql_firewall_rule" "esett_deprecated_powerbi_rule_1" {
   name              = "Esett Deprecated PowerBI Rule #1"
   server_id         = module.mssql_data_additional.id
