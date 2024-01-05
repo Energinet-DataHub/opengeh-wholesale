f5b14e5494c5052507e64f0f0cdb0e7d77b0b784
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2024-01-05 10:27:41 +0100

    bugfix: created DNS zone for txt record (#897)
    * bugfix: created DNS zone for txt record

--- ui/terraform/env/prod_001/ui-txt-record.tf
+++ ui/terraform/env/prod_001/ui-txt-record.tf
@@ -1,6 +1,11 @@
+resource "azurerm_dns_zone" "this" {
+  name                = local.frontend_url
+  resource_group_name = azurerm_resource_group.this.name
+}
+
 resource "azurerm_dns_txt_record" "this" {
   name                = "@"
-  zone_name           = local.frontend_url
+  zone_name           = azurerm_dns_zone.this.name
   resource_group_name = azurerm_resource_group.this.name
   ttl                 = 3600
   record {
