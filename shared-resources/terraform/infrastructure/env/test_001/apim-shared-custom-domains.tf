5806b24b3032f792be1c075c64c285e16ec06374
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2024-01-05 09:17:01 +0100

    API Management: Add domains to dev and test environments (#890)
    * API Management: SSL certificates for dev and test environments
    
    * API Management: prod ebix domain - negotiate client certificates

--- shared-resources/terraform/infrastructure/env/test_001/apim-shared-custom-domains.tf
+++ shared-resources/terraform/infrastructure/env/test_001/apim-shared-custom-domains.tf
@@ -3,7 +3,7 @@ resource "azurerm_key_vault_certificate" "b2b_datahub3_certificate" {
   key_vault_id = module.kv_shared.id
 
   certificate {
-    contents = filebase64("${path.module}/assets/b2b_datahub3_dk.pfx")
+    contents = filebase64("${path.module}/assets/test_b2b_datahub3_dk.pfx")
     password = var.cert_b2b_datahub3_password
   }
 }
@@ -13,7 +13,7 @@ resource "azurerm_key_vault_certificate" "ebix_datahub3_certificate" {
   key_vault_id = module.kv_shared.id
 
   certificate {
-    contents = filebase64("${path.module}/assets/ebix_datahub3_dk.pfx")
+    contents = filebase64("${path.module}/assets/test_ebix_datahub3_dk.pfx")
     password = var.cert_ebix_datahub3_password
   }
 }
@@ -22,12 +22,12 @@ resource "azurerm_api_management_custom_domain" "datahub3_custom_domains" {
   api_management_id = module.apim_shared.id
 
   gateway {
-    host_name    = "b2b.datahub3.dk"
+    host_name    = "test.b2b.datahub3.dk"
     key_vault_id = azurerm_key_vault_certificate.b2b_datahub3_certificate.versionless_secret_id
   }
 
   gateway {
-    host_name                    = "ebix.datahub3.dk"
+    host_name                    = "test.ebix.datahub3.dk"
     key_vault_id                 = azurerm_key_vault_certificate.ebix_datahub3_certificate.versionless_secret_id
     negotiate_client_certificate = true
   }
