0e3f43c71fa5d5a1503952b244007e2b45d34a70
Jonas Michelsen <jvm@energinet.dk>
2024-01-09 15:04:24 +0100

    Update 202401091040_Create_grid_loss_responsible_table.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202401091040_Create_grid_loss_responsible_table.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202401091040_Create_grid_loss_responsible_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS {INPUT_DATABASE_NAME}.grid_loss_responsible
 (
-    metering_point_id STRING NOT NULL,
+    metering_point_id STRING NOT NULL
 )
 USING DELTA
 -- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used.
