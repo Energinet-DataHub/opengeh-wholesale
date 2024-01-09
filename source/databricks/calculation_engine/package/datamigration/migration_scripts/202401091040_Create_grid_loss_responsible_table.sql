b39e06fa8ddbf143f63781b87b02c42a9c94fc98
Jonas Michelsen <jvm@energinet.dk>
2024-01-09 14:20:04 +0100

    .

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202401091040_Create_grid_loss_responsible_table.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202401091040_Create_grid_loss_responsible_table.sql
@@ -3,3 +3,6 @@ CREATE TABLE IF NOT EXISTS {INPUT_DATABASE_NAME}.grid_loss_responsible
     metering_point_id STRING NOT NULL,
 )
 USING DELTA
+-- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used.
+-- In the production it is set to empty and the respective location is used. This means the production tables won't be deleted if the schema is.
+{TEST}LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/grid_loss_responsible'
