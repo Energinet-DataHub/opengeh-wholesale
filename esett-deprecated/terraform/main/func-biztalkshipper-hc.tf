6887175cd08a72890c67823f72fee835fc39295e
René T. Nielsen <renetdev@gmail.com>
2024-01-10 19:18:56 +0100

    try remove manually added connection stuff (#933)

--- esett-deprecated/terraform/main/func-biztalkshipper-hc.tf
+++ esett-deprecated/terraform/main/func-biztalkshipper-hc.tf
@@ -1,18 +1,7 @@
+
 resource "azurerm_function_app_hybrid_connection" "biztalkshipper_biztalk" {
   function_app_id = module.func_biztalkshipper.id
   relay_id        = data.azurerm_key_vault_secret.hc_biztalk_id.value
   hostname        = var.biztalk_hybrid_connection_hostname
   port            = 443
-
-  lifecycle {
-    ignore_changes = [
-      relay_id
-    ]
-  }
-
-  # This is a workaround for a bug in the azurerm provider, for more info see:
-  # - https://stigvoss.dk/2023/11/16/creating-hybrid-connection-with-terraform/
-  provisioner "local-exec" {
-    command = "az functionapp hybrid-connection add --hybrid-connection ${data.azurerm_key_vault_secret.hc_biztalk_name.value} --namespace ${data.azurerm_key_vault_secret.relay_name.value} -n ${module.func_biztalkreceiver.name} -g ${azurerm_resource_group.this.name} --subscription ${var.subscription_id}"
-  }
 }
