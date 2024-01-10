17a63b2650980c794606bdf4a2693609ca878885
Jonas Michelsen <jvm@energinet.dk>
2024-01-10 09:50:28 +0100

    grid_loss_metering_points

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202401091040_Create_grid_loss_responsible_table.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202401091040_Create_grid_loss_responsible_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS {INPUT_DATABASE_NAME}.grid_loss_responsible
+CREATE TABLE IF NOT EXISTS {INPUT_DATABASE_NAME}.grid_loss_metering_points
 (
     metering_point_id STRING NOT NULL
 )
 USING DELTA
 -- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used.
 -- In the production it is set to empty and the respective location is used. This means the production tables won't be deleted if the schema is.
-{TEST}LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/grid_loss_responsible'
+{TEST}LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/grid_loss_metering_points'
--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202401091041_add_grid_loss_responsible_table_constraints.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202401091041_add_grid_loss_responsible_table_constraints.sql
@@ -1,6 +1,6 @@
-ALTER TABLE {INPUT_DATABASE_NAME}.grid_loss_responsible
+ALTER TABLE {INPUT_DATABASE_NAME}.grid_loss_metering_points
     DROP CONSTRAINT IF EXISTS metering_point_id_chk
 GO
-ALTER TABLE {INPUT_DATABASE_NAME}.grid_loss_responsible
+ALTER TABLE {INPUT_DATABASE_NAME}.grid_loss_metering_points
     ADD CONSTRAINT metering_point_id_chk CHECK (LENGTH(metering_point_id) = 18)
 GO
