80b74a9ddc85d5674c1b5d9c94a9b309e578f1de
René T. Nielsen <renetdev@gmail.com>
2024-01-10 13:29:12 +0100

    esettdepr: temp remove hybrid connection (#923)

--- esett-deprecated/terraform/env/prod_001/func-biztalkshipper-hc_override.tf
+++
@@ -1,3 +0,0 @@
-resource "azurerm_function_app_hybrid_connection" "biztalkshipper_biztalk" {
-  hostname    = "datahub.biztalk.energinet.local"
-}
--- esett-deprecated/terraform/env/prod_001/hc-biztalk_override.tf
+++
@@ -1,3 +0,0 @@
-resource "azurerm_relay_hybrid_connection" "biztalk" {
-    user_metadata   = "[{\"key\":\"endpoint\",\"value\":\"datahub.biztalk.energinet.local:443\"}]"
-}
