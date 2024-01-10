38d7f1c3d923d13f5b360288b494917229a0b133
René T. Nielsen <renetdev@gmail.com>
2024-01-10 17:07:39 +0100

    Update func-biztalkshipper-hc.tf (#932)

--- esett-deprecated/terraform/main/func-biztalkshipper-hc.tf
+++ esett-deprecated/terraform/main/func-biztalkshipper-hc.tf
@@ -13,6 +13,6 @@ resource "azurerm_function_app_hybrid_connection" "biztalkshipper_biztalk" {
   # This is a workaround for a bug in the azurerm provider, for more info see:
   # - https://stigvoss.dk/2023/11/16/creating-hybrid-connection-with-terraform/
   provisioner "local-exec" {
-    command = "az functionapp hybrid-connection add --hybrid-connection ${data.azurerm_key_vault_secret.hc_biztalk_name.value} --namespace ${data.azurerm_key_vault_secret.relay_name.value} -n ${module.func_biztalkreceiver.name} -g ${data.azurerm_resource_group.shared.name} --subscription ${var.subscription_id}"
+    command = "az functionapp hybrid-connection add --hybrid-connection ${data.azurerm_key_vault_secret.hc_biztalk_name.value} --namespace ${data.azurerm_key_vault_secret.relay_name.value} -n ${module.func_biztalkreceiver.name} -g ${azurerm_resource_group.this.name} --subscription ${var.subscription_id}"
   }
 }
