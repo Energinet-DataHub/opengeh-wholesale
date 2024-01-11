78f12076c7deb2b58974b78b114d26b4a8ff5f87
René T. Nielsen <renetdev@gmail.com>
2024-01-11 10:20:26 +0100

    PIM access to xdjml (#936)

--- shared-resources/terraform/infrastructure/main/resource-group.tf
+++ shared-resources/terraform/infrastructure/main/resource-group.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "this" {
   name     = "rg-${local.resources_suffix}"
   location = "West Europe"
 }
+
+resource "azurerm_role_assignment" "xjdml" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Contributor"
+  principal_id         = "e1834474-72e9-4edd-b49a-d8b959e2b6c3"
+}
