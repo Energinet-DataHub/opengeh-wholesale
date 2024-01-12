33e9b50114b95663049f5dd0c8ea6f472fb3f44b
Mads Magnus Due <xmadu@energinet.dk>
2024-01-12 10:55:37 +0100

    feat: resize edi database to 20gb (#940)

--- edi/terraform/main/mssqldb-this.tf
+++ edi/terraform/main/mssqldb-this.tf
@@ -17,6 +17,7 @@ module "mssqldb_edi" {
   # Find available SKU's for "single database" (not pooled) here: https://learn.microsoft.com/en-us/azure/azure-sql/database/resource-limits-vcore-single-databases?view=azuresql
   sku_name                    = "GP_S_Gen5_4" # General Purpose (GP) - serverless compute (S) - standard series (Gen5) - max vCores (<number>) : https://learn.microsoft.com/en-us/azure/azure-sql/database/resource-limits-vcore-single-databases?view=azuresql#gen5-hardware-part-1-1
   min_capacity                = 0.5
+  max_size_gb                 = 20
   auto_pause_delay_in_minutes = -1
 
   monitor_alerts_action_group_id     = data.azurerm_key_vault_secret.primary_action_group_id.value
@@ -26,7 +27,7 @@ module "mssqldb_edi" {
   pim_writer_ad_group_name = var.pim_sql_writer_ad_group_name
 }
 
-module "kvs_sql_ms_market_participant_database_name" {
+module "kvs_sql_ms_edi_database_name" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=v13"
 
   name         = "mssql-edi-database-name"
