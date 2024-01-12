c1a1fa5759311e6b2704c4bd743a9c5cd82fdab7
BjarkeMeier <hej@bjarkemeier.dk>
2024-01-12 15:04:44 +0100

    Rename migration to force rerun on dev_002

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202401121500_Add_calculation_input_tables_to_database.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202401121500_Add_calculation_input_tables_to_database.sql
@@ -1,19 +1,19 @@
-CREATE TABLE if not exists {INPUT_DATABASE_NAME}.metering_point_periods
+CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.metering_point_periods
     USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/metering_point_periods'
 GO
 
-CREATE TABLE if not exists {INPUT_DATABASE_NAME}.time_series_points
+CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.time_series_points
     USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/time_series_points'
 GO
 
-CREATE TABLE if not exists {INPUT_DATABASE_NAME}.charge_link_periods
+CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.charge_link_periods
     USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_link_periods'
 GO
 
-CREATE TABLE if not exists {INPUT_DATABASE_NAME}.charge_masterdata_periods
+CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.charge_masterdata_periods
     USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_masterdata_periods'
 GO
 
-CREATE TABLE if not exists {INPUT_DATABASE_NAME}.charge_price_points
+CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.charge_price_points
     USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_price_points'
 GO
