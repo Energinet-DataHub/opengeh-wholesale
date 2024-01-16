0b7bf2553da0d71e3ea636c4bc872fa04887f80a
Jonas Michelsen <jvm@energinet.dk>
2024-01-16 10:51:44 +0100

    Update 202401161030_Add_metering_point_id_to_energy_table.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202401161030_Add_metering_point_id_to_energy_table.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202401161030_Add_metering_point_id_to_energy_table.sql
@@ -1,3 +1,9 @@
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD COLUMN metering_point_id STRING NULL;
 GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS metering_point_id_chk
+GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    ADD CONSTRAINT metering_point_id_chk CHECK (LENGTH(metering_point_id) = 18)
+GO
