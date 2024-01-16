cf8109666ed1646f3e6b36ea01ef8ff5f1174092
Mads Magnus Due <xmadu@energinet.dk>
2024-01-16 12:28:53 +0100

    feat: added cim peek and dequeue to apim (#951)
    * feat: added cim peek and dequeue to apim
    
    * updated module names
    
    * updated module names
    
    * block of template
    
    * refactored to a list of param
    
    * typo

---
+++ edi/terraform/main/apimao-cim-dequeuemessage.tf
@@ -0,0 +1,34 @@
+module "apimao_cim_dequeue_message" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/api-management-api-operation?ref=v13"
+
+  operation_id            = "cim-dequeue-message"
+  api_management_api_name = module.apima_b2b.name
+  resource_group_name     = data.azurerm_key_vault_secret.apim_instance_resource_group_name.value
+  api_management_name     = data.azurerm_key_vault_secret.apim_instance_name.value
+  display_name            = "EDI: CIM Dequeue Message"
+  method                  = "DELETE"
+  url_template            = "/v1.0/cim/dequeue/{message-id}"
+  template_parameters = [
+    {
+      name     = "message-id"
+      type     = "string"
+      required = true
+    }
+  ]
+  policies = [
+    {
+      xml_content = <<XML
+        <policies>
+          <inbound>
+            <base />
+            <validate-jwt header-name="Authorization" failed-validation-httpcode="403" failed-validation-error-message="Unauthorized to access this endpoint.">
+                <openid-config url="https://login.microsoftonline.com/${data.azurerm_key_vault_secret.apim_b2c_tenant_id.value}/v2.0/.well-known/openid-configuration" />
+            </validate-jwt>
+            <set-backend-service backend-id="${azurerm_api_management_backend.edi.name}" />
+            <rewrite-uri template="/api/dequeue/{message-id}" />
+          </inbound>
+        </policies>
+      XML
+    }
+  ]
+}
