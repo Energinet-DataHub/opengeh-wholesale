c52dea1a39108e6d2774b170ad407f24c27d9cfd
Jonas Michelsen <jvm@energinet.dk>
2024-01-16 11:08:31 +0100

    Update 202401161030_Add_metering_point_id_to_energy_table.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202401161030_Add_metering_point_id_to_energy_table.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202401161030_Add_metering_point_id_to_energy_table.sql
@@ -7,3 +7,15 @@ GO
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT metering_point_id_chk CHECK (LENGTH(metering_point_id) = 18)
 GO
+--If time_series_type is 'negative_grid_loss' or 'positive_grid_loss', then metering_point_id must not be null.
+--If time_series_type is neither 'negative_grid_loss' nor 'positive_grid_loss', then metering_point_id must be null.
+If time_series_type is neither 'negative_grid_loss' nor 'positive_grid_loss', then metering_point_id must be null.
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    ADD CONSTRAINT metering_point_id_conditional_chk
+    CHECK (
+        (time_series_type IN ('negative_grid_loss', 'positive_grid_loss') AND metering_point_id IS NOT NULL)
+        OR
+        (time_series_type NOT IN ('negative_grid_loss', 'positive_grid_loss') AND metering_point_id IS NULL)
+    );
+GO
+
