7285b31c204d927edd85e87b1001a974c7f8cf1a
Karsten M <xnetsrak@gmail.com>
2024-01-24 11:06:48 +0100

    env update (#995)

--- market-participant/terraform/main/locals.tf
+++ market-participant/terraform/main/locals.tf
@@ -3,5 +3,5 @@ locals {
   CONNECTION_STRING_DB_MIGRATIONS         = "Server=tcp:${data.azurerm_key_vault_secret.mssql_data_url.value},1433;Initial Catalog=${module.mssqldb_market_participant.name};Persist Security Info=False;Authentication=Active Directory Default;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=120;"
   NAME_SUFFIX                             = "${lower(var.domain_name_short)}-${lower(var.environment_short)}-we-${lower(var.environment_instance)}"
   ip_restrictions_as_string               = join(",", [for rule in var.ip_restrictions : "${rule.ip_address}"])
-  ENV_DESC                                = "${var.environment}_${var.environment_instance}"
+  ENV_DESC                                = "${var.environment_short}_${var.environment_instance}"
 }
