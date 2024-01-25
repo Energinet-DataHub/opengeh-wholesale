305bfc699a866bb52e68549cddf8583129fb5135
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-01-25 13:16:45 +0100

    Fix order of columns

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202401241300_Create_master_data_table.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202401241300_Create_master_data_table.sql
@@ -3,14 +3,14 @@ CREATE TABLE IF NOT EXISTS {OUTPUT_DATABASE_NAME}.master_basis_data
     calculation_id STRING NOT NULL,
     calculation_type STRING NOT NULL,
     grid_area STRING NOT NULL,
-    energy_supplier_id STRING,
     metering_point_id STRING NOT NULL,
     period_start TIMESTAMP NOT NULL,
     period_end TIMESTAMP,
     in_grid_area STRING,
     out_grid_area STRING,
     metering_point_type STRING NOT NULL,
-    settlement_method STRING
+    settlement_method STRING,
+    energy_supplier_id STRING
 )
 USING DELTA
 -- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used.
