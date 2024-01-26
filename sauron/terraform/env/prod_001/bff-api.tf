269a4b76b4bcdafc9dee09f36f38b02e4e6e129a
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2024-01-26 12:47:38 +0100

    sauron apim fix CORS (#1008)
    * sauron APIM fix CORS
    
    * sauron APIM fix CORS
    
    * sauron APIM fix CORS

--- sauron/terraform/env/prod_001/bff-api.tf
+++ sauron/terraform/env/prod_001/bff-api.tf
@@ -12,6 +12,44 @@ module "apima_bff_api" {
   logger_verbosity           = "verbose"
   backend_service_url        = "https://${module.func_bff.default_hostname}"
   path                       = "sauron"
+  policies = [
+    {
+      xml_content = <<XML
+        <policies>
+          <inbound>
+            <base />
+            <set-header name="RequestTime" exists-action="override">
+                <value>@(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"))</value>
+            </set-header>
+            <cors allow-credentials="true">
+                <allowed-origins>
+                    <origin>https://${local.frontend_url}</origin>
+                    <origin>https://sauron.test002.datahub3.dk</origin>
+                </allowed-origins>
+                <allowed-methods preflight-result-max-age="300">
+                    <method>*</method>
+                </allowed-methods>
+                <allowed-headers>
+                    <header>*</header>
+                </allowed-headers>
+                <expose-headers>
+                    <header>*</header>
+                </expose-headers>
+            </cors>
+          </inbound>
+          <backend>
+              <base />
+          </backend>
+          <outbound>
+              <base />
+          </outbound>
+          <on-error>
+              <base />
+          </on-error>
+        </policies>
+      XML
+    }
+  ]
 }
 
 module "apimao_get_deployments" {
