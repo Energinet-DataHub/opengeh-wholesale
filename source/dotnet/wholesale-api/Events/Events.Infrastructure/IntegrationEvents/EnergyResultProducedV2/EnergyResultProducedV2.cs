bbe90b12f9e5ef0af6e2f9dfcb69d6c9222fd3c3
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-01-26 14:17:27 +0100

    bump minor

--- source/dotnet/wholesale-api/Events/Events.Infrastructure/IntegrationEvents/EnergyResultProducedV2/EnergyResultProducedV2.cs
+++ source/dotnet/wholesale-api/Events/Events.Infrastructure/IntegrationEvents/EnergyResultProducedV2/EnergyResultProducedV2.cs
@@ -24,7 +24,7 @@ public partial class EnergyResultProducedV2 : IEventMessage
     /// </summary>
     public const string EventName = "EnergyResultProducedV2";
 
-    public const int EventMinorVersion = 1;
+    public const int EventMinorVersion = 2;
 
     string IEventMessage.EventName => EventName;
 
