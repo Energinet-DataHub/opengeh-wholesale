2447e3d448986546debd46c17937f7201ab1cecd
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-01-29 10:24:07 +0100

    Use pre07 for eSett Exchange. (#1012)

--- esett-exchange/terraform/env/dev_002/locals_override.tf
+++ esett-exchange/terraform/env/dev_002/locals_override.tf
@@ -1,5 +1,5 @@
 locals {
-  DH2_ENDPOINT                    = "https://b2b.te6.datahub.dk"
+  DH2_ENDPOINT                    = "https://b2b.te7.datahub.dk"
   BIZ_TALK_SENDER_CODE            = "45V0000000000601"
   BIZ_TALK_RECEIVER_CODE          = "44V000000000028C"
 }
