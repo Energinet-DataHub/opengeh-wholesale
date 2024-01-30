7114cd405db64151eb73c772b55a19613b96a837
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2024-01-30 15:26:28 +0100

    Add service principal for running for azcopy workflow (#1020)

---
+++ shared-resources/terraform/infrastructure/env/prod_001/spn-azcopy.tf
@@ -0,0 +1,116 @@
+#
+# Application registration with service principal
+#
+resource "azuread_application" "app_azcopy" {
+  display_name = "sp-datahub-azcopy"
+  owners = [
+    data.azuread_client_config.current_client.object_id
+  ]
+}
+
+resource "azuread_service_principal" "spn_azcopy" {
+  application_id               = azuread_application.app_azcopy.application_id
+  app_role_assignment_required = false
+  owners = [
+    data.azuread_client_config.current_client.object_id
+  ]
+}
+
+#
+# Key Vault secrets
+#
+
+resource "azurerm_key_vault_secret" "kvs_azcopy_spn_id" {
+  name         = "AZURE-AZCOPY-SPN-ID"
+  value        = azuread_application.app_azcopy.application_id
+  key_vault_id = module.kv_shared.id
+
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "kvs_azcopy_spn_object_id" {
+  name         = "AZURE-AZCOPY-SPN-OBJECT-ID"
+  value        = azuread_service_principal.spn_azcopy.object_id
+  key_vault_id = module.kv_shared.id
+
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
+
+#
+# Federated identity credentials for the azcopy service principal
+#
+
+locals {
+  federated_identity_credential = {
+    audience_azuread = "api://AzureADTokenExchange"
+    issuer_github    = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "azuread_application_federated_identity_credential" "dev_001" {
+  application_object_id = azuread_application.app_azcopy.object_id
+
+  display_name = "dev_001"
+  subject      = "repo:Energinet-DataHub/dh3-environments:environment:dev_001"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "dev_002" {
+  application_object_id = azuread_application.app_azcopy.object_id
+
+  display_name = "dev_002"
+  subject      = "repo:Energinet-DataHub/dh3-environments:environment:dev_002"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "test_001" {
+  application_object_id = azuread_application.app_azcopy.object_id
+
+  display_name = "test_001"
+  subject      = "repo:Energinet-DataHub/dh3-environments:environment:test_001"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "test_002" {
+  application_object_id = azuread_application.app_azcopy.object_id
+
+  display_name = "test_002"
+  subject      = "repo:Energinet-DataHub/dh3-environments:environment:test_002"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "preprod_001" {
+  application_object_id = azuread_application.app_azcopy.object_id
+
+  display_name = "preprod_001"
+  subject      = "repo:Energinet-DataHub/dh3-environments:environment:preprod_001"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
+
+resource "azuread_application_federated_identity_credential" "prod_001" {
+  application_object_id = azuread_application.app_azcopy.object_id
+
+  display_name = "prod_001"
+  subject      = "repo:Energinet-DataHub/dh3-environments:environment:prod_001"
+
+  audiences = [local.federated_identity_credential.audience_azuread]
+  issuer    = local.federated_identity_credential.issuer_github
+}
