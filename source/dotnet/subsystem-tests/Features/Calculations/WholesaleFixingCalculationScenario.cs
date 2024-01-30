78bd00f9d9d94e265f487fe59c8e786145107518
Alexander <122119488+energinet-ajw@users.noreply.github.com>
2024-01-30 16:12:15 +0100

    Update source/dotnet/subsystem-tests/Features/Calculations/WholesaleFixingCalculationScenario.cs
    Co-authored-by: JesperGomes <112619721+JesperGomes@users.noreply.github.com>

--- source/dotnet/subsystem-tests/Features/Calculations/WholesaleFixingCalculationScenario.cs
+++ source/dotnet/subsystem-tests/Features/Calculations/WholesaleFixingCalculationScenario.cs
@@ -339,7 +339,7 @@ AppDependencies
 
         [ScenarioStep(16)]
         [SubsystemFact]
-        public async Task AndThen_ReceivedReceivedGridLossProducedV1EventContainsExpectedTimeSeriesPoints()
+        public async Task AndThen_ReceivedGridLossProducedV1EventContainsExpectedTimeSeriesPoints()
         {
             // Arrange
             var expectedTimeSeriesPoints = await Fixture.ParseGridLossTimeSeriesPointsFromCsvAsync("Positive_gridLoss 804.csv");
