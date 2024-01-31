62107bed3f83e860a6478163298ada71e3d0202d
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-01-31 13:11:14 +0100

    Add status to eSett. (#1023)

--- esett-exchange/terraform/main/app-webapi.tf
+++ esett-exchange/terraform/main/app-webapi.tf
@@ -24,6 +24,10 @@ module "app_webapi" {
     "DatabaseSettings:ConnectionString"   = local.MS_ESETT_EXCHANGE_CONNECTION_STRING
     "BlobStorageSettings:AccountUri"      = local.ESETT_DOCUMENT_STORAGE_ACCOUNT_URI
     "BlobStorageSettings:ContainerName"   = local.ESETT_DOCUMENT_STORAGE_CONTAINER_NAME
+    "StatusSettings:Dh2Uri"               = module.func_entrypoint_peek.default_hostname
+    "StatusSettings:ExchangeUri"          = module.func_entrypoint_exchange_event_receiver.default_hostname
+    "StatusSettings:IncomingUri"          = module.func_entrypoint_ecp_inbox.default_hostname
+    "StatusSettings:OutgoingUri"          = module.func_entrypoint_ecp_outbox.default_hostname
   }
   role_assignments = [
     {
