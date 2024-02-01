11e1f5b5870b96c7dd2bdae4daf52650ce088b53
René T. Nielsen <renetdev@gmail.com>
2024-02-01 14:47:31 +0100

    Sauron: use application insights connection string instead of instrumentation key (#1022)

--- sauron/terraform/main/func-bff.tf
+++ sauron/terraform/main/func-bff.tf
@@ -1,19 +1,19 @@
 module "func_bff" {
-  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=v13"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=v13"
 
-  name                                      = "bff"
-  project_name                              = var.domain_name_short
-  environment_short                         = var.environment_short
-  environment_instance                      = var.environment_instance
-  resource_group_name                       = azurerm_resource_group.this.name
-  location                                  = azurerm_resource_group.this.location
-  vnet_integration_subnet_id                = data.azurerm_key_vault_secret.snet_vnet_integration_id.value
-  private_endpoint_subnet_id                = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
-  app_service_plan_id                       = data.azurerm_key_vault_secret.plan_shared_id.value
-  application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
-  ip_restriction_allow_ip_range             = "*"
-  dotnet_framework_version                  = "v7.0"
+  name                                   = "bff"
+  project_name                           = var.domain_name_short
+  environment_short                      = var.environment_short
+  environment_instance                   = var.environment_instance
+  resource_group_name                    = azurerm_resource_group.this.name
+  location                               = azurerm_resource_group.this.location
+  vnet_integration_subnet_id             = data.azurerm_key_vault_secret.snet_vnet_integration_id.value
+  private_endpoint_subnet_id             = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
+  app_service_plan_id                    = data.azurerm_key_vault_secret.plan_shared_id.value
+  application_insights_connection_string = data.azurerm_key_vault_secret.appi_shared_connection_string.value
+  ip_restriction_allow_ip_range          = "*"
+  dotnet_framework_version               = "v7.0"
   app_settings = {
-    CONNECTION_STRING_DATABASE  = "Server=tcp:${data.azurerm_key_vault_secret.mssql_data_url.value},1433;Initial Catalog=${module.mssqldb.name};Persist Security Info=False;Authentication=Active Directory Managed Identity;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=120;"
+    CONNECTION_STRING_DATABASE = "Server=tcp:${data.azurerm_key_vault_secret.mssql_data_url.value},1433;Initial Catalog=${module.mssqldb.name};Persist Security Info=False;Authentication=Active Directory Managed Identity;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=120;"
   }
 }
--- sauron/terraform/main/kv-shared-resources.tf
+++ sauron/terraform/main/kv-shared-resources.tf
@@ -3,8 +3,8 @@ data "azurerm_key_vault" "kv_shared_resources" {
   resource_group_name = var.shared_resources_resource_group_name
 }
 
-data "azurerm_key_vault_secret" "appi_shared_instrumentation_key" {
-  name         = "appi-shared-instrumentation-key"
+data "azurerm_key_vault_secret" "appi_shared_connection_string" {
+  name         = "appi-shared-connection-string"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
 
