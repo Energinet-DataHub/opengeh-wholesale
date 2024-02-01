be178595dbc64676e5cdd5989cc644ec0aac6b7d
René T. Nielsen <renetdev@gmail.com>
2024-02-01 13:00:42 +0100

    add bff apim endpoints (#1026)
    * add bff apim endpoints
    
    * remove

---
+++ sauron/terraform/env/preprod_001/bff-api_override.tf
@@ -0,0 +1,7 @@
+module "apima_bff_api" {
+  count = 0
+}
+
+module "apimao_get_deployments" {
+  count = 0
+}
--- sauron/terraform/main/bff-api.tf
+++ sauron/terraform/main/bff-api.tf
@@ -1,6 +1,8 @@
 module "apima_bff_api" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/api-management-api?ref=v13"
 
+  count                      = 1
+
   name                       = "sauron-bff"
   project_name               = var.domain_name_short
   api_management_name        = data.azurerm_key_vault_secret.apim_instance_name.value
@@ -12,51 +14,14 @@ module "apima_bff_api" {
   logger_verbosity           = "verbose"
   backend_service_url        = "https://${module.func_bff.default_hostname}"
   path                       = "sauron"
-  policies = [
-    {
-      xml_content = <<XML
-        <policies>
-          <inbound>
-            <base />
-            <set-header name="RequestTime" exists-action="override">
-                <value>@(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"))</value>
-            </set-header>
-            <cors allow-credentials="true">
-                <allowed-origins>
-                    <origin>https://${local.frontend_url}</origin>
-                    <origin>https://sauron.test002.datahub3.dk</origin>
-                </allowed-origins>
-                <allowed-methods preflight-result-max-age="300">
-                    <method>*</method>
-                </allowed-methods>
-                <allowed-headers>
-                    <header>*</header>
-                </allowed-headers>
-                <expose-headers>
-                    <header>*</header>
-                </expose-headers>
-            </cors>
-          </inbound>
-          <backend>
-              <base />
-          </backend>
-          <outbound>
-              <base />
-          </outbound>
-          <on-error>
-              <base />
-          </on-error>
-        </policies>
-      XML
-    }
-  ]
 }
 
 module "apimao_get_deployments" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/api-management-api-operation?ref=v13"
 
+  count                   = 1
   operation_id            = "get-deployments"
-  api_management_api_name = module.apima_bff_api.name
+  api_management_api_name = module.apima_bff_api[0].name
   resource_group_name     = data.azurerm_key_vault_secret.apim_instance_resource_group_name.value
   api_management_name     = data.azurerm_key_vault_secret.apim_instance_name.value
   display_name            = "Sauron: GET deployments"
