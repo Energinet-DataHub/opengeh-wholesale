2b9f47b7197ebb0ada16455c1974e944c9f6cd4a
Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>
2024-02-02 13:14:42 +0100

    Refactor to use connection string setting from module (#1027)

--- wholesale/terraform/main/app-wholesale-api.tf
+++ wholesale/terraform/main/app-wholesale-api.tf
@@ -1,22 +1,22 @@
 module "app_wholesale_api" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=v13"
 
-  name                                     = "webapi"
-  project_name                             = var.domain_name_short
-  environment_short                        = var.environment_short
-  environment_instance                     = var.environment_instance
-  resource_group_name                      = azurerm_resource_group.this.name
-  location                                 = azurerm_resource_group.this.location
-  app_service_plan_id                      = data.azurerm_key_vault_secret.plan_shared_id.value
-  application_insights_instrumentation_key = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
-  vnet_integration_subnet_id               = data.azurerm_key_vault_secret.snet_vnet_integration_id.value
-  private_endpoint_subnet_id               = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
-  dotnet_framework_version                 = "v8.0"
-  health_check_path                        = "/monitor/ready"
-  health_check_alert_action_group_id       = data.azurerm_key_vault_secret.primary_action_group_id.value
-  health_check_alert_enabled               = var.enable_health_check_alerts
-  ip_restrictions                          = var.ip_restrictions
-  scm_ip_restrictions                      = var.ip_restrictions
+  name                                   = "webapi"
+  project_name                           = var.domain_name_short
+  environment_short                      = var.environment_short
+  environment_instance                   = var.environment_instance
+  resource_group_name                    = azurerm_resource_group.this.name
+  location                               = azurerm_resource_group.this.location
+  app_service_plan_id                    = data.azurerm_key_vault_secret.plan_shared_id.value
+  application_insights_connection_string = data.azurerm_key_vault_secret.appi_shared_connection_string.value
+  vnet_integration_subnet_id             = data.azurerm_key_vault_secret.snet_vnet_integration_id.value
+  private_endpoint_subnet_id             = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
+  dotnet_framework_version               = "v8.0"
+  health_check_path                      = "/monitor/ready"
+  health_check_alert_action_group_id     = data.azurerm_key_vault_secret.primary_action_group_id.value
+  health_check_alert_enabled             = var.enable_health_check_alerts
+  ip_restrictions                        = var.ip_restrictions
+  scm_ip_restrictions                    = var.ip_restrictions
   role_assignments = [
     {
       resource_id          = data.azurerm_key_vault_secret.st_shared_data_lake_id.value
@@ -32,9 +32,6 @@ module "app_wholesale_api" {
     EXTERNAL_OPEN_ID_URL = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=frontend-open-id-url)"
     INTERNAL_OPEN_ID_URL = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-open-id-url)"
     BACKEND_BFF_APP_ID   = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-bff-app-id)"
-
-    # Application Insights
-    APPLICATIONINSIGHTS_CONNECTION_STRING = data.azurerm_key_vault_secret.appi_shared_connection_string.value
 
     # Storage
     STORAGE_CONTAINER_NAME = local.STORAGE_CONTAINER_NAME
