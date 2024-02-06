41c5cd825cb10b31937c75b4ef2454dc22fa54bd
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2024-02-06 08:49:00 +0100

    TF provider files: remove = sign from azurerm version (#1040)

--- migrations/terraform/main/providers.tf
+++ migrations/terraform/main/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     # It is recommended to pin to a given version of the Azure provider
-    azurerm = "=3.85.0"
+    azurerm = "3.85.0"
 
     databricks = {
       source  = "databricks/databricks"
--- wholesale/terraform/main/providers.tf
+++ wholesale/terraform/main/providers.tf
@@ -7,7 +7,7 @@ terraform {
     # It is recommended to pin to a given version of the Azure provider
     # We cannot bump this to 3.85.0 currently, as Terraform has added an enhancement in 3.84.0 that states a preview feature must be enabled on SQL databases and elastic pools
     # See https://learn.microsoft.com/en-us/azure/azure-sql/database/always-encrypted-enclaves-enable?view=azuresql&tabs=VBSenclaves
-    azurerm = "=3.83.0"
+    azurerm = "3.83.0"
   }
 }
 
