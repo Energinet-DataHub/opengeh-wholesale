1edeebfca3d83a9b104447b652e65ee5eaae4a9e
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2024-02-08 14:41:50 +0100

    APIM dev001 - stv2: Re-add everything (#1072)
    * Revert "Dev001 apim remove all resources and roles (#1070)"
    
    This reverts commit 445244e2139f936d625b722e638125adec9c5e3d.
    
    * APIM instance: update TF module version

---
+++ shared-resources/terraform/infrastructure/env/dev_001/apim-shared-custom-domains.tf
@@ -0,0 +1,34 @@
+resource "azurerm_key_vault_certificate" "b2b_datahub3_certificate" {
+  name         = "cert-b2b-datahub3"
+  key_vault_id = module.kv_shared.id
+
+  certificate {
+    contents = filebase64("${path.module}/assets/dev_b2b_datahub3_dk.pfx")
+    password = var.cert_b2b_datahub3_password
+  }
+}
+
+resource "azurerm_key_vault_certificate" "ebix_datahub3_certificate" {
+  name         = "cert-ebix-datahub3"
+  key_vault_id = module.kv_shared.id
+
+  certificate {
+    contents = filebase64("${path.module}/assets/dev_ebix_datahub3_dk.pfx")
+    password = var.cert_ebix_datahub3_password
+  }
+}
+
+resource "azurerm_api_management_custom_domain" "datahub3_custom_domains" {
+  api_management_id = module.apim_shared.id
+
+  gateway {
+    host_name    = "dev.b2b.datahub3.dk"
+    key_vault_id = azurerm_key_vault_certificate.b2b_datahub3_certificate.versionless_secret_id
+  }
+
+  gateway {
+    host_name                    = "dev.ebix.datahub3.dk"
+    key_vault_id                 = azurerm_key_vault_certificate.ebix_datahub3_certificate.versionless_secret_id
+    negotiate_client_certificate = true
+  }
+}
