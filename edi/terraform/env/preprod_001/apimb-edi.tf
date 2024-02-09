53376373e94f28b4757792a97fb05a439ed723f4
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2024-02-09 08:48:37 +0100

    APIM preprod001: Remove resource (before upgrading to stv2) (#1065)

--- edi/terraform/env/preprod_001/apimb-edi.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_api_management_backend" "edi" {
-  name                = "edi"
-  resource_group_name = data.azurerm_key_vault_secret.apim_instance_resource_group_name.value
-  api_management_name = data.azurerm_key_vault_secret.apim_instance_name.value
-  protocol            = "http"
-  url                 = "https://${module.func_receiver.default_hostname}"
-}
--- sauron/terraform/env/preprod_001/bff-api_override.tf
+++
@@ -1,7 +0,0 @@
-# module "apima_bff_api" {
-#   count = 0
-# }
-
-# module "apimao_get_deployments" {
-#   count = 0
-# }
