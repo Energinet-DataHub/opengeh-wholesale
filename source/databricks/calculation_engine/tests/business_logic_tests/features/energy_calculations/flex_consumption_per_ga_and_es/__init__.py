0528fd5acd38a35dd8c83bc39194f5387e194ac7
Alexander <alexanderharrow@hotmail.com>
2024-02-22 14:30:50 +0100

    Updates

--- source/databricks/calculation_engine/tests/business_logic_tests/features/energy_calculations/flex_consumption_per_ga_and_es/__init__.py
+++ source/databricks/calculation_engine/tests/business_logic_tests/features/energy_calculations/flex_consumption_per_ga_and_es/__init__.py
@@ -11,27 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from business_logic_tests.features.energy_calculations import (
-    get_expected_results,
-)
-from business_logic_tests.scenario_factory import ScenarioFixture
-from helpers.data_frame_utils import (
-    assert_dataframes,
-)
-
-
-def test_execute__returns_expected(
-    scenario_fixture: ScenarioFixture,
-) -> None:
-    # Arrange
-    scenario_fixture.setup(get_expected_results)
-
-    # Act
-    results = scenario_fixture.execute()
-
-    # Assert
-    assert_dataframes(
-        results.energy_results.flex_consumption_per_ga_and_es.df,
-        scenario_fixture.expected,
-    )
