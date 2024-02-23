a4a405ba381d833417a7ac203a8d59f6c1c36a55
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-02-23 13:43:10 +0100

    copyright

--- source/databricks/calculation_engine/package/datamigration/schema_config.py
+++ source/databricks/calculation_engine/package/datamigration/schema_config.py
@@ -1,7 +1,18 @@
-from spark_sql_migrations import (
-    Schema,
-    Table
-)
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from spark_sql_migrations import Schema, Table
 from package.infrastructure.paths import (
     WHOLESALE_RESULT_TABLE_NAME,
     OUTPUT_DATABASE_NAME,
@@ -9,8 +20,12 @@ from package.infrastructure.paths import (
 )
 
 # calculation_output
-from package.calculation_output.schemas.wholesale_results_schema import wholesale_results_schema
-from package.calculation_output.schemas.energy_results_schema import energy_results_schema
+from package.calculation_output.schemas.wholesale_results_schema import (
+    wholesale_results_schema,
+)
+from package.calculation_output.schemas.energy_results_schema import (
+    energy_results_schema,
+)
 
 schema_config = [
     Schema(
@@ -23,7 +38,7 @@ schema_config = [
             Table(
                 name=ENERGY_RESULT_TABLE_NAME,
                 schema=energy_results_schema,
-            )
-        ]
+            ),
+        ],
     )
 ]
