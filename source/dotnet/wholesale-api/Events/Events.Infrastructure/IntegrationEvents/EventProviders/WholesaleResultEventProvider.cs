3ca96d7aa17e06f85d4968b5cd5c57a38b456313
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-02-28 17:35:00 +0100

    ...

--- source/dotnet/wholesale-api/Events/Events.Infrastructure/IntegrationEvents/EventProviders/WholesaleResultEventProvider.cs
+++ source/dotnet/wholesale-api/Events/Events.Infrastructure/IntegrationEvents/EventProviders/WholesaleResultEventProvider.cs
@@ -23,12 +23,22 @@ using Energinet.DataHub.Wholesale.Events.Infrastructure.IntegrationEvents.Monthl
 
 namespace Energinet.DataHub.Wholesale.Events.Infrastructure.IntegrationEvents.EventProviders
 {
-    public class WholesaleResultEventProvider(
-        IWholesaleResultQueries wholesaleResultQueries,
-        IAmountPerChargeResultProducedV1Factory amountPerChargeResultProducedV1Factory,
-        IMonthlyAmountPerChargeResultProducedV1Factory monthlyAmountPerChargeResultProducedV1Factory)
-        : ResultEventProvider, IWholesaleResultEventProvider
+    public class WholesaleResultEventProvider : ResultEventProvider, IWholesaleResultEventProvider
     {
+        private readonly IWholesaleResultQueries _wholesaleResultQueries;
+        private readonly IAmountPerChargeResultProducedV1Factory _amountPerChargeResultProducedV1Factory;
+        private readonly IMonthlyAmountPerChargeResultProducedV1Factory _monthlyAmountPerChargeResultProducedV1Factory;
+
+        public WholesaleResultEventProvider(
+            IWholesaleResultQueries wholesaleResultQueries,
+            IAmountPerChargeResultProducedV1Factory amountPerChargeResultProducedV1Factory,
+            IMonthlyAmountPerChargeResultProducedV1Factory monthlyAmountPerChargeResultProducedV1Factory)
+        {
+            _wholesaleResultQueries = wholesaleResultQueries;
+            _amountPerChargeResultProducedV1Factory = amountPerChargeResultProducedV1Factory;
+            _monthlyAmountPerChargeResultProducedV1Factory = monthlyAmountPerChargeResultProducedV1Factory;
+        }
+
         public bool CanContainWholesaleResults(CompletedCalculation calculation)
         {
             return calculation.CalculationType
@@ -40,7 +50,7 @@ namespace Energinet.DataHub.Wholesale.Events.Infrastructure.IntegrationEvents.Ev
 
         public async IAsyncEnumerable<IntegrationEvent> GetAsync(CompletedCalculation calculation)
         {
-            await foreach (var wholesaleResult in wholesaleResultQueries.GetAsync(calculation.Id).ConfigureAwait(false))
+            await foreach (var wholesaleResult in _wholesaleResultQueries.GetAsync(calculation.Id).ConfigureAwait(false))
             {
                 yield return CreateEventFromWholesaleResult(wholesaleResult);
             }
@@ -48,11 +58,11 @@ namespace Energinet.DataHub.Wholesale.Events.Infrastructure.IntegrationEvents.Ev
 
         private IntegrationEvent CreateEventFromWholesaleResult(WholesaleResult wholesaleResult)
         {
-            if (amountPerChargeResultProducedV1Factory.CanCreate(wholesaleResult))
-                return CreateIntegrationEvent(amountPerChargeResultProducedV1Factory.Create(wholesaleResult));
+            if (_amountPerChargeResultProducedV1Factory.CanCreate(wholesaleResult))
+                return CreateIntegrationEvent(_amountPerChargeResultProducedV1Factory.Create(wholesaleResult));
 
-            if (monthlyAmountPerChargeResultProducedV1Factory.CanCreate(wholesaleResult))
-                return CreateIntegrationEvent(monthlyAmountPerChargeResultProducedV1Factory.Create(wholesaleResult));
+            if (_monthlyAmountPerChargeResultProducedV1Factory.CanCreate(wholesaleResult))
+                return CreateIntegrationEvent(_monthlyAmountPerChargeResultProducedV1Factory.Create(wholesaleResult));
 
             throw new ArgumentException("Cannot create event from wholesale result.", nameof(wholesaleResult));
         }
