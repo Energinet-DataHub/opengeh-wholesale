8cd8bf1a7a01f601caa564d0751cb1b900c7697f
Rasmus Leth Friis <116742936+rf-halfspace@users.noreply.github.com>
2024-03-01 14:24:15 +0100

    Delete shared-resources/terraform/b2c/env/preprod_001/b2c-access_override.tf (#1148)

--- shared-resources/terraform/b2c/env/preprod_001/b2c-access_override.tf
+++
@@ -1,5 +0,0 @@
-#XRLFR
-resource "azuread_directory_role_assignment" "xrlfr" {
-  role_id             = azuread_directory_role.global_admin.template_id
-  principal_object_id = azuread_invitation.xrlfr[0].user_id
-}
