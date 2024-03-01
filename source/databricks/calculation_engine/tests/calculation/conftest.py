685c03f1906d01130125590453f0256a055b9dc0
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-03-01 11:56:48 +0100

    ...

--- source/databricks/calculation_engine/tests/calculation/conftest.py
+++ source/databricks/calculation_engine/tests/calculation/conftest.py
@@ -46,49 +46,6 @@ from package.constants import Colname
 
 
 @pytest.fixture(scope="session")
-def calculate_daily_subscription_price_factory(
-    spark: SparkSession,
-) -> Callable[..., DataFrame]:
-    def factory(
-        time: datetime,
-        price_per_day: Decimal,
-        charge_count: int,
-        total_daily_charge_price: Decimal = DataframeDefaults.default_charge_price,
-        charge_code: str = DataframeDefaults.default_charge_code,
-        charge_type: str = DataframeDefaults.default_charge_type,
-        charge_owner: str = DataframeDefaults.default_charge_owner,
-        charge_price: Decimal = DataframeDefaults.default_charge_price,
-        metering_point_type: str = DataframeDefaults.default_metering_point_type,
-        settlement_method: str = DataframeDefaults.default_settlement_method,
-        grid_area: str = DataframeDefaults.default_grid_area,
-        energy_supplier_id: str = DataframeDefaults.default_energy_supplier_id,
-    ) -> DataFrame:
-        charge_key: str = f"{charge_code}-{charge_owner}-{charge_type}"
-
-        data = [
-            {
-                Colname.charge_key: charge_key,
-                Colname.charge_code: charge_code,
-                Colname.charge_type: charge_type,
-                Colname.charge_owner: charge_owner,
-                Colname.charge_price: charge_price,
-                Colname.charge_time: time,
-                Colname.price_per_day: price_per_day,
-                Colname.charge_count: charge_count,
-                Colname.total_daily_charge_price: total_daily_charge_price,
-                Colname.metering_point_type: metering_point_type,
-                Colname.settlement_method: settlement_method,
-                Colname.grid_area: grid_area,
-                Colname.energy_supplier_id: energy_supplier_id,
-            }
-        ]
-
-        return spark.createDataFrame(data, schema=subscriptions_schema)
-
-    return factory
-
-
-@pytest.fixture(scope="session")
 def calculate_fee_charge_price_factory(spark: SparkSession) -> Callable[..., DataFrame]:
     def factory(
         time: datetime,
