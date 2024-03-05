a57fddd8e8a550632375ac42f973b6246beac5f2
Mads Magnus Due <xmadu@energinet.dk>
2024-03-05 14:03:05 +0100

    feat: new apim endpoint for wholesale settlement requests (#1153)
    * feat: new apim endpoint for wholesale settlement requests
    
    * update end point for incoming messages
    
    * updated path
    
    * updated path

--- edi/terraform/main/apimao-request-wholesale-settlement.tf
+++ edi/terraform/main/apimao-request-wholesale-settlement.tf
@@ -1,13 +1,13 @@
-module "apimao_request_aggregated_measure_data" {
+module "apimao_request_wholesale_settlement" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/api-management-api-operation?ref=v13"
 
-  operation_id            = "request-aggregated-measure-data"
+  operation_id            = "request-request-wholesale-settlement"
   api_management_api_name = module.apima_b2b.name
   resource_group_name     = data.azurerm_key_vault_secret.apim_instance_resource_group_name.value
   api_management_name     = data.azurerm_key_vault_secret.apim_instance_name.value
-  display_name            = "EDI: Request aggregated measure data"
+  display_name            = "EDI: Request wholesale settlement"
   method                  = "POST"
-  url_template            = "/v1.0/cim/requestaggregatedmeasuredata"
+  url_template            = "/v1.0/cim/requestwholesalesettlement"
   policies = [
     {
       xml_content = <<XML
@@ -18,14 +18,12 @@ module "apimao_request_aggregated_measure_data" {
                 <openid-config url="https://login.microsoftonline.com/${data.azurerm_key_vault_secret.apim_b2c_tenant_id.value}/v2.0/.well-known/openid-configuration" />
                 <required-claims>
                     <claim name="roles" match="any">
-                        <value>metereddataresponsible</value>
                         <value>energysupplier</value>
-                        <value>balanceresponsibleparty</value>
                     </claim>
                 </required-claims>
             </validate-jwt>
             <set-backend-service backend-id="${azurerm_api_management_backend.edi.name}" />
-            <rewrite-uri template="/api/incomingMessages/RequestAggregatedMeasureData" />
+            <rewrite-uri template="/api/incomingMessages/RequestWholesaleSettlement" />
           </inbound>
         </policies>
       XML
