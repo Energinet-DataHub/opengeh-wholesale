0132b418547d766112a0a63591223afa6ad8a810
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-03-07 15:04:09 +0100

    Update wholesale_results_schema.py

--- source/databricks/calculation_engine/package/calculation_output/schemas/wholesale_results_schema.py
+++ source/databricks/calculation_engine/package/calculation_output/schemas/wholesale_results_schema.py
@@ -45,7 +45,7 @@ wholesale_results_schema = StructType(
         StructField(
             WholesaleResultColumnNames.quantity_qualities,
             ArrayType(StringType()),
-            True,
+            False,
         ),
         StructField(WholesaleResultColumnNames.time, TimestampType(), False),
         StructField(WholesaleResultColumnNames.resolution, StringType(), False),
