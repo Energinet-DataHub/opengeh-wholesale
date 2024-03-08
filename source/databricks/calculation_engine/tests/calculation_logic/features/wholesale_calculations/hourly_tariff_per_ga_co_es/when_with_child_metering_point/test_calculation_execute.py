86776855525bb469a06165313a3d1b0fb97dfa62
Alexander <alexanderharrow@hotmail.com>
2024-03-08 12:35:02 +0100

    Updates

--- source/databricks/calculation_engine/tests/calculation_logic/features/wholesale_calculations/hourly_tariff_per_ga_co_es/when_with_child_metering_point/test_calculation_execute.py
+++ source/databricks/calculation_engine/tests/calculation_logic/features/wholesale_calculations/hourly_tariff_per_ga_co_es/when_with_child_metering_point/test_calculation_execute.py
@@ -11,11 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from calculation_logic.features.wholesale_calculations.hourly_tariff_per_ga_co_es.when_with_child_metering_point.states.scenario_state import (
+    get_expected,
+)
 from helpers.data_frame_utils import (
     assert_dataframe_and_schema,
 )
 from package.constants import WholesaleResultColumnNames
-from .states.scenario_state import get_expected
 
 
 def test_execute__returns_expected(  # type: ignore
