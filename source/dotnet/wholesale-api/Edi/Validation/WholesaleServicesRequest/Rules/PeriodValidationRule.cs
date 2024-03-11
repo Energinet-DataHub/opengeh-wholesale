0a0d102a0e47b1dc79279c1ecf73dd3dcee4f99e
Mathias Ormstrup Bjerregaard <mwo@energinet.dk>
2024-03-11 13:06:27 +0100

    can parse period start

--- source/dotnet/wholesale-api/Edi/Validation/WholesaleServicesRequest/Rules/PeriodValidationRule.cs
+++ source/dotnet/wholesale-api/Edi/Validation/WholesaleServicesRequest/Rules/PeriodValidationRule.cs
@@ -12,12 +12,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using NodaTime;
+using NodaTime.Text;
+
 namespace Energinet.DataHub.Wholesale.Edi.Validation.WholesaleServicesRequest.Rules;
 
 public sealed class PeriodValidationRule : IValidationRule<DataHub.Edi.Requests.WholesaleServicesRequest>
 {
+    private static readonly ValidationError _invalidDateFormat =
+        new(
+            "Forkert dato format for {PropertyName}, skal være YYYY-MM-DDT22:00:00Z eller YYYY-MM-DDT23:00:00Z / Wrong date format for {PropertyName}, must be YYYY-MM-DDT22:00:00Z or YYYY-MM-DDT23:00:00Z",
+            "D66");
+
     public Task<IList<ValidationError>> ValidateAsync(DataHub.Edi.Requests.WholesaleServicesRequest subject)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(subject);
+
+        var periodStart = subject.PeriodStart;
+
+        // This should not be possible, but we need to check for null due to the nullable type
+        ArgumentNullException.ThrowIfNull(periodStart);
+
+        var errors = new List<ValidationError>();
+
+        var startInstant = ParseToInstant(periodStart, "Start date", errors);
+
+        return Task.FromResult<IList<ValidationError>>(errors);
+    }
+
+    private static Instant? ParseToInstant(
+        string dateTimeString,
+        string propertyName,
+        ICollection<ValidationError> errors)
+    {
+        var parseResult = InstantPattern.General.Parse(dateTimeString);
+
+        if (parseResult.Success)
+            return parseResult.Value;
+
+        errors.Add(_invalidDateFormat.WithPropertyName(propertyName));
+        return null;
     }
 }
