710c6605783c65b6aac5c8055f470e793ccd52aa
Mathias Ormstrup Bjerregaard <152613120+MWO1024@users.noreply.github.com>
2024-03-11 10:11:08 +0100

    feat: DDM can request as if either DDM or MDR (#1178)

--- edi/terraform/main/apimao-request-aggregated-measuredata.tf
+++ edi/terraform/main/apimao-request-aggregated-measuredata.tf
@@ -21,6 +21,7 @@ module "apimao_request_aggregated_measure_data" {
                         <value>metereddataresponsible</value>
                         <value>energysupplier</value>
                         <value>balanceresponsibleparty</value>
+                        <value>gridoperator</value>
                     </claim>
                 </required-claims>
             </validate-jwt>
