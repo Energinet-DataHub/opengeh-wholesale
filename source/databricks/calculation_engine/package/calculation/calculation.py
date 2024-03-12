982b4813ed1dc33090a7973bc55a84d57632efb3
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-03-12 12:17:50 +0100

    refac: Use calculation internal typed data frames for grid loss (#2003)

--- source/databricks/calculation_engine/package/calculation/calculation.py
+++ source/databricks/calculation_engine/package/calculation/calculation.py
@@ -67,10 +67,12 @@ def _execute(
             ).cache()
         )
 
-    results.energy_results = energy_calculation.execute(
-        args,
-        metering_point_time_series,
-        grid_loss_responsible_df,
+    results.energy_results, positive_grid_loss, negative_grid_loss = (
+        energy_calculation.execute(
+            args,
+            metering_point_time_series,
+            grid_loss_responsible_df,
+        )
     )
 
     if (
@@ -87,8 +89,8 @@ def _execute(
             # This extends the content of metering_point_time_series with wholesale data.
             metering_point_time_series = get_wholesale_metering_point_times_series(
                 metering_point_time_series,
-                results.energy_results.positive_grid_loss,
-                results.energy_results.negative_grid_loss,
+                positive_grid_loss,
+                negative_grid_loss,
             )
 
             input_charges = prepared_data_reader.get_input_charges(
