071d884a63804184963c75a3e5ceebbb34c8967a
Alexander <alexanderharrow@hotmail.com>
2024-03-12 12:42:58 +0100

    Update wholesale_results_dataframe.py

--- source/databricks/calculation_engine/tests/calculation_logic/features/wholesale_calculations/wholesale_results_dataframe.py
+++ source/databricks/calculation_engine/tests/calculation_logic/features/wholesale_calculations/wholesale_results_dataframe.py
@@ -22,14 +22,13 @@ from pyspark.sql.types import (
 )
 
 
-def create_wholesale_result_dataframe(*args) -> DataFrame:  # type: ignore
+def create_wholesale_result_dataframe(*args) -> DataFrame:
     spark: SparkSession = args[0]
     df: DataFrame = args[1]
-    calculator_args: any = args[2]
+    calculator_args: any = args[2]  # type: ignore
 
     # Don't remove. Believed needed because this function is an argument to the setup function
     # and therefore the following packages are not automatically included.
-    from package.constants import Colname
     from package.calculation.output.schemas import wholesale_results_schema
     from package.constants import WholesaleResultColumnNames
 
@@ -47,10 +46,12 @@ def create_wholesale_result_dataframe(*args) -> DataFrame:  # type: ignore
         WholesaleResultColumnNames.quantity,
         col(WholesaleResultColumnNames.quantity).cast(DecimalType(28, 3)),
     )
+
     df = df.withColumn(
         WholesaleResultColumnNames.price,
         col(WholesaleResultColumnNames.price).cast(DecimalType(18, 6)),
     )
+
     df = df.withColumn(
         WholesaleResultColumnNames.amount,
         col(WholesaleResultColumnNames.amount).cast(DecimalType(18, 6)),
