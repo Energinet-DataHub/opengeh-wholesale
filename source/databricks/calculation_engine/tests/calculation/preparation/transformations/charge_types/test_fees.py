8519492a750ec32a5ee50da67c5e7e0ec36fc579
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-03-13 22:46:55 +0100

    ...

--- source/databricks/calculation_engine/tests/calculation/preparation/transformations/charge_types/test_fees.py
+++ source/databricks/calculation_engine/tests/calculation/preparation/transformations/charge_types/test_fees.py
@@ -100,6 +100,77 @@ class TestWhenOneLinkAndOnePrice:
         assert actual.df.collect()[0][Colname.charge_price] == charge_price
 
 
+class TestWhenTwoLinksAndOnePrice:
+    def test__returns_expected_price(
+        self,
+        spark: SparkSession,
+    ) -> None:
+        # Arrange
+        charge_time = JAN_1ST
+        charge_price = Decimal("1.123456")
+        from_date_link_1 = JAN_1ST
+        from_date_link_2 = JAN_3RD
+        metering_point_id_1 = "1"
+        metering_point_id_2 = "2"
+        charge_link_metering_points_rows = [
+            factory.create_charge_link_metering_point_periods_row(
+                charge_type=e.ChargeType.FEE,
+                metering_point_id=metering_point_id_1,
+                from_date=from_date_link_1,
+                to_date=from_date_link_1 + timedelta(days=1),
+            ),
+            factory.create_charge_link_metering_point_periods_row(
+                charge_type=e.ChargeType.FEE,
+                metering_point_id=metering_point_id_2,
+                from_date=from_date_link_2,
+                to_date=from_date_link_2 + timedelta(days=1),
+            ),
+        ]
+        charge_master_data_rows = [
+            factory.create_charge_master_data_row(
+                charge_type=e.ChargeType.FEE,
+                resolution=e.ChargeResolution.MONTH,
+                from_date=JAN_1ST,
+                to_date=FEB_1ST,
+            ),
+        ]
+        charge_prices_rows = [
+            factory.create_charge_prices_row(
+                charge_type=e.ChargeType.FEE,
+                charge_time=charge_time,
+                charge_price=charge_price,
+            ),
+        ]
+
+        charge_link_metering_point_periods = (
+            factory.create_charge_link_metering_point_periods(
+                spark, charge_link_metering_points_rows
+            )
+        )
+        charge_master_data = factory.create_charge_master_data(
+            spark, charge_master_data_rows
+        )
+        charge_prices = factory.create_charge_prices(spark, charge_prices_rows)
+
+        # Act
+        actual = get_fee_charges(
+            charge_master_data,
+            charge_prices,
+            charge_link_metering_point_periods,
+            DEFAULT_TIME_ZONE,
+        )
+
+        # Assert
+        actual_df = actual.df.orderBy(Colname.charge_time)
+        assert actual_df.count() == 2
+        assert actual_df.collect()[0][Colname.charge_time] == from_date_link_1
+        assert actual_df.collect()[0][Colname.charge_price] == charge_price
+        assert actual_df.collect()[0][Colname.metering_point_id] == metering_point_id_1
+        assert actual_df.collect()[1][Colname.charge_time] == from_date_link_2
+        assert actual_df.collect()[1][Colname.charge_price] == charge_price
+        assert actual_df.collect()[1][Colname.metering_point_id] == metering_point_id_2
+
+
 def test__get_fee_charges__filters_on_fee_charge_type(
     spark: SparkSession,
 ) -> None:
