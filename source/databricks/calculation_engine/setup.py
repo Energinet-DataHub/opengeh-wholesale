468d2c0b6bcc5a3c70b4f4663297ad36a6257c5e
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-03-14 13:55:51 +0100

    remove "v"

--- source/databricks/calculation_engine/setup.py
+++ source/databricks/calculation_engine/setup.py
@@ -40,7 +40,7 @@ setup(
         "delta-spark==3.1.0",
         "python-dateutil==2.8.2",
         "azure-monitor-opentelemetry==1.2.0",
-        "opengeh-spark-sql-migrations @ git+https://git@github.com/Energinet-DataHub/opengeh-python-packages@v1.4.1#subdirectory=source/spark_sql_migrations",
+        "opengeh-spark-sql-migrations @ git+https://git@github.com/Energinet-DataHub/opengeh-python-packages@1.4.1#subdirectory=source/spark_sql_migrations",
     ],
     entry_points={
         "console_scripts": [
