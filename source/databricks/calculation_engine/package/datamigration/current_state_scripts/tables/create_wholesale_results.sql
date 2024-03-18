ce97b27390bc10ac229201f62d2e2918bace8167
Jonas Michelsen <jvm@energinet.dk>
2024-03-18 16:36:09 +0100

    Update create_wholesale_results.sql

--- source/databricks/calculation_engine/package/datamigration/current_state_scripts/tables/create_wholesale_results.sql
+++ source/databricks/calculation_engine/package/datamigration/current_state_scripts/tables/create_wholesale_results.sql
@@ -153,3 +153,6 @@ GO
 ALTER TABLE {OUTPUT_DATABASE_NAME}.wholesale_results
     ADD CONSTRAINT amount_type_chk CHECK (amount_type IN ('amount_per_charge', 'monthly_amount_per_charge', 'total_monthly_amount'))
 GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.wholesale_results
+    ALTER COLUMN quantity DROP NOT NULL
+GO
