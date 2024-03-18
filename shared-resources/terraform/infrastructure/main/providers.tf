24a0fcbd5101df091c6db0106fd5cd7856b8b65d
Kristian Bank Erbou <contact@kbeconsult.dk>
2024-03-18 15:25:00 +0100

    Bump provider versions (#1204)

--- shared-resources/terraform/infrastructure/main/providers.tf
+++ shared-resources/terraform/infrastructure/main/providers.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     # It is recommended to pin to a given version of the Azure provider
-    azurerm = "3.85.0"
 
+    # --> DO NOT UPGRADE to >= 3.93.0 before this issue has been fixed: https://github.com/hashicorp/terraform-provider-azurerm/issues/25273
+    azurerm = "3.92.0" # See also https://github.com/Energinet-DataHub/geh-terraform-modules/pull/310 for details
     azuread = "2.47.0"
-
   }
 }
 
