cd9e6c50b0ae1cacc6f309b14c4cbd19888a5d85
Jonas Michelsen <jvm@energinet.dk>
2024-03-19 14:03:32 +0100

    Create 202403191000_Create_basisdata_tables.sql

---
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202403191000_Create_basisdata_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS {OUTPUT_DATABASE_NAME}.master_basisdata
+(
+    -- 36 characters UUID
+    calculation_id STRING NOT NULL,
+    -- Enum
+    calculation_type STRING NOT NULL,
+    calculation_execution_time_start TIMESTAMP NOT NULL,
+    
+    -- 36 characters UUID
+    calculation_result_id STRING NOT NULL,
+
+    metering_point_id STRING NOT NULL,
+    valid_from TIMESTAMP NOT NULL,
+    valid_to TIMESTAMP NOT NULL,
+    grid_area STRING NOT NULL,
+    to_grid_area STRING,
+    from_grid_area STRING,
+    metering_point_type STRING NOT NULL,
+    settlement_method STRING,
+    energy_supplier_id STRING NOT NULL,
+)
+USING DELTA
+-- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used. 
+-- In the production it is set to empty and the respective location is used. This means the production tables won't be deleted if the schema is.    
+{TEST}LOCATION '{CONTAINER_PATH}/{OUTPUT_FOLDER}/master_basisdata'
