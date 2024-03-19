f4060f5246b5c0b5b8aec124a39351391f917376
Jonas Michelsen <jvm@energinet.dk>
2024-03-19 14:13:15 +0100

    Update 202403191000_Create_basisdata_tables.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202403191000_Create_basisdata_tables.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202403191000_Create_basisdata_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS {OUTPUT_DATABASE_NAME}.master_basisdata
+CREATE TABLE IF NOT EXISTS {OUTPUT_DATABASE_NAME}.master_basis_data
 (
     -- 36 characters UUID
     calculation_id STRING NOT NULL,
@@ -22,4 +22,29 @@ CREATE TABLE IF NOT EXISTS {OUTPUT_DATABASE_NAME}.master_basisdata
 USING DELTA
 -- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used. 
 -- In the production it is set to empty and the respective location is used. This means the production tables won't be deleted if the schema is.    
-{TEST}LOCATION '{CONTAINER_PATH}/{OUTPUT_FOLDER}/master_basisdata'
+{TEST}LOCATION '{CONTAINER_PATH}/{OUTPUT_FOLDER}/master_basis_data'
+GO
+
+CREATE TABLE IF NOT EXISTS {OUTPUT_DATABASE_NAME}.time_series_basis_data
+(
+    -- 36 characters UUID
+    calculation_id STRING NOT NULL,
+    -- Enum
+    calculation_type STRING NOT NULL,
+    calculation_execution_time_start TIMESTAMP NOT NULL,
+
+    -- 36 characters UUID
+    calculation_result_id STRING NOT NULL,
+
+    grid_area STRING NOT NULL,
+    metering_point_id STRING NOT NULL,
+    metering_point_type STRING NOT NULL,
+    start_datetime TIMESTAMP NOT NULL,
+    energy_supplier_id STRING NOT NULL,
+    quantity STRING,
+    resolution STRING, NOT NULL
+)
+USING DELTA
+-- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used.
+-- In the production it is set to empty and the respective location is used. This means the production tables won't be deleted if the schema is.
+{TEST}LOCATION '{CONTAINER_PATH}/{OUTPUT_FOLDER}/time_series_basis_data'
