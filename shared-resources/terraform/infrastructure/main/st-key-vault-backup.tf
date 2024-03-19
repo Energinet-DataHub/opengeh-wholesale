c14803a658622fc3025b29136b941d288b32ce0f
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2024-03-19 16:07:45 +0100

    storage of backup key vault (#1208)
    * backup key vault
    
    * removed temp backupinstance
    
    * geo
    
    * comment

---
+++ shared-resources/terraform/infrastructure/main/st-key-vault-backup.tf
@@ -0,0 +1,21 @@
+module "st_key_vault_backup" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/storage-account?ref=13.55.0"
+
+  name                       = "kvbackup"
+  project_name               = var.domain_name_short
+  environment_short          = var.environment_short
+  environment_instance       = var.environment_instance
+  resource_group_name        = azurerm_resource_group.this.name
+  location                   = azurerm_resource_group.this.location
+  access_tier                = "Hot"
+  account_tier               = "Standard"
+  private_endpoint_subnet_id = data.azurerm_subnet.snet_private_endpoints.id
+  ip_rules                   = local.ip_restrictions_as_string
+  prevent_deletion           = true
+  role_assignments = [
+    {
+      principal_id         = data.azurerm_client_config.current.object_id
+      role_definition_name = "Storage Blob Data Contributor"
+    }
+  ]
+}
