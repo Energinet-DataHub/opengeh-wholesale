aa1c68209b4dd0c949fe731a16b96818d2003266
Alexander <alexanderharrow@hotmail.com>
2024-03-20 16:14:26 +0100

    Update correlations.py

--- source/databricks/calculation_engine/tests/calculation_logic/utils/correlations.py
+++ source/databricks/calculation_engine/tests/calculation_logic/utils/correlations.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from package.calculation.input import TableReader
+from package.calculation import TableReader
 from package.calculation.input.schemas import (
     metering_point_period_schema,
     time_series_point_schema,
