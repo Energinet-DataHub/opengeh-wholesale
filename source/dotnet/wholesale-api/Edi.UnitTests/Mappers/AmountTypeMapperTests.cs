ea6db41668fa5bd7628a74886ebccc8ad45b64ab
Ebbe Knudsen <xedkn@energinet.dk>
2024-03-20 11:17:30 +0100

    Add using statement

--- source/dotnet/wholesale-api/Edi.UnitTests/Mappers/AmountTypeMapperTests.cs
+++ source/dotnet/wholesale-api/Edi.UnitTests/Mappers/AmountTypeMapperTests.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults;
 using Energinet.DataHub.Wholesale.Edi.Mappers;
 using FluentAssertions;
 using Xunit;
@@ -21,9 +22,9 @@ namespace Energinet.DataHub.Wholesale.Edi.UnitTests.Mappers;
 public class AmountTypeMapperTests
 {
     [Theory]
-    [InlineData(CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults.Resolution.Month, CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults.AmountType.MonthlyAmountPerCharge)]
-    [InlineData(null, CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults.AmountType.AmountPerCharge)]
-    public void Map_WhenValid_ReturnsExpectedChargeType(CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults.Resolution? resolution, Energinet.DataHub.Wholesale.CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults.AmountType expectedResult)
+    [InlineData(Resolution.Month, AmountType.MonthlyAmountPerCharge)]
+    [InlineData(null, AmountType.AmountPerCharge)]
+    public void Map_WhenValid_ReturnsExpectedChargeType(Resolution? resolution, AmountType expectedResult)
     {
         // Act
         var actual = AmountTypeMapper.Map(resolution);
@@ -33,10 +34,10 @@ public class AmountTypeMapperTests
     }
 
     [Theory]
-    [InlineData((CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults.Resolution)int.MinValue)]
-    [InlineData(CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults.Resolution.Day)]
-    [InlineData(CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults.Resolution.Hour)]
-    public void Map_WhenInvalid_ThrowsArgumentOutOfRangeException(CalculationResults.Interfaces.CalculationResults.Model.WholesaleResults.Resolution? resolution)
+    [InlineData((Resolution)int.MinValue)]
+    [InlineData(Resolution.Day)]
+    [InlineData(Resolution.Hour)]
+    public void Map_WhenInvalid_ThrowsArgumentOutOfRangeException(Resolution? resolution)
     {
         // Act
         var act = () => AmountTypeMapper.Map(resolution);
