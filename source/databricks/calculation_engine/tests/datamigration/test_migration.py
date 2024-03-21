8f67d14ee8ebf41912ed8d6d92eed3c3fbf2c2dd
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-03-21 14:56:49 +0100

    fix test by setting location and prefix for tables

--- source/databricks/calculation_engine/tests/datamigration/test_migration.py
+++ source/databricks/calculation_engine/tests/datamigration/test_migration.py
@@ -293,6 +293,7 @@ def test__current_state_and_migration_scripts__should_give_same_result(
         spark,
         substitution_variables=migration_scripts_substitutions,
         location="migration_test",
+        table_prefix="migration_",
     )
     schema_migration_pipeline.migrate()
 
@@ -308,7 +309,10 @@ def test__current_state_and_migration_scripts__should_give_same_result(
         },
     )
     spark_sql_migration_helper.configure_spark_sql_migration(
-        spark, substitution_variables=substitutions, location="migration_test"
+        spark,
+        substitution_variables=substitutions,
+        location="migration_test",
+        table_prefix="migration_",
     )
     schema_migration_pipeline._migrate(0)
 
