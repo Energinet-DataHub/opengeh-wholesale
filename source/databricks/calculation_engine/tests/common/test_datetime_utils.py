c01e3d3c040ab3f3653f1713f93aafdf996fc4e9
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-03-22 12:39:31 +0100

    ...

--- source/databricks/calculation_engine/tests/common/test_datetime_utils.py
+++ source/databricks/calculation_engine/tests/common/test_datetime_utils.py
@@ -11,27 +11,108 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from datetime import datetime
 
-@pytest.mark.parametrize(
-    "log_method, log_func",
-    [
-        ("debug", logging.Logger.debug),
-        ("info", logging.Logger.info),
-        ("warning", logging.Logger.warning),
-    ],
-)
-def test__log_method__when_called_with_custom_extras__passes_correct_extras(
-    log_method, log_func
-):
-    # Arrange
-    logger = Logger("test_logger")
-    test_message = f"Test {log_method} message"
-    custom_extras = {"key": "value"}
-    expected_extras = custom_extras | logger._extras
+import pytest
 
-    with patch.object(logging.Logger, log_method) as mock_log_method:
+from package.common.datetime_utils import is_exactly_one_calendar_month
+
+DEFAULT_TIME_ZONE = "Europe/Copenhagen"
+
+
+class TestWhenPeriodIsNotOneCalendarMonth:
+    @pytest.mark.parametrize(
+        "period_start, period_end",
+        [
+            (  # Missing one day in the beginning
+                    datetime(2022, 6, 1, 22),
+                    datetime(2022, 6, 30, 22),
+            ),
+            (  # Missing one day at the end
+                    datetime(2022, 5, 31, 22),
+                    datetime(2022, 6, 29, 22),
+            ),
+            (  # Missing one hour in the beginning
+                    datetime(2022, 5, 31, 23),
+                    datetime(2022, 6, 30, 22),
+            ),
+            (  # Missing one hour at the end
+                    datetime(2022, 5, 31, 22),
+                    datetime(2022, 6, 30, 21),
+            ),
+            (  # One hour too much in the beginning
+                    datetime(2022, 5, 31, 21),
+                    datetime(2022, 6, 30, 22),
+            ),
+            (  # One hour too much at the end
+                    datetime(2022, 5, 31, 22),
+                    datetime(2022, 6, 30, 23),
+            ),
+            ( # Two months
+                    datetime(2022, 5, 31, 22),
+                    datetime(2022, 7, 31, 22),
+            ),
+            (  # Entering daylights saving time - not ending at midnight
+                    datetime(2020, 2, 29, 23, 0),
+                    datetime(2020, 3, 31, 23, 0),
+            ),
+            (  # Exiting daylights saving time - not ending at midnight
+                    datetime(2020, 9, 30, 22, 0),
+                    datetime(2020, 10, 31, 22, 0),
+            ),
+        ],
+    )
+    def test__is_exactly_one_calendar_month__returns_false(self,
+        period_start: datetime, period_end: datetime) -> None:
+        # Arrange
+        time_zone = "Europe/Copenhagen"
+
         # Act
-        getattr(logger, log_method)(test_message, extras=custom_extras)
+        actual = is_exactly_one_calendar_month(period_start, period_end, time_zone)
 
         # Assert
-        mock_log_method.assert_called_once_with(test_message, extra=expected_extras)
+        assert actual is False
+
+
+
+
+
+
+class TestWhenPeriodOneCalendarMonth:
+    @pytest.mark.parametrize(
+        "period_start, period_end",
+        [
+            (
+                    datetime(2022, 5, 31, 22),
+                    datetime(2022, 6, 30, 22),
+                    DEFAULT_TIME_ZONE,
+            ),
+            (
+                    datetime(2022, 5, 31, 22),
+                    datetime(2022, 6, 29, 22),
+                    DEFAULT_TIME_ZONE,
+            ),
+            (
+                    datetime(2022, 5, 31, 23),
+                    datetime(2022, 6, 30, 22),
+                    DEFAULT_TIME_ZONE,
+            ),
+            (  # Entering daylights saving time - not ending at midnight
+                    datetime(2020, 2, 29, 23, 0),
+                    datetime(2020, 3, 31, 22, 0),
+                
+            ),
+            (  # Exiting daylights saving time - not ending at midnight
+                    datetime(2020, 9, 30, 22, 0),
+                    datetime(2020, 10, 31, 23, 0),
+            ),
+        ],
+    )
+    def test__is_exactly_one_calendar_month__returns_true(self,
+        period_start: datetime, period_end: datetime, time_zone: str) -> None:
+
+        # Act
+        actual = is_exactly_one_calendar_month(period_start, period_end, time_zone)
+
+        # Assert
+        assert actual is True
