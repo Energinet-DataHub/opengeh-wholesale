11d938d883c0bc26fadb9e3329d6ccb8be13a1b1
Rasmus Leth Friis <116742936+rf-halfspace@users.noreply.github.com>
2024-03-25 10:16:22 +0100

    Migrate dh2-bridge kv to RBAC (#1232)
    * Migrate dh2-bridge kv to RBAC
    
    * Migrate dh2bridge kv to RBAC

--- dh2-bridge/terraform/main/kv-internal.tf
+++ dh2-bridge/terraform/main/kv-internal.tf
@@ -10,6 +10,7 @@ module "kv_internal" {
   sku_name                        = "premium"
   private_endpoint_subnet_id      = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
   ip_rules                        = local.ip_restrictions_as_string
+  enable_rbac_authorization       = true
 }
 
 module "kvs_sendgrid_api_key" {
