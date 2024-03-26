98a1c0fa0540456ec287f782dccacee7abbd6454
Bjarke Meier <hej@bjarkemeier.dk>
2024-03-26 17:46:26 +0100

    Fix black lint

--- source/databricks/calculation_engine/package/datamigration/schema_config.py
+++ source/databricks/calculation_engine/package/datamigration/schema_config.py
@@ -16,6 +16,7 @@ from spark_sql_migrations import Schema, Table
 
 import package.calculation.basis_data.schemas as basis_data_schemas
 import package.infrastructure.paths as paths
+
 # calculation_input
 from package.calculation.input.schemas.grid_loss_metering_points_schema import (
     grid_loss_metering_points_schema,
@@ -23,6 +24,7 @@ from package.calculation.input.schemas.grid_loss_metering_points_schema import (
 from package.calculation.output.schemas.energy_results_schema import (
     energy_results_schema,
 )
+
 # calculation_output
 from package.calculation.output.schemas.wholesale_results_schema import (
     wholesale_results_schema,
