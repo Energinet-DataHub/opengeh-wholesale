aedbc24ba1d1050f7feb627f36743fd91f5a05c5
Jonas Michelsen <jvm@energinet.dk>
2024-04-04 14:27:43 +0200

    hack

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202403061008_Add_calculation_input_tables_to_database.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202403061008_Add_calculation_input_tables_to_database.sql
@@ -1,19 +1,21 @@
-CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.metering_point_periods
-    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/metering_point_periods'
-GO
-
-CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.time_series_points
-    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/time_series_points'
-GO
-
-CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.charge_link_periods
-    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_link_periods'
-GO
-
-CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.charge_masterdata_periods
-    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_masterdata_periods'
-GO
-
-CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.charge_price_points
-    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_price_points'
-GO
+-- Hack to make sure that the script is not executed when running integration tests
+DESCRIBE DATABASE {INPUT_DATABASE_NAME};
+{TEST}CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.metering_point_periods
+{TEST}    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/metering_point_periods'
+{TEST}GO
+{TEST}
+{TEST}CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.time_series_points
+{TEST}    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/time_series_points'
+{TEST}GO
+{TEST}
+{TEST}CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.charge_link_periods
+{TEST}    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_link_periods'
+{TEST}GO
+{TEST}
+{TEST}CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.charge_masterdata_periods
+{TEST}    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_masterdata_periods'
+{TEST}GO
+{TEST}
+{TEST}CREATE EXTERNAL TABLE if not exists {INPUT_DATABASE_NAME}.charge_price_points
+{TEST}    USING DELTA LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_price_points'
+{TEST}GO
