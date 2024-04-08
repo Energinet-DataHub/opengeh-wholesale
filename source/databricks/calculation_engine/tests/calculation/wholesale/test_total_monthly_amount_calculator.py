cb26725936524a84e712100235188df3293aecb0
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-04-08 15:05:56 +0200

    ...

--- source/databricks/calculation_engine/tests/calculation/wholesale/test_total_monthly_amount_calculator.py
+++ source/databricks/calculation_engine/tests/calculation/wholesale/test_total_monthly_amount_calculator.py
@@ -12,161 +12,112 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import datetime
 from decimal import Decimal
 
 from pyspark.sql import SparkSession
-from package.calculation.wholesale.sum_within_month import sum_within_month
 from package.codelists import (
-    ChargeQuality,
+    ChargeType,
 )
 from package.constants import Colname
 import tests.calculation.wholesale.wholesale_results_factory as wholesale_results_factory
 from package.calculation.wholesale.total_monthly_amount_calculator import calculate
 
-PERIOD_START_DATETIME = datetime(2019, 12, 31, 23)
 
-
-def test__sum_within_month__sums_amount_per_month(
+def test__calculate__when_no_charge_tax_in_group__sums_all_amounts(
     spark: SparkSession,
 ) -> None:
     # Arrange
-    rows = [
-        wholesale_results_factory.create_row(
-            total_amount=Decimal("2"),
+    monthly_tariffs_from_hourly = wholesale_results_factory.create(
+        spark,
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.TARIFF,
+            total_amount=Decimal("1"),
+            charge_tax=False,
         ),
-        wholesale_results_factory.create_row(
+    )
+    monthly_tariffs_from_daily = wholesale_results_factory.create(
+        spark,
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.TARIFF,
             total_amount=Decimal("2"),
+            charge_tax=False,
         ),
-    ]
-    df = wholesale_results_factory.create(spark, rows)
+    )
+    monthly_fees = wholesale_results_factory.create(
+        spark,
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.FEE,
+            total_amount=Decimal("3"),
+            charge_tax=False,
+        ),
+    )
+    monthly_subscriptions = wholesale_results_factory.create(
+        spark,
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.SUBSCRIPTION,
+            total_amount=Decimal("4"),
+            charge_tax=False,
+        ),
+    )
+
     # Act
     actual = calculate(
-        df,
-        PERIOD_START_DATETIME,
+        monthly_subscriptions=monthly_subscriptions.df,
+        monthly_fees=monthly_fees.df,
+        monthly_tariffs_from_hourly=monthly_tariffs_from_hourly.df,
+        monthly_tariffs_from_daily=monthly_tariffs_from_daily.df,
     ).df
 
     # Assert
-    assert actual.collect()[0][Colname.total_amount] == Decimal("4.000000")
+    assert actual.collect()[0][Colname.total_amount] == Decimal("10.000000")
     assert actual.count() == 1
 
 
-def test__sum_within_month__qualities_always_set_to_none(
+def test__calculate__sets_charge_time(
     spark: SparkSession,
 ) -> None:
     # Arrange
-    rows = [
-        wholesale_results_factory.create_row(
-            qualities=[ChargeQuality.CALCULATED.value],
+    monthly_tariffs_from_hourly = wholesale_results_factory.create(
+        spark,
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.TARIFF,
+            total_amount=Decimal("1"),
+            charge_tax=False,
         ),
-        wholesale_results_factory.create_row(
-            qualities=[ChargeQuality.ESTIMATED.value],
+    )
+    monthly_tariffs_from_daily = wholesale_results_factory.create(
+        spark,
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.TARIFF,
+            total_amount=Decimal("2"),
+            charge_tax=False,
         ),
-    ]
-    df = wholesale_results_factory.create(spark, rows)
+    )
+    monthly_fees = wholesale_results_factory.create(
+        spark,
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.FEE,
+            total_amount=Decimal("3"),
+            charge_tax=False,
+        ),
+    )
+    monthly_subscriptions = wholesale_results_factory.create(
+        spark,
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.SUBSCRIPTION,
+            total_amount=Decimal("4"),
+            charge_tax=False,
+        ),
+    )
 
     # Act
-    actual = sum_within_month(
-        df,
-        PERIOD_START_DATETIME,
+    actual = calculate(
+        monthly_subscriptions=monthly_subscriptions.df,
+        monthly_fees=monthly_fees.df,
+        monthly_tariffs_from_hourly=monthly_tariffs_from_hourly.df,
+        monthly_tariffs_from_daily=monthly_tariffs_from_daily.df,
     ).df
 
     # Assert
-    assert actual.collect()[0][Colname.qualities] is None
-    assert actual.count() == 1
-
-
-def test__sum_within_month__charge_time_is_always_period_start_datetime(
-    spark: SparkSession,
-) -> None:
-    # Arrange
-    rows = [
-        wholesale_results_factory.create_row(
-            charge_time=datetime(2020, 1, 3, 0),
-        ),
-    ]
-    df = wholesale_results_factory.create(spark, rows)
-
-    # Act
-    actual = sum_within_month(
-        df,
-        PERIOD_START_DATETIME,
-    ).df
-
-    # Assert
-    assert actual.collect()[0][Colname.charge_time] == PERIOD_START_DATETIME
-
-
-def test__sum_within_month__total_quantity_always_set_to_none(
-    spark: SparkSession,
-) -> None:
-    # Arrange
-    rows = [
-        wholesale_results_factory.create_row(
-            total_quantity=Decimal("1"),
-        ),
-        wholesale_results_factory.create_row(
-            total_quantity=Decimal("1"),
-        ),
-    ]
-    df = wholesale_results_factory.create(spark, rows)
-
-    # Act
-    actual = sum_within_month(
-        df,
-        PERIOD_START_DATETIME,
-    ).df
-
-    # Assert
-    assert actual.collect()[0][Colname.total_quantity] is None
-    assert actual.count() == 1
-
-
-def test__sum_within_month__sets_charge_price_to_none(
-    spark: SparkSession,
-) -> None:
-    # Arrange
-    rows = [
-        wholesale_results_factory.create_row(
-            charge_price=Decimal("1"),
-        ),
-        wholesale_results_factory.create_row(
-            charge_price=Decimal("1"),
-        ),
-    ]
-    df = wholesale_results_factory.create(spark, rows)
-
-    # Act
-    actual = sum_within_month(
-        df,
-        PERIOD_START_DATETIME,
-    ).df
-
-    # Assert
-    assert actual.collect()[0][Colname.charge_price] is None
-    assert actual.count() == 1
-
-
-def test__sum_within_month__can_sum_when_one_value_is_none(
-    spark: SparkSession,
-) -> None:
-    # Arrange
-    rows = [
-        wholesale_results_factory.create_row(
-            total_amount=None,
-        ),
-        wholesale_results_factory.create_row(
-            total_amount=Decimal("6.000000"),
-        ),
-    ]
-    df = wholesale_results_factory.create(spark, rows)
-
-    # Act
-    actual = sum_within_month(
-        df,
-        datetime(2019, 12, 31, 23),
-    ).df
-
-    # Assert
-    assert actual.collect()[0][Colname.total_amount] == Decimal("6.000000")
+    assert actual.collect()[0][Colname.total_amount] == Decimal("10.000000")
     assert actual.count() == 1
