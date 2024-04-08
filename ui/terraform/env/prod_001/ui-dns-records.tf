8aafe6d6ecbcc53c4c4d0c1d66ca915bb4e95a51
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2024-04-08 14:44:33 +0200

    allow www in UI in prod (#1284)
    * allow www in UI in prod
    
    * comment

--- ui/terraform/env/prod_001/ui-dns-records.tf
+++ ui/terraform/env/prod_001/ui-dns-records.tf
@@ -19,3 +19,20 @@ resource "azurerm_dns_a_record" "this" {
   ttl                 = 3600
   target_resource_id  = azurerm_static_site.ui.id
 }
+
+resource "azurerm_dns_cname_record" "www" {
+  name                = "www"
+  zone_name           = var.frontend_url
+  resource_group_name = data.azurerm_resource_group.shared.name
+  ttl                 = 3600
+  record              = azurerm_static_site.ui.default_host_name
+}
+
+# Allow for www.datahub3.dk - needs to depend on the CNAME record for it to be validated
+resource "azurerm_static_site_custom_domain" "www" {
+  static_site_id  = azurerm_static_site.ui.id
+  domain_name     = "www.${local.frontend_url}"
+  validation_type = "cname-delegation"
+
+  depends_on = [ azurerm_dns_cname_record.www ]
+}
