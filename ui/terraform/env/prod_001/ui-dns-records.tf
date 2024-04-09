7ace980515eb47e44d43c05f73ce21609711d457
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2024-04-09 13:40:45 +0200

    DNS time wait on record (#1285)

--- ui/terraform/env/prod_001/ui-dns-records.tf
+++ ui/terraform/env/prod_001/ui-dns-records.tf
@@ -28,11 +28,18 @@ resource "azurerm_dns_cname_record" "www" {
   record              = azurerm_static_site.ui.default_host_name
 }
 
+# Add wait time to allow for the CNAME record to be propagated
+resource "time_sleep" "wait_60_seconds" {
+  create_duration = "60s"
+
+  depends_on = [azurerm_dns_cname_record.www]
+}
+
 # Allow for www.datahub3.dk - needs to depend on the CNAME record for it to be validated
 resource "azurerm_static_site_custom_domain" "www" {
   static_site_id  = azurerm_static_site.ui.id
   domain_name     = "www.${local.frontend_url}"
   validation_type = "cname-delegation"
 
-  depends_on = [ azurerm_dns_cname_record.www ]
+  depends_on = [azurerm_dns_cname_record.www, time_sleep.wait_60_seconds]
 }
