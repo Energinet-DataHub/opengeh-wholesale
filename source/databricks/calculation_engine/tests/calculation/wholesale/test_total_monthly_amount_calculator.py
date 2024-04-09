46282da19572f7b253ba1af6836d85648465408c
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-04-09 13:25:43 +0200

    handle None

--- source/databricks/calculation_engine/tests/calculation/wholesale/test_total_monthly_amount_calculator.py
+++ source/databricks/calculation_engine/tests/calculation/wholesale/test_total_monthly_amount_calculator.py
@@ -96,3 +96,41 @@ def test__calculate__adds_tax_amount_if_not_system_operator(
     # Assert
     assert actual.collect()[0][Colname.total_amount] == expected
     assert actual.count() == 1
+
+
+@pytest.mark.parametrize(
+    "non_tax_amount, tax_amount, expected",
+    [
+        [None, Decimal("1.000000"), Decimal("1.000000")],
+        [Decimal("1.000000"), None, Decimal("1.000000")],
+        [None, None, None],
+    ],
+)
+def test__calculate__when_amount_is_null__ignore_null_in_sum(
+    spark: SparkSession, non_tax_amount: Decimal, tax_amount: Decimal, expected: Decimal
+) -> None:
+    # Arrange
+    monthly_amounts_rows = [
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.SUBSCRIPTION,
+            total_amount=non_tax_amount,
+            charge_tax=False,
+            charge_owner=GRID_ACCESS_PROVIDER_ID,
+        ),
+        wholesale_results_factory.create_monthly_amount_row(
+            charge_type=ChargeType.TARIFF,
+            total_amount=tax_amount,
+            charge_tax=True,
+            charge_owner=SYSTEM_OPERATOR_ID,
+        ),
+    ]
+    monthly_amounts = wholesale_results_factory.create(spark, monthly_amounts_rows)
+
+    # Act
+    actual = calculate(
+        monthly_amounts.df,
+    ).df
+
+    # Assert
+    assert actual.collect()[0][Colname.total_amount] == expected
+    assert actual.count() == 1
