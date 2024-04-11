8745a79a4a3b4b9d0f8017b4ce4db04dbef3f2f8
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-04-11 21:37:39 +0200

    ...

--- source/databricks/calculation_engine/package/calculation/calculation.py
+++ source/databricks/calculation_engine/package/calculation/calculation.py
@@ -144,6 +144,8 @@ def _write_results(results: CalculationResultsContainer) -> None:
     write_energy_results(results.energy_results)
     if results.wholesale_results is not None:
         write_wholesale_results(results.wholesale_results)
+
+    if results.total_monthly_amounts is not None:
         write_total_monthly_amounts(results.total_monthly_amounts)
 
     # We write basis data at the end of the calculation to make it easier to analyze performance of the calculation part
