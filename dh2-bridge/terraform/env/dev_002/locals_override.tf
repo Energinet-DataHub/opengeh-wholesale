d7414d0fcd3eb79b2126fe19585048914400eb82
Karsten M <xnetsrak@gmail.com>
2024-04-15 08:41:20 +0200

    disable dev02 dh2 endpoint link (#1315)

--- dh2-bridge/terraform/env/dev_002/locals_override.tf
+++ dh2-bridge/terraform/env/dev_002/locals_override.tf
@@ -1,4 +1,3 @@
 locals {
-  DH2_ENDPOINT                      = "https://b2b.te6.datahub.dk"
   DH2BRIDGE_CERTIFICATE_THUMBPRINT  = resource.azurerm_key_vault_certificate.dh2_certificate.thumbprint
 }
