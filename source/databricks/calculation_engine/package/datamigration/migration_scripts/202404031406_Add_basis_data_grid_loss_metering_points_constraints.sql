78a326aa7498a622970262e2070c569fb601b552
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-04-17 13:29:34 +0200

    remove drop constraints

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202404031406_Add_basis_data_grid_loss_metering_points_constraints.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202404031406_Add_basis_data_grid_loss_metering_points_constraints.sql
@@ -1,13 +1,7 @@
 ALTER TABLE {BASIS_DATA_DATABASE_NAME}.grid_loss_metering_points
-    DROP CONSTRAINT IF EXISTS calculation_id_chk
-GO
-ALTER TABLE {BASIS_DATA_DATABASE_NAME}.grid_loss_metering_points
     ADD CONSTRAINT calculation_id_chk CHECK (LENGTH(calculation_id) = 36)
 GO
 
-ALTER TABLE {BASIS_DATA_DATABASE_NAME}.grid_loss_metering_points
-    DROP CONSTRAINT IF EXISTS metering_point_id_chk
-GO
 ALTER TABLE {BASIS_DATA_DATABASE_NAME}.grid_loss_metering_points
     ADD CONSTRAINT metering_point_id_chk CHECK  (LENGTH(metering_point_id) = 18)
 GO
--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202404150901_test.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE {OUTPUT_DATABASE_NAME}.total_monthly_amounts
-    ADD CONSTRAINT calculation_id_chk_v2 CHECK (LENGTH(calculation_id) = 36)
-GO
-
-ALTER TABLE {OUTPUT_DATABASE_NAME}.total_monthly_amounts
-    DROP CONSTRAINT IF EXISTS calculation_id_chk_v2
-GO
