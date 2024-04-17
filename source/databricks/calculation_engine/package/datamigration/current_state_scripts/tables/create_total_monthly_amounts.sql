71a07647929a9e0cf045c8f1289891f393c1fc6b
Christian Hansen <120550591+cenh-halfspace@users.noreply.github.com>
2024-04-17 10:15:59 +0200

    Update create_total_monthly_amounts.sql

--- source/databricks/calculation_engine/package/datamigration/current_state_scripts/tables/create_total_monthly_amounts.sql
+++ source/databricks/calculation_engine/package/datamigration/current_state_scripts/tables/create_total_monthly_amounts.sql
@@ -24,25 +24,49 @@ TBLPROPERTIES (delta.deletedFileRetentionDuration = 'interval 30 days')
 GO
 
 -- Constraints --
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.total_monthly_amounts
+    DROP CONSTRAINT IF EXISTS calculation_id_chk
+GO
+
 ALTER TABLE {OUTPUT_DATABASE_NAME}.total_monthly_amounts
     ADD CONSTRAINT calculation_id_chk CHECK (LENGTH(calculation_id) = 36)
 GO
 
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.total_monthly_amounts
+    DROP CONSTRAINT IF EXISTS calculation_type_chk
+GO
+    
 ALTER TABLE {OUTPUT_DATABASE_NAME}.total_monthly_amounts
     ADD CONSTRAINT calculation_type_chk CHECK (calculation_type IN ('WholesaleFixing', 'FirstCorrectionSettlement', 'SecondCorrectionSettlement', 'ThirdCorrectionSettlement'))
 GO
 
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.total_monthly_amounts
+    DROP CONSTRAINT IF EXISTS calculation_result_id_chk
+GO
+    
 ALTER TABLE {OUTPUT_DATABASE_NAME}.total_monthly_amounts
     ADD CONSTRAINT calculation_result_id_chk CHECK (LENGTH(calculation_result_id) = 36)
+GO
+
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.total_monthly_amounts
+    DROP CONSTRAINT IF EXISTS grid_area_chk
 GO
 
 ALTER TABLE {OUTPUT_DATABASE_NAME}.total_monthly_amounts
     ADD CONSTRAINT grid_area_chk CHECK (LENGTH(grid_area) = 3)
 GO
 
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.total_monthly_amounts
+    DROP CONSTRAINT IF EXISTS energy_supplier_id_chk
+GO
+    
 -- Length is 16 when EIC and 13 when GLN
 ALTER TABLE {OUTPUT_DATABASE_NAME}.total_monthly_amounts
     ADD CONSTRAINT energy_supplier_id_chk CHECK (LENGTH(energy_supplier_id) = 13 OR LENGTH(energy_supplier_id) = 16)
+GO
+
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.total_monthly_amounts
+    DROP CONSTRAINT IF EXISTS charge_owner_id_chk
 GO
 
 -- Length is 16 when EIC and 13 when GLN
