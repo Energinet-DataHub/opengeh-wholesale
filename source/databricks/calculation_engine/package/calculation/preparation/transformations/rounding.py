61db986c35f58882ed2bd60fd34367cad4df1f82
Jonas Michelsen <jvm@energinet.dk>
2024-04-17 13:02:34 +0200

    Update rounding.py

--- source/databricks/calculation_engine/package/calculation/preparation/transformations/rounding.py
+++ source/databricks/calculation_engine/package/calculation/preparation/transformations/rounding.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from pyspark.sql import DataFrame
 import pyspark.sql.functions as f
 
@@ -20,11 +21,17 @@ from package.constants import Colname
 def special_quantity_rounding(df: DataFrame) -> DataFrame:
     """
     The function rounds the quantity to 3 decimal places.
-    All Quantities that come in has a scale of 3.
+    All Quantities that comes in from time series has a scale of 3.
     The scale will be increased to 6 in different way depending on resolution.
     Quantities with resolution of PT15M get added three zero at the end to increase the scale to 6.
     Quantities with resolution of PT1H gets divided into 4 to act as a PT15M resolution.
     To be able to get the same PT1H quantity result again we have to round to 3 decimal places in a specific way.
+
+    Note: this is done for every row no matter which resolution it came from, but they are only affected if
+    the quantity has digits other than 0 after the 3rd decimal place and that is only possible for quantities from PT1H.
+    This function is built on the assumption that quantities from PT15M always have 3 zeros after the 3rd decimal place,
+    which means anything after the 3rd decimal place is always the same in all 4 rows within the same hour
+
     example: if we have a PT1H quantity of 0.003, and we divide it into 4 we get 0.00075.
     if we just round the new value normally we get 0.001. and we add that up 4 times we get 0.004.
     Which is not the same as the original value. To get original value we need to somthing like this:
@@ -36,10 +43,6 @@ def special_quantity_rounding(df: DataFrame) -> DataFrame:
     the original value and the rounded value is 0.000250 - 0.000 = 0.000250. We add that to the next quantity.
     0.000750 + 0.000250 = 0.001000. We round that to 3 decimal places we get 0.001.
     Now we can add them up and get the original value of 0.003.
-    Note: this is done for every row no matter which resolution it came from, but they are only affected if
-    the quantity has digits other than 0 after the 3rd decimal place and that is only possible for quantities from PT1H.
-    This function is built on the assumption that quantities from PT15M always have 3 zeros after the 3rd decimal place,
-    which means anything after the 3rd decimal place is always the same in all 4 rows within the same hour
     """
     df = df.orderBy(Colname.observation_time)
     df = df.withColumn(
