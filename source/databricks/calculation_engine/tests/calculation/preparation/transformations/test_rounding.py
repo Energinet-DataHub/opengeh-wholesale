f30f79a9208b9caafe2950561dfd1bde941b069c
Jonas Michelsen <jvm@energinet.dk>
2024-04-17 13:24:45 +0200

    Update test_rounding.py

--- source/databricks/calculation_engine/tests/calculation/preparation/transformations/test_rounding.py
+++ source/databricks/calculation_engine/tests/calculation/preparation/transformations/test_rounding.py
@@ -80,22 +80,22 @@ def test_special_quantity_rounding_when_two_energy_supplier_at_the_same_time(
         },
         {
             Colname.observation_time: datetime(2022, 1, 1, 1, 0),
-            Colname.quantity: Decimal("222.03075"),
+            Colname.quantity: Decimal("25.00125"),
             Colname.energy_supplier_id: "2",
         },
         {
             Colname.observation_time: datetime(2022, 1, 1, 1, 15),
-            Colname.quantity: Decimal("222.03075"),
+            Colname.quantity: Decimal("25.00125"),
             Colname.energy_supplier_id: "2",
         },
         {
             Colname.observation_time: datetime(2022, 1, 1, 1, 30),
-            Colname.quantity: Decimal("222.03075"),
+            Colname.quantity: Decimal("25.00125"),
             Colname.energy_supplier_id: "2",
         },
         {
             Colname.observation_time: datetime(2022, 1, 1, 1, 45),
-            Colname.quantity: Decimal("222.03075"),
+            Colname.quantity: Decimal("25.00125"),
             Colname.energy_supplier_id: "2",
         },
     ]
@@ -115,7 +115,7 @@ def test_special_quantity_rounding_when_two_energy_supplier_at_the_same_time(
     assert actual.collect()[2][Colname.quantity] == Decimal("222.030")
     assert actual.collect()[3][Colname.quantity] == Decimal("222.031")
     # Energy supplier 2
-    assert actual.collect()[4][Colname.quantity] == Decimal("222.031")
-    assert actual.collect()[5][Colname.quantity] == Decimal("222.031")
-    assert actual.collect()[6][Colname.quantity] == Decimal("222.030")
-    assert actual.collect()[7][Colname.quantity] == Decimal("222.031")
+    assert actual.collect()[4][Colname.quantity] == Decimal("25.001")
+    assert actual.collect()[5][Colname.quantity] == Decimal("25.002")
+    assert actual.collect()[6][Colname.quantity] == Decimal("25.001")
+    assert actual.collect()[7][Colname.quantity] == Decimal("25.001")
