797a0c2c1581b59e008912fb4f22ac8690aa656d
Jonas Michelsen <jvm@energinet.dk>
2024-04-17 12:13:13 +0200

    Update test_rounding.py

--- source/databricks/calculation_engine/tests/calculation/preparation/transformations/test_rounding.py
+++ source/databricks/calculation_engine/tests/calculation/preparation/transformations/test_rounding.py
@@ -21,7 +21,7 @@ from package.calculation.preparation.transformations.rounding import (
 from package.constants import Colname
 
 
-def test_get_rounded(spark: SparkSession) -> None:
+def test_special_quantity_rounding(spark: SparkSession) -> None:
     # Arrange
     rows = [
         {
@@ -45,10 +45,77 @@ def test_get_rounded(spark: SparkSession) -> None:
 
     # Act
     actual = special_quantity_rounding(df)
-    actual.show()
 
     # Assert
     assert actual.collect()[0][Colname.quantity] == Decimal("222.031")
     assert actual.collect()[1][Colname.quantity] == Decimal("222.031")
     assert actual.collect()[2][Colname.quantity] == Decimal("222.030")
     assert actual.collect()[3][Colname.quantity] == Decimal("222.031")
+
+
+def test_special_quantity_rounding_when_two_energy_supplier_at_the_same_time(
+    spark: SparkSession,
+) -> None:
+    # Arrange
+    rows = [
+        {
+            Colname.observation_time: datetime(2022, 1, 1, 1, 0),
+            Colname.quantity: Decimal("222.03075"),
+            Colname.energy_supplier_id: "1",
+        },
+        {
+            Colname.observation_time: datetime(2022, 1, 1, 1, 15),
+            Colname.quantity: Decimal("222.03075"),
+            Colname.energy_supplier_id: "1",
+        },
+        {
+            Colname.observation_time: datetime(2022, 1, 1, 1, 30),
+            Colname.quantity: Decimal("222.03075"),
+            Colname.energy_supplier_id: "1",
+        },
+        {
+            Colname.observation_time: datetime(2022, 1, 1, 1, 45),
+            Colname.quantity: Decimal("222.03075"),
+            Colname.energy_supplier_id: "1",
+        },
+        {
+            Colname.observation_time: datetime(2022, 1, 1, 1, 0),
+            Colname.quantity: Decimal("222.03075"),
+            Colname.energy_supplier_id: "2",
+        },
+        {
+            Colname.observation_time: datetime(2022, 1, 1, 1, 15),
+            Colname.quantity: Decimal("222.03075"),
+            Colname.energy_supplier_id: "2",
+        },
+        {
+            Colname.observation_time: datetime(2022, 1, 1, 1, 30),
+            Colname.quantity: Decimal("222.03075"),
+            Colname.energy_supplier_id: "2",
+        },
+        {
+            Colname.observation_time: datetime(2022, 1, 1, 1, 45),
+            Colname.quantity: Decimal("222.03075"),
+            Colname.energy_supplier_id: "2",
+        },
+    ]
+    df = spark.createDataFrame(rows)
+
+    # Act
+    actual = special_quantity_rounding(df)
+    actual = actual.orderBy(
+        Colname.energy_supplier_id,
+        Colname.observation_time,
+    )
+
+    # Assert
+    # Energy supplier 1
+    assert actual.collect()[0][Colname.quantity] == Decimal("222.031")
+    assert actual.collect()[1][Colname.quantity] == Decimal("222.031")
+    assert actual.collect()[2][Colname.quantity] == Decimal("222.030")
+    assert actual.collect()[3][Colname.quantity] == Decimal("222.031")
+    # Energy supplier 2
+    assert actual.collect()[4][Colname.quantity] == Decimal("222.031")
+    assert actual.collect()[5][Colname.quantity] == Decimal("222.031")
+    assert actual.collect()[6][Colname.quantity] == Decimal("222.030")
+    assert actual.collect()[7][Colname.quantity] == Decimal("222.031")
