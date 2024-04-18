eba33793523a51c5d8d8a98f424e52a2f365d08e
Mads Magnus Due <xmadu@energinet.dk>
2024-04-18 12:56:14 +0200

    feat: ensure actors can access messag from prod (#1341)

---
+++ edi/terraform/env/prod_001/apimao-ebix-peekmessage_override.tf
@@ -0,0 +1,4 @@
+# Disabled Peek Message API in APIM
+module "apimao_ebix_peekmessage" {
+  count = 0
+}
---
+++ edi/terraform/env/prod_001/apimao-request-wholesale-settlement_override.tf
@@ -0,0 +1,4 @@
+# Disabled Request Wholesale Settlement API in APIM
+module "apimao_request_wholesale_settlement" {
+  count = 0
+}
