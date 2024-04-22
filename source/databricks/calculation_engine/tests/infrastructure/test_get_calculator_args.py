beccbac71c3d8a4d5d9bc0a4539db4275fae5144
Jonas Michelsen <jvm@energinet.dk>
2024-04-22 08:40:35 +0200

    Update test_get_calculator_args.py

--- source/databricks/calculation_engine/tests/infrastructure/test_get_calculator_args.py
+++ source/databricks/calculation_engine/tests/infrastructure/test_get_calculator_args.py
@@ -86,9 +86,7 @@ def job_environment_variables() -> dict:
         EnvironmentVariable.TENANT_ID.name: "550e8400-e29b-41d4-a716-446655440000",
         EnvironmentVariable.SPN_APP_ID.name: "some_spn_app_id",
         EnvironmentVariable.SPN_APP_SECRET.name: "some_spn_app_secret",
-        EnvironmentVariable.INTERSECTION_TIME.name: datetime.strftime(
-            "2023-02-01T23:00:00Z", "%Y-%m-%dT%H:%M:%SZ"
-        ),
+        EnvironmentVariable.INTERSECTION_TIME.name: "2023-02-01T23:00:00Z",
     }
 
 
