ebb6bcece02017120b525062c9fa03d857931b2f
Dan Stenrøjl <XDAST@energinet.dk>
2024-04-22 17:11:31 +0200

    Add test

--- source/dotnet/wholesale-api/Orchestrations.IntegrationTests/Functions/Calculation/CalculationOrchestrationTests.cs
+++ source/dotnet/wholesale-api/Orchestrations.IntegrationTests/Functions/Calculation/CalculationOrchestrationTests.cs
@@ -154,4 +154,92 @@ public class CalculationOrchestrationTests : IAsyncLifetime
         var wait = verifyServiceBusMessages.Wait(TimeSpan.FromMinutes(1));
         wait.Should().BeTrue();
     }
+
+    /// <summary>
+    /// Verify the job status monitor (loop) is working.
+    /// </summary>
+    [Fact]
+    public async Task MockJobStatus_WhenCallingStartCalculationEndPoint_OrchestrationCompletesWithExpectedHistory()
+    {
+        // Arrange
+        // => Http catch all
+        Fixture.MockServer
+            .CatchAll();
+
+        // => Databricks Jobs API
+        var jobId = Random.Shared.Next(1, 1000);
+        var runId = Random.Shared.Next(1000, 2000);
+        Fixture.MockServer
+            .MockJobsList(jobId)
+            .MockJobsGet(jobId)
+            .MockJobsRunNow(runId)
+            .MockJobsRunsGet(runId, "TERMINATED", "SUCCESS");
+
+        // => Databricks SQL Statement API
+        var chunkIndex = 0;
+        var statementId = Guid.NewGuid().ToString();
+        var path = "GetDatabricksDataPath";
+        Fixture.MockServer
+            .MockSqlStatements(statementId, chunkIndex)
+            .MockSqlStatementsResultChunks(statementId, chunkIndex, path)
+            .MockSqlStatementsResultStream(path);
+
+        // TODO: For test to be robust it must filter by some known value; otherwise a test could be receiving a message from another test and think it's successful.
+        var verifyServiceBusMessages = await Fixture.ServiceBusListenerMock
+            .WhenAny()
+            .VerifyCountAsync(1);
+
+        // Act
+        var dateAtMidnight = new LocalDate(2024, 5, 17)
+            .AtMidnight()
+            .InZoneStrictly(_dateTimeZone)
+            .ToDateTimeOffset();
+
+        var beforeCreated = DateTime.UtcNow;
+        using var actualResponse = await Fixture.AppHostManager.HttpClient.PostAsync(
+            "api/StartCalculation",
+            new StringContent(
+                JsonConvert.SerializeObject(new CalculationRequestDto(
+                CalculationType.Aggregation,
+                ["256", "512"],
+                dateAtMidnight,
+                dateAtMidnight.AddDays(2))),
+                Encoding.UTF8,
+                "application/json"));
+
+        // Assert
+        actualResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        var calculationId = await actualResponse.Content.ReadAsAsync<Guid>();
+
+        // => Verify activities was executed by searching the orchestration history
+        var filter = new OrchestrationStatusQueryCondition()
+        {
+            CreatedTimeFrom = beforeCreated,
+            RuntimeStatus =
+            [
+                OrchestrationRuntimeStatus.Running,
+                OrchestrationRuntimeStatus.Completed,
+            ],
+        };
+
+        // => If we only need to verify information in custom status we can do it using the instance we can get from 'ListInstancesAsync'
+        var queryResult = await Fixture.DurableClient.ListInstancesAsync(filter, CancellationToken.None);
+        var orchestration = queryResult.DurableOrchestrationState.Single();
+        var calculationMetadata = orchestration.CustomStatus.ToObject<CalculationMetadata>();
+        calculationMetadata!.Id.Should().Be(calculationId);
+
+        // => If we want to verify information in history or output, we must use 'GetStatusAsync'
+        var completeOrchestrationStatus = await Fixture.DurableClient.GetStatusAsync(orchestration.InstanceId, showHistory: true, showHistoryOutput: true);
+        var orderedHistoryEntries = completeOrchestrationStatus.History
+            .OrderBy(entry => entry["Timestamp"])
+            .ToList();
+        // => Just showing how we can verify the execution by looking in history. Instead of using the function app log, which is sketchy, we could use history.
+        orderedHistoryEntries
+            .First()
+            .Value<string>("FunctionName").Should().Be("Calculation");
+
+        // => Verify ServiceBus
+        var wait = verifyServiceBusMessages.Wait(TimeSpan.FromMinutes(1));
+        wait.Should().BeTrue();
+    }
 }
