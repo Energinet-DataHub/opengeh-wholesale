cd10d13f4a7d0c04074976bbfeaa739a5f80b9e8
Michael <52096428+michaelmajgaard@users.noreply.github.com>
2024-04-22 08:59:46 +0200

    chore: new auth core pack app settings (#1345)

--- grid-loss-imbalance-prices/terraform/main/app-webapi.tf
+++ grid-loss-imbalance-prices/terraform/main/app-webapi.tf
@@ -41,6 +41,11 @@ locals {
     "JwtBearerSettings:ExternalOpenIdUrl"      = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=frontend-open-id-url)"
     "JwtBearerSettings:InternalOpenIdUrl"      = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-open-id-url)"
     "JwtBearerSettings:BackendBffAppId"        = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-bff-app-id)"
+
+    "UserAuthentication:MitIdExternalMetadataAddress" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=mitid-frontend-open-id-url)"
+    "UserAuthentication:ExternalMetadataAddress"      = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=frontend-open-id-url)"
+    "UserAuthentication:InternalMetadataAddress"      = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-open-id-url)"
+    "UserAuthentication:BackendBffAppId"              = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-bff-app-id)"
     "DatabaseSettings:ConnectionString"        = local.MS_GRID_LOSS_IMBALANCE_PRICES_CONNECTION_STRING
   }
 }
