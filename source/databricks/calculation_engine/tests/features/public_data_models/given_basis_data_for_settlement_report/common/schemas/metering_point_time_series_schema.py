37dda1028f40c68bb6621238a25daf13f949a055
Alexander <122119488+energinet-ajw@users.noreply.github.com>
2024-04-23 15:06:34 +0200

    test: Added test of metering point time series view (#2099)
    * Updates
    
    * Update test_view_reader.py
    
    * Updates
    
    * Update view_reader.py
    
    * Updates
    
    * Update test_view_reader.py
    
    * Update test_view_reader.py
    
    * Updates
    
    * Updates
    
    * Update basis_data_metering_point_periods_factory.py
    
    * Updates
    
    * Update scenario_state.py
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Update test_metering_point_time_series_view.py
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * updates
    
    * Update ci-databricks.yml
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Update data_frame_utils.py
    
    * Updates
    
    * Create metering_point_time_series.csv
    
    * Update ci-databricks.yml
    
    * Update ci-databricks.yml
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Update csv_to_dataframe_parser.py
    
    * Update metering_point_time_series.csv
    
    * Update metering_point_time_series.csv
    
    * Update readme.md
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Uopdates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Update metering_point_time_series_schema.py
    
    * Updates

---
+++ source/databricks/calculation_engine/tests/features/public_data_models/given_basis_data_for_settlement_report/common/schemas/metering_point_time_series_schema.py
@@ -0,0 +1,55 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from pyspark.sql.types import (
+    StructField,
+    StringType,
+    TimestampType,
+    StructType,
+    ArrayType,
+    DecimalType,
+)
+
+from features.public_data_models.given_basis_data_for_settlement_report.common.column_names.settlement_report_colname import (
+    MeteringPointTimeSeriesColname,
+)
+from package.constants import TimeSeriesColname, MeteringPointPeriodColname
+
+element = StructType(
+    [
+        StructField(TimeSeriesColname.observation_time, TimestampType(), False),
+        StructField(TimeSeriesColname.quantity, DecimalType(18, 3), False),
+    ]
+)
+
+
+metering_point_time_series_schema = StructType(
+    [
+        StructField(MeteringPointPeriodColname.calculation_id, StringType(), False),
+        StructField(MeteringPointPeriodColname.metering_point_id, StringType(), False),
+        StructField(
+            MeteringPointPeriodColname.metering_point_type, StringType(), False
+        ),
+        StructField(MeteringPointPeriodColname.resolution, StringType(), False),
+        StructField(MeteringPointPeriodColname.grid_area, StringType(), False),
+        StructField(MeteringPointPeriodColname.energy_supplier_id, StringType(), True),
+        StructField(
+            MeteringPointTimeSeriesColname.observation_day, TimestampType(), False
+        ),
+        StructField(
+            MeteringPointTimeSeriesColname.quantities,
+            ArrayType(element, False),
+            False,
+        ),
+    ]
+)
---
+++ source/databricks/calculation_engine/tests/features/utils/dataframes/settlement_report/view_results_dataframe.py
@@ -0,0 +1,77 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from ast import literal_eval
+
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.functions import col, from_json
+from pyspark.sql.types import (
+    TimestampType,
+    ArrayType,
+)
+
+from features.public_data_models.given_basis_data_for_settlement_report.common.schemas.metering_point_time_series_schema import (
+    element,
+)
+
+
+def create_metering_point_periods_view(df: DataFrame, spark: SparkSession) -> DataFrame:
+
+    # Don't remove. Believed needed because this function is an argument to the setup function
+    # and therefore the following packages are not automatically included.
+    from package.constants import MeteringPointPeriodColname
+    from features.public_data_models.given_basis_data_for_settlement_report.common import (
+        metering_point_period_schema,
+    )
+
+    df = df.withColumn(
+        MeteringPointPeriodColname.from_date,
+        col(MeteringPointPeriodColname.from_date).cast(TimestampType()),
+    )
+    df = df.withColumn(
+        MeteringPointPeriodColname.to_date,
+        col(MeteringPointPeriodColname.to_date).cast(TimestampType()),
+    )
+
+    return spark.createDataFrame(df.rdd, metering_point_period_schema)
+
+
+def create_metering_point_time_series_view(
+    df: DataFrame, spark: SparkSession
+) -> DataFrame:
+
+    # Don't remove. Believed needed because this function is an argument to the setup function
+    # and therefore the following packages are not automatically included.
+    from features.public_data_models.given_basis_data_for_settlement_report.common import (
+        MeteringPointTimeSeriesColname,
+    )
+    from features.public_data_models.given_basis_data_for_settlement_report.common import (
+        metering_point_time_series_schema,
+    )
+
+    df = df.withColumn(
+        MeteringPointTimeSeriesColname.observation_day,
+        col(MeteringPointTimeSeriesColname.observation_day).cast(TimestampType()),
+    )
+
+    df = df.withColumn(
+        MeteringPointTimeSeriesColname.quantities,
+        from_json(col(MeteringPointTimeSeriesColname.quantities), ArrayType(element)),
+    )
+
+    return spark.createDataFrame(df.rdd, metering_point_time_series_schema)
+
+
+def _parse_qualities(qualities_str: str) -> list[dict]:
+    # Parse the string into a list of dictionaries
+    return literal_eval(qualities_str)
