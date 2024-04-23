3e27e84aa20ae0c49d85e93c7743d47b22e01b4c
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-04-23 12:18:16 +0200

    Update locals_override.tf (#1362)

--- dh2-bridge/terraform/env/test_001/locals_override.tf
+++ dh2-bridge/terraform/env/test_001/locals_override.tf
@@ -1,6 +1,6 @@
 locals {
   DH2_ENDPOINT                      = "https://b2b.te7.datahub.dk"
   DH2BRIDGE_CERTIFICATE_THUMBPRINT  = resource.azurerm_key_vault_certificate.dh2_certificate.thumbprint
-  DH2_BRIDGE_RECIPIENT_PARTY_GLN    = "45V000000000184K"
+  DH2_BRIDGE_RECIPIENT_PARTY_GLN    = "5790001330552"
   DH2_BRIDGE_SENDER_PARTY_GLN       = "45V000000000184K"
 }
