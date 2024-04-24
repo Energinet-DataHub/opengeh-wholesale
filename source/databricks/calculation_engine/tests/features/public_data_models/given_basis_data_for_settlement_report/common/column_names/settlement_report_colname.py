1b0b908a3e69a0b7eea8001a6be2ea790bcf7071
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-04-24 21:36:18 +0200

    ...

--- source/databricks/calculation_engine/tests/features/public_data_models/given_basis_data_for_settlement_report/common/column_names/settlement_report_colname.py
+++ source/databricks/calculation_engine/tests/features/public_data_models/given_basis_data_for_settlement_report/common/column_names/settlement_report_colname.py
@@ -11,9 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from package.constants import EnergyResultColumnNames, Colname
-
-
 class MeteringPointTimeSeriesColname:
     observation_day = "observation_day"
     quantities = "quantities"
