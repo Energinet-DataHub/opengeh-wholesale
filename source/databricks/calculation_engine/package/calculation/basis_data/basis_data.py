ad0682d875518d25df3df0b68878085fdb7e0328
Christian Hansen <cenh@halfspace.io>
2024-04-29 13:33:12 +0200

    cast to decimal type for basis data when extracting

--- source/databricks/calculation_engine/package/calculation/basis_data/basis_data.py
+++ source/databricks/calculation_engine/package/calculation/basis_data/basis_data.py
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pyspark.sql.functions as f
+import pyspark.sql.functions as F
+from pyspark.sql.types import DecimalType
 from pyspark.sql import DataFrame
 
 from package.calculation.preparation.data_structures.prepared_metering_point_time_series import (
@@ -29,31 +30,31 @@ def get_metering_point_periods_basis_data(
     metering_point_df: DataFrame,
 ) -> DataFrame:
     return metering_point_df.select(
-        f.lit(calculation_id).alias(MeteringPointPeriodColname.calculation_id),
-        f.col(Colname.metering_point_id).alias(
+        F.lit(calculation_id).alias(MeteringPointPeriodColname.calculation_id),
+        F.col(Colname.metering_point_id).alias(
             MeteringPointPeriodColname.metering_point_id
         ),
-        f.col(Colname.metering_point_type).alias(
+        F.col(Colname.metering_point_type).alias(
             MeteringPointPeriodColname.metering_point_type
         ),
-        f.col(Colname.settlement_method).alias(
+        F.col(Colname.settlement_method).alias(
             MeteringPointPeriodColname.settlement_method
         ),
-        f.col(Colname.grid_area).alias(MeteringPointPeriodColname.grid_area),
-        f.col(Colname.resolution).alias(MeteringPointPeriodColname.resolution),
-        f.col(Colname.from_grid_area).alias(MeteringPointPeriodColname.from_grid_area),
-        f.col(Colname.to_grid_area).alias(MeteringPointPeriodColname.to_grid_area),
-        f.col(Colname.parent_metering_point_id).alias(
+        F.col(Colname.grid_area).alias(MeteringPointPeriodColname.grid_area),
+        F.col(Colname.resolution).alias(MeteringPointPeriodColname.resolution),
+        F.col(Colname.from_grid_area).alias(MeteringPointPeriodColname.from_grid_area),
+        F.col(Colname.to_grid_area).alias(MeteringPointPeriodColname.to_grid_area),
+        F.col(Colname.parent_metering_point_id).alias(
             MeteringPointPeriodColname.parent_metering_point_id
         ),
-        f.col(Colname.energy_supplier_id).alias(
+        F.col(Colname.energy_supplier_id).alias(
             MeteringPointPeriodColname.energy_supplier_id
         ),
-        f.col(Colname.balance_responsible_id).alias(
+        F.col(Colname.balance_responsible_id).alias(
             MeteringPointPeriodColname.balance_responsible_id
         ),
-        f.col(Colname.from_date).alias(MeteringPointPeriodColname.from_date),
-        f.col(Colname.to_date).alias(MeteringPointPeriodColname.to_date),
+        F.col(Colname.from_date).alias(MeteringPointPeriodColname.from_date),
+        F.col(Colname.to_date).alias(MeteringPointPeriodColname.to_date),
     )
 
 
@@ -63,11 +64,11 @@ def get_time_series_points_basis_data(
     metering_point_time_series: PreparedMeteringPointTimeSeries,
 ) -> DataFrame:
     return metering_point_time_series.df.select(
-        f.lit(calculation_id).alias(TimeSeriesColname.calculation_id),
-        f.col(Colname.metering_point_id).alias(TimeSeriesColname.metering_point_id),
-        f.col(Colname.quantity).alias(TimeSeriesColname.quantity),
-        f.col(Colname.quality).alias(TimeSeriesColname.quality),
-        f.col(Colname.observation_time).alias(TimeSeriesColname.observation_time),
+        F.lit(calculation_id).alias(TimeSeriesColname.calculation_id),
+        F.col(Colname.metering_point_id).alias(TimeSeriesColname.metering_point_id),
+        F.col(Colname.quantity).alias(TimeSeriesColname.quantity).cast(DecimalType(18,3)),
+        F.col(Colname.quality).alias(TimeSeriesColname.quality),
+        F.col(Colname.observation_time).alias(TimeSeriesColname.observation_time),
     )
 
 
@@ -78,15 +79,15 @@ def get_charge_master_data_basis_data(
 ) -> DataFrame:
     if input_charges_container:
         return input_charges_container.charge_master_data._df.select(
-            f.lit(calculation_id).alias(ChargeMasterDataPeriodsColname.calculation_id),
-            f.col(Colname.charge_key).alias(ChargeMasterDataPeriodsColname.charge_key),
-            f.col(Colname.charge_code).alias(ChargeMasterDataPeriodsColname.charge_code),
-            f.col(Colname.charge_type).alias(ChargeMasterDataPeriodsColname.charge_type),
-            f.col(Colname.charge_owner).alias(ChargeMasterDataPeriodsColname.charge_owner_id),
-            f.col(Colname.resolution).alias(ChargeMasterDataPeriodsColname.resolution),
-            f.col(Colname.charge_tax).alias(ChargeMasterDataPeriodsColname.is_tax),
-            f.col(Colname.from_date).alias(ChargeMasterDataPeriodsColname.from_date),
-            f.col(Colname.to_date).alias(ChargeMasterDataPeriodsColname.to_date),
+            F.lit(calculation_id).alias(ChargeMasterDataPeriodsColname.calculation_id),
+            F.col(Colname.charge_key).alias(ChargeMasterDataPeriodsColname.charge_key),
+            F.col(Colname.charge_code).alias(ChargeMasterDataPeriodsColname.charge_code),
+            F.col(Colname.charge_type).alias(ChargeMasterDataPeriodsColname.charge_type),
+            F.col(Colname.charge_owner).alias(ChargeMasterDataPeriodsColname.charge_owner_id),
+            F.col(Colname.resolution).alias(ChargeMasterDataPeriodsColname.resolution),
+            F.col(Colname.charge_tax).alias(ChargeMasterDataPeriodsColname.is_tax),
+            F.col(Colname.from_date).alias(ChargeMasterDataPeriodsColname.from_date),
+            F.col(Colname.to_date).alias(ChargeMasterDataPeriodsColname.to_date),
         )
     else:
         return None
@@ -99,13 +100,13 @@ def get_charge_prices_basis_data(
 ) -> DataFrame:
     if input_charges_container:
         return input_charges_container.charge_prices._df.select(
-            f.lit(calculation_id).alias(ChargePricePointsColname.calculation_id),
-            f.col(Colname.charge_key).alias(ChargePricePointsColname.charge_key),
-            f.col(Colname.charge_code).alias(ChargePricePointsColname.charge_code),
-            f.col(Colname.charge_type).alias(ChargePricePointsColname.charge_type),
-            f.col(Colname.charge_owner).alias(ChargePricePointsColname.charge_owner_id),
-            f.col(Colname.charge_price).alias(ChargePricePointsColname.charge_price),
-            f.col(Colname.charge_time).alias(ChargePricePointsColname.charge_time),
+            F.lit(calculation_id).alias(ChargePricePointsColname.calculation_id),
+            F.col(Colname.charge_key).alias(ChargePricePointsColname.charge_key),
+            F.col(Colname.charge_code).alias(ChargePricePointsColname.charge_code),
+            F.col(Colname.charge_type).alias(ChargePricePointsColname.charge_type),
+            F.col(Colname.charge_owner).alias(ChargePricePointsColname.charge_owner_id),
+            F.col(Colname.charge_price).alias(ChargePricePointsColname.charge_price),
+            F.col(Colname.charge_time).alias(ChargePricePointsColname.charge_time),
         )
     else:
         return None
@@ -118,15 +119,15 @@ def get_charge_links_basis_data(
 ) -> DataFrame:
     if input_charges_container:
         return input_charges_container.charge_links.select(
-            f.lit(calculation_id).alias(ChargeLinkPeriodsColname.calculation_id),
-            f.col(Colname.charge_key).alias(ChargeLinkPeriodsColname.charge_key),
-            f.col(Colname.charge_code).alias(ChargeLinkPeriodsColname.charge_code),
-            f.col(Colname.charge_type).alias(ChargeLinkPeriodsColname.charge_type),
-            f.col(Colname.charge_owner).alias(ChargeLinkPeriodsColname.charge_owner_id),
-            f.col(Colname.metering_point_id).alias(ChargeLinkPeriodsColname.metering_point_id),
-            f.col(Colname.quantity).alias(ChargeLinkPeriodsColname.quantity),
-            f.col(Colname.from_date).alias(ChargeLinkPeriodsColname.from_date),
-            f.col(Colname.to_date).alias(ChargeLinkPeriodsColname.to_date),
+            F.lit(calculation_id).alias(ChargeLinkPeriodsColname.calculation_id),
+            F.col(Colname.charge_key).alias(ChargeLinkPeriodsColname.charge_key),
+            F.col(Colname.charge_code).alias(ChargeLinkPeriodsColname.charge_code),
+            F.col(Colname.charge_type).alias(ChargeLinkPeriodsColname.charge_type),
+            F.col(Colname.charge_owner).alias(ChargeLinkPeriodsColname.charge_owner_id),
+            F.col(Colname.metering_point_id).alias(ChargeLinkPeriodsColname.metering_point_id),
+            F.col(Colname.quantity).alias(ChargeLinkPeriodsColname.quantity),
+            F.col(Colname.from_date).alias(ChargeLinkPeriodsColname.from_date),
+            F.col(Colname.to_date).alias(ChargeLinkPeriodsColname.to_date),
         )
     else:
         return None
