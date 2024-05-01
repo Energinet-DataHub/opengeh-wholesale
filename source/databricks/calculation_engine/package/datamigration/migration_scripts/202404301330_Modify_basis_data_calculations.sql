ee2f8973675da4e569d5c78ae127279b302affb6
Alexander <alexanderharrow@hotmail.com>
2024-05-01 21:05:59 +0200

    Update 202404301330_Modify_basis_data_calculations.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202404301330_Modify_basis_data_calculations.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202404301330_Modify_basis_data_calculations.sql
@@ -1,10 +1,3 @@
 ALTER TABLE {BASIS_DATA_DATABASE_NAME}.calculations
 DROP COLUMN created_time
-USING DELTA
-TBLPROPERTIES (
-    delta.constraints.created_by_user_id_chk = "LENGTH ( created_by_user_id ) = 36"
-)
--- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used.
--- In the production it is set to empty and the respective location is used. This means the production tables won't be deleted if the schema is.
-{TEST}LOCATION '{CONTAINER_PATH}/{BASIS_DATA_FOLDER}/calculations'
 GO
