51f8b204ef302ba942418ec46862ef8b72447c64
Alexander <alexanderharrow@hotmail.com>
2024-05-02 08:41:40 +0200

    Update 202404301330_Add_basis_data_calculations.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202404301330_Add_basis_data_calculations.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202404301330_Add_basis_data_calculations.sql
@@ -1,3 +1,6 @@
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.calculations SET TBLPROPERTIES ('delta.columnMapping.mode' = 'name')
+GO
+
 ALTER TABLE {BASIS_DATA_DATABASE_NAME}.calculations
 DROP COLUMN created_time
 GO
