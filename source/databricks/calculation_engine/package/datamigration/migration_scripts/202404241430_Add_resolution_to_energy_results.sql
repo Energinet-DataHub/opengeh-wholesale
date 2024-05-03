604bd5649a95c11da7d046a0222c364196f0388c
Jonas Michelsen <jvm@energinet.dk>
2024-05-03 14:04:35 +0200

    Update 202404241430_Add_resolution_to_energy_results.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202404241430_Add_resolution_to_energy_results.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202404241430_Add_resolution_to_energy_results.sql
@@ -16,5 +16,5 @@ ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     DROP CONSTRAINT IF EXISTS resolution_chk
 GO
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
-    ADD CONSTRAINT resolution_chk CHECK (resolution IN ('PT15M', 'PT1H'))
+    ADD CONSTRAINT resolution_chk CHECK (resolution IN ('PT15M', 'PT1H', 'PT1D'))
 GO
