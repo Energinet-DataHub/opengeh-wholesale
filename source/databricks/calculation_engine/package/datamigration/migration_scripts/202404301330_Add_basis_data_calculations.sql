bbb357e73106f8939cb6c25caf4a4598f3492573
Jonas Michelsen <jvm@energinet.dk>
2024-05-03 13:07:09 +0200

    Update 202404301330_Add_basis_data_calculations.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202404301330_Add_basis_data_calculations.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202404301330_Add_basis_data_calculations.sql
@@ -18,6 +18,7 @@ TBLPROPERTIES (
     delta.constraints.calculation_id_chk = "LENGTH ( calculation_id ) = 36",
     delta.constraints.calculation_type_chk = "calculation_type IN ( 'BalanceFixing' , 'Aggregation' , 'WholesaleFixing' , 'FirstCorrectionSettlement' , 'SecondCorrectionSettlement' , 'ThirdCorrectionSettlement' )"
 )
+-- test
 -- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used.
 -- In the production it is set to empty and the respective location is used. This means the production tables won't be deleted if the schema is.
 {TEST}LOCATION '{CONTAINER_PATH}/{BASIS_DATA_FOLDER}/calculations'
