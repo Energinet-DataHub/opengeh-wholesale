d2e1a38b79788563cd60da930b6f7f549906bcb9
Christian Hansen <120550591+cenh-halfspace@users.noreply.github.com>
2024-05-04 12:53:23 +0200

    Update source/databricks/calculation_engine/tests/calculation/__init__.py
    Co-authored-by: BjarkeMeier <BjarkeMeier@users.noreply.github.com>

--- source/databricks/calculation_engine/tests/calculation/__init__.py
+++ source/databricks/calculation_engine/tests/calculation/__init__.py
@@ -11,6 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-By having a __init__.py in this root directory, we can use the test explorer in VS Code.
-"""
