74ad7968a1cb0b94278fc1291ee6c50d9a4a6cad
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-05-06 21:21:03 +0200

    fix tests

--- source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_metering_point_periods.sql
+++ source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_metering_point_periods.sql
@@ -4,8 +4,6 @@ SELECT calculation_id,
        from_date,
        to_date,
        grid_area_code,
-       from_grid_area_code,
-       to_grid_area_code,
        metering_point_type,
        settlement_method,
        energy_supplier_id
