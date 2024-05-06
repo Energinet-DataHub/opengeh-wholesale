43136ad2c43a556d5e980c67e658a2d8b07bbf04
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-05-06 11:13:53 +0200

    Add token mock.

--- source/dotnet/wholesale-api/Orchestrations.IntegrationTests/Functions/SettlementReports/SettlementReportOrchestratorTests.cs
+++ source/dotnet/wholesale-api/Orchestrations.IntegrationTests/Functions/SettlementReports/SettlementReportOrchestratorTests.cs
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.IdentityModel.Tokens.Jwt;
 using System.Net;
 using System.Net.Http.Json;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.SettlementReports_v2.Models;
 using Energinet.DataHub.Wholesale.Common.Interfaces.Models;
@@ -21,6 +24,7 @@ using Energinet.DataHub.Wholesale.Orchestrations.Functions.SettlementReports.Mod
 using Energinet.DataHub.Wholesale.Orchestrations.IntegrationTests.DurableTask;
 using Energinet.DataHub.Wholesale.Orchestrations.IntegrationTests.Fixtures;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Xunit.Abstractions;
 using GridAreaCode = Energinet.DataHub.Wholesale.CalculationResults.Interfaces.SettlementReports_v2.Models.GridAreaCode;
@@ -62,7 +66,6 @@ public class SettlementReportOrchestratorTests : IAsyncLifetime
     /// Verifies that:
     ///  - The orchestration can complete a full run.
     ///  - Every activity is executed once and in correct order.
-    ///  - A service bus message is sent as expected.
     /// </summary>
     [Fact]
     public async Task MockExternalDependencies_WhenCallingDurableFunctionEndPoint_OrchestrationCompletesWithExpectedServiceBusMessage()
@@ -77,12 +80,16 @@ public class SettlementReportOrchestratorTests : IAsyncLifetime
                 null));
 
         // Act
-        using var actualResponse = await Fixture.AppHostManager.HttpClient.PostAsync(
-            "api/RequestSettlementReport",
-            new StringContent(
-                JsonConvert.SerializeObject(settlementReportRequest),
-                Encoding.UTF8,
-                "application/json"));
+        using var request = new HttpRequestMessage(HttpMethod.Post, "api/RequestSettlementReport");
+        request.Content = new StringContent(
+            JsonConvert.SerializeObject(settlementReportRequest),
+            Encoding.UTF8,
+            "application/json");
+
+        var token = CreateFakeInternalToken();
+        request.Headers.Add("Authorization", $"Bearer {token}");
+
+        var actualResponse = await Fixture.AppHostManager.HttpClient.SendAsync(request);
 
         // Assert
         Assert.Equal(HttpStatusCode.OK, actualResponse.StatusCode);
@@ -95,5 +102,36 @@ public class SettlementReportOrchestratorTests : IAsyncLifetime
             TimeSpan.FromMinutes(10));
 
         Assert.Equal(OrchestrationRuntimeStatus.Completed, completeOrchestrationStatus.RuntimeStatus);
+    }
+
+    /// <summary>
+    /// Create a fake token which is used by the 'UserMiddleware' to create
+    /// the 'UserContext'.
+    /// </summary>
+    // TODO: Move this somewhere. Why does it work at all?
+    private static string CreateFakeInternalToken()
+    {
+        var kid = "049B6F7F-F5A5-4D2C-A407-C4CD170A759F";
+        RsaSecurityKey testKey = new(RSA.Create()) { KeyId = kid };
+
+        var issuer = "https://test.datahub.dk";
+        var audience = Guid.Empty.ToString();
+        var validFrom = DateTime.UtcNow;
+        var validTo = DateTime.UtcNow.AddMinutes(15);
+
+        var userClaim = new Claim(JwtRegisteredClaimNames.Sub, "A1AAB954-136A-444A-94BD-E4B615CA4A78");
+        var actorClaim = new Claim(JwtRegisteredClaimNames.Azp, "A1DEA55A-3507-4777-8CF3-F425A6EC2094");
+
+        var internalToken = new JwtSecurityToken(
+            issuer,
+            audience,
+            new[] { userClaim, actorClaim },
+            validFrom,
+            validTo,
+            new SigningCredentials(testKey, SecurityAlgorithms.RsaSha256));
+
+        var handler = new JwtSecurityTokenHandler();
+        var writtenToken = handler.WriteToken(internalToken);
+        return writtenToken;
     }
 }
