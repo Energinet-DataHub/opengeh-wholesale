9c601fa08ad3109ceb655d7fc396426ceb1d32dd
Jonas Michelsen <jvm@energinet.dk>
2024-05-06 15:23:32 +0200

    Update 202405061500_Alter_basis_data_calculations.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202405061500_Alter_basis_data_calculations.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405061500_Alter_basis_data_calculations.sql
@@ -1,3 +1,8 @@
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.calculations SET TBLPROPERTIES (
+    'delta.columnMapping.mode' = 'name',
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '5')
+GO
 ALTER TABLE {BASIS_DATA_DATABASE_NAME}.calculations
     DROP COLUMN created_time
 GO
