8520374495934d3eb51ef0950a64cacc808dc49b
Jonas Michelsen <jvm@energinet.dk>
2024-05-06 15:12:53 +0200

    calculation delta

---
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405061500_Alter_basis_data_calculations.sql
@@ -0,0 +1,9 @@
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.calculations
+    DROP COLUMN created_time
+GO
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.calculations
+    DROP CONSTRAINT IF EXISTS created_by_user_id_chk
+GO
+ALTER TABLE {BASIS_DATA_DATABASE_NAME}.calculations
+    ADD CONSTRAINT created_by_user_id_chk CHECK (LENGTH(created_by_user_id) = 36)
+GO
