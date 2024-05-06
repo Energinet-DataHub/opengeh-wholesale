22bef6f08670c716ea32c0ece6fc1b2328ba4f67
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2024-05-06 21:04:58 +0200

    DH2-Bridge: MPSJ step 2 (#1454)

--- dh2-bridge/terraform/main/func-grid-loss-event-receiver.tf
+++ dh2-bridge/terraform/main/func-grid-loss-event-receiver.tf
@@ -1,49 +1,48 @@
-# module "func_entrypoint_grid_loss_event_receiver" {
-#   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=v13"
+module "func_entrypoint_grid_loss_event_receiver" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app-elastic?ref=v14"
 
-#   name                                   = "grid-loss-event-receiver"
-#   project_name                           = var.domain_name_short
-#   environment_short                      = var.environment_short
-#   environment_instance                   = var.environment_instance
-#   resource_group_name                    = azurerm_resource_group.this.name
-#   location                               = azurerm_resource_group.this.location
-#   vnet_integration_subnet_id             = data.azurerm_key_vault_secret.snet_vnet_integration_id.value
-#   private_endpoint_subnet_id             = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
-#   app_service_plan_id                    = module.func_service_plan.id
-#   application_insights_connection_string = data.azurerm_key_vault_secret.appi_shared_connection_string.value
-#   ip_restrictions                        = var.ip_restrictions
-#   scm_ip_restrictions                    = var.ip_restrictions
-#   always_on                              = true
-#   health_check_path                      = "/api/monitor/ready"
-#   health_check_alert = {
-#     action_group_id = data.azurerm_key_vault_secret.primary_action_group_id.value
-#     enabled         = var.enable_health_check_alerts
-#   }
-#   dotnet_framework_version    = "v8.0"
-#   use_dotnet_isolated_runtime = true
-#   role_assignments = [
-#     {
-#       resource_id          = module.storage_dh2_bridge_documents.id
-#       role_definition_name = "Storage Blob Data Contributor"
-#     },
-#     {
-#       resource_id          = data.azurerm_key_vault.kv_shared_resources.id
-#       role_definition_name = "Key Vault Secrets User"
-#     }
-#   ]
-#   app_settings = local.default_event_receiver_app_settings
-# }
+  name                                   = "grid-loss-event-receiver"
+  project_name                           = var.domain_name_short
+  environment_short                      = var.environment_short
+  environment_instance                   = var.environment_instance
+  resource_group_name                    = azurerm_resource_group.this.name
+  location                               = azurerm_resource_group.this.location
+  vnet_integration_subnet_id             = data.azurerm_key_vault_secret.snet_vnet_integration_id.value
+  private_endpoint_subnet_id             = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
+  app_service_plan_id                    = module.func_service_plan.id
+  application_insights_connection_string = data.azurerm_key_vault_secret.appi_shared_connection_string.value
+  ip_restrictions                        = var.ip_restrictions
+  scm_ip_restrictions                    = var.ip_restrictions
+  health_check_path                      = "/api/monitor/ready"
+  health_check_alert = {
+    action_group_id = data.azurerm_key_vault_secret.primary_action_group_id.value
+    enabled         = var.enable_health_check_alerts
+  }
+  dotnet_framework_version    = "v8.0"
+  use_dotnet_isolated_runtime = true
+  role_assignments = [
+    {
+      resource_id          = module.storage_dh2_bridge_documents.id
+      role_definition_name = "Storage Blob Data Contributor"
+    },
+    {
+      resource_id          = data.azurerm_key_vault.kv_shared_resources.id
+      role_definition_name = "Key Vault Secrets User"
+    }
+  ]
+  app_settings = local.default_event_receiver_app_settings
+}
 
-# locals {
-#   default_event_receiver_app_settings = {
-#     "BlobStorageSettings:AccountUri"                        = local.DH2_BRIDGE_DOCUMENT_STORAGE_ACCOUNT_URI
-#     "BlobStorageSettings:ContainerName"                     = local.DH2_BRIDGE_DOCUMENT_STORAGE_CONTAINER_NAME
-#     "DatabaseSettings:ConnectionString"                     = local.MS_DH2_BRIDGE_CONNECTION_STRING
-#     "ConsumeServiceBusSettings:ConnectionString"            = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-listen-connection-string)"
-#     "ConsumeServiceBusSettings:HealthCheckConnectionString" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-manage-connection-string)"
-#     "ConsumeServiceBusSettings:SharedIntegrationEventTopic" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sbt-shres-integrationevent-received-name)"
-#     "ConsumeServiceBusSettings:GridLossSubscription"        = module.sbtsub_dh2_bridge_event_listener.name
-#     "ConverterSettings:RecipientPartyGLN"                   = local.DH2_BRIDGE_RECIPIENT_PARTY_GLN
-#     "ConverterSettings:SenderPartyGLN"                      = local.DH2_BRIDGE_SENDER_PARTY_GLN
-#   }
-# }
+locals {
+  default_event_receiver_app_settings = {
+    "BlobStorageSettings:AccountUri"                        = local.DH2_BRIDGE_DOCUMENT_STORAGE_ACCOUNT_URI
+    "BlobStorageSettings:ContainerName"                     = local.DH2_BRIDGE_DOCUMENT_STORAGE_CONTAINER_NAME
+    "DatabaseSettings:ConnectionString"                     = local.MS_DH2_BRIDGE_CONNECTION_STRING
+    "ConsumeServiceBusSettings:ConnectionString"            = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-listen-connection-string)"
+    "ConsumeServiceBusSettings:HealthCheckConnectionString" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-manage-connection-string)"
+    "ConsumeServiceBusSettings:SharedIntegrationEventTopic" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sbt-shres-integrationevent-received-name)"
+    "ConsumeServiceBusSettings:GridLossSubscription"        = module.sbtsub_dh2_bridge_event_listener.name
+    "ConverterSettings:RecipientPartyGLN"                   = local.DH2_BRIDGE_RECIPIENT_PARTY_GLN
+    "ConverterSettings:SenderPartyGLN"                      = local.DH2_BRIDGE_SENDER_PARTY_GLN
+  }
+}
--- dh2-bridge/terraform/main/mssqldb-applicationaccess.tf
+++ dh2-bridge/terraform/main/mssqldb-applicationaccess.tf
@@ -1,18 +1,18 @@
-# module "mssql_database_application_access" {
-#   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database-application-access?ref=v13"
+module "mssql_database_application_access" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database-application-access?ref=v14"
 
-#   sql_server_name = data.azurerm_mssql_server.mssqlsrv.name
-#   database_name   = module.mssqldb_dh2_bridge.name
+  sql_server_name = data.azurerm_mssql_server.mssqlsrv.name
+  database_name   = module.mssqldb_dh2_bridge.name
 
-#   application_hosts_names = [
-#     module.func_entrypoint_grid_loss_sender.name,
-#     module.func_entrypoint_grid_loss_peek.name,
-#     module.func_entrypoint_grid_loss_event_receiver.name,
-#   ]
+  application_hosts_names = [
+    module.func_entrypoint_grid_loss_sender.name,
+    module.func_entrypoint_grid_loss_peek.name,
+    module.func_entrypoint_grid_loss_event_receiver.name,
+  ]
 
-#   depends_on = [
-#     module.func_entrypoint_grid_loss_event_receiver.name,
-#     module.func_entrypoint_grid_loss_sender.name,
-#     module.func_entrypoint_grid_loss_peek.name,
-#   ]
-# }
+  depends_on = [
+    module.func_entrypoint_grid_loss_event_receiver.name,
+    module.func_entrypoint_grid_loss_sender.name,
+    module.func_entrypoint_grid_loss_peek.name,
+  ]
+}
