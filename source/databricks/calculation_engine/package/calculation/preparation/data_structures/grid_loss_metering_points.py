51111ae1af06cb14bbca0030302fa47188a6e26d
Christian Hansen <cenh@halfspace.io>
2024-05-08 11:57:38 +0200

    Grid loss meterings points

---
+++ source/databricks/calculation_engine/package/calculation/preparation/data_structures/grid_loss_metering_points.py
@@ -0,0 +1,47 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pyspark.sql.types as t
+from pyspark.sql import DataFrame
+
+from package.codelists import ChargeType
+from package.common import DataFrameWrapper
+from package.constants import Colname
+
+
+class GridLossMeteringPoints(DataFrameWrapper):
+    """
+    Represents the charge prices.
+    """
+
+    def __init__(self, df: DataFrame):
+        super().__init__(
+            df,
+            grid_loss_metering_points_schema,
+            # We ignore_nullability because it has turned out to be too hard and even possibly
+            # introducing more errors than solving in order to stay in exact sync with the
+            # logically correct schema.
+            ignore_nullability=True,
+            ignore_decimal_scale=True,
+            ignore_decimal_precision=True,
+        )
+
+
+# The nullability and decimal types are not precisely representative of the actual data frame schema at runtime,
+# See comments to the `assert_schema()` invocation.
+grid_loss_metering_points_schema = t.StructType(
+    [
+        t.StructField(Colname.metering_point_id, t.StringType(), False),
+    ]
+)
