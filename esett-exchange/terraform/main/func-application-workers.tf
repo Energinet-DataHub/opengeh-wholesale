7a79c71edaf0b3d1c6d97338631818c8c3363e38
Rasmus Leth Friis <116742936+rf-halfspace@users.noreply.github.com>
2024-05-13 16:46:09 +0200

    mpsj-step2-fix (#1478)
    * mpsj-step2-fix
    
    * Update func-exchange-event-receiver.tf

--- esett-exchange/terraform/main/func-application-workers.tf
+++ esett-exchange/terraform/main/func-application-workers.tf
@@ -33,7 +33,8 @@ locals {
   default_application_workers_app_settings = {
     "DatabaseSettings:ConnectionString"                     = local.MS_ESETT_EXCHANGE_CONNECTION_STRING
     "PublishServiceBusSettings:ConnectionString"            = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-send-connection-string)"
-    "PublishServiceBusSettings:HealthCheckConnectionString" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-manage-connection-string)"
+    "PublishServiceBusSettings:HealthCheckConnectionString" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-transceiver-connection-string)"
     "PublishServiceBusSettings:SharedIntegrationEventTopic" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sbt-shres-integrationevent-received-name)"
+    "ConsumeServiceBusSettings:EsettExchangeSubscription"   = module.sbtsub_esett_exchange_event_listener.name
   }
 }
