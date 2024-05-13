4bd25a1aea9bfc89290fcf9453b1fbb008baaa8d
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-05-13 20:55:32 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_metering_point_time_series.sql
+++ source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_metering_point_time_series.sql
@@ -14,5 +14,5 @@ FROM {BASIS_DATA_DATABASE_NAME}.metering_point_periods AS m
              .calculation_id
 WHERE t.observation_time >= m.from_date
   AND (m.to_date IS NULL OR t.observation_time < m.to_date)
-GROUP BY m.calculation_id, m.metering_point_id, m.metering_point_type, observation_day, m.resolution, m.grid_area_code, m.energy_supplier_id
+GROUP BY c.calculation_id, m.metering_point_id, m.metering_point_type, observation_day, m.resolution, m.grid_area_code, m.energy_supplier_id
 ORDER BY observation_day
