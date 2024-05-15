f4e7b32ec4f4898b47d3c436bdebd0e061d61067
Alexander <alexanderharrow@hotmail.com>
2024-05-15 12:56:51 +0200

    Upodates

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202405131200_Modify_settlement_report_metering_point_periods_v1_view.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405131200_Modify_settlement_report_metering_point_periods_v1_view.sql
@@ -2,16 +2,16 @@ DROP VIEW IF EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.metering_point_periods_v1
 GO
 
 CREATE VIEW IF NOT EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.metering_point_periods_v1
-      (calculation_id,
-    calculation_type COMMENT /* 'BalanceFixing' | 'Aggregation' | 'WholesaleFixing' | 'FirstCorrectionSettlement' | 'SecondCorrectionSettlement' | 'ThirdCorrectionSettlement' */,
+     (calculation_id,
+    calculation_type COMMENT 'BalanceFixing | Aggregation | WholesaleFixing | FirstCorrectionSettlement | SecondCorrectionSettlement | ThirdCorrectionSettlement',
     metering_point_id,
     from_date,
     to_date COMMENT '<value> | NULL',
     grid_area_code,
     from_grid_area_code COMMENT '<value> | NULL',
     to_grid_area_code COMMENT '<value> | NULL',
-    metering_point_type COMMENT /* 'production' | 'consumption' | 'exchange' | 've_production' | 've_consumption' | 've_exchange' | 've_total_consumption' | 've_non_profiled_consumption' | 've_flex_consumption' */,
-    settlement_method COMMENT /* 'non_profiled' | 'flex' | NULL */,
+    metering_point_type COMMENT 'production | consumption | exchange | ve_production | ve_consumption | ve_exchange | ve_total_consumption | ve_non_profiled_consumption | ve_flex_consumption',
+    settlement_method COMMENT 'non_profiled | flex | NULL',
     energy_supplier_id COMMENT '<value> | NULL')
 AS
 SELECT c.calculation_id,
