db9947c6d62fbf14091eba72b8a2094bdbb46ab6
Jeppe M <jmw@energinet.dk>
2024-05-15 17:34:36 +0200

    Migrations/adjust: change to bigger plan, but fewer burst instance (#1495)

--- migrations/terraform/main/service-plans.tf
+++ migrations/terraform/main/service-plans.tf
@@ -7,8 +7,8 @@ module "func_service_plan" {
   environment_instance           = var.environment_instance
   resource_group_name            = azurerm_resource_group.this.name
   location                       = azurerm_resource_group.this.location
-  sku_name                       = "EP1"
-  maximum_elastic_worker_count   = 20
+  sku_name                       = "EP2"
+  maximum_elastic_worker_count   = 5
   monitor_alerts_action_group_id = module.monitor_action_group.id
   cpu_alert_information = {
     alerts_enabled = true
