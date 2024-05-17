c7b9db0d90eca55417b782cb30818624c6ada6e6
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-05-17 16:09:04 +0200

    add files

---
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405171200_Add_settlement_report_charge_prices_v1_view.sql
@@ -0,0 +1,22 @@
+CREATE VIEW {SETTLEMENT_REPORT_DATABASE_NAME}.charge_prices_v1 as
+SELECT
+  c.calculation_id,
+  FIRST_VALUE(c.calculation_type) as calculation_type,
+  FIRST_VALUE(cm.charge_type) as charge_type,
+  FIRST_VALUE(cm.charge_code) as charge_code,
+  FIRST_VALUE(cm.charge_owner_id) as charge_owner_id,
+  FIRST_VALUE(cm.resolution) as resolution,
+  FIRST_VALUE(cm.is_tax) as is_tax,
+  TO_UTC_TIMESTAMP(DATE_TRUNC('day', FROM_UTC_TIMESTAMP(cp.charge_time, 'Europe/Copenhagen')),'Europe/Copenhagen') AS start_date_time,
+  ARRAY_SORT(ARRAY_AGG(struct(cp.charge_time, cp.charge_price))) AS prices,
+  es_ga.grid_area_code,
+  es_ga.energy_supplier_id
+FROM basis_data.charge_masterdata_periods as cm
+INNER JOIN basis_data.charge_price_points as cp ON cm.calculation_id = cp.calculation_id AND cm.charge_key = cp.charge_key
+INNER JOIN (
+  SELECT distinct mp.calculation_id, charge_key, energy_supplier_id, grid_area_code FROM basis_data.charge_link_periods AS cl
+  INNER JOIN basis_data.metering_point_periods AS mp ON mp.calculation_id = cl.calculation_id AND mp.metering_point_id = cl.metering_point_id
+) AS es_ga ON cm.calculation_id = es_ga.calculation_id AND cm.charge_key = es_ga.charge_key
+INNER JOIN basis_data.calculations AS c ON cm.calculation_id = c.calculation_id
+WHERE cm.calculation_id = "46ec14d2-4693-4ef9-b66f-edda23824b68"
+GROUP BY c.calculation_id, cm.charge_key, es_ga.grid_area_code, es_ga.energy_supplier_id, DATE_TRUNC('day', FROM_UTC_TIMESTAMP(cp.charge_time, 'Europe/Copenhagen'))
