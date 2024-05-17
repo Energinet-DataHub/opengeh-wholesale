220fa402953ef29f16ecb589b42f76ffbb1a9836
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-05-17 16:11:24 +0200

    forgot schema

--- source/databricks/calculation_engine/package/datamigration/schema_config.py
+++ source/databricks/calculation_engine/package/datamigration/schema_config.py
@@ -104,6 +104,7 @@ schema_config = [
             View(name=paths.METERING_POINT_PERIODS_SETTLEMENT_REPORT_VIEW_NAME_V1),
             View(name=paths.METERING_POINT_TIME_SERIES_SETTLEMENT_REPORT_VIEW_NAME_V1),
             View(name=paths.ENERGY_RESULTS_SETTLEMENT_REPORT_VIEW_NAME_V1),
+            View(name=paths.CHARGE_LINK_PERIODS_SETTLEMENT_REPORT_VIEW_NAME_V1),
         ],
     ),
 ]
