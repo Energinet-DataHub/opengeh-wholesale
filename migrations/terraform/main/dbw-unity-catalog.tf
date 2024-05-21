0bbc1860cff0bab5efd9f3e1e64b061cf29d4584
Jan Duelund <jdd@energinet.dk>
2024-05-21 10:30:25 +0200

    Update dbw-unity-catalog.tf (#1521)

--- migrations/terraform/main/dbw-unity-catalog.tf
+++ migrations/terraform/main/dbw-unity-catalog.tf
@@ -64,44 +64,6 @@ resource "databricks_schema" "migrations_gold" {
   depends_on = [module.dbw, module.kvs_databricks_dbw_workspace_token, databricks_catalog_workspace_binding.shared]
 }
 
-resource "databricks_external_location" "eloverblik_storage" {
-  provider        = databricks.dbw
-  name            = "${azurerm_storage_container.eloverblik.name}_${module.st_migrations.name}"
-  url             = "abfss://${azurerm_storage_container.eloverblik.name}@${module.st_migrations.name}.dfs.core.windows.net/"
-  credential_name = data.azurerm_key_vault_secret.unity_storage_credential_id.value
-  comment         = "Managed by TF"
-  depends_on      = [module.dbw, databricks_catalog_workspace_binding.shared, module.st_migrations]
-}
-
-resource "databricks_schema" "eloverblik" {
-  provider     = databricks.dbw
-  catalog_name = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
-  name         = "eloverblik"
-  comment      = "Eloverblik Schema"
-  storage_root = databricks_external_location.eloverblik_storage.url
-
-  depends_on = [module.dbw, module.kvs_databricks_dbw_workspace_token, databricks_catalog_workspace_binding.shared]
-}
-
-resource "databricks_external_location" "migrations_storage" {
-  provider        = databricks.dbw
-  name            = "${azurerm_storage_container.schema_migration.name}_${module.st_migrations.name}"
-  url             = "abfss://${azurerm_storage_container.schema_migration.name}@${module.st_migrations.name}.dfs.core.windows.net/"
-  credential_name = data.azurerm_key_vault_secret.unity_storage_credential_id.value
-  comment         = "Managed by TF"
-  depends_on      = [module.dbw, databricks_catalog_workspace_binding.shared, module.st_migrations]
-}
-
-resource "databricks_schema" "migrations" {
-  provider     = databricks.dbw
-  catalog_name = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
-  name         = "migrations"
-  comment      = "Migrations Schema"
-  storage_root = databricks_external_location.migrations_storage.url
-
-  depends_on = [module.dbw, module.kvs_databricks_dbw_workspace_token, databricks_catalog_workspace_binding.shared]
-}
-
 data "azurerm_key_vault_secret" "unity_storage_credential_id" {
   name         = "unity-storage-credential-id"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
