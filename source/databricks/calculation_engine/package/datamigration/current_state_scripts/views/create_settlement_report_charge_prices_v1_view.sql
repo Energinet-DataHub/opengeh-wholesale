f587f377361614810cb08ffce90348ca9e30383d
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-05-21 15:20:07 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_charge_prices_v1_view.sql
+++ source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_charge_prices_v1_view.sql
@@ -11,11 +11,11 @@ SELECT
   ARRAY_SORT(ARRAY_AGG(struct(cp.charge_time, cp.charge_price))) AS prices,
   es_ga.grid_area_code,
   es_ga.energy_supplier_id
-FROM basis_data.charge_masterdata_periods as cm
-INNER JOIN basis_data.charge_price_points as cp ON cm.calculation_id = cp.calculation_id AND cm.charge_key = cp.charge_key
+FROM {BASIS_DATA_DATABASE_NAME}.charge_masterdata_periods as cm
+INNER JOIN {BASIS_DATA_DATABASE_NAME}.charge_price_points as cp ON cm.calculation_id = cp.calculation_id AND cm.charge_key = cp.charge_key
 INNER JOIN (
-  SELECT distinct mp.calculation_id, charge_key, energy_supplier_id, grid_area_code FROM basis_data.charge_link_periods AS cl
-  INNER JOIN basis_data.metering_point_periods AS mp ON mp.calculation_id = cl.calculation_id AND mp.metering_point_id = cl.metering_point_id
+  SELECT distinct mp.calculation_id, charge_key, energy_supplier_id, grid_area_code FROM {BASIS_DATA_DATABASE_NAME}.charge_link_periods AS cl
+  INNER JOIN {BASIS_DATA_DATABASE_NAME}.metering_point_periods AS mp ON mp.calculation_id = cl.calculation_id AND mp.metering_point_id = cl.metering_point_id
 ) AS es_ga ON cm.calculation_id = es_ga.calculation_id AND cm.charge_key = es_ga.charge_key
-INNER JOIN basis_data.calculations AS c ON cm.calculation_id = c.calculation_id
+INNER JOIN {BASIS_DATA_DATABASE_NAME}.calculations AS c ON cm.calculation_id = c.calculation_id
 GROUP BY c.calculation_id, cm.charge_key, es_ga.grid_area_code, es_ga.energy_supplier_id, DATE_TRUNC('day', FROM_UTC_TIMESTAMP(cp.charge_time, 'Europe/Copenhagen'))
