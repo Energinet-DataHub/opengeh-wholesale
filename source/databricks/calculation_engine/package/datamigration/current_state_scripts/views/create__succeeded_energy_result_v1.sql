8e355340ff7835d319313067acb02ddc7c43b2cf
Bjarke Meier <hej@bjarkemeier.dk>
2024-05-22 17:43:35 +0200

    update views with more detail

--- source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create__succeeded_energy_result_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create__succeeded_energy_result_v1.sql
@@ -18,19 +18,25 @@ SELECT c.calculation_id,
        e.time,
        e.aggregation_level,
        e.resolution,
+       e.time_series_type,
        CASE
            WHEN e.time_series_type = 'production' THEN 'production'
            WHEN e.time_series_type = 'non_profiled_consumption' THEN 'consumption'
            WHEN e.time_series_type = 'flex_consumption' THEN 'consumption'
            WHEN e.time_series_type = 'net_exchange_per_ga' THEN 'exchange'
+           WHEN e.time_series_type = 'net_exchange_per_neighboring_ga' THEN 'exchange'
            WHEN e.time_series_type = 'total_consumption' THEN 'consumption'
+           WHEN e.time_series_type = 'grid_loss' AND e.quantity >= 0 THEN 'consumption'
+           WHEN e.time_series_type = 'grid_loss' AND e.quantity < 0 THEN 'production'
+           WHEN e.time_series_type = 'negative_grid_loss' THEN 'production'
+           WHEN e.time_series_type = 'positive_grid_loss' THEN 'consumption'
+           WHEN e.time_series_type = 'temp_flex_consumption' THEN 'consumption'
+           WHEN e.time_series_type = 'temp_production' THEN 'production'
        END as metering_point_type,
        CASE
-           WHEN e.time_series_type = 'production' THEN NULL
            WHEN e.time_series_type = 'non_profiled_consumption' THEN 'non_profiled'
            WHEN e.time_series_type = 'flex_consumption' THEN 'flex'
-           WHEN e.time_series_type = 'net_exchange_per_ga' THEN NULL
-           WHEN e.time_series_type = 'total_consumption' THEN NULL
+           WHEN e.time_series_type = 'temp_flex_consumption' THEN 'flex'
        END as settlement_method,
     e.metering_point_id
 FROM {OUTPUT_DATABASE_NAME}.energy_results AS e
--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202405221311_Create_edi_results_energy_results_per_ga_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405221311_Create_edi_results_energy_results_per_ga_v1.sql
@@ -14,3 +14,8 @@ SELECT calculation_id,
        unit,
        quantity_qualities
 FROM {OUTPUT_DATABASE_NAME}.succeeded_energy_results_v1
+WHERE
+    -- Only include results that must be sent to the actors
+    time_series_type in ('production', 'non_profiled_consumption', 'net_exchange_per_ga', 'flex_consumption', 'total_consumption')
+    -- Only include results that are aggregated per grid area
+    AND aggregation_level = 'total_ga'
