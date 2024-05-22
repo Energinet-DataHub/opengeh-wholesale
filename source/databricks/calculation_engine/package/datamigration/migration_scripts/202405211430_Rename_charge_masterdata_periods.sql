41b85e21c4e5f8286d1890116f44f6d8629cb460
Jonas Michelsen <jvm@energinet.dk>
2024-05-22 07:52:26 +0200

    Update 202405211430_Rename_charge_masterdata_periods.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202405211430_Rename_charge_masterdata_periods.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405211430_Rename_charge_masterdata_periods.sql
@@ -1,8 +1,14 @@
-ALTER TABLE {INPUT_DATABASE_NAME}.charge_masterdata_periods RENAME TO charge_price_information_periods
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{INPUT_DATABASE_NAME}' AND TABLE_NAME = 'charge_masterdata_periods')
+BEGIN
+    ALTER TABLE {INPUT_DATABASE_NAME}.charge_masterdata_periods RENAME TO charge_price_information_periods
+END
 GO
 ALTER TABLE {INPUT_DATABASE_NAME}.charge_price_information_periods SET LOCATION '{CONTAINER_PATH}/{INPUT_FOLDER}/charge_price_information_periods'
 GO
-ALTER TABLE {BASIS_DATA_DATABASE_NAME}.charge_masterdata_periods RENAME TO charge_price_information_periods
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{BASIS_DATA_DATABASE_NAME}' AND TABLE_NAME = 'charge_masterdata_periods')
+BEGIN
+    ALTER TABLE {BASIS_DATA_DATABASE_NAME}.charge_masterdata_periods RENAME TO charge_price_information_periods
+END
 GO
 ALTER TABLE {BASIS_DATA_DATABASE_NAME}.charge_price_information_periods SET LOCATION '{CONTAINER_PATH}/{BASIS_DATA_FOLDER}/charge_price_information_periods'
 GO
