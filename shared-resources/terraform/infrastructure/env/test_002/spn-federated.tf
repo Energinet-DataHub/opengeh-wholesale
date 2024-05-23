ef7d56b3ea9cfba3bb2f5ee6bd80eee5d2d6b0c4
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2024-05-23 11:05:52 +0200

    remove temporary fix for federated credentials (#1535)
    * remove temporary fix for federated credentials
    
    * remove temporary fix for federated credentials
    
    * cleanup

--- shared-resources/terraform/infrastructure/env/test_002/spn-federated.tf
+++ shared-resources/terraform/infrastructure/env/test_002/spn-federated.tf
@@ -1,23 +1,23 @@
 resource "azuread_application_federated_identity_credential" "dh3_infrastructure_test_002" {
   application_id = data.azuread_application.current.id
-  display_name   = "dh3-infrastructure-test_002"
-  subject        = "repo:Energinet-DataHub/dh3-infrastructure:environment:test_002"
+  display_name   = "dh3-infrastructure-${local.environment_name}_${var.environment_instance}"
+  subject        = "repo:Energinet-DataHub/dh3-infrastructure:environment:${local.environment_name}_${var.environment_instance}"
   audiences      = [local.federated_identity_credential.audience_azuread]
   issuer         = local.federated_identity_credential.issuer_github
 }
 
 resource "azuread_application_federated_identity_credential" "automation_test_002" {
   application_id = data.azuread_application.current.id
-  display_name   = "dh3-automation-test-002"
-  subject        = "repo:Energinet-DataHub/dh3-automation:environment:test_002"
+  display_name   = "dh3-automation-${local.environment_name}_${var.environment_instance}"
+  subject        = "repo:Energinet-DataHub/dh3-automation:environment:${local.environment_name}_${var.environment_instance}"
   audiences      = [local.federated_identity_credential.audience_azuread]
   issuer         = local.federated_identity_credential.issuer_github
 }
 
 resource "azuread_application_federated_identity_credential" "modules_azureauth_test_002" {
   application_id = data.azuread_application.current.id
-  display_name   = "geh-terraform-modules-azureauth-test-002"
-  subject        = "repo:Energinet-DataHub/geh-terraform-modules:environment:test_002"
+  display_name   = "geh-terraform-modules-azureauth-${local.environment_name}_${var.environment_instance}"
+  subject        = "repo:Energinet-DataHub/geh-terraform-modules:environment:${local.environment_name}_${var.environment_instance}"
   audiences      = [local.federated_identity_credential.audience_azuread]
   issuer         = local.federated_identity_credential.issuer_github
 }
--- shared-resources/terraform/infrastructure/main/spn-federated-credentials.tf
+++ shared-resources/terraform/infrastructure/main/spn-federated-credentials.tf
@@ -24,26 +24,9 @@ data "azuread_application" "current" {
 resource "azuread_application_federated_identity_credential" "recovery" {
   application_id = data.azuread_application.current.id
 
-  display_name = "dh3-recovery-${local.environment_name}-${var.environment_instance}"
+  display_name = "dh3-recovery-${local.environment_name}_${var.environment_instance}"
   subject      = "repo:Energinet-DataHub/dh3-recovery:environment:${local.environment_name}_${var.environment_instance}"
 
   audiences = [local.federated_identity_credential.audience_azuread]
   issuer    = local.federated_identity_credential.issuer_github
-
-  depends_on = [ null_resource.delete_fed ]
 }
-
-
-# TEMPORARY FIX: delete the current federated credential by using az cli
-resource "null_resource" "delete_fed" {
-    provisioner "local-exec" {
-        command = "az ad app federated-credential delete --federated-credential-id ${local.environment_name}_${var.environment_instance}-dh3-recovery --id '${data.azuread_application.current.object_id}'"
-    }
-}
-
-resource "null_resource" "delete_federated_automation" {
-    provisioner "local-exec" {
-        command = "az ad app federated-credential delete --federated-credential-id ${local.environment_name}_${var.environment_instance}-automation --id '${data.azuread_application.current.object_id}'"
-    }
-}
-
