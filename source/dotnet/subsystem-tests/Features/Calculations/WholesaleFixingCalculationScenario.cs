cd7f488d44dd3427e0df4d5f1e4fd81b44f11faf
Alexander <alexanderharrow@hotmail.com>
2024-05-23 21:38:42 +0200

    Update WholesaleFixingCalculationScenario.cs

--- source/dotnet/subsystem-tests/Features/Calculations/WholesaleFixingCalculationScenario.cs
+++ source/dotnet/subsystem-tests/Features/Calculations/WholesaleFixingCalculationScenario.cs
@@ -374,6 +374,7 @@ AppDependencies
         var publicDataModelsAndTables = new List<(string Name, string TableName)>
         {
             new("settlement_report", "metering_point_periods_v1"),
+            new("wholesale_edi_results", "energy_result_points_per_ga_v1"),
         };
 
         // Act
