875104329cda4eed4879d359cf52ace990c3ff0c
Ebbe Knudsen <xedkn@energinet.dk>
2024-05-23 17:07:43 +0200

    Add ActorMessagesEnqueuedTrigger

---
+++ source/dotnet/wholesale-api/Orchestrations/Functions/Calculation/Contracts/MessagesEnqueued.cs
@@ -0,0 +1,22 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// ReSharper disable once CheckNamespace -- Protobuf is in with another namespace
+namespace Energinet.DataHub.EnergySupplying.RequestResponse.IntegrationEvents;
+
+public partial class MessagesEnqueuedV1
+{
+    public const string EventName = "MessagesEnqueuedV1";
+    public const int CurrentMinorVersion = 1;
+}
