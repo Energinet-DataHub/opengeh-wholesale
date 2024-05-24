747740d0bd8489c90526abeb07ca3fb23d2f1349
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2024-05-24 15:03:54 +0200

    bump app services to v14 (#1542)
    * bump app services to v14
    
    * bump app services to v14

--- market-participant/terraform/main/app-api.tf
+++ market-participant/terraform/main/app-api.tf
@@ -1,5 +1,5 @@
 module "app_api" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=v13"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=v14"
 
   name                                   = "api"
   project_name                           = var.domain_name_short
@@ -62,21 +62,21 @@ locals {
     "UserAuthentication:InternalMetadataAddress"      = "https://app-api-${var.domain_name_short}-${var.environment_short}-we-${var.environment_instance}.azurewebsites.net/.well-known/openid-configuration"
     "UserAuthentication:BackendBffAppId"              = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-bff-app-id)"
 
-    "Database:ConnectionString"                       = local.MS_MARKET_PARTICIPANT_CONNECTION_STRING
+    "Database:ConnectionString" = local.MS_MARKET_PARTICIPANT_CONNECTION_STRING
 
-    "AzureB2c:Tenant"                                 = var.b2c_tenant
-    "AzureB2c:SpnId"                                  = var.b2c_spn_id
-    "AzureB2c:SpnSecret"                              = var.b2c_spn_secret
-    "AzureB2c:BackendObjectId"                        = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-b2b-app-obj-id)"
-    "AzureB2c:BackendSpnObjectId"                     = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-b2b-app-sp-id)"
-    "AzureB2c:BackendId"                              = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-b2b-app-id)"
+    "AzureB2c:Tenant"             = var.b2c_tenant
+    "AzureB2c:SpnId"              = var.b2c_spn_id
+    "AzureB2c:SpnSecret"          = var.b2c_spn_secret
+    "AzureB2c:BackendObjectId"    = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-b2b-app-obj-id)"
+    "AzureB2c:BackendSpnObjectId" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-b2b-app-sp-id)"
+    "AzureB2c:BackendId"          = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-b2b-app-id)"
 
-    "KeyVault:TokenSignKeyVault"                      = module.kv_internal.vault_uri
-    "KeyVault:TokenSignKeyName"                       = azurerm_key_vault_key.token_sign.name
-    "KeyVault:CertificatesKeyVault"                   = module.kv_dh2_certificates.vault_uri
+    "KeyVault:TokenSignKeyVault"    = module.kv_internal.vault_uri
+    "KeyVault:TokenSignKeyName"     = azurerm_key_vault_key.token_sign.name
+    "KeyVault:CertificatesKeyVault" = module.kv_dh2_certificates.vault_uri
 
-    "CvrRegister:BaseAddress"                         = var.cvr_base_address
-    "CvrRegister:Username"                            = var.cvr_username
-    "CvrRegister:Password"                            = "@Microsoft.KeyVault(VaultName=${module.kv_internal.name};SecretName=${module.kvs_cvr_password.name})"
+    "CvrRegister:BaseAddress" = var.cvr_base_address
+    "CvrRegister:Username"    = var.cvr_username
+    "CvrRegister:Password"    = "@Microsoft.KeyVault(VaultName=${module.kv_internal.name};SecretName=${module.kvs_cvr_password.name})"
   }
 }
--- migrations/terraform/main/app-time-series-api.tf
+++ migrations/terraform/main/app-time-series-api.tf
@@ -1,22 +1,22 @@
 module "app_time_series_api" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=v13"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=v14"
 
-  name                                     = "timeseriesapi"
-  project_name                             = var.domain_name_short
-  environment_short                        = var.environment_short
-  environment_instance                     = var.environment_instance
-  resource_group_name                      = azurerm_resource_group.this.name
-  location                                 = azurerm_resource_group.this.location
-  vnet_integration_subnet_id               = data.azurerm_key_vault_secret.snet_vnet_integration_id.value
-  private_endpoint_subnet_id               = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
-  ip_restrictions                          = var.ip_restrictions
-  scm_ip_restrictions                      = var.ip_restrictions
-  app_service_plan_id                      = module.webapp_service_plan.id
-  application_insights_instrumentation_key = data.azurerm_key_vault_secret.appi_instrumentation_key.value
-  dotnet_framework_version                 = "v8.0"
-  health_check_path                        = "/monitor/ready"
-  health_check_alert_action_group_id       = data.azurerm_key_vault_secret.primary_action_group_id.value
-  health_check_alert_enabled               = var.enable_health_check_alerts
+  name                                   = "timeseriesapi"
+  project_name                           = var.domain_name_short
+  environment_short                      = var.environment_short
+  environment_instance                   = var.environment_instance
+  resource_group_name                    = azurerm_resource_group.this.name
+  location                               = azurerm_resource_group.this.location
+  vnet_integration_subnet_id             = data.azurerm_key_vault_secret.snet_vnet_integration_id.value
+  private_endpoint_subnet_id             = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
+  ip_restrictions                        = var.ip_restrictions
+  scm_ip_restrictions                    = var.ip_restrictions
+  app_service_plan_id                    = module.webapp_service_plan.id
+  application_insights_connection_string = data.azurerm_key_vault_secret.appi_shared_connection_string.value
+  dotnet_framework_version               = "v8.0"
+  health_check_path                      = "/monitor/ready"
+  health_check_alert_action_group_id     = data.azurerm_key_vault_secret.primary_action_group_id.value
+  health_check_alert_enabled             = var.enable_health_check_alerts
 
   app_settings = local.default_time_series_api_app_settings
   role_assignments = [
