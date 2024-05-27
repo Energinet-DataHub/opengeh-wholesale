d6397425842912000f428108b0355127d6c46bd6
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2024-05-27 12:18:59 +0200

    allow migrations to configure job names (#1550)
    * allow migrations to configure job names
    
    * allow migrations to configure job names

--- migrations/terraform/main/dbw-jobs.tf
+++ migrations/terraform/main/dbw-jobs.tf
@@ -1,29 +1,3 @@
-locals {
-  job_names = [
-    "Schema_Migration",
-    "Bulk_Charge_Links_Landing_to_Gold",
-    "Bulk_Charges_Landing_to_Gold",
-    "duplicates_in_time_series_eloverblik",
-    "monitor_eloverblik_time_series",
-    "duplicates_in_metering_points_gold",
-    "Gold_To_Eloverblik",
-    "Gold_To_Wholesale",
-    "Landing_To_Gold",
-    "optimize_bronze_time_series",
-    "Reprocess_Metering_Points_Silver_Quarantine",
-    "Reprocess_Time_Series_Silver_Quarantine",
-    "duplicates_in_time_series_silver",
-    "Syncronize_To_Bronze_Time_Series",
-    "Stream_Time_Series_Bronze_To_Silver",
-    "Stream_Time_Series_Landing_To_Bronze",
-    "Stream_Time_Series_Silver_To_Gold",
-    "duplicates_in_metering_points_wholesale",
-    "monitor_wholesale_metering_points",
-    "monitor_wholesale_time_series",
-    "Reprocess_Metering_Points_Bronze_Quarantine",
-  ]
-}
-
 resource "databricks_job" "this" {
   for_each = toset(local.job_names)
 
