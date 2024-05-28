5e78a3fb2905405d937a1ceed47ee1d83e35cba6
Jonas Michelsen <jvm@energinet.dk>
2024-05-28 15:03:56 +0200

    .

--- source/databricks/calculation_engine/tests/features/utils/views/view_input_specifications.py
+++ source/databricks/calculation_engine/tests/features/utils/views/view_input_specifications.py
@@ -16,6 +16,7 @@ import package.calculation.basis_data.schemas as basis_data_schemas
 from features.utils.dataframes import (
     create_energy_result_dataframe,
     create_wholesale_result_dataframe,
+    create_total_monthly_amounts_dataframe,
 )
 from features.utils.dataframes.basis_data.calculations_dataframe import (
     create_calculations_dataframe,
@@ -24,6 +25,9 @@ from features.utils.readers import BasisDataTableReader, EnergyResultViewReader
 from package.calculation.output.schemas import (
     energy_results_schema,
     wholesale_results_schema,
+)
+from package.calculation.output.schemas.total_monthly_amounts_schema import (
+    total_monthly_amounts_schema,
 )
 from package.infrastructure.paths import (
     BASIS_DATA_DATABASE_NAME,
@@ -87,6 +91,12 @@ def get_input_specifications() -> dict[str, tuple]:
             wholesale_results_schema,
             EnergyResultViewReader.read_wholesale_results,
             create_wholesale_result_dataframe,
+            OUTPUT_DATABASE_NAME,
+        ),
+        "total_monthly_amounts.csv": (
+            total_monthly_amounts_schema,
+            EnergyResultViewReader.read_total_monthly_amounts,
+            create_total_monthly_amounts_dataframe,
             OUTPUT_DATABASE_NAME,
         ),
     }
