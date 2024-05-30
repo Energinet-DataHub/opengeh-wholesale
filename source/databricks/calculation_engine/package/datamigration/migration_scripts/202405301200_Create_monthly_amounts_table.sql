7ce76fb64a868e58fe1ae8953d5b7eb9019015aa
Jonas Michelsen <jvm@energinet.dk>
2024-05-30 14:56:26 +0200

    .

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202405301200_Create_monthly_amounts_table.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405301200_Create_monthly_amounts_table.sql
@@ -5,17 +5,19 @@ CREATE TABLE IF NOT EXISTS {OUTPUT_DATABASE_NAME}.monthly_amounts
     -- Enum
     calculation_type STRING NOT NULL,
     calculation_execution_time_start TIMESTAMP NOT NULL,
-    
+
     -- 36 characters UUID
     calculation_result_id STRING NOT NULL,
 
     grid_area_code STRING NOT NULL,
     energy_supplier_id STRING NOT NULL,
+    quantity_unit STRING NOT NULL,
     time TIMESTAMP NOT NULL,
     amount DECIMAL(18, 6),
-    charge_owner_id STRING NOT NULL,
+    is_tax BOOLEAN,
     charge_code STRING,
     charge_type STRING NOT NULL
+    charge_owner_id STRING NOT NULL,
 )
 USING DELTA
 TBLPROPERTIES (
@@ -26,8 +28,9 @@ TBLPROPERTIES (
     delta.constraints.grid_area_code_chk = "LENGTH ( grid_area_code ) = 3",
     delta.constraints.energy_supplier_id_chk = "LENGTH ( energy_supplier_id ) = 13 OR LENGTH ( energy_supplier_id ) = 16",
     delta.constraints.charge_owner_id_chk = "LENGTH ( charge_owner_id ) = 13 OR LENGTH ( charge_owner_id ) = 16",
-    delta.constraints.charge_type_chk = "charge_type IN ( 'subscription' , 'fee' , 'tariff' )"
+    delta.constraints.charge_type_chk = "charge_type IN ( 'subscription' , 'fee' , 'tariff' )",
+    delta.constraints.quantity_unit_chk = "quantity_unit IN ( 'kWh' , 'pcs' )"
 )
--- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used. 
--- In the production it is set to empty and the respective location is used. This means the production tables won't be deleted if the schema is.    
+-- In the test environment the TEST keyword is set to "--" (commented out) and the default location is used.
+-- In the production it is set to empty and the respective location is used. This means the production tables won't be deleted if the schema is.
 {TEST}LOCATION '{CONTAINER_PATH}/{OUTPUT_FOLDER}/monthly_amounts'
