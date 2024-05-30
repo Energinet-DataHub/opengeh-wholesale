3f6cadf8e52e62599cc7fb736f34e4f1be440cc5
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-05-30 22:38:01 +0200

    don't add aggregation

--- source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_latest_calculations_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_latest_calculations_v1.sql
@@ -2,5 +2,5 @@ CREATE VIEW IF NOT EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.latest_calculations_
 SELECT calculation_type,
        MAX(version) as version
 FROM {BASIS_DATA_DATABASE_NAME}.calculations
-GROUP BY calculation_type
 WHERE calculation_type IN ('BalanceFixing', 'WholesaleFixing', 'FirstCorrectionSettlement', 'SecondCorrectionSettlement', 'ThirdCorrectionSettlement')
+GROUP BY calculation_type
