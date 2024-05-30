527473f061db2dc458ed63ae96297f26b9473a8d
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2024-05-30 12:48:44 +0200

    Rename SB subscription and add new (#1581)
    * Rename SB subscription and add new
    
    * Revert breaking changes
    
    * Missing in last commit

---
+++ migrations/terraform/main/sbs-time-series-sync-processing.tf
@@ -0,0 +1,7 @@
+﻿module "sbtsub_time_series_sync_processing" {
+  source             = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic-subscription?ref=v13"
+  name               = "sbtsub-${lower(var.domain_name_short)}-time-series-sync-processing"
+  topic_id           = azurerm_servicebus_topic.time_series_imported_messages_topic.id
+  project_name       = var.domain_name_short
+  max_delivery_count = 10
+}
