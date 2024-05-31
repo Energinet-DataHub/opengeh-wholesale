3802704fed33bb98c22c2bb3c28376ca610117f6
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-05-31 13:50:33 +0200

    feat: Add views to support EDI (#2328)

---
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405310730_Create_edi_energy_results_per_brp_ga_v1.sql
@@ -0,0 +1,21 @@
+CREATE VIEW IF NOT EXISTS {EDI_RESULTS_DATABASE_NAME}.energy_result_points_per_brp_ga_v1 AS
+SELECT calculation_id,
+       calculation_type,
+       calculation_period_start,
+       calculation_period_end,
+       calculation_version,
+       result_id,
+       grid_area_code,
+       balance_responsible_party_id,
+       metering_point_type,
+       settlement_method,
+       resolution,
+       time,
+       quantity,
+       unit,
+       quantity_qualities
+FROM {OUTPUT_DATABASE_NAME}.succeeded_energy_results_v1
+WHERE
+    calculation_type in ('BalanceFixing', 'Aggregation')
+    AND aggregation_level = 'brp_ga'
+    AND time_series_type in ('production', 'non_profiled_consumption', 'flex_consumption')
