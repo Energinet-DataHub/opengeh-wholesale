a95131d23a58fe3b463f21e46d84227556c2249a
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-05-31 22:37:02 +0200

    .

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202405310930_Add_version_to_settlement_report_wholesale_results_v1_view.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405310930_Add_version_to_settlement_report_wholesale_results_v1_view.sql
@@ -5,6 +5,7 @@ CREATE VIEW {SETTLEMENT_REPORT_DATABASE_NAME}.wholesale_results_v1 as
 SELECT c.calculation_id,
        c.calculation_type,
        c.version as calculation_version,
+       wr.calculation_result_id as result_id,
        wr.grid_area_code,
        wr.energy_supplier_id,
        wr.time,
