23672e71bc768e31d5a2f16666377761d3b6c8aa
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-05-31 18:41:52 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202405311800_Create_settlement_report_energy_result_per_ga_v1_view.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202405311800_Create_settlement_report_energy_result_per_ga_v1_view.sql
@@ -1,6 +1,10 @@
-CREATE VIEW {SETTLEMENT_REPORT_DATABASE_NAME}.energy_results_per_ga_v1 as
+DROP VIEW IF EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.energy_results_v1
+GO
+
+CREATE VIEW {SETTLEMENT_REPORT_DATABASE_NAME}.energy_result_points_per_ga_v1 as
 SELECT calculation_id,
        calculation_type,
+       calculation_version,
        calculation_result_id as result_id,
        grid_area_code,
        metering_point_type,
@@ -10,4 +14,5 @@ SELECT calculation_id,
        quantity,
 FROM {OUTPUT_DATABASE_NAME}.succeeded_energy_results_v1
 WHERE time_series_type IN ('production', 'non_profiled_consumption', 'flex_consumption', 'net_exchange_per_ga', 'total_consumption')
+WHERE calculation_type IN ('BalanceFixing', 'WholesaleFixing', 'FirstCorrectionSettlement', 'SecondCorrectionSettlement', 'ThirdCorrectionSettlement')
 AND aggregation_level = 'total_ga'
