c19789f2012524248858467dcc7057a171511fa0
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-06-04 12:59:51 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_current_balance_fixing_calculation_version_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_current_balance_fixing_calculation_version_v1.sql
@@ -1,4 +1,4 @@
-CREATE VIEW IF NOT EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.current_balance_fixing_calculation_version as
+CREATE VIEW IF NOT EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.current_balance_fixing_calculation_versions_v1 as
 SELECT MAX(version) as calculation_version
 FROM {BASIS_DATA_DATABASE_NAME}.calculations
 WHERE calculation_type = 'BalanceFixing'
