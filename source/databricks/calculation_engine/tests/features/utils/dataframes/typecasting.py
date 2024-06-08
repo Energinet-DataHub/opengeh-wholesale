ee95591f097c151a14c825440585b99bb29a2c11
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-06-08 15:25:31 +0200

    refac: Simplify public data model feature testing (#2346)

---
+++ source/databricks/calculation_engine/tests/features/utils/dataframes/typecasting.py
@@ -0,0 +1,110 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pyspark.sql.functions as f
+from pyspark.sql import DataFrame
+from pyspark.sql.types import (
+    BooleanType,
+    IntegerType,
+)
+from pyspark.sql.types import (
+    StructField,
+    StringType,
+    TimestampType,
+    StructType,
+    ArrayType,
+    DecimalType,
+    LongType,
+)
+
+from package.constants import TimeSeriesColname
+
+
+def cast_column_types(df: DataFrame, table_or_view_name: str = "") -> DataFrame:
+    for column in df.schema:
+        df = _cast_column(df, column.name, table_or_view_name)
+    return df
+
+
+def _cast_column(df: DataFrame, column_name: str, table_or_view_name: str) -> DataFrame:
+
+    # Needed to avoid "time_series_type" will be cast to a timestamp.
+    if column_name == "time_series_type":
+        return df
+
+    if "time" in column_name or "period" in column_name or "date" in column_name:
+        return df.withColumn(column_name, f.col(column_name).cast(TimestampType()))
+
+    if column_name.endswith("version"):
+        return df.withColumn(column_name, f.col(column_name).cast(LongType()))
+
+    if column_name == "quantity":
+        if "charge" in table_or_view_name:
+            return df.withColumn(column_name, f.col(column_name).cast(IntegerType()))
+        else:
+            return df.withColumn(
+                column_name, f.col(column_name).cast(DecimalType(18, 3))
+            )
+
+    if column_name == "quantities":
+        """Settlement report quantities are stored as a string in the format "[{observation_time: timestamp, quantity: decimal}, ...]"."""
+        return df.withColumn(
+            column_name,
+            f.from_json(
+                f.col(column_name), ArrayType(_settlement_report_quantity_schema)
+            ),
+        )
+
+    if "qualities" in column_name:
+        return df.withColumn(
+            column_name,
+            f.split(
+                f.regexp_replace(
+                    f.regexp_replace(f.col(column_name), r"[\[\]']", ""),
+                    " ",
+                    "",
+                ),
+                ",",
+            ).cast(ArrayType(StringType())),
+        )
+
+    if column_name == "price_points":
+        return df.withColumn(
+            column_name,
+            f.from_json(f.col(column_name), ArrayType(_price_point)),
+        )
+
+    if "price" in column_name or column_name == "amount":
+        return df.withColumn(column_name, f.col(column_name).cast(DecimalType(18, 6)))
+
+    if column_name == "is_tax":
+        return df.withColumn(column_name, f.col(column_name).cast(BooleanType()))
+
+    return df
+
+
+# TODO BJM: Replace these by recursive type casting
+_settlement_report_quantity_schema = StructType(
+    [
+        StructField(TimeSeriesColname.observation_time, TimestampType(), False),
+        StructField(TimeSeriesColname.quantity, DecimalType(18, 3), False),
+    ]
+)
+
+
+_price_point = StructType(
+    [
+        StructField("time", TimestampType(), True),
+        StructField("price", DecimalType(18, 6), True),
+    ]
+)
--- source/databricks/calculation_engine/tests/features/utils/views/view_input_specifications.py
+++ source/databricks/calculation_engine/tests/features/utils/views/view_input_specifications.py
@@ -11,79 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import features.utils.dataframes.basis_data.basis_data_dataframes as basis_data_dataframes
-import package.calculation.basis_data.schemas as basis_data_schemas
-from features.utils.dataframes import (
-    create_energy_result_dataframe,
-    create_wholesale_result_dataframe,
-    create_total_monthly_amounts_dataframe,
-)
-from features.utils.dataframes.basis_data.calculations_dataframe import (
-    create_calculations,
-)
-from features.utils.dataframes.monthly_amounts_dataframe import (
-    create_monthly_amounts_dataframe,
-)
-from package.calculation.output.schemas import (
-    energy_results_schema,
-    wholesale_results_schema,
-)
-from package.calculation.output.schemas.monthly_amounts_schema import (
-    monthly_amounts_schema,
-)
-from package.calculation.output.schemas.total_monthly_amounts_schema import (
-    total_monthly_amounts_schema,
-)
-
-
-def get_input_specifications() -> dict[str, tuple]:
-    """
-    Contains the specifications for view scenario inputs.
-    The key is the name of the file to be read.
-    The value is a tuple containing the schema, the name of the method that reads the data,
-    the method that to corrects the dataframe types, and the database name.
-    """
+def get_input_specifications() -> dict[str, None]:
     return {
         # basis data
-        "basis_data.metering_point_periods.csv": (
-            basis_data_schemas.metering_point_period_schema,
-            basis_data_dataframes.create_metering_point_periods,
-        ),
-        "basis_data.time_series_points.csv": (
-            basis_data_schemas.time_series_point_schema,
-            basis_data_dataframes.create_time_series_points,
-        ),
-        "basis_data.calculations.csv": (
-            basis_data_schemas.calculations_schema,
-            create_calculations,
-        ),
-        "basis_data.charge_price_information_periods.csv": (
-            basis_data_schemas.charge_price_information_periods_schema,
-            basis_data_dataframes.create_charge_price_information_periods,
-        ),
-        "basis_data.charge_link_periods.csv": (
-            basis_data_schemas.charge_link_periods_schema,
-            basis_data_dataframes.create_charge_link_periods,
-        ),
-        "basis_data.charge_price_points.csv": (
-            basis_data_schemas.charge_price_points_schema,
-            basis_data_dataframes.create_charge_price_points,
-        ),
+        "basis_data.metering_point_periods.csv": None,
+        "basis_data.time_series_points.csv": None,
+        "basis_data.calculations.csv": None,
+        "basis_data.charge_price_information_periods.csv": None,
+        "basis_data.charge_link_periods.csv": None,
+        "basis_data.charge_price_points.csv": None,
         # results
-        "wholesale_output.energy_results.csv": (
-            energy_results_schema,
-            create_energy_result_dataframe,
-        ),
-        "wholesale_output.wholesale_results.csv": (
-            wholesale_results_schema,
-            create_wholesale_result_dataframe,
-        ),
-        "wholesale_output.monthly_amounts.csv": (
-            monthly_amounts_schema,
-            create_monthly_amounts_dataframe,
-        ),
-        "wholesale_output.total_monthly_amounts.csv": (
-            total_monthly_amounts_schema,
-            create_total_monthly_amounts_dataframe,
-        ),
+        "wholesale_output.energy_results.csv": None,
+        "wholesale_output.wholesale_results.csv": None,
+        "wholesale_output.total_monthly_amounts.csv": None,
+        "wholesale_output.monthly_amounts.csv": None,
     }
