6c434b1836d7c5275f565ac621a35a7c1470a132
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-06-12 13:08:49 +0200

    Ignore errors.

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Fixtures/DatabricksSchemaManager.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Fixtures/DatabricksSchemaManager.cs
@@ -75,14 +75,20 @@ public class DatabricksSchemaManager
 
     public async Task DropSchemaAsync()
     {
-        var sqlStatement = @$"DROP SCHEMA {SchemaName} CASCADE";
-        await ExecuteSqlAsync(sqlStatement);
+        try
+        {
+            var sqlStatement = @$"DROP SCHEMA {SchemaName} CASCADE";
+            await ExecuteSqlAsync(sqlStatement);
 
-        sqlStatement = @$"DROP SCHEMA {DeltaTableOptions.Value.BasisDataSchemaName} CASCADE";
-        await ExecuteSqlAsync(sqlStatement);
+            sqlStatement = @$"DROP SCHEMA {DeltaTableOptions.Value.BasisDataSchemaName} CASCADE";
+            await ExecuteSqlAsync(sqlStatement);
 
-        sqlStatement = @$"DROP SCHEMA {DeltaTableOptions.Value.SettlementReportSchemaName} CASCADE";
-        await ExecuteSqlAsync(sqlStatement);
+            sqlStatement = @$"DROP SCHEMA {DeltaTableOptions.Value.SettlementReportSchemaName} CASCADE";
+            await ExecuteSqlAsync(sqlStatement);
+        }
+        catch
+        {
+        }
     }
 
     /// <summary>
