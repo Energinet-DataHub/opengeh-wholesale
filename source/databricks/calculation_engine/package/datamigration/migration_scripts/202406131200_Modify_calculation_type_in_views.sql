c08bf6a2699e4f1f6b15ba19a29210a40872af23
Jonas Michelsen <jvm@energinet.dk>
2024-06-13 14:15:24 +0200

    Update 202406131200_Modify_calculation_type_in_views.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202406131200_Modify_calculation_type_in_views.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202406131200_Modify_calculation_type_in_views.sql
@@ -1,3 +1,12 @@
+DROP VIEW IF EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.current_balance_fixing_calculation_version_v1
+GO
+
+CREATE VIEW IF NOT EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.current_balance_fixing_calculation_version_v1 as
+SELECT MAX(version) as calculation_version
+FROM {BASIS_DATA_DATABASE_NAME}.calculations
+WHERE calculation_type = 'balance_fixing'
+GO
+
 DROP VIEW IF EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.energy_result_points_per_ga_v1
 GO
 
