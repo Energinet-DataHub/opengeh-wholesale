79fe4f73ebe74fa4eb7e46949ec7a6a59d98ecc7
Alexander <alexanderharrow@hotmail.com>
2024-06-13 21:12:56 +0200

    Updates

--- source/databricks/calculation_engine/tests/features/public_data_models/test__public_data_model_views_have_correct_schemas.py
+++ source/databricks/calculation_engine/tests/features/public_data_models/test__public_data_model_views_have_correct_schemas.py
@@ -15,8 +15,16 @@
 import pytest
 from pyspark.sql import SparkSession
 
-from features.utils.views.expected_schemas.charge_link_periods_v1_schema import (
+from features.utils.views.expected_schemas import (
     charge_link_periods_v1_schema,
+    charge_prices_v1_schema,
+    current_balance_fixing_calculation_version_v1_schema,
+    energy_result_points_per_es_ga_v1_schema,
+    energy_result_points_per_ga_v1_schema,
+    metering_point_periods_v1_schema,
+    metering_point_time_series_v1_schema,
+    monthly_amounts_v1_schema,
+    wholesale_results_v1_schema,
 )
 from package.common import assert_schema
 
@@ -37,20 +45,34 @@ def test__public_data_model_views_have_correct_schemas(
 
     spark.catalog.setCurrentDatabase(database_name)
     views = spark.catalog.listTables()
-    schemas = get_schemas()
+    schemas = get_expected_schemas()
     errors = []
 
     for view in views:
-        df = spark.read.format("delta").table(f"{database_name}.{view.name}")
         try:
-            assert_schema(df.schema, schemas[view.name])
+            actual_df = spark.read.format("delta").table(f"{database_name}.{view.name}")
+            expected_df = spark.createDataFrame([], schemas[view.name])
+
+            assert_schema(
+                actual_df.schema,
+                expected_df.schema,
+                ignore_nullability=True,
+            )
         except Exception as e:
             errors.append(f"{view.name}: {e}")
 
     assert not errors, "\n".join(errors) if errors else "All assertions passed."
 
 
-def get_schemas() -> dict:
+def get_expected_schemas() -> dict:
     return {
         "charge_link_periods_v1": charge_link_periods_v1_schema,
+        "charge_prices_v1": charge_prices_v1_schema,
+        "current_balance_fixing_calculation_version_v1": current_balance_fixing_calculation_version_v1_schema,
+        "energy_result_points_per_es_ga_v1": energy_result_points_per_es_ga_v1_schema,
+        "energy_result_points_per_ga_v1": energy_result_points_per_ga_v1_schema,
+        "metering_point_periods_v1": metering_point_periods_v1_schema,
+        "metering_point_time_series_v1": metering_point_time_series_v1_schema,
+        "monthly_amounts_v1": monthly_amounts_v1_schema,
+        "wholesale_results_v1": wholesale_results_v1_schema,
     }
