6b2423158c68ccb27b7fa3877c5c5f25e75d7bea
Dan Stenrøjl <XDAST@energinet.dk>
2024-06-14 14:53:20 +0200

    Refactor WholesaleResultQueriesTests

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Fixtures/MigrationsFreeDatabricksSchemaManager.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Fixtures/MigrationsFreeDatabricksSchemaManager.cs
@@ -59,6 +59,7 @@ public class MigrationsFreeDatabricksSchemaManager
         await CreateTableAsync(DeltaTableOptions.Value.WHOLESALE_RESULTS_V1_VIEW_NAME, SettlementReportWholesaleViewColumns.SchemaDefinition);
         await CreateTableAsync(DeltaTableOptions.Value.CHARGE_LINK_PERIODS_V1_VIEW_NAME, SettlementReportChargeLinkPeriodsViewColumns.SchemaDefinition);
         await CreateTableAsync(DeltaTableOptions.Value.ENERGY_RESULTS_TABLE_NAME, EnergyResultsTableSchemaDefinition.SchemaDefinition);
+        await CreateTableAsync(DeltaTableOptions.Value.WHOLESALE_RESULTS_TABLE_NAME, WholesaleResultsTableSchemaDefinition.SchemaDefinition);
     }
 
     public async Task DropSchemaAsync()
