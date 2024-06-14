5e39c7888e6960f293944ceca43baa3faab34bdd
Alexander <122119488+energinet-ajw@users.noreply.github.com>
2024-06-14 14:08:30 +0200

    test: Added test of public data model view schemas (#2385)
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Updates
    
    * Update amount_per_charge_v1.py
    
    * Updates
    
    * Updates
    
    * Delete __init__.py

---
+++ source/databricks/calculation_engine/tests/features/utils/views/expected_schemas/calculation_results/energy_per_ga_v1_schema.py
@@ -0,0 +1,42 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pyspark.sql.types import (
+    StructField,
+    StringType,
+    TimestampType,
+    StructType,
+    LongType,
+    DecimalType,
+    ArrayType,
+)
+
+energy_per_ga_v1_schema = StructType(
+    [
+        StructField("calculation_id", StringType(), False),
+        StructField("calculation_type", StringType(), False),
+        StructField("calculation_period_start", TimestampType(), False),
+        StructField("calculation_period_end", TimestampType(), False),
+        StructField("calculation_version", LongType(), False),
+        StructField("result_id", StringType(), False),
+        StructField("grid_area_code", StringType(), False),
+        StructField("metering_point_type", StringType(), False),
+        StructField("settlement_method", StringType(), False),
+        StructField("resolution", StringType(), False),
+        StructField("time", TimestampType(), False),
+        StructField("quantity", DecimalType(18, 3), False),
+        StructField("quantity_unit", StringType(), False),
+        StructField("quantity_qualities", ArrayType(StringType()), False),
+    ]
+)
