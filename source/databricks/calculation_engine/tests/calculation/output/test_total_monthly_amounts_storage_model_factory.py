0381f17283e20407678bb28c0868b4617ad31a7f
Bjarke Meier <hej@bjarkemeier.dk>
2024-06-14 11:10:48 +0200

    remove/update obsolete databricks tests

--- source/databricks/calculation_engine/tests/calculation/output/test_total_monthly_amounts_storage_model_factory.py
+++ source/databricks/calculation_engine/tests/calculation/output/test_total_monthly_amounts_storage_model_factory.py
@@ -19,7 +19,6 @@ from typing import Any
 import pytest
 from pyspark.sql import SparkSession
 
-from contract_utils import assert_contract_matches_schema
 from package.calculation.calculator_args import CalculatorArgs
 from package.calculation.output import (
     total_monthly_amounts_storage_model_factory as sut,
@@ -100,22 +99,6 @@ def _create_multiple_total_monthly_amounts(
     ]
 
     return TotalMonthlyAmount(spark.createDataFrame(data=rows))
-
-
-def test__create__columns_matching_contract(
-    spark: SparkSession,
-    contracts_path: str,
-    args: CalculatorArgs,
-) -> None:
-    # Arrange
-    contract_path = f"{contracts_path}/total-monthly-amounts-table-column-names.json"
-    total_monthly_amounts = _create_default_total_monthly_amounts(spark)
-
-    # Act
-    actual = sut.create(args, total_monthly_amounts)
-
-    # Assert
-    assert_contract_matches_schema(contract_path, actual.schema)
 
 
 @pytest.mark.parametrize(
