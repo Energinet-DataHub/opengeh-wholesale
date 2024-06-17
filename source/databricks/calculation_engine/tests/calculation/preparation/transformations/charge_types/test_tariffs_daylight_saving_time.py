5cdbabd586003f9969b1eed852a89a56fcdb1429
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-06-17 21:53:04 +0200

    ...

--- source/databricks/calculation_engine/tests/calculation/preparation/transformations/charge_types/test_tariffs_daylight_saving_time.py
+++ source/databricks/calculation_engine/tests/calculation/preparation/transformations/charge_types/test_tariffs_daylight_saving_time.py
@@ -27,117 +27,118 @@ from package.calculation.preparation.transformations import (
 DEFAULT_TIME_ZONE = "Europe/Copenhagen"
 
 
-def test__get_prepared_tariffs__when_summer_time_and_hourly_resolution__returns_expected(
-    spark: SparkSession,
-) -> None:
-    """
-    TODO JMG: When transitioning to daylight saving time the returned number of rows .
-    """
-    # Arrange
-    time_series_rows = [
-        factory.create_time_series_row(observation_time=datetime(2021, 3, 27, 23)),
-        factory.create_time_series_row(observation_time=datetime(2021, 3, 28, 0)),
-        factory.create_time_series_row(observation_time=datetime(2021, 3, 28, 1)),
-        factory.create_time_series_row(observation_time=datetime(2021, 3, 28, 2)),
-        factory.create_time_series_row(observation_time=datetime(2021, 3, 28, 3)),
-    ]
-    charge_price_information_rows = [
-        factory.create_charge_price_information_row(
-            from_date=datetime(2021, 3, 26, 23),
-            to_date=datetime(2021, 4, 2, 0),
-            resolution=e.ChargeResolution.HOUR,
-        ),
-    ]
-    charge_prices_rows = [
-        factory.create_charge_prices_row(),
-    ]
-    charge_link_metering_points_rows = [
-        factory.create_charge_link_metering_point_periods_row(
-            from_date=datetime(2021, 3, 26, 23),
-            to_date=datetime(2021, 4, 2, 0),
-        ),
-    ]
+class TestWhenEnteringDaylightSavingTime:
+    def test__get_prepared_tariffs__when_hourly_resolution__returns_expected(
+        self,
+        spark: SparkSession,
+    ) -> None:
+        """
+        TODO JMG: When transitioning to daylight saving time the returned number of rows .
+        """
+        # Arrange
+        time_series_rows = [
+            factory.create_time_series_row(observation_time=datetime(2021, 3, 27, 23)),
+            factory.create_time_series_row(observation_time=datetime(2021, 3, 28, 0)),
+            factory.create_time_series_row(observation_time=datetime(2021, 3, 28, 1)),
+            factory.create_time_series_row(observation_time=datetime(2021, 3, 28, 2)),
+            factory.create_time_series_row(observation_time=datetime(2021, 3, 28, 3)),
+        ]
+        from_date = datetime(2021, 3, 26, 23)
+        to_date = datetime(2021, 4, 2, 22)
+        charge_price_information_rows = [
+            factory.create_charge_price_information_row(
+                from_date=from_date,
+                to_date=to_date,
+                resolution=e.ChargeResolution.HOUR,
+            ),
+        ]
+        charge_link_metering_points_rows = [
+            factory.create_charge_link_metering_point_periods_row(
+                from_date=from_date,
+                to_date=to_date,
+            ),
+        ]
 
-    charge_link_metering_point_periods = (
-        factory.create_charge_link_metering_point_periods(
-            spark, charge_link_metering_points_rows
+        charge_link_metering_point_periods = (
+            factory.create_charge_link_metering_point_periods(
+                spark, charge_link_metering_points_rows
+            )
         )
-    )
-    time_series = prepared_metering_point_time_series_factory.create(
-        spark, time_series_rows
-    )
-    charge_price_information = factory.create_charge_price_information(
-        spark, charge_price_information_rows
-    )
-    charge_prices = factory.create_charge_prices(spark, charge_prices_rows)
-
-    # Act
-    actual = get_prepared_tariffs(
-        time_series,
-        charge_price_information,
-        charge_prices,
-        charge_link_metering_point_periods,
-        e.ChargeResolution.HOUR,
-        DEFAULT_TIME_ZONE,
-    )
-
-    # Assert
-    assert actual.df.count() == 5
-
-
-def test__get_prepared_tariffs__when_winter_time_and_hourly_resolution__returns_expected(
-    spark: SparkSession,
-) -> None:
-    """
-    TODO JMG: When transitioning to daylight saving time the returned number of rows .
-    """
-    # Arrange
-    time_series_rows = [
-        factory.create_time_series_row(observation_time=datetime(2021, 10, 30, 22)),
-        factory.create_time_series_row(observation_time=datetime(2021, 10, 30, 23)),
-        factory.create_time_series_row(observation_time=datetime(2021, 10, 31, 0)),
-        factory.create_time_series_row(observation_time=datetime(2021, 10, 31, 1)),
-        factory.create_time_series_row(observation_time=datetime(2021, 10, 31, 2)),
-    ]
-    charge_price_information_rows = [
-        factory.create_charge_price_information_row(
-            from_date=datetime(2021, 10, 29, 22),
-            to_date=datetime(2021, 11, 1, 23),
-            resolution=e.ChargeResolution.HOUR,
-        ),
-    ]
-    charge_prices_rows = [
-        factory.create_charge_prices_row(),
-    ]
-    charge_link_metering_points_rows = [
-        factory.create_charge_link_metering_point_periods_row(
-            from_date=datetime(2021, 10, 29, 22),
-            to_date=datetime(2021, 11, 1, 23),
-        ),
-    ]
-
-    charge_link_metering_point_periods = (
-        factory.create_charge_link_metering_point_periods(
-            spark, charge_link_metering_points_rows
+        time_series = prepared_metering_point_time_series_factory.create(
+            spark, time_series_rows
         )
-    )
-    time_series = prepared_metering_point_time_series_factory.create(
-        spark, time_series_rows
-    )
-    charge_price_information = factory.create_charge_price_information(
-        spark, charge_price_information_rows
-    )
-    charge_prices = factory.create_charge_prices(spark, charge_prices_rows)
+        charge_price_information = factory.create_charge_price_information(
+            spark, charge_price_information_rows
+        )
+        charge_prices = factory.create_charge_prices(spark, [])
 
-    # Act
-    actual = get_prepared_tariffs(
-        time_series,
-        charge_price_information,
-        charge_prices,
-        charge_link_metering_point_periods,
-        e.ChargeResolution.HOUR,
-        DEFAULT_TIME_ZONE,
-    )
+        # Act
+        actual = get_prepared_tariffs(
+            time_series,
+            charge_price_information,
+            charge_prices,
+            charge_link_metering_point_periods,
+            e.ChargeResolution.HOUR,
+            DEFAULT_TIME_ZONE,
+        )
 
-    # Assert
-    assert actual.df.count() == 5
+        # Assert
+        assert actual.df.count() == 5
+
+
+class TestWhenExitingDaylightSavingTime:
+    def test__get_prepared_tariffs__when_hourly_resolution__returns_expected(
+        self,
+        spark: SparkSession,
+    ) -> None:
+        """
+        TODO JMG: When transitioning to daylight saving time the returned number of rows .
+        """
+        # Arrange
+        time_series_rows = [
+            factory.create_time_series_row(observation_time=datetime(2021, 10, 30, 22)),
+            factory.create_time_series_row(observation_time=datetime(2021, 10, 30, 23)),
+            factory.create_time_series_row(observation_time=datetime(2021, 10, 31, 0)),
+            factory.create_time_series_row(observation_time=datetime(2021, 10, 31, 1)),
+            factory.create_time_series_row(observation_time=datetime(2021, 10, 31, 2)),
+        ]
+        charge_price_information_rows = [
+            factory.create_charge_price_information_row(
+                from_date=datetime(2021, 10, 29, 22),
+                to_date=datetime(2021, 11, 1, 23),
+                resolution=e.ChargeResolution.HOUR,
+            ),
+        ]
+
+        charge_link_metering_points_rows = [
+            factory.create_charge_link_metering_point_periods_row(
+                from_date=datetime(2021, 10, 29, 22),
+                to_date=datetime(2021, 11, 1, 23),
+            ),
+        ]
+
+        charge_link_metering_point_periods = (
+            factory.create_charge_link_metering_point_periods(
+                spark, charge_link_metering_points_rows
+            )
+        )
+        time_series = prepared_metering_point_time_series_factory.create(
+            spark, time_series_rows
+        )
+        charge_price_information = factory.create_charge_price_information(
+            spark, charge_price_information_rows
+        )
+        charge_prices = factory.create_charge_prices(spark, [])
+
+        # Act
+        actual = get_prepared_tariffs(
+            time_series,
+            charge_price_information,
+            charge_prices,
+            charge_link_metering_point_periods,
+            e.ChargeResolution.HOUR,
+            DEFAULT_TIME_ZONE,
+        )
+
+        # Assert
+        assert actual.df.count() == 5
