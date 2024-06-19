d887eb958595df7c6b512707e7dbae79f52823db
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2024-06-19 15:01:59 +0200

    Enable duplicate detection within 2 days (#1666)

--- migrations/terraform/main/sbt-time-series-imported.tf
+++ migrations/terraform/main/sbt-time-series-imported.tf
@@ -1,5 +1,7 @@
 resource "azurerm_servicebus_topic" "time_series_imported_messages_topic" {
-  name                          = "sbt-${lower(var.domain_name_short)}-imported-time-series-messages"
-  namespace_id                  = data.azurerm_key_vault_secret.sb_domain_relay_namespace_id.value
-  max_message_size_in_kilobytes = 102400
+  name                                    = "sbt-${lower(var.domain_name_short)}-imported-time-series-messages"
+  namespace_id                            = data.azurerm_key_vault_secret.sb_domain_relay_namespace_id.value
+  max_message_size_in_kilobytes           = 102400
+  requires_duplicate_detection            = true
+  duplicate_detection_history_time_window = "P2D"
 }
