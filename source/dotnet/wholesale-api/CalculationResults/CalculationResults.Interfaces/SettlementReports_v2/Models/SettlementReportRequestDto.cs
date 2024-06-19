b242d01fe0dcc946786be19fa1096659bdb2e454
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-06-19 14:41:45 +0200

    Update SettlementReportRequestDto.cs
    removed unused using

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.Interfaces/SettlementReports_v2/Models/SettlementReportRequestDto.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.Interfaces/SettlementReports_v2/Models/SettlementReportRequestDto.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.Wholesale.Common.Interfaces.Models;
-
 namespace Energinet.DataHub.Wholesale.CalculationResults.Interfaces.SettlementReports_v2.Models;
 
 public sealed record SettlementReportRequestDto(
