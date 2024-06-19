99814840e7c201b7de9be211c2fa5382d4f565b4
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-06-19 14:30:05 +0200

    feat: Prepare data product contracts for publishing (#2428)

---
+++ source/databricks/calculation_engine/contracts/data_products/settlement_report/wholesale_results_v1.py
@@ -0,0 +1,24 @@
+import pyspark.sql.types as t
+
+wholesale_results_v1 = t.StructType(
+    [
+        t.StructField("calculation_id", t.StringType(), False),
+        t.StructField("calculation_type", t.StringType(), False),
+        t.StructField("calculation_version", t.LongType(), False),
+        t.StructField("result_id", t.StringType(), False),
+        t.StructField("grid_area_code", t.StringType(), False),
+        t.StructField("energy_supplier_id", t.StringType(), False),
+        t.StructField("time", t.TimestampType(), False),
+        t.StructField("resolution", t.StringType(), False),
+        t.StructField("metering_point_type", t.StringType(), False),
+        t.StructField("settlement_method", t.StringType(), True),
+        t.StructField("quantity_unit", t.StringType(), False),
+        t.StructField("currency", t.StringType(), False),
+        t.StructField("quantity", t.DecimalType(18, 3), False),
+        t.StructField("price", t.DecimalType(18, 6), False),
+        t.StructField("amount", t.DecimalType(18, 6), True),
+        t.StructField("charge_type", t.StringType(), True),
+        t.StructField("charge_code", t.StringType(), True),
+        t.StructField("charge_owner_id", t.StringType(), True),
+    ]
+)
