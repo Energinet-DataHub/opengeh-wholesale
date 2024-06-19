986b8b3c73444e2d28aef0d1a2a06c4c0cb90e9c
Jonas Michelsen <jvm@energinet.dk>
2024-06-19 10:26:56 +0200

    Create 202406191000_Modify_energy_table.sql

---
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202406191000_Modify_energy_table.sql
@@ -0,0 +1,9 @@
+DELETE FROM {OUTPUT_DATABASE_NAME}.energy_results
+WHERE aggregation_level = 'es_ga'
+GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    DROP CONSTRAINT IF EXISTS aggregation_level_chk
+GO
+ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
+    ADD CONSTRAINT aggregation_level_chk CHECK (aggregation_level IN ('total_ga', 'es_brp_ga', 'brp_ga'))
+GO
