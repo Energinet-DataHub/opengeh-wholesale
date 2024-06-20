558b53ec8329e711ca70de08c4014e6f61a8ae69
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-06-20 13:42:42 +0200

    doc: Document data product "calculation_results" contracts (#2429)
    * doc: Document data product "calculation_results" contracts
    
    * revert field reordering
    
    * wip
    
    * Updates
    
    * fix quotes
    
    * Updates
    
    ---------
    
    Co-authored-by: Alexander <alexanderharrow@hotmail.com>
    Co-authored-by: Alexander <122119488+energinet-ajw@users.noreply.github.com>

--- source/databricks/calculation_engine/contracts/data_products/settlement_report/current_balance_fixing_calculation_version_v1.py
+++ source/databricks/calculation_engine/contracts/data_products/settlement_report/current_balance_fixing_calculation_version_v1.py
@@ -1,7 +1,10 @@
 import pyspark.sql.types as t
 
+nullable = True
+
 current_balance_fixing_calculation_version_v1 = t.StructType(
     [
-        t.StructField("calculation_version", t.LongType(), False),
+        # BitInt
+        t.StructField("calculation_version", t.LongType(), not nullable),
     ]
 )
