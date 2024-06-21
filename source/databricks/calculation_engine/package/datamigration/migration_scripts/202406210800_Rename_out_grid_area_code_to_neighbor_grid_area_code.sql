cc4e11d2fac085f484111096977bd5039b0f86ea
Jonas Michelsen <jvm@energinet.dk>
2024-06-21 08:51:55 +0200

    view

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202406210800_Rename_out_grid_area_code_to_neighbor_grid_area_code.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202406210800_Rename_out_grid_area_code_to_neighbor_grid_area_code.sql
@@ -13,11 +13,3 @@ GO
 ALTER TABLE {OUTPUT_DATABASE_NAME}.energy_results
     ADD CONSTRAINT neighbor_grid_area_code_chk CHECK (neighbor_grid_area_code IS NULL OR LENGTH(neighbor_grid_area_code) = 3)
 GO
-
---
--- Rename succeeded_energy_results_v1.out_grid_area_code to neighbour_grid_area_code
---
-
-ALTER TABLE {OUTPUT_DATABASE_NAME}.succeeded_energy_results_v1
-RENAME COLUMN out_grid_area_code TO neighbor_grid_area_code
-GO
