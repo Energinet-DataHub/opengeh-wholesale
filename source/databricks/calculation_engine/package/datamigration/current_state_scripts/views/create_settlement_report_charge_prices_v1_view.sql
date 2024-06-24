a6d5091fcf94955f3c464e750e9ce404d2d1c577
Alexander <alexanderharrow@hotmail.com>
2024-06-24 13:16:15 +0200

    Update create_settlement_report_charge_prices_v1_view.sql

--- source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_charge_prices_v1_view.sql
+++ source/databricks/calculation_engine/package/datamigration/current_state_scripts/views/create_settlement_report_charge_prices_v1_view.sql
@@ -9,7 +9,7 @@ SELECT
   COALESCE(FIRST_VALUE(cm.resolution), 'ERROR') as resolution, -- Hack to make column NOT NULL. Defaults to 'ERROR'.
   COALESCE(FIRST_VALUE(cm.is_tax), false) as is_tax, -- Hack to make column NOT NULL. Defaults to false.
   COALESCE(TO_UTC_TIMESTAMP(DATE_TRUNC('day', FROM_UTC_TIMESTAMP(cp.charge_time, 'Europe/Copenhagen')),'Europe/Copenhagen'), TIMESTAMP '1970-01-01 00:00:00') AS start_date_time, -- Hack to make
-  -- column NOT NULL. Defaults to false.
+  -- column NOT NULL. Defaults to '1970-01-01 00:00:00'.
   ARRAY_SORT(ARRAY_AGG(struct(cp.charge_time AS time, cp.charge_price AS price))) AS price_points,
   es_ga.grid_area_code,
   es_ga.energy_supplier_id
