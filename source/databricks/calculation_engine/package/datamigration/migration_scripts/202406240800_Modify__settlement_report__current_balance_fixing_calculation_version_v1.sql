bcbb3c0f7a35aeba4c22a05c84ade69f66a794b4
Alexander <alexanderharrow@hotmail.com>
2024-06-24 14:50:42 +0200

    Updates

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202406240800_Modify__settlement_report__current_balance_fixing_calculation_version_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202406240800_Modify__settlement_report__current_balance_fixing_calculation_version_v1.sql
@@ -4,6 +4,6 @@ DROP VIEW IF EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.current_balance_fixing_cal
 GO
 
 CREATE VIEW IF NOT EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.current_balance_fixing_calculation_version_v1 as
-SELECT COALESCE(MAX(version), -1) as version -- Hack to make column NOT NULL. Defaults to -1.
+SELECT CAST(COALESCE(MAX(version), -1) AS BIGINT) as version -- Hack to make column NOT NULL. Defaults to -1.
 FROM {BASIS_DATA_DATABASE_NAME}.calculations
 WHERE calculation_type = 'balance_fixing'
