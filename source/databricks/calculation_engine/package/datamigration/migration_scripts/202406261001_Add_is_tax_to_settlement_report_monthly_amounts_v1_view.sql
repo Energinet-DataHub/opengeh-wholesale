26f3399e97eda19fcfb3658a03204e0c4d7c67d3
Alexander <alexanderharrow@hotmail.com>
2024-06-26 10:35:27 +0200

    Updates

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202406261001_Add_is_tax_to_settlement_report_monthly_amounts_v1_view.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202406261001_Add_is_tax_to_settlement_report_monthly_amounts_v1_view.sql
@@ -1,3 +1,6 @@
+DROP VIEW IF EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.monthly_amounts_v1
+GO
+
 CREATE VIEW IF NOT EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.monthly_amounts_v1 as
 SELECT c.calculation_id,
        c.calculation_type,
@@ -33,6 +36,6 @@ SELECT c.calculation_id,
        NULL as charge_type,
        NULL as charge_code,
        tma.charge_owner_id,
-       ma.is_tax
+       tma.is_tax
 FROM {OUTPUT_DATABASE_NAME}.total_monthly_amounts AS tma
 INNER JOIN {BASIS_DATA_DATABASE_NAME}.calculations AS c ON c.calculation_id = tma.calculation_id
