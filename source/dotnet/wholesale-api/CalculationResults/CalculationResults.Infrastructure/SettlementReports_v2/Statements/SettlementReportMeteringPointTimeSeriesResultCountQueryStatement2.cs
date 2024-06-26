5dcc7688b51c67e5764d7f144002e74147eb7c81
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-06-26 18:50:13 +0200

    Learn to count

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/SettlementReports_v2/Statements/SettlementReportMeteringPointTimeSeriesResultCountQueryStatement2.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/SettlementReports_v2/Statements/SettlementReportMeteringPointTimeSeriesResultCountQueryStatement2.cs
@@ -33,7 +33,7 @@ public sealed class SettlementReportMeteringPointTimeSeriesResultCountQueryState
     protected override string GetSqlStatement()
     {
         return $"""
-                 SELECT DISTINCT({SettlementReportMeteringPointTimeSeriesViewColumns.StartDateTime})
+                 SELECT COUNT(DISTINCT({SettlementReportMeteringPointTimeSeriesViewColumns.StartDateTime})) AS count
                  FROM (
                      SELECT
                          c.calculation_id,
