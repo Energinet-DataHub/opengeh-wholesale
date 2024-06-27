e351f0f71a53bb827716a76e4ea569c58768fec0
Rasmus Leth Friis <116742936+rf-halfspace@users.noreply.github.com>
2024-06-27 11:50:50 +0200

    bump pim version (#1696)

--- sauron/terraform/main/resource-group.tf
+++ sauron/terraform/main/resource-group.tf
@@ -35,7 +35,7 @@ data "azurerm_role_definition" "locks_contributor_access" {
 module "pim_contributor_security_group_permissions" {
   count = var.pim_contributor_group_name != "" ? 1 : 0
 
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/resource-group-role-assignments?ref=14.22.0"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/resource-group-role-assignments?ref=14.25.0"
 
   resource_group_name = azurerm_resource_group.this.name
   security_group_name = var.pim_contributor_group_name
@@ -50,7 +50,7 @@ module "pim_contributor_security_group_permissions" {
 module "pim_reader_security_group_permissions" {
   count = var.pim_reader_group_name != "" ? 1 : 0
 
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/resource-group-role-assignments?ref=14.22.0"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/resource-group-role-assignments?ref=14.25.0"
 
   resource_group_name = azurerm_resource_group.this.name
   security_group_name = var.pim_reader_group_name
