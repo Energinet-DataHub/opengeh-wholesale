f72a5754cdadc4219f2dcf2cba42edd5db7a05a5
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-06-28 10:56:34 +0200

    adjust doc

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202406271000_Modify_settlement_report_metering_point_time_series_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202406271000_Modify_settlement_report_metering_point_time_series_v1.sql
@@ -10,6 +10,9 @@ SELECT c.calculation_id,
        m.resolution,
        m.grid_area_code,
        m.energy_supplier_id,
+       -- There is a row for each 'observation_time' with an associated 'quantity' although the final settlement report
+       -- has a row per day with multiple quantity columns for that day. It turns out to that performance increases when
+       -- rows to columns are done on the consumer's side outside this view.
        t.observation_time,
        t.quantity
 FROM {BASIS_DATA_DATABASE_NAME}.metering_point_periods AS m
