b46d913449324cae35c07ba7b952a993b0285568
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-01 21:44:08 +0200

    ...

--- .github/actions/add-databricks-release-assets/Add-Assets.Tests.ps1
+++ .github/actions/add-databricks-release-assets/Add-Assets.Tests.ps1
@@ -31,31 +31,3 @@ Describe "Add-Assets" {
         Remove-Item -LiteralPath ".\test-files" -Force -Recurse
     }
 }
-
-#Describe "Add-Assets-Hive" {
-#    BeforeAll {
-#        . $PSScriptRoot/Add-Assets.ps1
-#    }
-#
-#    BeforeEach {
-#        New-Item -Path '.\test-files\package' -ItemType 'directory'
-#        New-Item -Path '.\test-files\package\datamigration_hive' -ItemType 'directory'
-#        New-Item -Path '.\test-files\package\datamigration_hive\migration_scripts' -ItemType 'directory'
-#        New-Item -Path '.\test-files\package\datamigration_hive\migration_scripts\test-script-1.sql' -ItemType 'file'
-#    }
-#
-#    Context "Given a working directory containing wheel distribution files" {
-#        It "resulting artifacts folder should contain schema migration scripts" {
-#            # Act
-#            Add-Assets -WorkingDirectory '.\test-files'
-#
-#            # Assert
-#            Test-Path '.\test-files\artifacts\hive\migration_scripts\test-script-1.sql' |
-#                Should -Be $true
-#        }
-#    }
-#
-#    AfterEach {
-#        Remove-Item -LiteralPath ".\test-files" -Force -Recurse
-#    }
-#}
