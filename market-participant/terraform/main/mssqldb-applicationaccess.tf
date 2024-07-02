2aed67fdacf88d2d8574514344c92827650629bc
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2024-07-02 14:25:18 +0200

    Market Participant: Deployment slots (#1706)

--- market-participant/terraform/main/mssqldb-applicationaccess.tf
+++ market-participant/terraform/main/mssqldb-applicationaccess.tf
@@ -5,6 +5,7 @@ module "mssql_database_application_access" {
   database_name   = module.mssqldb_market_participant.name
   application_hosts_names = [
     module.app_api.name,
+    module.app_api.slot_name,
     module.func_organization.name,
   ]
 
