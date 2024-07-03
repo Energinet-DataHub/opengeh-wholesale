ae5e2ca3091bc070076c4274ac3cca104363d543
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-03 19:33:23 +0200

    ...

--- source/databricks/calculation_engine/tests/datamigration/subsystem/test_monthly_amounts_per_charge_migrations.py
+++ source/databricks/calculation_engine/tests/datamigration/subsystem/test_monthly_amounts_per_charge_migrations.py
@@ -248,25 +248,22 @@ def test__migrated_table_does_not_round_valid_decimal(
     assert actual_df.collect()[0].amount == amount
 
 
-def test__monthly_amounts_table__is_not_managed(
+def test__monthly_amounts_per_charge_table__is_managed(
     spark: SparkSession, migrations_executed: None
 ) -> None:
     """
-    It is desired that the table is unmanaged to provide for greater flexibility.
-    According to https://learn.microsoft.com/en-us/azure/databricks/lakehouse/data-objects#--what-is-a-database:
-    "To manage data life cycle independently of database, save data to a location that is not nested under any database locations."
-    Thus we check whether the table is managed by comparing its location to the location of the database/schema.
+    It has been decided that all Delta Tables in the system should be managed, since it gives several benefits
+    such enabling more Databricks features and ensuring that access rights are only managed by Unity Catalog
     """
-    database_details = spark.sql(
-        f"DESCRIBE DATABASE {WholesaleResultsInternalDatabase.DATABASE_NAME}"
+
+    table_description = spark.sql(
+        f"DESCRIBE EXTENDED {WholesaleResultsInternalDatabase.DATABASE_NAME}.{WholesaleResultsInternalDatabase.MONTHLY_AMOUNTS_PER_CHARGE_TABLE_NAME}"
     )
-    table_details = spark.sql(
-        f"DESCRIBE DETAIL {WholesaleResultsInternalDatabase.DATABASE_NAME}.{WholesaleResultsInternalDatabase.MONTHLY_AMOUNTS_PER_CHARGE_TABLE_NAME}"
+    table_description.show()
+
+    is_managed = any(
+        prop["col_name"] == "Type" and prop["data_type"] == "MANAGED"
+        for prop in table_description.collect()
     )
 
-    database_location = database_details.where(
-        col("info_name") == "Location"
-    ).collect()[0]["info_value"]
-    table_location = table_details.collect()[0]["location"]
-
-    assert not table_location.startswith(database_location)
+    assert is_managed
