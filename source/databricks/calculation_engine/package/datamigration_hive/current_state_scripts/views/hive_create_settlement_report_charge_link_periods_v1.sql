f3aabfdce66ead593b0fc01728a8311140d2e6bd
Alexander <alexanderharrow@hotmail.com>
2024-07-03 15:53:46 +0200

    Update hive_create_settlement_report_charge_link_periods_v1.sql

--- source/databricks/calculation_engine/package/datamigration_hive/current_state_scripts/views/hive_create_settlement_report_charge_link_periods_v1.sql
+++ source/databricks/calculation_engine/package/datamigration_hive/current_state_scripts/views/hive_create_settlement_report_charge_link_periods_v1.sql
@@ -8,14 +8,31 @@ SELECT c.calculation_id,
        l.charge_code,
        l.charge_owner_id,
        l.quantity as charge_link_quantity,
-       l.from_date,
-       l.to_date,
+    CASE
+        WHEN l.from_date > m.from_date THEN l.from_date
+        ELSE m.from_date
+    END as from_date,
+    CASE
+        WHEN l.to_date < m.to_date THEN l.to_date
+        ELSE m.to_date
+    END as to_date
        m.grid_area_code,
        m.energy_supplier_id,
        cp.is_tax
 FROM {BASIS_DATA_DATABASE_NAME}.charge_link_periods AS l
 INNER JOIN (
-  SELECT distinct calculation_id, metering_point_id, metering_point_type, energy_supplier_id, grid_area_code FROM {BASIS_DATA_DATABASE_NAME}.metering_point_periods
-) AS m ON m.metering_point_id = l.metering_point_id AND m.calculation_id = l.calculation_id
+    SELECT DISTINCT
+        calculation_id,
+        metering_point_id,
+        metering_point_type,
+        from_date,
+        to_date,
+        energy_supplier_id,
+        grid_area_code
+    FROM
+        {BASIS_DATA_DATABASE_NAME}.metering_point_periods
+) AS m ON m.metering_point_id = l.metering_point_id
+    AND m.calculation_id = l.calculation_id
 INNER JOIN {BASIS_DATA_DATABASE_NAME}.calculations AS c ON c.calculation_id = l.calculation_id
 INNER JOIN {BASIS_DATA_DATABASE_NAME}.charge_price_information_periods AS cp ON cp.calculation_id = l.calculation_id AND cp.charge_key = l.charge_key
+WHERE l.from_date < m.to_date AND l.to_date > m.from_date;
