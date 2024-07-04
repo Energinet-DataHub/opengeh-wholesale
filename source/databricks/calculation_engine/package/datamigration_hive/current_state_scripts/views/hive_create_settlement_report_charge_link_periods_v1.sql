d06ace078554ffac457dc049d9ef44df2ec6d936
Alexander <alexanderharrow@hotmail.com>
2024-07-04 08:08:01 +0200

    Updates

--- source/databricks/calculation_engine/package/datamigration_hive/current_state_scripts/views/hive_create_settlement_report_charge_link_periods_v1.sql
+++ source/databricks/calculation_engine/package/datamigration_hive/current_state_scripts/views/hive_create_settlement_report_charge_link_periods_v1.sql
@@ -20,19 +20,20 @@ SELECT c.calculation_id,
        m.energy_supplier_id,
        cp.is_tax
 FROM {BASIS_DATA_DATABASE_NAME}.charge_link_periods AS l
-INNER JOIN (
-    SELECT DISTINCT
-        calculation_id,
-        metering_point_id,
-        metering_point_type,
-        from_date,
-        to_date,
-        energy_supplier_id,
-        grid_area_code
-    FROM
-        {BASIS_DATA_DATABASE_NAME}.metering_point_periods
-) AS m ON m.metering_point_id = l.metering_point_id
-    AND m.calculation_id = l.calculation_id
+INNER JOIN {BASIS_DATA_DATABASE_NAME}.metering_point_periods AS m ON m.metering_point_id = l.metering_point_id
+--INNER JOIN (
+  --  SELECT DISTINCT
+    --    calculation_id,
+      --  metering_point_id,
+      --  metering_point_type,
+      --  from_date,
+      --  to_date,
+      --  energy_supplier_id,
+      --  grid_area_code
+   -- FROM
+     --   {BASIS_DATA_DATABASE_NAME}.metering_point_periods
+--) AS m ON m.metering_point_id = l.metering_point_id
+  --  AND m.calculation_id = l.calculation_id
 INNER JOIN {BASIS_DATA_DATABASE_NAME}.calculations AS c ON c.calculation_id = l.calculation_id
 INNER JOIN {BASIS_DATA_DATABASE_NAME}.charge_price_information_periods AS cp ON cp.calculation_id = l.calculation_id AND cp.charge_key = l.charge_key
 WHERE l.from_date < m.to_date AND l.to_date > m.from_date;
