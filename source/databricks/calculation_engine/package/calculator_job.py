21f29464745de5aba4330e3a20ceffe25d46459e
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-07-05 10:24:28 +0200

    feat: Add `--simulation` flag for calculation invocation (#2518)

--- source/databricks/calculation_engine/package/calculator_job.py
+++ source/databricks/calculation_engine/package/calculator_job.py
@@ -80,6 +80,9 @@ def start_with_deps(
             args, infrastructure_settings = parse_job_args(command_line_args)
             create_and_configure_container(infrastructure_settings)
 
+            if args.is_simulation:
+                raise NotImplementedError("Simulation is not implemented.")
+
             spark = initialize_spark()
             prepared_data_reader = create_prepared_data_reader(
                 infrastructure_settings, spark
