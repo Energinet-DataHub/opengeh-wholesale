9f75fb04f6c8f2ee3878e89e08b1f49083f9a93e
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-05 09:57:35 +0200

    rename BasisDataDatabase to HiveBasisDataDatabase

--- source/databricks/calculation_engine/package/datamigration_hive/migration_scripts/202407050900_Modify_charge_basis_data_table.sql
+++ source/databricks/calculation_engine/package/datamigration_hive/migration_scripts/202407050900_Modify_charge_basis_data_table.sql
@@ -1,7 +1,7 @@
-ALTER TABLE {HIVE_OUTPUT_DATABASE_NAME}.charge_link_periods
+ALTER TABLE {{BASIS_DATA_DATABASE_NAME}}.charge_link_periods
     ALTER COLUMN to_date SET NOT NULL
 GO
 
-ALTER TABLE {HIVE_OUTPUT_DATABASE_NAME}.charge_price_information_periods
+ALTER TABLE {{BASIS_DATA_DATABASE_NAME}}.charge_price_information_periods
     ALTER COLUMN to_date SET NOT NULL
 GO
