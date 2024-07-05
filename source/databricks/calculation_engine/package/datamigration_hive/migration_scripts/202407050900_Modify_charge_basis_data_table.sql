a4bab852db87a9097a1c9cc9491a365f323546c8
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-05 11:31:22 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration_hive/migration_scripts/202407050900_Modify_charge_basis_data_table.sql
+++ source/databricks/calculation_engine/package/datamigration_hive/migration_scripts/202407050900_Modify_charge_basis_data_table.sql
@@ -1,7 +1,7 @@
-ALTER TABLE {{BASIS_DATA_DATABASE_NAME}}.charge_link_periods
+ALTER TABLE {HIVE_BASIS_DATA_DATABASE_NAME}.charge_link_periods
     ALTER COLUMN to_date SET NOT NULL
 GO
 
-ALTER TABLE {{BASIS_DATA_DATABASE_NAME}}.charge_price_information_periods
+ALTER TABLE {HIVE_BASIS_DATA_DATABASE_NAME}.charge_price_information_periods
     ALTER COLUMN to_date SET NOT NULL
 GO
