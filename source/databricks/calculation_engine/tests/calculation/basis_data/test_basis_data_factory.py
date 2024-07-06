158786fcea9bcc17713507abf6a2edc0842ece92
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-06 18:42:03 +0200

    refac: Prefix basis data metering point periods with 'hive_' (#2528)
    * hive_ prefix
    
    * Add ToDo

--- source/databricks/calculation_engine/tests/calculation/basis_data/test_basis_data_factory.py
+++ source/databricks/calculation_engine/tests/calculation/basis_data/test_basis_data_factory.py
@@ -28,7 +28,7 @@ from package.calculation.basis_data.schemas.grid_loss_metering_points_schema imp
     grid_loss_metering_points_schema,
 )
 from package.calculation.basis_data.schemas.metering_point_period_schema import (
-    metering_point_period_schema,
+    hive_metering_point_period_schema,
 )
 from package.calculation.basis_data.schemas.time_series_point_schema import (
     time_series_point_schema,
@@ -43,7 +43,7 @@ from tests.calculation.basis_data.basis_data_test_factory import (
     [
         (
             "metering_point_periods",
-            metering_point_period_schema,
+            hive_metering_point_period_schema,
         ),
         (
             "time_series_points",
