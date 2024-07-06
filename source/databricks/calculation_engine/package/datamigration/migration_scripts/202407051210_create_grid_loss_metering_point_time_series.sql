647c403624665b4b817133169b0ec426ec18cdf5
Bjarke Meier <hej@bjarkemeier.dk>
2024-07-06 19:47:31 +0200

    fix/update migration scripts

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202407051210_create_grid_loss_metering_point_time_series.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202407051210_create_grid_loss_metering_point_time_series.sql
@@ -5,16 +5,16 @@ CREATE TABLE IF NOT EXISTS {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_N
     grid_area_code STRING NOT NULL,
     energy_supplier_id STRING,
     balance_responsible_id STRING,
+    metering_point_type STRING NOT NULL,
+    settlement_method STRING,
+    metering_point_id STRING NOT NULL,
+    resolution STRING NOT NULL,
+    -- The time when the energy was consumed/produced/exchanged
+    time TIMESTAMP NOT NULL,
     -- Energy quantity in kWh for the given observation time.
     -- Example: 1234.534
     quantity DECIMAL(18, 3) NOT NULL,
-    quantity_qualities ARRAY<STRING> NOT NULL,
-    -- The time when the energy was consumed/produced/exchanged
-    time TIMESTAMP NOT NULL,
-    metering_point_type STRING NOT NULL
-    settlement_method STRING
-    metering_point_id STRING NOT NULL,
-    resolution STRING NOT NULL
+    quantity_qualities ARRAY<STRING> NOT NULL
 )
 USING DELTA
 TBLPROPERTIES (
@@ -24,8 +24,10 @@ TBLPROPERTIES (
     delta.constraints.grid_area_code_chk = "LENGTH ( grid_area_code ) = 3",
     delta.constraints.energy_supplier_id_chk = "energy_supplier_id IS NULL OR LENGTH ( energy_supplier_id ) = 13 OR LENGTH ( energy_supplier_id ) = 16",
     delta.constraints.balance_responsible_id_chk = "balance_responsible_id IS NULL OR LENGTH ( balance_responsible_id ) = 13 OR LENGTH ( balance_responsible_id ) = 16",
-    delta.constraints.quantity_qualities_chk = "array_size ( array_except ( quantity_qualities , array ( 'missing' , 'calculated' , 'measured' , 'estimated' ) ) ) = 0 AND array_size ( quantity_qualities ) > 0",
+    delta.constraints.metering_point_type_chk = "metering_point_type IN ( 'production' , 'consumption')",
+    delta.constraints.settlement_method_chk = "settlement_method IS NULL OR ( metering_point_type = 'consumption' AND settlement_method IN ( 'non_profiled' , 'flex' ) )",
     delta.constraints.metering_point_id_chk = "metering_point_id IS NULL OR LENGTH ( metering_point_id ) = 18",
     delta.constraints.metering_point_id_conditional_chk = "( time_series_type IN ( 'negative_grid_loss' , 'positive_grid_loss' ) AND metering_point_id IS NOT NULL ) OR ( time_series_type NOT IN ( 'negative_grid_loss' , 'positive_grid_loss' ) AND metering_point_id IS NULL )",
-    delta.constraints.resolution_chk = "resolution IN ( 'PT15M' , 'PT1H' )"
+    delta.constraints.resolution_chk = "resolution IN ( 'PT15M' , 'PT1H' )",
+    delta.constraints.quantity_qualities_chk = "array_size ( array_except ( quantity_qualities , array ( 'missing' , 'calculated' , 'measured' , 'estimated' ) ) ) = 0 AND array_size ( quantity_qualities ) > 0"
 )
