afbb235effdc96bc051096544d51d8a8ae734d64
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-07-08 14:41:47 +0200

    feat: Use EF Core as DB SQL mapper. (#2504)

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/Extensions/DependencyInjection/CalculationResultsExtensions.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/Extensions/DependencyInjection/CalculationResultsExtensions.cs
@@ -20,6 +20,7 @@ using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.CalculationR
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.DataLake;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.JsonSerialization;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.Persistence;
+using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.Persistence.Databricks;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.Persistence.SettlementReportRequest;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.SettlementReports;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.SettlementReports_v2;
@@ -108,13 +109,12 @@ public static class CalculationResultsExtensions
         services.AddScoped<ISettlementReportDownloadHandler, SettlementReportDownloadHandler>();
         services.AddScoped<IUpdateFailedSettlementReportsHandler, UpdateFailedSettlementReportsHandler>();
 
+        services.AddScoped<ISettlementReportDatabricksContext, SettlementReportDatabricksContext>();
         services.AddScoped<ISettlementReportRepository, SettlementReportRepository>();
         services.AddScoped<IRemoveExpiredSettlementReports, RemoveExpiredSettlementReports>();
         services.AddScoped<ISettlementReportFileGeneratorFactory, SettlementReportFileGeneratorFactory>();
         services.AddScoped<ISettlementReportWholesaleRepository, SettlementReportWholesaleRepository>();
-        services.AddScoped<ISettlementReportWholesaleResultQueries, SettlementReportWholesaleResultQueries>();
         services.AddScoped<ISettlementReportEnergyResultRepository, SettlementReportEnergyResultRepository>();
-        services.AddScoped<ISettlementReportEnergyResultQueries, SettlementReportEnergyResultQueries>();
         services.AddScoped<ISettlementReportMeteringPointTimeSeriesResultRepository, SettlementReportMeteringPointTimeSeriesResultRepository>();
         services.AddScoped<ISettlementReportMeteringPointTimeSeriesResultQueries, SettlementReportMeteringPointTimeSeriesResultQueries>();
         services.AddScoped<ISettlementReportChargeLinkPeriodsRepository, SettlementReportChargeLinkPeriodsRepository>();
