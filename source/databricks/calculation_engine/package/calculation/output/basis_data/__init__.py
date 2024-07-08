fef8ad29bc1881165c4ea76750bcf8a7ff48edbe
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-08 10:31:56 +0200

    ...

--- source/databricks/calculation_engine/package/calculation/output/basis_data/__init__.py
+++ source/databricks/calculation_engine/package/calculation/output/basis_data/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from basis_data_results import write_basis_data
+from .basis_data_results import write_basis_data
