a93125b8fcbf4ce092ead2f17014d4e0a59555bd
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-08 16:54:27 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202407081400_Alter_monthly_amounts_tables.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202407081400_Alter_monthly_amounts_tables.sql
@@ -1,9 +1,21 @@
+ALTER TABLE {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.monthly_amounts_per_charge SET TBLPROPERTIES (
+    'delta.columnMapping.mode' = 'name',
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '5')
+GO
+
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.monthly_amounts_per_charge
     DROP COLUMNS (calculation_type, calculation_execution_time_start)
 GO
 
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.monthly_amounts_per_charge
     RENAME COLUMN calculation_result_id TO result_id
+GO
+
+ALTER TABLE {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.total_monthly_amounts SET TBLPROPERTIES (
+    'delta.columnMapping.mode' = 'name',
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '5')
 GO
 
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.total_monthly_amounts
