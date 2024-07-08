3c2cf9c61b2772ce8cfb4a08f94e1cc5968387ba
Alexander <alexanderharrow@hotmail.com>
2024-07-08 12:39:21 +0200

    Update schema_config.py

--- source/databricks/calculation_engine/package/datamigration_hive/schema_config.py
+++ source/databricks/calculation_engine/package/datamigration_hive/schema_config.py
@@ -15,6 +15,7 @@ from spark_sql_migrations import Schema, Table, View
 
 import package.calculation.output.basis_data.schemas as basis_data_schemas
 import package.infrastructure.paths as paths
+
 # calculation_input
 from package.calculation.input.schemas.grid_loss_metering_points_schema import (
     grid_loss_metering_points_schema,
@@ -28,6 +29,7 @@ from package.calculation.output.results.schemas import (
 from package.calculation.output.results.schemas import (
     total_monthly_amounts_schema,
 )
+
 # calculation_output
 from package.calculation.output.results.schemas.wholesale_results_schema import (
     wholesale_results_schema,
