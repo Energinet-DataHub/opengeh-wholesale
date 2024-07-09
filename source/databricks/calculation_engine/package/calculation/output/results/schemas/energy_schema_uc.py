e584b40f546aaa562c9b16c7c9dbc95a91e9af25
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-07-09 13:55:59 +0200

    refac: Remove `_ga` from names (#2541)

--- source/databricks/calculation_engine/package/calculation/output/results/schemas/energy_schema_uc.py
+++ source/databricks/calculation_engine/package/calculation/output/results/schemas/energy_schema_uc.py
@@ -6,17 +6,19 @@ from package.calculation.output.output_table_column_names import (
 
 nullable = True
 
-exchange_per_neighbor_ga_schema_uc = t.StructType(
+energy_schema_uc = t.StructType(
     [
         t.StructField(cname.calculation_id, t.StringType(), not nullable),
         t.StructField(cname.result_id, t.StringType(), not nullable),
         t.StructField(cname.grid_area_code, t.StringType(), not nullable),
-        t.StructField(cname.neighbor_grid_area_code, t.StringType(), not nullable),
+        t.StructField(cname.time_series_type, t.StringType(), not nullable),
         t.StructField(cname.resolution, t.StringType(), not nullable),
         t.StructField(cname.time, t.TimestampType(), not nullable),
         t.StructField(cname.quantity, t.DecimalType(18, 3), not nullable),
         t.StructField(
-            cname.quantity_qualities, t.ArrayType(t.StringType()), not nullable
+            cname.quantity_qualities,
+            t.ArrayType(t.StringType()),
+            not nullable,
         ),
     ]
 )
