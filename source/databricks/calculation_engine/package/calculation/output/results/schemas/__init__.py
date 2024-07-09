faf9fee4792f27c715e28ba1ad6cfeb4881d8233
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-09 14:57:55 +0200

    ...

--- source/databricks/calculation_engine/package/calculation/output/results/schemas/__init__.py
+++ source/databricks/calculation_engine/package/calculation/output/results/schemas/__init__.py
@@ -15,15 +15,14 @@
 
 from .hive_energy_results_schema import hive_energy_results_schema
 from .energy_schema_uc import energy_schema_uc
-from .amounts_per_charge_schema import amounts_per_charge_schema
-from .energy_results_schema import hive_energy_results_schema
-from .energy_per_ga_schema_uc import energy_per_ga_schema_uc
 from .energy_per_brp_schema_uc import energy_per_brp_schema_uc
 from .energy_per_es_schema_uc import energy_per_es_schema_uc
 from .exchange_per_neighbor_schema_uc import exchange_per_neighbor_schema_uc
 from .grid_loss_metering_point_time_series_schema_uc import (
     grid_loss_metering_point_time_series_schema_uc,
 )
+from .amounts_per_charge_schema import amounts_per_charge_schema
+from .hive_wholesale_results_schema import hive_wholesale_results_schema
 from .monthly_amounts_schema import (
     hive_monthly_amounts_schema,
     monthly_amounts_schema_uc,
@@ -32,4 +31,3 @@ from .total_monthly_amounts_schema import (
     hive_total_monthly_amounts_schema,
     total_monthly_amounts_schema_uc,
 )
-from .hive_wholesale_results_schema import hive_wholesale_results_schema
