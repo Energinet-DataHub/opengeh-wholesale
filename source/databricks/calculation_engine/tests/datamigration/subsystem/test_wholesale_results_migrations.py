d2c3bcbb808204b0e040de77c95ad849c9454e26
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-09 07:26:07 +0200

    ...

--- source/databricks/calculation_engine/tests/datamigration/subsystem/test_wholesale_results_migrations.py
+++ source/databricks/calculation_engine/tests/datamigration/subsystem/test_wholesale_results_migrations.py
@@ -23,18 +23,12 @@ from pyspark.sql.functions import col, lit
 from package.calculation.output.results.schemas import (
     hive_wholesale_results_schema,
 )
-from package.codelists import (
-    AmountType,
-    ChargeQuality,
-    ChargeType,
-    ChargeUnit,
-    MeteringPointType,
-    CalculationType,
-    SettlementMethod,
-    WholesaleResultResolution,
-)
+
 from package.constants import WholesaleResultColumnNames
-from package.infrastructure.paths import HiveOutputDatabase
+from package.infrastructure.paths import (
+    HiveOutputDatabase,
+    WholesaleResultsInternalDatabase,
+)
 from tests.helpers.data_frame_utils import set_column
 
 
@@ -71,13 +65,8 @@ def _create_df(spark: SparkSession) -> DataFrame:
     [
         (WholesaleResultColumnNames.calculation_id, None),
         (WholesaleResultColumnNames.calculation_id, "not-a-uuid"),
-        (WholesaleResultColumnNames.calculation_type, None),
-        (WholesaleResultColumnNames.calculation_type, "foo"),
-        (WholesaleResultColumnNames.calculation_execution_time_start, None),
         (WholesaleResultColumnNames.calculation_result_id, None),
-        (WholesaleResultColumnNames.calculation_result_id, "not-a-uuid"),
-        (WholesaleResultColumnNames.amount_type, None),
-        (WholesaleResultColumnNames.amount_type, "foo"),
+        (WholesaleResultColumnNames.result_id, "not-a-uuid"),
         (WholesaleResultColumnNames.grid_area_code, None),
         (WholesaleResultColumnNames.grid_area_code, "12"),
         (WholesaleResultColumnNames.grid_area_code, "1234"),
@@ -93,9 +82,11 @@ def _create_df(spark: SparkSession) -> DataFrame:
         (WholesaleResultColumnNames.time, None),
         (WholesaleResultColumnNames.resolution, None),
         (WholesaleResultColumnNames.resolution, "foo"),
+        (WholesaleResultColumnNames.metering_point_type, None),
         (WholesaleResultColumnNames.metering_point_type, "foo"),
         (WholesaleResultColumnNames.settlement_method, "foo"),
         (WholesaleResultColumnNames.charge_owner_id, "neither-16-nor-13-digits-long"),
+        (WholesaleResultColumnNames.is_tax, None),
     ],
 )
 def test__migrated_table_rejects_invalid_data(
@@ -112,7 +103,7 @@ def test__migrated_table_rejects_invalid_data(
     # Act
     with pytest.raises(Exception) as ex:
         invalid_df.write.format("delta").option("mergeSchema", "false").insertInto(
-            f"{HiveOutputDatabase.DATABASE_NAME}.{HiveOutputDatabase.WHOLESALE_RESULT_TABLE_NAME}",
+            f"{WholesaleResultsInternalDatabase.DATABASE_NAME}.{WholesaleResultsInternalDatabase.AMOUNTS_PER_CHARGE_TABLE_NAME}",
             overwrite=False,
         )
 
@@ -144,10 +135,9 @@ actor_eic = "1234567890123456"
         ),
         (WholesaleResultColumnNames.calculation_type, "wholesale_fixing"),
         (
-            WholesaleResultColumnNames.calculation_result_id,
+            WholesaleResultColumnNames.result_id,
             "9252d7a0-4363-42cc-a2d6-e04c026523f8",
         ),
-        (WholesaleResultColumnNames.amount_type, "amount_per_charge"),
         (WholesaleResultColumnNames.grid_area_code, "123"),
         (WholesaleResultColumnNames.grid_area_code, "007"),
         (WholesaleResultColumnNames.energy_supplier_id, actor_gln),
@@ -159,7 +149,6 @@ actor_eic = "1234567890123456"
         (WholesaleResultColumnNames.quantity_qualities, None),
         (WholesaleResultColumnNames.time, datetime(2020, 1, 1, 0, 0)),
         (WholesaleResultColumnNames.resolution, "P1D"),
-        (WholesaleResultColumnNames.metering_point_type, None),
         (WholesaleResultColumnNames.metering_point_type, "consumption"),
         (WholesaleResultColumnNames.settlement_method, None),
         (WholesaleResultColumnNames.settlement_method, "flex"),
@@ -168,7 +157,6 @@ actor_eic = "1234567890123456"
         (WholesaleResultColumnNames.price, min_18_6_decimal),
         (WholesaleResultColumnNames.amount, max_18_6_decimal),
         (WholesaleResultColumnNames.amount, min_18_6_decimal),
-        (WholesaleResultColumnNames.is_tax, None),
         (WholesaleResultColumnNames.charge_code, "any-string"),
         (WholesaleResultColumnNames.charge_type, "fee"),
         (WholesaleResultColumnNames.charge_owner_id, actor_gln),
@@ -181,57 +169,6 @@ def test__migrated_table_accepts_valid_data(
     column_value: str | list,
     migrations_executed: None,
 ) -> None:
-    # Arrange
-    result_df = _create_df(spark)
-    result_df = set_column(result_df, column_name, column_value)
-
-    # Act and assert: Expectation is that no exception is raised
-    result_df.write.format("delta").option("mergeSchema", "false").insertInto(
-        f"{HiveOutputDatabase.DATABASE_NAME}.{HiveOutputDatabase.WHOLESALE_RESULT_TABLE_NAME}"
-    )
-
-
-@pytest.mark.parametrize(
-    "column_name,column_value",
-    [
-        *[
-            (WholesaleResultColumnNames.calculation_type, x)
-            for x in [
-                CalculationType.WHOLESALE_FIXING.value,
-                CalculationType.FIRST_CORRECTION_SETTLEMENT.value,
-                CalculationType.SECOND_CORRECTION_SETTLEMENT.value,
-                CalculationType.THIRD_CORRECTION_SETTLEMENT.value,
-            ]
-        ],
-        *[(WholesaleResultColumnNames.quantity_unit, x.value) for x in ChargeUnit],
-        *[
-            (WholesaleResultColumnNames.quantity_qualities, [x.value])
-            for x in ChargeQuality
-        ],
-        *[
-            (WholesaleResultColumnNames.resolution, x.value)
-            for x in WholesaleResultResolution
-        ],
-        *[
-            (WholesaleResultColumnNames.metering_point_type, x.value)
-            for x in MeteringPointType
-        ],
-        *[
-            (WholesaleResultColumnNames.settlement_method, x.value)
-            for x in SettlementMethod
-        ],
-        *[(WholesaleResultColumnNames.charge_type, x.value) for x in ChargeType],
-        *[(WholesaleResultColumnNames.amount_type, x.value) for x in AmountType],
-    ],
-)
-def test__migrated_table_accepts_enum_value(
-    spark: SparkSession,
-    column_name: str,
-    column_value: str,
-    migrations_executed: None,
-) -> None:
-    "Test that all enum values are accepted by the delta table"
-
     # Arrange
     result_df = _create_df(spark)
     result_df = set_column(result_df, column_name, column_value)
@@ -276,28 +213,3 @@ def test__migrated_table_does_not_round_valid_decimal(
         f"{HiveOutputDatabase.DATABASE_NAME}.{HiveOutputDatabase.WHOLESALE_RESULT_TABLE_NAME}"
     ).where(col(WholesaleResultColumnNames.calculation_id) == calculation_id)
     assert actual_df.collect()[0].quantity == quantity
-
-
-# ToDo JMG: Remove when on Unity Catalog
-def test__wholesale_results_table__is_not_managed(
-    spark: SparkSession, migrations_executed: None
-) -> None:
-    """
-    It is desired that the table is unmanaged to provide for greater flexibility.
-    According to https://learn.microsoft.com/en-us/azure/databricks/lakehouse/data-objects#--what-is-a-database:
-    "To manage data life cycle independently of database, save data to a location that is not nested under any database locations."
-    Thus we check whether the table is managed by comparing its location to the location of the database/schema.
-    """
-    database_details = spark.sql(
-        f"DESCRIBE DATABASE {HiveOutputDatabase.DATABASE_NAME}"
-    )
-    table_details = spark.sql(
-        f"DESCRIBE DETAIL {HiveOutputDatabase.DATABASE_NAME}.{HiveOutputDatabase.WHOLESALE_RESULT_TABLE_NAME}"
-    )
-
-    database_location = database_details.where(
-        col("info_name") == "Location"
-    ).collect()[0]["info_value"]
-    table_location = table_details.collect()[0]["location"]
-
-    assert not table_location.startswith(database_location)
