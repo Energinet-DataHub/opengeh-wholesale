12a047d50e36c1833623c99ef30efcc102d950d0
Jonas Michelsen <jvm@energinet.dk>
2024-07-10 15:16:14 +0200

    script

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202407101400_create_grid_loss_metering_points.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202407101400_create_grid_loss_metering_points.sql
@@ -9,3 +9,12 @@ TBLPROPERTIES (
     delta.constraints.calculation_id_chk = "LENGTH ( calculation_id ) = 36",
     delta.constraints.metering_point_id_chk = "LENGTH ( metering_point_id ) = 18"
 )
+GO
+
+IF EXISTS (SELECT * FROM information_schema.tables
+           WHERE table_schema = '{HIVE_BASIS_DATA_DATABASE_NAME}'
+           AND table_name = 'grid_loss_metering_points')
+BEGIN
+    INSERT INTO {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.grid_loss_metering_points
+    SELECT * FROM {HIVE_BASIS_DATA_DATABASE_NAME}.grid_loss_metering_points
+END
