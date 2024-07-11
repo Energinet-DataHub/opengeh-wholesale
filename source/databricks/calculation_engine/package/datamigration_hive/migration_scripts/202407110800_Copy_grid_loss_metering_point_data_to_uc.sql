0e2e7719b7559bffe68cb56e925f4f330d40608c
Jonas Michelsen <jvm@energinet.dk>
2024-07-11 11:23:25 +0200

    Update 202407110800_Copy_grid_loss_metering_point_data_to_uc.sql

--- source/databricks/calculation_engine/package/datamigration_hive/migration_scripts/202407110800_Copy_grid_loss_metering_point_data_to_uc.sql
+++ source/databricks/calculation_engine/package/datamigration_hive/migration_scripts/202407110800_Copy_grid_loss_metering_point_data_to_uc.sql
@@ -1,5 +1,7 @@
-TRUNCATE TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.grid_loss_metering_points
-GO
-
 INSERT INTO {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.grid_loss_metering_points
 SELECT * FROM {INPUT_DATABASE_NAME}.grid_loss_metering_points
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.grid_loss_metering_points 
+    WHERE {INPUT_DATABASE_NAME}.grid_loss_metering_points.id = {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.grid_loss_metering_points.id
+)
