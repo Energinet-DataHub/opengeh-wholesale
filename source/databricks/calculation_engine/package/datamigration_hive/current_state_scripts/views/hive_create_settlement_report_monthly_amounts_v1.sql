873187418a5142c5603c2092b0d7b30ace93c257
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-11 10:47:35 +0200

    refac: Prefix variables to "hive_settlement_report_database_name" (#2557)
    * rename
    
    * ...

--- source/databricks/calculation_engine/package/datamigration_hive/current_state_scripts/views/hive_create_settlement_report_monthly_amounts_v1.sql
+++ source/databricks/calculation_engine/package/datamigration_hive/current_state_scripts/views/hive_create_settlement_report_monthly_amounts_v1.sql
@@ -1,4 +1,4 @@
-CREATE VIEW IF NOT EXISTS {SETTLEMENT_REPORT_DATABASE_NAME}.monthly_amounts_v1 as
+CREATE VIEW IF NOT EXISTS {HIVE_SETTLEMENT_REPORT_DATABASE_NAME}.monthly_amounts_v1 as
 SELECT c.calculation_id,
        c.calculation_type,
        c.version as calculation_version,
