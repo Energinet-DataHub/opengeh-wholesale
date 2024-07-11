68f8b4aa2477c92578e396ff9bca49f15e846d6f
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-07-11 12:18:57 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202407101600_create_calculations.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202407101600_create_calculations.sql
@@ -14,7 +14,7 @@ TBLPROPERTIES (
     delta.columnMapping.mode = 'name',
     delta.constraints.calculation_id_chk = "LENGTH ( calculation_id ) = 36",
     delta.constraints.calculation_type_chk = "calculation_type IN ( 'balance_fixing' , 'aggregation' , 'wholesale_fixing' , 'first_correction_settlement' , 'second_correction_settlement' , 'third_correction_settlement' )",
-    delta.constraints.created_by_user_id_chk = "LENGTH ( created_by_user_id ) = 36"
+    delta.constraints.created_by_user_id_chk = "LENGTH ( created_by_user_id ) = 36",
     delta.constraints.calculation_version_chk = "calculation_version > 0",
     delta.constraints.calculation_period_chk = "calculation_period_end > calculation_period_start"
 )
