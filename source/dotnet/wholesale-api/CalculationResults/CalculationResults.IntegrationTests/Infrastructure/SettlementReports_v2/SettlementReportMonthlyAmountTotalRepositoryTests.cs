8650ec1308980f3afe6a4fc02a3904d5e0402969
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-07-12 11:47:04 +0200

    feat: add Metering Point Master data And Monthly Amounts for balance fixing (#2544)
    * Create SettlementReportMeteringPointLatestMasterDataCountQueryStatement.cs
    
    * Copied files, not edited yet
    
    * First implementation of count pr. day for mpmaster
    
    * Implemented Count query for latest for metering point masterdata
    
    * Implemented get and count normally
    
    * Implemented pr energysupplier .start
    
    * Update filter and test.
    
    * Fixed count and get statements
    
    * Fixed prES query, aligned query text and updated test
    
    * Converted to EF Core syntax
    
    * Added file back to settlement report files
    
    also updated test fixture
    
    * Update SettlementReportMeteringPointMasterDataRepositoryTests.cs
    
    * Update SettlementReportMeteringPointMasterDataRepository.cs
    
    * Updated queries after review
    
    * Update SettlementReportMeteringPointMasterDataRepository.cs
    
    * Refactored Monthly Amounts to EF syntax
    
    * Update SettlementReportMeteringPointMasterDataRepositoryTests.cs
    
    * Removed unneeded usings, and removed commented code

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Infrastructure/SettlementReports_v2/SettlementReportMonthlyAmountTotalRepositoryTests.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Infrastructure/SettlementReports_v2/SettlementReportMonthlyAmountTotalRepositoryTests.cs
@@ -14,23 +14,18 @@
 
 using AutoFixture;
 using Energinet.DataHub.Core.TestCommon;
+using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.Persistence.Databricks;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.SettlementReports_v2;
 using Energinet.DataHub.Wholesale.CalculationResults.Infrastructure.SettlementReports_v2.Statements;
 using Energinet.DataHub.Wholesale.CalculationResults.IntegrationTests.Fixtures;
-using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.SettlementReports;
 using Energinet.DataHub.Wholesale.CalculationResults.Interfaces.SettlementReports_v2.Models;
-using Energinet.DataHub.Wholesale.Calculations.Interfaces;
-using Energinet.DataHub.Wholesale.Calculations.Interfaces.Models;
-using Energinet.DataHub.Wholesale.Common.Infrastructure.Options;
 using Energinet.DataHub.Wholesale.Common.Interfaces.Models;
-using FluentAssertions;
-using Microsoft.Extensions.Options;
-using Moq;
 using Xunit;
 
 namespace Energinet.DataHub.Wholesale.CalculationResults.IntegrationTests.Infrastructure.SettlementReports_v2;
 
-public class SettlementReportMonthlyAmountTotalRepositoryTests : TestBase<SettlementReportMonthlyAmountTotalRepository>, IClassFixture<MigrationsFreeDatabricksSqlStatementApiFixture>
+[Collection(nameof(SettlementReportCollectionFixture))]
+public class SettlementReportMonthlyAmountTotalRepositoryTests : TestBase<SettlementReportMonthlyAmountTotalRepository>
 {
     private readonly MigrationsFreeDatabricksSqlStatementApiFixture _databricksSqlStatementApiFixture;
 
@@ -38,17 +33,11 @@ public class SettlementReportMonthlyAmountTotalRepositoryTests : TestBase<Settle
     {
         _databricksSqlStatementApiFixture = databricksSqlStatementApiFixture;
 
-        var mockedOptions = new Mock<IOptions<DeltaTableOptions>>();
-        mockedOptions.Setup(x => x.Value).Returns(new DeltaTableOptions
-        {
-            SettlementReportSchemaName = _databricksSqlStatementApiFixture.DatabricksSchemaManager.DeltaTableOptions.Value.SCHEMA_NAME,
-            SCHEMA_NAME = _databricksSqlStatementApiFixture.DatabricksSchemaManager.DeltaTableOptions.Value.SCHEMA_NAME,
-        });
+        _databricksSqlStatementApiFixture.DatabricksSchemaManager.DeltaTableOptions.Value.SettlementReportSchemaName =
+            databricksSqlStatementApiFixture.DatabricksSchemaManager.DeltaTableOptions.Value.SCHEMA_NAME;
 
-        Fixture.Inject(mockedOptions);
-        Fixture.Inject(_databricksSqlStatementApiFixture.GetDatabricksExecutor());
-        Fixture.Inject<ISettlementReportMonthlyAmountTotalQueries>(new SettlementReportMonthlyAmountTotalQueries(
-            mockedOptions.Object,
+        Fixture.Inject<ISettlementReportDatabricksContext>(new SettlementReportDatabricksContext(
+            _databricksSqlStatementApiFixture.DatabricksSchemaManager.DeltaTableOptions,
             _databricksSqlStatementApiFixture.GetDatabricksExecutor()));
     }
 
