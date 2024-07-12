4d0f353a73cb136b38ca8ff78b1dfa149b03a6f1
Jonas Michelsen <jvm@energinet.dk>
2024-07-12 12:50:50 +0200

    remove hive grid loss metering points table

--- source/databricks/calculation_engine/package/datamigration_hive/schema_config.py
+++ source/databricks/calculation_engine/package/datamigration_hive/schema_config.py
@@ -57,18 +57,6 @@ schema_config = [
         ],
     ),
     Schema(
-        # Tables in this schema are externals and schemas are not defined in the SQL scripts.
-        # This will be changed to Views in the future.
-        name=paths.InputDatabase.DATABASE_NAME,
-        tables=[
-            Table(
-                name=paths.InputDatabase.GRID_LOSS_METERING_POINTS_TABLE_NAME,
-                schema=grid_loss_metering_points_schema,
-            )
-        ],
-        views=[],
-    ),
-    Schema(
         name=paths.HiveBasisDataDatabase.DATABASE_NAME,
         tables=[
             Table(
