16f8c47511eef20845c9ce9114bf4b365478739d
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-07-15 09:05:01 +0200

    Fix tests.

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Infrastructure/SettlementReports_v2/SettlementReportWholesaleRepositoryTests.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.IntegrationTests/Infrastructure/SettlementReports_v2/SettlementReportWholesaleRepositoryTests.cs
@@ -184,7 +184,7 @@ public class SettlementReportWholesaleRepositoryTests : TestBase<SettlementRepor
     [Theory]
     [InlineData("8397670583104", 1, MarketRole.EnergySupplier, null)]
     [InlineData(null, 3, MarketRole.EnergySupplier, null)]
-    [InlineData("8397670583119", 1, MarketRole.GridAccessProvider, "7397670583109")]
+    [InlineData("8397670583119", 3, MarketRole.GridAccessProvider, "7397670583109")]
     [InlineData(null, 2, MarketRole.GridAccessProvider, "7397670583119")]
     [InlineData("8397670583129", 2, MarketRole.SystemOperator, "7397670583129")]
     [InlineData(null, 1, MarketRole.SystemOperator, "7397670583139")]
