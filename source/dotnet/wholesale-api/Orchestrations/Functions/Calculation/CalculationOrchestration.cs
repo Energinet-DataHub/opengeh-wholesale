0020140ac2f54a55543c52146e09ca7fa6c8f773
Dan Stenrøjl <XDAST@energinet.dk>
2024-07-23 15:58:13 +0200

    Remove TODO

--- source/dotnet/wholesale-api/Orchestrations/Functions/Calculation/CalculationOrchestration.cs
+++ source/dotnet/wholesale-api/Orchestrations/Functions/Calculation/CalculationOrchestration.cs
@@ -111,8 +111,6 @@ internal class CalculationOrchestration
                 new CreateCompletedCalculationInput(calculationMetadata.Id, context.InstanceId),
                 defaultRetryOptions);
 
-            //// TODO XDAST: Wait for warehouse to start (could use retry policy); could be done using fan-out/fan-in
-
             // TODO XDAST: Add retry policy when we have removed the old integration events
             // Send calculation results (ServiceBus)
             await context.CallActivityAsync(
