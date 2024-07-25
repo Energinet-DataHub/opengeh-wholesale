391b94b5ebb7d21446629f763a02a7fb64380650
Dan Stenrøjl <XDAST@energinet.dk>
2024-07-25 14:29:44 +0200

    Use AddCalculationResultsModule instead of V2

--- source/dotnet/wholesale-api/Orchestrations/Program.cs
+++ source/dotnet/wholesale-api/Orchestrations/Program.cs
@@ -53,7 +53,7 @@ var host = new HostBuilder()
 
         // Modules
         services.AddCalculationsModule(context.Configuration);
-        services.AddCalculationResultsV2Module(context.Configuration);
+        services.AddCalculationResultsModule(context.Configuration);
 
         // => Sub-modules of Events
         services.AddIntegrationEventsPublishing(context.Configuration);
