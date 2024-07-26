428d5299445e46167f78ece38d8889abf3be2f4f
Jeppe M <jmw@energinet.dk>
2024-07-26 09:48:32 +0200

    Migrations/Adjust: Add alert for missing receival of Charge Links (#1772)

--- migrations/terraform/env/prod_001/alerting-additional.tf
+++ migrations/terraform/env/prod_001/alerting-additional.tf
@@ -56,6 +56,35 @@ resource "azurerm_monitor_metric_alert" "dropzoneunzipper_ch_metric_alert" {
   window_size = "P1D"
 }
 
+resource "azurerm_monitor_metric_alert" "dropzoneunzipper_cl_metric_alert" {
+  name                = "alert-dropzoneunzipper-not-received-cl-metric-${local.resources_suffix}"
+  resource_group_name = azurerm_resource_group.this.name
+  scopes              = [azurerm_eventgrid_system_topic.st_dh2data.id]
+  description         = "Check every 1 hour if there has been any CL data received within the last 24 hours, alert if not"
+  severity            = 1
+  enabled             = false
+
+  criteria {
+    metric_namespace = "Microsoft.EventGrid/systemTopics"
+    metric_name      = "DeliverySuccessCount"
+    aggregation      = "Count"
+    operator         = "LessThanOrEqual"
+    threshold        = 0
+    dimension {
+      name     = "EventSubscriptionName"
+      operator = "Include"
+      values   = [azurerm_eventgrid_system_topic_event_subscription.dh2data_charge_links.name]
+    }
+  }
+
+  action {
+    action_group_id = module.monitor_action_group_mig[0].id
+  }
+
+  frequency   = "PT1H"
+  window_size = "P1D"
+}
+
 resource "azurerm_monitor_metric_alert" "timeseriessynchronization_ts_metric_alert" {
   name                = "alert-timeseriessynchronization-ts-sync-not-received-${local.resources_suffix}"
   resource_group_name = azurerm_resource_group.this.name
