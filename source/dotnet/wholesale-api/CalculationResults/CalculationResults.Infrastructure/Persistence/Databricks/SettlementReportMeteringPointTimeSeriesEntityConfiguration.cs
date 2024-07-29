4dd053e304a669af441092716d51779443d71412
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-07-29 14:52:40 +0200

    perf: Revert view name after test.

--- source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/Persistence/Databricks/SettlementReportMeteringPointTimeSeriesEntityConfiguration.cs
+++ source/dotnet/wholesale-api/CalculationResults/CalculationResults.Infrastructure/Persistence/Databricks/SettlementReportMeteringPointTimeSeriesEntityConfiguration.cs
@@ -21,7 +21,7 @@ public sealed class SettlementReportMeteringPointTimeSeriesEntityConfiguration :
 {
     public void Configure(EntityTypeBuilder<SettlementReportMeteringPointTimeSeriesEntity> builder)
     {
-        builder.ToTable("metering_point_time_series_v3");
+        builder.ToTable("metering_point_time_series_v1");
         builder.HasNoKey();
     }
 }
