902e7edcdf75d2f5eb6a2b0ff5e205c2409ccd8d
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2024-08-01 15:08:16 +0200

    migrated rest of subsystems to use the new PIM data plane (#1795)

--- dh2-bridge/terraform/main/resource-groups.tf
+++ dh2-bridge/terraform/main/resource-groups.tf
@@ -36,16 +36,15 @@ data "azurerm_role_definition" "locks_contributor_access" {
   scope = data.azurerm_subscription.this.id
 }
 
-# Readd after the previous assignment has been deleted
-# module "pim_contributor_security_group_permissions" {
-#   count = var.pim_contributor_data_plane_group_name != "" ? 1 : 0
-#
-#   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/resource-group-role-assignments?ref=resource-group-role-assignments_2.0.0"
-#
-#   resource_group_name = azurerm_resource_group.this.name
-#   security_group_name = var.pim_contributor_data_plane_group_name
-#   role_level          = "Contributor Data Plane"
-# }
+module "pim_contributor_security_group_permissions" {
+  count = var.pim_contributor_data_plane_group_name != "" ? 1 : 0
+
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/resource-group-role-assignments?ref=resource-group-role-assignments_2.0.0"
+
+  resource_group_name = azurerm_resource_group.this.name
+  security_group_name = var.pim_contributor_data_plane_group_name
+  role_level          = "Contributor Data Plane"
+}
 
 module "pim_contributor_control_plane_security_group_permissions" {
   count = var.pim_contributor_control_plane_group_name != "" ? 1 : 0
