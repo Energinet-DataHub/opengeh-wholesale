98c714f406a117f699b84055f0eb604d632884b9
Lars Nielsen <lanni@energinet.dk>
2024-08-02 18:58:53 +0200

    Use new modules on settlement-report (#1799)

--- settlement-report/terraform/main/func-settlement-reports-df.tf
+++ settlement-report/terraform/main/func-settlement-reports-df.tf
@@ -1,5 +1,5 @@
 module "func_settlement_reports_df" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app-elastic?ref=14.19.1"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app-elastic?ref=function-app-elastic_4.0.1"
 
   name                                   = "settlement-reports-df"
   project_name                           = var.domain_name_short
@@ -39,7 +39,7 @@ module "func_settlement_reports_df" {
 }
 
 module "kvs_func_settlement_reports_df_base_url" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=14.19.1"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=key-vault-secret_4.0.1"
 
   name         = "func-settlement-reports-df-base-url"
   value        = "https://${module.func_settlement_reports_df.default_hostname}"
--- settlement-report/terraform/main/mssqldb-settlement-report.tf
+++ settlement-report/terraform/main/mssqldb-settlement-report.tf
@@ -4,7 +4,7 @@ data "azurerm_mssql_server" "mssqlsrv" {
 }
 
 module "mssqldb_settlement_report" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=14.32.0"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=mssql-database_4.0.1"
 
   name                 = "settlement-report"
   location             = azurerm_resource_group.this.location
@@ -22,7 +22,7 @@ module "mssqldb_settlement_report" {
 }
 
 module "kvs_sql_ms_settlement_report_database_name" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=14.22.0"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=key-vault-secret_4.0.1"
 
   name         = "mssql-settlement-report-database-name"
   value        = module.mssqldb_settlement_report.name
