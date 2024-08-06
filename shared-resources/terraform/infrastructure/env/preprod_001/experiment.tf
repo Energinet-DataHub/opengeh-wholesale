99122130d59f913bf8d5eb82f50e86cad5e76e9d
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2024-08-06 15:15:28 +0200

    Fix role assignment (#1825)

--- shared-resources/terraform/infrastructure/env/preprod_001/experiment.tf
+++ shared-resources/terraform/infrastructure/env/preprod_001/experiment.tf
@@ -1,11 +1,15 @@
+data "azurerm_resource_group" "vnet_resource_group" {
+  name = data.azurerm_virtual_network.this.resource_group_name
+}
+
 resource "azurerm_role_assignment" "dbj_aadadmin" {
-  scope                = data.azurerm_virtual_network.this.resource_group_name
+  scope                = data.azurerm_resource_group.vnet_resource_group.id
   role_definition_name = "Contributor"
   principal_id         = "4b6a4911-0c21-4c94-a285-596cf66a4db2"
 }
 
 resource "azurerm_role_assignment" "nhq_aadadmin" {
-  scope                = data.azurerm_virtual_network.this.resource_group_name
+  scope                = data.azurerm_resource_group.vnet_resource_group.id
   role_definition_name = "Contributor"
   principal_id         = "cc31804d-be36-486b-8a35-c24ae806385c"
 }
