e98dbca06baf406c84cc2a52cd6a3fe3f9371582
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-08-07 13:26:52 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202407120900_create_settlement_report_current_balance_fixing_calculation_version_v1.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW IF NOT EXISTS {CATALOG_NAME}.{WHOLESALE_SETTLEMENT_REPORTS_DATABASE_NAME}.current_balance_fixing_calculation_version_v1 as
-SELECT MAX(version) as calculation_version
-FROM {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.calculations
-WHERE calculation_type = 'balance_fixing'
