b6bdf0e461103babff68c58b65e45e1b5c59ce71
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2024-08-08 09:41:57 +0200

    added API for sauron (#1831)

--- sauron/terraform/main/bff-api.tf
+++ sauron/terraform/main/bff-api.tf
@@ -27,3 +27,16 @@ module "apimao_get_deployments" {
   method                  = "GET"
   url_template            = "/api/GetDeployments"
 }
+
+module "apimao_get_status" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/api-management-api-operation?ref=api-management-api-operation_4.0.1"
+
+  count                   = 1
+  operation_id            = "get-status"
+  api_management_api_name = module.apima_bff_api[0].name
+  resource_group_name     = data.azurerm_key_vault_secret.apim_instance_resource_group_name.value
+  api_management_name     = data.azurerm_key_vault_secret.apim_instance_name.value
+  display_name            = "Sauron: GET status"
+  method                  = "GET"
+  url_template            = "/api/GetStatus"
+}
