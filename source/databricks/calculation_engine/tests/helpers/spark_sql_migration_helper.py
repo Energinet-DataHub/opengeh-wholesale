5d07e0cda5eac0ed88b960470be533f0a3c097ac
Bjarke Meier <hej@bjarkemeier.dk>
2024-08-08 13:11:48 +0200

    add missing hive database

--- source/databricks/calculation_engine/tests/helpers/spark_sql_migration_helper.py
+++ source/databricks/calculation_engine/tests/helpers/spark_sql_migration_helper.py
@@ -53,10 +53,16 @@ def _create_databases(spark: SparkSession) -> None:
     """
     Create Unity Catalog databases as they are not created by migration scripts.
     They are created by infrastructure (in the real environments)
-    In tests they are created in the single available default database."""
+    In tests they are created in the single available default database.
+
+    Create hive database for migrations as well.
+    """
     for database in UnityCatalogDatabaseNames.get_names():
         print(f"Creating database {database}")
         spark.sql(f"CREATE DATABASE IF NOT EXISTS {database}")
+
+    # TODO BJM: Remove when removing hive
+    spark.sql(f"CREATE DATABASE IF NOT EXISTS schema_migration")
 
 
 def migrate(
