9e483d640e58d11f8a64f6d87827b177c953c365
Jonas Michelsen <jvm@energinet.dk>
2024-08-09 08:30:05 +0200

    mw

--- source/databricks/calculation_engine/package/databases/wholesale_internal/table_reader.py
+++ source/databricks/calculation_engine/package/databases/wholesale_internal/table_reader.py
@@ -19,7 +19,11 @@ from package.infrastructure.paths import (
     WholesaleInternalDatabase,
     HiveBasisDataDatabase,
 )
-from .schemas import hive_calculations_schema, grid_loss_metering_points_schema
+from .schemas import (
+    hive_calculations_schema,
+    grid_loss_metering_points_schema,
+    calculations_schema,
+)
 from ..repository_helper import read_table
 
 
@@ -47,11 +51,10 @@ class TableReader:
         )
 
     def read_calculations(self) -> DataFrame:
-        table_name = f"{HiveBasisDataDatabase.DATABASE_NAME}.{HiveBasisDataDatabase.CALCULATIONS_TABLE_NAME}"
-        df = self._spark.read.format("delta").table(table_name)
-
-        # Though it's our own table, we still want to make sure it has the expected schema as
-        # it might have been changed due to e.g. (failed) migrations or backup/restore.
-        assert_schema(df.schema, hive_calculations_schema)
-
-        return df
+        return read_table(
+            self._spark,
+            self._catalog_name,
+            WholesaleInternalDatabase.DATABASE_NAME,
+            WholesaleInternalDatabase.CALCULATIONS_TABLE_NAME,
+            calculations_schema,
+        )
