e321f579031ccc6e2e6b3bd7852ba54e4742aac9
Alexander <alexanderharrow@hotmail.com>
2024-08-09 11:06:55 +0200

    Update calculation_results.py

--- source/databricks/calculation_engine/package/calculation/calculation_results.py
+++ source/databricks/calculation_engine/package/calculation/calculation_results.py
@@ -66,7 +66,7 @@ class BasisDataContainer:
     charge_price_points: DataFrame | None
     charge_link_periods: DataFrame | None
     grid_loss_metering_points: DataFrame
-    """Data frame containing grid areas covered in the calculation."""
+    """Data frame containing the calculation id and grid areas used in the calculation."""
     calculation_grid_areas: DataFrame
 
 
