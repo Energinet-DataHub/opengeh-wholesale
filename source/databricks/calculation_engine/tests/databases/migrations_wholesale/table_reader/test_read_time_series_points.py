a66d4a49bcc5cbf525e5d4f073601ac0b0a90cf7
Jonas Michelsen <jvm@energinet.dk>
2024-08-09 14:28:48 +0200

    hardcode time_series_points in test

--- source/databricks/calculation_engine/tests/databases/migrations_wholesale/table_reader/test_read_time_series_points.py
+++ source/databricks/calculation_engine/tests/databases/migrations_wholesale/table_reader/test_read_time_series_points.py
@@ -70,7 +70,7 @@ class TestWhenValidInput:
         # Arrange
         calculation_input_path = f"{str(tmp_path)}/{calculation_input_folder}"
         time_series_points_table_location = (
-            f"{calculation_input_path}/{InputDatabase.TIME_SERIES_POINTS_TABLE_NAME}"
+            f"{calculation_input_path}/time_series_points"
         )
         row = _create_time_series_point_row()
         df = spark.createDataFrame(data=[row], schema=time_series_points_schema)
@@ -78,7 +78,7 @@ class TestWhenValidInput:
             spark,
             df,
             "test_database",
-            InputDatabase.TIME_SERIES_POINTS_TABLE_NAME,
+            "time_series_points",
             time_series_points_table_location,
             time_series_points_schema,
         )
