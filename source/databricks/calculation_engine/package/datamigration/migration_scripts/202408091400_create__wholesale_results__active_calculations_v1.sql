fddc05ec0da70b417b0927d547b8156f27329dd8
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-08-11 22:54:31 +0200

    draft version of view

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408091400_create__wholesale_results__active_calculations_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408091400_create__wholesale_results__active_calculations_v1.sql
@@ -1,10 +1,41 @@
 CREATE VIEW IF NOT EXISTS {CATALOG_NAME}.{WHOLESALE_RESULTS_DATABASE_NAME}.active_calculations_v1 as
-SELECT calculation_id,
-       calculation_type,
-       calculation_version,
-       grid_area_code,
-       date,
-       active_from_date,
-       active_to_date
-FROM {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.calculations as c
-INNER JOIN {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.calculation_grid_areas as cga ON c.calculation_id = cga.calculation_id
+WITH exploded_dates AS (
+  SELECT
+    c.calculation_id,
+    calculation_type,
+    calculation_version,
+    explode(sequence(
+      calculation_period_start,
+      calculation_period_end,
+      interval 1 day
+    )) AS date,
+    calculation_period_start,
+    calculation_period_end,
+    calculation_execution_time_start,
+    cga.grid_area_code
+  FROM {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.calculations c
+  INNER JOIN {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.calculation_grid_areas cga ON c.calculation_id = cga.calculation_id
+),
+ranked_versions AS (
+  SELECT
+    calculation_id,
+    calculation_type,
+    calculation_version,
+    date,
+    calculation_period_start,
+    calculation_period_end,
+    calculation_execution_time_start,
+    grid_area_code,
+    ROW_NUMBER() OVER (PARTITION BY grid_area_code, date ORDER BY calculation_version DESC) AS rn,
+    LEAD(calculation_execution_time_start) OVER (PARTITION BY grid_area_code, date ORDER BY calculation_version ASC) AS retired
+  FROM exploded_dates
+)
+SELECT
+  calculation_id,
+  calculation_type,
+  calculation_version,
+  grid_area_code,
+  date,
+  calculation_execution_time_start as active_from_date,
+  retired as active_to_date
+FROM ranked_versions
