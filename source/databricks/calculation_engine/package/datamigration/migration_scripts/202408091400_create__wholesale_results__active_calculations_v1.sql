e17d3a9b6e877426164154f1a66be86514a99724
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-08-12 09:56:11 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408091400_create__wholesale_results__active_calculations_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408091400_create__wholesale_results__active_calculations_v1.sql
@@ -1,5 +1,5 @@
 CREATE VIEW IF NOT EXISTS {CATALOG_NAME}.{WHOLESALE_RESULTS_DATABASE_NAME}.active_calculations_v1 as
-WITH exploded_dates AS (
+WITH calculations_by_day AS (
   SELECT
     c.calculation_id,
     calculation_type,
@@ -9,8 +9,6 @@ WITH exploded_dates AS (
       calculation_period_end,
       interval 1 day
     )) AS date,
-    calculation_period_start,
-    calculation_period_end,
     calculation_execution_time_start,
     cga.grid_area_code
   FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations c
@@ -22,13 +20,11 @@ ranked_versions AS (
     calculation_type,
     calculation_version,
     date,
-    calculation_period_start,
-    calculation_period_end,
     calculation_execution_time_start,
     grid_area_code,
     ROW_NUMBER() OVER (PARTITION BY grid_area_code, date ORDER BY calculation_version DESC) AS rn,
-    LEAD(calculation_execution_time_start) OVER (PARTITION BY grid_area_code, date ORDER BY calculation_version ASC) AS retired
-  FROM exploded_dates
+    LEAD(calculation_execution_time_start) OVER (PARTITION BY grid_area_code, date ORDER BY calculation_version ASC) AS active_to_date
+  FROM calculations_by_day
 )
 SELECT
   calculation_id,
@@ -37,5 +33,5 @@ SELECT
   grid_area_code,
   date,
   calculation_execution_time_start as active_from_date,
-  retired as active_to_date
+  active_to_date
 FROM ranked_versions
