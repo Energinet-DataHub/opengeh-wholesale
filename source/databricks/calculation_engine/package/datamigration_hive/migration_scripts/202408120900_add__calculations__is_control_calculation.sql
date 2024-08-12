4a8f0d8dd818977252524bba7750b569b24fc8f3
Bjarke Meier <hej@bjarkemeier.dk>
2024-08-12 10:14:35 +0200

    drop hive support (revert)

--- source/databricks/calculation_engine/package/datamigration_hive/migration_scripts/202408120900_add__calculations__is_control_calculation.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE {HIVE_BASIS_DATA_DATABASE_NAME}.calculations SET TBLPROPERTIES (
-    'delta.columnMapping.mode' = 'name',
-    'delta.minReaderVersion' = '2',
-    'delta.minWriterVersion' = '5')
-GO
-
-ALTER TABLE {HIVE_BASIS_DATA_DATABASE_NAME}.calculations
-ADD COLUMN is_control_calculation BOOLEAN
-GO
