2dc16fc7e7a1c2afb4839d5dcfaa3ad4c5246be2
Bjarke Meier <hej@bjarkemeier.dk>
2024-08-12 11:09:41 +0200

    rename cmd line flag + use it

--- source/databricks/calculation_engine/package/databases/wholesale_results_internal/calculations_storage_model_factory.py
+++ source/databricks/calculation_engine/package/databases/wholesale_results_internal/calculations_storage_model_factory.py
@@ -57,6 +57,7 @@ def _create_calculation(
         CalculationColumnNames.execution_time_start: args.calculation_execution_time_start,
         CalculationColumnNames.created_by_user_id: args.created_by_user_id,
         CalculationColumnNames.version: next_version,
+        CalculationColumnNames.is_control_calculation: args.is_control_calculation,
     }
 
     return spark.createDataFrame(
