7347b177c431a86274f8affe557bbed92ac59951
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-08-13 22:39:38 +0200

    add comments

--- source/databricks/calculation_engine/contracts/data_products/wholesale_results/active_calculations_v1.py
+++ source/databricks/calculation_engine/contracts/data_products/wholesale_results/active_calculations_v1.py
@@ -16,16 +16,18 @@ active_calculations_v1 = t.StructType(
         t.StructField("grid_area_code", t.StringType(), not nullable),
         #
         # The beginning of a day in the calculation period. Represents midnight in local time but given in UTC.
+        # The date is inclusive.
         t.StructField("from_date", t.TimestampType(), not nullable),
         #
         # The end of a day in the calculation period. Represents midnight in local time but given in UTC.
-        # This is exactly one day after the from_date.
+        # This is exactly one day after the from_date. This date is exclusive.
         t.StructField("to_date", t.TimestampType(), not nullable),
         #
-        # Time (UTC) from which the calculation's results are active
+        # Time (UTC) from which the calculation's results are active. This time is inclusive.
         t.StructField("active_from_time", t.TimestampType(), not nullable),
         #
-        # Time (UTC) from which the calculation's results are no longer active. NULL if the calculation is still active.
+        # Time (UTC) from which the calculation's results are no longer active. This time is exclusive.
+        # NULL means that the calculation is still active.
         t.StructField("active_to_time", t.TimestampType(), nullable),
     ]
 )
