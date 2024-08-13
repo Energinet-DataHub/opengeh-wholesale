849d0131142c90961f6f96c759adc0a9a5cdd52a
Kristian Bank Erbou <contact@kbeconsult.dk>
2024-08-13 11:08:50 +0200

    Add permissions to devs for Datahub BI endpoint (#1857)

--- wholesale/terraform/main/dbw-datahub-bi.tf
+++ wholesale/terraform/main/dbw-datahub-bi.tf
@@ -9,3 +9,17 @@ resource "databricks_sql_endpoint" "datahub_bi" {
   auto_stop_mins   = 60
   warehouse_type   = "PRO"
 }
+
+resource "databricks_permissions" "databricks_permissions_datahub_bi_endpoint" {
+  count            = var.datahub_bi_endpoint_enabled ? 1 : 0
+
+  provider        = databricks.dbw
+  sql_endpoint_id = databricks_sql_endpoint.datahub_bi[0].id
+
+  access_control {
+    group_name       = "SEC-G-Datahub-DevelopersAzure"
+    permission_level = "CAN_MONITOR"
+  }
+
+  depends_on = [module.dbw, null_resource.scim_developers]
+}
