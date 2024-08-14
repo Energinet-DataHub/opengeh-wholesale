79fbf26024ac8072a82a2b42b313f02c89dd523e
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-08-14 21:28:45 +0200

    update commment

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408141300_create__wholesale_results__latest_calculations_per_day_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408141300_create__wholesale_results__latest_calculations_per_day_v1.sql
@@ -1,7 +1,9 @@
 -- This view breaks down each calculation period into daily intervals for each of the calculation's grid areas,
--- and calculates the active time period for each the rows. The active time period is defined as the time period
--- between the calculation's execution start time and the execution start time of the next calculation (if another
--- calculation covers the same day, calculation type and grid area)
+-- and calculates the time period for which the calculation is the latest calculation. The time period is defined as
+-- the time between the calculation's execution start time and the execution start time of the next calculation (if a
+-- newer calculation covers the same day, calculation type and grid area). Note that internal and external calculations
+-- are not distinguished in this view - the view returns the latest calculation no matter if it is internal or external.
+
 
 CREATE VIEW IF NOT EXISTS {CATALOG_NAME}.{WHOLESALE_RESULTS_DATABASE_NAME}.latest_calculations_per_day_v1 as
 WITH calculations_by_day AS (
@@ -20,7 +22,7 @@ WITH calculations_by_day AS (
     )) AS from_date_local,
     -- All rows should represent a full day, so the to_date is the day after the from_date
     DATE_ADD(from_date_local, 1) AS to_date_local,
-    calculation_execution_time_start as active_from_time
+    calculation_execution_time_start as latest_from_time
   FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations c
   INNER JOIN {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculation_grid_areas cga ON c.calculation_id = cga.calculation_id
 )
@@ -31,8 +33,8 @@ SELECT
   grid_area_code,
   TO_UTC_TIMESTAMP(from_date_local, 'Europe/Copenhagen') AS from_date,
   TO_UTC_TIMESTAMP(to_date_local, 'Europe/Copenhagen') AS to_date,
-  active_from_time,
-  -- The active_to_time is the active_from_time of the next calculation that has been calculated (with the same, calc. type, grid are and from date).
-  LEAD(active_from_time) OVER (PARTITION BY calculation_type, grid_area_code, from_date_local ORDER BY calculation_version ASC) AS active_to_time
+  latest_from_time,
+  -- The latest_to_time is the latest_from_time of the next calculation that has been calculated (with the same, calc. type, grid are and from date).
+  LEAD(latest_from_time) OVER (PARTITION BY calculation_type, grid_area_code, from_date_local ORDER BY calculation_version ASC) AS latest_to_time
 FROM calculations_by_day
 
