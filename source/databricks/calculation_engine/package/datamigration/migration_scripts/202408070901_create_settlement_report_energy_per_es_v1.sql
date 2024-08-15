62b3cc38912d2b09513e580dd227d89e4591f16a
Bjarke Meier <hej@bjarkemeier.dk>
2024-08-15 08:59:43 +0200

    clear scripts not needed anymore

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408070901_create_settlement_report_energy_per_es_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408070901_create_settlement_report_energy_per_es_v1.sql
@@ -1,14 +1,2 @@
-CREATE VIEW IF NOT EXISTS {CATALOG_NAME}.{WHOLESALE_SETTLEMENT_REPORTS_DATABASE_NAME}.energy_per_es_v1 as
-SELECT calculation_id,
-       calculation_type,
-       calculation_version,
-       result_id,
-       grid_area_code,
-       metering_point_type,
-       settlement_method,
-       resolution,
-       time,
-       quantity,
-       energy_supplier_id
-FROM {CATALOG_NAME}.{WHOLESALE_RESULTS_DATABASE_NAME}.energy_per_es_v1
-WHERE calculation_type IN ('balance_fixing', 'wholesale_fixing', 'first_correction_settlement', 'second_correction_settlement', 'third_correction_settlement')
+-- TODO BJM: Remove when we can remove migration scripts
+select 1
