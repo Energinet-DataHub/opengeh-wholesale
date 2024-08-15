5226ce76da6c86bfdfcd758bf68028c6a6119259
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2024-08-15 15:30:36 +0200

    feat: add modify right to shared_wholesale_input dbw-unity-access-additional.tf (#1877)

---
+++ migrations/terraform/env/preprod_001/dbw-unity-access-additional.tf
@@ -0,0 +1,9 @@
+resource "databricks_grant" "shared_wholesale_input" {
+  provider = databricks.dbw
+  schema   = databricks_schema.shared_wholesale_input.id
+
+  principal  = "SEC-G-Datahub-DevelopersAzure"
+  privileges = ["USE_SCHEMA", "MODIFY", "SELECT", "REFRESH", "EXECUTE"]
+
+  depends_on = [module.dbw, module.kvs_databricks_dbw_workspace_token, databricks_catalog_workspace_binding.shared]
+}
