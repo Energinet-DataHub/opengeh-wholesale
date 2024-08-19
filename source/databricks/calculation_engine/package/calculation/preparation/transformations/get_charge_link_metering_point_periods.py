bce890fd2ee9882c2d89690e0171add0e46dd84c
ClausQAEnerginet <159432508+ClausQAEnerginet@users.noreply.github.com>
2024-08-19 13:00:51 +0200

    test: Update feature test with mp outside grid area (#2636)
    * Add mp outside grid area
    
    * update
    
    * Update energy_calculation.py
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update metering_point_periods.csv
    
    * Updates
    
    * updates
    
    * Updates
    
    * Update prepared_data_reader.py
    
    * Update get_charge_link_metering_point_periods.py
    
    * Update readme.md
    
    * Update readme.md
    
    * Updates
    
    * Update prepared_data_reader.py
    
    * Update prepared_data_reader.py
    
    * Update prepared_data_reader.py
    
    * Updates
    
    * updates
    
    * Updates
    
    * Updates
    
    * Update calculations.csv
    
    * Update prepared_data_reader.py
    
    * Updates
    
    ---------
    
    Co-authored-by: Alexander <alexanderharrow@hotmail.com>
    Co-authored-by: Alexander <122119488+energinet-ajw@users.noreply.github.com>

--- source/databricks/calculation_engine/package/calculation/preparation/transformations/get_charge_link_metering_point_periods.py
+++ source/databricks/calculation_engine/package/calculation/preparation/transformations/get_charge_link_metering_point_periods.py
@@ -25,41 +25,39 @@ def get_charge_link_metering_point_periods(
     charge_links: DataFrame,
     metering_points: DataFrame,
 ) -> ChargeLinkMeteringPointPeriods:
+
     charge_link_metering_point_periods = (
         charge_links.join(
             metering_points,
-            [
-                charge_links[Colname.metering_point_id]
-                == metering_points[Colname.metering_point_id],
-            ],
+            Colname.metering_point_id,
             "inner",
         )
         # We only want the overlap between the metering point period and the charge link period.
         .where(
             (charge_links[Colname.from_date] < metering_points[Colname.to_date])
             & (charge_links[Colname.to_date] > metering_points[Colname.from_date])
-        ).select(
-            charge_links[Colname.charge_key],
-            charge_links[Colname.charge_type],
-            charge_links[Colname.metering_point_id],
-            charge_links[Colname.quantity],
-            f.when(
-                charge_links[Colname.from_date] > metering_points[Colname.from_date],
-                charge_links[Colname.from_date],
-            )
-            .otherwise(metering_points[Colname.from_date])
-            .alias(Colname.from_date),
-            f.when(
-                charge_links[Colname.to_date] < metering_points[Colname.to_date],
-                charge_links[Colname.to_date],
-            )
-            .otherwise(metering_points[Colname.to_date])
-            .alias(Colname.to_date),
-            metering_points[Colname.metering_point_type],
-            metering_points[Colname.settlement_method],
-            metering_points[Colname.grid_area_code],
-            metering_points[Colname.energy_supplier_id],
         )
+    ).select(
+        charge_links[Colname.charge_key],
+        charge_links[Colname.charge_type],
+        charge_links[Colname.metering_point_id],
+        charge_links[Colname.quantity],
+        f.when(
+            charge_links[Colname.from_date] > metering_points[Colname.from_date],
+            charge_links[Colname.from_date],
+        )
+        .otherwise(metering_points[Colname.from_date])
+        .alias(Colname.from_date),
+        f.when(
+            charge_links[Colname.to_date] < metering_points[Colname.to_date],
+            charge_links[Colname.to_date],
+        )
+        .otherwise(metering_points[Colname.to_date])
+        .alias(Colname.to_date),
+        metering_points[Colname.metering_point_type],
+        metering_points[Colname.settlement_method],
+        Colname.grid_area_code,
+        Colname.energy_supplier_id,
     )
 
     return ChargeLinkMeteringPointPeriods(charge_link_metering_point_periods)
