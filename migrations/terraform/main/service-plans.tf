037ad49f38ef4b5b029d93ab923d33e4aab61ac2
Jeppe M <jmw@energinet.dk>
2024-08-21 08:50:43 +0200

    Migrations/feat: Add new app service plan for later development (#1892)

--- migrations/terraform/main/service-plans.tf
+++ migrations/terraform/main/service-plans.tf
@@ -28,6 +28,37 @@ module "func_service_plan" {
   }
 }
 
+module "message_retriever_service_plan" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-plan?ref=service-plan_4.1.0"
+
+  type                           = "func"
+  name                           = "message-retriever"
+  project_name                   = var.domain_name_short
+  environment_short              = var.environment_short
+  environment_instance           = var.environment_instance
+  resource_group_name            = azurerm_resource_group.this.name
+  location                       = azurerm_resource_group.this.location
+  sku_name                       = "EP3"
+  maximum_elastic_worker_count   = 1
+  monitor_alerts_action_group_id = length(module.monitor_action_group_mig) != 1 ? null : module.monitor_action_group_mig[0].id
+
+  cpu_alert_information = {
+    alerts_enabled = length(module.monitor_action_group_mig) != 1 ? false : true
+    frequency      = "PT1M"
+    window_size    = "PT5M"
+    threshold      = 80
+    severity       = 2
+  }
+
+  memory_alert_information = {
+    alerts_enabled = length(module.monitor_action_group_mig) != 1 ? false : true
+    frequency      = "PT1M"
+    window_size    = "PT5M"
+    threshold      = 80
+    severity       = 2
+  }
+}
+
 module "webapp_service_plan" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-plan?ref=service-plan_4.0.1"
 
