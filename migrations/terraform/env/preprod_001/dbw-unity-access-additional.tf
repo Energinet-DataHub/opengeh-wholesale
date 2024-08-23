8c6a19c449c3ab49db2bffa3fb31f9392a761631
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2024-08-23 11:23:40 +0200

    refac: Add create_table rights to shared_wholesale_input in preprod (#1910)

--- migrations/terraform/env/preprod_001/dbw-unity-access-additional.tf
+++ migrations/terraform/env/preprod_001/dbw-unity-access-additional.tf
@@ -3,7 +3,7 @@ resource "databricks_grant" "shared_wholesale_input" {
   schema   = databricks_schema.shared_wholesale_input.id
 
   principal  = "SEC-G-Datahub-DevelopersAzure"
-  privileges = ["USE_SCHEMA", "MODIFY", "SELECT", "REFRESH", "EXECUTE"]
+  privileges = ["USE_SCHEMA", "MODIFY", "SELECT", "REFRESH", "EXECUTE", "CREATE_TABLE"]
 
   depends_on = [module.dbw, module.kvs_databricks_dbw_workspace_token, databricks_catalog_workspace_binding.shared]
 }
