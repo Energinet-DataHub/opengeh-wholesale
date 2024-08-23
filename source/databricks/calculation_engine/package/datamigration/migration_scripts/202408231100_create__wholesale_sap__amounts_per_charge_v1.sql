3d79dcfa9b610dfdec6a3b027a658f771f52a638
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-08-23 13:24:16 +0200

    ...

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408231100_create__wholesale_sap__amounts_per_charge_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408231100_create__wholesale_sap__amounts_per_charge_v1.sql
@@ -21,5 +21,5 @@ SELECT c.calculation_id,
        apc.amount
 FROM {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.amounts_per_charge as apc
 INNER JOIN {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations as c
-ON c.calculation_id = e.calculation_id
+ON c.calculation_id = apc.calculation_id
 WHERE c.calculation_succeeded_time IS NOT NULL
