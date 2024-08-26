11bf8205a480efacea588e29a05d4d3f95393431
Christian Hansen <120550591+cenh-halfspace@users.noreply.github.com>
2024-08-26 15:07:31 +0200

    Wholesale optimise Delta tables (#1884)
    * added workflow
    
    * optimise scheduele
    
    * fixed error
    
    * changed compute
    
    * Added override for scheduele
    
    * Updated override
    
    * remove pausing in envs
    
    * catalog
    
    * rename file + permissions

---
+++ wholesale/terraform/main/dbwj-optimise-tables.tf
@@ -0,0 +1,58 @@
+resource "databricks_job" "optimise_tables_job" {
+  provider            = databricks.dbw
+  name                = "OptimiseTablesJob"
+  max_concurrent_runs = 1
+  always_running      = false
+
+  task {
+    task_key    = "optimise_tables_job_${uuid()}"
+    max_retries = 1
+
+    new_cluster {
+      spark_version = local.spark_version
+      node_type_id  = "Standard_D8as_v4"
+      autoscale {
+        min_workers = 1
+        max_workers = 4
+      }
+      spark_conf = {
+        "fs.azure.account.oauth2.client.endpoint.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net" : "https://login.microsoftonline.com/${var.tenant_id}/oauth2/token"
+        "fs.azure.account.auth.type.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net" : "OAuth"
+        "fs.azure.account.oauth.provider.type.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net" : "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider"
+        "fs.azure.account.oauth2.client.id.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net" : databricks_secret.spn_app_id.config_reference
+        "fs.azure.account.oauth2.client.secret.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net" : databricks_secret.spn_app_secret.config_reference
+        "spark.databricks.delta.preview.enabled" : true
+      }
+      spark_env_vars = {
+        "TENANT_ID"                         = var.tenant_id,
+        "SPN_APP_ID"                        = databricks_secret.spn_app_id.config_reference
+        "SPN_APP_SECRET"                    = databricks_secret.spn_app_secret.config_reference
+        "DATA_STORAGE_ACCOUNT_NAME"         = data.azurerm_key_vault_secret.st_data_lake_name.value
+        "TIME_ZONE"                         = local.TIME_ZONE
+        "CATALOG_NAME"                      = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "CALCULATION_INPUT_FOLDER_NAME"     = var.calculation_input_folder
+        "CALCULATION_INPUT_DATABASE_NAME"   = var.calculation_input_database
+      }
+    }
+
+    library {
+      whl = "dbfs:/opengeh-wholesale/package-1.0-py3-none-any.whl"
+    }
+
+    python_wheel_task {
+      package_name = "package"
+      # The entry point is defined in setup.py
+      entry_point = "optimize_delta_tables"
+    }
+  }
+  
+  schedule {
+    quartz_cron_expression = "0 0 20 ? * *"
+    timezone_id = "UTC"
+	pause_status = "UNPAUSED"
+  }
+
+  email_notifications {
+    no_alert_for_skipped_runs = true
+  }
+}
--- wholesale/terraform/main/dbwj-permissions.tf
+++ wholesale/terraform/main/dbwj-permissions.tf
@@ -19,3 +19,14 @@ resource "databricks_permissions" "migrations_job" {
   }
   depends_on = [module.dbw, null_resource.scim_developers]
 }
+
+resource "databricks_permissions" "optimise_tables_job" {
+  provider = databricks.dbw
+  job_id   = databricks_job.optimise_tables_job.id
+
+  access_control {
+    group_name       = "SEC-G-Datahub-DevelopersAzure"
+    permission_level = "CAN_MANAGE"
+  }
+  depends_on = [module.dbw, null_resource.scim_developers]
+}
