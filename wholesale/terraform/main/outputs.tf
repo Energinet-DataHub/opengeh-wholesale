f43dfd87d9f373847b581dfeac040f8d27d49214
Lars Nielsen <lanni@energinet.dk>
2024-08-27 10:38:05 +0200

    Add CD required outputs to wholesale (#1924)

--- wholesale/terraform/main/outputs.tf
+++ wholesale/terraform/main/outputs.tf
@@ -15,3 +15,45 @@ output "ms_wholesale_database_server" {
   value       = data.azurerm_key_vault_secret.mssql_data_url.value
   sensitive   = true
 }
+
+output "dbw_workspace_url" {
+  description = "Databricks workspace url"
+  value       = module.dbw.workspace_url
+  sensitive   = true
+}
+
+output "dbw_workspace_token" {
+  description = "Databricks workspace token."
+  value       = module.dbw.databricks_token
+  sensitive   = true
+}
+
+output "dbw_databricks_sql_endpoint_id_deployment" {
+  description = "Databricks deployment warehouse sql endpoint id."
+  value       = resource.databricks_sql_endpoint.deployment_warehouse.id
+  sensitive   = true
+}
+
+output "st_data_lake_name" {
+  description = "Shared storage account data lake name."
+  value       = data.azurerm_key_vault_secret.st_data_lake_name.value
+  sensitive   = true
+}
+
+output "spn_datalake_contributor_app_id" {
+  description = "Shared datalake contributor id."
+  value       = data.azurerm_key_vault_secret.spn_datalake_contributor_app_id.value
+  sensitive   = true
+}
+
+output "spn_datalake_contributor_secret" {
+  description = "Shared datalake contributor secret."
+  value       = data.azurerm_key_vault_secret.spn_datalake_contributor_secret.value
+  sensitive   = true
+}
+
+output "shared_unity_catalog_name" {
+  description = "Databricks shared unity catalog name."
+  value       = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+  sensitive   = true
+}
