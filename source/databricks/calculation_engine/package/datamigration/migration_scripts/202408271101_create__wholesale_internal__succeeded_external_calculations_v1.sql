1910060dc6718988d4e2a26f85feb2be529a7ac3
Alexander <alexanderharrow@hotmail.com>
2024-08-27 14:48:05 +0200

    Update 202408271101_create__wholesale_internal__succeeded_external_calculations_v1.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408271101_create__wholesale_internal__succeeded_external_calculations_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408271101_create__wholesale_internal__succeeded_external_calculations_v1.sql
@@ -7,6 +7,6 @@ SELECT calculation_id,
        calculation_type,
        calculation_period_start,
        calculation_period_end,
-       calculation_version,
+       calculation_version
 FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations
 WHERE is_internal_calculation = false AND calculation_succeeded_time IS NOT NULL
