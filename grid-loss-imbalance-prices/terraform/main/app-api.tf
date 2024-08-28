31bd6d36c36724194ee0a30da82da62e141ae799
Michael <52096428+michaelmajgaard@users.noreply.github.com>
2024-08-28 08:18:39 +0200

    feat: imbalance prices rev log - rev log endpoint (#1939)

--- grid-loss-imbalance-prices/terraform/main/app-api.tf
+++ grid-loss-imbalance-prices/terraform/main/app-api.tf
@@ -45,5 +45,6 @@ locals {
     "UserAuthentication:InternalMetadataAddress"      = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=api-backend-open-id-url)"
     "UserAuthentication:BackendBffAppId"              = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=backend-bff-app-id)"
     "DatabaseSettings:ConnectionString"               = local.MS_GRID_LOSS_IMBALANCE_PRICES_CONNECTION_STRING
+    "RevisionLogOptions:ApiAddress"                   = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=func-log-ingestion-api-url)"
   }
 }
