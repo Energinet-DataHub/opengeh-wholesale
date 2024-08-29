f47d51775069dd6b8293bad7ca5cd985fb6301e8
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2024-08-29 17:12:29 +0200

    additional pim dev_003 (#1951)
    * additional pim dev_003
    
    * additional pim dev_003
    
    * additional pim dev_003

--- shared-resources/terraform/infrastructure/env/dev_003/subscr-main-security-group-access.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_role_assignment" "developers_contributor" {
-  scope                = data.azurerm_subscription.this.id
-  role_definition_name = "Contributor"
-  principal_id         = data.azuread_group.developer_security_group_name.object_id
-}
