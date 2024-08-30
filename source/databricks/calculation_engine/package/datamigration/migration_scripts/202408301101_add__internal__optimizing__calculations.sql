43eb92685bb1fc953115432c3812dd4ee1f1268f
Alexander <alexanderharrow@hotmail.com>
2024-08-30 14:29:42 +0200

    Updates

---
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408301101_add__internal__optimizing__calculations.sql
@@ -0,0 +1,3 @@
+-- Use liquid clustering. Liquid clustering requires Databricks runtime 13.3+, which is not used in the testsuite
+
+{DATABRICKS-ONLY}OPTIMIZE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations
