29a8521785582fbc3f0557941d736d0c1347d12b
Alexander <alexanderharrow@hotmail.com>
2024-08-30 12:57:20 +0200

    Update 202408301100_add__basis_data__clustering__databricks.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408301100_add__basis_data__clustering__databricks.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408301100_add__basis_data__clustering__databricks.sql
@@ -1,16 +1,19 @@
 -- Use liquid clustering. Liquid clustering requires Databricks runtime 13.3+, which is not used in the testsuite
 
 {DATABRICKS-ONLY}ALTER TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations;
+
 {DATABRICKS-ONLY}CLUSTER BY (calculation_id, calculation_type);
 
 {DATABRICKS-ONLY}OPTIMIZE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations;
 
 {DATABRICKS-ONLY}ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points;
+
 {DATABRICKS-ONLY}CLUSTER BY (calculation_id, metering_point_id, grid_area_code, observation_time);
 
 {DATABRICKS-ONLY}OPTIMIZE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points;
 
 {DATABRICKS-ONLY}ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.metering_point_periods;
+
 {DATABRICKS-ONLY}CLUSTER BY (calculation_id, metering_point_id);
 
 {DATABRICKS-ONLY}OPTIMIZE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.metering_point_periods;
