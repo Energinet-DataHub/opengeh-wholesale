52b6ee9fcea2107b2f40e6e092e40c417009cf06
Jonas Michelsen <jvm@energinet.dk>
2024-08-30 08:14:10 +0200

    Update 202408290840_rename__basis_data_time_series_points.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408290840_rename__basis_data_time_series_points.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408290840_rename__basis_data_time_series_points.sql
@@ -1,2 +1,8 @@
+ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points SET TBLPROPERTIES (
+    'delta.columnMapping.mode' = 'name',
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '5')
+GO
+
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
 RENAME TO {WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points_old
