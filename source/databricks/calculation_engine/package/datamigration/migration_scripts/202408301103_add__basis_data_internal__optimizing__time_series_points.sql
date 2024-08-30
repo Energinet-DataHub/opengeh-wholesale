3c136acb3af6743cb3e1fff10d0a51a8c1e82652
Alexander <alexanderharrow@hotmail.com>
2024-08-30 14:45:00 +0200

    Updates

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408301103_add__basis_data_internal__optimizing__time_series_points.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408301103_add__basis_data_internal__optimizing__time_series_points.sql
@@ -1,3 +1,3 @@
 -- Use liquid clustering. Liquid clustering requires Databricks runtime 13.3+, which is not used in the testsuite
 
-{DATABRICKS-ONLY}OPTIMIZE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
+{DATABRICKS-ONLY}OPTIMIZE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points;
