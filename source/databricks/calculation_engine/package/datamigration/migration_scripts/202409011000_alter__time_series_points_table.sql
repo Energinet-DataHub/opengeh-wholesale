4c587b04ce9e8a97a066d6ec45389ff0c2ac9d63
Jonas Michelsen <jvm@energinet.dk>
2024-09-02 14:14:20 +0200

    Update 202409011000_alter__time_series_points_table.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409011000_alter__time_series_points_table.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409011000_alter__time_series_points_table.sql
@@ -1,3 +1,8 @@
+ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points SET TBLPROPERTIES (
+   'delta.columnMapping.mode' = 'name',
+   'delta.minReaderVersion' = '2',
+   'delta.minWriterVersion' = '5')
+GO
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
 DROP COLUMN IF EXISTS metering_point_type
 GO
