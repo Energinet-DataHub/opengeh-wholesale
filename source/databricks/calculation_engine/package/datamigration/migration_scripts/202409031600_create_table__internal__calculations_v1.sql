4b81380e30c941ffd637ed2ae93338ed8eb52e95
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-03 22:11:31 +0200

    set reader/writer min version

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409031600_create_table__internal__calculations_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409031600_create_table__internal__calculations_v1.sql
@@ -5,14 +5,16 @@ CREATE TABLE IF NOT EXISTS {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.cal
     calculation_period_start TIMESTAMP NOT NULL,
     calculation_period_end TIMESTAMP NOT NULL,
     calculation_execution_time_start TIMESTAMP NOT NULL,
+    calculation_succeeded_time TIMESTAMP,
     {DATABRICKS-ONLY}calculation_version BIGINT GENERATED ALWAYS AS IDENTITY
 )
 USING DELTA
 TBLPROPERTIES (
     delta.deletedFileRetentionDuration = 'interval 30 days',
     delta.columnMapping.mode = 'name',
+    delta.minReaderVersion = '2',
+    delta.minWriterVersion = '5',
     delta.constraints.calculation_id_chk = "LENGTH ( calculation_id ) = 36",
     delta.constraints.calculation_type_chk = "calculation_type IN ( 'balance_fixing' , 'aggregation' , 'wholesale_fixing' , 'first_correction_settlement' , 'second_correction_settlement' , 'third_correction_settlement' )",
-    delta.constraints.created_by_user_id_chk = "LENGTH ( created_by_user_id ) = 36",
     delta.constraints.calculation_period_chk = "calculation_period_end > calculation_period_start"
 )
