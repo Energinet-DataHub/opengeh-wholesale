15b2ec221ccba88c463add34cf29e2ce64218b46
Daniel Jørgensen <32477395+djorgensendk@users.noreply.github.com>
2024-09-03 17:29:28 +0200

    B2C: remove DBJ as global admin (#1978)

--- shared-resources/terraform/b2c/env/prod_001/b2c-access_override.tf
+++
@@ -1,4 +0,0 @@
-resource "azuread_directory_role_assignment" "dbj" {
-  role_id             = azuread_directory_role.global_admin.template_id
-  principal_object_id = azuread_invitation.dbj[0].user_id
-}
