8d69295204517cd03745a6c4ca46fd44dff34160
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-04 15:20:32 +0200

    delta.minWriterVersion = '5',

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409031600_create_table__internal__calculations_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409031600_create_table__internal__calculations_v1.sql
@@ -14,7 +14,7 @@ TBLPROPERTIES (
     delta.deletedFileRetentionDuration = 'interval 30 days',
     delta.columnMapping.mode = 'name',
     delta.minReaderVersion = '2',
-    delta.minWriterVersion = '6',
+    delta.minWriterVersion = '5',
     delta.constraints.calculation_id_chk = "LENGTH ( calculation_id ) = 36",
     delta.constraints.calculation_type_chk = "calculation_type IN ( 'balance_fixing' , 'aggregation' , 'wholesale_fixing' , 'first_correction_settlement' , 'second_correction_settlement' , 'third_correction_settlement' )",
     delta.constraints.calculation_period_chk = "calculation_period_end > calculation_period_start"
