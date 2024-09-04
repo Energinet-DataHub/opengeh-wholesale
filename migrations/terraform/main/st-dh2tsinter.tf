642200d7f005c91d520abf902cb5078dee469d42
René Nielsen <hej@byggemand-bob.dk>
2024-09-04 09:35:51 +0200

    disable preven_destroy on migrations storage accounts (#1981)
    * Update st-dh2tsinter.tf
    
    * Update st-dh2data.tf
    
    * Fix prevent_destroy
    
    * Update st-dh2tsinter.tf
    
    * Update st-dh2data.tf
    
    ---------
    
    Co-authored-by: Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>

--- migrations/terraform/main/st-dh2tsinter.tf
+++ migrations/terraform/main/st-dh2tsinter.tf
@@ -11,6 +11,7 @@ module "st_dh2timeseries_intermediary" {
   access_tier                = "Hot"
   private_endpoint_subnet_id = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
   ip_rules                   = local.ip_restrictions_as_string
+  prevent_deletion           = false
 }
 
 #---- Role assignments
