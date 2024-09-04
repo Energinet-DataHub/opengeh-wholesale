ed39dae60986774946f7b7b50b0db0d968970fc9
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-04 21:50:30 +0200

    revert a few files

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202408301104_add__basis_data_internal__clustering__metering_point_periods.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202408301104_add__basis_data_internal__clustering__metering_point_periods.sql
@@ -1,2 +1,7 @@
-ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.metering_point_periods
-CLUSTER BY (calculation_id, metering_point_id)
+-- Use liquid clustering. Liquid clustering requires Databricks runtime 13.3+, which is not used in the testsuite
+
+{DATABRICKS-ONLY}ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.metering_point_periods
+{DATABRICKS-ONLY}CLUSTER BY (calculation_id, metering_point_id);
+
+-- Avoid problem with empty script while running in test
+{TEST-ONLY}SELECT 1;
--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409010958_add__basis_data_internal__clustering__time_series_points.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409010958_add__basis_data_internal__clustering__time_series_points.sql
@@ -1,2 +1,7 @@
-ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
-CLUSTER BY (calculation_id, metering_point_id, observation_time);
+-- Use liquid clustering. Liquid clustering requires Databricks runtime 13.3+, which is not used in the testsuite
+
+{DATABRICKS-ONLY}ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
+{DATABRICKS-ONLY}CLUSTER BY (calculation_id, metering_point_id, observation_time);
+
+-- Avoid problem with empty script while running in test
+{TEST-ONLY}SELECT 1;
