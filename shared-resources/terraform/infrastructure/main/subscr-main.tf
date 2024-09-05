0caf597db1238058f9b71c0f128d70199de8fbee
Lars Nielsen <lanni@energinet.dk>
2024-09-05 18:21:45 +0200

    Added cost anomaly alerting (#1997)

--- shared-resources/terraform/infrastructure/main/subscr-main.tf
+++ shared-resources/terraform/infrastructure/main/subscr-main.tf
@@ -1,3 +1,11 @@
 data "azurerm_subscription" "this" {
   subscription_id = var.subscription_id
 }
+
+resource "azurerm_cost_anomaly_alert" "anomaly_alert" {
+  name            = "alert-costanomaly-${var.environment_short}-${var.environment_instance}"
+  display_name    = "Cost anomaly alert"
+  subscription_id = data.azurerm_subscription.this.id
+  email_subject   = "Cost Anomaly detected"
+  email_addresses = ["3efe72c1.energinet.onmicrosoft.com@emea.teams.ms"]
+}
