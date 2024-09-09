011f8d24da3a0ef6d8c92417762ebd411558d290
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2024-09-09 09:32:23 +0200

    eventhub namespace IAM (#2011)

--- integration-test/terraform/main/evhns-this.tf
+++ integration-test/terraform/main/evhns-this.tf
@@ -4,6 +4,12 @@ resource "azurerm_eventhub_namespace" "this" {
   resource_group_name = azurerm_resource_group.this.name
   sku                 = "Standard"
   capacity            = 1
+  # TODO: to be disabled when all have updated to new TestCommon where IAM is used
+  # local_authentication_enabled = false
+
+  identity {
+    type = "SystemAssigned"
+  }
 
   lifecycle {
     ignore_changes = [
@@ -14,6 +20,24 @@ resource "azurerm_eventhub_namespace" "this" {
   tags = local.tags
 }
 
+resource "azurerm_key_vault_secret" "kvs_evhns_name" {
+  name         = "AZURE-EVENTHUB-NAME"
+  value        = azurerm_eventhub_namespace.this.name
+  key_vault_id = azurerm_key_vault.this.id
+
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+
+  depends_on = [
+    azurerm_role_assignment.kv_self
+  ]
+}
+
+
+# TODO: to be deleted when all have updated to new TestCommon where IAM is used
 resource "azurerm_key_vault_secret" "kvs_evhns_connection_string" {
   name         = "AZURE-EVENTHUB-CONNECTIONSTRING"
   value        = azurerm_eventhub_namespace.this.default_primary_connection_string
