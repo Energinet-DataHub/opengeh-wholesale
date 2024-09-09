8864e663f02cddd16d122335d2a5436302b5cb54
Jonas Michelsen <jvm@energinet.dk>
2024-09-09 14:32:18 +0200

    Update calculation_writer.py

--- source/databricks/calculation_engine/package/databases/wholesale_internal/calculation_writer.py
+++ source/databricks/calculation_engine/package/databases/wholesale_internal/calculation_writer.py
@@ -50,7 +50,7 @@ def write_calculation(
     )[:-3]
 
     spark.sql(
-        f"INSERT INTO spark_catalog.{WholesaleInternalDatabase.DATABASE_NAME}.{WholesaleInternalDatabase.CALCULATIONS_V1_TABLE_NAME}"
+        f"INSERT INTO {infrastructure_settings.catalog_name}.{WholesaleInternalDatabase.DATABASE_NAME}.{WholesaleInternalDatabase.CALCULATIONS_V1_TABLE_NAME}"
         f" ({TableColumnNames.calculation_id}, {TableColumnNames.calculation_type}, {TableColumnNames.calculation_period_start}, {TableColumnNames.calculation_period_end}, {TableColumnNames.calculation_execution_time_start}, {TableColumnNames.calculation_succeeded_time})"
         f" VALUES ('{args.calculation_id}', '{args.calculation_type.value}', '{calculation_period_start_datetime}', '{calculation_period_end_datetime}', '{calculation_execution_time_start}', NULL);"
     )
