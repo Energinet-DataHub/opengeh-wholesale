227cc38ffe4b96b74d77a52de865798b7348e8b6
Lasse Ringgren Nielsen <sixxth@gmail.com>
2024-09-10 08:17:40 +0200

    Enable DataHub3 API on PRODUCTION (#2023)
    * Removed overrides on EDI api operations
    
    * Update subsystem.environment.auto.tfvars

--- edi/terraform/env/prod_001/apimao-cim-peekmessage_override.tf
+++
@@ -1,4 +0,0 @@
-# Disabled Peek Message API in APIM
-module "apimao_cim_peek_message" {
-  count = 0
-}
--- edi/terraform/env/prod_001/apimao-request-aggregated-measuredata_override.tf
+++
@@ -1,4 +0,0 @@
-# Disabled Request Aggregated Measure Data API in APIM
-module "apimao_request_aggregated_measure_data" {
-  count = 0
-}
