734f5613ff9c64cb3e958a324d713f1a1bd38749
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2024-09-10 15:06:58 +0200

    fix

---
+++ source/databricks/settlement_report/setup.py
@@ -0,0 +1,41 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from setuptools import setup, find_packages
+
+setup(
+    name="package_settlement_report",
+    version=1.0,
+    description="Tools for settlement report",
+    long_description="",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    # Make sure these packages are added to the docker container and pinned to the same versions
+    install_requires=[
+        "ConfigArgParse==1.5.3",
+        "pyspark==3.5.1",
+        "azure-identity==1.12.0",
+        "azure-storage-file-datalake==12.11.0",
+        "dependency_injector==4.41.0",
+        "urllib3==2.2.*",
+        "delta-spark==3.1.0",
+        "python-dateutil==2.8.2",
+        "azure-monitor-opentelemetry==1.2.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "create_settlement_report = package.settlement_report_job.settlement_report:run",
+        ]
+    },
+)
