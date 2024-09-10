b35b54a85074b03903a60c43673b18dda35e6a9f
ClausQAEnerginet <159432508+ClausQAEnerginet@users.noreply.github.com>
2024-09-10 14:08:18 +0200

    test: Covernator 2.0 (new branch) (#2760)
    * Tilføj covernator mappe
    
    * Tilføj view tests
    
    * Revert "Tilføj view tests"
    
    This reverts commit d89a9e4d4aa1470353fc7268c45a48a01fb80ebc.
    
    * Tilføj 1 coverage fil
    
    * data product opdateret nu
    
    * energy
    
    * energy finished
    
    * wholesale updated
    
    * final

---
+++ source/databricks/calculation_engine/tests/features/wholesale_calculation/given_extra_fees_or_subscriptions_are_added/Coverage.py
@@ -0,0 +1,10 @@
+from test_coverage.all_test_cases import Cases
+
+"""
+## PURPOSE
+This is a simple check of adding 10 extra fees and subscriptions to an MP in a calculation period.
+
+## CASES TESTED
+"""
+Cases.CalculationTests.PriceElementUpdates.Extra_subscriptions_are_added_to_MP_in_calculation_period
+Cases.CalculationTests.PriceElementUpdates.Extra_fees_are_added_to_MP_in_calculation_period
