c7790d5a5fb979f87ddf650d448a151afe9d0142
Jonas Michelsen <jvm@energinet.dk>
2024-09-10 13:02:13 +0200

    Update 202409051000_update__wholesale_internal__calculations_v1.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409051000_update__wholesale_internal__calculations_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409051000_update__wholesale_internal__calculations_v1.sql
@@ -15,6 +15,11 @@ WHEN MATCHED THEN
   UPDATE SET target.is_internal_calculation = source.is_internal_calculation;
 GO
 
+UPDATE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations_v1
+SET is_internal_calculation = FALSE
+WHERE is_internal_calculation IS NULL;
+GO
+
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations_v1
     ADD CONSTRAINT is_internal_calculation_chk
     CHECK (is_internal_calculation IS NOT NULL)
