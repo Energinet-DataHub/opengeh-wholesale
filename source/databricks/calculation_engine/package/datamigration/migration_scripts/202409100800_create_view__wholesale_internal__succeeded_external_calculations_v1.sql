2ed929c187ddf74ec9108a2660a665b60c1d6475
Jonas Michelsen <jvm@energinet.dk>
2024-09-10 14:32:31 +0200

    Update 202409100800_create_view__wholesale_internal__succeeded_external_calculations_v1.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409100800_create_view__wholesale_internal__succeeded_external_calculations_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409100800_create_view__wholesale_internal__succeeded_external_calculations_v1.sql
@@ -7,6 +7,7 @@ SELECT calculation_id,
        calculation_type,
        calculation_period_start,
        calculation_period_end,
-       calculation_version
+       calculation_version,
+       is_internal_calculation
 FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations_v1
 WHERE is_internal_calculation = false AND calculation_succeeded_time IS NOT NULL
