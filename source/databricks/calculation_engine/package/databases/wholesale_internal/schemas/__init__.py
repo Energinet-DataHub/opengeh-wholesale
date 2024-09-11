b98c8880dc581c482b5be7ac846515da6b371d3e
Alexander <alexanderharrow@hotmail.com>
2024-09-11 14:48:24 +0200

    Update __init__.py

--- source/databricks/calculation_engine/package/databases/wholesale_internal/schemas/__init__.py
+++ source/databricks/calculation_engine/package/databases/wholesale_internal/schemas/__init__.py
@@ -1,4 +1,5 @@
 from .calculation_grid_areas_schema import calculation_grid_areas_schema
+from .calculations_schema import calculations_schema
 from .grid_loss_metering_points_schema import (
     grid_loss_metering_points_schema,
 )
