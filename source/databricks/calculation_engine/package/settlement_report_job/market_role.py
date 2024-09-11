2c31cd15769583eeb692c5f34f4115b908199289
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-11 14:09:19 +0200

    doc string

--- source/databricks/calculation_engine/package/settlement_report_job/market_role.py
+++ source/databricks/calculation_engine/package/settlement_report_job/market_role.py
@@ -16,6 +16,11 @@ from enum import Enum
 
 
 class MarketRole(Enum):
+    """
+    The market role value affects what is included in the settlement report.
+    The 'market-role' command line argument must use one of these values.
+    """
+
     DATAHUB_ADMINISTRATOR = "datahub_administrator"
     ENERGY_SUPPLIER = "energy_supplier"
     GRID_ACCESS_PROVIDER = "grid_access_provider"
