1fc47930cbd16c5c48160c4b0808ba4cbda2be8e
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2024-09-11 14:20:53 +0200

    refac: Use new calculations table (#2761)

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409100808_create_view__wholesale_results__total_monthly_amounts_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409100808_create_view__wholesale_results__total_monthly_amounts_v1.sql
@@ -1,3 +1,6 @@
+DROP VIEW IF EXISTS {CATALOG_NAME}.{WHOLESALE_RESULTS_DATABASE_NAME}.total_monthly_amounts_v1
+GO
+
 CREATE VIEW IF NOT EXISTS {CATALOG_NAME}.{WHOLESALE_RESULTS_DATABASE_NAME}.total_monthly_amounts_v1 AS
 SELECT c.calculation_id,
        c.calculation_type,
@@ -10,4 +13,4 @@ SELECT c.calculation_id,
        tma.time,
        tma.amount
 FROM {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.total_monthly_amounts as tma
-INNER JOIN {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations AS c ON c.calculation_id = tma.calculation_id
+INNER JOIN {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.succeeded_external_calculations_v1 AS c ON c.calculation_id = tma.calculation_id
