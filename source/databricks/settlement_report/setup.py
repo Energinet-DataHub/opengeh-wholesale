a77858771de25cb76bc99f80264dbf7e7c26b794
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2024-09-12 13:05:52 +0200

    feat: Support multiple wheels (#2764)
    * support multiple wheels
    
    * support multiple wheels
    
    * empty commit
    
    * fix
    
    * fix
    
    * empty commit
    
    * empty commit
    
    * fixed
    
    * fixed
    
    * Update .github/workflows/ci-databricks.yml
    
    Co-authored-by: Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
    
    * renamed folder
    
    * fix path
    
    * fix test
    
    * fix test
    
    * Update ci-databricks.yml
    
    * renamed
    
    * updated package
    
    * updated
    
    * added azure core
    
    * restructured
    
    * moved tests
    
    * removed tests
    
    ---------
    
    Co-authored-by: Christian Hansen <120550591+cenh-halfspace@users.noreply.github.com>
    Co-authored-by: Christian Hansen <cenh@halfspace.io>

--- source/databricks/settlement_report/setup.py
+++ source/databricks/settlement_report/setup.py
@@ -14,40 +14,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="package",
+    name="opengeh-settlement-report",
     version=1.0,
-    description="Tools for wholesale",
+    description="Tools for settlement report",
     long_description="",
     long_description_content_type="text/markdown",
     license="MIT",
-    package_data={
-        "package": [
-            "datamigration/migration_scripts/*.sql",
-            "datamigration_hive/migration_scripts/*.sql",
-            "datamigration_hive/current_state_scripts/schemas/*.sql",
-            "datamigration_hive/current_state_scripts/tables/*.sql",
-        ]
-    },
-    packages=find_packages(exclude=["tests*"]),
+    packages=find_packages(),
     # Make sure these packages are added to the docker container and pinned to the same versions
     install_requires=[
         "ConfigArgParse==1.5.3",
         "pyspark==3.5.1",
-        "azure-identity==1.12.0",
-        "azure-storage-file-datalake==12.11.0",
-        "dependency_injector==4.41.0",
-        "urllib3==2.2.*",
         "delta-spark==3.1.0",
         "python-dateutil==2.8.2",
         "azure-monitor-opentelemetry==1.6.0",
         "azure-core==1.30.0",
-        "opengeh-spark-sql-migrations @ git+https://git@github.com/Energinet-DataHub/opengeh-python-packages@1.9.0#subdirectory=source/spark_sql_migrations",
     ],
     entry_points={
         "console_scripts": [
-            "start_calculator = package.calculator_job:start",
-            "migrate_data_lake = package.datamigration.migration:migrate_data_lake",
-            "optimize_delta_tables = package.optimize_job.delta_optimization:optimize_tables",
+            "create_settlement_report = settlement_report_job.settlement_report:start",
         ]
     },
 )
