c16563ebcb878f02712e99d0eefaf2530c27a6b7
Jonas Michelsen <jvm@energinet.dk>
2024-09-12 09:54:02 +0200

    refac: Use calculations table again

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409110809_create_view__wholesale_sap__amounts_per_charge_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409110809_create_view__wholesale_sap__amounts_per_charge_v1.sql
@@ -23,6 +23,6 @@ SELECT c.calculation_id,
        apc.price,
        apc.amount
 FROM {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.amounts_per_charge as apc
-INNER JOIN {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations_v1 as c
+INNER JOIN {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations as c
 ON c.calculation_id = apc.calculation_id
 WHERE c.calculation_succeeded_time IS NOT NULL
