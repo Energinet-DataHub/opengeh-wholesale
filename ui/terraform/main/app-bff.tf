09bcfd1dd45389fc232dce267f19f8d2179210e0
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-09-13 08:58:41 +0200

    Add Settlementreports WebAPI base url to settings for BFF (#2059)
    * Update app-bff.tf
    
    * Update app-api.config.tf
    
    * Revert "Update app-api.config.tf"
    
    This reverts commit 2f5c66d4433ca841fd27dd26617e2dfb0c054af6.

--- ui/terraform/main/app-bff.tf
+++ ui/terraform/main/app-bff.tf
@@ -35,6 +35,7 @@ module "backend_for_frontend" {
     ApiClientSettings__ESettExchangeBaseUrl                           = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=app-esett-webapi-base-url)"
     ApiClientSettings__EdiB2CWebApiBaseUrl                            = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=app-edi-b2cwebapi-base-url)"
     ApiClientSettings__ImbalancePricesBaseUrl                         = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=app-grid-loss-imbalance-prices-api-base-url)"
+    ApiClientSettings__SettlementReportsAPIBaseUrl                    = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=app-settlement-report-webapi-base-url)"
     MITID_EXTERNAL_OPEN_ID_URL                                        = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=mitid-frontend-open-id-url)"
     EXTERNAL_OPEN_ID_URL                                              = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=frontend-open-id-url)"
     INTERNAL_OPEN_ID_URL                                              = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=api-backend-open-id-url)"
