42c3bd8a01d4c50973a61f4ec442445aadc73362
Bjarke Meier <hej@bjarkemeier.dk>
2024-09-15 19:39:00 +0200

    revert to setup.py
    Because workflows currently doesn't support pyproject.toml
    https://github.com/Energinet-DataHub/.github/blob/d85e8235210f0088258ccb2e8e5309a214f9e989/.github/workflows/databricks-build-prerelease.yml#L84

---
+++ source/databricks/app/setup.py
@@ -0,0 +1,28 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from setuptools import setup, find_packages
+
+setup(
+    name="opengeh-app",
+    version="1.0",
+    description="Tools for apps",
+    long_description="",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    # Make sure these packages are added to the docker container and pinned to the same versions
+    install_requires=[
+        "azure-monitor-opentelemetry==1.6.0",
+        "azure-core==1.30.0",
+    ],
+)
