352df75a1e8814d2e88fb6377e8880d7478cc7d1
Alexander <alexanderharrow@hotmail.com>
2024-09-16 13:38:42 +0200

    Update

--- source/databricks/calculation_engine/package/infrastructure/paths.py
+++ source/databricks/calculation_engine/package/infrastructure/paths.py
@@ -143,8 +143,6 @@ class HiveOutputDatabase:
     DATABASE_NAME = "wholesale_output"
     ENERGY_RESULT_TABLE_NAME = "energy_results"
     WHOLESALE_RESULT_TABLE_NAME = "wholesale_results"
-    MONTHLY_AMOUNTS_TABLE_NAME = "monthly_amounts"
-    TOTAL_MONTHLY_AMOUNTS_TABLE_NAME = "total_monthly_amounts"
     SUCCEEDED_ENERGY_RESULTS_V1_VIEW_NAME = "succeeded_energy_results_v1"
 
 
