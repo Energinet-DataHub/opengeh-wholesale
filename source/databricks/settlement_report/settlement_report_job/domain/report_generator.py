596b6a32d55ff1707876888db8dcefb3c931a3ce
Jens Peder Meldgaard <jpm@halfspace.io>
2024-09-16 14:44:27 +0200

    feat: Implements settlement report time series factory (#2758)
    * refac: enable logging in jobs
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * chore: add classes for column names
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * chore: add constants
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * chore: add utility functions
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * feat: implement time series factory
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * test: add tests for time series factory utils
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * fix: mypy errors
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * style: rename constants
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * refac: move from entrypoint to report_generator
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * style: give zip file a new name
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * style: add arrange, act, assert comments
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * style: rename query dir to report dir
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * test: remove test data
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * test: remove link to test data
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * style: change to get_metering_point_time_series_view_name
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * fix: calculation id is never null
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * style: simplify _generate_time_series
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * style: we should not add zeros
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * test: fix names in test
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * refac: simplify filtering logicx
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * fix: should loop on items not keys
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    ---------
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/databricks/settlement_report/settlement_report_job/domain/report_generator.py
+++ source/databricks/settlement_report/settlement_report_job/domain/report_generator.py
@@ -3,9 +3,26 @@ from pyspark.sql import SparkSession
 from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
 from settlement_report_job.domain.time_series_factory import create_time_series
 
+from settlement_report_job.utils import create_zip_file, get_dbutils
+from settlement_report_job.constants import get_output_volume_name
+from settlement_report_job.logger import Logger
+
+log = Logger(__name__)
+
 
 def execute(spark: SparkSession, args: SettlementReportArgs) -> None:
     """
     Entry point for the logic of creating settlement reports.
     """
     create_time_series(spark, args)
+    dbutils = get_dbutils(spark)
+    report_directory = f"{get_output_volume_name()}/{args.report_id}"
+    zip_file_path = f"{report_directory}/final_report.zip"
+
+    files_to_zip = []
+    time_series_files = create_time_series(spark, args, report_directory)
+    files_to_zip.extend(time_series_files)
+
+    log.info(f"Creating zip file at '{report_directory}.zip'")
+    create_zip_file(dbutils, args.report_id, zip_file_path, files_to_zip)
+    log.info(f"Finished creating '{report_directory}/some-name.zip'")
