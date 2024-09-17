0cb4739bf209e87de8838e460f475fe4bcc0345d
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-17 14:12:13 +0200

    clean up

--- source/databricks/settlement_report/settlement_report_job/domain/time_series_factory.py
+++ source/databricks/settlement_report/settlement_report_job/domain/time_series_factory.py
@@ -17,17 +17,20 @@ from pyspark.sql import DataFrame, Column, functions as F, types as T
 from pyspark.sql.window import Window
 from pyspark.sql.session import SparkSession
 
-from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
-from settlement_report_job.infrastructure.table_column_names import (
-    DataProductColumnNames,
-)
-from settlement_report_job.utils import map_from_dict, get_dbutils
-from settlement_report_job.constants import (
+from settlement_report_job.domain.market_naming_convention import (
     METERING_POINT_TYPE_DICT,
-    get_metering_point_time_series_view_name,
     RESOLUTION_NAMES,
 )
-
+from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
+from settlement_report_job.infrastructure.database_definitions import (
+    get_metering_point_time_series_view_name,
+)
+from settlement_report_job.infrastructure.table_column_names import (
+    DataProductColumnNames,
+    TimeSeriesPointCsvColumnNames,
+    EphemeralColumns,
+)
+from settlement_report_job.utils import map_from_dict, get_dbutils
 from settlement_report_job.infrastructure import logging_configuration
 from settlement_report_job.infrastructure.logger import Logger
 
