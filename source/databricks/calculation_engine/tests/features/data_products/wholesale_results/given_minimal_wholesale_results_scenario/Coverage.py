d2c1b0fe324a47680d7595bf7c0e87e5fc472b87
ClausQAEnerginet <xclpe@energinet.dk>
2024-09-18 14:20:02 +0200

    update

--- source/databricks/calculation_engine/tests/features/data_products/wholesale_results/given_minimal_wholesale_results_scenario/Coverage.py
+++ source/databricks/calculation_engine/tests/features/data_products/wholesale_results/given_minimal_wholesale_results_scenario/Coverage.py
@@ -4,6 +4,10 @@ from coverage.all_test_cases import Cases
 ## Purpose
 The purpose is checking views related to amount per charge view.
 
+## DESIGN CONSIDERATIONS
+- wholesale_internal.calculations table has three calculation_id's, only one of which is valid.
+- The two others are either internal or doesn't have an end date.
+
 ## CASES TESTED
 """
 Cases.DataProductTests.WholesaleResultsTests.Only_calculation_ids_in_internal_calculations_included
