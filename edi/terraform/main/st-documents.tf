15fb3dc317cd5d3e32f1d6003593312a3687a9e8
Kristian Bank Erbou <contact@kbeconsult.dk>
2024-09-18 10:25:46 +0200

    Remove delete protection temporarily (#2095)

--- edi/terraform/main/st-documents.tf
+++ edi/terraform/main/st-documents.tf
@@ -1,16 +1,18 @@
 module "st_documents" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/storage-account-dfs?ref=storage-account-dfs_5.0.0"
 
-  name                       = "documents"
-  project_name               = var.domain_name_short
-  environment_short          = var.environment_short
-  environment_instance       = var.environment_instance
-  resource_group_name        = azurerm_resource_group.this.name
-  location                   = azurerm_resource_group.this.location
-  account_replication_type   = "LRS"
-  private_endpoint_subnet_id = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
-  ip_rules                   = local.ip_restrictions_as_string
+  name                                  = "documents"
+  project_name                          = var.domain_name_short
+  environment_short                     = var.environment_short
+  environment_instance                  = var.environment_instance
+  resource_group_name                   = azurerm_resource_group.this.name
+  location                              = azurerm_resource_group.this.location
+  account_replication_type              = "LRS"
+  private_endpoint_subnet_id            = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
+  ip_rules                              = local.ip_restrictions_as_string
   lifecycle_retention_delete_after_days = 3285 # 9 years = (5 + 3 + current year) * 365 days
+
+  prevent_deletion = false
 }
 
 resource "azurerm_storage_container" "outgoing" {
