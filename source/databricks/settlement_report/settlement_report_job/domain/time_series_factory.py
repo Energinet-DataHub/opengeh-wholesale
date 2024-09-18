9d44eec49bac2c2aa83d01d674bae192ef7c7a39
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-18 15:20:53 +0200

    ...

--- source/databricks/settlement_report/settlement_report_job/domain/time_series_factory.py
+++ source/databricks/settlement_report/settlement_report_job/domain/time_series_factory.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Union
-from pyspark.sql import DataFrame, Column, functions as F, types as T, Window
+from pyspark.sql import DataFrame, functions as F, Window
 from pyspark.sql.session import SparkSession
 
 from settlement_report_job.domain.metering_point_resolution import (
