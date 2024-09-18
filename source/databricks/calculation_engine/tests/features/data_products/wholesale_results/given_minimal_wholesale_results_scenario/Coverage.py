e5ba8007243889e670466e0967681dc9461547a2
ClausQAEnerginet <xclpe@energinet.dk>
2024-09-18 14:06:28 +0200

    update

--- source/databricks/calculation_engine/tests/features/data_products/wholesale_results/given_minimal_wholesale_results_scenario/Coverage.py
+++ source/databricks/calculation_engine/tests/features/data_products/wholesale_results/given_minimal_wholesale_results_scenario/Coverage.py
@@ -7,3 +7,5 @@ The purpose is checking views related to amount per charge view.
 ## CASES TESTED
 """
 Cases.DataProductTests.WholesaleResultsTests.Only_calculation_ids_in_internal_calculations_included
+Cases.DataProductTests.WholesaleResultsTests.Only_external_calculation_ids_included
+Cases.DataProductTests.WholesaleResultsTests.Calculation_ids_without_calculation_succeeded_time_not_included
