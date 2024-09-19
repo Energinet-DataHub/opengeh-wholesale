e0baf7303c310b49de08344b63222066c8f33a68
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2024-09-19 17:08:05 +0200

    readd locks migrations (#2123)
    Co-authored-by: Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>

--- migrations/terraform/main/st-dh2tsaudit.tf
+++ migrations/terraform/main/st-dh2tsaudit.tf
@@ -10,7 +10,6 @@ module "st_dh2timeseries_audit" {
   account_replication_type   = "LRS"
   private_endpoint_subnet_id = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
   ip_rules                   = local.ip_restrictions_as_string
-  prevent_deletion           = false
   audit_storage_account = var.enable_audit_logs ? {
     id = data.azurerm_key_vault_secret.st_audit_shres_id.value
   } : null
