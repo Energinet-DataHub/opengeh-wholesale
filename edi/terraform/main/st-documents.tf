8ac534339705f1ffc61c2e670fea76d766869b00
Kristian Bank Erbou <contact@kbeconsult.dk>
2024-09-19 13:59:14 +0200

    Rename Datafactory resources for EDI backup (#2115)

--- edi/terraform/main/st-documents.tf
+++ edi/terraform/main/st-documents.tf
@@ -1,5 +1,5 @@
 module "st_documents" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/storage-account-dfs?ref=storage-account-dfs_5.0.0"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/storage-account-dfs?ref=storage-account-dfs_6.1.0"
 
   name                                  = "documents"
   project_name                          = var.domain_name_short
@@ -11,6 +11,7 @@ module "st_documents" {
   private_endpoint_subnet_id            = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
   ip_rules                              = local.ip_restrictions_as_string
   lifecycle_retention_delete_after_days = 3285 # 9 years = (5 + 3 + current year) * 365 days
+  audit_storage_account                 = null #audit log disabled as part of debugging EDI backups
 
   prevent_deletion = false
 }
