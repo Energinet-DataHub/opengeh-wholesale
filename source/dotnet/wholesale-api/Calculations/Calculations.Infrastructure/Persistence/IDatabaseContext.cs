cb85b3207f04ff720bc29e5cfc1314edf44a34d0
Ebbe Knudsen <xedkn@energinet.dk>
2024-09-19 15:15:55 +0200

    Remove unused dependency

--- source/dotnet/wholesale-api/Calculations/Calculations.Infrastructure/Persistence/IDatabaseContext.cs
+++ source/dotnet/wholesale-api/Calculations/Calculations.Infrastructure/Persistence/IDatabaseContext.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.Core.Outbox.Infrastructure.DbContext;
 using Energinet.DataHub.Wholesale.Calculations.Application.Model.Calculations;
 using Microsoft.EntityFrameworkCore;
 
