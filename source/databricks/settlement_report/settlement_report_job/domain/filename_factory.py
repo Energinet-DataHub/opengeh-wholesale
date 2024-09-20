a0e71eb281f02b2a586f12f8c217d42b04f84aaf
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-20 17:19:41 +0200

    ...

---
+++ source/databricks/settlement_report/settlement_report_job/domain/filename_factory.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+from settlement_report_job.domain.report_naming_convention import MARKET_ROLES
+from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
+
+
+class ReportType(Enum):
+    TimeSeriesHourly = "time_series_hourly"
+    TimeSeriesQuarterly = "time_series_quarterly"
+
+
+class FilenameFactory:
+    def __init__(self, report_type: ReportType, args: SettlementReportArgs):
+        self.report_type = report_type
+        self.args = args
+
+    def create(self, grid_area_code: str) -> str:
+        if self.report_type == ReportType.TimeSeriesHourly:
+            return self._create_time_series_hourly_filename(grid_area_code)
+
+    def _create_time_series_hourly_filename(
+        self, grid_area_code: str, split_index: str = None
+    ) -> str:
+
+        return (
+            f"TSSD60"
+            f"_{grid_area_code}"
+            f"_{self.args.gln}"
+            f"_{MARKET_ROLES[self.args.market_role]}"
+            f"_{self.args.period_start.strftime('%d-%m-%Y')}"
+            f"_{self.args.period_end.strftime('%d-%m-%Y')}"
+            f"_{split_index}"
+            if split_index is not None
+            else "" ".csv"
+        )
--- source/databricks/settlement_report/settlement_report_job/domain/report_generator.py
+++ source/databricks/settlement_report/settlement_report_job/domain/report_generator.py
@@ -21,7 +21,6 @@ def execute(spark: SparkSession, args: SettlementReportArgs) -> None:
     """
     dbutils = get_dbutils(spark)
     report_directory = f"{get_output_volume_name()}/{args.report_id}"
-    zip_file_path = f"{report_directory}/final_report.zip"
 
     hourly_time_series_files = create_time_series(
         spark,
@@ -39,9 +38,8 @@ def execute(spark: SparkSession, args: SettlementReportArgs) -> None:
     files_to_zip = []
     files_to_zip.extend(hourly_time_series_files)
     files_to_zip.extend(quarterly_time_series_files)
-    print(files_to_zip)
-    print(hourly_time_series_files)
-    print(quarterly_time_series_files)
+    zip_file_path = f"{report_directory}/final_report.zip"
     log.info(f"Creating zip file: '{zip_file_path}'")
+    log.info(f"Files to zip: {files_to_zip}")
     create_zip_file(dbutils, args.report_id, zip_file_path, files_to_zip)
     log.info(f"Finished creating '{zip_file_path}'")
