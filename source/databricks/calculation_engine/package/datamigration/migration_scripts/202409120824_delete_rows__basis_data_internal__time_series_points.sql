5921976da8ea29e981ff2bfb06178a82d4c3b058
Jonas Michelsen <jvm@energinet.dk>
2024-09-23 13:06:05 +0200

    .

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409120824_delete_rows__basis_data_internal__time_series_points.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
-WHERE metering_point_type IS NULL
-  AND resolution IS NULL
-  AND grid_area_code IS NULL
-  AND energy_supplier_id IS NULL;
