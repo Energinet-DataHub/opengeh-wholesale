ac44ae7cca9cacc8188ffc5e756ef3f39d6ba1bf
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2024-09-24 11:53:27 +0200

    sendgrid PIM (#2140)
    * sendgrid PIM
    
    * management lock

--- shared-resources/terraform/infrastructure/env/prod_001/rg-email.tf
+++ shared-resources/terraform/infrastructure/env/prod_001/rg-email.tf
@@ -6,28 +6,6 @@ data "azurerm_resource_group" "email_resource_group" {
   name = "rg-email-shres-p-we-001"
 }
 
-resource "azurerm_role_assignment" "xrtni_owner" {
-  scope                = data.azurerm_resource_group.email_resource_group.id
-  role_definition_name = "Owner"
-  principal_id         = "72dac0b0-db26-4fe5-a8f8-d6e34da67f87"
-}
-
-module "pim_contributor_security_group_permissions_email" {
-  count = var.pim_contributor_data_plane_group_name != "" ? 1 : 0
-
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/resource-group-role-assignments?ref=resource-group-role-assignments_4.3.1"
-
-  resource_group_name = data.azurerm_resource_group.email_resource_group.name
-  security_group_name = var.pim_contributor_data_plane_group_name
-  role_level          = "Contributor Data Plane"
-  custom_roles_contributor_data_plane = [
-    azurerm_role_definition.contributor_app_developers.name,
-    azurerm_role_definition.apim_groups_contributor_access.name,
-  ]
-
-  depends_on = [azurerm_resource_group.this]
-}
-
 module "pim_contributor_control_plane_security_group_permissions_email" {
   count = var.pim_contributor_control_plane_group_name != "" ? 1 : 0
 
@@ -36,22 +14,17 @@ module "pim_contributor_control_plane_security_group_permissions_email" {
   resource_group_name = data.azurerm_resource_group.email_resource_group.name
   security_group_name = var.pim_contributor_control_plane_group_name
   role_level          = "Contributor Control Plane"
+  # We add owner to this resource group, as Owner is required to access SendGrid
   custom_roles_contributor_control_plane = [
     azurerm_role_definition.contributor_app_developers.name,
     azurerm_role_definition.apim_groups_contributor_access.name,
     azurerm_role_definition.locks_contributor_access.name,
+    "Owner"
   ]
 }
 
-module "pim_reader_security_group_permissions_email" {
-  count = var.pim_reader_group_name != "" ? 1 : 0
-
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/resource-group-role-assignments?ref=resource-group-role-assignments_4.3.1"
-
-  resource_group_name = data.azurerm_resource_group.email_resource_group.name
-  security_group_name = var.pim_reader_group_name
-  role_level          = "Reader"
-  custom_roles_reader = [
-    azurerm_role_definition.app_config_settings_read_access.name
-  ]
+resource "azurerm_management_lock" "sendgrid_lock" {
+  name       = "sendgrid-lock"
+  scope      = data.azurerm_resource_group.email_resource_group.id
+  lock_level = "CanNotDelete"
 }
