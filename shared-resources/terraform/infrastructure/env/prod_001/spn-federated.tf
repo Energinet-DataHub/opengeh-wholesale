f30db01d4881fd470de5f994080f1c436b89a08e
Kristian Bank Erbou <contact@kbeconsult.dk>
2024-09-24 14:35:30 +0200

    Remove federated credential (#2141)

--- shared-resources/terraform/infrastructure/env/prod_001/spn-federated.tf
+++ shared-resources/terraform/infrastructure/env/prod_001/spn-federated.tf
@@ -1,12 +1,3 @@
-// Used for dh-githubautomation repository login and for accessing the current Terraform state on the `plan` job
-resource "azuread_application_federated_identity_credential" "dh_githubautomation_plan" {
-  application_id = data.azuread_application.current.id
-  display_name   = "dh-githubautomation-plan"
-  subject        = "repo:Energinet-DataHub/dh-githubautomation:environment:plan"
-  audiences      = [local.federated_identity_credential.audience_azuread]
-  issuer         = local.federated_identity_credential.issuer_github
-}
-
 // Used for dh-githubautomation repository login and for accessing the current Terraform state on the `apply` job
 resource "azuread_application_federated_identity_credential" "dh_githubautomation_apply" {
   application_id = data.azuread_application.current.id
