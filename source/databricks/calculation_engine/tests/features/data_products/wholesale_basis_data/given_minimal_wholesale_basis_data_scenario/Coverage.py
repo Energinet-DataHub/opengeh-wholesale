b12ae47d93baf78e6acf9ff9252504183d76b8ff
Svend (SLB) <slb@halfspace.io>
2024-09-24 06:58:46 +0000

    Fixing bad naming of one file

--- source/databricks/calculation_engine/tests/features/data_products/wholesale_basis_data/given_minimal_wholesale_basis_data_scenario/Coverage.py
+++ source/databricks/calculation_engine/tests/features/data_products/wholesale_basis_data/given_minimal_wholesale_basis_data_scenario/Coverage.py
@@ -7,9 +7,10 @@ The purpose is checking views related to amount per charge view.
 ## DESIGN CONSIDERATIONS
 - wholesale_internal.calculations table has three calculation_id's, only one of which is valid.
 - The two others are either internal or doesn't have an end date.
+- These three ids are used across all 6 basis_data tables.
 
 ## CASES TESTED
 """
-Cases.DataProductTests.WholesaleResultsTests.Only_calculation_ids_in_internal_calculations_included
-Cases.DataProductTests.WholesaleResultsTests.Only_external_calculation_ids_included
-Cases.DataProductTests.WholesaleResultsTests.Calculation_ids_without_calculation_succeeded_time_not_included
+Cases.DataProductTests.WholesaleBasisDataTests.Only_calculation_ids_in_internal_calculations_included
+Cases.DataProductTests.WholesaleBasisDataTests.Only_external_calculation_ids_included
+Cases.DataProductTests.WholesaleBasisDataTests.Calculation_ids_without_calculation_succeeded_time_not_included
