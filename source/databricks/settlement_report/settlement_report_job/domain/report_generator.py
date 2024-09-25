f5073f4a0dc296d5d3e3de448486419bb209ca14
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-25 12:12:27 +0200

    repository pattern

--- source/databricks/settlement_report/settlement_report_job/domain/report_generator.py
+++ source/databricks/settlement_report/settlement_report_job/domain/report_generator.py
@@ -4,6 +4,7 @@ from settlement_report_job.domain import time_series_writer
 from settlement_report_job.domain.metering_point_resolution import (
     DataProductMeteringPointResolution,
 )
+from settlement_report_job.domain.repository import WholesaleRepository
 from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
 from settlement_report_job.domain.time_series_factory import create_time_series
 from settlement_report_job.infrastructure.database_definitions import (
@@ -23,10 +24,11 @@ def execute(spark: SparkSession, args: SettlementReportArgs) -> None:
     dbutils = get_dbutils(spark)
     report_directory = f"{get_output_volume_name()}/{args.report_id}"
 
+    repository = WholesaleRepository(spark, args.catalog_name)
     hourly_time_series_df = create_time_series(
-        spark,
         args,
         DataProductMeteringPointResolution.HOUR,
+        repository,
     )
     hourly_time_series_files = time_series_writer.write(
         dbutils,
@@ -37,9 +39,9 @@ def execute(spark: SparkSession, args: SettlementReportArgs) -> None:
     )
 
     quarterly_time_series_df = create_time_series(
-        spark,
         args,
         DataProductMeteringPointResolution.QUARTER,
+        repository,
     )
     quarterly_time_series_files = time_series_writer.write(
         dbutils,
