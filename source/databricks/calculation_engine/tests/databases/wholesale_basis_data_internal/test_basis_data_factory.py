31bb85b8cb38725866af7aca526f36cc5bcc1f45
Jonas Michelsen <jvm@energinet.dk>
2024-09-25 12:38:22 +0200

    .

--- source/databricks/calculation_engine/tests/databases/wholesale_basis_data_internal/test_basis_data_factory.py
+++ source/databricks/calculation_engine/tests/databases/wholesale_basis_data_internal/test_basis_data_factory.py
@@ -13,11 +13,18 @@
 # limitations under the License.
 import pytest
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    DecimalType,
+    TimestampType,
+)
 
 from databases.wholesale_basis_data_internal.basis_data_test_factory import (
     create_basis_data_factory,
 )
+from package.databases.table_column_names import TableColumnNames
 from package.databases.wholesale_basis_data_internal.schemas import (
     charge_price_information_periods_schema,
     charge_price_points_schema,
@@ -28,8 +35,20 @@ from package.databases.wholesale_basis_data_internal.schemas import (
 from package.databases.wholesale_basis_data_internal.schemas.charge_link_periods_schema import (
     charge_link_periods_schema,
 )
-from package.databases.wholesale_basis_data_internal.schemas.time_series_points_schema import (
-    time_series_points_schema_temp,
+
+# TODO JVM: Schema need for a test and need to be removed when delta table is fixed
+time_series_points_schema_temp = StructType(
+    [
+        StructField(TableColumnNames.calculation_id, StringType(), False),
+        StructField(TableColumnNames.metering_point_id, StringType(), False),
+        StructField(TableColumnNames.quantity, DecimalType(18, 3), False),
+        StructField(TableColumnNames.quality, StringType(), False),
+        StructField(TableColumnNames.observation_time, TimestampType(), False),
+        StructField(TableColumnNames.metering_point_type, StringType(), False),
+        StructField(TableColumnNames.resolution, StringType(), False),
+        StructField(TableColumnNames.grid_area_code, StringType(), False),
+        StructField(TableColumnNames.energy_supplier_id, StringType(), True),
+    ]
 )
 
 
@@ -42,7 +61,7 @@ from package.databases.wholesale_basis_data_internal.schemas.time_series_points_
         ),
         (
             "time_series_points",
-            time_series_points_schema_temp,
+            time_series_points_schema_temp,  # TODO JVM: replace with real schema from time_series_points_schema, when delta table and code has the same nullability
         ),
         ("charge_link_periods", charge_link_periods_schema),
         (
