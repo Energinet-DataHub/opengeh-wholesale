8b228150255bd54f81deadf85945e9802a9b078f
Christian Hansen <120550591+cenh-halfspace@users.noreply.github.com>
2024-09-25 11:46:20 +0200

    feat: Multiple entry points for settlement reports job (#2819)
    * initial commit
    
    * removed unused task type
    
    * updated entry point and fixed comments
    
    * fixed
    
    * updated callable and fixed comments
    
    * update
    
    * readded entry
    
    * execute
    
    * fixed name

---
+++ source/databricks/settlement_report/settlement_report_job/domain/task_type.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class TaskType(Enum):
+    """
+    Enum for the different types of tasks that can be executed.
+    The value needs to correspond to the name of the task in the SettlementReportJob workflow in the infrastructure.
+    """
+
+    HOURLY_TIME_SERIES = "hourly_time_series"
+    QUARTERLY_TIME_SERIES = "quarterly_time_series"
+    ZIP = "zip"
