60684cad155a9ccb4d3c4884fec3660d542d1da1
Christian Hansen <120550591+cenh-halfspace@users.noreply.github.com>
2024-09-25 13:07:12 +0200

    feat: Settlement Report Split Tasks in Job (#2087)
    * feat: splits settlement report job in multiple dependent tasks
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * fix: align task keys
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * style: change zip task_key
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    
    * updated tasks
    
    * named params
    
    * updated terraform
    
    * updated params
    
    * updated package
    
    * task_key -> task_type
    
    * updated params
    
    * updated params
    
    * remove
    
    * updated values
    
    * updated default
    
    * updated dependencies
    
    * updated workflow
    
    ---------
    
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    Co-authored-by: Jens Peder Meldgaard <jpm@halfspace.io>
    Co-authored-by: Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>

--- wholesale/terraform/main/dbwj-settlement-report.tf
+++ wholesale/terraform/main/dbwj-settlement-report.tf
@@ -1,26 +1,51 @@
-resource "databricks_job" "settlement_report_job" {
+resource "databricks_job" "settlement_report_job_balancing" {
   provider            = databricks.dbw
   name                = "SettlementReportJob"
   max_concurrent_runs = 40
-
-  task {
-    task_key    = "settlement_report_job_${uuid()}"
-    max_retries = 1
-
-    new_cluster {
-      spark_version = local.spark_version
-      node_type_id  = "Standard_D8as_v4"
+  
+  job_cluster {
+	job_cluster_key = "hourly_time_series_cluster"
+	
+	new_cluster {
+      spark_version  = local.spark_version
+      node_type_id   = "Standard_D8as_v4"
       runtime_engine = "PHOTON"
       autoscale {
         min_workers = 1
         max_workers = 10
       }
       spark_env_vars = {
-        "DATA_STORAGE_ACCOUNT_NAME"         = data.azurerm_key_vault_secret.st_data_lake_name.value
-        "TIME_ZONE"                         = local.TIME_ZONE
-        "CATALOG_NAME"                      = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "DATA_STORAGE_ACCOUNT_NAME" = data.azurerm_key_vault_secret.st_data_lake_name.value
+        "TIME_ZONE"                 = local.TIME_ZONE
+        "CATALOG_NAME"              = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
       }
     }
+  }
+  
+  job_cluster {
+	job_cluster_key = "settlement_report_cluster"
+	
+	new_cluster {
+      spark_version  = local.spark_version
+      node_type_id   = "Standard_D8as_v4"
+      runtime_engine = "PHOTON"
+      autoscale {
+        min_workers = 1
+        max_workers = 10
+      }
+	  
+      spark_env_vars = {
+        "DATA_STORAGE_ACCOUNT_NAME" = data.azurerm_key_vault_secret.st_data_lake_name.value
+        "TIME_ZONE"                 = local.TIME_ZONE
+        "CATALOG_NAME"              = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+      }
+    }
+  }
+
+  task {
+    task_key    = "hourly_time_series"
+    max_retries = 1
+	job_cluster_key = "hourly_time_series_cluster"
 
     library {
       whl = "/Workspace/Shared/PythonWheels/settlement_report/opengeh_settlement_report-1.0-py3-none-any.whl"
@@ -29,7 +54,45 @@ resource "databricks_job" "settlement_report_job" {
     python_wheel_task {
       package_name = "opengeh_settlement_report"
       # The entry point is defined in setup.py
-      entry_point = "create_settlement_report"
+      entry_point = "create_hourly_time_series"
+    }
+  }
+
+  task {
+    task_key    = "quarterly_time_series"
+    max_retries = 1
+	job_cluster_key = "settlement_report_cluster"
+
+    library {
+      whl = "/Workspace/Shared/PythonWheels/settlement_report/opengeh_settlement_report-1.0-py3-none-any.whl"
+    }
+
+    python_wheel_task {
+      package_name = "opengeh_settlement_report"
+      # The entry point is defined in setup.py
+      entry_point = "create_quarterly_time_series"
+    }
+  }
+
+  task {
+    task_key    = "zip"
+    max_retries = 1
+	job_cluster_key = "settlement_report_cluster"
+
+    library {
+      whl = "/Workspace/Shared/PythonWheels/settlement_report/opengeh_settlement_report-1.0-py3-none-any.whl"
+    }
+
+    python_wheel_task {
+      package_name = "opengeh_settlement_report"
+      # The entry point is defined in setup.py
+      entry_point = "create_zip"
+    }
+    depends_on {
+      task_key = "hourly_time_series"
+    }
+    depends_on {
+      task_key = "quarterly_time_series"
     }
   }
 
