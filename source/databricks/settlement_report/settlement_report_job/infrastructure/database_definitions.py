781d7b62618fdcc9d413242322a3c890713dfce4
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-26 12:18:39 +0200

    ...

--- source/databricks/settlement_report/settlement_report_job/infrastructure/database_definitions.py
+++ source/databricks/settlement_report/settlement_report_job/infrastructure/database_definitions.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from settlement_report_job.infrastructure.environment_variables import get_catalog_name
 
 
 class WholesaleSettlementReportDatabase:
--- source/databricks/settlement_report/tests/conftest.py
+++ source/databricks/settlement_report/tests/conftest.py
@@ -19,11 +19,14 @@ from typing import Callable, Generator
 
 from delta import configure_spark_with_delta_pip
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType
 
 from settlement_report_job.domain.calculation_type import CalculationType
 from settlement_report_job.domain.market_role import MarketRole
 from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
 from tests.fixtures import DBUtilsFixture
+
+from settlement_report_job.infrastructure import database_definitions
 
 
 @pytest.fixture(scope="session")
@@ -52,6 +55,57 @@ def any_settlement_report_args() -> SettlementReportArgs:
         requesting_actor_market_role=MarketRole.DATAHUB_ADMINISTRATOR,
         requesting_actor_id="1111111111111",
     )
+
+
+@pytest.fixture(scope="session")
+def metering_point_periods_written_to_delta_table(spark: SparkSession) -> str:
+    table_name = "metering_point_periods"
+    file_name = ""
+    _write_input_test_data_to_table(
+        spark,
+        file_name,
+        database_definitions.WholesaleWholesaleResultsDatabase.DATABASE_NAME,
+        database_definitions.WholesaleSettlementReportDatabase.METERING_POINT_TIME_SERIES_VIEW_NAME,
+        table_location=f"{calculation_input_path}/{database_definitions.WholesaleSettlementReportDatabase.METERING_POINT_TIME_SERIES_VIEW_NAME}",
+    )
+
+    return table_name
+
+
+def _write_input_test_data_to_table(
+    spark: SparkSession,
+    file_name: str,
+    database_name: str,
+    table_name: str,
+    table_location: str,
+    schema: StructType,
+    mode: str = "overwrite",
+) -> None:
+    df = spark.read.csv(file_name, header=True, schema=schema)
+
+    spark.sql(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+
+    sql_schema = _struct_type_to_sql_schema(schema)
+    spark.sql(
+        f"CREATE TABLE IF NOT EXISTS {database_name}.{table_name} ({sql_schema}) USING DELTA LOCATION '{table_location}'"
+    )
+    df.write.format("delta").mode(mode).saveAsTable(f"{database_name}.{table_name}")
+
+
+def _struct_type_to_sql_schema(schema: StructType) -> str:
+    schema_string = ""
+    for field in schema.fields:
+        field_name = field.name
+        field_type = field.dataType.simpleString()
+
+        if not field.nullable:
+            field_type += " NOT NULL"
+
+        schema_string += f"{field_name} {field_type}, "
+
+    # Remove the trailing comma and space
+    schema_string = schema_string.rstrip(", ")
+    return schema_string
 
 
 @pytest.fixture(scope="session")
