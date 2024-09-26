89d1b3a02eaaba829955dc41ebaa03f92282713d
Jonas Michelsen <jvm@energinet.dk>
2024-09-26 13:40:19 +0200

    Update 202409251800_update_table__wholesale_basis_data_internal__time_series_points.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409251800_update_table__wholesale_basis_data_internal__time_series_points.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409251800_update_table__wholesale_basis_data_internal__time_series_points.sql
@@ -1,5 +1,24 @@
 MERGE INTO {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points AS t
-USING (SELECT DISTINCT(*) FROM {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.metering_point_periods) AS m
+    USING (WITH ranked_rows AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY calculation_id, metering_point_id, from_date, to_date ORDER BY energy_supplier_id) AS row_num
+    FROM
+        (SELECT DISTINCT * FROM ctl_shres_d_we_003.wholesale_basis_data_internal.metering_point_periods)
+    WHERE
+        calculation_id IN (
+            SELECT calculation_id
+            FROM ctl_shres_d_we_003.wholesale_basis_data_internal.metering_point_periods
+            GROUP BY calculation_id, metering_point_id, from_date, to_date
+            HAVING COUNT(DISTINCT energy_supplier_id) > 1
+        )
+)
+SELECT
+    calculation_id, metering_point_id, metering_point_type, settlement_method, grid_area_code, resolution, from_grid_area_code, to_grid_area_code, parent_metering_point_id, energy_supplier_id, balance_responsible_party_id, from_date, to_date
+FROM
+    ranked_rows
+WHERE
+    row_num = 1) AS m
 ON t.calculation_id = m.calculation_id
    AND t.metering_point_id = m.metering_point_id
    AND t.observation_time >= m.from_date
