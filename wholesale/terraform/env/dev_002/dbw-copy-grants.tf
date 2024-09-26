50fde8bc3aa3a624a13e04b00f48504ea3ad3ea0
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2024-09-26 10:02:24 +0200

    add external location and containers for copy in wholesale (#2147)
    * more copy for wholesale
    
    * more copy for wholesale

--- wholesale/terraform/env/dev_002/dbw-copy-grants.tf
+++ wholesale/terraform/env/dev_002/dbw-copy-grants.tf
@@ -15,3 +15,33 @@ resource "databricks_grant" "wholesale_internal_copy" {
 
   depends_on = [module.dbw]
 }
+
+resource "azurerm_storage_container" "wholesale_migrations_wholesale_copy" {
+  name                  = "wholesale-migrations-wholesale-restore-data"
+  storage_account_name  = module.st_data_wholesale.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "wholesale_internal_copy" {
+  name                  = "wholesale-internal-restore-data"
+  storage_account_name  = module.st_data_wholesale.name
+  container_access_type = "private"
+}
+
+resource "databricks_external_location" "wholesale_migrations_wholesale_copy" {
+  provider        = databricks.dbw
+  name            = "${azurerm_storage_container.wholesale_migrations_wholesale_copy.name}_${module.st_data_wholesale.name}"
+  url             = "abfss://${azurerm_storage_container.wholesale_migrations_wholesale_copy.name}@${module.st_data_wholesale.name}.dfs.core.windows.net/"
+  credential_name = data.azurerm_key_vault_secret.unity_storage_credential_id.value
+  comment         = "Managed by TF"
+  depends_on      = [module.dbw]
+}
+
+resource "databricks_external_location" "wholesale_internal_copy" {
+  provider        = databricks.dbw
+  name            = "${azurerm_storage_container.wholesale_internal_copy.name}_${module.st_data_wholesale.name}"
+  url             = "abfss://${azurerm_storage_container.wholesale_internal_copy.name}@${module.st_data_wholesale.name}.dfs.core.windows.net/"
+  credential_name = data.azurerm_key_vault_secret.unity_storage_credential_id.value
+  comment         = "Managed by TF"
+  depends_on      = [module.dbw]
+}
