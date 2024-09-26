6ea63db1f0f1eaf587511e643f12837be2163f08
Alexander <122119488+energinet-ajw@users.noreply.github.com>
2024-09-26 12:13:43 +0200

    more copy for wholesale v2 (#2149)
    Co-authored-by: Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>

--- wholesale/terraform/env/dev_003/dbw-copy-grants.tf
+++ wholesale/terraform/env/dev_003/dbw-copy-grants.tf
@@ -45,3 +45,21 @@ resource "databricks_external_location" "wholesale_internal_copy" {
   comment         = "Managed by TF"
   depends_on      = [module.dbw]
 }
+
+resource "databricks_grant" "wholesale_migrations_wholesale_target" {
+  provider          = databricks.dbw
+  external_location = databricks_external_location.wholesale_migrations_wholesale_copy.id
+  principal         = var.databricks_contributor_dataplane_group.name
+  privileges        = ["READ_FILES", "WRITE_FILES"]
+
+  depends_on = [module.dbw]
+}
+
+resource "databricks_grant" "wholesale_internal_target" {
+  provider          = databricks.dbw
+  external_location = databricks_external_location.wholesale_internal_copy.id
+  principal         = var.databricks_contributor_dataplane_group.name
+  privileges        = ["READ_FILES", "WRITE_FILES"]
+
+  depends_on = [module.dbw]
+}
