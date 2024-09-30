a814911886f3a5c017f7395056085f89fc8af3c6
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-09-30 22:00:51 +0200

    ...

--- source/databricks/settlement_report/tests/domain/test_execute_quarterly_time_series.py
+++ source/databricks/settlement_report/tests/domain/test_execute_quarterly_time_series.py
@@ -3,6 +3,9 @@ from pyspark.sql import SparkSession
 from fixtures import DBUtilsFixture
 from settlement_report_job.domain.report_generator import execute_quarterly_time_series
 from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
+from settlement_report_job.infrastructure.column_names import (
+    TimeSeriesPointCsvColumnNames,
+)
 
 
 def test_execute_quarterly_time_series__when_default_wholesale_scenario__returns_expected_number_of_files(
@@ -12,7 +15,12 @@ def test_execute_quarterly_time_series__when_default_wholesale_scenario__returns
     standard_wholesale_fixing_scenario_data_written_to_delta: None,
 ):
     # Arrange
-    expected_file_count = 2
+    expected_file_count = 2  # corresponding to the number of grid areas
+    expected_columns = [
+        TimeSeriesPointCsvColumnNames.metering_point_id,
+        TimeSeriesPointCsvColumnNames.metering_point_type,
+        TimeSeriesPointCsvColumnNames.start_of_day,
+    ] + [f"ENERGYQUANTITY{i}" for i in range(1, 101)]
 
     # Act
     execute_quarterly_time_series(
@@ -22,3 +30,7 @@ def test_execute_quarterly_time_series__when_default_wholesale_scenario__returns
     # Assert
     actual_files = dbutils.jobs.taskValues.get("quarterly_time_series_files")
     assert len(actual_files) == expected_file_count
+    for file_path in actual_files:
+        df = spark.read.csv(file_path, header=True)
+        assert df.count() > 0
+        assert df.columns == expected_columns
