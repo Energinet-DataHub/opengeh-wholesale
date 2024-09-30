f7db8c55b5ee214bf068c74ee5559a9caf581986
ClausQAEnerginet <xclpe@energinet.dk>
2024-09-30 08:31:57 +0200

    Update Coverage.py

--- source/databricks/calculation_engine/tests/features/energy_calculation/given_withdrawal_period_partially_covers_previous_calculation/Coverage.py
+++ source/databricks/calculation_engine/tests/features/energy_calculation/given_withdrawal_period_partially_covers_previous_calculation/Coverage.py
@@ -1,7 +1,25 @@
-from test_coverage.all_test_cases import Cases
+from coverage.all_test_cases import Cases
 
 """
 ## PURPOSE
+
+## DESIGN CONSIDERATIONS
+### 1st run - Day 1 to 3
+Combination:
+MP E02
+es 3000000000000
+ga 800
+
+  Day 1   Day 2   Day 3  
+|-------|-------|-------|
+ Results Results Results
+
+
+### 2nd run - Day 1 to 4
+Combination disappears on day 2 and after day 3:
+   Day 1     Day 2     Day 3     Day 4
+|---------|---------|---------|---------|
+| Results |   QM    | Results | nothing |
 
 ## CASES TESTED
 """
