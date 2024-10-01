e35f1eee55794afd9cf587ae365ab91e53af25cf
ClausQAEnerginet <xclpe@energinet.dk>
2024-10-01 09:18:56 +0200

    Update all_test_cases.py

--- source/databricks/calculation_engine/tests/coverage/all_test_cases.py
+++ source/databricks/calculation_engine/tests/coverage/all_test_cases.py
@@ -58,7 +58,7 @@ class Cases:
             WithDrawal_when_time_series_removed_which_were_previously_withdrawn: str
             WithDrawal_when_previous_calculation_was_internal: str
             WithDrawal_when_current_calculation_is_internal: str
-            WithDrawal_when_new_calculation_period_only_partially_covers_previous: str
+            WithDrawal_when_new_calculation_period_only_partially_covers_previous_calculation: str
 
     class DataProductTests:
 
