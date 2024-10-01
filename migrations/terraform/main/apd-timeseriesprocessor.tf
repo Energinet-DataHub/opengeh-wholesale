59e4ad4129a06be05b9dae65769e4a4d08cf9f6e
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2024-10-01 13:59:35 +0200

    Add dashboards for new TS sync (#2168)
    * Add dashboards for new TS sync
    
    * fix names

---
+++ migrations/terraform/main/apd-timeseriesprocessor.tf
@@ -0,0 +1,18 @@
+resource "azurerm_portal_dashboard" "timeseriesprocessor" {
+  name                = "apd-timeseriesprocessor-${local.resources_suffix}"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  dashboard_properties = templatefile("dashboard-templates/timeseriessync_dashboard.tpl",
+    {
+      timeseriessync_id   = module.func_timeseriesprocessor.id,
+      timeseriessync_name = module.func_timeseriesprocessor.name,
+      appi_sharedres_id   = data.azurerm_key_vault_secret.appi_id.value,
+      appi_sharedres_name = data.azurerm_key_vault_secret.appi_name.value,
+      plan_services_id    = module.message_processor_service_plan.id
+      plan_services_name  = module.message_processor_service_plan.name
+      sbns_shared_id      = data.azurerm_key_vault_secret.sb_domain_relay_namespace_id.value,
+      sbns_shared_name    = data.azurerm_key_vault_secret.sb_domain_relay_namespace_name.value,
+  })
+
+  tags = local.tags
+}
