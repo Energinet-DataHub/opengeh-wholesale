814251ef4601c3de6e24769cc279235c85824dfd
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-01 13:52:54 +0200

    test: Test execute_quarterly_time_series (#2838)

--- source/databricks/settlement_report/tests/data_seeding/write_test_data.py
+++ source/databricks/settlement_report/tests/data_seeding/write_test_data.py
@@ -1,18 +1,36 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType
 
+from settlement_report_job.infrastructure import database_definitions
+from settlement_report_job.infrastructure.schemas.metering_point_time_series_v1 import (
+    metering_point_time_series_v1,
+)
 
-def write_input_test_data_to_table(
+
+def write_metering_point_time_series_to_delta_table(
     spark: SparkSession,
-    file_name: str,
+    df: DataFrame,
+    table_location: str,
+) -> None:
+    write_dataframe_to_table(
+        spark,
+        df=df,
+        database_name=database_definitions.WholesaleSettlementReportDatabase.DATABASE_NAME,
+        table_name=database_definitions.WholesaleSettlementReportDatabase.METERING_POINT_TIME_SERIES_VIEW_NAME,
+        table_location=f"{table_location}/{database_definitions.WholesaleSettlementReportDatabase.METERING_POINT_TIME_SERIES_VIEW_NAME}",
+        schema=metering_point_time_series_v1,
+    )
+
+
+def write_dataframe_to_table(
+    spark: SparkSession,
+    df: DataFrame,
     database_name: str,
     table_name: str,
     table_location: str,
     schema: StructType,
     mode: str = "overwrite",
 ) -> None:
-    df = spark.read.csv(file_name, header=True, schema=schema)
-
     spark.sql(f"CREATE DATABASE IF NOT EXISTS {database_name}")
 
     sql_schema = _struct_type_to_sql_schema(schema)
