b43f78055097e5d205285d2e3d6b144df11f1990
Michael <52096428+michaelmajgaard@users.noreply.github.com>
2024-10-02 11:47:45 +0200

    chore: dh2 bridge - service bus iam options (#2167)

--- dh2-bridge/terraform/main/func-grid-loss-event-receiver.config.tf
+++ dh2-bridge/terraform/main/func-grid-loss-event-receiver.config.tf
@@ -4,6 +4,11 @@ locals {
       "BlobStorageSettings:AccountUri"                        = local.DH2_BRIDGE_DOCUMENT_STORAGE_ACCOUNT_URI
       "BlobStorageSettings:ContainerName"                     = local.DH2_BRIDGE_DOCUMENT_STORAGE_CONTAINER_NAME
       "DatabaseSettings:ConnectionString"                     = local.MS_DH2_BRIDGE_CONNECTION_STRING
+
+      "IntegrationEvents:TopicName"                           = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sbt-shres-integrationevent-received-name)"
+      "IntegrationEvents:SubscriptionName"                    = module.sbtsub_dh2_bridge_event_listener.name
+      "ServiceBus.FullyQualifiedNamespace"                    = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-namespace-endpoint)"
+
       "ConsumeServiceBusSettings:FullyQualifiedNamespace"     = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-namespace-endpoint)"
       "ConsumeServiceBusSettings:ConnectionString"            = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-listen-connection-string)"
       "ConsumeServiceBusSettings:HealthCheckConnectionString" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-transceiver-connection-string)"
--- dh2-bridge/terraform/main/func-grid-loss-simulator.app-settings.tf
+++ dh2-bridge/terraform/main/func-grid-loss-simulator.app-settings.tf
@@ -1,6 +1,10 @@
 locals {
   entrypoint_grid_loss_simulator = {
     app_settings = {
+      "IntegrationEvents:TopicName"                           = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sbt-shres-integrationevent-received-name)"
+      "IntegrationEvents:SubscriptionName"                    = module.sbtsub_dh2_bridge_event_listener.name
+      "ServiceBus.FullyQualifiedNamespace"                    = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-namespace-endpoint)"
+
       "ConsumeServiceBusSettings:HealthCheckConnectionString" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-transceiver-connection-string)"
       "ConsumeServiceBusSettings:GridLossSubscription"        = module.sbtsub_dh2_bridge_event_listener.name
       "PublisherOptions:ServiceBusConnectionString"           = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=sb-domain-relay-send-connection-string)"
