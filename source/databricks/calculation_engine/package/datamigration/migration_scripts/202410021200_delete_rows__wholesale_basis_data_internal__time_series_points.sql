261e313e0cfa6cf6269b5ee6caf8e0a5bb95b9f1
Jonas Michelsen <jvm@energinet.dk>
2024-10-02 13:23:45 +0200

    Update 202410021200_delete_rows__wholesale_basis_data_internal__time_series_points.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202410021200_delete_rows__wholesale_basis_data_internal__time_series_points.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202410021200_delete_rows__wholesale_basis_data_internal__time_series_points.sql
@@ -7,5 +7,5 @@ WHERE calculation_id IN (
 AND metering_point_id IN (
     SELECT metering_point_id
     FROM {CATALOG_NAME}.{SHARED_WHOLESALE_INPUT_DATABASE_NAME}.metering_point_periods_view_v1
-    WHERE type IN ('D01', 'D05', 'D06', 'D07', 'D08', 'D09', 'D10', 'D11', 'D12', 'D14', 'D15', 'D19')
+    WHERE type NOT IN ('E18', 'E17', 'E20')
 )
