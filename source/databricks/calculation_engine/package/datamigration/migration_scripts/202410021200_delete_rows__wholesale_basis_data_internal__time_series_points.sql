99a2f3a13c77af38a6aaf8fdb0680a8aa75d59e2
Jonas Michelsen <jvm@energinet.dk>
2024-10-02 13:33:42 +0200

    Update 202410021200_delete_rows__wholesale_basis_data_internal__time_series_points.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202410021200_delete_rows__wholesale_basis_data_internal__time_series_points.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202410021200_delete_rows__wholesale_basis_data_internal__time_series_points.sql
@@ -1,11 +1,13 @@
-DELETE FROM {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
-WHERE calculation_id IN (
-    SELECT calculation_id
-    FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations
-    WHERE calculation_type IN ('balance_fixing', 'aggregation')
-)
-AND metering_point_id IN (
-    SELECT metering_point_id
-    FROM {CATALOG_NAME}.{SHARED_WHOLESALE_INPUT_DATABASE_NAME}.metering_point_periods_view_v1
-    WHERE type NOT IN ('E18', 'E17', 'E20')
-)
+{DATABRICKS-ONLY}DELETE FROM {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
+{DATABRICKS-ONLY}WHERE calculation_id IN (
+{DATABRICKS-ONLY}    SELECT calculation_id
+{DATABRICKS-ONLY}    FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations
+{DATABRICKS-ONLY}    WHERE calculation_type IN ('balance_fixing', 'aggregation')
+{DATABRICKS-ONLY})
+{DATABRICKS-ONLY}AND metering_point_id IN (
+{DATABRICKS-ONLY}    SELECT metering_point_id
+{DATABRICKS-ONLY}    FROM {CATALOG_NAME}.{SHARED_WHOLESALE_INPUT_DATABASE_NAME}.metering_point_periods_view_v1
+{DATABRICKS-ONLY}    WHERE type NOT IN ('E18', 'E17', 'E20')
+{DATABRICKS-ONLY})
+
+{TEST-ONLY}SELECT 1;
