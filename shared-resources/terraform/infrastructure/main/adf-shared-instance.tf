b6927389fc5b7c839b51a0800fec723af5551854
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2024-10-03 14:59:12 +0200

    alerts if ADF fails jobs (#2188)
    * alerts if ADF fails jobs
    
    * alerts if ADF fails jobs
    
    * alerts if ADF fails jobs
    
    * alerts if ADF fails jobs
    
    * update name

--- shared-resources/terraform/infrastructure/main/adf-shared-instance.tf
+++ shared-resources/terraform/infrastructure/main/adf-shared-instance.tf
@@ -17,6 +17,31 @@ resource "azurerm_data_factory" "this" {
   tags = local.tags
 }
 
+resource "azurerm_monitor_metric_alert" "adf_failed_runs" {
+  count = var.alert_email_address != null ? 1 : 0
+
+  name                = "adf-failed-run-${local.resources_suffix}"
+  resource_group_name = azurerm_resource_group.this.name
+  scopes              = [azurerm_data_factory.this.id]
+  description         = "Action will be triggered if a pipeline run fails."
+  severity            = 0     # Critical
+  auto_mitigate       = false # We want to handle it manually and check up on the issue
+
+  criteria {
+    metric_namespace = "Microsoft.DataFactory/factories"
+    metric_name      = "PipelineFailedRuns"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 0
+  }
+
+  action {
+    action_group_id = module.monitor_action_group_shres[0].id
+  }
+
+  tags = local.tags
+}
+
 module "kvs_azure_data_factory_id" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=key-vault-secret_4.0.1"
 
