dcfc19acbf0aca52c2bda051990b38028cdcd14a
Jonas Michelsen <jvm@energinet.dk>
2024-10-03 08:57:56 +0200

    Revert "Update 202410021200_delete_rows__wholesale_basis_data_internal__time_series_points.sql"
    This reverts commit 5904fcf4517072d43793798d5f3c7e35931d9720.

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202410021200_delete_rows__wholesale_basis_data_internal__time_series_points.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202410021200_delete_rows__wholesale_basis_data_internal__time_series_points.sql
@@ -1,10 +1,13 @@
 {DATABRICKS-ONLY}DELETE FROM {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
-{DATABRICKS-ONLY}WHERE (metering_point_type IS NULL
-{DATABRICKS-ONLY}OR metering_point_type NOT IN ('production', 'consumption', 'exchange'))
-{DATABRICKS-ONLY}AND calculation_id IN (
+{DATABRICKS-ONLY}WHERE calculation_id IN (
 {DATABRICKS-ONLY}    SELECT calculation_id
 {DATABRICKS-ONLY}    FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations
 {DATABRICKS-ONLY}    WHERE calculation_type IN ('balance_fixing', 'aggregation')
+{DATABRICKS-ONLY})
+{DATABRICKS-ONLY}AND metering_point_id IN (
+{DATABRICKS-ONLY}    SELECT metering_point_id
+{DATABRICKS-ONLY}    FROM {CATALOG_NAME}.{SHARED_WHOLESALE_INPUT_DATABASE_NAME}.metering_point_periods_view_v1
+{DATABRICKS-ONLY}    WHERE type NOT IN ('E18', 'E17', 'E20')
 {DATABRICKS-ONLY})
 
 {TEST-ONLY}SELECT 1;
