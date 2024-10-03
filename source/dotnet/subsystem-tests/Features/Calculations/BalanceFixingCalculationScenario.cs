6c35b815efee9ffdcb849d932ec69f16a8e09c9b
Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>
2024-10-03 13:03:28 +0200

    feat: Set xUnit filters in .NET (#2857)
    * ExecutionEnvironment trait
    
    * Add ExecutionEnvironment attribute
    
    * Support environment and trigger in one attribute
    
    * Execution context
    
    * Rename AnyDev to AllDev

--- source/dotnet/subsystem-tests/Features/Calculations/BalanceFixingCalculationScenario.cs
+++ source/dotnet/subsystem-tests/Features/Calculations/BalanceFixingCalculationScenario.cs
@@ -24,6 +24,7 @@ using Xunit;
 
 namespace Energinet.DataHub.Wholesale.SubsystemTests.Features.Calculations;
 
+[ExecutionContext(AzureEnvironment.AllDev)]
 [TestCaseOrderer(
     ordererTypeName: "Energinet.DataHub.Wholesale.SubsystemTests.Fixtures.Orderers.ScenarioStepOrderer",
     ordererAssemblyName: "Energinet.DataHub.Wholesale.SubsystemTests")]
