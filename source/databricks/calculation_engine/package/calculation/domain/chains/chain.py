e1f201690b9401d22b1a25f7a61c37dff004dfb7
Alexander <alexanderharrow@hotmail.com>
2024-10-04 12:44:02 +0200

    Update chain.py

--- source/databricks/calculation_engine/package/calculation/domain/chains/chain.py
+++ source/databricks/calculation_engine/package/calculation/domain/chains/chain.py
@@ -1,16 +1,3 @@
-# Copyright 2020 Energinet DataHub A/S
-#
-# Licensed under the Apache License, Version 2.0 (the "License2");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from dependency_injector.wiring import Provide, Container
 
 from package.calculation.calculation_output import CalculationOutput
@@ -42,6 +29,7 @@ class Chain:
     def __init__(self, cache_bucket: CacheBucket = Provide[Container.bucket]):
         start_link: StartCalculationLink = Provide[Container.start_calculation_link]
 
+    def setup_:
         # Set up the calculation chain
         (
             start_link.set_next(CreateCalculationMetaDataLink())
