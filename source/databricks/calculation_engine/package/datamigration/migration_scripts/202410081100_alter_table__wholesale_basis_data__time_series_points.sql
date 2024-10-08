c1a31f2c7121c1a0de46f001919dc1d366244799
Jonas Michelsen <jvm@energinet.dk>
2024-10-08 13:53:05 +0200

    Add constraints to time_series_points basis data

---
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202410081100_alter_table__wholesale_basis_data__time_series_points.sql
@@ -0,0 +1,8 @@
+ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
+SET TBLPROPERTIES (
+    delta.deletedFileRetentionDuration = 'interval 30 days',
+    delta.constraints.metering_point_type_chk = "metering_point_type IS NULL OR metering_point_type IN ( 'production' , 'consumption' , 'exchange' , 've_production' , 'net_production' , 'supply_to_grid' , 'consumption_from_grid' , 'wholesale_services_information' , 'own_production' , 'net_from_grid' , 'net_to_grid' , 'total_consumption' , 'electrical_heating' , 'net_consumption' , 'effect_settlement' )",
+    delta.constraints.grid_area_code_chk = "LENGTH ( grid_area_code ) = 3",
+    delta.constraints.resolution_chk = "resolution IN ( 'PT1H' , 'PT15M' )",
+    delta.constraints.energy_supplier_id_chk = "energy_supplier_id IS NULL OR LENGTH ( energy_supplier_id ) = 13 OR LENGTH ( energy_supplier_id ) = 16",
+);
