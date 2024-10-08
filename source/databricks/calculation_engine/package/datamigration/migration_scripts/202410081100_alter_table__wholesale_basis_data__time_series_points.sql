fd88875dece62b87605dae9923419316768882c4
Jonas Michelsen <jvm@energinet.dk>
2024-10-08 14:24:47 +0200

    Update 202410081100_alter_table__wholesale_basis_data__time_series_points.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202410081100_alter_table__wholesale_basis_data__time_series_points.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202410081100_alter_table__wholesale_basis_data__time_series_points.sql
@@ -1,14 +1,17 @@
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
 ADD CONSTRAINT metering_point_type_chk
     CHECK (metering_point_type IN ( 'production' , 'consumption' , 'exchange' , 've_production' , 'net_production' , 'supply_to_grid' , 'consumption_from_grid' , 'wholesale_services_information' , 'own_production' , 'net_from_grid' , 'net_to_grid' , 'total_consumption' , 'electrical_heating' , 'net_consumption' , 'effect_settlement' ))
+GO
 
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
 ADD CONSTRAINT grid_area_code_chk
     CHECK (LENGTH ( grid_area_code ) = 3)
+GO
 
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
 ADD CONSTRAINT resolution_chk
     CHECK (resolution IN ( 'PT1H' , 'PT15M' ))
+GO
 
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_BASIS_DATA_INTERNAL_DATABASE_NAME}.time_series_points
 ADD CONSTRAINT energy_supplier_id_chk
