76cdb72800246a518fb4287a1ce7bfdde0919089
Lars Nielsen <lanni@energinet.dk>
2024-10-09 13:30:57 +0200

    Upgrade modules and azurerm provider on settlement-report (#2225)

--- settlement-report/terraform/main/mssqldb-applicationaccess.tf
+++ settlement-report/terraform/main/mssqldb-applicationaccess.tf
@@ -1,7 +1,7 @@
 module "mssql_database_application_access" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database-application-access?ref=mssql-database-application-access_4.0.1"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database-application-access?ref=mssql-database-application-access_5.0.0"
 
-  sql_server_name = data.azurerm_mssql_server.mssqlsrv.name
+  sql_server_name = module.mssqldb_settlement_report.server_name
   database_name   = module.mssqldb_settlement_report.name
   application_hosts_names = [
     module.app_api.name,
--- settlement-report/terraform/main/mssqldb-settlement-report.tf
+++ settlement-report/terraform/main/mssqldb-settlement-report.tf
@@ -1,19 +1,21 @@
-data "azurerm_mssql_server" "mssqlsrv" {
-  name                = data.azurerm_key_vault_secret.mssql_data_name.value
-  resource_group_name = data.azurerm_resource_group.shared.name
-}
-
 module "mssqldb_settlement_report" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=mssql-database_4.0.1"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=mssql-database_8.0.0"
 
   name                 = "settlement-report"
   location             = azurerm_resource_group.this.location
   project_name         = var.domain_name_short
   environment_short    = var.environment_short
   environment_instance = var.environment_instance
-  server_id            = data.azurerm_mssql_server.mssqlsrv.id
-  sql_server_name      = data.azurerm_mssql_server.mssqlsrv.name
-  elastic_pool_id      = data.azurerm_key_vault_secret.mssql_data_elastic_pool_id.value
+
+  server = {
+    name                = data.azurerm_key_vault_secret.mssql_data_name.value
+    resource_group_name = data.azurerm_key_vault_secret.mssql_data_resource_group_name.value
+  }
+
+  elastic_pool = {
+    name                = data.azurerm_key_vault_secret.mssql_data_elastic_pool_name.value
+    resource_group_name = data.azurerm_key_vault_secret.mssql_data_elastic_pool_resource_group_name.value
+  }
 
   monitor_action_group = length(module.monitor_action_group_setr) != 1 ? null : {
     id                  = module.monitor_action_group_setr[0].id
@@ -22,7 +24,7 @@ module "mssqldb_settlement_report" {
 }
 
 module "kvs_sql_ms_settlement_report_database_name" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=key-vault-secret_4.0.1"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=key-vault-secret_5.0.0"
 
   name         = "mssql-settlement-report-database-name"
   value        = module.mssqldb_settlement_report.name
