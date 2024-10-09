6c18544707234341d4a2df9bf1f841d8ae022f72
Lars Nielsen <lanni@energinet.dk>
2024-10-09 08:56:46 +0200

    Upgrade modules and azurerm provider on gridloss (#2219)

--- grid-loss-imbalance-prices/terraform/main/mssqldb-applicationaccess.tf
+++ grid-loss-imbalance-prices/terraform/main/mssqldb-applicationaccess.tf
@@ -1,7 +1,7 @@
 module "mssql_database_application_access" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database-application-access?ref=mssql-database-application-access_4.0.1"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database-application-access?ref=mssql-database-application-access_5.0.0"
 
-  sql_server_name = data.azurerm_mssql_server.mssqlsrv.name
+  sql_server_name = module.mssqldb_grid_loss_imbalance_prices.server_name
   database_name   = module.mssqldb_grid_loss_imbalance_prices.name
 
   application_hosts_names = [
