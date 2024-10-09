bd216fefe035da9be0103465503dda47593cbf7d
Lars Nielsen <lanni@energinet.dk>
2024-10-09 07:45:23 +0200

    Upgrade modules and azurerm provider on sauron-healthchecks (#2218)

--- sauron-healthchecks-api/terraform/main/service-plans.tf
+++ sauron-healthchecks-api/terraform/main/service-plans.tf
@@ -1,5 +1,5 @@
 module "func_service_plan" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-plan?ref=service-plan_4.0.1"
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-plan?ref=service-plan_5.0.0"
 
   type                 = "webapp"
   project_name         = var.domain_name_short
