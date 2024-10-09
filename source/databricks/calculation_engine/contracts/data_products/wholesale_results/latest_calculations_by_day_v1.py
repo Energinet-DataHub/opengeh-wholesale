6cc1bb34d91b2d1dac1e7bc95a1fc0a4e412f54c
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-09 14:47:12 +0200

    ...

--- source/databricks/calculation_engine/contracts/data_products/wholesale_results/latest_calculations_by_day_v1.py
+++ source/databricks/calculation_engine/contracts/data_products/wholesale_results/latest_calculations_by_day_v1.py
@@ -2,7 +2,7 @@ import pyspark.sql.types as t
 
 nullable = True
 
-latest_calculation_by_day_v1 = t.StructType(
+latest_calculations_by_day_v1 = t.StructType(
     [
         # UUID
         t.StructField("calculation_id", t.StringType(), not nullable),
