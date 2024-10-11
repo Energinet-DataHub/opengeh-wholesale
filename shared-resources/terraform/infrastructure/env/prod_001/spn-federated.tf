89b211dff51c1ac54622e4395ccb116eb44d20dd
Kristian Bank Erbou <contact@kbeconsult.dk>
2024-10-11 11:15:48 +0200

    Remove cyclic dependency with dh-githubautomation (#2241)

--- shared-resources/terraform/infrastructure/env/prod_001/spn-federated.tf
+++
@@ -1,8 +0,0 @@
-// Used for dh-githubautomation repository login and for accessing the current Terraform state on the `apply` job
-resource "azuread_application_federated_identity_credential" "dh_githubautomation_apply" {
-  application_id = data.azuread_application.current.id
-  display_name   = "dh-githubautomation-apply"
-  subject        = "repo:Energinet-DataHub/dh-githubautomation:environment:apply"
-  audiences      = [local.federated_identity_credential.audience_azuread]
-  issuer         = local.federated_identity_credential.issuer_github
-}
