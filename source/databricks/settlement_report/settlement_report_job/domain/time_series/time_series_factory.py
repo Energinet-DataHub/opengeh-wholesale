f3c4916229d1326ab1aaeaf450a0cf599289eccf
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-15 12:22:03 +0200

    ...

--- source/databricks/settlement_report/settlement_report_job/domain/time_series/time_series_factory.py
+++ source/databricks/settlement_report/settlement_report_job/domain/time_series/time_series_factory.py
@@ -50,7 +50,7 @@ def create_time_series_for_balance_fixing(
     time_series_points = read_and_filter_for_balance_fixing(
         period_start=period_start,
         period_end=period_end,
-        grid_are_codes=grid_area_codes,
+        grid_area_codes=grid_area_codes,
         energy_supplier_ids=energy_supplier_ids,
         metering_point_resolution=metering_point_resolution,
         requesting_actor_market_role=requesting_actor_market_role,
