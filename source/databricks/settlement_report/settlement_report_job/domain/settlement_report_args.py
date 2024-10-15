427351281ea0ddc1bf3c78b687cb8e867cda1d4b
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-15 10:39:11 +0200

    ...

--- source/databricks/settlement_report/settlement_report_job/domain/settlement_report_args.py
+++ source/databricks/settlement_report/settlement_report_job/domain/settlement_report_args.py
@@ -1,6 +1,7 @@
 import uuid
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 from settlement_report_job.infrastructure.calculation_type import CalculationType
 from settlement_report_job.domain.market_role import MarketRole
@@ -14,11 +15,11 @@ class SettlementReportArgs:
     calculation_type: CalculationType
     requesting_actor_market_role: MarketRole
     requesting_actor_id: str
-    calculation_id_by_grid_area: dict[str, uuid.UUID] | None
+    calculation_id_by_grid_area: Optional[dict[str, uuid.UUID]]
     """ A dictionary containing grid area codes (keys) and calculation ids (values). None for balance fixing"""
-    grid_area_codes: list[str] | None
+    grid_area_codes: Optional[list[str]]
     """ None if NOT balance fixing"""
-    energy_supplier_ids: list[str] | None
+    energy_supplier_ids: Optional[list[str]]
     split_report_by_grid_area: bool
     prevent_large_text_files: bool
     time_zone: str
