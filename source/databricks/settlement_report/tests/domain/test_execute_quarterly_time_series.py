07f5c0d53551c910966d0121bb93f3c4d082b433
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-17 21:33:29 +0200

    ...

--- source/databricks/settlement_report/tests/domain/test_execute_quarterly_time_series.py
+++ source/databricks/settlement_report/tests/domain/test_execute_quarterly_time_series.py
@@ -3,7 +3,10 @@ import pytest
 
 from tests.fixtures import DBUtilsFixture
 
-from data_seeding import standard_wholesale_fixing_scenario_data_generator
+from data_seeding import (
+    standard_wholesale_fixing_scenario_data_generator,
+    standard_balance_fixing_scenario_data_generator,
+)
 from settlement_report_job.domain.market_role import MarketRole
 from settlement_report_job.domain.report_generator import execute_quarterly_time_series
 from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
@@ -196,3 +199,41 @@ def test_execute_quarterly_time_series__when_include_basis_data_false__returns_n
     # Assert
     actual_files = dbutils.jobs.taskValues.get("quarterly_time_series_files")
     assert actual_files is None or len(actual_files) == 0
+
+
+def test_execute_quarterly_time_series__when_energy_supplier_and_balance_fixing__returns_expected(
+    spark: SparkSession,
+    dbutils: DBUtilsFixture,
+    standard_balance_fixing_scenario_args: SettlementReportArgs,
+    standard_balance_fixing_scenario_data_written_to_delta: None,
+):
+    # Arrange
+    standard_balance_fixing_scenario_args.requesting_actor_market_role = (
+        MarketRole.ENERGY_SUPPLIER
+    )
+    energy_supplier_id = (
+        standard_balance_fixing_scenario_data_generator.ENERGY_SUPPLIER_IDS[0]
+    )
+    standard_balance_fixing_scenario_args.requesting_actor_id = energy_supplier_id
+    standard_balance_fixing_scenario_args.energy_supplier_ids = [energy_supplier_id]
+    expected_file_names = [
+        f"TSSD15_804_{energy_supplier_id}_DDQ_02-01-2024_02-01-2024.csv",
+        f"TSSD15_805_{energy_supplier_id}_DDQ_02-01-2024_02-01-2024.csv",
+    ]
+    expected_columns = [
+        CsvColumnNames.metering_point_id,
+        CsvColumnNames.metering_point_type,
+        CsvColumnNames.start_of_day,
+    ] + [f"ENERGYQUANTITY{i}" for i in range(1, 101)]
+
+    # Act
+    execute_quarterly_time_series(spark, dbutils, standard_balance_fixing_scenario_args)
+
+    # Assert
+    actual_files = dbutils.jobs.taskValues.get(key="quarterly_time_series_files")
+    assert len(actual_files) == len(expected_file_names)
+    for file_path in actual_files:
+        df = spark.read.option("delimiter", ";").csv(file_path, header=True)
+        assert df.count() > 0
+        assert df.columns == expected_columns
+        assert any(file_name in file_path for file_name in expected_file_names)
