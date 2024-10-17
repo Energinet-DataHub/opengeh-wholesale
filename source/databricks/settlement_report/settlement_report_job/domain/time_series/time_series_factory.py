dd118a4bb37162eb817b82cdc04a2a0256c93024
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-17 07:48:31 +0200

    ...

--- source/databricks/settlement_report/settlement_report_job/domain/time_series/time_series_factory.py
+++ source/databricks/settlement_report/settlement_report_job/domain/time_series/time_series_factory.py
@@ -92,5 +92,6 @@ def create_time_series_for_wholesale(
         filtered_time_series_points=time_series_points,
         metering_point_resolution=metering_point_resolution,
         time_zone=time_zone,
+        requesting_market_role=requesting_actor_market_role,
     )
     return prepared_time_series
