2953ffa34fea50fd423b4f69b119b701b9d83ec0
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-18 09:13:27 +0200

    ...

--- source/databricks/settlement_report/tests/domain/test_execute_quarterly_time_series.py
+++ source/databricks/settlement_report/tests/domain/test_execute_quarterly_time_series.py
@@ -1,3 +1,5 @@
+import copy
+
 from pyspark.sql import SparkSession
 import pytest
 
@@ -30,13 +32,12 @@ def test_execute_quarterly_time_series__when_energy_supplier__returns_expected(
     standard_wholesale_fixing_scenario_data_written_to_delta: None,
 ):
     # Arrange
-    standard_wholesale_fixing_scenario_args.requesting_actor_market_role = (
-        MarketRole.ENERGY_SUPPLIER
-    )
+    args = copy.deepcopy(standard_wholesale_fixing_scenario_args)
+    args.requesting_actor_market_role = MarketRole.ENERGY_SUPPLIER
     energy_supplier_id = (
         standard_wholesale_fixing_scenario_data_generator.ENERGY_SUPPLIER_IDS[0]
     )
-    standard_wholesale_fixing_scenario_args.energy_supplier_ids = [energy_supplier_id]
+    args.energy_supplier_ids = [energy_supplier_id]
     expected_file_names = [
         f"TSSD15_804_{energy_supplier_id}_DDQ_02-01-2024_02-01-2024.csv",
         f"TSSD15_805_{energy_supplier_id}_DDQ_02-01-2024_02-01-2024.csv",
@@ -48,9 +49,7 @@ def test_execute_quarterly_time_series__when_energy_supplier__returns_expected(
     ] + [f"ENERGYQUANTITY{i}" for i in range(1, 101)]
 
     # Act
-    execute_quarterly_time_series(
-        spark, dbutils, standard_wholesale_fixing_scenario_args
-    )
+    execute_quarterly_time_series(spark, dbutils, args)
 
     # Assert
     actual_files = dbutils.jobs.taskValues.get(key="quarterly_time_series_files")
@@ -69,16 +68,13 @@ def test_execute_quarterly_time_series__when_grid_access_provider__returns_expec
     standard_wholesale_fixing_scenario_data_written_to_delta: None,
 ):
     # Arrange
-    standard_wholesale_fixing_scenario_args.requesting_actor_market_role = (
-        MarketRole.GRID_ACCESS_PROVIDER
-    )
-    standard_wholesale_fixing_scenario_args.energy_supplier_ids = None
-    actor_id = standard_wholesale_fixing_scenario_args.requesting_actor_id
+    args = copy.deepcopy(standard_wholesale_fixing_scenario_args)
+    args.requesting_actor_market_role = MarketRole.GRID_ACCESS_PROVIDER
+    args.energy_supplier_ids = None
     expected_file_names = [
-        f"TSSD15_804_{actor_id}_DDM_02-01-2024_02-01-2024.csv",
-        f"TSSD15_805_{actor_id}_DDM_02-01-2024_02-01-2024.csv",
+        f"TSSD15_804_{args.requesting_actor_id}_DDM_02-01-2024_02-01-2024.csv",
+        f"TSSD15_805_{args.requesting_actor_id}_DDM_02-01-2024_02-01-2024.csv",
     ]
-    standard_wholesale_fixing_scenario_args.energy_supplier_ids = None
     expected_columns = [
         CsvColumnNames.metering_point_id,
         CsvColumnNames.metering_point_type,
@@ -86,9 +82,7 @@ def test_execute_quarterly_time_series__when_grid_access_provider__returns_expec
     ] + [f"ENERGYQUANTITY{i}" for i in range(1, 101)]
 
     # Act
-    execute_quarterly_time_series(
-        spark, dbutils, standard_wholesale_fixing_scenario_args
-    )
+    execute_quarterly_time_series(spark, dbutils, args)
 
     # Assert
     actual_files = dbutils.jobs.taskValues.get("quarterly_time_series_files")
@@ -112,11 +106,12 @@ def test_execute_quarterly_time_series__when_system_operator_or_datahub_admin_wi
     market_role: MarketRole,
 ):
     # Arrange
-    standard_wholesale_fixing_scenario_args.requesting_actor_market_role = market_role
+    args = copy.deepcopy(standard_wholesale_fixing_scenario_args)
+    args.requesting_actor_market_role = market_role
     energy_supplier_id = (
         standard_wholesale_fixing_scenario_data_generator.ENERGY_SUPPLIER_IDS[0]
     )
-    standard_wholesale_fixing_scenario_args.energy_supplier_ids = [energy_supplier_id]
+    args.energy_supplier_ids = [energy_supplier_id]
     expected_file_names = [
         f"TSSD15_804_{energy_supplier_id}_02-01-2024_02-01-2024.csv",
         f"TSSD15_805_{energy_supplier_id}_02-01-2024_02-01-2024.csv",
@@ -128,9 +123,7 @@ def test_execute_quarterly_time_series__when_system_operator_or_datahub_admin_wi
     ] + [f"ENERGYQUANTITY{i}" for i in range(1, 101)]
 
     # Act
-    execute_quarterly_time_series(
-        spark, dbutils, standard_wholesale_fixing_scenario_args
-    )
+    execute_quarterly_time_series(spark, dbutils, args)
 
     # Assert
     actual_files = dbutils.jobs.taskValues.get("quarterly_time_series_files")
@@ -154,8 +147,9 @@ def test_execute_quarterly_time_series__when_system_operator_or_datahub_admin_wi
     market_role: MarketRole,
 ):
     # Arrange
-    standard_wholesale_fixing_scenario_args.requesting_actor_market_role = market_role
-    standard_wholesale_fixing_scenario_args.energy_supplier_ids = None
+    args = copy.deepcopy(standard_wholesale_fixing_scenario_args)
+    args.requesting_actor_market_role = market_role
+    args.energy_supplier_ids = None
     expected_file_names = [
         "TSSD15_804_02-01-2024_02-01-2024.csv",
         "TSSD15_805_02-01-2024_02-01-2024.csv",
@@ -168,9 +162,7 @@ def test_execute_quarterly_time_series__when_system_operator_or_datahub_admin_wi
     ] + [f"ENERGYQUANTITY{i}" for i in range(1, 101)]
 
     # Act
-    execute_quarterly_time_series(
-        spark, dbutils, standard_wholesale_fixing_scenario_args
-    )
+    execute_quarterly_time_series(spark, dbutils, args)
 
     # Assert
     actual_files = dbutils.jobs.taskValues.get("quarterly_time_series_files")
@@ -189,12 +181,11 @@ def test_execute_quarterly_time_series__when_include_basis_data_false__returns_n
     standard_wholesale_fixing_scenario_data_written_to_delta: None,
 ):
     # Arrange
-    standard_wholesale_fixing_scenario_args.include_basis_data = False
+    args = copy.deepcopy(standard_wholesale_fixing_scenario_args)
+    args.include_basis_data = False
 
     # Act
-    execute_quarterly_time_series(
-        spark, dbutils, standard_wholesale_fixing_scenario_args
-    )
+    execute_quarterly_time_series(spark, dbutils, args)
 
     # Assert
     actual_files = dbutils.jobs.taskValues.get("quarterly_time_series_files")
@@ -208,14 +199,13 @@ def test_execute_quarterly_time_series__when_energy_supplier_and_balance_fixing_
     standard_balance_fixing_scenario_data_written_to_delta: None,
 ):
     # Arrange
-    standard_balance_fixing_scenario_args.requesting_actor_market_role = (
-        MarketRole.ENERGY_SUPPLIER
-    )
+    args = copy.deepcopy(standard_balance_fixing_scenario_args)
+    args.requesting_actor_market_role = MarketRole.ENERGY_SUPPLIER
     energy_supplier_id = (
         standard_balance_fixing_scenario_data_generator.ENERGY_SUPPLIER_IDS[0]
     )
-    standard_balance_fixing_scenario_args.requesting_actor_id = energy_supplier_id
-    standard_balance_fixing_scenario_args.energy_supplier_ids = [energy_supplier_id]
+    args.requesting_actor_id = energy_supplier_id
+    args.energy_supplier_ids = [energy_supplier_id]
     expected_file_names = [
         f"TSSD15_804_{energy_supplier_id}_DDQ_02-01-2024_02-01-2024.csv",
         f"TSSD15_805_{energy_supplier_id}_DDQ_02-01-2024_02-01-2024.csv",
@@ -227,7 +217,7 @@ def test_execute_quarterly_time_series__when_energy_supplier_and_balance_fixing_
     ] + [f"ENERGYQUANTITY{i}" for i in range(1, 101)]
 
     # Act
-    execute_quarterly_time_series(spark, dbutils, standard_balance_fixing_scenario_args)
+    execute_quarterly_time_series(spark, dbutils, args)
 
     # Assert
     actual_files = dbutils.jobs.taskValues.get(key="quarterly_time_series_files")
