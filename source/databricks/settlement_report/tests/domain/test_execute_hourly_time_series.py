f758fc5385f53cdd97e0bf459042b0255f48317d
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-18 09:20:15 +0200

    ...

--- source/databricks/settlement_report/tests/domain/test_execute_hourly_time_series.py
+++ source/databricks/settlement_report/tests/domain/test_execute_hourly_time_series.py
@@ -1,3 +1,4 @@
+import pytest
 from pyspark.sql import SparkSession
 
 from tests.fixtures import DBUtilsFixture
@@ -7,12 +8,16 @@ from settlement_report_job.domain.csv_column_names import (
     CsvColumnNames,
 )
 
+# NOTE: The tests in test_execute_quarterly_time_series.py should cover execute_hourly also, so we don't need to test
+# all the same things again here also.
 
+
+@pytest.fixture(scope="function", autouse=True)
 def reset_task_values(dbutils: DBUtilsFixture):
-    try:
-        dbutils.jobs.taskValues.set("hourly_time_series_files", [])
-    except Exception:
-        pass
+    print("Resetting task values before test")
+    yield
+    print("Resetting task values")
+    dbutils.jobs.taskValues.reset()
 
 
 def test_execute_hourly_time_series__when_standard_wholesale_fixing_scenario__returns_expected_number_of_files_and_content(
@@ -21,27 +26,26 @@ def test_execute_hourly_time_series__when_standard_wholesale_fixing_scenario__re
     standard_wholesale_fixing_scenario_args: SettlementReportArgs,
     standard_wholesale_fixing_scenario_data_written_to_delta: None,
 ):
-    try:
-        # Arrange
-        expected_file_count = 2  # corresponding to the number of grid areas in standard_wholesale_fixing_scenario
-        expected_columns = [
-            CsvColumnNames.energy_supplier_id,
-            CsvColumnNames.metering_point_id,
-            CsvColumnNames.metering_point_type,
-            CsvColumnNames.start_of_day,
-        ] + [f"ENERGYQUANTITY{i}" for i in range(1, 26)]
+    # Arrange
+    expected_file_names = [
+        f"TSSD60_804__02-01-2024_02-01-2024.csv",
+        f"TSSD60_805__02-01-2024_02-01-2024.csv",
+    ]
+    expected_columns = [
+        CsvColumnNames.energy_supplier_id,
+        CsvColumnNames.metering_point_id,
+        CsvColumnNames.metering_point_type,
+        CsvColumnNames.start_of_day,
+    ] + [f"ENERGYQUANTITY{i}" for i in range(1, 26)]
 
-        # Act
-        execute_hourly_time_series(
-            spark, dbutils, standard_wholesale_fixing_scenario_args
-        )
+    # Act
+    execute_hourly_time_series(spark, dbutils, standard_wholesale_fixing_scenario_args)
 
-        # Assert
-        actual_files = dbutils.jobs.taskValues.get("hourly_time_series_files")
-        assert len(actual_files) == expected_file_count
-        for file_path in actual_files:
-            df = spark.read.option("delimiter", ";").csv(file_path, header=True)
-            assert df.count() > 0
-            assert df.columns == expected_columns
-    finally:
-        reset_task_values(dbutils)
+    # Assert
+    actual_files = dbutils.jobs.taskValues.get("hourly_time_series_files")
+    assert len(actual_files) == len(expected_file_names)
+    for file_path in actual_files:
+        df = spark.read.option("delimiter", ";").csv(file_path, header=True)
+        assert df.count() > 0
+        assert df.columns == expected_columns
+        assert any(file_name in file_path for file_name in expected_file_names)
