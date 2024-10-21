535fc2c4a05e0dde7837dc46cbc5418c38594828
Jonas Michelsen <jvm@energinet.dk>
2024-10-21 15:39:05 +0200

    Update test_read_and_filter.py

--- source/databricks/settlement_report/tests/domain/wholesale_results/test_read_and_filter.py
+++ source/databricks/settlement_report/tests/domain/wholesale_results/test_read_and_filter.py
@@ -1,2 +1,56 @@
-def test_read_and_filter():
-    pass
+import uuid
+from datetime import datetime
+from unittest.mock import Mock
+
+import pytest
+from pyspark.sql import SparkSession
+
+import test_factories.default_test_data_spec as default_data
+from settlement_report_job.domain.wholesale_results.read_and_filter import (
+    read_and_filter_from_view,
+)
+from test_factories.default_test_data_spec import create_wholesale_data_spec
+from test_factories.wholesale_factory import create_wholesale
+
+
+DEFAULT_FROM_DATE = default_data.DEFAULT_FROM_DATE
+DEFAULT_TO_DATE = default_data.DEFAULT_TO_DATE
+DATAHUB_ADMINISTRATOR_ID = "1234567890123"
+SYSTEM_OPERATOR_ID = "3333333333333"
+NOT_SYSTEM_OPERATOR_ID = "4444444444444"
+DEFAULT_TIME_ZONE = "Europe/Copenhagen"
+ENERGY_SUPPLIER_IDS = ["1234567890123", "2345678901234"]
+
+
+@pytest.mark.parametrize(
+    "args_start_date, args_end_date, expected_rows",
+    [
+        (datetime(2024, 1, 2, 23), datetime(2024, 1, 10, 23), 1),
+        (datetime(2024, 1, 5, 23), datetime(2024, 1, 10, 23), 0),
+    ],
+)
+def test_time(
+    spark: SparkSession, args_start_date, args_end_date, expected_rows
+) -> None:
+    # Arrange
+    time = datetime(2024, 1, 3, 23)
+
+    df = create_wholesale(spark, create_wholesale_data_spec(time=time))
+    mock_repository = Mock()
+    mock_repository.read_wholesale_results.return_value = df
+
+    # Act
+    actual = read_and_filter_from_view(
+        energy_supplier_ids=ENERGY_SUPPLIER_IDS,
+        calculation_id_by_grid_area={
+            default_data.DEFAULT_GRID_AREA_CODE: uuid.UUID(
+                default_data.DEFAULT_CALCULATION_ID
+            )
+        },
+        period_start=args_start_date,
+        period_end=args_end_date,
+        repository=mock_repository,
+    )
+
+    # Assert
+    assert actual.count() == expected_rows
