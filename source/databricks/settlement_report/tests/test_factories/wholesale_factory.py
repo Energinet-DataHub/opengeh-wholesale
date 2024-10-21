8e16db4ece51eb54d251dabf8f63705f3564d976
Jonas Michelsen <jvm@energinet.dk>
2024-10-21 08:57:24 +0200

    Update wholesale_factory.py

--- source/databricks/settlement_report/tests/test_factories/wholesale_factory.py
+++ source/databricks/settlement_report/tests/test_factories/wholesale_factory.py
@@ -41,33 +41,28 @@ class WholesaleTestDataSpec:
 
 
 def _get_base_wholesale_rows_from_spec(data_spec: WholesaleTestDataSpec):
-    rows = []
-    resolution = (
-        timedelta(hours=1) if data_spec.resolution == "PT1H" else timedelta(minutes=15)
-    )
-    current_time = data_spec.from_date
-    while current_time < data_spec.to_date:
-        rows.append(
-            {
-                DataProductColumnNames.calculation_id: data_spec.calculation_id,
-                DataProductColumnNames.calculation_type: data_spec.calculation_type,
-                DataProductColumnNames.calculation_period_start: data_spec.calculation_period_start,
-                DataProductColumnNames.calculation_period_end: data_spec.calculation_period_end,
-                DataProductColumnNames.calculation_version: data_spec.calculation_version,
-                DataProductColumnNames.result_id: data_spec.result_id,
-                DataProductColumnNames.grid_area_code: data_spec.grid_area_code,
-                DataProductColumnNames.energy_supplier_id: data_spec.energy_supplier_id,
-                DataProductColumnNames.balance_responsible_party_id: data_spec.balance_responsible_party_id,
-                DataProductColumnNames.metering_point_type: data_spec.metering_point_type,
-                DataProductColumnNames.settlement_method: data_spec.settlement_method,
-                DataProductColumnNames.resolution: data_spec.resolution,
-                DataProductColumnNames.time: current_time,
-                DataProductColumnNames.quantity: data_spec.quantity,
-                DataProductColumnNames.quantity_unit: data_spec.quantity_unit,
-                DataProductColumnNames.quantity_qualities: data_spec.quantity_qualities,
-            }
-        )
-        current_time += resolution
+    rows = [{
+        DataProductColumnNames.calculation_id: data_spec.calculation_id,
+        DataProductColumnNames.calculation_type: data_spec.calculation_type,
+        DataProductColumnNames.calculation_version: data_spec.calculation_version,
+        DataProductColumnNames.result_id: data_spec.result_id,
+        DataProductColumnNames.grid_area_code: data_spec.grid_area_code,
+        DataProductColumnNames.energy_supplier_id: data_spec.energy_supplier_id,
+        DataProductColumnNames.charge_code: data_spec.charge_code,
+        DataProductColumnNames.charge_type: data_spec.charge_type,
+        DataProductColumnNames.charge_owner_id: data_spec.charge_owner_id,
+        DataProductColumnNames.resolution: data_spec.resolution,
+        DataProductColumnNames.quantity_unit: data_spec.quantity_unit,
+        DataProductColumnNames.metering_point_type: data_spec.metering_point_type,
+        DataProductColumnNames.settlement_method: data_spec.settlement_method,
+        DataProductColumnNames.is_tax: data_spec.is_tax,
+        DataProductColumnNames.currency: data_spec.currency,
+        DataProductColumnNames.time: data_spec.time,
+        DataProductColumnNames.quantity: data_spec.quantity,
+        DataProductColumnNames.quantity_qualities: data_spec.quantity_qualities,
+        DataProductColumnNames.price: data_spec.price,
+        DataProductColumnNames.amount: data_spec.amount,
+    }]
 
     return rows
 
