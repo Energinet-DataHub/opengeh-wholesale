200cdc93e432e1453748206cd7d1bc22e2fec934
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-22 22:23:26 +0200

    ...

--- source/databricks/settlement_report/settlement_report_job/domain/system_operator_filter.py
+++ source/databricks/settlement_report/settlement_report_job/domain/system_operator_filter.py
@@ -3,6 +3,32 @@ from pyspark.sql import DataFrame, functions as F
 from settlement_report_job.wholesale.column_names import DataProductColumnNames
 
 
+def filter_charge_links_on_charge_owner_and_tax_value(
+    charge_link_periods: DataFrame,
+    charge_price_information_periods: DataFrame,
+    charge_owner_id: str,
+    is_tax: bool,
+) -> DataFrame:
+
+    charge_price_information_periods = charge_price_information_periods.where(
+        (F.col(DataProductColumnNames.is_tax) == is_tax)
+        & (F.col(DataProductColumnNames.charge_owner_id) == charge_owner_id)
+    )
+
+    charge_link_periods = charge_link_periods.where
+
+    return charge_link_periods.join(
+        charge_price_information_periods,
+        on=[DataProductColumnNames.calculation_id, DataProductColumnNames.charge_key],
+        how="inner",
+    ).select(
+        charge_link_periods[DataProductColumnNames.calculation_id],
+        charge_link_periods[DataProductColumnNames.metering_point_id],
+        charge_link_periods[DataProductColumnNames.from_date],
+        charge_link_periods[DataProductColumnNames.to_date],
+    )
+
+
 def filter_time_series_on_charge_owner(
     time_series: DataFrame,
     system_operator_id: str,
@@ -13,20 +39,11 @@ def filter_time_series_on_charge_owner(
     Filters away all time series data that is not related to the system operator, and which is not a tax.
     """
 
-    charge_price_information_periods = charge_price_information_periods.where(
-        (~F.col(DataProductColumnNames.is_tax))
-        & (F.col(DataProductColumnNames.charge_owner_id) == system_operator_id)
-    )
-
-    filtered_charge_link_periods = charge_link_periods.join(
+    filtered_charge_link_periods = filter_charge_links_on_charge_owner_and_tax_value(
+        charge_link_periods,
         charge_price_information_periods,
-        on=[DataProductColumnNames.calculation_id, DataProductColumnNames.charge_key],
-        how="inner",
-    ).select(
-        charge_link_periods[DataProductColumnNames.calculation_id],
-        charge_link_periods[DataProductColumnNames.metering_point_id],
-        charge_link_periods[DataProductColumnNames.from_date],
-        charge_link_periods[DataProductColumnNames.to_date],
+        system_operator_id,
+        is_tax=False,
     )
 
     filtered_df = time_series.join(
