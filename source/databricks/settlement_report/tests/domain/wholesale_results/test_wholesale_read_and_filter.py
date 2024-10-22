fb5903c0c4ac06f9a3db45f74b38fdf3e5dc88f6
Jonas Michelsen <jvm@energinet.dk>
2024-10-22 11:20:02 +0200

    Update test_wholesale_read_and_filter.py

--- source/databricks/settlement_report/tests/domain/wholesale_results/test_wholesale_read_and_filter.py
+++ source/databricks/settlement_report/tests/domain/wholesale_results/test_wholesale_read_and_filter.py
@@ -25,12 +25,25 @@ ENERGY_SUPPLIER_IDS = ["1234567890123", "2345678901234"]
 @pytest.mark.parametrize(
     "args_start_date, args_end_date, expected_rows",
     [
-        (datetime(2024, 1, 2, 23), datetime(2024, 1, 10, 23), 1),
-        (datetime(2024, 1, 5, 23), datetime(2024, 1, 10, 23), 0),
+        pytest.param(
+            datetime(2024, 1, 2, 23),
+            datetime(2024, 1, 10, 23),
+            1,
+            id="when time is within the range, return 1 row",
+        ),
+        pytest.param(
+            datetime(2024, 1, 5, 23),
+            datetime(2024, 1, 10, 23),
+            0,
+            id="when time is outside the range, return 0 rows",
+        ),
     ],
 )
-def test_time(
-    spark: SparkSession, args_start_date, args_end_date, expected_rows
+def test_time_within_and_outside_of_date_range(
+    spark: SparkSession,
+    args_start_date: datetime,
+    args_end_date: datetime,
+    expected_rows: int,
 ) -> None:
     # Arrange
     time = datetime(2024, 1, 3, 23)
@@ -52,5 +65,61 @@ def test_time(
         repository=mock_repository,
     )
 
+    # Assert
+    assert actual.count() == expected_rows
+
+
+@pytest.mark.parametrize(
+    "energy_supplier_ids, energy_supplier_id, expected_rows",
+    [
+        pytest.param(
+            ["1234567890123"], "1234567890123", 1, id="time is within the range"
+        ),
+        pytest.param(
+            ["1234567890123"],
+            None,
+            0,
+            id="time is outside the range (None energy_supplier_id)",
+        ),
+        pytest.param(
+            ["2345678901234"],
+            "1234567890123",
+            0,
+            id="time is outside the range (different energy_supplier_id)",
+        ),
+        pytest.param(
+            None,
+            "1234567890123",
+            0,
+            id="time is outside the range (None energy_supplier_ids)",
+        ),
+    ],
+)
+def test_when_time_is_within_and_outside_of_date_range(
+    spark: SparkSession,
+    energy_supplier_ids: list[str] | None,
+    energy_supplier_id: str,
+    expected_rows: int,
+) -> None:
+    # Arrange
+    df = create_wholesale(
+        spark, create_wholesale_data_spec(energy_supplier_id=energy_supplier_id)
+    )
+    mock_repository = Mock()
+    mock_repository.read_wholesale_results.return_value = df
+
+    # Act
+    actual = read_and_filter_from_view(
+        energy_supplier_ids=energy_supplier_ids,
+        calculation_id_by_grid_area={
+            default_data.DEFAULT_GRID_AREA_CODE: uuid.UUID(
+                default_data.DEFAULT_CALCULATION_ID
+            )
+        },
+        period_start=default_data.DEFAULT_FROM_DATE,
+        period_end=default_data.DEFAULT_TO_DATE,
+        repository=mock_repository,
+    )
+    actual.show()
     # Assert
     assert actual.count() == expected_rows
