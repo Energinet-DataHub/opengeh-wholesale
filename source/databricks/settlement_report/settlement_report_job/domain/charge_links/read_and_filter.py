2988a3ee77aa2ed6e5765820626e865312f99d07
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-24 15:40:48 +0200

    ...

--- source/databricks/settlement_report/settlement_report_job/domain/charge_links/read_and_filter.py
+++ source/databricks/settlement_report/settlement_report_job/domain/charge_links/read_and_filter.py
@@ -14,9 +14,12 @@
 from datetime import datetime
 from uuid import UUID
 
-from pyspark.sql import DataFrame, functions as F, Column, Window
+from pyspark.sql import DataFrame, functions as F
 
 from settlement_report_job import logging
+from settlement_report_job.domain.dataframe_utils.factory_filters import (
+    filter_by_calculation_id_by_grid_area,
+)
 from settlement_report_job.domain.market_role import MarketRole
 from settlement_report_job.domain.repository import WholesaleRepository
 from settlement_report_job.wholesale.column_names import DataProductColumnNames
@@ -39,6 +42,7 @@ def read_and_filter(
     metering_point_periods = _read_metering_point_periods(
         period_start=period_start,
         period_end=period_end,
+        calculation_id_by_grid_area=calculation_id_by_grid_area,
         energy_supplier_ids=energy_supplier_ids,
         repository=repository,
     )
@@ -50,13 +54,78 @@ def read_and_filter(
         repository=repository,
     )
 
-    metering_point_periods = metering_point_periods.where(
-        _filter_on_calculation_id_by_grid_area(calculation_id_by_grid_area)
+    charge_link_periods = _join_charge_link_and_metering_point_periods(
+        charge_link_periods=charge_link_periods,
+        metering_point_periods=metering_point_periods,
     )
 
-    metering_point_periods.show()
-    charge_link_periods.show()
+    return charge_link_periods
 
+
+def _read_metering_point_periods(
+    period_start: datetime,
+    period_end: datetime,
+    energy_supplier_ids: list[str] | None,
+    calculation_id_by_grid_area: dict[str, UUID],
+    repository: WholesaleRepository,
+) -> DataFrame:
+
+    metering_point_periods = repository.read_metering_point_periods().where(
+        (F.col(DataProductColumnNames.from_date) < period_end)
+        & (F.col(DataProductColumnNames.to_date) > period_start)
+    )
+
+    metering_point_periods = metering_point_periods.where(
+        filter_by_calculation_id_by_grid_area(calculation_id_by_grid_area)
+    )
+
+    if energy_supplier_ids is not None:
+        metering_point_periods = metering_point_periods.where(
+            F.col(DataProductColumnNames.energy_supplier_id).isin(energy_supplier_ids)
+        )
+
+    return metering_point_periods
+
+
+def _read_charge_link_periods(
+    period_start: datetime,
+    period_end: datetime,
+    requesting_actor_id: str,
+    requesting_actor_market_role: MarketRole,
+    repository: WholesaleRepository,
+) -> DataFrame:
+    charge_link_periods = repository.read_charge_link_periods().where(
+        (F.col(DataProductColumnNames.from_date) < period_end)
+        & (F.col(DataProductColumnNames.to_date) > period_start)
+    )
+
+    if requesting_actor_market_role in [
+        MarketRole.SYSTEM_OPERATOR,
+        MarketRole.GRID_ACCESS_PROVIDER,
+    ]:
+        charge_price_information_periods = (
+            repository.read_charge_price_information_periods()
+        )
+
+        is_tax = (
+            False
+            if requesting_actor_market_role == MarketRole.SYSTEM_OPERATOR
+            else True
+        )
+
+        charge_link_periods = _filter_on_charge_owner_and_tax(
+            charge_link_periods=charge_link_periods,
+            charge_price_information_periods=charge_price_information_periods,
+            charge_owner_id=requesting_actor_id,
+            is_tax=is_tax,
+        )
+
+    return charge_link_periods
+
+
+def _join_charge_link_and_metering_point_periods(
+    charge_link_periods: DataFrame, metering_point_periods: DataFrame
+) -> DataFrame:
     link_from_date = "link_from_date"
     link_to_date = "link_to_date"
     metering_point_from_date = "metering_point_from_date"
@@ -98,82 +167,12 @@ def read_and_filter(
             DataProductColumnNames.grid_area_code,
             DataProductColumnNames.energy_supplier_id,
         )
-        .distinct()
     )
 
     return charge_link_periods
 
 
-def _read_metering_point_periods(
-    period_start: datetime,
-    period_end: datetime,
-    energy_supplier_ids: list[str] | None,
-    repository: WholesaleRepository,
-) -> DataFrame:
-
-    metering_point_periods = repository.read_metering_point_periods().where(
-        (F.col(DataProductColumnNames.from_date) < period_end)
-        & (F.col(DataProductColumnNames.to_date) > period_start)
-    )
-    if energy_supplier_ids is not None:
-        metering_point_periods = metering_point_periods.where(
-            F.col(DataProductColumnNames.energy_supplier_id).isin(energy_supplier_ids)
-        )
-
-    return metering_point_periods
-
-
-def _read_charge_link_periods(
-    period_start: datetime,
-    period_end: datetime,
-    requesting_actor_id: str,
-    requesting_actor_market_role: MarketRole,
-    repository: WholesaleRepository,
-) -> DataFrame:
-    charge_link_periods = repository.read_charge_link_periods().where(
-        (F.col(DataProductColumnNames.from_date) < period_end)
-        & (F.col(DataProductColumnNames.to_date) > period_start)
-    )
-
-    if requesting_actor_market_role in [
-        MarketRole.SYSTEM_OPERATOR,
-        MarketRole.GRID_ACCESS_PROVIDER,
-    ]:
-        charge_price_information_periods = (
-            repository.read_charge_price_information_periods()
-        )
-
-        is_tax = (
-            False
-            if requesting_actor_market_role == MarketRole.SYSTEM_OPERATOR
-            else True
-        )
-
-        charge_link_periods = filter_on_charge_owner_and_tax(
-            charge_link_periods=charge_link_periods,
-            charge_price_information_periods=charge_price_information_periods,
-            charge_owner_id=requesting_actor_id,
-            is_tax=is_tax,
-        )
-
-    return charge_link_periods
-
-
-def _filter_on_calculation_id_by_grid_area(
-    calculation_id_by_grid_area: dict[str, UUID],
-) -> Column:
-    calculation_id_by_grid_area_structs = [
-        F.struct(F.lit(grid_area_code), F.lit(str(calculation_id)))
-        for grid_area_code, calculation_id in calculation_id_by_grid_area.items()
-    ]
-
-    return F.struct(
-        F.col(DataProductColumnNames.grid_area_code),
-        F.col(DataProductColumnNames.calculation_id),
-    ).isin(calculation_id_by_grid_area_structs)
-
-
-def filter_on_charge_owner_and_tax(
+def _filter_on_charge_owner_and_tax(
     charge_link_periods: DataFrame,
     charge_price_information_periods: DataFrame,
     charge_owner_id: str,
