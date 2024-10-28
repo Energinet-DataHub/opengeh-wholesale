eb94c3dd8df40f9d42b9ea7cf733446c488b3a13
René Nielsen <hej@byggemand-bob.dk>
2024-10-28 14:03:00 +0100

    archive: change import storage account (#2362)

--- archive/terraform/main/st-backup-import.tf
+++ archive/terraform/main/st-backup-import.tf
@@ -1,26 +1,7 @@
-module "st_backup_import" {
-  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/storage-account?ref=storage-account_6.2.0"
-
-  name                       = "import"
-  project_name               = var.domain_name_short
-  environment_short          = var.environment_short
-  environment_instance       = var.environment_instance
-  resource_group_name        = azurerm_resource_group.this.name
-  location                   = azurerm_resource_group.this.location
-  access_tier                = "Hot"
-  private_endpoint_subnet_id = data.azurerm_key_vault_secret.snet_private_endpoints_002_id.value
-  ip_rules                   = local.ip_restrictions_as_string
-  role_assignments = [
-    {
-      principal_id         = data.azurerm_client_config.this.object_id
-      role_definition_name = "Storage Blob Data Contributor"
-    }
-  ]
-  containers = [
-    {
-      name = "import"
-    },
-  ]
-
-  audit_storage_account = null
+resource "azurerm_storage_account" "st_backup_import" {
+  name                     = "stimp${local.resources_suffix_no_dash}"
+  resource_group_name       = azurerm_resource_group.this.name
+  location                  = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
