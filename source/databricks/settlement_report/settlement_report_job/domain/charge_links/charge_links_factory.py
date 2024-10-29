e9dcb6a5433a5d9a98c1ece37dda78b4fe31bab7
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-10-29 15:23:17 +0100

    ...

--- source/databricks/settlement_report/settlement_report_job/domain/charge_links/charge_links_factory.py
+++ source/databricks/settlement_report/settlement_report_job/domain/charge_links/charge_links_factory.py
@@ -28,7 +28,15 @@ def create_charge_links(
     args: SettlementReportArgs,
     repository: WholesaleRepository,
 ) -> DataFrame:
-    charge_link_periods = read_and_filter(args, repository)
+    charge_link_periods = read_and_filter(
+        args.period_start,
+        args.period_end,
+        args.calculation_id_by_grid_area,
+        args.energy_supplier_ids,
+        args.requesting_actor_market_role,
+        args.requesting_actor_id,
+        repository,
+    )
 
     return prepare_for_csv(
         charge_link_periods,
