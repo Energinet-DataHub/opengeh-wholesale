3656ffa5e56ab2ea9af725e9bb6adfde188b859b
Christian Hansen <120550591+cenh-halfspace@users.noreply.github.com>
2024-10-30 15:10:18 +0100

    Added Application Insights Connection String Settlement Report (#2366)
    * added application insights string
    
    * removed job
    
    * removed permissions

--- wholesale/terraform/main/dbwj-settlement-report.tf
+++ wholesale/terraform/main/dbwj-settlement-report.tf
@@ -1,129 +1,3 @@
-resource "databricks_job" "settlement_report_job_balancing" {
-  provider            = databricks.dbw
-  name                = "SettlementReportJob"
-  max_concurrent_runs = 40
-
-  job_cluster {
-    job_cluster_key = "hourly_time_series_cluster"
-
-    new_cluster {
-      spark_version  = local.spark_version
-      node_type_id   = "Standard_D8as_v4"
-      runtime_engine = "PHOTON"
-      autoscale {
-        min_workers = 1
-        max_workers = 10
-      }
-      spark_env_vars = {
-        "DATA_STORAGE_ACCOUNT_NAME" = data.azurerm_key_vault_secret.st_data_lake_name.value
-        "TIME_ZONE"                 = local.TIME_ZONE
-        "CATALOG_NAME"              = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
-      }
-    }
-  }
-
-  job_cluster {
-    job_cluster_key = "settlement_report_cluster"
-
-    new_cluster {
-      spark_version  = local.spark_version
-      node_type_id   = "Standard_D8as_v4"
-      runtime_engine = "PHOTON"
-      autoscale {
-        min_workers = 1
-        max_workers = 10
-      }
-
-      spark_env_vars = {
-        "DATA_STORAGE_ACCOUNT_NAME" = data.azurerm_key_vault_secret.st_data_lake_name.value
-        "TIME_ZONE"                 = local.TIME_ZONE
-        "CATALOG_NAME"              = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
-      }
-    }
-  }
-
-  task {
-    task_key        = "hourly_time_series"
-    max_retries     = 1
-    job_cluster_key = "hourly_time_series_cluster"
-
-    library {
-      whl = "/Workspace/Shared/PythonWheels/settlement_report/opengeh_settlement_report-1.0-py3-none-any.whl"
-    }
-
-    python_wheel_task {
-      package_name = "opengeh_settlement_report"
-      # The entry point is defined in setup.py
-      entry_point = "create_hourly_time_series"
-    }
-  }
-
-  task {
-    task_key        = "quarterly_time_series"
-    max_retries     = 1
-    job_cluster_key = "settlement_report_cluster"
-
-    library {
-      whl = "/Workspace/Shared/PythonWheels/settlement_report/opengeh_settlement_report-1.0-py3-none-any.whl"
-    }
-
-    python_wheel_task {
-      package_name = "opengeh_settlement_report"
-      # The entry point is defined in setup.py
-      entry_point = "create_quarterly_time_series"
-    }
-  }
-
-  task {
-    task_key        = "energy_results"
-    max_retries     = 1
-    job_cluster_key = "settlement_report_cluster"
-
-    library {
-      whl = "/Workspace/Shared/PythonWheels/settlement_report/opengeh_settlement_report-1.0-py3-none-any.whl"
-    }
-
-    python_wheel_task {
-      package_name = "opengeh_settlement_report"
-      # The entry point is defined in setup.py
-      entry_point = "create_energy_results"
-    }
-  }
-
-  task {
-    task_key        = "zip"
-    max_retries     = 1
-    job_cluster_key = "settlement_report_cluster"
-
-    library {
-      whl = "/Workspace/Shared/PythonWheels/settlement_report/opengeh_settlement_report-1.0-py3-none-any.whl"
-    }
-
-    python_wheel_task {
-      package_name = "opengeh_settlement_report"
-      # The entry point is defined in setup.py
-      entry_point = "create_zip"
-    }
-    depends_on {
-      task_key = "hourly_time_series"
-    }
-    depends_on {
-      task_key = "quarterly_time_series"
-    }
-    depends_on {
-      task_key = "energy_results"
-    }
-  }
-
-  email_notifications {
-    no_alert_for_skipped_runs = true
-  }
-
-  queue {
-    enabled = true
-  }
-}
-
 resource "databricks_job" "settlement_report_job_balance_fixing" {
   provider            = databricks.dbw
   name                = "SettlementReportBalanceFixing"
@@ -141,9 +15,10 @@ resource "databricks_job" "settlement_report_job_balance_fixing" {
         max_workers = 10
       }
       spark_env_vars = {
-        "DATA_STORAGE_ACCOUNT_NAME" = data.azurerm_key_vault_secret.st_data_lake_name.value
-        "TIME_ZONE"                 = local.TIME_ZONE
-        "CATALOG_NAME"              = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "DATA_STORAGE_ACCOUNT_NAME"             = data.azurerm_key_vault_secret.st_data_lake_name.value
+        "TIME_ZONE"                             = local.TIME_ZONE
+        "CATALOG_NAME"                          = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "APPLICATIONINSIGHTS_CONNECTION_STRING" = data.azurerm_key_vault_secret.appi_shared_connection_string.value
       }
     }
   }
@@ -160,9 +35,10 @@ resource "databricks_job" "settlement_report_job_balance_fixing" {
         max_workers = 10
       }
       spark_env_vars = {
-        "DATA_STORAGE_ACCOUNT_NAME" = data.azurerm_key_vault_secret.st_data_lake_name.value
-        "TIME_ZONE"                 = local.TIME_ZONE
-        "CATALOG_NAME"              = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "DATA_STORAGE_ACCOUNT_NAME"             = data.azurerm_key_vault_secret.st_data_lake_name.value
+        "TIME_ZONE"                             = local.TIME_ZONE
+        "CATALOG_NAME"                          = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "APPLICATIONINSIGHTS_CONNECTION_STRING" = data.azurerm_key_vault_secret.appi_shared_connection_string.value
       }
     }
   }
@@ -266,9 +142,10 @@ resource "databricks_job" "settlement_report_job_wholesale" {
         max_workers = 10
       }
       spark_env_vars = {
-        "DATA_STORAGE_ACCOUNT_NAME" = data.azurerm_key_vault_secret.st_data_lake_name.value
-        "TIME_ZONE"                 = local.TIME_ZONE
-        "CATALOG_NAME"              = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "DATA_STORAGE_ACCOUNT_NAME"             = data.azurerm_key_vault_secret.st_data_lake_name.value
+        "TIME_ZONE"                             = local.TIME_ZONE
+        "CATALOG_NAME"                          = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "APPLICATIONINSIGHTS_CONNECTION_STRING" = data.azurerm_key_vault_secret.appi_shared_connection_string.value
       }
     }
   }
@@ -286,9 +163,10 @@ resource "databricks_job" "settlement_report_job_wholesale" {
       }
 
       spark_env_vars = {
-        "DATA_STORAGE_ACCOUNT_NAME" = data.azurerm_key_vault_secret.st_data_lake_name.value
-        "TIME_ZONE"                 = local.TIME_ZONE
-        "CATALOG_NAME"              = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "DATA_STORAGE_ACCOUNT_NAME"             = data.azurerm_key_vault_secret.st_data_lake_name.value
+        "TIME_ZONE"                             = local.TIME_ZONE
+        "CATALOG_NAME"                          = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "APPLICATIONINSIGHTS_CONNECTION_STRING" = data.azurerm_key_vault_secret.appi_shared_connection_string.value
       }
     }
   }
