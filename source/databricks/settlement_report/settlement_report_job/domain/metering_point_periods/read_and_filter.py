7bf87bb77367ff30319ecba30fb58a4dd60cd9da
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-11-01 15:05:57 +0100

    ..

--- source/databricks/settlement_report/settlement_report_job/domain/metering_point_periods/read_and_filter.py
+++ source/databricks/settlement_report/settlement_report_job/domain/metering_point_periods/read_and_filter.py
@@ -17,6 +17,9 @@ from uuid import UUID
 from pyspark.sql import DataFrame, functions as F
 
 from settlement_report_job import logging
+from settlement_report_job.domain.dataframe_utils.join_metering_points_periods_and_charge_links_periods import (
+    join_metering_points_periods_and_charge_links_periods,
+)
 from settlement_report_job.domain.dataframe_utils.merge_periods import (
     merge_connected_periods,
 )
@@ -41,6 +44,8 @@ def read_and_filter_for_wholesale(
     requesting_actor_id: str,
     repository: WholesaleRepository,
 ) -> DataFrame:
+    select_columns = _get_select_columns(requesting_actor_market_role)
+
     metering_point_periods = read_metering_point_periods_by_calculation_ids(
         repository=repository,
         period_start=period_start,
@@ -59,7 +64,30 @@ def read_and_filter_for_wholesale(
             repository=repository,
         )
 
+    metering_point_periods = metering_point_periods.select(*select_columns)
+
+    metering_point_periods = merge_connected_periods(metering_point_periods)
+
     return metering_point_periods
+
+
+def _get_select_columns(requesting_actor_market_role: MarketRole) -> list[str]:
+    select_columns = [
+        DataProductColumnNames.metering_point_id,
+        DataProductColumnNames.from_date,
+        DataProductColumnNames.to_date,
+        DataProductColumnNames.grid_area_code,
+        DataProductColumnNames.from_grid_area_code,
+        DataProductColumnNames.to_grid_area_code,
+        DataProductColumnNames.metering_point_type,
+        DataProductColumnNames.settlement_method,
+    ]
+    if requesting_actor_market_role in [
+        MarketRole.SYSTEM_OPERATOR,
+        MarketRole.DATAHUB_ADMINISTRATOR,
+    ]:
+        select_columns.append(DataProductColumnNames.energy_supplier_id)
+    return select_columns
 
 
 def _filter_by_charge_owner(
@@ -77,61 +105,9 @@ def _filter_by_charge_owner(
         charge_owner_id=requesting_actor_id,
         requesting_actor_market_role=requesting_actor_market_role,
     )
-    metering_point_periods = _join_metering_point_periods_and_charge_link_periods(
+    metering_point_periods = join_metering_points_periods_and_charge_links_periods(
         charge_link_periods=charge_link_periods,
         metering_point_periods=metering_point_periods,
     )
+
     return metering_point_periods
-
-
-def _join_metering_point_periods_and_charge_link_periods(
-    charge_link_periods: DataFrame,
-    metering_point_periods: DataFrame,
-) -> DataFrame:
-    select_columns = metering_point_periods.columns
-
-    charge_link_periods = charge_link_periods.select(
-        DataProductColumnNames.calculation_id,
-        DataProductColumnNames.metering_point_id,
-        DataProductColumnNames.from_date,
-        DataProductColumnNames.to_date,
-    )
-
-    charge_link_periods = merge_connected_periods(charge_link_periods)
-
-    link_from_date = "link_from_date"
-    link_to_date = "link_to_date"
-    metering_point_from_date = "metering_point_from_date"
-    metering_point_to_date = "metering_point_to_date"
-
-    charge_link_periods = charge_link_periods.withColumnRenamed(
-        DataProductColumnNames.from_date, link_from_date
-    ).withColumnRenamed(DataProductColumnNames.to_date, link_to_date)
-    metering_point_periods = metering_point_periods.withColumnRenamed(
-        DataProductColumnNames.from_date, metering_point_from_date
-    ).withColumnRenamed(DataProductColumnNames.to_date, metering_point_to_date)
-
-    metering_point_periods = (
-        metering_point_periods.join(
-            charge_link_periods,
-            on=[
-                DataProductColumnNames.calculation_id,
-                DataProductColumnNames.metering_point_id,
-            ],
-            how="inner",
-        )
-        .where(
-            (F.col(link_from_date) < F.col(metering_point_to_date))
-            & (F.col(link_to_date) > F.col(metering_point_from_date))
-        )
-        .withColumn(
-            DataProductColumnNames.from_date,
-            F.greatest(F.col(link_from_date), F.col(metering_point_from_date)),
-        )
-        .withColumn(
-            DataProductColumnNames.to_date,
-            F.least(F.col(link_to_date), F.col(metering_point_to_date)),
-        )
-    )
-
-    return metering_point_periods.select(*select_columns)
