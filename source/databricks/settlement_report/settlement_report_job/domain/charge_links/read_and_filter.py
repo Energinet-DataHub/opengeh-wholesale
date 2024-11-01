b1aca46d395dec6cf2c617a05e3bce1523b70934
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-11-01 14:30:13 +0100

    ..

--- source/databricks/settlement_report/settlement_report_job/domain/charge_links/read_and_filter.py
+++ source/databricks/settlement_report/settlement_report_job/domain/charge_links/read_and_filter.py
@@ -17,6 +17,9 @@ from uuid import UUID
 from pyspark.sql import DataFrame, functions as F
 
 from settlement_report_job import logging
+from settlement_report_job.domain.dataframe_utils.join_metering_points_periods_and_charge_links_periods import (
+    join_metering_points_periods_and_charge_links_periods,
+)
 from settlement_report_job.domain.dataframe_utils.merge_periods import (
     merge_connected_periods,
 )
@@ -43,13 +46,6 @@ def read_and_filter_for_wholesale(
 ) -> DataFrame:
     logger.info("Creating charge links")
 
-    metering_point_periods = read_metering_point_periods_by_calculation_ids(
-        repository=repository,
-        period_start=period_start,
-        period_end=period_end,
-        calculation_id_by_grid_area=calculation_id_by_grid_area,
-        energy_supplier_ids=energy_supplier_ids,
-    )
     charge_link_periods = read_charge_link_periods(
         repository=repository,
         period_start=period_start,
@@ -58,68 +54,56 @@ def read_and_filter_for_wholesale(
         requesting_actor_market_role=requesting_actor_market_role,
     )
 
-    charge_link_periods = _join_charge_link_and_metering_point_periods(
-        charge_link_periods=charge_link_periods,
-        metering_point_periods=metering_point_periods,
-        requesting_actor_market_role=requesting_actor_market_role,
+    charge_link_periods = _add_additional_columns_from_metering_point_periods(
+        charge_link_periods,
+        period_start,
+        period_end,
+        calculation_id_by_grid_area,
+        energy_supplier_ids,
+        requesting_actor_market_role,
+        repository,
     )
 
     return charge_link_periods
 
 
-def _join_charge_link_and_metering_point_periods(
-    charge_link_periods: DataFrame,
-    metering_point_periods: DataFrame,
+def _add_additional_columns_from_metering_point_periods(
+    charge_link_periods,
+    period_start: datetime,
+    period_end: datetime,
+    calculation_id_by_grid_area: dict[str, UUID],
+    energy_supplier_ids: list[str] | None,
     requesting_actor_market_role: MarketRole,
-) -> DataFrame:
+    repository: WholesaleRepository,
+):
+    metering_point_periods = read_metering_point_periods_by_calculation_ids(
+        repository=repository,
+        period_start=period_start,
+        period_end=period_end,
+        calculation_id_by_grid_area=calculation_id_by_grid_area,
+        energy_supplier_ids=energy_supplier_ids,
+    )
+
     metering_point_periods = _merge_metering_point_periods(
         metering_point_periods, requesting_actor_market_role
     )
 
-    link_from_date = "link_from_date"
-    link_to_date = "link_to_date"
-    metering_point_from_date = "metering_point_from_date"
-    metering_point_to_date = "metering_point_to_date"
-
-    charge_link_periods = charge_link_periods.withColumnRenamed(
-        DataProductColumnNames.from_date, link_from_date
-    ).withColumnRenamed(DataProductColumnNames.to_date, link_to_date)
-    metering_point_periods = metering_point_periods.withColumnRenamed(
-        DataProductColumnNames.from_date, metering_point_from_date
-    ).withColumnRenamed(DataProductColumnNames.to_date, metering_point_to_date)
-
-    charge_link_periods = (
-        charge_link_periods.join(
-            metering_point_periods,
-            on=[
-                DataProductColumnNames.calculation_id,
-                DataProductColumnNames.metering_point_id,
-            ],
-            how="inner",
-        )
-        .where(
-            (F.col(link_from_date) < F.col(metering_point_to_date))
-            & (F.col(link_to_date) > F.col(metering_point_from_date))
-        )
-        .select(
-            DataProductColumnNames.metering_point_id,
-            DataProductColumnNames.metering_point_type,
-            DataProductColumnNames.charge_type,
-            DataProductColumnNames.charge_code,
-            DataProductColumnNames.charge_owner_id,
-            DataProductColumnNames.quantity,
-            F.greatest(F.col(link_from_date), F.col(metering_point_from_date)).alias(
-                DataProductColumnNames.from_date
-            ),
-            F.least(F.col(link_to_date), F.col(metering_point_to_date)).alias(
-                DataProductColumnNames.to_date
-            ),
-            DataProductColumnNames.grid_area_code,
-            DataProductColumnNames.energy_supplier_id,
-        )
+    charge_link_periods = join_metering_points_periods_and_charge_links_periods(
+        charge_link_periods, metering_point_periods
     )
 
-    return charge_link_periods
+    return charge_link_periods.select(
+        DataProductColumnNames.metering_point_id,
+        DataProductColumnNames.metering_point_type,
+        DataProductColumnNames.charge_type,
+        DataProductColumnNames.charge_code,
+        DataProductColumnNames.charge_owner_id,
+        DataProductColumnNames.quantity,
+        DataProductColumnNames.from_date,
+        DataProductColumnNames.to_date,
+        DataProductColumnNames.grid_area_code,
+        DataProductColumnNames.energy_supplier_id,
+    )
 
 
 def _merge_metering_point_periods(
