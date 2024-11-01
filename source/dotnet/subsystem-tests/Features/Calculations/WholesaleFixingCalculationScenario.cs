51b6e0e921d9aaf7e55f423faaffe6d808b27b86
Alexander <alexanderharrow@hotmail.com>
2024-11-01 19:58:27 +0100

    Updates

--- source/dotnet/subsystem-tests/Features/Calculations/WholesaleFixingCalculationScenario.cs
+++ source/dotnet/subsystem-tests/Features/Calculations/WholesaleFixingCalculationScenario.cs
@@ -153,8 +153,8 @@ AppTraces
     {
         var query = $@"
 AppDependencies
-| where Target == ""exchange""
-| where Name == ""exchange""
+| where Target == ""calculate_exchange""
+| where Name == ""calculate_exchange""
 | where DependencyType == ""InProc""
 | where Success == true
 | where ResultCode == 0
