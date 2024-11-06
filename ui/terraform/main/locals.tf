0d9ede4cfb78d55aa079e87d9fa0eb3eef0c77ae
Lars Nielsen <lanni@energinet.dk>
2024-11-06 13:35:31 +0100

    UI - Migrate to azurerm_static_web_app (#2410)

--- ui/terraform/main/locals.tf
+++ ui/terraform/main/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  frontend_url              = var.frontend_url != null ? var.frontend_url : azurerm_static_site.ui.default_host_name
+  frontend_url              = var.frontend_url != null ? var.frontend_url : azurerm_static_web_app.ui.default_host_name
   ip_restrictions_as_string = join(",", [for rule in var.ip_restrictions : "${rule.ip_address}"])
 
   tags = {
--- ui/terraform/main/ui-app.tf
+++ ui/terraform/main/ui-app.tf
@@ -1,4 +1,17 @@
-resource "azurerm_static_site" "ui" {
+removed {
+  from = azurerm_static_site.ui
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.this.name}/providers/Microsoft.Web/staticSites/stapp-ui-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
+  to = azurerm_static_web_app.ui
+}
+
+
+resource "azurerm_static_web_app" "ui" {
   name                = "stapp-ui-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
@@ -20,6 +33,6 @@ module "kvs_stapp_ui_web_app_api_key" {
   source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=key-vault-secret_6.0.0"
 
   name         = "stapp-ui-web-app-api-key"
-  value        = azurerm_static_site.ui.api_key
+  value        = azurerm_static_web_app.ui.api_key
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
