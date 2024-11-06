bc9154b3e603b3c8457ba534b5e05e396b611883
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-11-06 16:00:19 +0100

    ...

--- source/databricks/settlement_report/settlement_report_job/domain/report_name_factory.py
+++ source/databricks/settlement_report/settlement_report_job/domain/report_name_factory.py
@@ -101,6 +101,8 @@ class FileNameFactory:
             return "TSSD60"
         elif self.report_data_type == ReportDataType.TimeSeriesQuarterly:
             return "TSSD15"
+        elif self.report_data_type == ReportDataType.MeteringPointPeriods:
+            return "MDMP"
         elif self.report_data_type == ReportDataType.ChargeLinks:
             return "CHARGELINK"
         elif self.report_data_type == ReportDataType.EnergyResults:
