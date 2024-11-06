691d2fbd23f548ac0a15520cd08d675c6836c45c
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-11-06 15:07:07 +0100

    ...

--- source/databricks/settlement_report/tests/conftest.py
+++ source/databricks/settlement_report/tests/conftest.py
@@ -61,30 +61,8 @@ def cleanup_before_tests(
         shutil.rmtree(input_database_location)
 
     yield
+
     # Add cleanup code to be run after the tests
-
-
-@pytest.fixture(scope="function")
-def standard_balance_fixing_scenario_args(
-    settlement_reports_output_path: str,
-) -> SettlementReportArgs:
-    return SettlementReportArgs(
-        report_id=str(uuid.uuid4()),
-        period_start=standard_balance_fixing_scenario_data_generator.FROM_DATE,
-        period_end=standard_balance_fixing_scenario_data_generator.TO_DATE,
-        calculation_type=CalculationType.BALANCE_FIXING,
-        calculation_id_by_grid_area=None,
-        grid_area_codes=standard_balance_fixing_scenario_data_generator.GRID_AREAS,
-        split_report_by_grid_area=True,
-        prevent_large_text_files=False,
-        time_zone="Europe/Copenhagen",
-        catalog_name="spark_catalog",
-        energy_supplier_ids=None,
-        requesting_actor_market_role=MarketRole.SYSTEM_OPERATOR,
-        requesting_actor_id="1212121212121",
-        settlement_reports_output_path=settlement_reports_output_path,
-        include_basis_data=True,
-    )
 
 
 @pytest.fixture(scope="function")
