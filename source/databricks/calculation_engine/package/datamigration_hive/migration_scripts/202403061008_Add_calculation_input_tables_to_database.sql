ae4e7eaeb1ce4e072019deddb92c51152a3c63fd
Alexander <alexanderharrow@hotmail.com>
2024-11-06 21:26:36 +0100

    Update 202403061008_Add_calculation_input_tables_to_database.sql

--- source/databricks/calculation_engine/package/datamigration_hive/migration_scripts/202403061008_Add_calculation_input_tables_to_database.sql
+++ source/databricks/calculation_engine/package/datamigration_hive/migration_scripts/202403061008_Add_calculation_input_tables_to_database.sql
@@ -1,30 +1,24 @@
 -- Hack to make sure that the script is not executed when running integration tests
-CREATE
-{TEST} EXTERNAL
-TABLE if not exists {INPUT_DATABASE_NAME}.metering_point_periods
+CREATE TABLE if not exists {INPUT_DATABASE_NAME}.metering_point_periods
     USING DELTA
 GO
 
-CREATE
-{TEST} EXTERNAL
+CREATE EXTERNAL
 TABLE if not exists {INPUT_DATABASE_NAME}.time_series_points
     USING DELTA
 GO
 
-CREATE
-{TEST} EXTERNAL
+CREATE EXTERNAL
 TABLE if not exists {INPUT_DATABASE_NAME}.charge_link_periods
 USING DELTA
 GO
 
-CREATE
-{TEST} EXTERNAL
+CREATE EXTERNAL
 TABLE if not exists {INPUT_DATABASE_NAME}.charge_masterdata_periods
     USING DELTA
 GO
 
-CREATE
-{TEST} EXTERNAL
+CREATE EXTERNAL
 TABLE if not exists {INPUT_DATABASE_NAME}.charge_price_points
     USING DELTA
 GO
