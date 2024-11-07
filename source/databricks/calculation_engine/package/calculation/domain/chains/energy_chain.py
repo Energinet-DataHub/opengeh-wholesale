5e1065944d7d2d1322fc5e67eb1f14cf1c8caa5a
Alexander <alexanderharrow@hotmail.com>
2024-11-07 20:59:17 +0100

    Updates

---
+++ source/databricks/calculation_engine/package/calculation/domain/chains/energy_chain.py
@@ -0,0 +1,27 @@
+from package.calculation.calculation_output import CalculationOutput
+from package.calculation.domain.calculation_links import (
+    StartCalculationLink,
+    EndCalculationLink,
+    CacheMeteringPointPeriodsLink,
+    CalculateGridLossMeteringPointPeriodsLink,
+    CalculateTotalEnergyConsumptionLink,
+)
+
+
+class EnergyChain:
+
+    def __init__(
+        self,
+    ) -> None:
+
+        # Set up the calculation chain
+        self.start_link = StartCalculationLink()
+        (
+            self.start_link.set_next(CacheMeteringPointPeriodsLink())
+            .set_next(CalculateGridLossMeteringPointPeriodsLink())
+            .set_next(CalculateTotalEnergyConsumptionLink())
+            .set_next(EndCalculationLink())
+        )
+
+    def execute(self) -> CalculationOutput:
+        return self.start_link.execute(CalculationOutput())
