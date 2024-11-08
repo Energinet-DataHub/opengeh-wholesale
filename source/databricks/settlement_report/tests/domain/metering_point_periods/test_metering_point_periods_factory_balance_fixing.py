7eda6bf86f56f9a0cae79431ae53e8b91d2bd737
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-11-08 10:52:22 +0100

    ...

--- source/databricks/settlement_report/tests/domain/metering_point_periods/test_metering_point_periods_factory_balance_fixing.py
+++ source/databricks/settlement_report/tests/domain/metering_point_periods/test_metering_point_periods_factory_balance_fixing.py
@@ -1,20 +1,13 @@
-import uuid
 from unittest.mock import Mock
 
 from pyspark.sql import SparkSession, DataFrame
 import test_factories.default_test_data_spec as default_data
-import test_factories.charge_link_periods_factory as input_charge_links_factory
 import test_factories.metering_point_periods_factory as input_metering_point_periods_factory
-import test_factories.charge_price_information_periods_factory as input_charge_price_information_periods_factory
 from settlement_report_job.domain.csv_column_names import CsvColumnNames
 from settlement_report_job.domain.metering_point_periods.metering_point_periods_factory import (
     create_metering_point_periods,
 )
 from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
-from settlement_report_job.wholesale.data_values import (
-    MeteringPointTypeDataProductValue,
-    SettlementMethodDataProductValue,
-)
 from utils import Dates as d
 
 
@@ -69,6 +62,34 @@ def test_create_metering_point_periods__when_grid_access_provider__returns_expec
 
     # Assert
     assert actual.columns == expected_columns
+
+
+def test_create_metering_point_periods__when_and_metering_point_period_exceeds_selected_period__returns_period_that_ends_on_the_selected_end_date(
+    spark: SparkSession,
+    standard_balance_fixing_scenario_args: SettlementReportArgs,
+) -> None:
+    # Arrange
+    standard_balance_fixing_scenario_args.period_start = d.JAN_2ND
+    standard_balance_fixing_scenario_args.period_end = d.JAN_3RD
+
+    metering_point_periods = input_metering_point_periods_factory.create(
+        spark,
+        default_data.create_metering_point_periods_row(
+            from_date=d.JAN_1ST, to_date=d.JAN_4TH
+        ),
+    )
+    mock_repository = _get_repository_mock(metering_point_periods)
+
+    # Act
+    actual = create_metering_point_periods(
+        args=standard_balance_fixing_scenario_args,
+        repository=mock_repository,
+    )
+
+    # Assert
+    assert actual.count() == 1
+    assert actual.collect()[0][CsvColumnNames.metering_point_from_date] == d.JAN_2ND
+    assert actual.collect()[0][CsvColumnNames.metering_point_to_date] == d.JAN_3RD
 
 
 def test_create_metering_point_periods__when_and_metering_point_period_exceeds_selected_period__returns_period_that_ends_on_the_selected_end_date(
--- source/databricks/settlement_report/tests/domain/metering_point_periods/test_metering_point_periods_factory_wholesale.py
+++ source/databricks/settlement_report/tests/domain/metering_point_periods/test_metering_point_periods_factory_wholesale.py
@@ -6,7 +6,6 @@ import test_factories.default_test_data_spec as default_data
 import test_factories.charge_link_periods_factory as input_charge_links_factory
 import test_factories.metering_point_periods_factory as input_metering_point_periods_factory
 import test_factories.charge_price_information_periods_factory as input_charge_price_information_periods_factory
-from settlement_report_job.domain.csv_column_names import CsvColumnNames
 from settlement_report_job.domain.metering_point_periods.metering_point_periods_factory import (
     create_metering_point_periods,
 )
@@ -15,7 +14,6 @@ from settlement_report_job.wholesale.data_values import (
     MeteringPointTypeDataProductValue,
     SettlementMethodDataProductValue,
 )
-from utils import Dates as d
 
 
 def _get_repository_mock(
@@ -172,6 +170,8 @@ def test_create_metering_point_periods__when_grid_access_provider__returns_expec
         "VALIDFROM",
         "VALIDTO",
         "GRIDAREAID",
+        "TOGRIDAREAID",
+        "FROMGRIDAREAID",
         "TYPEOFMP",
         "SETTLEMENTMETHOD",
     ]
