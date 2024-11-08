3ef3fe23374d04f4e47d0e58961bb68e17fa9fbb
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-11-08 10:27:39 +0100

    ...

--- source/databricks/settlement_report/tests/domain/metering_point_periods/test_metering_point_periods_factory.py
+++ source/databricks/settlement_report/tests/domain/metering_point_periods/test_metering_point_periods_factory.py
@@ -6,6 +6,7 @@ import test_factories.default_test_data_spec as default_data
 import test_factories.charge_link_periods_factory as input_charge_links_factory
 import test_factories.metering_point_periods_factory as input_metering_point_periods_factory
 import test_factories.charge_price_information_periods_factory as input_charge_price_information_periods_factory
+from settlement_report_job.domain.csv_column_names import CsvColumnNames
 from settlement_report_job.domain.metering_point_periods.metering_point_periods_factory import (
     create_metering_point_periods,
 )
@@ -14,6 +15,7 @@ from settlement_report_job.wholesale.data_values import (
     MeteringPointTypeDataProductValue,
     SettlementMethodDataProductValue,
 )
+from utils import Dates as d
 
 
 def _get_repository_mock(
@@ -192,40 +194,7 @@ def test_create_metering_point_periods__when_wholesale_and_grid_access_provider_
 
 def test_create_metering_point_periods__when_balance_fixing_and_grid_access_provider__returns_expected_columns(
     spark: SparkSession,
-    standard_wholesale_fixing_scenario_energy_supplier_args: SettlementReportArgs,
-) -> None:
-
-    # Arrange
-    expected_columns = [
-        "grid_area_code_partitioning",
-        "METERINGPOINTID",
-        "VALIDFROM",
-        "VALIDTO",
-        "GRIDAREAID",
-        "TYPEOFMP",
-        "SETTLEMENTMETHOD",
-    ]
-
-    metering_point_periods = input_metering_point_periods_factory.create(
-        spark,
-        default_data.create_metering_point_periods_row(),
-    )
-    mock_repository = _get_repository_mock(metering_point_periods)
-
-    # Act
-    actual = create_metering_point_periods(
-        args=standard_wholesale_fixing_scenario_energy_supplier_args,
-        repository=mock_repository,
-    )
-
-    # Assert
-    assert actual.columns == expected_columns
-
-
-def test_create_metering_point_periods__when_balance_fixing_and_metering_point_period_exceeds_selected_period__returns_period_that_ends_on_the_selected_end_date(
-    spark: SparkSession,
     standard_balance_fixing_scenario_args: SettlementReportArgs,
-    standard_wholesale_fixing_scenario_grid_access_provider_args: SettlementReportArgs,
 ) -> None:
 
     # Arrange
@@ -235,8 +204,6 @@ def test_create_metering_point_periods__when_balance_fixing_and_metering_point_p
         "VALIDFROM",
         "VALIDTO",
         "GRIDAREAID",
-        "TOGRIDAREAID",
-        "FROMGRIDAREAID",
         "TYPEOFMP",
         "SETTLEMENTMETHOD",
     ]
@@ -255,3 +222,31 @@ def test_create_metering_point_periods__when_balance_fixing_and_metering_point_p
 
     # Assert
     assert actual.columns == expected_columns
+
+
+def test_create_metering_point_periods__when_balance_fixing_and_metering_point_period_exceeds_selected_period__returns_period_that_ends_on_the_selected_end_date(
+    spark: SparkSession,
+    standard_balance_fixing_scenario_args: SettlementReportArgs,
+) -> None:
+    # Arrange
+    standard_balance_fixing_scenario_args.period_start = d.JAN_2ND
+    standard_balance_fixing_scenario_args.period_end = d.JAN_3RD
+
+    metering_point_periods = input_metering_point_periods_factory.create(
+        spark,
+        default_data.create_metering_point_periods_row(
+            from_date=d.JAN_1ST, to_date=d.JAN_4TH
+        ),
+    )
+    mock_repository = _get_repository_mock(metering_point_periods)
+
+    # Act
+    actual = create_metering_point_periods(
+        args=standard_balance_fixing_scenario_args,
+        repository=mock_repository,
+    )
+
+    # Assert
+    assert actual.count() == 1
+    assert actual.collect()[0][CsvColumnNames.metering_point_from_date] == d.JAN_2ND
+    assert actual.collect()[0][CsvColumnNames.metering_point_to_date] == d.JAN_3RD
