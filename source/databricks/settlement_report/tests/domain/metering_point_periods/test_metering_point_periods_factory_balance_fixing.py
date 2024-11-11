c1d02642f0a6a3c9b7e124f7b3e3ee9aa9382e5d
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-11-11 14:45:44 +0100

    ...

--- source/databricks/settlement_report/tests/domain/metering_point_periods/test_metering_point_periods_factory_balance_fixing.py
+++ source/databricks/settlement_report/tests/domain/metering_point_periods/test_metering_point_periods_factory_balance_fixing.py
@@ -8,16 +8,19 @@ from settlement_report_job.domain.metering_point_periods.metering_point_periods_
     create_metering_point_periods,
 )
 from settlement_report_job.domain.settlement_report_args import SettlementReportArgs
+from test_factories import latest_calculations_factory
 from utils import Dates as d
 
 
 def _get_repository_mock(
     metering_point_period: DataFrame,
+    latest_calculations: DataFrame,
     charge_link_periods: DataFrame | None = None,
     charge_price_information_periods: DataFrame | None = None,
 ) -> Mock:
     mock_repository = Mock()
     mock_repository.read_metering_point_periods.return_value = metering_point_period
+    mock_repository.read_latest_calculations.return_value = latest_calculations
 
     if charge_link_periods:
         mock_repository.read_charge_link_periods.return_value = charge_link_periods
@@ -52,7 +55,10 @@ def test_create_metering_point_periods__when_grid_access_provider__returns_expec
         spark,
         default_data.create_metering_point_periods_row(),
     )
-    mock_repository = _get_repository_mock(metering_point_periods)
+    latest_calculations = latest_calculations_factory.create(
+        spark, default_data.create_latest_calculations_per_day_row()
+    )
+    mock_repository = _get_repository_mock(metering_point_periods, latest_calculations)
 
     # Act
     actual = create_metering_point_periods(
@@ -72,13 +78,21 @@ def test_create_metering_point_periods__when_and_metering_point_period_exceeds_s
     standard_balance_fixing_scenario_args.period_start = d.JAN_2ND
     standard_balance_fixing_scenario_args.period_end = d.JAN_3RD
 
+    latest_calculations = latest_calculations_factory.create(
+        spark,
+        [
+            default_data.create_latest_calculations_per_day_row(start_of_day=d.JAN_1ST),
+            default_data.create_latest_calculations_per_day_row(start_of_day=d.JAN_2ND),
+            default_data.create_latest_calculations_per_day_row(start_of_day=d.JAN_3RD),
+        ],
+    )
     metering_point_periods = input_metering_point_periods_factory.create(
         spark,
         default_data.create_metering_point_periods_row(
             from_date=d.JAN_1ST, to_date=d.JAN_4TH
         ),
     )
-    mock_repository = _get_repository_mock(metering_point_periods)
+    mock_repository = _get_repository_mock(metering_point_periods, latest_calculations)
 
     # Act
     actual = create_metering_point_periods(
