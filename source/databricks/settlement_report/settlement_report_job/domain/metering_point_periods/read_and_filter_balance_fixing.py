180d9920c92b194b41bc27122728f5dc4c237b45
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-11-11 13:37:08 +0100

    ...

--- source/databricks/settlement_report/settlement_report_job/domain/metering_point_periods/read_and_filter_balance_fixing.py
+++ source/databricks/settlement_report/settlement_report_job/domain/metering_point_periods/read_and_filter_balance_fixing.py
@@ -106,7 +106,7 @@ def _explode_into_daily_period(df: DataFrame, time_zone: str) -> DataFrame:
         F.to_utc_timestamp("local_daily_from_date", time_zone),
     ).withColumn(
         DataProductColumnNames.to_date,
-        F.to_utc_timestamp(F.date_add("local_daily_to_date", 1), time_zone),
+        F.to_utc_timestamp("local_daily_to_date", time_zone),
     )
 
     return df
