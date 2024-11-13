784844e170f1cd379a2c7797f5e64becf24a4b7b
Svend (SLB) <slb@halfspace.io>
2024-11-13 11:36:11 +0100

    Fixed missing negation, and unit test covering it

--- source/databricks/settlement_report/tests/domain/test_execute_monthly_amounts.py
+++ source/databricks/settlement_report/tests/domain/test_execute_monthly_amounts.py
@@ -11,6 +11,7 @@ from settlement_report_job.entry_points.job_args.settlement_report_args import (
 from settlement_report_job.domain.utils.csv_column_names import (
     CsvColumnNames,
 )
+from settlement_report_job.domain.utils.market_role import MarketRole
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -93,6 +94,62 @@ def test_execute_monthly_amounts__when_split_report_by_grid_area_is_false__retur
 
     expected_file_names = [
         f"RESULTMONTHLY_flere-net_{args.requesting_actor_id}_DDQ_02-01-2024_02-01-2024.csv",
+    ]
+
+    report_generator_instance = ReportGenerator(
+        spark, dbutils, standard_wholesale_fixing_scenario_energy_supplier_args
+    )
+
+    # Act
+    report_generator_instance.execute_monthly_amounts()
+
+    # Assert
+    actual_files = dbutils.jobs.taskValues.get("monthly_amounts_files")
+    assert_file_names_and_columns(
+        path=get_report_output_path(
+            standard_wholesale_fixing_scenario_energy_supplier_args
+        ),
+        actual_files=actual_files,
+        expected_columns=expected_columns,
+        expected_file_names=expected_file_names,
+        spark=spark,
+    )
+
+
+@pytest.mark.parametrize(
+    "requesting_market_role",
+    [MarketRole.GRID_ACCESS_PROVIDER, MarketRole.SYSTEM_OPERATOR],
+)
+def test_execute_monthly_amounts__when_grid_or_syo__returns_expected_number_of_files_and_content(
+    spark: SparkSession,
+    dbutils: DBUtilsFixture,
+    standard_wholesale_fixing_scenario_energy_supplier_args: SettlementReportArgs,
+    standard_wholesale_fixing_scenario_data_written_to_delta: None,
+    requesting_market_role: MarketRole,
+):
+    args = standard_wholesale_fixing_scenario_energy_supplier_args
+    args.split_report_by_grid_area = False
+    # Arrange
+    expected_columns = [
+        CsvColumnNames.calculation_type,
+        CsvColumnNames.correction_settlement_number,
+        CsvColumnNames.grid_area_code,
+        CsvColumnNames.energy_supplier_id,
+        CsvColumnNames.time,
+        CsvColumnNames.resolution,
+        CsvColumnNames.quantity_unit,
+        CsvColumnNames.currency,
+        CsvColumnNames.amount,
+        CsvColumnNames.charge_type,
+        CsvColumnNames.charge_code,
+    ]
+
+    file_name_id = (
+        "DDM_" if requesting_market_role == MarketRole.GRID_ACCESS_PROVIDER else "_"
+    )
+
+    expected_file_names = [
+        f"RESULTMONTHLY_flere-net_{args.requesting_actor_id}_{file_name_id}02-01-2024_02-01-2024.csv",
     ]
 
     report_generator_instance = ReportGenerator(
