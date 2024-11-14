651cd8de2cbb24e2b583903e0b4c192223c515b2
Mathias Ormstrup Bjerregaard <152613120+MWO1024@users.noreply.github.com>
2024-11-14 10:58:35 +0100

    Update source/dotnet/wholesale-api/Edi.UnitTests/Validators/AggregatedTimeSeriesRequest/AggregatedTimeSeriesRequestValidatorTests.cs
    Co-authored-by: Mads Magnus Due <xmadu@energinet.dk>

--- source/dotnet/wholesale-api/Edi.UnitTests/Validators/AggregatedTimeSeriesRequest/AggregatedTimeSeriesRequestValidatorTests.cs
+++ source/dotnet/wholesale-api/Edi.UnitTests/Validators/AggregatedTimeSeriesRequest/AggregatedTimeSeriesRequestValidatorTests.cs
@@ -135,7 +135,7 @@ public class AggregatedTimeSeriesRequestValidatorTests
         Validate_WhenPeriodOverlapsCutOffAt3AndAHalfYearBackInTime_ReturnsSuccessfulValidation()
     {
         // Prerequisite: The current time is NOT the start of a month.
-        _clockMock.Object.GetCurrentInstant().ToDateTimeUtc().Date.Should().NotHaveDay(1).And.NotHaveDay(2);
+        _clockMock.Setup(c => c.GetCurrentInstant()).Returns(Instant.FromUtc(2024, 11, 15, 16, 46, 43));
 
         // Arrange
         var periodStart = _clockMock.Object.GetCurrentInstant() // Assuming 2024-11-15 16:46:43 UTC
