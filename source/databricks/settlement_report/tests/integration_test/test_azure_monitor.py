c8e9679c738ca71564350eb66d1401fa13482aab
Christian Hansen <cenh@halfspace.io>
2024-11-14 16:42:03 +0100

    test

--- source/databricks/settlement_report/tests/integration_test/test_azure_monitor.py
+++ source/databricks/settlement_report/tests/integration_test/test_azure_monitor.py
@@ -28,7 +28,7 @@ from settlement_report_job.entry_points.job_args.settlement_report_args import (
 from settlement_report_job.entry_points.entry_point import (
     start_task_with_deps,
 )
-from settlement_report_job.domain.report_generator import ReportGenerator
+import settlement_report_job.domain.report_generator as report_generator
 from tests.integration_test_configuration import IntegrationTestConfiguration
 
 
