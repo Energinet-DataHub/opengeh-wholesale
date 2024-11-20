041fd710839d8c186c1322bfdb769eda4bf430fa
Alexander <alexanderharrow@hotmail.com>
2024-11-20 12:44:20 +0100

    Update test_sum_within_month.py

--- source/databricks/calculation_engine/tests/calculation/wholesale/test_sum_within_month.py
+++ source/databricks/calculation_engine/tests/calculation/wholesale/test_sum_within_month.py
@@ -19,9 +19,6 @@ from pyspark.sql import SparkSession
 
 import tests.calculation.wholesale.factories.wholesale_results_factory as wholesale_results_factory
 from package.calculation.wholesale.sum_within_month import sum_within_month
-from package.codelists import (
-    ChargeQuality,
-)
 from package.constants import Colname
 
 PERIOD_START_DATETIME = datetime(2019, 12, 31, 23)
@@ -51,31 +48,6 @@ def test__sum_within_month__sums_amount_per_month(
     assert actual.count() == 1
 
 
-def test__sum_within_month__qualities_always_set_to_none(
-    spark: SparkSession,
-) -> None:
-    # Arrange
-    rows = [
-        wholesale_results_factory.create_row(
-            qualities=[ChargeQuality.CALCULATED.value],
-        ),
-        wholesale_results_factory.create_row(
-            qualities=[ChargeQuality.ESTIMATED.value],
-        ),
-    ]
-    df = wholesale_results_factory.create(spark, rows)
-
-    # Act
-    actual = sum_within_month(
-        df,
-        PERIOD_START_DATETIME,
-    ).df
-
-    # Assert
-    assert actual.collect()[0][Colname.qualities] is None
-    assert actual.count() == 1
-
-
 def test__sum_within_month__charge_time_is_always_period_start_datetime(
     spark: SparkSession,
 ) -> None:
@@ -95,56 +67,6 @@ def test__sum_within_month__charge_time_is_always_period_start_datetime(
 
     # Assert
     assert actual.collect()[0][Colname.charge_time] == PERIOD_START_DATETIME
-
-
-def test__sum_within_month__total_quantity_always_set_to_none(
-    spark: SparkSession,
-) -> None:
-    # Arrange
-    rows = [
-        wholesale_results_factory.create_row(
-            total_quantity=Decimal("1"),
-        ),
-        wholesale_results_factory.create_row(
-            total_quantity=Decimal("1"),
-        ),
-    ]
-    df = wholesale_results_factory.create(spark, rows)
-
-    # Act
-    actual = sum_within_month(
-        df,
-        PERIOD_START_DATETIME,
-    ).df
-
-    # Assert
-    assert actual.collect()[0][Colname.total_quantity] is None
-    assert actual.count() == 1
-
-
-def test__sum_within_month__sets_charge_price_to_none(
-    spark: SparkSession,
-) -> None:
-    # Arrange
-    rows = [
-        wholesale_results_factory.create_row(
-            charge_price=Decimal("1"),
-        ),
-        wholesale_results_factory.create_row(
-            charge_price=Decimal("1"),
-        ),
-    ]
-    df = wholesale_results_factory.create(spark, rows)
-
-    # Act
-    actual = sum_within_month(
-        df,
-        PERIOD_START_DATETIME,
-    ).df
-
-    # Assert
-    assert actual.collect()[0][Colname.charge_price] is None
-    assert actual.count() == 1
 
 
 def test__sum_within_month__can_sum_when_one_value_is_none(
