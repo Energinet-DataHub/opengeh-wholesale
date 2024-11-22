a11c29836d1e48a57f9ee61f1a0e5c5c261668b2
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2024-11-22 10:26:23 +0100

    Set ts processor autoscaling to 1-2 instances (#2506)

--- migrations/terraform/env/prod_001/service-plans_override.tf
+++ migrations/terraform/env/prod_001/service-plans_override.tf
@@ -10,7 +10,7 @@ module "message_retriever_service_plan" {
 
 module "message_processor_service_plan" {
   sku_name                     = "EP3"
-  maximum_elastic_worker_count = 1
+  maximum_elastic_worker_count = 2
 }
 
 module "webapp_service_plan" {
