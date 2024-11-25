bcc40393253db13020e0b8272093265ef4ff81b2
Jonas Michelsen <jvm@energinet.dk>
2024-11-25 14:49:50 +0100

    Update 202409110811_create_view__wholesale_sap__latest_calculations_history_v1.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202409110811_create_view__wholesale_sap__latest_calculations_history_v1.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202409110811_create_view__wholesale_sap__latest_calculations_history_v1.sql
@@ -7,27 +7,41 @@ DROP VIEW IF EXISTS {CATALOG_NAME}.{WHOLESALE_SAP_DATABASE_NAME}.latest_calculat
 GO
 
 CREATE VIEW IF NOT EXISTS {CATALOG_NAME}.{WHOLESALE_SAP_DATABASE_NAME}.latest_calculations_history_v1 as
-WITH calculations_by_day AS (
-  SELECT
-    c.calculation_id,
-    calculation_type,
-    calculation_version,
-    cga.grid_area_code,
-    -- Explode calculation period into daily intervals.
-    -- The end date is exclusive, and the period is therefore subtracted by one day.
-    -- The calculation period is in UTC, so we convert it to local time.
-    explode(sequence(
-       from_utc_timestamp(calculation_period_start, 'Europe/Copenhagen'),
-       from_utc_timestamp(calculation_period_end, 'Europe/Copenhagen') - interval 1 day,
-       interval 1 day
-    )) AS from_date_local,
-    -- All rows should represent a full day, so the to_date is the day after the from_date
-    DATE_ADD(from_date_local, 1) AS to_date_local,
-    calculation_succeeded_time as latest_from_time
-  FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations c
-  INNER JOIN {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculation_grid_areas cga ON c.calculation_id = cga.calculation_id
-  WHERE calculation_succeeded_time IS NOT NULL
+WITH calculations_by_day_and_grid_area_local_time AS (
+    SELECT c.calculation_id, c.calculation_type, c.calculation_version, g.grid_area_code,
+           explode(sequence(
+               from_utc_timestamp(calculation_period_start, 'Europe/Copenhagen'),
+               from_utc_timestamp(calculation_period_end, 'Europe/Copenhagen') - interval 1 day,
+               interval 1 day
+           )) AS from_date_local,
+            DATE_ADD(from_date_local, 1) AS to_date_local,
+            calculation_succeeded_time as latest_from_time
+    FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations c
+    JOIN {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculation_grid_areas g
+    ON c.calculation_id = g.calculation_id
+    WHERE calculation_succeeded_time IS NOT NULL
 )
+-- WITH calculations_by_day AS (
+--   SELECT
+--     c.calculation_id,
+--     c.calculation_type,
+--     c.calculation_version,
+--     cga.grid_area_code,
+--     -- Explode calculation period into daily intervals.
+--     -- The end date is exclusive, and the period is therefore subtracted by one day.
+--     -- The calculation period is in UTC, so we convert it to local time.
+--     explode(sequence(
+--        from_utc_timestamp(calculation_period_start, 'Europe/Copenhagen'),
+--        from_utc_timestamp(calculation_period_end, 'Europe/Copenhagen') - interval 1 day,
+--        interval 1 day
+--     )) AS from_date_local,
+--     -- All rows should represent a full day, so the to_date is the day after the from_date
+--     DATE_ADD(from_date_local, 1) AS to_date_local,
+--     calculation_succeeded_time as latest_from_time
+--   FROM {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations c
+--   INNER JOIN {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculation_grid_areas cga ON c.calculation_id = cga.calculation_id
+--   WHERE calculation_succeeded_time IS NOT NULL
+-- )
 SELECT
   calculation_id,
   calculation_type,
@@ -35,8 +49,7 @@ SELECT
   grid_area_code,
   TO_UTC_TIMESTAMP(from_date_local, 'Europe/Copenhagen') AS from_date,
   TO_UTC_TIMESTAMP(to_date_local, 'Europe/Copenhagen') AS to_date,
-  latest_from_time,
-  -- The latest_to_time is the latest_from_time of the next calculation that has been calculated (with the same, calc. type, grid are and from date).
+  from_date_local,
   LEAD(latest_from_time) OVER (PARTITION BY calculation_type, grid_area_code, from_date_local ORDER BY calculation_version ASC) AS latest_to_time
-FROM calculations_by_day
+FROM calculations_by_day_and_grid_area_local_time
 
