4ebe0b4e17de3367376f39f5a054eabb813c9e37
Svend Lund Breddam <118453412+slb-halfspace@users.noreply.github.com>
2024-11-25 13:10:24 +0100

    Added application insights to optimize tables job (#2514)

--- wholesale/terraform/main/dbwj-optimise-tables.tf
+++ wholesale/terraform/main/dbwj-optimise-tables.tf
@@ -22,14 +22,15 @@ resource "databricks_job" "optimise_tables_job" {
         "fs.azure.account.oauth2.client.secret.${data.azurerm_key_vault_secret.st_data_lake_name.value}.dfs.core.windows.net" : databricks_secret.spn_app_secret.config_reference
       }
       spark_env_vars = {
-        "TENANT_ID"                       = var.tenant_id,
-        "SPN_APP_ID"                      = databricks_secret.spn_app_id.config_reference
-        "SPN_APP_SECRET"                  = databricks_secret.spn_app_secret.config_reference
-        "DATA_STORAGE_ACCOUNT_NAME"       = data.azurerm_key_vault_secret.st_data_lake_name.value
-        "TIME_ZONE"                       = local.TIME_ZONE
-        "CATALOG_NAME"                    = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
-        "CALCULATION_INPUT_FOLDER_NAME"   = var.calculation_input_folder
-        "CALCULATION_INPUT_DATABASE_NAME" = var.calculation_input_database
+        "TENANT_ID"                             = var.tenant_id,
+        "SPN_APP_ID"                            = databricks_secret.spn_app_id.config_reference
+        "SPN_APP_SECRET"                        = databricks_secret.spn_app_secret.config_reference
+        "DATA_STORAGE_ACCOUNT_NAME"             = data.azurerm_key_vault_secret.st_data_lake_name.value
+        "TIME_ZONE"                             = local.TIME_ZONE
+        "CATALOG_NAME"                          = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "CALCULATION_INPUT_FOLDER_NAME"         = var.calculation_input_folder
+        "CALCULATION_INPUT_DATABASE_NAME"       = var.calculation_input_database
+        "APPLICATIONINSIGHTS_CONNECTION_STRING" = data.azurerm_key_vault_secret.appi_shared_connection_string.value
       }
     }
 
