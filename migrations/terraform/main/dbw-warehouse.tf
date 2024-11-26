35a5ed3a7d90b1a74da73de13e6e14ff157198df
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2024-11-26 14:45:57 +0100

    Migration: Create SQL warehouse for data investigation (#2526)
    * Create SQL warehouse for data investigation
    * 2X-Large
    * Remove entitlements

--- migrations/terraform/main/dbw-warehouse.tf
+++ migrations/terraform/main/dbw-warehouse.tf
@@ -67,3 +67,32 @@ resource "databricks_permissions" "backup_endpoint" {
 
   depends_on = [module.dbw]
 }
+
+# SQL warehouse investigate data, used for ad-hoc queries in big data
+resource "databricks_sql_endpoint" "investigate_sql_endpoint" {
+  provider                  = databricks.dbw
+  name                      = "Investigate SQL endpoint"
+  cluster_size              = "2X-Large"
+  max_num_clusters          = 1
+  auto_stop_mins            = 30
+  warehouse_type            = "PRO"
+  enable_serverless_compute = false
+}
+
+resource "databricks_permissions" "investigate_sql_endpoint_permissions" {
+  provider        = databricks.dbw
+  sql_endpoint_id = databricks_sql_endpoint.investigate_sql_endpoint.id
+
+  access_control {
+    group_name       = var.databricks_contributor_dataplane_group.name
+    permission_level = "CAN_MANAGE"
+  }
+  dynamic "access_control" {
+    for_each = local.readers
+    content {
+      group_name       = access_control.key
+      permission_level = "CAN_MONITOR"
+    }
+  }
+  depends_on = [module.dbw]
+}
