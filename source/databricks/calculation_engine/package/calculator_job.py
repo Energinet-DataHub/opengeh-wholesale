594f40702adb2c5d6d291e549d0eb605d67c532e
Svend (SLB) <slb@halfspace.io>
2024-11-26 10:20:03 +0100

    Manually revert checkpoints

--- source/databricks/calculation_engine/package/calculator_job.py
+++ source/databricks/calculation_engine/package/calculator_job.py
@@ -84,7 +84,7 @@ def start_with_deps(
 
             args, infrastructure_settings = parse_job_args(command_line_args)
 
-            spark = initialize_spark(infrastructure_settings)
+            spark = initialize_spark()
             create_and_configure_container(spark, infrastructure_settings)
 
             prepared_data_reader = create_prepared_data_reader(
