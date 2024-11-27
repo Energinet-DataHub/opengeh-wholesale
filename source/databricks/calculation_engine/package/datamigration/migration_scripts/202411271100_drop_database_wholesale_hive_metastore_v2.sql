56700e74673443211835f0dd58f5ace3f31d31aa
Alexander <alexanderharrow@hotmail.com>
2024-11-27 15:35:44 +0100

    Update 202411271100_drop_database_wholesale_hive_metastore_v2.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202411271100_drop_database_wholesale_hive_metastore_v2.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202411271100_drop_database_wholesale_hive_metastore_v2.sql
@@ -1,4 +1,4 @@
 -- This script is used to drop the hive_metastore database if it exists.
 -- The CASCADE option is used to drop all objects in the database before dropping the database.
-DROP CATALOG IF EXISTS hive_metastore CASCADE;
+DROP DATABASE IF EXISTS hive_metastore CASCADE;
 GO
