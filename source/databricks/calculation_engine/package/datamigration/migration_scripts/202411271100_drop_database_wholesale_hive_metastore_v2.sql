3f1a7fbbd039fc078463e531c495d7f60edfe436
Alexander <alexanderharrow@hotmail.com>
2024-11-27 16:57:34 +0100

    Update 202411271100_drop_database_wholesale_hive_metastore_v2.sql

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202411271100_drop_database_wholesale_hive_metastore_v2.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202411271100_drop_database_wholesale_hive_metastore_v2.sql
@@ -1,11 +1,11 @@
 -- This script is used to drop the hive_metastore database if it exists.
 -- The CASCADE option is used to drop all objects in the database before dropping the database.
-DROP DATABASE IF EXISTS hive_metastore.basis_data CASCADE;
-DROP DATABASE IF EXISTS hive_metastore.schema_migration CASCADE;
-DROP DATABASE IF EXISTS hive_metastore.settlement_report CASCADE;
-DROP DATABASE IF EXISTS hive_metastore.wholesale_calculation_results CASCADE;
-DROP DATABASE IF EXISTS hive_metastore.wholesale_input CASCADE;
-DROP DATABASE IF EXISTS hive_metastore.wholesale_internal CASCADE;
-DROP DATABASE IF EXISTS hive_metastore.wholesale_output CASCADE;
-DROP DATABASE IF EXISTS hive_metastore.wholesale_output_anonymised CASCADE;
+DROP DATABASE IF EXISTS hive_metastore.basis_data CASCADE
+DROP DATABASE IF EXISTS hive_metastore.schema_migration CASCADE
+DROP DATABASE IF EXISTS hive_metastore.settlement_report CASCADE
+DROP DATABASE IF EXISTS hive_metastore.wholesale_calculation_results CASCADE
+DROP DATABASE IF EXISTS hive_metastore.wholesale_input CASCADE
+DROP DATABASE IF EXISTS hive_metastore.wholesale_internal CASCADE
+DROP DATABASE IF EXISTS hive_metastore.wholesale_output CASCADE
+DROP DATABASE IF EXISTS hive_metastore.wholesale_output_anonymised CASCADE
 GO
