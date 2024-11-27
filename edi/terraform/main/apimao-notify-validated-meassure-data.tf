390640b51835410cee6a4de3c0b69a68dfbcb821
Mads Magnus Due <xmadu@energinet.dk>
2024-11-27 11:42:23 +0100

    feat: new b2b cim endpoint for rsm012 (#2527)
    * feat: new b2b endpoint for rsm012 in cim

---
+++ edi/terraform/main/apimao-notify-validated-meassure-data.tf
@@ -0,0 +1,33 @@
+module "apimao_notify_validated_meassure_data" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/api-management-api-operation?ref=api-management-api-operation_6.0.1"
+
+  operation_id            = "notify-validated-meassure-data"
+  api_management_api_name = module.apima_b2b.name
+  resource_group_name     = data.azurerm_key_vault_secret.apim_instance_resource_group_name.value
+  api_management_name     = data.azurerm_key_vault_secret.apim_instance_name.value
+  display_name            = "EDI: Notify Validated Meassure Data (RSM-012)"
+  method                  = "POST"
+  url_template            = "/v1.0/cim/notifyvalidatedmeasuredata"
+  policies = [
+    {
+      xml_content = <<XML
+        <policies>
+          <inbound>
+            <base />
+            <validate-jwt header-name="Authorization" failed-validation-httpcode="403" failed-validation-error-message="Unauthorized to access this endpoint.">
+                <openid-config url="https://login.microsoftonline.com/${data.azurerm_key_vault_secret.apim_b2c_tenant_id.value}/v2.0/.well-known/openid-configuration" />
+                <required-claims>
+                    <claim name="roles" match="any">
+                        <value>delegated</value>
+                        <value>gridaccessprovider</value>
+                    </claim>
+                </required-claims>
+            </validate-jwt>
+            <set-backend-service backend-id="${azurerm_api_management_backend.edi.name}" />
+            <rewrite-uri template="/api/incomingMessages/MeteredDataForMeasurementPoint" />
+          </inbound>
+        </policies>
+      XML
+    }
+  ]
+}
