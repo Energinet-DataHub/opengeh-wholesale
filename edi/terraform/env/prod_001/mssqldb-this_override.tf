8ab5e8c9b16d6c5db105f52b0336000ac4219f11
Mathias Ormstrup Bjerregaard <152613120+MWO1024@users.noreply.github.com>
2024-12-03 11:38:51 +0100

    chore (EDI): Update mssqldb max_size_gb (#2573)

--- edi/terraform/env/prod_001/mssqldb-this_override.tf
+++ edi/terraform/env/prod_001/mssqldb-this_override.tf
@@ -1,4 +1,7 @@
 module "mssqldb_edi" {
+  # Max size of the database in GB
+  max_size_gb = 128
+
   # Enabling zone redundancy for the database, as it is not in the elastic pool
   zone_redundant  = true
   security_groups = local.pim_security_group_rules_001
