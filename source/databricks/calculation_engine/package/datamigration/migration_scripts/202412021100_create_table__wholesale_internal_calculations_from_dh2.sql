726084186e302cca389e7d7e945c9f02d9694d21
Svend (SLB) <slb@halfspace.io>
2024-12-03 12:36:52 +0100

    Added extra columns to calculations

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202412021100_create_table__wholesale_internal_calculations_from_dh2.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202412021100_create_table__wholesale_internal_calculations_from_dh2.sql
@@ -1,12 +1,8 @@
 -- This copy of dh2 calculations is done, such that Wholesale can re-write their input as they please, without affecting Wholesale. 
 -- In theory this could be replaced by a view on the Volt input, but then we might accidentally lose data.
-CREATE OR REPLACE TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations_from_dh2 AS (
-    SELECT 
-        calculation_id, 
-        calculation_type,
-        calculation_period_start,
-        calculation_period_end,
-        calculation_execution_time_start,
-        calculation_succeeded_time, 
-    FROM {CATALOG_NAME}.{SHARED_WHOLESALE_INPUT}.calculations_view_v1
-)
+ALTER TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations 
+RENAME COLUMN calculation_version TO calculation_version_dh3
+GO 
+
+ALTER TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations
+ADD COLUMN (calculation_version_dh2 bigint, calculation_version GENERATED ALWAYS AS (CASE WHEN calculation_version_dh2 IS NOT NULL THEN 0 ELSE calculation_version_dh3 END))
