04f76c565e45c1a8765672f073fc6cd01ef89b65
Svend (SLB) <slb@halfspace.io>
2024-12-06 12:46:50 +0100

    Restructured some scripts

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202412061130_update_table__wholesale_internal_calculations__default_for_calculation_version_dh3.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202412061130_update_table__wholesale_internal_calculations__default_for_calculation_version_dh3.sql
@@ -1,4 +1,4 @@
 -- Adds new columns for DH2 calculation versions to be filled out my a manual migration script.
 -- Sets the default values for this new column, so that it is not null always.
-UPDATE TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations
+UPDATE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations
 SET calculation_version_dh3 = CASE WHEN calculation_version_dh2 IS NOT NULL THEN calculation_version_dh2 ELSE calculation_version
