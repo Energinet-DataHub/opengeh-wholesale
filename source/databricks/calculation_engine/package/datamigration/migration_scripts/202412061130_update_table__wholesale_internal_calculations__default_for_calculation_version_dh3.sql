6e063af8b1e1d8de21c2d7935e4c3fa04314db5b
Svend (SLB) <slb@halfspace.io>
2024-12-09 09:47:45 +0100

    Fixed typo

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202412061130_update_table__wholesale_internal_calculations__default_for_calculation_version_dh3.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202412061130_update_table__wholesale_internal_calculations__default_for_calculation_version_dh3.sql
@@ -1,3 +1,3 @@
 -- Sets the default values for this new column, so that it is not null always.
 UPDATE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations
-SET calculation_version_dh3 = CASE WHEN calculation_version_dh2 IS NOT NULL THEN calculation_version_dh2 ELSE calculation_version END
+SET calculation_version_dh3_temp = CASE WHEN calculation_version_dh2 IS NOT NULL THEN calculation_version_dh2 ELSE calculation_version END
