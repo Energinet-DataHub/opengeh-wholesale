b9faa7c557e16dc7590a7d4727d6fc433c1963e8
Svend (SLB) <slb@halfspace.io>
2024-12-09 10:02:57 +0100

    Fixed rename script

--- source/databricks/calculation_engine/package/datamigration/migration_scripts/202412061200_alter_table__wholesale_internal_calculations__rename_version_columns.sql
+++ source/databricks/calculation_engine/package/datamigration/migration_scripts/202412061200_alter_table__wholesale_internal_calculations__rename_version_columns.sql
@@ -1,11 +1,7 @@
 -- Swaps the column namings to make the calculation_version the "combined" column rather than just for DH3.
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations 
-RENAME COLUMN calculation_version TO calculation_version_dh3_temp
-GO 
-
-ALTER TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations 
-RENAME COLUMN calculation_version_dh3 TO calculation_version
+RENAME COLUMN calculation_version TO calculation_version_dh3
 GO
 
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_INTERNAL_DATABASE_NAME}.calculations 
-RENAME COLUMN calculation_version_dh3_temp TO calculation_version_dh3
+RENAME COLUMN calculation_version_dh3_temp TO calculation_version
