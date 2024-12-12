ee06162d4b098b8d5f813d7dc092988775fbf0bc
Svend (SLB) <slb@halfspace.io>
2024-12-12 14:34:37 +0100

    Changes to the DH2 migration script

--- source/databricks/calculation_engine/package/datamigration/manual_migration_scripts/202412021300_migrate_dh2_calculations.sql
+++ source/databricks/calculation_engine/package/datamigration/manual_migration_scripts/202412021300_migrate_dh2_calculations.sql
@@ -1,5 +1,6 @@
 -- Reusable migration script for DH2 calculations in Wholesale.
 -- It works in three general steps:
+-- * 0: Verify that all calculations we are about to migrate also have a corresponding calculation_id in it's input.
 -- * 1: Delete all rows from all tables with calculation version = 0 in calculations.
 -- * 2: Remove the calculations from our main table
 -- * 3: Re-insert the new calculations from DH2 into the main table with version = 0
@@ -14,61 +15,102 @@
 --   * calculation_grid_areas
 --
 
--- STEP 0:
--- Manually declare your environment variables.
-DECLARE OR REPLACE VARIABLE CATALOG_NAME STRING = "ctl_shres_t_we_001";
-DECLARE OR REPLACE VARIABLE WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME STRING = "wholesale_results_internal";
-DECLARE OR REPLACE VARIABLE WHOLESALE_INTERNAL_DATABASE_NAME STRING = "wholesale_internal";
-DECLARE OR REPLACE VARIABLE SHARED_WHOLESALE_INPUT STRING = "shared_wholesale_input";
+-- STEP 0: 
+-- Verify the input
+CREATE OR REPLACE TEMP VIEW calc_ids_from_dh2_view AS
+SELECT calc_ids.calculation_id
+FROM (
+    SELECT DISTINCT calculation_id 
+    FROM ctl_shres_t_we_001.shared_wholesale_input.calculation_results_energy_view_v1
+    UNION
+    SELECT DISTINCT calculation_id 
+    FROM ctl_shres_t_we_001.shared_wholesale_input.calculation_results_energy_per_brp_view_v1
+    UNION
+    SELECT DISTINCT calculation_id 
+    FROM ctl_shres_t_we_001.shared_wholesale_input.calculation_results_energy_per_es_view_v1
+    UNION
+    SELECT DISTINCT calculation_id 
+    FROM ctl_shres_t_we_001.shared_wholesale_input.calculation_grid_areas_view_v1
+) AS calc_ids
+LEFT JOIN ctl_shres_t_we_001.shared_wholesale_input.calculations_view_v1 AS calc_view
+ON calc_ids.calculation_id = calc_view.calculation_id
+WHERE calc_view.calculation_id IS NULL;
 
+-- Check the count and throw an error if the count is greater than 0
+WITH no_unparanted_calculations_check AS (
+    SELECT COUNT(*) AS cnt FROM calc_ids_from_dh2_view
+)
+SELECT CASE 
+    WHEN cnt > 0 THEN RAISE_ERROR("Some calculations are unparented! Please check the input data before running this script!")
+    ELSE "All calculations have an entry in the input's calculation table"
+END AS UnparentedCalculationIdsCheck
+FROM no_unparanted_calculations_check;
 
 -- STEP 1: Delete existing rows across Wholesale's domain
-MERGE INTO CATALOG_NAME.WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME.energy e1
-USING CATALOG_NAME.WHOLESALE_INTERNAL_DATABASE_NAME.calculations c
+MERGE INTO ctl_shres_t_we_001.wholesale_results_internal.energy e1
+USING ctl_shres_t_we_001.wholesale_internal.calculations c
 ON c.calculation_id <=> e1.calculation_id and c.calculation_version_dh2 is not null
 WHEN MATCHED THEN DELETE;
 
-MERGE INTO CATALOG_NAME.WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME.energy_per_brp e2
-USING CATALOG_NAME.WHOLESALE_INTERNAL_DATABASE_NAME.calculations c
+MERGE INTO ctl_shres_t_we_001.wholesale_results_internal.energy_per_brp e2
+USING ctl_shres_t_we_001.wholesale_internal.calculations c
 ON c.calculation_id <=> e2.calculation_id and c.calculation_version_dh2 is not null
 WHEN MATCHED THEN DELETE;
 
-MERGE INTO CATALOG_NAME.WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME.energy_per_es e3
-USING CATALOG_NAME.WHOLESALE_INTERNAL_DATABASE_NAME.calculations c
+MERGE INTO ctl_shres_t_we_001.wholesale_results_internal.energy_per_es e3
+USING ctl_shres_t_we_001.wholesale_internal.calculations c
 ON c.calculation_id <=> e3.calculation_id and c.calculation_version_dh2 is not null
 WHEN MATCHED THEN DELETE;
 
-MERGE INTO CATALOG_NAME.WHOLESALE_INTERNAL_DATABASE_NAME.calculation_grid_areas g1
-USING CATALOG_NAME.WHOLESALE_INTERNAL_DATABASE_NAME.calculations c
+MERGE INTO ctl_shres_t_we_001.wholesale_internal.calculation_grid_areas g1
+USING ctl_shres_t_we_001.wholesale_internal.calculations c
 ON c.calculation_id <=> g1.calculation_id and c.calculation_version_dh2 is not null
 WHEN MATCHED THEN DELETE;
 
 -- STEP 2: Remove the DH2 calculations from the main table
-DELETE FROM CATALOG_NAME.WHOLESALE_INTERNAL_DATABASE_NAME.calculations
+DELETE FROM ctl_shres_t_we_001.wholesale_internal.calculations
 WHERE calculation_version_dh2 is not null;
 
 -- STEP 3: Re-migrate each of the tables with calculations from DH2.
 -- TODO: Replace "0" with whatever version is given by VOLT later.
-INSERT INTO CATALOG_NAME.WHOLESALE_INTERNAL_DATABASE_NAME.calculations 
-(calculation_id, calculation_type, calculation_period_start, calculation_period_end, calculation_execution_time_start, calculation_succeeded_time, is_internal_calculation, calculation_version_dh2, calculation_version)
-SELECT (calculation_id, calculation_type, calculation_period_start, calculation_period_end, calculation_execution_time_start, calculation_succeeded_time, False, 0, 0) FROM CATALOG_NAME.SHARED_WHOLESALE_INPUT.calculations_view_v1;
+INSERT INTO ctl_shres_t_we_001.wholesale_internal.calculations 
+  (
+    calculation_id,
+    calculation_type,
+    calculation_period_start,
+    calculation_period_end,
+    calculation_execution_time_start,
+    calculation_succeeded_time,
+    is_internal_calculation,
+    calculation_version_dh2,
+    calculation_version
+  )
+SELECT
+  calculation_id,
+  calculation_type,
+  calculation_period_start,
+  calculation_period_end,
+  calculation_period_execution_time_start,
+  calculation_period_succeeded_time,
+  False as is_internal_calculation,
+  0 as calculation_version_dh2,
+  0 as calculation_version
+FROM
+  ctl_shres_t_we_001.shared_wholesale_input.calculations_view_v1;
 
 -- Result ID for the energy-tables should be unique per: 
--- calculation_id, grid_area_code, balance_responsible_party_id, energy_supplier_id, time_series_type
--- [energy_supplier_id's last 8 digits]-[grid_area_code]-[BRP's last 4 digits]-[time_series_type abbreviated]-[calculation_id's final 12 characters]
-INSERT INTO CATALOG_NAME.WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME.energy 
+-- table_name, calculation_id, grid_area_code, balance_responsible_party_id, energy_supplier_id, time_series_type
+-- To make it unique per table, each one's result_id follows a different formula:
+-- [time_series_type's first 8 characters]-[grid_area_code]-Null-Null-[calculation_id's final 10 characters]P1 
+INSERT INTO ctl_shres_t_we_001.wholesale_results_internal.energy 
 SELECT 
   calculation_id,   
   CONCAT(
-    SUBSTRING(energy_supplier_id, -8), '-', 
-    grid_area_code, '-', 
-    SUBSTRING(balance_responsible_party_id, -4), '-', 
-    CASE WHEN time_series_type = 'non_profiled_consumption' THEN 'nonp' 
-         WHEN time_series_type = 'production' THEN 'prod' 
-         WHEN time_series_type = 'flex_consumption' THEN 'flex'
-    ELSE SUBSTRING(time_series_type, 1, 4) END, 
-    '-', 
-    SUBSTRING(calculation_id, -12)
+    SUBSTRING(time_series_type, 1, 8), '-',
+    LPAD(grid_area_code, 4, '0'), '-', 
+    'Null', '-', 
+    'Null', '-', 
+    SUBSTRING(calculation_id, -10), 'P1'
   ) as result_id,  
   grid_area_code, 
   time_series_type, 
@@ -76,54 +118,51 @@ SELECT
   time, 
   quantity, 
   quantity_qualities
-FROM CATALOG_NAME.SHARED_WHOLESALE_INPUT.calculation_results;
+FROM ctl_shres_t_we_001.shared_wholesale_input.calculation_results_energy_view_v1;
 
-INSERT INTO CATALOG_NAME.WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME.energy_per_brp
+-- ResultID follows:
+-- [balance_responsible_party_id's last 8 characters]-[grid_area_code]-[time_series_type's first 4 characters]-[time_series_type's last 4 characters]-[calculation_id's final 10 characters]P2 
+INSERT INTO ctl_shres_t_we_001.wholesale_results_internal.energy_per_brp
 SELECT 
   calculation_id,
   CONCAT(
-    SUBSTRING(energy_supplier_id, -8), '-', 
-    grid_area_code, '-', 
-    SUBSTRING(balance_responsible_party_id, -4), '-', 
-    CASE WHEN time_series_type = 'non_profiled_consumption' THEN 'nonp' 
-         WHEN time_series_type = 'production' THEN 'prod' 
-         WHEN time_series_type = 'flex_consumption' THEN 'flex'
-    ELSE SUBSTRING(time_series_type, 1, 4) END, 
-    '-', 
-    SUBSTRING(calculation_id, -12)
-  ) as result_id, 
+    SUBSTRING(balance_responsible_party_id, -8), '-',
+    LPAD(grid_area_code, 4, '0'), '-', 
+    SUBSTRING(time_series_type, 1, 4), '-',
+    SUBSTRING(time_series_type, -4), '-',
+    SUBSTRING(calculation_id, -8), 'Ver2'
+  ) as result_id,  
   grid_area_code, 
-  balance_responsible_party, 
+  balance_responsible_party_id, 
   time_series_type, 
   resolution,
   time, 
   quantity, 
   quantity_qualities
-FROM CATALOG_NAME.SHARED_WHOLESALE_INPUT.calculation_results_energy_per_brp_view_v1;
-
-INSERT INTO CATALOG_NAME.WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME.energy_per_es
+FROM ctl_shres_t_we_001.shared_wholesale_input.calculation_results_energy_per_brp_view_v1; 
+ 
+-- ResultID follows:
+-- [balance_responsible_party_id's last 8 characters]-[grid_area_code]-[time_series_type's first 4 characters]-[energy_supplier's last 4 characters]-[calculation_id's final 10 characters]P2 
+INSERT INTO ctl_shres_t_we_001.wholesale_results_internal.energy_per_es
 SELECT 
-  calculation_id, 
+  calculation_id,
   CONCAT(
-    SUBSTRING(energy_supplier_id, -8), '-', 
-    grid_area_code, '-', 
-    SUBSTRING(balance_responsible_party_id, -4), '-', 
-    CASE WHEN time_series_type = 'non_profiled_consumption' THEN 'nonp' 
-         WHEN time_series_type = 'production' THEN 'prod' 
-         WHEN time_series_type = 'flex_consumption' THEN 'flex'
-    ELSE SUBSTRING(time_series_type, 1, 4) END, 
-    '-', 
-    SUBSTRING(calculation_id, -12)
-  ) as result_id, 
+    SUBSTRING(balance_responsible_party_id, -8), '-',
+    LPAD(grid_area_code, 4, '0'), '-', 
+    SUBSTRING(time_series_type, 1, 4), '-',
+    SUBSTRING(energy_supplier_id, -4), '-',
+    SUBSTRING(calculation_id, -8), 'Ver2'
+  ) as result_id,  
   grid_area_code, 
-  energy_supplier_id, 
-  balance_responsible_party, 
+  energy_supplier_id,
+  balance_responsible_party_id, 
   time_series_type, 
-  resolution, 
+  resolution,
   time, 
   quantity, 
   quantity_qualities
-FROM CATALOG_NAME.SHARED_WHOLESALE_INPUT.calculation_results_energy_per_es_view_v1;
+FROM ctl_shres_t_we_001.shared_wholesale_input.calculation_results_energy_per_es_view_v1; 
 
-INSERT INTO CATALOG_NAME.WHOLESALE_INTERNAL_DATABASE_NAME.calculation_grid_areas
-SELECT calculation_id, grid_area_code FROM CATALOG_NAME.SHARED_WHOLESALE_INPUT.calculation_grid_areas_view_v1;
+
+INSERT INTO ctl_shres_t_we_001.wholesale_internal.calculation_grid_areas
+SELECT calculation_id, grid_area_code FROM ctl_shres_t_we_001.shared_wholesale_input.calculation_grid_areas_view_v1;
