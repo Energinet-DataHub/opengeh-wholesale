9782879c4125997c403fcd044561c03f5f74c7d5
Lars Nielsen <lanni@energinet.dk>
2024-12-12 10:57:04 +0100

    Increase revisionlog db to 100 GB for test001 (#2639)

--- revision-log/terraform/env/test_001/mssqldb-revision-log_override.tf
+++ revision-log/terraform/env/test_001/mssqldb-revision-log_override.tf
@@ -1,3 +1,4 @@
 module "mssqldb_revision_log" {
-  security_groups  = concat(local.pim_security_group_rules_001, local.developer_security_group_rules_001_dev_test)
+  security_groups = concat(local.pim_security_group_rules_001, local.developer_security_group_rules_001_dev_test)
+  max_size_gb     = 100
 }
