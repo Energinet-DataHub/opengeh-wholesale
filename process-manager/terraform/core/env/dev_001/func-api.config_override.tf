b400a4e8e978d89894f5b221e4652a3cd7796756
Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>
2024-12-20 12:56:32 +0100

    Disable timer triggers in dev_001 (#2670)

--- process-manager/terraform/core/env/dev_001/func-api.config_override.tf
+++
@@ -1,8 +0,0 @@
-module "func_api" {
-  app_settings = merge(local.func_api.app_settings, {
-    # Timer triggers
-    # Override settings to enable them i development/tests environments
-    "AzureWebJobs.StartScheduledOrchestrationInstances.Disabled" = false
-    "AzureWebJobs.PerformRecurringPlanning.Disabled"             = false
-  })
-}
