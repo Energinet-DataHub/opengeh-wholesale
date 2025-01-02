120eadd57d78a243614737ad37fb6aa50903726c
Mads Magnus Due <xmadu@energinet.dk>
2025-01-02 14:44:03 +0100

    test: adding more db cpu and adjusted function scalability (#2694)
    test: adding more db cpu and adjusted function scalability

---
+++ edi/terraform/env/test_001/func-api_override.tf
@@ -0,0 +1,4 @@
+module "func_service_plan" {
+  pre_warmed_instance_count = 8
+  elastic_instance_minimum = 2
+}
--- edi/terraform/env/test_001/mssqldb-this_override.tf
+++ edi/terraform/env/test_001/mssqldb-this_override.tf
@@ -1,3 +1,5 @@
 module "mssqldb_edi" {
   security_groups = concat(local.pim_security_group_rules_001, local.developer_security_group_rules_001_dev_test)
+  sku_name                    = "GP_S_Gen5_40" # General Purpose (GP) - serverless compute (S) - standard series (Gen5) - max vCores (<number>) : https://learn.microsoft.com/en-us/azure/azure-sql/database/resource-limits-vcore-single-databases?view=azuresql#gen5-hardware-part-1-1
+  min_capacity                = 10
 }
