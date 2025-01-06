653a6ee5b639e9d1747575cc936dafc7a7a0d753
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2025-01-06 09:57:38 +0100

    2552 cycle shared (#2665)

--- shared-resources/terraform/infrastructure/env/preprod_002/recycle-additional.tf
+++ shared-resources/terraform/infrastructure/env/preprod_002/recycle-additional.tf
@@ -6,3 +6,19 @@ module "shared_databricks_workspace_url" {
   value        = azurerm_databricks_workspace.this.workspace_url
   key_vault_id = module.kv_shared.id
 }
+
+module "grafana_dashboard_endpoint" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=key-vault-secret_6.0.0"
+
+  name         = "grafana-dashboard-endpoint"
+  value        = azurerm_dashboard_grafana.this.endpoint
+  key_vault_id = module.kv_shared.id
+}
+
+module "grafana_key" {
+  source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=key-vault-secret_6.0.0"
+
+  name         = "grafana-key"
+  value        = shell_script.create_key.output["key"]
+  key_vault_id = module.kv_shared.id
+}
