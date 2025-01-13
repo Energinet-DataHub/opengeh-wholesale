03b580fb3d52fa60909a26bc20431f64f6a79ac2
Christian Hansen <cenh@halfspace.io>
2025-01-13 12:20:09 +0100

    removed settlement reports

--- source/databricks/settlement_report/settlement_report_job/domain/utils/get_start_of_day.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import Column
-from pyspark.sql import functions as F
-
-
-def get_start_of_day(col: Column | str, time_zone: str) -> Column:
-    col = F.col(col) if isinstance(col, str) else col
-    return F.to_utc_timestamp(
-        F.date_trunc("DAY", F.from_utc_timestamp(col, time_zone)), time_zone
-    )
