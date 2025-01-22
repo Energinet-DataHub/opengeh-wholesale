6ef5647bc9b3b160b56cce5406541afaa93759f5
Alex <77341673+defectiveAi@users.noreply.github.com>
2025-01-22 15:14:45 +0100

    Enable Databricks health check for test only. (#2789)

--- electricity-market/terraform/env/test_001/func-import.config_override.tf
+++ electricity-market/terraform/env/test_001/func-import.config_override.tf
@@ -3,5 +3,7 @@ module "func_import" {
     "Databricks:WorkspaceUrl"   = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=dbw-migration-workspace-url)"
     "Databricks:WorkspaceToken" = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=dbw-migration-workspace-token)"
     "Databricks:WarehouseId"    = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.kv_shared_resources.name};SecretName=dbw-migration-warehouse-id)"
+
+    "EnableDatabricksHealthCheck" = true
   })
 }
