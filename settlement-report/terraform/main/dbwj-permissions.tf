a73d9f6f99d2fcfed4a3b2d060be92a7e20f1751
Christian Hansen <120550591+cenh-halfspace@users.noreply.github.com>
2025-01-23 10:27:08 +0100

    Remove Settlement Reports from Wholesale (#2732)

--- settlement-report/terraform/main/dbwj-permissions.tf
+++ settlement-report/terraform/main/dbwj-permissions.tf
@@ -1,6 +1,6 @@
-resource "databricks_permissions" "calculator_job" {
+resource "databricks_permissions" "settlement_report_job_balance_fixing" {
   provider = databricks.dbw
-  job_id   = databricks_job.calculator_job.id
+  job_id   = databricks_job.settlement_report_job_balance_fixing.id
 
   access_control {
     group_name       = var.databricks_contributor_dataplane_group.name
@@ -16,27 +16,9 @@ resource "databricks_permissions" "calculator_job" {
   depends_on = [module.dbw]
 }
 
-resource "databricks_permissions" "migrations_job" {
+resource "databricks_permissions" "settlement_report_job_wholesale" {
   provider = databricks.dbw
-  job_id   = databricks_job.migrations_job.id
-
-  access_control {
-    group_name       = var.databricks_contributor_dataplane_group.name
-    permission_level = "CAN_MANAGE"
-  }
-  dynamic "access_control" {
-    for_each = local.readers
-    content {
-      group_name       = access_control.key
-      permission_level = "CAN_VIEW"
-    }
-  }
-  depends_on = [module.dbw]
-}
-
-resource "databricks_permissions" "optimise_tables_job" {
-  provider = databricks.dbw
-  job_id   = databricks_job.optimise_tables_job.id
+  job_id   = databricks_job.settlement_report_job_wholesale.id
 
   access_control {
     group_name       = var.databricks_contributor_dataplane_group.name
