81940a7de10e91b6fcc7959009c6857c17d7d704
Jakob Stricker Nielsen <jakob.s.nielsen@twoday.com>
2025-01-24 11:20:13 +0100

    Modify the data products tests

--- source/databricks/calculation_engine/tests/features/data_products/wholesale_results/given_minimal_wholesale_results_scenario/test_output.py
+++ source/databricks/calculation_engine/tests/features/data_products/wholesale_results/given_minimal_wholesale_results_scenario/test_output.py
@@ -8,11 +8,11 @@ from testcommon.dataframes import (
 
 @pytest.mark.parametrize("name", get_then_names())
 def test__equals_expected(
-    test_cases_views: TestCases,
+    test_cases: TestCases,
     name: str,
     assert_dataframes_configuration: AssertDataframesConfiguration,
 ) -> None:
-    test_case = test_cases_views[name]
+    test_case = test_cases[name]
 
     assert_dataframes_and_schemas(
         actual=test_case.actual,
