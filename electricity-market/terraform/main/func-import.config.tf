da4904b49bb6d87365bcd5449e926b2875c83d98
Michael <52096428+michaelmajgaard@users.noreply.github.com>
2025-01-24 14:19:25 +0100

    chore: retrigger infrastructure deploy (#2804)

--- electricity-market/terraform/main/func-import.config.tf
+++ electricity-market/terraform/main/func-import.config.tf
@@ -2,7 +2,6 @@ locals {
   func_import = {
     app_settings = {
       "Database:ConnectionString" = local.MS_MARKPART_DB_CONNECTION_STRING
-
       "Databricks:WorkspaceUrl"   = "https://localhost"
       "Databricks:WorkspaceToken" = "tbd"
       "Databricks:WarehouseId"    = "tbd"
