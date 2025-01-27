55d1f337600d501ec7e4f286225a0ded08911fd6
Mads Magnus Due <xmadu@energinet.dk>
2025-01-27 12:29:22 +0100

    test: increase pm db size for load test (#2807)

--- process-manager/terraform/core/env/test_001/mssqldb-process-manager_override.tf
+++ process-manager/terraform/core/env/test_001/mssqldb-process-manager_override.tf
@@ -1,4 +1,4 @@
 module "mssqldb_process_manager" {
   security_groups = concat(local.pim_security_group_rules_001, local.developer_security_group_rules_001_dev_test)
-  max_size_gb                 = 80
+  max_size_gb                 = 1020
 }
