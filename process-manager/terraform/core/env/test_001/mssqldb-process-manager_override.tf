1ea6e75f09a3ea19e5e9c143e1873bdff796c5ef
Mads Magnus Due <xmadu@energinet.dk>
2025-01-28 13:16:49 +0100

    test: increase vCores to get more workers on pm db (test001) (#2819)

--- process-manager/terraform/core/env/test_001/mssqldb-process-manager_override.tf
+++ process-manager/terraform/core/env/test_001/mssqldb-process-manager_override.tf
@@ -1,5 +1,5 @@
 module "mssqldb_process_manager" {
   security_groups = concat(local.pim_security_group_rules_001, local.developer_security_group_rules_001_dev_test)
-  max_size_gb                 = 1020
-  sku_name                    = "GP_S_Gen5_40" # General Purpose (GP) - serverless compute (S) - standard series (Gen5) - max vCores (<number>) : https://learn.microsoft.com/en-us/azure/azure-sql/database/resource-limits-vcore-single-databases?view=azuresql#gen5-hardware-part-1-1
+  max_size_gb     = 1020
+  sku_name        = "GP_S_Gen5_80" # General Purpose (GP) - serverless compute (S) - standard series (Gen5) - max vCores (<number>) : https://learn.microsoft.com/en-us/azure/azure-sql/database/resource-limits-vcore-single-databases?view=azuresql#gen5-hardware-part-1-1
 }
