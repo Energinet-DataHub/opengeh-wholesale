f0f665ea09a1e98ba66c6e45660c070094f5a22b
Jeppe M <jmw@energinet.dk>
2025-01-29 11:52:50 +0100

    Measurements/Feat: Create new Silver to Gold Measurements pipeline (#2818)

--- measurements/terraform/core/main/dbwj-gold.tf
+++ measurements/terraform/core/main/dbwj-gold.tf
@@ -1,3 +1,64 @@
+resource "databricks_job" "silver_to_gold_measurements" {
+  provider            = databricks.dbw
+  name                = "Silver to Gold Measurements"
+  max_concurrent_runs = 1
+
+  job_cluster {
+    job_cluster_key = "silver_to_gold_measurements_cluster"
+
+    new_cluster {
+      spark_version  = local.spark_version
+      node_type_id   = "Standard_D8as_v4"
+      runtime_engine = "PHOTON"
+      autoscale {
+        min_workers = 1
+        max_workers = 5
+      }
+      spark_conf = {
+        "spark.databricks.sql.initial.catalog.name" = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+      }
+
+      spark_env_vars = {
+        "CATALOG_NAME"                          = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "APPLICATIONINSIGHTS_CONNECTION_STRING" = data.azurerm_key_vault_secret.appi_shared_connection_string.value
+        "DATALAKE_STORAGE_ACCOUNT"              = module.st_measurements.name
+      }
+    }
+  }
+
+  task {
+    task_key        = "silver_to_gold_measurements_task"
+    max_retries     = 1
+    job_cluster_key = "silver_to_gold_measurements_cluster"
+
+    library {
+      whl = "/Workspace/Shared/PythonWheels/core/opengeh_gold-0.1.0-py3-none-any.whl"
+    }
+
+    python_wheel_task {
+      package_name = "opengeh_gold"
+      entry_point  = "stream"
+    }
+  }
+}
+
+resource "databricks_permissions" "silver_to_gold_measurements" {
+  provider = databricks.dbw
+  job_id   = databricks_job.silver_to_gold_measurements.id
+
+  access_control {
+    group_name       = var.databricks_contributor_dataplane_group.name
+    permission_level = "CAN_MANAGE"
+  }
+  dynamic "access_control" {
+    for_each = local.readers
+    content {
+      group_name       = access_control.key
+      permission_level = "CAN_VIEW"
+    }
+  }
+}
+
 resource "databricks_job" "gold_database_migration" {
   provider            = databricks.dbw
   name                = "Gold Database Migration"
@@ -37,8 +98,7 @@ resource "databricks_job" "gold_database_migration" {
 
     python_wheel_task {
       package_name = "gold"
-      # The entry point is defined in pyproject.toml
-      entry_point = "migrate"
+      entry_point  = "migrate"
     }
   }
 
