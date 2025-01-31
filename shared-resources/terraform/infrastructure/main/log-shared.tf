5ec4d7beede79169b829df0ef81ddd4d1505927d
Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>
2025-01-31 12:21:20 +0100

    2810: increase log retention on tables (#2832)

--- shared-resources/terraform/infrastructure/main/log-shared.tf
+++ shared-resources/terraform/infrastructure/main/log-shared.tf
@@ -32,3 +32,40 @@ module "kvs_log_shared_workspace_id" {
   value        = module.log_workspace_shared.workspace_id
   key_vault_id = module.kv_shared.id
 }
+
+# Tables - only adjusted on preprod and prod
+resource "azurerm_log_analytics_workspace_table" "alert" {
+  count = var.environment_short == "p" || var.environment_short == "b" ? 1 : 0
+
+  workspace_id            = module.log_workspace_shared.id
+  name                    = "Alert"
+  retention_in_days       = 365
+  total_retention_in_days = 365
+}
+
+resource "azurerm_log_analytics_workspace_table" "azure_activity" {
+  count = var.environment_short == "p" || var.environment_short == "b" ? 1 : 0
+
+  workspace_id            = module.log_workspace_shared.id
+  name                    = "AzureActivity"
+  retention_in_days       = 183
+  total_retention_in_days = 183
+}
+
+resource "azurerm_log_analytics_workspace_table" "azure_diagnostics" {
+  count = var.environment_short == "p" || var.environment_short == "b" ? 1 : 0
+
+  workspace_id            = module.log_workspace_shared.id
+  name                    = "AzureDiagnostics"
+  retention_in_days       = 183
+  total_retention_in_days = 183
+}
+
+resource "azurerm_log_analytics_workspace_table" "azure_metrics" {
+  count = var.environment_short == "p" || var.environment_short == "b" ? 1 : 0
+
+  workspace_id            = module.log_workspace_shared.id
+  name                    = "AzureMetrics"
+  retention_in_days       = 183
+  total_retention_in_days = 183
+}
