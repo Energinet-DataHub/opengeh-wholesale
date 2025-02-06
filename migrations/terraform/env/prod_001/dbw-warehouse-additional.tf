9879411e46d6c40831a62465a6334edc2d7f039b
Jeppe M <jmw@energinet.dk>
2025-02-06 08:35:32 +0100

    Migrations/adj: Change naming and retrigger deploy (#2889)

--- migrations/terraform/env/prod_001/dbw-warehouse-additional.tf
+++ migrations/terraform/env/prod_001/dbw-warehouse-additional.tf
@@ -1,14 +1,14 @@
-resource "databricks_query" "ts_api_sql_endpoint_keep_alive" {
+resource "databricks_query" "ts_api_sql_warehouse_keep_alive" {
   provider     = databricks.dbw
   warehouse_id = databricks_sql_endpoint.ts_api_sql_endpoint.id
-  display_name = "ts_api_sql_endpoint_keep_alive"
+  display_name = "ts_api_sql_warehouse_keep_alive"
   query_text   = "SELECT 42 as value"
   parent_path  = "/Shared/Queries"
 }
 
-resource "databricks_job" "ts_api_sql_endpoint_keep_alive" {
+resource "databricks_job" "ts_api_sql_warehouse_keep_alive" {
   provider = databricks.dbw
-  name     = "ts_api_sql_endpoint_keep_alive"
+  name     = "ts_api_sql_warehouse_keep_alive"
 
   schedule {
     quartz_cron_expression = "0 0 7-16 ? * MON-FRI"
@@ -16,11 +16,11 @@ resource "databricks_job" "ts_api_sql_endpoint_keep_alive" {
   }
 
   task {
-    task_key = "ts_api_sql_endpoint_keep_alive"
+    task_key = "ts_api_sql_warehouse_keep_alive"
 
     sql_task {
       query {
-        query_id = databricks_query.ts_api_sql_endpoint_keep_alive.id
+        query_id = databricks_query.ts_api_sql_warehouse_keep_alive.id
       }
       warehouse_id = databricks_sql_endpoint.ts_api_sql_endpoint.id
     }
--- migrations/terraform/env/test_001/dbw-warehouse-additional.tf
+++ migrations/terraform/env/test_001/dbw-warehouse-additional.tf
@@ -8,17 +8,17 @@ module "kvs_mig_dbw_warehouse_id" {
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
 
-resource "databricks_query" "ts_api_sql_endpoint_keep_alive" {
+resource "databricks_query" "ts_api_sql_warehouse_keep_alive" {
   provider     = databricks.dbw
   warehouse_id = databricks_sql_endpoint.ts_api_sql_endpoint.id
-  display_name = "ts_api_sql_endpoint_keep_alive"
+  display_name = "ts_api_sql_warehouse_keep_alive"
   query_text   = "SELECT 42 as value"
   parent_path  = "/Shared/Queries"
 }
 
-resource "databricks_job" "ts_api_sql_endpoint_keep_alive" {
+resource "databricks_job" "ts_api_sql_warehouse_keep_alive" {
   provider = databricks.dbw
-  name     = "ts_api_sql_endpoint_keep_alive"
+  name     = "ts_api_sql_warehouse_keep_alive"
 
   schedule {
     quartz_cron_expression = "0 0 6-16 ? * *"
@@ -26,11 +26,11 @@ resource "databricks_job" "ts_api_sql_endpoint_keep_alive" {
   }
 
   task {
-    task_key = "ts_api_sql_endpoint_keep_alive"
+    task_key = "ts_api_sql_warehouse_keep_alive"
 
     sql_task {
       query {
-        query_id = databricks_query.ts_api_sql_endpoint_keep_alive.id
+        query_id = databricks_query.ts_api_sql_warehouse_keep_alive.id
       }
       warehouse_id = databricks_sql_endpoint.ts_api_sql_endpoint.id
     }
