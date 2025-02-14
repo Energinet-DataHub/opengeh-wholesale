9ae4c975b2a08923cc92b0e7051ea8268bedbf84
Christian Hansen <120550591+cenh-halfspace@users.noreply.github.com>
2025-02-14 12:15:57 +0100

    feat: Added Schemas for Measurements Calculated (#2947)

--- measurements/terraform/core/main/dbw-unity.tf
+++ measurements/terraform/core/main/dbw-unity.tf
@@ -124,6 +124,15 @@ resource "databricks_external_location" "measurements_calculated_internal_storag
   depends_on      = [module.dbw, module.st_measurements]
 }
 
+resource "databricks_schema" "measurements_calculated_internal" {
+  provider     = databricks.dbw
+  catalog_name = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+  name         = "measurements_calculated_internal"
+  comment      = "Measurements Calculated Internal Schema"
+  storage_root = databricks_external_location.measurements_calculated_internal_storage.url
+  depends_on = [module.dbw, module.kvs_databricks_dbw_workspace_token]
+}
+
 resource "databricks_external_location" "measurements_calculated_storage" {
   provider = databricks.dbw
   name     = "${azurerm_storage_container.measurements_calculated.name}_${module.st_measurements.name}"
@@ -132,6 +141,14 @@ resource "databricks_external_location" "measurements_calculated_storage" {
   credential_name = data.azurerm_key_vault_secret.unity_storage_credential_id.value
   comment         = "Managed by TF"
   depends_on      = [module.dbw, module.st_measurements]
+}
+
+resource "databricks_schema" "measurements_calculated" {
+  provider     = databricks.dbw
+  catalog_name = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+  name         = "measurements_calculated"
+  comment      = "Measurements Calculated Schema"
+  storage_root = databricks_external_location.measurements_calculated_storage.url
 }
 
 #
