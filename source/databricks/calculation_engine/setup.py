6acf830ff38073f8cb138cfb7758e520f4b8b904
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-02-20 09:40:44 +0100

    bump version

--- source/databricks/calculation_engine/setup.py
+++ source/databricks/calculation_engine/setup.py
@@ -38,7 +38,7 @@ setup(
         "azure-monitor-opentelemetry==1.6.4",
         "azure-core==1.32.0",
         "pytest-xdist",
-        "geh_common @ git+https://git@github.com/Energinet-DataHub/opengeh-python-packages@geh_common_4.0.0",
+        "geh_common @ git+https://git@github.com/Energinet-DataHub/opengeh-python-packages@geh_common_5.0.0",
     ],
     entry_points={
         "console_scripts": [
