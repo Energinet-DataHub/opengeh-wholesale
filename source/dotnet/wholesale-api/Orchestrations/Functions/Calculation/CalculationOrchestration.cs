56b5ecdb59a2f693c94322c743a4efa04196b302
Ebbe Knudsen <xedkn@energinet.dk>
2025-03-03 09:38:06 +0100

    Disable calculation trigger/orchestration

--- source/dotnet/wholesale-api/Orchestrations/Functions/Calculation/CalculationOrchestration.cs
+++ source/dotnet/wholesale-api/Orchestrations/Functions/Calculation/CalculationOrchestration.cs
@@ -29,162 +29,163 @@ internal class CalculationOrchestration
     public async Task<string> Run(
         [OrchestrationTrigger] TaskOrchestrationContext context)
     {
-        var input = context.GetInput<CalculationOrchestrationInput>();
-        if (input == null)
-        {
-            return "Error: No input specified.";
-        }
-
-        var defaultRetryOptions = CreateDefaultRetryOptions();
-
-        // Set instance id on calculation
-        await context.CallActivityAsync(
-            nameof(SetCalculationAsStartedActivity),
-            new SetCalculationAsStartedInput(input.CalculationId.Id, context.InstanceId),
-            defaultRetryOptions);
-
-        // Set custom calculation. This is being waited for in the ScheduledCalculationTrigger to ensure that the calculation is started.
-        var calculationMetadata = new CalculationMetadata
-        {
-            IsStarted = true,
-            Id = input.CalculationId.Id,
-            Input = input,
-            OrchestrationProgress = "OrchestrationStarted",
-        };
-        context.SetCustomStatus(calculationMetadata);
-
-        // Start calculation (Databricks)
-        calculationMetadata.JobId = await context.CallActivityAsync<CalculationJobId>(
-            nameof(StartCalculationActivity),
-            calculationMetadata.Id,
-            defaultRetryOptions);
-        calculationMetadata.OrchestrationProgress = "CalculationJobQueued";
-        context.SetCustomStatus(calculationMetadata);
-
-        var expiryTime =
-            context.CurrentUtcDateTime.AddSeconds(input.OrchestrationMonitorOptions
-                .CalculationJobStatusExpiryTimeInSeconds);
-        while (context.CurrentUtcDateTime < expiryTime)
-        {
-            // Monitor calculation (Databricks)
-            calculationMetadata.JobStatus = await context.CallActivityAsync<CalculationState>(
-                nameof(GetJobStatusActivity),
-                calculationMetadata.JobId,
-                defaultRetryOptions);
-            context.SetCustomStatus(calculationMetadata);
-
-            if (calculationMetadata.JobStatus is CalculationState.Running
-                or CalculationState.Pending
-                or CalculationState.Canceled)
-            {
-                // Update calculation execution status (SQL)
-                await context.CallActivityAsync(
-                    nameof(UpdateCalculationStateFromJobStatusActivity),
-                    calculationMetadata,
-                    defaultRetryOptions);
-
-                if (calculationMetadata.JobStatus is CalculationState.Canceled)
-                {
-                    // (Re) Start calculation (Databricks)
-                    calculationMetadata.JobId = await context.CallActivityAsync<CalculationJobId>(
-                        nameof(StartCalculationActivity),
-                        calculationMetadata.Id,
-                        defaultRetryOptions);
-                    calculationMetadata.OrchestrationProgress = "CalculationJobQueuedAgain";
-                    context.SetCustomStatus(calculationMetadata);
-                }
-
-                // Wait for the next checkpoint
-                var nextCheckpoint = context.CurrentUtcDateTime.AddSeconds(input.OrchestrationMonitorOptions
-                    .CalculationJobStatusPollingIntervalInSeconds);
-                await context.CreateTimer(nextCheckpoint, CancellationToken.None);
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        // Update calculation execution status (SQL)
-        await context.CallActivityAsync(
-            nameof(UpdateCalculationStateFromJobStatusActivity),
-            calculationMetadata,
-            defaultRetryOptions);
-
-        if (calculationMetadata.JobStatus == CalculationState.Completed)
-        {
-            calculationMetadata.OrchestrationProgress = "CalculationJobCompleted";
-            context.SetCustomStatus(calculationMetadata);
-
-            // If the calculation is internal, we don't want to send the calculation results event.
-            if (!input.IsInternalCalculation)
-            {
-                // Send calculation results (ServiceBus)
-                await context.CallActivityAsync(
-                    nameof(SendCalculationResultsActivity),
-                    new SendCalculationResultsInput(calculationMetadata.Id, context.InstanceId),
-                    defaultRetryOptions);
-
-                await UpdateCalculationOrchestrationStateAsync(
-                    context,
-                    calculationMetadata.Id,
-                    CalculationOrchestrationState.ActorMessagesEnqueuing,
-                    defaultRetryOptions);
-
-                calculationMetadata.OrchestrationProgress = "ActorMessagesEnqueuing";
-
-                context.SetCustomStatus(calculationMetadata);
-            }
-        }
-        else
-        {
-            calculationMetadata.OrchestrationProgress = "CalculationJobFailed";
-            context.SetCustomStatus(calculationMetadata);
-            return $"Error: Job status '{calculationMetadata.JobStatus}'";
-        }
-
-        // If the calculation is internal, we are not enqueueing messages to the actors
-        if (!input.IsInternalCalculation)
-        {
-            // Wait for an ActorMessagesEnqueued event to notify us that messages are ready to be consumed by actors
-            var waitForActorMessagesEnqueuedEventResult = await WaitForActorMessagesEnqueuedEventAsync(
-                context,
-                calculationMetadata.Id,
-                input.OrchestrationMonitorOptions.MessagesEnqueuingExpiryTimeInSeconds);
-            if (!waitForActorMessagesEnqueuedEventResult.IsSuccess)
-            {
-                calculationMetadata.OrchestrationProgress = waitForActorMessagesEnqueuedEventResult.ErrorSubject ??
-                                                            "UnknownWaitForActorMessagesEnqueuedEventError";
-                context.SetCustomStatus(calculationMetadata);
-                await UpdateCalculationOrchestrationStateAsync(
-                    context,
-                    calculationMetadata.Id,
-                    CalculationOrchestrationState.ActorMessagesEnqueuingFailed,
-                    defaultRetryOptions);
-                return
-                    $"Error: {waitForActorMessagesEnqueuedEventResult.ErrorDescription ?? "Unknown error waiting for actor messages enqueued event"}";
-            }
-
-            // Update state to ActorMessagesEnqueued
-            calculationMetadata.OrchestrationProgress = "ActorMessagesEnqueued";
-            context.SetCustomStatus(calculationMetadata);
-            await UpdateCalculationOrchestrationStateAsync(
-                context,
-                calculationMetadata.Id,
-                CalculationOrchestrationState.ActorMessagesEnqueued,
-                defaultRetryOptions);
-        }
-
-        // Update state to Completed
-        calculationMetadata.OrchestrationProgress = "Completed";
-        context.SetCustomStatus(calculationMetadata);
-        await UpdateCalculationOrchestrationStateAsync(
-            context,
-            calculationMetadata.Id,
-            CalculationOrchestrationState.Completed,
-            defaultRetryOptions);
-
-        return "Success";
+        throw new Exception("Calculations should no longer run in Wholesale, they should run in the Process Manager subsystem instead");
+        // var input = context.GetInput<CalculationOrchestrationInput>();
+        // if (input == null)
+        // {
+        //     return "Error: No input specified.";
+        // }
+        //
+        // var defaultRetryOptions = CreateDefaultRetryOptions();
+        //
+        // // Set instance id on calculation
+        // await context.CallActivityAsync(
+        //     nameof(SetCalculationAsStartedActivity),
+        //     new SetCalculationAsStartedInput(input.CalculationId.Id, context.InstanceId),
+        //     defaultRetryOptions);
+        //
+        // // Set custom calculation. This is being waited for in the ScheduledCalculationTrigger to ensure that the calculation is started.
+        // var calculationMetadata = new CalculationMetadata
+        // {
+        //     IsStarted = true,
+        //     Id = input.CalculationId.Id,
+        //     Input = input,
+        //     OrchestrationProgress = "OrchestrationStarted",
+        // };
+        // context.SetCustomStatus(calculationMetadata);
+        //
+        // // Start calculation (Databricks)
+        // calculationMetadata.JobId = await context.CallActivityAsync<CalculationJobId>(
+        //     nameof(StartCalculationActivity),
+        //     calculationMetadata.Id,
+        //     defaultRetryOptions);
+        // calculationMetadata.OrchestrationProgress = "CalculationJobQueued";
+        // context.SetCustomStatus(calculationMetadata);
+        //
+        // var expiryTime =
+        //     context.CurrentUtcDateTime.AddSeconds(input.OrchestrationMonitorOptions
+        //         .CalculationJobStatusExpiryTimeInSeconds);
+        // while (context.CurrentUtcDateTime < expiryTime)
+        // {
+        //     // Monitor calculation (Databricks)
+        //     calculationMetadata.JobStatus = await context.CallActivityAsync<CalculationState>(
+        //         nameof(GetJobStatusActivity),
+        //         calculationMetadata.JobId,
+        //         defaultRetryOptions);
+        //     context.SetCustomStatus(calculationMetadata);
+        //
+        //     if (calculationMetadata.JobStatus is CalculationState.Running
+        //         or CalculationState.Pending
+        //         or CalculationState.Canceled)
+        //     {
+        //         // Update calculation execution status (SQL)
+        //         await context.CallActivityAsync(
+        //             nameof(UpdateCalculationStateFromJobStatusActivity),
+        //             calculationMetadata,
+        //             defaultRetryOptions);
+        //
+        //         if (calculationMetadata.JobStatus is CalculationState.Canceled)
+        //         {
+        //             // (Re) Start calculation (Databricks)
+        //             calculationMetadata.JobId = await context.CallActivityAsync<CalculationJobId>(
+        //                 nameof(StartCalculationActivity),
+        //                 calculationMetadata.Id,
+        //                 defaultRetryOptions);
+        //             calculationMetadata.OrchestrationProgress = "CalculationJobQueuedAgain";
+        //             context.SetCustomStatus(calculationMetadata);
+        //         }
+        //
+        //         // Wait for the next checkpoint
+        //         var nextCheckpoint = context.CurrentUtcDateTime.AddSeconds(input.OrchestrationMonitorOptions
+        //             .CalculationJobStatusPollingIntervalInSeconds);
+        //         await context.CreateTimer(nextCheckpoint, CancellationToken.None);
+        //     }
+        //     else
+        //     {
+        //         break;
+        //     }
+        // }
+        //
+        // // Update calculation execution status (SQL)
+        // await context.CallActivityAsync(
+        //     nameof(UpdateCalculationStateFromJobStatusActivity),
+        //     calculationMetadata,
+        //     defaultRetryOptions);
+        //
+        // if (calculationMetadata.JobStatus == CalculationState.Completed)
+        // {
+        //     calculationMetadata.OrchestrationProgress = "CalculationJobCompleted";
+        //     context.SetCustomStatus(calculationMetadata);
+        //
+        //     // If the calculation is internal, we don't want to send the calculation results event.
+        //     if (!input.IsInternalCalculation)
+        //     {
+        //         // Send calculation results (ServiceBus)
+        //         await context.CallActivityAsync(
+        //             nameof(SendCalculationResultsActivity),
+        //             new SendCalculationResultsInput(calculationMetadata.Id, context.InstanceId),
+        //             defaultRetryOptions);
+        //
+        //         await UpdateCalculationOrchestrationStateAsync(
+        //             context,
+        //             calculationMetadata.Id,
+        //             CalculationOrchestrationState.ActorMessagesEnqueuing,
+        //             defaultRetryOptions);
+        //
+        //         calculationMetadata.OrchestrationProgress = "ActorMessagesEnqueuing";
+        //
+        //         context.SetCustomStatus(calculationMetadata);
+        //     }
+        // }
+        // else
+        // {
+        //     calculationMetadata.OrchestrationProgress = "CalculationJobFailed";
+        //     context.SetCustomStatus(calculationMetadata);
+        //     return $"Error: Job status '{calculationMetadata.JobStatus}'";
+        // }
+        //
+        // // If the calculation is internal, we are not enqueueing messages to the actors
+        // if (!input.IsInternalCalculation)
+        // {
+        //     // Wait for an ActorMessagesEnqueued event to notify us that messages are ready to be consumed by actors
+        //     var waitForActorMessagesEnqueuedEventResult = await WaitForActorMessagesEnqueuedEventAsync(
+        //         context,
+        //         calculationMetadata.Id,
+        //         input.OrchestrationMonitorOptions.MessagesEnqueuingExpiryTimeInSeconds);
+        //     if (!waitForActorMessagesEnqueuedEventResult.IsSuccess)
+        //     {
+        //         calculationMetadata.OrchestrationProgress = waitForActorMessagesEnqueuedEventResult.ErrorSubject ??
+        //                                                     "UnknownWaitForActorMessagesEnqueuedEventError";
+        //         context.SetCustomStatus(calculationMetadata);
+        //         await UpdateCalculationOrchestrationStateAsync(
+        //             context,
+        //             calculationMetadata.Id,
+        //             CalculationOrchestrationState.ActorMessagesEnqueuingFailed,
+        //             defaultRetryOptions);
+        //         return
+        //             $"Error: {waitForActorMessagesEnqueuedEventResult.ErrorDescription ?? "Unknown error waiting for actor messages enqueued event"}";
+        //     }
+        //
+        //     // Update state to ActorMessagesEnqueued
+        //     calculationMetadata.OrchestrationProgress = "ActorMessagesEnqueued";
+        //     context.SetCustomStatus(calculationMetadata);
+        //     await UpdateCalculationOrchestrationStateAsync(
+        //         context,
+        //         calculationMetadata.Id,
+        //         CalculationOrchestrationState.ActorMessagesEnqueued,
+        //         defaultRetryOptions);
+        // }
+        //
+        // // Update state to Completed
+        // calculationMetadata.OrchestrationProgress = "Completed";
+        // context.SetCustomStatus(calculationMetadata);
+        // await UpdateCalculationOrchestrationStateAsync(
+        //     context,
+        //     calculationMetadata.Id,
+        //     CalculationOrchestrationState.Completed,
+        //     defaultRetryOptions);
+        //
+        // return "Success";
     }
 
     private static async Task UpdateCalculationOrchestrationStateAsync(
