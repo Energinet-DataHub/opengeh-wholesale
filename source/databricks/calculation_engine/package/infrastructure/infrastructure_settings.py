ff33b7cb6a2d5f68c758124cf90b9e06e1da77da
Thomas Erland Clausen <twotcl@inox.dk>
2025-03-03 13:41:20 +0000

    fix infrastructuresettings

--- source/databricks/calculation_engine/package/infrastructure/infrastructure_settings.py
+++ source/databricks/calculation_engine/package/infrastructure/infrastructure_settings.py
@@ -18,8 +18,10 @@ from pydantic import AliasChoices, Field
 from package.infrastructure import paths
 from geh_common.application.settings import ApplicationSettings
 
+from typing import Any
 
-class InfrastructureSettings(ApplicationSettings, cli_kebab_case=False):
+
+class InfrastructureSettings(ApplicationSettings, cli_kebab_case=False):  # type: ignore
     """
     InfrastructureSettings class uses Pydantic BaseSettings to configure and validate parameters.
     Parameters can come from both runtime (CLI) or from environment variables.
@@ -30,9 +32,9 @@ class InfrastructureSettings(ApplicationSettings, cli_kebab_case=False):
     calculation_input_database_name: str  # From ENVIRONMENT
     data_storage_account_name: str  # From ENVIRONMENT
     # Prevent the credentials from being printed or logged (using e.g., print() or repr())
-    tenant_id: str = Field(repr=False, default="foo")  # Ud? #From ENVIRONMENT
-    spn_app_id: str = Field(repr=False, default="foo")  # Ud? #From ENVIRONMENT
-    spn_app_secret: str = Field(repr=False, default="foo")  # Ud? #From ENVIRONMENT
+    tenant_id: str = Field(repr=False, init=False)  # Ud? #From ENVIRONMENT
+    spn_app_id: str = Field(repr=False, init=False)  # Ud? #From ENVIRONMENT
+    spn_app_secret: str = Field(repr=False, init=False)  # Ud? #From ENVIRONMENT
 
     calculation_input_folder_name: str | None = Field(
         default=None,
@@ -68,7 +70,7 @@ class InfrastructureSettings(ApplicationSettings, cli_kebab_case=False):
     wholesale_container_path: str | None = Field(default=None)
     calculation_input_path: str | None = Field(default=None)
 
-    def model_post_init(self, __context) -> None:
+    def model_post_init(self, __context: Any) -> None:
         self.data_storage_account_credentials = (
             self.data_storage_account_credentials
             or ClientSecretCredential(
