72c6cdcfedc16babed7a944413604d7a39afd64c
Alex <77341673+defectiveAi@users.noreply.github.com>
2025-03-07 09:09:55 +0100

    Misspelled kv name in config. (#3096)

--- electricity-market/terraform/env/prod_001/func-mp-import-df.config_override.tf
+++ electricity-market/terraform/env/prod_001/func-mp-import-df.config_override.tf
@@ -2,6 +2,6 @@ module "func_mp_import_df" {
   app_settings = merge(local.func_mp_import_df.app_settings, {
     "Databricks:WorkspaceUrl"   = "@Microsoft.KeyVault(VaultName=${module.kv_internal.name};SecretName=dbw-workspace-url)"
     "Databricks:WorkspaceToken" = "@Microsoft.KeyVault(VaultName=${module.kv_internal.name};SecretName=dbw-workspace-token)"
-    "Databricks:WarehouseId"    = "@Microsoft.KeyVault(VaultName=${module.kv_internal.name};SecretName=dbw-electricity-market-warehouse-id)"
+    "Databricks:WarehouseId"    = "@Microsoft.KeyVault(VaultName=${module.kv_internal.name};SecretName=electricity-market-warehouse-id)"
   })
 }
