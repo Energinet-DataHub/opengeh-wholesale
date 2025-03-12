4c3bccfd021bf798ca67c64d342c3180de4106cb
Alexander <122119488+energinet-ajw@users.noreply.github.com>
2025-03-12 15:19:05 +0100

    Create dbwj-missing-measurements-log.tf (#3130)

---
+++ measurements/terraform/calculated-measurements/main/dbwj-missing-measurements-log.tf
@@ -0,0 +1,66 @@
+resource "databricks_job" "missing_measurements_log" {
+  provider            = databricks.dbw
+  name                = "MissingMeasurementsLog"
+  max_concurrent_runs = 1
+
+  job_cluster {
+    job_cluster_key = "missing_measurements_log_cluster"
+
+    new_cluster {
+      spark_version  = local.spark_version
+      node_type_id   = "Standard_D8as_v4"
+      runtime_engine = "PHOTON"
+      autoscale {
+        min_workers = 1
+        max_workers = 10
+      }
+      spark_env_vars = {
+        "TIME_ZONE"                             = local.TIME_ZONE
+        "CATALOG_NAME"                          = data.azurerm_key_vault_secret.shared_unity_catalog_name.value
+        "APPLICATIONINSIGHTS_CONNECTION_STRING" = data.azurerm_key_vault_secret.appi_shared_connection_string.value
+        "ELECTRICITY_MARKET_DATA_PATH"          = "/Volumes/${data.azurerm_key_vault_secret.shared_unity_catalog_name.value}/measurements_internal/shared_electricity_market_electrical_heating_container"
+      }
+    }
+  }
+
+  task {
+    task_key        = "missing_measurements_log_task"
+    max_retries     = 1
+    job_cluster_key = "missing_measurements_log_cluster"
+
+    library {
+      whl = "/Workspace/Shared/PythonWheels/geh_calculated_measurements/geh_calculated_measurements-0.1.0-py3-none-any.whl"
+    }
+
+    python_wheel_task {
+      package_name = "geh_calculated_measurements"
+      # The entry point is defined in setup.py
+      entry_point = "execute_missing_measurements_log"
+    }
+  }
+
+  email_notifications {
+    no_alert_for_skipped_runs = false
+  }
+
+  queue {
+    enabled = false
+  }
+}
+
+resource "databricks_permissions" "missing_measurements_log" {
+  provider = databricks.dbw
+  job_id   = databricks_job.missing_measurements_log.id
+
+  access_control {
+    group_name       = var.databricks_contributor_dataplane_group.name
+    permission_level = "CAN_MANAGE"
+  }
+  dynamic "access_control" {
+    for_each = local.readers
+    content {
+      group_name       = access_control.key
+      permission_level = "CAN_VIEW"
+    }
+  }
+}
