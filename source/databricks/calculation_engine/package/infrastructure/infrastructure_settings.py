f3c6b8b998a54a9dfe1e6cd6bec41a0d5cafe334
Jens Peder Meldgaard <jpm@halfspace.io>
2025-03-13 07:40:24 +0100

    chore: remove unused properties from infrastructure settings
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/databricks/calculation_engine/package/infrastructure/infrastructure_settings.py
+++ source/databricks/calculation_engine/package/infrastructure/infrastructure_settings.py
@@ -12,85 +12,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from azure.identity import ClientSecretCredential
-from pydantic import AliasChoices, Field
-
-from package.infrastructure import paths
-from geh_common.application.settings import ApplicationSettings
-
-from typing import Any
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class InfrastructureSettings(ApplicationSettings, cli_kebab_case=False):  # type: ignore
+class InfrastructureSettings(BaseSettings):  # type: ignore
     """
     InfrastructureSettings class uses Pydantic BaseSettings to configure and validate parameters.
     Parameters can come from both runtime (CLI) or from environment variables.
     The priority is CLI parameters first and then environment variables.
     """
 
-    catalog_name: str = Field(init=False)  # From ENVIRONMENT
-    calculation_input_database_name: str = Field(init=False)  # From ENVIRONMENT
-    data_storage_account_name: str = Field(init=False)  # From ENVIRONMENT
-    # Prevent the credentials from being printed or logged (using e.g., print() or repr())
-    tenant_id: str = Field(repr=False, init=False)  # Ud? #From ENVIRONMENT
-    spn_app_id: str = Field(repr=False, init=False)  # Ud? #From ENVIRONMENT
-    spn_app_secret: str = Field(repr=False, init=False)  # Ud? #From ENVIRONMENT
-
-    calculation_input_folder_name: str | None = Field(
-        init=False,
-        default=None,
-        validation_alias=AliasChoices(
-            "calculation_input_folder_name", "calculation-input-folder-name"
-        ),
-    )  # Ud? From CLI/ENVIRONMENT
-
-    time_series_points_table_name: str | None = Field(
-        init=False,
-        default=None,
-        validation_alias=AliasChoices(
-            "time_series_points_table_name", "time-series-points-table-name"
-        ),
-    )  # From CLI
-    metering_point_periods_table_name: str | None = Field(
-        init=False,
-        default=None,
-        validation_alias=AliasChoices(
-            "metering_point_periods_table_name", "metering-point-periods-table-name"
-        ),
-    )  # From CLI
-    grid_loss_metering_point_ids_table_name: str | None = Field(
-        init=False,
-        default=None,
-        validation_alias=AliasChoices(
-            "grid_loss_metering_points_table_name",
-            "grid-loss-metering-points-table-name",
-            "grid_loss_metering_point_ids_table_name",
-        ),
-    )  # From CLI
-
-    data_storage_account_credentials: ClientSecretCredential | None = Field(
-        default=None
+    model_config = SettingsConfigDict(
+        cli_prog_name="infrastructure",
+        cli_parse_args=True,
+        cli_kebab_case=True,
+        cli_ignore_unknown_args=True,
+        cli_implicit_flags=True,
     )
-    wholesale_container_path: str | None = Field(default=None)
-    calculation_input_path: str | None = Field(default=None)
 
-    def model_post_init(self, __context: Any) -> None:
-        self.data_storage_account_credentials = (
-            self.data_storage_account_credentials
-            or ClientSecretCredential(
-                tenant_id=self.tenant_id,
-                client_id=self.spn_app_id,
-                client_secret=self.spn_app_secret,
-            )
-        )
-        self.wholesale_container_path = (
-            self.wholesale_container_path
-            or paths.get_container_root_path(str(self.data_storage_account_name))
-        )
-        self.calculation_input_path = (
-            self.calculation_input_path
-            or paths.get_calculation_input_path(
-                str(self.data_storage_account_name),
-                str(self.calculation_input_folder_name),
-            )
-        )
+    catalog_name: str = Field(init=False)
+    calculation_input_database_name: str = Field(init=False)
+    data_storage_account_name: str = Field(init=False)
+
+    # (repr=False) prevents the field from being printed in the repr of the model
+    tenant_id: str = Field(init=False, repr=False)
+    spn_app_id: str = Field(init=False, repr=False)
+    spn_app_secret: str = Field(init=False, repr=False)
+
+    calculation_input_folder_name: str | None = Field(init=False, default=None)
+    time_series_points_table_name: str | None = Field(init=False, default=None)
+    metering_point_periods_table_name: str | None = Field(init=False, default=None)
+    grid_loss_metering_point_ids_table_name: str | None = Field(
+        init=False, default=None
+    )
