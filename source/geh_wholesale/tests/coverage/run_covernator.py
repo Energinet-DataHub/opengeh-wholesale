01ae00df09368eb14b37d04ecda40c5a7ac4a732
Felix Achter <fa@halfspace.io>
2025-03-21 10:24:37 +0100

    import using package name of covernator

--- source/geh_wholesale/tests/coverage/run_covernator.py
+++ source/geh_wholesale/tests/coverage/run_covernator.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from geh_common.testing.covernator.commands import create_all_cases_file, create_result_and_all_scenario_files
+from geh_common.testing.covernator import create_all_cases_file, create_result_and_all_scenario_files
 
 output_folder = Path("/workspace/source/geh_wholesale/run_outputs")
 create_all_cases_file(
