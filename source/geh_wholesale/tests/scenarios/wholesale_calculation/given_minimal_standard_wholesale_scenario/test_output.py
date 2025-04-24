10060aaafaecd7252935aad00dc704e8d6526140
Soren <XSSFR@energinet.dk>
2025-04-24 08:40:37 +0000

    test

--- source/geh_wholesale/tests/scenarios/wholesale_calculation/given_minimal_standard_wholesale_scenario/test_output.py
+++ source/geh_wholesale/tests/scenarios/wholesale_calculation/given_minimal_standard_wholesale_scenario/test_output.py
@@ -4,16 +4,19 @@ from geh_common.testing.dataframes import (
     assert_dataframes_and_schemas,
 )
 from geh_common.testing.scenario_testing import TestCase, get_then_names
+from pyspark.sql import SparkSession
 
 
 @pytest.mark.parametrize("name", get_then_names())
 def test__equals_expected(
     test_cases: TestCase,
     name: str,
+    spark: SparkSession,
     assert_dataframes_configuration: AssertDataframesConfiguration,
 ) -> None:
-    test_case = test_cases[name]
+    spark.catalog.clearCache()
 
+    test_case = test_cases[name]
     assert_dataframes_and_schemas(
         actual=test_case.actual,
         expected=test_case.expected,
