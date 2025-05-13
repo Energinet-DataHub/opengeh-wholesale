d2a7e758ad8735d9dafb531166f8504b68b52adf
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-13 13:27:09 +0200

    chore: all tables must be clustered
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_wholesale/src/geh_wholesale/datamigration/migration_scripts/202505121319_alter_table__internal_results__grid_loss_metering_point_time_series.sql
+++ source/geh_wholesale/src/geh_wholesale/datamigration/migration_scripts/202505121319_alter_table__internal_results__grid_loss_metering_point_time_series.sql
@@ -1,2 +1,4 @@
+ALTER TABLE {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.grid_loss_metering_points
+CLUSTER BY (calculation_id)
 ALTER TABLE {CATALOG_NAME}.{WHOLESALE_RESULTS_INTERNAL_DATABASE_NAME}.grid_loss_metering_point_time_series
 CLUSTER BY (calculation_id, grid_area_code, time)
