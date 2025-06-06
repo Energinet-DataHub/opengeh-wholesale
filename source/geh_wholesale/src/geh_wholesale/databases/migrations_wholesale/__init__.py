deacb390ece0709e3c953a8c1b97e3151f629b5f
Alexander <alexanderharrow@hotmail.com>
2025-06-06 13:51:02 +0200

    Update __init__.py

--- source/geh_wholesale/src/geh_wholesale/databases/migrations_wholesale/__init__.py
+++ source/geh_wholesale/src/geh_wholesale/databases/migrations_wholesale/__init__.py
@@ -6,5 +6,4 @@ Only minor transformations to data frames are done merely compensating
 for minor inappropriatenesses in the input table formats.
 """
 
-from .measurements_repository import MeasurementsRepository as MeasurementsRepository
 from .repository import MigrationsWholesaleRepository as MigrationsWholesaleRepository
