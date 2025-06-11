0c093fb47a583b7e448feed23dd403ce41655666
Alexander <alexanderharrow@hotmail.com>
2025-06-11 08:42:23 +0200

    Updates

--- source/geh_wholesale/src/geh_wholesale/databases/feature_flag_manager.py
+++ source/geh_wholesale/src/geh_wholesale/databases/feature_flag_manager.py
@@ -1,28 +1,11 @@
-# Copyright 2020 Energinet DataHub A/S
-#
-# Licensed under the Apache License, Version 2.0 (the "License2");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-# This class contains all relevant feature flag management logic for the wholesale calculation.
-# The bussiness uses the term release toggle to describe feature flags.
-# The actual feature flags can be found in Azure -> App Configuration -> Operations -> Feature manager.
-
-
 from azure.appconfiguration.provider import WatchKey, load
 from azure.identity import ClientSecretCredential
 from featuremanagement import FeatureManager
 
 
+# This class contains all relevant feature flag management logic for the wholesale calculation.
+# The bussiness uses the term release toggle to describe feature flags.
+# The actual feature flags can be found in Azure -> App Configuration -> Operations -> Feature manager.
 class FeatureManagerFactory:
     def __init__(self, client_secret_credential: ClientSecretCredential, feature_manager_endpoint: str) -> None:
         self._client_secret_credential = client_secret_credential
